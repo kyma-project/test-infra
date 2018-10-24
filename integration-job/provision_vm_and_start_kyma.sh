ed5e4964e8265c5c513ce20eca3d9bcd4a659936
I332735 <suleyman.akbas@sap.com>
2018-10-24 16:10:48 +0200

    Apply feedback

--- integration-job/provision_vm_and_start_kyma.sh
+++ integration-job/provision_vm_and_start_kyma.sh
@@ -20,12 +20,13 @@ RANDOM_ID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
 echo "Creating a new instance named kyma-integration-test-${RANDOM_ID}..."
 gcloud compute instances create kyma-integration-test-${RANDOM_ID} \
     --metadata enable-oslogin=TRUE --image debian-9-stretch-v20181009 \
-    --image-project debian-cloud --machine-type n1-standard-4 --boot-disk-size 20
+    --image-project debian-cloud --machine-type n1-standard-4 --boot-disk-size 20 \
+    --labels pull-number=$PULL_NUMBER,job-name=kyma-integration
 
 trap cleanup exit
 
 echo "Copying the installation script to the instance..."
-gcloud compute scp --quiet install.sh kyma-integration-test-${RANDOM_ID}:~/install.sh 
+gcloud compute scp --quiet install_deps_and_run_kyma.sh kyma-integration-test-${RANDOM_ID}:~/install_deps_and_run_kyma.sh
 
 echo "Triggering the installation script..."
-gcloud compute ssh --quiet kyma-integration-test-${RANDOM_ID} -- ./install.sh --repo-owner $REPO_OWNER --branch $PULL_BASE_REF --pr-number $PULL_NUMBER
+gcloud compute ssh --quiet kyma-integration-test-${RANDOM_ID} -- ./install_deps_and_run_kyma.sh --repo-owner $REPO_OWNER --branch $PULL_BASE_REF --pr-number $PULL_NUMBER
--- prow-configurations/config.yaml.tpl
+++ prow-configurations/config.yaml.tpl
@@ -2,6 +2,18 @@ triggers:
 - repos:
   - {{ .OrganizationOrUser }}/kyma
 
+presets:
+- labels:
+    preset-service-account: "true"
+  volumes:
+  - name: service
+    secret:
+      secretName: gc-service-account
+  volumeMounts:
+  - name: service
+    mountPath: /var/run/secret/cloud.google.com
+    readOnly: true
+
 presubmits: # runs on PRs
   {{ .OrganizationOrUser }}/kyma:
   - name: prow/components/ui-api-layer
@@ -12,6 +24,16 @@ presubmits: # runs on PRs
       containers:
       - image: alpine
         command: ["/bin/printenv"]
+  - name: kyma-integration
+    trigger: "(?m)^/test kyma-integration"
+    rerun_command: "/test kyma-integration"
+    context: kyma-integration
+    skip_report: false # from documentation: SkipReport skips commenting and setting status on GitHub.
+    labels:
+      preset-service-account: "true"
+    spec:
+      containers:
+      - image: eu.gcr.io/kyma-project/snapshot/test/integration:0.0.1 # created by running `docker build -t <image> .` in the integration-job directory.
 
 plank:
   allow_cancellations: true # AllowCancellations enables aborting presubmit jobs for commits that have been superseded by newer commits in Github pull requests.
