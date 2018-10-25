962688807aab3b2d70657b3cb86132b3aa4f2542
Suleyman Akbas <suleymanakbas91@gmail.com>
2018-10-25 14:14:52 +0200

    Run integration tests on vm (#44)
    * Add integration job running tests on vm triggered by prow

---
+++ integration-job/provision_vm_and_start_kyma.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -o errexit
+
+cleanup() {
+    ARG=$?
+    echo "Removing instance kyma-integration-test-${RANDOM_ID}..."
+    gcloud compute instances delete kyma-integration-test-${RANDOM_ID}
+    exit $ARG
+}
+
+echo "Authenticating to Google Cloud..."
+export CLOUDSDK_COMPUTE_ZONE="europe-west3-a"
+export CLOUDSDK_COMPUTE_REGION="europe-west3"
+gcloud config set project kyma-project
+gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
+
+RANDOM_ID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
+
+LABELS=""
+if [[ -z "${PULL_NUMBER}" ]]; then
+    LABELS="--labels branch=$PULL_BASE_REF,job-name=kyma-integration"
+else
+    LABELS="--labels pull-number=$PULL_NUMBER,job-name=kyma-integration"
+fi
+
+echo "Creating a new instance named kyma-integration-test-${RANDOM_ID}..."
+gcloud compute instances create kyma-integration-test-${RANDOM_ID} \
+    --metadata enable-oslogin=TRUE --image debian-9-stretch-v20181009 \
+    --image-project debian-cloud --machine-type n1-standard-4 --boot-disk-size 20 $LABELS
+
+trap cleanup exit
+
+echo "Copying the installation script to the instance..."
+gcloud compute scp --quiet install_deps_and_run_kyma.sh kyma-integration-test-${RANDOM_ID}:~/install_deps_and_run_kyma.sh
+
+echo "Triggering the installation script..."
+gcloud compute ssh --quiet kyma-integration-test-${RANDOM_ID} -- ./install_deps_and_run_kyma.sh --repo-owner $REPO_OWNER --branch $PULL_BASE_REF --pr-number $PULL_NUMBER
--- prow-configurations/config.yaml.tpl
+++ prow-configurations/config.yaml.tpl
@@ -2,6 +2,21 @@ triggers:
 - repos:
   - {{ .OrganizationOrUser }}/kyma
 
+presets:
+- labels:
+    preset-compute-service-account: "true" # Service account with "Compute Admin" and "Compute OS Admin Login" roles
+  env:
+    - name: GOOGLE_APPLICATION_CREDENTIALS
+      value: /etc/service-account/compute-service-account.json
+  volumes:
+  - name: compute-service-account
+    secret:
+      secretName: compute-service-account
+  volumeMounts:
+  - name: compute-service-account
+    mountPath: /etc/service-account
+    readOnly: true
+
 presubmits: # runs on PRs
   {{ .OrganizationOrUser }}/kyma:
   - name: prow/components/ui-api-layer
@@ -12,6 +27,17 @@ presubmits: # runs on PRs
       containers:
       - image: alpine
         command: ["/bin/printenv"]
+  - name: kyma-integration
+    trigger: "(?m)^/test kyma-integration"
+    rerun_command: "/test kyma-integration"
+    context: kyma-integration
+    skip_report: false # from documentation: SkipReport skips commenting and setting status on GitHub.
+    max_concurrency: 10
+    labels:
+      preset-compute-service-account: "true"
+    spec:
+      containers:
+      - image: eu.gcr.io/kyma-project/snapshot/test/integration:0.0.1 # created by running `docker build -t <image> .` in the integration-job directory.
 
 plank:
   allow_cancellations: true # AllowCancellations enables aborting presubmit jobs for commits that have been superseded by newer commits in Github pull requests.
