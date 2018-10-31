c83c5758b75f229b0c84322f1c190007ddabf840
Krystian Cieślik <crabtree@users.noreply.github.com>
2018-10-31 09:09:49 +0100

    Strategy for organising jobs (#76)

---
+++ development/update-plugins.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DEVELOPMENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+usage () {
+    echo "Usage: \$ ${BASH_SOURCE[1]} /path/to/plugins.yaml"
+    exit 1
+}
+
+PLUGINS_PATH=$1
+if [[ -z "${PLUGINS_PATH}" ]]; then
+    usage
+fi
+
+kubectl create configmap plugins --from-file=plugins.yaml=${PLUGINS_PATH} --dry-run -o yaml | kubectl replace configmap plugins -f -
