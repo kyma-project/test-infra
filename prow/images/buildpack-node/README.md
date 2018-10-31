4a9d00e9527245c13250a94276be3a69b8a06cff
Michał Hudy <26445970+michal-hudy@users.noreply.github.com>
2018-10-31 13:36:51 +0100

    Add prow presubmit configuration for building components (#81)

---
+++ prow/images/buildpack-node/README.md
@@ -0,0 +1,21 @@
+# Buildpack Node Docker Image
+
+## Overview
+
+This project includes Buildpack Node image that is based on Bootstrap image. It can be used for building node components.
+
+This image contains:
+
+- nodejs
+- eslint
+- tslint
+- prettier
+- whitesource
+
+## Build
+
+To build the Docker image, run this command:
+
+```bash
+docker build buildpack-node .
+```
---
+++ prow/scripts/pipeline.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+trap popd EXIT
+
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# shellcheck disable=SC1090
+source "${SCRIPT_DIR}/library.sh"
+
+if [[ -z "${SOURCES_DIR}" ]]; then
+    echo "Missing SOURCES_DIR variable"
+    exit 1
+fi
+
+init
+
+pushd "${SOURCES_DIR}"
+
+if [[ "${BUILD_TYPE}" == "pr" ]]; then
+    make ci-pr
+elif [[ "${BUILD_TYPE}" == "master" ]]; then
+    # TODO: Add support for release pipeline
+    make ci-master
+else
+    echo "Not supported build type - ${BUILD_TYPE}"
+    exit 1
+fi
