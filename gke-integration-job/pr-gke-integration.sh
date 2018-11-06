ea5c11004f98e505b10dbb629c78f892969fd1a3
Jakub Kabza <jakub.kabza@sap.com>
2018-11-06 17:41:16 +0100

    test

--- gke-integration-job/pr-gke-integration.sh
+++ gke-integration-job/pr-gke-integration.sh
@@ -6,13 +6,12 @@ set -o errexit
 # REPO_OWNER, REPO_NAME and PULL_NUMBER are set by ProwJob #
 ############################################################
 
-set -o errexit
-
 ROOT_PATH="/home/prow/go/src/github.com/kyma-project/kyma"
 
 echo "
 ################################################################################
-# Provisioning gke cluster
+# Provisioning GKE cluster
 ################################################################################
 "
-env CLUSTER_NAME="${REPO_OWNER}-${REPO_NAME}-${PULL_NUMBER}" ${ROOT_PATH}/prow/scripts/provision-gke-cluster.sh
+
+CLUSTER_NAME=${REPO_OWNER}-${REPO_NAME}-${PULL_NUMBER} bash ${ROOT_PATH}/prow/scripts/provision-gke-cluster.sh
