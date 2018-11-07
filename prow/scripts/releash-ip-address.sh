535e30c07b04f922a598229d2873b3a8bdff7d62
Karol Furgol <furgolkarol@gmail.com>
2018-11-07 11:02:13 +0100

    add script that releash ip address

---
+++ prow/scripts/releash-ip-address.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+if [ -z "$GCLOUD_IP_ADDRESS_NAME" ]; then
+    echo "\$GCLOUD_IP_ADDRESS_NAME is empty"
+    exit 1
+fi
+
+if [ -z "$GCLOUD_REGION" ]; then
+    echo "\$GCLOUD_REGION is empty"
+    exit 1
+fi
+
+gcloud compute addresses delete ${GCLOUD_IP_ADDRESS_NAME} --region ${GCLOUD_REGION} --quiet
