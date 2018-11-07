9ec4c2eefc8f63abfa4518ad63bc3de96a5fed5a
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2018-11-07 10:13:49 +0100

    Update pr-gke-integration.sh

--- gke-integration-job/pr-gke-integration.sh
+++ gke-integration-job/pr-gke-integration.sh
@@ -6,12 +6,38 @@ set -o errexit
 # REPO_OWNER, REPO_NAME and PULL_NUMBER are set by ProwJob #
 ############################################################
 
-ROOT_PATH="/home/prow/go/src/github.com/kyma-project/kyma"
+discoverUnsetVar=false
+
+for var in REPO_OWNER REPO_NAME PULL_NUMBER; do
+    if [ -z "${!var}" ] ; then
+        echo "ERROR: $var is not set"
+        discoverUnsetVar=true
+    fi
+done
+if [ "${discoverUnsetVar}" = true ] ; then
+    exit 1
+fi
+
+SOURCES_DIR="/home/prow/go/src/github.com/kyma-project"
+KYMA_SOURCES_DIR="${SOURCES_DIR}/kyma"
+
+CLUSTER_NAME="${REPO_OWNER}-${REPO_NAME}-${PULL_NUMBER}"
 
 echo "
 ################################################################################
-# Provisioning GKE cluster
+# Provisioning cluster: \"${CLUSTER_NAME}\"
+################################################################################
+"
+ 
+${KYMA_SOURCES_DIR}/prow/scripts/provision-gke-cluster.sh
+
+echo "MOCK: Installing Kyma, testing, etc..."
+sleep 10
+
+echo "
+################################################################################
+# Deprovisioning cluster: \"${CLUSTER_NAME}\"
 ################################################################################
 "
 
-CLUSTER_NAME="${REPO_OWNER}-${REPO_NAME}-${PULL_NUMBER}" bash ${ROOT_PATH}/prow/scripts/provision-gke-cluster.sh
+${KYMA_SOURCES_DIR}/prow/scripts/deprovision-gke-cluster.sh
