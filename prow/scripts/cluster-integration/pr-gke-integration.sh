6fa75fe7fbe50c33daa2732527fa3dfa4c2efda6
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2018-11-09 12:08:14 +0100

    Provision and clean-up all external resources

--- prow/scripts/cluster-integration/pr-gke-integration.sh
+++ prow/scripts/cluster-integration/pr-gke-integration.sh
@@ -26,42 +26,67 @@ cleanup() {
     #Turn off exit-on-error so that next step is executed even if previous one fails.
     set +e
 
-    echo "
-################################################################################
-# Deprovisioning cluster: \"${CLUSTER_NAME}\"
-################################################################################
-"
-    bash "${KYMA_SOURCES_DIR}/prow/scripts/deprovision-gke-cluster.sh"
+    if [ -n "${CLEANUP_CLUSTER}" ]; then
+      echo "################################################################################"
+      echo "# Deprovisioning cluster: \"${CLUSTER_NAME}\""
+      echo "################################################################################"
+      "${KYMA_SOURCES_DIR}/prow/scripts/deprovision-gke-cluster.sh"
+    fi
 
+    if [ -n "${CLEANUP_DNS_ENTRY}" ]; then
+      echo "################################################################################"
+      echo "# Remove DNS Entry"
+      echo "################################################################################"
+      #TODO: script!
+      ${TEST_INFRA_SOURCES_DIR}/prow/scripts/cluster-integration/remove-dns-entry.sh
+    fi
 
-    #TODO: Add more deprovisioning steps here
+    if [ -n "${CLEANUP_IP_ADDRESS}" ]; then
+      echo "################################################################################"
+      echo "# Remove IP Address"
+      echo "################################################################################"
+      #TODO: script!
+      ${TEST_INFRA_SOURCES_DIR}/prow/scripts/cluster-integration/remove-ip-address.sh
+    fi
 
-    echo "
-################################################################################
-# Job is finished
-################################################################################
-"
+    echo "################################################################################"
+    echo "# Job is finished "
+    echo "################################################################################"
     set -e
 }
 
 #TODO: Externalize this path somewhere (Job Config?)
 SOURCES_DIR="/home/prow/go/src/github.com/kyma-project"
+TEST_INFRA_SOURCES_DIR="${SOURCES_DIR}/test-infra"
 KYMA_SOURCES_DIR="${SOURCES_DIR}/kyma"
 
+echo "################################################################################"
+echo "# Reserve IP Address"
+echo "################################################################################"
+#TODO: script!
+${TEST_INFRA_SOURCES_DIR}/prow/scripts/cluster-integration/reserve-ip-address.sh
+CLEANUP_IP_ADDRESS="true"
+
+
+echo "################################################################################"
+echo "# Add DNS Entry"
+echo "################################################################################"
+#TODO: script!
+${TEST_INFRA_SOURCES_DIR}/prow/scripts/cluster-integration/setup-dns-entry.sh
+CLEANUP_DNS_ENTRY="true"
+
+
+echo "################################################################################"
+echo "# Provisioning cluster: \"${CLUSTER_NAME}\""
+echo "################################################################################"
 export CLUSTER_NAME="${REPO_OWNER}-${REPO_NAME}-${PULL_NUMBER}"
-
-echo "
-################################################################################
-# Provisioning cluster: \"${CLUSTER_NAME}\"
-################################################################################
-"
-
 ${KYMA_SOURCES_DIR}/prow/scripts/provision-gke-cluster.sh
+CLEANUP_CLUSTER="true"
 
-echo "
-################################################################################
-# MOCK: Installing Kyma, testing, etc...
-################################################################################
-"
+
+echo "################################################################################"
+echo "# MOCK: Installing Kyma, testing, etc..."
+echo "################################################################################"
+
 sleep 60
 
