4e1e6950545cc798693bec97f8d60a49d5363a03
Michał Hudy <26445970+michal-hudy@users.noreply.github.com>
2018-11-14 00:12:37 +0100

    Add bash scripts validation (#101)

--- development/check.sh
+++ development/check.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-DEVELOPMENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+set -e
+set -o pipefail
+
+readonly DEVELOPMENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 usage () {
     echo "Usage: \$ ${BASH_SOURCE[1]} /path/to/plugins.yaml /path/to/config.yaml"
     exit 1
 }
 
-PLUGINS_PATH=$1
-CONFIG_PATH=$2
+readonly PLUGINS_PATH=$1
+readonly CONFIG_PATH=$2
 
 if [[ -z "${PLUGINS_PATH}" ]] || [[ -z "${CONFIG_PATH}" ]]; then
     usage
@@ -17,13 +20,13 @@ fi
 echo "Checking plugin configuration from '${PLUGINS_PATH}' and prow configuration from '${CONFIG_PATH}'"
 
 vendoredChecker="${DEVELOPMENT_DIR}/checker/vendor/k8s.io/test-infra/prow/cmd/checkconfig/main.go"
-if [ ! -f ${vendoredChecker} ]; then
+if [ ! -f "${vendoredChecker}" ]; then
     echo "Vendoring 'k8s.io/test-infra/prow/cmd/checkconfig'"
     cd "${DEVELOPMENT_DIR}/checker"
     dep ensure -v -vendor-only
 fi
 
-go run ${vendoredChecker} --plugin-config=${PLUGINS_PATH} --config-path=${CONFIG_PATH}
+go run "${vendoredChecker}" --plugin-config="${PLUGINS_PATH}" --config-path="${CONFIG_PATH}"
 status=$?
 
 if [ ${status} -ne 0 ]
