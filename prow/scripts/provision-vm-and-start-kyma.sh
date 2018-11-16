ff41b7b8fc76336c77de0d98ea4f02195f7c4a61
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2018-11-16 15:57:23 +0100

    Adding zone flags to scp and ssh

--- prow/scripts/provision-vm-and-start-kyma.sh
+++ prow/scripts/provision-vm-and-start-kyma.sh
@@ -46,10 +46,10 @@ shout "Copying Kyma to the instance"
 
 for i in $(seq 1 5); do
     [[ ${i} -gt 1 ]] && echo 'Retrying in 15 seconds..' && sleep 15;
-    gcloud compute scp --quiet --recurse /home/prow/go/src/github.com/kyma-project/kyma "kyma-integration-test-${RANDOM_ID}":~/kyma && break;
+    gcloud compute scp --quiet --recurse --zone="${ZONE}" /home/prow/go/src/github.com/kyma-project/kyma "kyma-integration-test-${RANDOM_ID}":~/kyma && break;
     [[ ${i} -ge 5 ]] && echo "Failed after $i attempts." && exit 1
 done;
 
 shout "Triggering the installation"
 
-gcloud compute ssh --quiet "kyma-integration-test-${RANDOM_ID}" -- ./kyma/prow/kyma-integration-on-debian/deploy-and-test-kyma.sh
+gcloud compute ssh --quiet --zone="${ZONE}" "kyma-integration-test-${RANDOM_ID}" -- ./kyma/prow/kyma-integration-on-debian/deploy-and-test-kyma.sh
