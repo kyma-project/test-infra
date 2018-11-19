f87560b1b4d89700309cdf43750aef332fc545c6
Jakub Błaszczyk <jakub.blaszczyk@hybris.com>
2018-11-19 09:34:41 +0100

    Guard instance creation from single zone resource allocation problem (#125)
    * Adding local prow configuration
    
    * Set command for debug
    
    * Move job to job folder
    
    * Clean local files
    
    * Enable naming convention in name, add run on change
    
    * Change files name, change triggers
    
    * Simplify job
    
    * Enable reporting
    
    * Export shout function, loop instance creation over several EU zones
    
    * Add LC type for random id generation
    
    * Change grep to tail for more clarity
    
    * Fix quoting
    
    * Move trap signal after loop, add verbose
    
    * Fix quoting
    
    * Add more verbose
    
    * Remove obsolete labels from job, remove unused parameters
    
    * Explain RANDOM_ID creation
    
    * Change RANDOM_ID function to openssl
    
    * Typo
    
    * Adding zone flags to scp and ssh
    
    * Adding zone flag to delete

--- prow/scripts/provision-vm-and-start-kyma.sh
+++ prow/scripts/provision-vm-and-start-kyma.sh
@@ -2,24 +2,20 @@
 
 set -o errexit
 
-cleanup() {
-    ARG=$?
-    echo "
-################################################################################
-# Removing instance kyma-integration-test-${RANDOM_ID}
-################################################################################
-    "
-
-    gcloud compute instances delete "kyma-integration-test-${RANDOM_ID}"
-    exit $ARG
-}
-
 readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # shellcheck disable=SC1090
 source "${SCRIPT_DIR}/library.sh"
+
+cleanup() {
+    ARG=$?
+    shout "Removing instance kyma-integration-test-${RANDOM_ID}"
+    gcloud compute instances delete --zone="${ZONE}" "kyma-integration-test-${RANDOM_ID}"
+    exit $ARG
+}
+
 authenticate
 
-RANDOM_ID=$(< /dev/urandom tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
+RANDOM_ID=$(openssl rand -hex 4)
 
 LABELS=""
 if [[ -z "${PULL_NUMBER}" ]]; then
@@ -28,34 +24,32 @@ else
     LABELS=(--labels "pull-number=$PULL_NUMBER,job-name=kyma-integration")
 fi
 
-echo "
-################################################################################
-# Creating a new instance named kyma-integration-test-${RANDOM_ID}
-################################################################################
-"
+ZONE_LIMIT=${ZONE_LIMIT:-5}
+EU_ZONES=$(gcloud compute zones list --filter="name~europe" --limit="${ZONE_LIMIT}" | tail -n +2 | awk '{print $1}')
 
-gcloud compute instances create "kyma-integration-test-${RANDOM_ID}" \
-    --metadata enable-oslogin=TRUE --image debian-9-stretch-v20181011 \
-    --image-project debian-cloud --machine-type n1-standard-4 --boot-disk-size 20 "${LABELS[@]}"
+for ZONE in ${EU_ZONES}; do
+    shout "Attempting to create a new instance named kyma-integration-test-${RANDOM_ID} in zone ${ZONE}"
+    gcloud compute instances create "kyma-integration-test-${RANDOM_ID}" \
+        --metadata enable-oslogin=TRUE \
+        --image debian-9-stretch-v20181011 \
+        --image-project debian-cloud \
+        --machine-type n1-standard-4 \
+        --zone "${ZONE}" \
+        --boot-disk-size 20 "${LABELS[@]}" &&\
+    shout "Created kyma-integration-test-${RANDOM_ID} in zone ${ZONE}" && break
+    shout "Could not create machine in zone ${ZONE}"
+done || exit 1
 
 trap cleanup exit
 
-echo "
-################################################################################
-# Copying Kyma to the instance
-################################################################################
-"
+shout "Copying Kyma to the instance"
 
 for i in $(seq 1 5); do
     [[ ${i} -gt 1 ]] && echo 'Retrying in 15 seconds..' && sleep 15;
-    gcloud compute scp --quiet --recurse /home/prow/go/src/github.com/kyma-project/kyma "kyma-integration-test-${RANDOM_ID}":~/kyma && break;
+    gcloud compute scp --quiet --recurse --zone="${ZONE}" /home/prow/go/src/github.com/kyma-project/kyma "kyma-integration-test-${RANDOM_ID}":~/kyma && break;
     [[ ${i} -ge 5 ]] && echo "Failed after $i attempts." && exit 1
 done;
 
-echo "
-################################################################################
-# Triggering the installation
-################################################################################
-"
+shout "Triggering the installation"
 
-gcloud compute ssh --quiet "kyma-integration-test-${RANDOM_ID}" -- ./kyma/prow/kyma-integration-on-debian/deploy-and-test-kyma.sh
+gcloud compute ssh --quiet --zone="${ZONE}" "kyma-integration-test-${RANDOM_ID}" -- ./kyma/prow/kyma-integration-on-debian/deploy-and-test-kyma.sh
