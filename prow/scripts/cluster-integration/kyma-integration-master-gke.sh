ca77cd0b5ba509150f3f1b4883a45fa9a4ec8be2
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2018-11-21 13:29:43 +0100

    Remove PULL_NUMBER

--- prow/scripts/cluster-integration/kyma-integration-master-gke.sh
+++ prow/scripts/cluster-integration/kyma-integration-master-gke.sh
@@ -3,14 +3,14 @@
 set -o errexit
 
 ############################################################
-# REPO_OWNER, REPO_NAME and PULL_NUMBER are set by ProwJob #
+# REPO_OWNER, REPO_NAME are set by ProwJob #
 ############################################################
 # MACHINE_TYPE (optional): GKE machine type                #
 ############################################################
 
 discoverUnsetVar=false
 
-for var in REPO_OWNER REPO_NAME PULL_NUMBER KYMA_PROJECT_DIR CLOUDSDK_CORE_PROJECT CLOUDSDK_COMPUTE_REGION CLOUDSDK_DNS_ZONE_NAME GOOGLE_APPLICATION_CREDENTIALS; do
+for var in REPO_OWNER REPO_NAME KYMA_PROJECT_DIR CLOUDSDK_CORE_PROJECT CLOUDSDK_COMPUTE_REGION CLOUDSDK_DNS_ZONE_NAME GOOGLE_APPLICATION_CREDENTIALS; do
     if [ -z "${!var}" ] ; then
         echo "ERROR: $var is not set"
         discoverUnsetVar=true
@@ -28,11 +28,11 @@ readonly KYMA_RESOURCES_DIR="${KYMA_SOURCES_DIR}/installation/resources"
 readonly INSTALLER_YAML="${KYMA_RESOURCES_DIR}/installer.yaml"
 readonly INSTALLER_CONFIG="${KYMA_RESOURCES_DIR}/installer-config-cluster.yaml.tpl"
 readonly INSTALLER_CR="${KYMA_RESOURCES_DIR}/installer-cr-cluster.yaml.tpl"
+readonly COMMIT_ID=$(cd "$KYMA_SOURCES_DIR" && git rev-parse --short HEAD)
 
-IP_ADDRESS_NAME=$(echo "pr-${PULL_NUMBER}-job-${PROW_JOB_ID}" | tr "[:upper:]" "[:lower:]")
-export IP_ADDRESS_NAME
+export IP_ADDRESS_NAME=$(echo "commit-${COMMIT_ID}-job-${PROW_JOB_ID}" | tr "[:upper:]" "[:lower:]")
 export DNS_SUBDOMAIN="${IP_ADDRESS_NAME}"
-export CLUSTER_NAME="${REPO_OWNER}-${REPO_NAME}-${PULL_NUMBER}"
+export CLUSTER_NAME="${REPO_OWNER}-${REPO_NAME}-${COMMIT_ID}"
 export IP_ADDRESS="will_be_generated"
 
 #For provision-gke-cluster.sh
