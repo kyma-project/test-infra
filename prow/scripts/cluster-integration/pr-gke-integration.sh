4f7364300ee5c36271d2029179258391a3b1985c
Jakub Kabza <jakub.kabza@sap.com>
2018-11-22 09:24:24 +0100

    echo replaced by shout

--- prow/scripts/cluster-integration/pr-gke-integration.sh
+++ prow/scripts/cluster-integration/pr-gke-integration.sh
@@ -199,12 +199,9 @@ date
 kubectl label installation/kyma-installation action=install
 "${KYMA_SCRIPTS_DIR}"/is-installed.sh
 
-echo "################################################################################"
-echo "Test Kyma"
-echo "################################################################################"
+shout "Test Kyma"
 date
 "${KYMA_SCRIPTS_DIR}"/testing.sh
 
 #!!! Must be at the end of the script !!!
 ERROR_LOGGING_GUARD="false"
-
