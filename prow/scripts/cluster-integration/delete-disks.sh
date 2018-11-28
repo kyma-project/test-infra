b1af64034bdbbc3c50346c70492a3e37e2f552ee
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2018-11-28 11:24:28 +0100

    Update docu

--- prow/scripts/cluster-integration/delete-disks.sh
+++ prow/scripts/cluster-integration/delete-disks.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-################################################################################
-# Removes disks from gcloud
-################################################################################
+#Description: Removes orphaned disks from gcloud
+# Note: This script is executed AFTER cluster is deleted.
+#
+# The purpose of the script is to clean up disks that are provisioned in gcloud for PVCs used by Kyma.
+# These disks are NOT removed along with the cluster and are left orphaned after cluster is deleted, causing resource leak.
+# We can get partial names of these objects from the cluster itself, while it's still running, like this:
+# DISKS=$(kubectl get pvc --all-namespaces -o jsonpath="{.items[*].spec.volumeName}" | xargs -n1 echo)
+# This allows to get a gcloud disk name suffix (not entire name), which is enough to find the proper object and delete it.
+#
+#Expected vars:
+# - DISKS: list of gcloud disk names (name can be a name suffix)
+#
+#Permissions: In order to run this script you need to use a service account with "Compute Admin" role
 
 set +e
 echo "Removing remaining PVC disks"
