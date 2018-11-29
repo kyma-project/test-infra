15dd2f6fb648276360a8c039cb7b6826e46488e0
Agata Kucharska <agata.kucharska@sap.com>
2018-11-29 11:27:14 +0100

    Add website.yaml and documentation-generator.yaml configuration and tests

--- development/tools/jobs/website/website_tools_documentation_generator_test.go
+++ development/tools/jobs/website/website_tools_documentation_generator_test.go
@@ -1,4 +1,4 @@
-package console_test
+package website_test
 
 import (
 	"testing"
@@ -8,18 +8,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestReactComponentsJobPresubmit(t *testing.T) {
+func TestWebsiteToolsDocumentationGeneratorJobPresubmit(t *testing.T) {
 	// WHEN
-	jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/console/components/react/react-components.yaml")
+	jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/website/tools/documentation-generator/website-tools-documentation-generator.yaml")
 	// THEN
 	require.NoError(t, err)
 
 	assert.Len(t, jobConfig.Presubmits, 1)
-	kymaPresubmits, ex := jobConfig.Presubmits["kyma-project/console"]
+	kymaPresubmits, ex := jobConfig.Presubmits["kyma-project/website"]
 	assert.True(t, ex)
 	assert.Len(t, kymaPresubmits, 1)
 
-	expName := "console-react-components"
+	expName := "website-tools-documentation-generator"
 	actualPresubmit := tester.FindPresubmitJobByName(kymaPresubmits, expName)
 	require.NotNil(t, actualPresubmit)
 	assert.Equal(t, expName, actualPresubmit.Name)
@@ -28,28 +28,28 @@ func TestReactComponentsJobPresubmit(t *testing.T) {
 	assert.True(t, actualPresubmit.SkipReport)
 	assert.True(t, actualPresubmit.Decorate)
 	assert.False(t, actualPresubmit.Optional)
-	assert.Equal(t, "github.com/kyma-project/console", actualPresubmit.PathAlias)
+	assert.Equal(t, "github.com/kyma-project/website", actualPresubmit.PathAlias)
 	tester.AssertThatHasExtraRefTestInfra(t, actualPresubmit.JobBase.UtilityConfig)
-	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, tester.PresetBuildPr)
-	assert.Equal(t, "^components/react/", actualPresubmit.RunIfChanged)
-	tester.AssertThatJobRunIfChanged(t, *actualPresubmit, "components/react/some_random_file.js")
+	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepo, tester.PresetGcrPush, tester.PresetBuildPr)
+	assert.Equal(t, "^tools/documentation-generator/", actualPresubmit.RunIfChanged)
+	tester.AssertThatJobRunIfChanged(t, *actualPresubmit, "tools/documentation-generator/some_random_file.js")
 	assert.Equal(t, tester.ImageNodeBuildpackLatest, actualPresubmit.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build.sh"}, actualPresubmit.Spec.Containers[0].Command)
-	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/console/components/react"}, actualPresubmit.Spec.Containers[0].Args)
+	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/website/tools/documentation-generator"}, actualPresubmit.Spec.Containers[0].Args)
 }
 
-func TestReactComponentsJobPostsubmit(t *testing.T) {
+func TestWebsiteToolsDocumentationGeneratorJobPostsubmit(t *testing.T) {
 	// WHEN
-	jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/console/components/react/react-components.yaml")
+	jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/website/tools/documentation-generator/website-tools-documentation-generator.yaml")
 	// THEN
 	require.NoError(t, err)
 
 	assert.Len(t, jobConfig.Postsubmits, 1)
-	kymaPost, ex := jobConfig.Postsubmits["kyma-project/console"]
+	kymaPost, ex := jobConfig.Postsubmits["kyma-project/website"]
 	assert.True(t, ex)
 	assert.Len(t, kymaPost, 1)
 
-	expName := "console-react-components"
+	expName := "website-tools-documentation-generator"
 	actualPost := tester.FindPostsubmitJobByName(kymaPost, expName)
 	require.NotNil(t, actualPost)
 	assert.Equal(t, expName, actualPost.Name)
@@ -57,10 +57,10 @@ func TestReactComponentsJobPostsubmit(t *testing.T) {
 
 	assert.Equal(t, 10, actualPost.MaxConcurrency)
 	assert.True(t, actualPost.Decorate)
-	assert.Equal(t, "github.com/kyma-project/console", actualPost.PathAlias)
+	assert.Equal(t, "github.com/kyma-project/website", actualPost.PathAlias)
 	tester.AssertThatHasExtraRefTestInfra(t, actualPost.JobBase.UtilityConfig)
-	tester.AssertThatHasPresets(t, actualPost.JobBase, tester.PresetBuildMaster)
+	tester.AssertThatHasPresets(t, actualPost.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepo, tester.PresetGcrPush, tester.PresetBuildMaster)
 	assert.Equal(t, tester.ImageNodeBuildpackLatest, actualPost.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build.sh"}, actualPost.Spec.Containers[0].Command)
-	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/console/components/react"}, actualPost.Spec.Containers[0].Args)
+	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/website/tools/documentation-generator"}, actualPost.Spec.Containers[0].Args)
 }
