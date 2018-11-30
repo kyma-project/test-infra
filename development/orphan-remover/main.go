59eafa57b0a2ff3502684abb2315b89fd7fd04f9
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2018-11-30 16:31:35 +0100

    Fix httphealtchcheck error, adding instancegroup deletion

--- development/orphan-remover/main.go
+++ development/orphan-remover/main.go
@@ -14,12 +14,15 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 var (
-	project     = flag.String("project", "", "Project ID")
-	targetPool  = []TargetPool{}
-	garbagePool = []TargetPool{}
+	project        = flag.String("project", "", "Project ID")
+	dryRun         = flag.Bool("dry-run", true, "Dry Run enabled")
+	targetPool     = []TargetPool{}
+	garbagePool    = []TargetPool{}
+	instanceGroups = []InstanceGroup{}
 )
 
 //TargetPool ???
@@ -39,6 +42,12 @@ type Instance struct {
 	exists bool
 }
 
+// InstanceGroup ???
+type InstanceGroup struct {
+	name string
+	zone string
+}
+
 func main() {
 	flag.Parse()
 
@@ -47,6 +56,7 @@ func main() {
 		flag.Usage()
 		os.Exit(2)
 	}
+
 	context := context.Background()
 	connenction, err := google.DefaultClient(context, compute.CloudPlatformScope)
 	if err != nil {
@@ -58,34 +68,76 @@ func main() {
 		log.Fatalf("Could not initialize gke client: %v", err)
 	}
 
-	targetPool, _ := lookupTargetPools(svc, *project)
-	for _, target := range targetPool {
-		markCount := 0
-		for _, instance := range target.instances {
-			markInstance(svc, *project, &instance)
-			if !instance.exists {
-				markCount++
+	// targetPool, _ := lookupTargetPools(svc, *project)
+	// for _, target := range targetPool {
+	// 	markCount := 0
+	// 	for _, instance := range target.instances {
+	// 		markInstance(svc, *project, &instance)
+	// 		if !instance.exists {
+	// 			markCount++
+	// 		}
+	// 	}
+	// 	if markCount == target.instanceCount {
+	// 		garbagePool = append(garbagePool, target)
+	// 	}
+	// }
+	// fmt.Printf("All items: %d\n", len(targetPool))
+	// fmt.Printf("Garbage items: %d\n", len(garbagePool))
+
+	zones, _ := lookupZones(svc, *project, "europe-*")
+	for _, zone := range zones {
+		igList, _ := lookupInstanceGroup(svc, *project, zone)
+		if len(igList) > 0 {
+			for _, ig := range igList {
+				// fmt.Printf("IG: %s, ZONE: %s\n", ig, zone)
+				instanceGroups = append(instanceGroups, InstanceGroup{ig, zone})
 			}
-		}
-		if markCount == target.instanceCount {
-			garbagePool = append(garbagePool, target)
+
 		}
 	}
-	fmt.Printf("All items: %d\n", len(targetPool))
-	fmt.Printf("Garbage items: %d\n", len(garbagePool))
-	fmt.Printf("Processing item: %s\n", garbagePool[0].name)
-	fmt.Printf("HealthChecks item: %s\n", garbagePool[0].healthChecks)
-	// deleteForwardingRule(svc, *project, garbagePool[0].name, garbagePool[0].region)
-	// deleteTargetPool(svc, *project, garbagePool[0].name, garbagePool[0].region)
-	// deleteHealthChecks(svc, *project, garbagePool[0].healthChecks)
+	fmt.Printf("%s", instanceGroups)
+	deleteInstanceGroup(svc, *project, instanceGroups[0].zone, instanceGroups[0].name)
+	// log.Print("---> Looking up BackendServices!")
 	// lookupBackendServices(svc, *project, garbagePool[0].healthChecks)
+
+	// log.Print("---> Looking up InstanceGroups!\n Item: %s", garbagePool[0])
+	// lookupInstanceGroup(svc, *project, garbagePool[0].instances[0].zone)
+
+	if !*dryRun {
+		purge(svc, garbagePool)
+	}
+}
+
+func purge(svc *compute.Service, pool []TargetPool) {
+	for _, target := range pool {
+		log.Print("Processing item: %s\n", target.name)
+
+		log.Print("Delete ForwardingRules: %s\n", target.name)
+		deleteForwardingRule(svc, *project, target.name, target.region)
+		time.Sleep(5 * time.Second)
+
+		log.Print("Delete HealthCheck : %s\n", target.healthChecks)
+		deleteHealthChecks(svc, *project, target.healthChecks)
+		time.Sleep(5 * time.Second)
+
+		log.Print("Delete TargetPool : %s\n", target.name)
+		deleteTargetPool(svc, *project, target.name, target.region)
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func deleteInstanceGroup(svc *compute.Service, project string, zone string, instanceGroup string) {
+	_, err := svc.InstanceGroups.Delete(project, zone, instanceGroup).Do()
+	if err != nil {
+		log.Print(err)
+	}
 }
 
 func deleteHealthChecks(svc *compute.Service, project string, names []string) {
 	for _, check := range names {
-		_, err := svc.HealthChecks.Delete(project, check).Do()
+		_, err := svc.HttpHealthChecks.Delete(project, check).Do()
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
 		}
 	}
 }
@@ -93,14 +145,14 @@ func deleteHealthChecks(svc *compute.Service, project string, names []string) {
 func deleteForwardingRule(svc *compute.Service, project string, name string, region string) {
 	_, err := svc.ForwardingRules.Delete(project, region, name).Do()
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 }
 
 func deleteTargetPool(svc *compute.Service, project string, name string, region string) {
 	_, err := svc.TargetPools.Delete(project, region, name).Do()
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 }
 
@@ -113,12 +165,27 @@ func markInstance(svc *compute.Service, project string, instance *Instance) {
 
 func lookupBackendServices(svc *compute.Service, project string, checks []string) ([]string, error) {
 	call := svc.BackendServices.List(project)
-	call := call.Filter("")
 	var items []string
 	f := func(page *compute.BackendServiceList) error {
 		for _, list := range page.Items {
 			js, _ := json.Marshal(list)
 			fmt.Printf("%s\n\n", js)
+		}
+		return nil
+	}
+	if err := call.Pages(oauth2.NoContext, f); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+func lookupInstanceGroup(svc *compute.Service, project string, zone string) ([]string, error) {
+	call := svc.InstanceGroups.List(project, zone)
+	call = call.Filter("size: 0")
+	var items []string
+	f := func(page *compute.InstanceGroupList) error {
+		for _, list := range page.Items {
+			items = append(items, list.Name)
 		}
 		return nil
 	}
@@ -157,4 +224,24 @@ func lookupTargetPools(svc *compute.Service, project string) ([]TargetPool, erro
 		return nil, err
 	}
 	return items, nil
+}
+
+func lookupZones(svc *compute.Service, project, pattern string) ([]string, error) {
+	call := svc.Zones.List(project)
+	if pattern != "" {
+		call = call.Filter("name: " + pattern)
+	}
+
+	var zones []string
+	f := func(page *compute.ZoneList) error {
+		for _, v := range page.Items {
+			zones = append(zones, v.Name)
+		}
+		return nil
+	}
+
+	if err := call.Pages(oauth2.NoContext, f); err != nil {
+		return nil, err
+	}
+	return zones, nil
 }
