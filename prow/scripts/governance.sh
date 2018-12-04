3aa5226751e5cefaf81a8bb8d2d20903cf32b09e
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2018-12-04 16:37:45 +0100

    Improve governance.sh

--- prow/scripts/governance.sh
+++ prow/scripts/governance.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 readonly MILV_IMAGE="magicmatatjahu/milv:0.0.6"
 readonly REPOSITORY_NAME=$1
 readonly BRANCH_NAME=$(git branch | grep \* | cut -d ' ' -f2)
@@ -11,33 +9,30 @@ if [[ -z "${REPOSITORY_NAME}" ]]; then
     exit 1
 fi
 
-echo "${BRANCH_NAME}"
-
 function run_milv_docker() {
     docker run --rm --dns=8.8.8.8 --dns=8.8.4.4 -v $PWD:/${REPOSITORY_NAME}:ro ${MILV_IMAGE} --base-path=/${REPOSITORY_NAME} $1
 }
 
-function changeset() {
-    echo "Fetching changes between origin/${BRANCH_NAME}/head and origin/master."
-    return $(git --no-pager diff --name-only origin/master...origin/${BRANCH_NAME}/head | grep '.md' || echo '')
-}
-
 function validate_internal_links() {
     echo "Validate internal links"
-    run_milv_docker --ignore-external
+    run_milv_docker "--ignore-external"
 }
 
-changeset
+function validate_external_links() {
+    echo "Fetching changes between origin/master...origin/${BRANCH_NAME}"
+    local files=$(git --no-pager diff --name-only origin/master...origin/${BRANCH_NAME} | grep '.md' || echo '')
+    local changed_files=""
+    for file in $files; do
+        changed_files="${changed_files} ${file}"
+    done
 
-# function validate_external_links() {
-#     echo "Validate external links in changed markdown files"
-#     readonly CHANGED_FILES=changed_markdown_files
-#     run_milv_docker --ignore-internal ${CHANGED_FILES}
-# }
-
-# function changed_markdown_files() {
-
-# }
+    echo "Validate external links in changed markdown files"
+    if [ "${changed_files}" = "-lt 1" ]; then
+        run_milv_docker "--ignore-internal ${changed_files}"
+    else
+        echo "Any markdown files to checking external links"
+    fi
+}
 
 validate_internal_links
-# validate_external_links
+validate_external_links
