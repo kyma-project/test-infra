f11a9e2dbadb9519f04aabdcf366e17135993e52
Szecowka, Adam <adam.szecowka@gmail.com>
2018-12-06 12:50:59 +0100

    Remove limit range

--- prow/install-prow.sh
+++ prow/install-prow.sh
@@ -28,8 +28,5 @@ kubectl apply -f cluster/03-tls-ing_ingress.yaml
 # Install branch protector
 kubectl apply -f cluster/04-branchprotector_cronjob.yaml
 
-# Define limit range
-kubectl apply -f cluster/05-limit-range.yaml
-
 # Remove Insecure ingress 
 kubectl delete ingress ing
