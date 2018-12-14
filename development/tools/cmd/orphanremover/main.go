9472e6e772d154b55321a6ac93be62cebbd480d1
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2018-12-14 09:24:05 +0100

    Orphan remover (#220)
    * Adding first draft of remover
    
    * Adding loop over all orpahned resources
    
    * First draft of go garbage collector
    
    * Adding backendservice lookup
    
    * Fix httphealtchcheck error, adding instancegroup deletion
    
    * Add logic for extracting list of backendservices
    
    * Add URLMap removal
    
    * Implement godep
    
    * Update .gitignore
    
    * Remove vendor files
    
    * Remove global vars that do not need to be global
    
    * Move script, add missing elements
    
    * Update gopkg.lock file
    
    * Fix gopkg.lock file, add sample prowjob
    
    * Y u no work?
    
    * Use container dep
    
    * Format using goimports
    
    * Fix gopkg.lock
    
    * Refactoring, split into pkg
    
    * Remove comments
    
    * Overall code refactor
    
    * Regenerate lockfile
    
    * Remove change in gitignore
    
    * Adding mockery
    
    * Adding initial test
    
    * Extract filter function from main, implement tests for it
    
    * Add wrapper scripts, remove prototype, move prowjob
    
    * Update kyma-integration.yaml
    
    Change cron schedule

---
+++ development/tools/cmd/orphanremover/main.go
@@ -0,0 +1,47 @@
+// See https://cloud.google.com/docs/authentication/.
+// Use GOOGLE_APPLICATION_CREDENTIALS environment variable to specify
+// a service account key file to authenticate to the API.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/kyma-project/test-infra/development/tools/pkg/orphanremover"
+	"golang.org/x/oauth2/google"
+	compute "google.golang.org/api/compute/v1"
+)
+
+var (
+	project = flag.String("project", "", "Project ID")
+	dryRun  = flag.Bool("dry-run", true, "Dry Run enabled")
+)
+
+func main() {
+	flag.Parse()
+
+	if *project == "" {
+		fmt.Fprintln(os.Stderr, "missing -project flag")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	log.Printf("Running with arguments: project: \"%s\", dryRun: \"%t\"", *project, *dryRun)
+	ctx := context.Background()
+	connenction, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
+	if err != nil {
+		log.Fatalf("Could not get authenticated client: %v", err)
+	}
+
+	svc, err := compute.New(connenction)
+	if err != nil {
+		log.Fatalf("Could not initialize gke client: %v", err)
+	}
+
+	computeServiceWrapper := &orphanremover.ComputeServiceWrapper{Context: ctx, Compute: svc}
+	remover := orphanremover.NewRemover(computeServiceWrapper)
+	remover.Run(*dryRun, *project)
+}
