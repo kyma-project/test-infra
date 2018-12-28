e839a53d3b5f9cc9ffa91d42dcd46959439437fa
Szecowka, Adam <adam.szecowka@gmail.com>
2018-12-28 08:30:47 +0100

    cleanup

---
+++ development/tools/jobs/kyma/kyma_artifacts_test.go
@@ -0,0 +1,32 @@
+package kyma
+
+import (
+	"testing"
+
+	"github.com/kyma-project/test-infra/development/tools/jobs/tester"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestKymaArtifactsReleases(t *testing.T) {
+	for _, currentRelease := range tester.GetAllKymaReleaseBranches() {
+		t.Run(currentRelease, func(t *testing.T) {
+			jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/kyma/kyma-artifacts.yaml")
+			require.NoError(t, err)
+			actualPresubmit := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-project/kyma"], "kyma-artifacts", currentRelease)
+			require.NotNil(t, actualPresubmit)
+			assert.True(t, actualPresubmit.SkipReport)
+			assert.True(t, actualPresubmit.Decorate)
+			assert.Equal(t, "github.com/kyma-project/kyma", actualPresubmit.PathAlias)
+			tester.AssertThatHasExtraRefTestInfra(t, actualPresubmit.JobBase.UtilityConfig, currentRelease)
+			tester.AssertThatHasPresets(t, actualPresubmit.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepo, tester.PresetGcrPush)
+			assert.False(t, actualPresubmit.AlwaysRun)
+			assert.Len(t, actualPresubmit.Spec.Containers, 1)
+			testContainer := actualPresubmit.Spec.Containers[0]
+			assert.Equal(t, tester.ImageBootstrap20181204, testContainer.Image)
+			assert.Len(t, testContainer.Command, 1)
+			assert.Equal(t, "/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build-kyma-artifacts.sh", testContainer.Command[0])
+			tester.AssertThatSpecifiesResourceRequests(t, actualPresubmit.JobBase)
+		})
+	}
+}
--- prow/scripts/build-kyma-artifacts.sh
+++ prow/scripts/build-kyma-artifacts.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 
+# This script is executed during release process and generates kyma artifacts. Artifacts are stored in $(ARTIFACTS) location
+# that is automatically uploaded by Prow to GCS bucket in the following location:
+# <plank gcs bucket>/pr-logs/pull/<org_repository>/<pull_request_number>/kyma-artifacts/<build_id>/artifacts
+# Information about latest build id is stored in:
+# <plank gcs bucket>/pr-logs/pull/<org_repository>/<pull_request_number>/kyma-artifacts/latest-build.txt
+
 set -e
 
 readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # shellcheck disable=SC1090
 source "${SCRIPT_DIR}/library.sh"
-
-usage () {
-    echo "Usage: \$ ${BASH_SOURCE[1]} /path/to/component"
-    exit 1
-}
-
-readonly SOURCES_DIR=$1
-
-if [[ -z "${SOURCES_DIR}" ]]; then
-    usage
-fi
 
 function export_variables() {
    DOCKER_TAG=$(cat "${SCRIPT_DIR}/../RELEASE_VERSION")
@@ -25,7 +20,4 @@ function export_variables() {
 init
 export_variables
 
-echo "LS -la logs"
-ls -la /logs
-
-make -C "${SOURCES_DIR}" ci-create-release-artifacts
+make -C /home/prow/go/src/github.com/kyma-project/kyma/tools/kyma-installer ci-create-release-artifacts
