b6c2fdd8558a6f05d46fe7ee0ac754c28e4157f6
Szecowka, Adam <adam.szecowka@gmail.com>
2019-01-08 10:36:10 +0100

    Update branch protection rules for release 0.6. Kyma-installer should be run manually because it has to build after component/installer

--- development/tools/cmd/secretspopulator/main_test.go
+++ development/tools/cmd/secretspopulator/main_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/api/cloudkms/v1"
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
