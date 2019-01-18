830e71948e5c3a23b8c1875d358d67472a8aebde
Magda Strek <magdalena.strek@sap.com>
2019-01-18 11:11:01 +0100

    Stability checker job - cron change + stability checker changes

--- prow/scripts/cluster-integration/kyma-gke-nightly.sh
+++ prow/scripts/cluster-integration/kyma-gke-nightly.sh
@@ -143,6 +143,7 @@ function installStabilityChecker() {
 	        --set stats.enabled="${STATS_ENABLED}" \
 	        --set stats.failingTestRegexp="${STATS_FAILING_TEST_REGEXP}" \
 	        --set stats.successfulTestRegexp="${STATS_SUCCESSFUL_TEST_REGEXP}" \
+			--set testResultWindowTime="3h" \
 	        "${SC_DIR}/deploy/chart/stability-checker" \
 	        --namespace=kyma-system \
 	        --name=stability-checker
