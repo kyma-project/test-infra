d389d4ad311910b0eb6165bb552fccce3daefc6e
strekm <magdalena.strek@sap.com>
2019-01-22 12:48:55 +0100

    Stability checker - fix (#527)

--- prow/scripts/cluster-integration/kyma-gke-nightly.sh
+++ prow/scripts/cluster-integration/kyma-gke-nightly.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 discoverUnsetVar=false
 
-for var in DOCKER_PUSH_REPOSITORY DOCKER_PUSH_DIRECTORY KYMA_PROJECT_DIR CLOUDSDK_CORE_PROJECT CLOUDSDK_COMPUTE_REGION CLOUDSDK_DNS_ZONE_NAME GOOGLE_APPLICATION_CREDENTIALS SLACK_CLIENT_TOKEN SLACK_CLIENT_WEBHOOK_URL SLACK_CLIENT_CHANNEL_ID; do
+for var in DOCKER_PUSH_REPOSITORY DOCKER_PUSH_DIRECTORY KYMA_PROJECT_DIR CLOUDSDK_CORE_PROJECT CLOUDSDK_COMPUTE_REGION CLOUDSDK_COMPUTE_ZONE CLOUDSDK_DNS_ZONE_NAME GOOGLE_APPLICATION_CREDENTIALS SLACK_CLIENT_TOKEN SLACK_CLIENT_WEBHOOK_URL SLACK_CLIENT_CHANNEL_ID; do
     if [ -z "${!var}" ] ; then
         echo "ERROR: $var is not set"
         discoverUnsetVar=true
@@ -14,9 +14,35 @@ if [ "${discoverUnsetVar}" = true ] ; then
     exit 1
 fi
 
+export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
 export TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS="${TEST_INFRA_SOURCES_DIR}/prow/scripts/cluster-integration/helpers"
+export KYMA_SOURCES_DIR="${KYMA_PROJECT_DIR}/kyma"
+export KYMA_SCRIPTS_DIR="${KYMA_SOURCES_DIR}/installation/scripts"
+
+export GCLOUD_PROJECT_NAME="${CLOUDSDK_CORE_PROJECT}"
+export GCLOUD_COMPUTE_ZONE="${CLOUDSDK_COMPUTE_ZONE}"
+export GCLOUD_SERVICE_KEY_PATH="${GOOGLE_APPLICATION_CREDENTIALS}"
+
+DNS_DOMAIN="$(gcloud dns managed-zones describe "${CLOUDSDK_DNS_ZONE_NAME}" --format="value(dnsName)")"
+export DNS_DOMAIN
+
+readonly REPO_OWNER="kyma-project"
+readonly REPO_NAME="kyma"
+readonly NAME_ROOT="gkeint-nightly"
+readonly CURRENT_TIMESTAMP=$(date +%Y%m%d)
+
+readonly COMMON_NAME=$(echo "${NAME_ROOT}-${CURRENT_TIMESTAMP}" | tr "[:upper:]" "[:lower:]")
+readonly DNS_SUBDOMAIN="${COMMON_NAME}"
+
+export CLUSTER_NAME="${COMMON_NAME}"
+
+# shellcheck disable=SC1090
+source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/library.sh"
 
 function removeCluster() {
+	#Turn off exit-on-error so that next step is executed even if previous one fails.
+	set +e
+
 	CLUSTER_NAME=$1
 
 	EXIT_STATUS=$?
@@ -30,14 +56,14 @@ function removeCluster() {
 	date
 	GATEWAY_IP_ADDRESS=$(gcloud compute addresses describe "${CLUSTER_NAME}" --format json --region "${CLOUDSDK_COMPUTE_REGION}" | jq '.address' | tr -d '"')
 	GATEWAY_DNS_FULL_NAME="*.${CLUSTER_NAME}.build.kyma-project.io."
-	IP_ADDRESS=${GATEWAY_IP_ADDRESS} DNS_FULL_NAME=${GATEWAY_DNS_FULL_NAME} "${TEST_INFRA_SOURCES_DIR}"/prow/scripts/cluster-integration/delete-dns-record.sh
+	IP_ADDRESS=${GATEWAY_IP_ADDRESS} DNS_FULL_NAME=${GATEWAY_DNS_FULL_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/delete-dns-record.sh
 	TMP_STATUS=$?
 	if [[ ${TMP_STATUS} -ne 0 ]]; then EXIT_STATUS=${TMP_STATUS}; fi
 
 	shout "Release Gateway IP Address"
 	date
 	GATEWAY_IP_ADDRESS_NAME=${CLUSTER_NAME}
-	IP_ADDRESS_NAME=${GATEWAY_IP_ADDRESS_NAME} "${TEST_INFRA_SOURCES_DIR}"/prow/scripts/cluster-integration/release-ip-address.sh
+	IP_ADDRESS_NAME=${GATEWAY_IP_ADDRESS_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/release-ip-address.sh
 	TMP_STATUS=$?
 	if [[ ${TMP_STATUS} -ne 0 ]]; then EXIT_STATUS=${TMP_STATUS}; fi
 
@@ -45,21 +71,22 @@ function removeCluster() {
 	date
 	REMOTEENVS_IP_ADDRESS=$(gcloud compute addresses describe "remoteenvs-${CLUSTER_NAME}" --format json --region "${CLOUDSDK_COMPUTE_REGION}" | jq '.address' | tr -d '"')
 	REMOTEENVS_DNS_FULL_NAME="gateway.${CLUSTER_NAME}.build.kyma-project.io."
-	IP_ADDRESS=${REMOTEENVS_IP_ADDRESS} DNS_FULL_NAME=${REMOTEENVS_DNS_FULL_NAME} "${TEST_INFRA_SOURCES_DIR}"/prow/scripts/cluster-integration/delete-dns-record.sh
+	IP_ADDRESS=${REMOTEENVS_IP_ADDRESS} DNS_FULL_NAME=${REMOTEENVS_DNS_FULL_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/delete-dns-record.sh
 	TMP_STATUS=$?
 	if [[ ${TMP_STATUS} -ne 0 ]]; then EXIT_STATUS=${TMP_STATUS}; fi
 
 	shout "Release Remote Environments IP Address"
 	date
 	REMOTEENVS_IP_ADDRESS_NAME="remoteenvs-${CLUSTER_NAME}"
-	IP_ADDRESS_NAME=${REMOTEENVS_IP_ADDRESS_NAME} "${TEST_INFRA_SOURCES_DIR}"/prow/scripts/cluster-integration/release-ip-address.sh
+	IP_ADDRESS_NAME=${REMOTEENVS_IP_ADDRESS_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/release-ip-address.sh
 	TMP_STATUS=$?
 	if [[ ${TMP_STATUS} -ne 0 ]]; then EXIT_STATUS=${TMP_STATUS}; fi
 
 	shout "Delete temporary Kyma-Installer Docker image"
 	date
 
-	"${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/delete-image.sh
+    readonly OLD_TIMESTAMP=$(echo "${CLUSTER_NAME}" | cut -d '-' -f3)
+    KYMA_INSTALLER_IMAGE="${DOCKER_PUSH_REPOSITORY}${DOCKER_PUSH_DIRECTORY}/${NAME_ROOT}/${REPO_OWNER}/${REPO_NAME}:${OLD_TIMESTAMP}" "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/delete-image.sh
 	TMP_STATUS=$?
 	if [[ ${TMP_STATUS} -ne 0 ]]; then EXIT_STATUS=${TMP_STATUS}; fi
 
@@ -67,14 +94,11 @@ function removeCluster() {
 	if [[ ${EXIT_STATUS} -ne 0 ]]; then MSG="(exit status: ${EXIT_STATUS})"; fi
 	shout "Job is finished ${MSG}"
 	date
+
+	set -e
 }
 
 function createCluster() {
-	DNS_SUBDOMAIN="${COMMON_NAME}"
-	shout "Build Kyma-Installer Docker image"
-	date
-	"${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/create-image.sh
-
 	shout "Reserve IP Address for Ingressgateway"
 	date
 	GATEWAY_IP_ADDRESS_NAME="${COMMON_NAME}"
@@ -123,8 +147,12 @@ function installKyma() {
     	exit 1
 	fi
 
-	DNS_SUBDOMAIN="${COMMON_NAME}"
-	
+	KYMA_INSTALLER_IMAGE="${DOCKER_PUSH_REPOSITORY}${DOCKER_PUSH_DIRECTORY}/${NAME_ROOT}/${REPO_OWNER}/${REPO_NAME}:${CURRENT_TIMESTAMP}"
+
+	shout "Build Kyma-Installer Docker image"
+	date
+	KYMA_INSTALLER_IMAGE="${KYMA_INSTALLER_IMAGE}" "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/create-image.sh
+
 	KYMA_RESOURCES_DIR="${KYMA_SOURCES_DIR}/installation/resources"
 	INSTALLER_YAML="${KYMA_RESOURCES_DIR}/installer.yaml"
 	INSTALLER_CONFIG="${KYMA_RESOURCES_DIR}/installer-config-cluster.yaml.tpl"
@@ -186,55 +214,26 @@ function installStabilityChecker() {
 	        --name=stability-checker
 }
 
-readonly REPO_OWNER="kyma-project"
-export REPO_OWNER
-readonly REPO_NAME="kyma"
-export REPO_NAME
+function cleanup() {
+    OLD_CLUSTERS=$(gcloud container clusters list --filter="name~^${NAME_ROOT}-" --format json | jq '.[].name' | tr -d '"')
+    CLUSTERS_SIZE=$(echo "$OLD_CLUSTERS" | wc -l)
+    if [[ "$CLUSTERS_SIZE" -gt 0 ]]; then
+	    shout "Delete old cluster"
+	    date
+	    for CLUSTER in $OLD_CLUSTERS; do
+		    removeCluster "${CLUSTER}"
+	    done
+    fi
 
-export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
-export KYMA_SOURCES_DIR="${KYMA_PROJECT_DIR}/kyma"
-export KYMA_SCRIPTS_DIR="${KYMA_SOURCES_DIR}/installation/scripts"
-export GCLOUD_SERVICE_KEY_PATH="${GOOGLE_APPLICATION_CREDENTIALS}"
-
-readonly CURRENT_TIMESTAMP=$(date +%Y%m%d)
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	readonly LAST_TIMESTAMP=$(date -v -1d '+%Y%m%d')
-else
-	readonly LAST_TIMESTAMP=$(date +%Y%m%d --date="yesterday")
-fi
-
-readonly NAME_ROOT="gkeint-nightly"
-readonly COMMON_NAME=$(echo "${NAME_ROOT}-${CURRENT_TIMESTAMP}" | tr "[:upper:]" "[:lower:]")
-export COMMON_NAME
-readonly KYMA_INSTALLER_IMAGE="${DOCKER_PUSH_REPOSITORY}${DOCKER_PUSH_DIRECTORY}/${NAME_ROOT}/${REPO_OWNER}/${REPO_NAME}:${CURRENT_TIMESTAMP}"
-export KYMA_INSTALLER_IMAGE
-
-### Cluster name must be less than 40 characters!
-export CLUSTER_NAME="${COMMON_NAME}"
-
-### For provision-gke-cluster.sh
-export GCLOUD_PROJECT_NAME="${CLOUDSDK_CORE_PROJECT}"
-export GCLOUD_COMPUTE_ZONE="${CLOUDSDK_COMPUTE_ZONE}"
-
-# shellcheck disable=SC1090
-source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/library.sh"
+}
 
 shout "Authenticate"
 date
 init
-readonly DNS_DOMAIN="$(gcloud dns managed-zones describe "${CLOUDSDK_DNS_ZONE_NAME}" --format="value(dnsName)")"
-export DNS_DOMAIN
 
-OLD_CLUSTERS=$(gcloud container clusters list --filter="name~${NAME_ROOT}" --format json | jq '.[].name' | tr -d '"')
-CLUSTERS_SIZE=$(echo "$OLD_CLUSTERS" | wc -l)
-if [[ "$CLUSTERS_SIZE" -gt 0 ]]; then
-	shout "Delete old cluster"
-	date
-	for CLUSTER in $OLD_CLUSTERS; do
-		removeCluster "${CLUSTER}"
-	done
-fi
+shout "Cleanup"
+date
+cleanup
 
 shout "Create new cluster"
 date
