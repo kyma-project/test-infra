bf25e52e598579344a393073a22dd1010c05fe2e
AHabes <33485177+AHabes@users.noreply.github.com>
2019-01-23 14:39:51 +0100

    Fixing machine-id issue
    clearing machine-id

--- development/custom-image/create-custom-image.sh
+++ development/custom-image/create-custom-image.sh
@@ -19,14 +19,15 @@ DATE=$(date +v%Y%m%d)
 ZONE_LIMIT=${ZONE_LIMIT:-5}
 EU_ZONES=$(gcloud compute zones list --filter="name~europe" --limit="${ZONE_LIMIT}" | tail -n +2 | awk '{print $1}')
 for ZONE in ${EU_ZONES}; do
-    shout "Attempting to create a new instance named kyma-deps-image-vm-${RANDOM_ID} in zone ${ZONE}"
+    shout "Attempting to create a new instance named kyma-deps-image-vm-${RANDOM_ID} in zone ${ZONE} ..."
     gcloud compute instances create "kyma-deps-image-vm-${RANDOM_ID}" \
         --metadata enable-oslogin=TRUE \
         --machine-type n1-standard-4 \
         --image-family debian-9 \
         --image-project debian-cloud \
         --zone "${ZONE}" \
-        --boot-disk-size 20 &&\
+        --boot-disk-size 20 \
+        --metadata-from-file startup-script=./machine-id-clean-up.sh  &&\
     shout "Created kyma-deps-image-vm-${RANDOM_ID} in zone ${ZONE}" && break
     shout "Could not create machine in zone ${ZONE}"
 done
@@ -44,8 +45,12 @@ done;
 shout "Running install-deps-debian.sh ..."
 gcloud compute ssh --quiet --zone="${ZONE}" "kyma-deps-image-vm-${RANDOM_ID}" -- ./install-deps-debian.sh
 
+shout "Clearing kyma-deps-image-vm-${RANDOM_ID} machine-id ..."
+gcloud compute ssh --zone "${ZONE}" "kyma-deps-image-vm-${RANDOM_ID}" --command "sudo sh -c 'echo "" > /etc/machine-id'"
+gcloud compute ssh --zone "${ZONE}" "kyma-deps-image-vm-${RANDOM_ID}" --command "sudo sh -c 'echo "" > /var/lib/dbus/machine-id'"
 
-shout "Stopping kyma-deps-image-vm-${RANDOM_ID} in zone ${ZONE}"
+
+shout "Stopping kyma-deps-image-vm-${RANDOM_ID} in zone ${ZONE} ..."
 gcloud compute instances stop --zone="${ZONE}" "kyma-deps-image-vm-${RANDOM_ID}"
 
 
