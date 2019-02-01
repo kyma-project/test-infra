2f1a60b9bc84f752cc59854d518f172cff52561a
AHabes <33485177+AHabes@users.noreply.github.com>
2019-02-01 18:20:35 +0100

    Adding --image flag to specify the required image
    Adding a new flag --image that would allow to manually specify the image to be used upon the creation of the VM, if no image is provided, the latest default custom image will be automatically selected

--- prow/scripts/provision-vm-and-start-kyma.sh
+++ prow/scripts/provision-vm-and-start-kyma.sh
@@ -24,15 +24,50 @@ else
     LABELS=(--labels "pull-number=$PULL_NUMBER,job-name=kyma-integration")
 fi
 
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+
+    key="$1"
+
+    case ${key} in
+        --image)
+            IMAGE="$2"
+            shift
+            shift
+            ;;
+        --*)
+            echo "Unknown flag ${1}"
+            exit 1
+            ;;
+        *)    # unknown option
+            POSITIONAL+=("$1") # save it in an array for later
+            shift # past argument
+            ;;
+    esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+
+if [[ -z "$IMAGE" ]]; then
+    shout "Provisioning vm using the latest default custom image ..."   
+
+    DEFAULT_IMAGES=$(gcloud compute images list --project "kyma-project" \
+         --sort-by "~creationTimestamp" \
+         --filter "family:custom images AND labels.default:yes" | tail -n +2 | awk '{print $1}')
+
+    default_image_arr=($DEFAULT_IMAGES)
+    IMAGE=${default_image_arr[0]}
+   fi
+
 ZONE_LIMIT=${ZONE_LIMIT:-5}
 EU_ZONES=$(gcloud compute zones list --filter="name~europe" --limit="${ZONE_LIMIT}" | tail -n +2 | awk '{print $1}')
 
 for ZONE in ${EU_ZONES}; do
-    shout "Attempting to create a new instance named kyma-integration-test-${RANDOM_ID} in zone ${ZONE}"
+    shout "Attempting to create a new instance named kyma-integration-test-${RANDOM_ID} in zone ${ZONE} using image ${IMAGE}"
     gcloud compute instances create "kyma-integration-test-${RANDOM_ID}" \
         --metadata enable-oslogin=TRUE \
-        --image debian-9-stretch-v20181011 \
-        --image-project debian-cloud \
+        --image "${IMAGE}" \
         --machine-type n1-standard-4 \
         --zone "${ZONE}" \
         --boot-disk-size 20 "${LABELS[@]}" &&\
@@ -48,8 +83,8 @@ for i in $(seq 1 5); do
     [[ ${i} -gt 1 ]] && echo 'Retrying in 15 seconds..' && sleep 15;
     gcloud compute scp --quiet --recurse --zone="${ZONE}" /home/prow/go/src/github.com/kyma-project/kyma "kyma-integration-test-${RANDOM_ID}":~/kyma && break;
     [[ ${i} -ge 5 ]] && echo "Failed after $i attempts." && exit 1
-done;
+done
 
 shout "Triggering the installation"
 
-gcloud compute ssh --quiet --zone="${ZONE}" "kyma-integration-test-${RANDOM_ID}" -- ./kyma/installation/scripts/prow/kyma-integration-on-debian/deploy-and-test-kyma.sh
+gcloud compute ssh --quiet --zone="${ZONE}" "kyma-integration-test-${RANDOM_ID}" -- ./kyma/prow/kyma-integration-on-debian/deploy-and-test-kyma.sh
