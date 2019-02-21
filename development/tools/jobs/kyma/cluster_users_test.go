276bbbb915c3cc14a655c08e85e7cb7a1240d1d7
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2019-02-21 13:18:32 +0100

    Fix typo

--- development/tools/jobs/kyma/cluster_users_test.go
+++ development/tools/jobs/kyma/cluster_users_test.go
@@ -49,7 +49,7 @@ func TestClusterUsersTestJobPostsubmit(t *testing.T) {
 	assert.True(t, actualPost.Decorate)
 	assert.Equal(t, "github.com/kyma-project/kyma", actualPost.PathAlias)
 	tester.AssertThatHasExtraRefTestInfra(t, actualPost.JobBase.UtilityConfig, "master")
-	tester.AssertThatHasPresets(t, actualPost.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepo, tester.PresetGcrPush, tester.PresetBuildPr)
+	tester.AssertThatHasPresets(t, actualPost.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepo, tester.PresetGcrPush, tester.PresetBuildMaster)
 	assert.Equal(t, "^tests/cluster-users-test/", actualPost.RunIfChanged)
 	assert.Equal(t, tester.ImageBootstrapLatest, actualPost.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build.sh"}, actualPost.Spec.Containers[0].Command)
