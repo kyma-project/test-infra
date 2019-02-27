d698ca9428718f217361474d4194ad1214a1b04d
Krystian Cieslik <crabtree@users.noreply.github.com>
2019-02-27 09:26:45 +0100

    Add tests for *-master-kyma-gke-central jobs

--- prow/scripts/cluster-integration/kyma-gke-central.sh
+++ prow/scripts/cluster-integration/kyma-gke-central.sh
@@ -1,63 +1,61 @@
 #!/usr/bin/env bash
 
-# Description: Kyma Central plan on GKE. The purpose is to install and test Kyma on real GKE cluster in central mode.
+#Description: Kyma with central connector-service plan on GKE. This scripts implements a pipeline that consists of many steps. The purpose is to install and test Kyma on real GKE cluster with central connector-service.
 #
 #
-# Expected vars:
+#Expected vars:
 #
-#  - REPO_OWNER - Set up by prow, repository owner/organization
-#  - REPO_NAME - Set up by prow, repository name
-#  - BUILD_TYPE - Set up by prow, pr/master/release
-#  - DOCKER_PUSH_REPOSITORY - Docker repository hostname
-#  - DOCKER_PUSH_DIRECTORY - Docker "top-level" directory (with leading "/")
-#  - KYMA_PROJECT_DIR - directory path with Kyma sources to use for installation
-#  - CLOUDSDK_CORE_PROJECT - GCP project for all GCP resources used during execution (Service Account, IP Address, DNS Zone, image registry etc.)
-#  - CLOUDSDK_COMPUTE_REGION - GCP compute region
-#  - CLOUDSDK_DNS_ZONE_NAME - GCP zone name (not its DNS name!)
-#  - GOOGLE_APPLICATION_CREDENTIALS - GCP Service Account key file path
-#  - MACHINE_TYPE (optional): GKE machine type
-#  - CLUSTER_VERSION (optional): GKE cluster version
-#  - KYMA_ARTIFACTS_BUCKET: GCP bucket
+# - REPO_OWNER - Set up by prow, repository owner/organization
+# - REPO_NAME - Set up by prow, repository name
+# - BUILD_TYPE - Set up by prow, pr/master/release
+# - DOCKER_PUSH_REPOSITORY - Docker repository hostname
+# - DOCKER_PUSH_DIRECTORY - Docker "top-level" directory (with leading "/")
+# - KYMA_PROJECT_DIR - directory path with Kyma sources to use for installation
+# - CLOUDSDK_CORE_PROJECT - GCP project for all GCP resources used during execution (Service Account, IP Address, DNS Zone, image registry etc.)
+# - CLOUDSDK_COMPUTE_REGION - GCP compute region
+# - CLOUDSDK_DNS_ZONE_NAME - GCP zone name (not its DNS name!)
+# - GOOGLE_APPLICATION_CREDENTIALS - GCP Service Account key file path
+# - MACHINE_TYPE (optional): GKE machine type
+# - CLUSTER_VERSION (optional): GKE cluster version
+# - KYMA_ARTIFACTS_BUCKET: GCP bucket
 #
-# Permissions: In order to run this script you need to use a service account with permissions equivalent to the following GCP roles:
-#  - Compute Admin
-#  - Kubernetes Engine Admin
-#  - Kubernetes Engine Cluster Admin
-#  - DNS Administrator
-#  - Service Account User
-#  - Storage Admin
+#Permissions: In order to run this script you need to use a service account with permissions equivalent to the following GCP roles:
+# - Compute Admin
+# - Kubernetes Engine Admin
+# - Kubernetes Engine Cluster Admin
+# - DNS Administrator
+# - Service Account User
+# - Storage Admin
 
 set -o errexit
 
 discoverUnsetVar=false
 
 for var in REPO_OWNER REPO_NAME DOCKER_PUSH_REPOSITORY KYMA_PROJECT_DIR CLOUDSDK_CORE_PROJECT CLOUDSDK_COMPUTE_REGION CLOUDSDK_DNS_ZONE_NAME GOOGLE_APPLICATION_CREDENTIALS KYMA_ARTIFACTS_BUCKET; do
-    if [[ -z "${!var}" ]] ; then
+    if [ -z "${!var}" ] ; then
         echo "ERROR: $var is not set"
         discoverUnsetVar=true
     fi
 done
-if [[ "${discoverUnsetVar}" = true ]] ; then
+if [ "${discoverUnsetVar}" = true ] ; then
     exit 1
 fi
 
 #Exported variables
 export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
 export KYMA_SOURCES_DIR="${KYMA_PROJECT_DIR}/kyma"
-export KYMA_SCRIPTS_DIR="${KYMA_SOURCES_DIR}/installation/scripts"
 export TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS="${TEST_INFRA_SOURCES_DIR}/prow/scripts/cluster-integration/helpers"
-export KYMA_INSTALL_TIMEOUT="30m"
-
 # shellcheck disable=SC1090
 source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/library.sh"
 
 trap cleanup EXIT
 
+#!Put cleanup code in this function!
 cleanup() {
-    ## Save status of failed script execution
+    #!!! Must be at the beginning of this function !!!
     EXIT_STATUS=$?
 
-    if [[ "${ERROR_LOGGING_GUARD}" = "true" ]]; then
+    if [ "${ERROR_LOGGING_GUARD}" = "true" ]; then
         shout "AN ERROR OCCURED! Take a look at preceding log entries."
         echo
     fi
@@ -65,7 +63,7 @@ cleanup() {
     #Turn off exit-on-error so that next step is executed even if previous one fails.
     set +e
 
-    if [[ -n "${CLEANUP_CLUSTER}" ]]; then
+    if [ -n "${CLEANUP_CLUSTER}" ]; then
         shout "Deprovision cluster: \"${CLUSTER_NAME}\""
         date
 
@@ -86,7 +84,7 @@ cleanup() {
         if [[ ${TMP_STATUS} -ne 0 ]]; then EXIT_STATUS=${TMP_STATUS}; fi
     fi
 
-    if [[ -n "${CLEANUP_GATEWAY_DNS_RECORD}" ]]; then
+    if [ -n "${CLEANUP_GATEWAY_DNS_RECORD}" ]; then
         shout "Delete Gateway DNS Record"
         date
         IP_ADDRESS=${GATEWAY_IP_ADDRESS} DNS_FULL_NAME=${GATEWAY_DNS_FULL_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/delete-dns-record.sh"
@@ -94,7 +92,7 @@ cleanup() {
         if [[ ${TMP_STATUS} -ne 0 ]]; then EXIT_STATUS=${TMP_STATUS}; fi
     fi
 
-    if [[ -n "${CLEANUP_GATEWAY_IP_ADDRESS}" ]]; then
+    if [ -n "${CLEANUP_GATEWAY_IP_ADDRESS}" ]; then
         shout "Release Gateway IP Address"
         date
         IP_ADDRESS_NAME=${GATEWAY_IP_ADDRESS_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/release-ip-address.sh"
@@ -102,7 +100,7 @@ cleanup() {
         if [[ ${TMP_STATUS} -ne 0 ]]; then EXIT_STATUS=${TMP_STATUS}; fi
     fi
 
-    if [[ -n "${CLEANUP_REMOTEENVS_DNS_RECORD}" ]]; then
+    if [ -n "${CLEANUP_REMOTEENVS_DNS_RECORD}" ]; then
         shout "Delete Remote Environments DNS Record"
         date
         IP_ADDRESS=${REMOTEENVS_IP_ADDRESS} DNS_FULL_NAME=${REMOTEENVS_DNS_FULL_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/delete-dns-record.sh"
@@ -110,7 +108,7 @@ cleanup() {
         if [[ ${TMP_STATUS} -ne 0 ]]; then EXIT_STATUS=${TMP_STATUS}; fi
     fi
 
-    if [[ -n "${CLEANUP_REMOTEENVS_IP_ADDRESS}" ]]; then
+    if [ -n "${CLEANUP_REMOTEENVS_IP_ADDRESS}" ]; then
         shout "Release Remote Environments IP Address"
         date
         IP_ADDRESS_NAME=${REMOTEENVS_IP_ADDRESS_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/release-ip-address.sh"
@@ -118,7 +116,7 @@ cleanup() {
         if [[ ${TMP_STATUS} -ne 0 ]]; then EXIT_STATUS=${TMP_STATUS}; fi
     fi
 
-    if [[ -n "${CLEANUP_DOCKER_IMAGE}" ]]; then
+    if [ -n "${CLEANUP_DOCKER_IMAGE}" ]; then
         shout "Delete temporary Kyma-Installer Docker image"
         date
         "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/delete-image.sh"
@@ -136,137 +134,156 @@ cleanup() {
     exit "${EXIT_STATUS}"
 }
 
-function generateAndExportClusterName() {
-    readonly REPO_OWNER=$(echo "${REPO_OWNER}" | tr '[:upper:]' '[:lower:]')
-    readonly REPO_NAME=$(echo "${REPO_NAME}" | tr '[:upper:]' '[:lower:]')
-    readonly RANDOM_NAME_SUFFIX=$(LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c10)
+# Enforce lowercase
+readonly REPO_OWNER=$(echo "${REPO_OWNER}" | tr '[:upper:]' '[:lower:]')
+export REPO_OWNER
+readonly REPO_NAME=$(echo "${REPO_NAME}" | tr '[:upper:]' '[:lower:]')
+export REPO_NAME
 
-    if [[ "$BUILD_TYPE" == "pr" ]]; then
-        # In case of PR, operate on PR number
-        COMMON_NAME=$(echo "gke-central-pr-${PULL_NUMBER}-${RANDOM_NAME_SUFFIX}" | tr "[:upper:]" "[:lower:]")
-    elif [[ "$BUILD_TYPE" == "release" ]]; then
-        readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-        readonly RELEASE_VERSION=$(cat "${SCRIPT_DIR}/../../RELEASE_VERSION")
-        shout "Reading release version from RELEASE_VERSION file, got: ${RELEASE_VERSION}"
-        COMMON_NAME=$(echo "gke-central-rel-${RANDOM_NAME_SUFFIX}" | tr "[:upper:]" "[:lower:]")
-    else
-        # Otherwise (master), operate on triggering commit id
-        COMMIT_ID=$(cd "$KYMA_SOURCES_DIR" && git rev-parse --short HEAD)
-        COMMON_NAME=$(echo "gke-central-commit-${COMMIT_ID}-${RANDOM_NAME_SUFFIX}" | tr "[:upper:]" "[:lower:]")
-    fi
+RANDOM_NAME_SUFFIX=$(LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c10)
 
-    ### Cluster name must be less than 40 characters!
-    export CLUSTER_NAME="${COMMON_NAME}"
-}
+if [[ "$BUILD_TYPE" == "pr" ]]; then
+    # In case of PR, operate on PR number
+    COMMON_NAME=$(echo "gkeint-pr-${PULL_NUMBER}-${RANDOM_NAME_SUFFIX}" | tr "[:upper:]" "[:lower:]")
+    KYMA_INSTALLER_IMAGE="${DOCKER_PUSH_REPOSITORY}${DOCKER_PUSH_DIRECTORY}/gke-central-connector/${REPO_OWNER}/${REPO_NAME}:PR-${PULL_NUMBER}"
+    export KYMA_INSTALLER_IMAGE
+elif [[ "$BUILD_TYPE" == "release" ]]; then
+    readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    readonly RELEASE_VERSION=$(cat "${SCRIPT_DIR}/../../RELEASE_VERSION")
+    shout "Reading release version from RELEASE_VERSION file, got: ${RELEASE_VERSION}"
+    COMMON_NAME=$(echo "gkeint-rel-${RANDOM_NAME_SUFFIX}" | tr "[:upper:]" "[:lower:]")
+else
+    # Otherwise (master), operate on triggering commit id
+    readonly COMMIT_ID=$(cd "$KYMA_SOURCES_DIR" && git rev-parse --short HEAD)
+    COMMON_NAME=$(echo "gkeint-commit-${COMMIT_ID}-${RANDOM_NAME_SUFFIX}" | tr "[:upper:]" "[:lower:]")
+    KYMA_INSTALLER_IMAGE="${DOCKER_PUSH_REPOSITORY}${DOCKER_PUSH_DIRECTORY}/gke-central-connector/${REPO_OWNER}/${REPO_NAME}:COMMIT-${COMMIT_ID}"
+    export KYMA_INSTALLER_IMAGE
+fi
 
-function reserveIPsAndCreateDNSRecords() {
-    DNS_SUBDOMAIN="${COMMON_NAME}"
-    shout "Authenticate with GCP"
+
+### Cluster name must be less than 40 characters!
+export CLUSTER_NAME="${COMMON_NAME}"
+
+### For provision-gke-cluster.sh
+export GCLOUD_PROJECT_NAME="${CLOUDSDK_CORE_PROJECT}"
+export GCLOUD_COMPUTE_ZONE="${CLOUDSDK_COMPUTE_ZONE}"
+
+#Local variables
+DNS_SUBDOMAIN="${COMMON_NAME}"
+KYMA_SCRIPTS_DIR="${KYMA_SOURCES_DIR}/installation/scripts"
+KYMA_RESOURCES_DIR="${KYMA_SOURCES_DIR}/installation/resources"
+
+INSTALLER_YAML="${KYMA_RESOURCES_DIR}/installer.yaml"
+INSTALLER_CONFIG="${KYMA_RESOURCES_DIR}/installer-config-cluster.yaml.tpl"
+INSTALLER_CR="${KYMA_RESOURCES_DIR}/installer-cr-cluster.yaml.tpl"
+
+#Used to detect errors for logging purposes
+ERROR_LOGGING_GUARD="true"
+
+shout "Authenticate"
+date
+init
+DNS_DOMAIN="$(gcloud dns managed-zones describe "${CLOUDSDK_DNS_ZONE_NAME}" --format="value(dnsName)")"
+
+if [[ "$BUILD_TYPE" != "release" ]]; then
+    shout "Build Kyma-Installer Docker image"
     date
-    init
+    CLEANUP_DOCKER_IMAGE="true"
+    "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-image.sh"
+fi
 
-    DNS_DOMAIN="$(gcloud dns managed-zones describe "${CLOUDSDK_DNS_ZONE_NAME}" --format="value(dnsName)")"
+shout "Reserve IP Address for Ingressgateway"
+date
+GATEWAY_IP_ADDRESS_NAME="${COMMON_NAME}"
+GATEWAY_IP_ADDRESS=$(IP_ADDRESS_NAME=${GATEWAY_IP_ADDRESS_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/reserve-ip-address.sh")
+CLEANUP_GATEWAY_IP_ADDRESS="true"
+echo "Created IP Address for Ingressgateway: ${GATEWAY_IP_ADDRESS}"
 
-    shout "Reserve IP Address for Ingressgateway"
-    date
-    GATEWAY_IP_ADDRESS_NAME="${COMMON_NAME}"
-    GATEWAY_IP_ADDRESS=$(IP_ADDRESS_NAME=${GATEWAY_IP_ADDRESS_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/reserve-ip-address.sh")
-    CLEANUP_GATEWAY_IP_ADDRESS="true"
-    echo "Created IP Address for Ingressgateway: ${GATEWAY_IP_ADDRESS}"
 
-    shout "Create DNS Record for Ingressgateway IP"
-    date
-    GATEWAY_DNS_FULL_NAME="*.${DNS_SUBDOMAIN}.${DNS_DOMAIN}"
-    CLEANUP_GATEWAY_DNS_RECORD="true"
-    IP_ADDRESS=${GATEWAY_IP_ADDRESS} DNS_FULL_NAME=${GATEWAY_DNS_FULL_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-dns-record.sh"
+shout "Create DNS Record for Ingressgateway IP"
+date
+GATEWAY_DNS_FULL_NAME="*.${DNS_SUBDOMAIN}.${DNS_DOMAIN}"
+CLEANUP_GATEWAY_DNS_RECORD="true"
+IP_ADDRESS=${GATEWAY_IP_ADDRESS} DNS_FULL_NAME=${GATEWAY_DNS_FULL_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-dns-record.sh"
 
-    shout "Reserve IP Address for Remote Environments"
-    date
-    REMOTEENVS_IP_ADDRESS_NAME="remoteenvs-${COMMON_NAME}"
-    REMOTEENVS_IP_ADDRESS=$(IP_ADDRESS_NAME=${REMOTEENVS_IP_ADDRESS_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/reserve-ip-address.sh")
-    CLEANUP_REMOTEENVS_IP_ADDRESS="true"
-    echo "Created IP Address for Remote Environments: ${REMOTEENVS_IP_ADDRESS}"
 
-    shout "Create DNS Record for Remote Environments IP"
-    date
-    REMOTEENVS_DNS_FULL_NAME="gateway.${DNS_SUBDOMAIN}.${DNS_DOMAIN}"
-    CLEANUP_REMOTEENVS_DNS_RECORD="true"
-    IP_ADDRESS=${REMOTEENVS_IP_ADDRESS} DNS_FULL_NAME=${REMOTEENVS_DNS_FULL_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-dns-record.sh"
+shout "Reserve IP Address for Remote Environments"
+date
+REMOTEENVS_IP_ADDRESS_NAME="remoteenvs-${COMMON_NAME}"
+REMOTEENVS_IP_ADDRESS=$(IP_ADDRESS_NAME=${REMOTEENVS_IP_ADDRESS_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/reserve-ip-address.sh")
+CLEANUP_REMOTEENVS_IP_ADDRESS="true"
+echo "Created IP Address for Remote Environments: ${REMOTEENVS_IP_ADDRESS}"
 
-    DOMAIN="${DNS_SUBDOMAIN}.${DNS_DOMAIN%?}"
-    export DOMAIN
-}
 
-function generateAndExportCerts() {
-    shout "Generate self-signed certificate"
-    date
-    CERT_KEY=$("${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/generate-self-signed-cert.sh")
+shout "Create DNS Record for Remote Environments IP"
+date
+REMOTEENVS_DNS_FULL_NAME="gateway.${DNS_SUBDOMAIN}.${DNS_DOMAIN}"
+CLEANUP_REMOTEENVS_DNS_RECORD="true"
+IP_ADDRESS=${REMOTEENVS_IP_ADDRESS} DNS_FULL_NAME=${REMOTEENVS_DNS_FULL_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-dns-record.sh"
 
-    TLS_CERT=$(echo "${CERT_KEY}" | head -1)
-    export TLS_CERT
-    TLS_KEY=$(echo "${CERT_KEY}" | tail -1)
-    export TLS_KEY
-}
 
-function createCluster() {
-    shout "Provision cluster: \"${CLUSTER_NAME}\""
-    date
-    ### For provision-gke-cluster.sh
-    export GCLOUD_SERVICE_KEY_PATH="${GOOGLE_APPLICATION_CREDENTIALS}"
-    export GCLOUD_PROJECT_NAME="${CLOUDSDK_CORE_PROJECT}"
-    export GCLOUD_COMPUTE_ZONE="${CLOUDSDK_COMPUTE_ZONE}"
-    if [[ -z "${MACHINE_TYPE}" ]]; then
-        export MACHINE_TYPE="${DEFAULT_MACHINE_TYPE}"
-    fi
-    if [[ -z "${CLUSTER_VERSION}" ]]; then
-        export CLUSTER_VERSION="${DEFAULT_CLUSTER_VERSION}"
-    fi
+shout "Provision cluster: \"${CLUSTER_NAME}\""
+date
+export GCLOUD_SERVICE_KEY_PATH="${GOOGLE_APPLICATION_CREDENTIALS}"
+if [ -z "$MACHINE_TYPE" ]; then
+      export MACHINE_TYPE="${DEFAULT_MACHINE_TYPE}"
+fi
+if [ -z "${CLUSTER_VERSION}" ]; then
+      export CLUSTER_VERSION="${DEFAULT_CLUSTER_VERSION}"
+fi
+CLEANUP_CLUSTER="true"
+"${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/provision-gke-cluster.sh"
 
-    "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/provision-gke-cluster.sh"
-    CLEANUP_CLUSTER="true"
-}
 
-function installKyma() {
-    shout "Install Tiller"
-    date
-    kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user="$(gcloud config get-value account)"
-    "${KYMA_SCRIPTS_DIR}"/install-tiller.sh
+shout "Install Tiller"
+date
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user="$(gcloud config get-value account)"
+"${KYMA_SCRIPTS_DIR}"/install-tiller.sh
 
-    shout "Apply Kyma config"
-    date
 
-    if [[ "$BUILD_TYPE" == "release" ]]; then
-        echo "Use released artifacts"
-        gsutil cp "${KYMA_ARTIFACTS_BUCKET}/${RELEASE_VERSION}/kyma-config-cluster.yaml" /tmp/kyma-gke-central/downloaded-config.yaml
-        gsutil cp "${KYMA_ARTIFACTS_BUCKET}/${RELEASE_VERSION}/kyma-installer-cluster.yaml" /tmp/kyma-gke-central/downloaded-installer.yaml
+shout "Generate self-signed certificate"
+date
+DOMAIN="${DNS_SUBDOMAIN}.${DNS_DOMAIN%?}"
+export DOMAIN
+CERT_KEY=$("${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/generate-self-signed-cert.sh")
+TLS_CERT=$(echo "${CERT_KEY}" | head -1)
+TLS_KEY=$(echo "${CERT_KEY}" | tail -1)
 
-        kubectl apply -f /tmp/kyma-gke-central/downloaded-installer.yaml
+shout "Apply Kyma config"
+date
 
-        sed -e "s/__DOMAIN__/${DOMAIN}/g" /tmp/kyma-gke-central/downloaded-config.yaml \
-            | sed -e "s/__REMOTE_ENV_IP__/${REMOTEENVS_IP_ADDRESS}/g" \
-            | sed -e "s/__TLS_CERT__/${TLS_CERT}/g" \
-            | sed -e "s/__TLS_KEY__/${TLS_KEY}/g" \
-            | sed -e "s/__EXTERNAL_PUBLIC_IP__/${GATEWAY_IP_ADDRESS}/g" \
-            | sed -e "s/__SKIP_SSL_VERIFY__/true/g" \
-            | sed -e "s/__.*__//g" \
-            | kubectl apply -f-
-    else
-        echo "Manual concatenating yamls"
-        "${KYMA_SCRIPTS_DIR}"/concat-yamls.sh "${INSTALLER_YAML}" "${INSTALLER_CONFIG}" "${INSTALLER_CR}" \
-        | sed -e 's;image: eu.gcr.io/kyma-project/.*/installer:.*$;'"image: ${KYMA_INSTALLER_IMAGE};" \
-        | sed -e "s/__DOMAIN__/${DOMAIN}/g" \
+if [[ "$BUILD_TYPE" == "release" ]]; then
+    echo "Use released artifacts"
+    gsutil cp "${KYMA_ARTIFACTS_BUCKET}/${RELEASE_VERSION}/kyma-config-cluster.yaml" /tmp/kyma-gke-central/downloaded-config.yaml
+    gsutil cp "${KYMA_ARTIFACTS_BUCKET}/${RELEASE_VERSION}/kyma-installer-cluster.yaml" /tmp/kyma-gke-central/downloaded-installer.yaml
+
+    kubectl apply -f /tmp/kyma-gke-central/downloaded-installer.yaml
+
+    sed -e "s/__DOMAIN__/${DOMAIN}/g" /tmp/kyma-gke-central/downloaded-config.yaml \
         | sed -e "s/__REMOTE_ENV_IP__/${REMOTEENVS_IP_ADDRESS}/g" \
         | sed -e "s/__TLS_CERT__/${TLS_CERT}/g" \
         | sed -e "s/__TLS_KEY__/${TLS_KEY}/g" \
         | sed -e "s/__EXTERNAL_PUBLIC_IP__/${GATEWAY_IP_ADDRESS}/g" \
         | sed -e "s/__SKIP_SSL_VERIFY__/true/g" \
-        | sed -e "s/__VERSION__/0.0.1/g" \
         | sed -e "s/__.*__//g" \
         | kubectl apply -f-
-    fi
+else
+    echo "Manual concatenating yamls"
+    "${KYMA_SCRIPTS_DIR}"/concat-yamls.sh "${INSTALLER_YAML}" "${INSTALLER_CONFIG}" "${INSTALLER_CR}" \
+    | sed -e 's;image: eu.gcr.io/kyma-project/.*/installer:.*$;'"image: ${KYMA_INSTALLER_IMAGE};" \
+    | sed -e "s/__DOMAIN__/${DOMAIN}/g" \
+    | sed -e "s/__REMOTE_ENV_IP__/${REMOTEENVS_IP_ADDRESS}/g" \
+    | sed -e "s/__TLS_CERT__/${TLS_CERT}/g" \
+    | sed -e "s/__TLS_KEY__/${TLS_KEY}/g" \
+    | sed -e "s/__EXTERNAL_PUBLIC_IP__/${GATEWAY_IP_ADDRESS}/g" \
+    | sed -e "s/__SKIP_SSL_VERIFY__/true/g" \
+    | sed -e "s/__VERSION__/0.0.1/g" \
+    | sed -e "s/__.*__//g" \
+    | kubectl apply -f-
+fi
 
-    shout "Apply override for central connector-service"
-    cat <<EOF | kubectl apply -f -
+shout "Apply override for central connector-service"
+cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: ConfigMap
 metadata:
@@ -277,36 +294,19 @@ metadata:
     kyma-project.io/installation: ""
 data:
   connector-service.deployment.args.central: "true"
+  connector-service.tests.central: "true"
 EOF
 
-    shout "Trigger installation with timeout ${KYMA_INSTALL_TIMEOUT}"
-    date
-    kubectl label installation/kyma-installation action=install
-    "${KYMA_SCRIPTS_DIR}"/is-installed.sh --timeout ${KYMA_INSTALL_TIMEOUT}
-}
+shout "Trigger installation"
+date
+kubectl label installation/kyma-installation action=install
+"${KYMA_SCRIPTS_DIR}"/is-installed.sh --timeout 30m
 
-function testKyma() {
-    shout "Test Kyma"
-    date
-    "${KYMA_SCRIPTS_DIR}"/testing.sh
-}
+shout "Test Kyma"
+date
+"${KYMA_SCRIPTS_DIR}"/testing.sh
 
-# Used to detect errors for logging purposes
-ERROR_LOGGING_GUARD="true"
+shout "Success"
 
-generateAndExportClusterName
-
-reserveIPsAndCreateDNSRecords
-
-generateAndExportCerts
-
-createCluster
-
-installKyma
-
-testKyma
-
-shout "Job finished with success"
-
-# Mark execution as successfully
+#!!! Must be at the end of the script !!!
 ERROR_LOGGING_GUARD="false"
