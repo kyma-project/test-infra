610cc221bf13be041bbaf1b68beb8f9fd9c696bc
Sayan Hazra <sayan.mnit@gmail.com>
2019-02-28 14:22:57 +0100

    Fix the name of the job and path it should scan for changes (#676)
    * Fix the name of the job and path it should scan for changes

--- development/tools/jobs/kyma/end_to_end_kubeless_integration_tests_test.go
+++ development/tools/jobs/kyma/end_to_end_kubeless_integration_tests_test.go
@@ -14,9 +14,9 @@ func TestKubelessIntegrationTestsJobsPresubmit(t *testing.T) {
 	// THEN
 	require.NoError(t, err)
 
-	actualPresubmit := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-project/kyma"], "pre-master-kyma-tests-kubeless-integration", "master")
+	actualPresubmit := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-project/kyma"], "pre-master-kyma-tests-end-to-end-kubeless-integration", "master")
 
-	expName := "pre-master-kyma-tests-kubeless-integration"
+	expName := "pre-master-kyma-tests-end-to-end-kubeless-integration"
 	assert.Equal(t, expName, actualPresubmit.Name)
 	assert.Equal(t, []string{"master"}, actualPresubmit.Branches)
 	assert.Equal(t, 10, actualPresubmit.MaxConcurrency)
@@ -25,7 +25,7 @@ func TestKubelessIntegrationTestsJobsPresubmit(t *testing.T) {
 	assert.Equal(t, "github.com/kyma-project/kyma", actualPresubmit.PathAlias)
 	tester.AssertThatHasExtraRefTestInfra(t, actualPresubmit.JobBase.UtilityConfig, "master")
 	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepo, tester.PresetGcrPush, tester.PresetBuildPr)
-	assert.Equal(t, "^tests/kubeless-integration/", actualPresubmit.RunIfChanged)
+	assert.Equal(t, "^tests/end-to-end/kubeless-integration/", actualPresubmit.RunIfChanged)
 	assert.Equal(t, tester.ImageGolangBuildpackLatest, actualPresubmit.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build.sh"}, actualPresubmit.Spec.Containers[0].Command)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/kyma/tests/end-to-end/kubeless-integration"}, actualPresubmit.Spec.Containers[0].Args)
@@ -43,7 +43,7 @@ func TestKubelessIntegrationTestsJobPostsubmit(t *testing.T) {
 	assert.Len(t, kymaPost, 1)
 
 	actualPost := kymaPost[0]
-	expName := "post-master-kyma-tests-kubeless-integration"
+	expName := "post-master-kyma-tests-end-to-end-kubeless-integration"
 	assert.Equal(t, expName, actualPost.Name)
 	assert.Equal(t, []string{"master"}, actualPost.Branches)
 
@@ -52,7 +52,7 @@ func TestKubelessIntegrationTestsJobPostsubmit(t *testing.T) {
 	assert.Equal(t, "github.com/kyma-project/kyma", actualPost.PathAlias)
 	tester.AssertThatHasExtraRefTestInfra(t, actualPost.JobBase.UtilityConfig, "master")
 	tester.AssertThatHasPresets(t, actualPost.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepo, tester.PresetGcrPush, tester.PresetBuildMaster)
-	assert.Equal(t, "^tests/kubeless-integration/", actualPost.RunIfChanged)
+	assert.Equal(t, "^tests/end-to-end/kubeless-integration/", actualPost.RunIfChanged)
 	assert.Equal(t, tester.ImageGolangBuildpackLatest, actualPost.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build.sh"}, actualPost.Spec.Containers[0].Command)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/kyma/tests/end-to-end/kubeless-integration"}, actualPost.Spec.Containers[0].Args)
@@ -80,7 +80,7 @@ func TestKubelessIntegrationReleases(t *testing.T) {
 				jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/kyma/tests/end-to-end/kubeless-integration/kubeless-integration.yaml")
 				// THEN
 				require.NoError(t, err)
-				actualPresubmit := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-project/kyma"], tester.GetReleaseJobName("kyma-tests-kubeless-integration", currentRelease), currentRelease)
+				actualPresubmit := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-project/kyma"], tester.GetReleaseJobName("kyma-tests-end-to-end-kubeless-integration", currentRelease), currentRelease)
 				require.NotNil(t, actualPresubmit)
 				assert.False(t, actualPresubmit.SkipReport)
 				assert.True(t, actualPresubmit.Decorate)
