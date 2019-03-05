f526e3d5ca0a80a3782d07f12a6da381c718fbdb
Mateusz Puczyński <mati6095@gmail.com>
2019-03-05 08:53:56 +0100

    Add OData component Prowjob (#670)
    * add react-odata prowjob
    
    * fix tests

--- development/tools/jobs/console/react-odata_test.go
+++ development/tools/jobs/console/react-odata_test.go
@@ -8,13 +8,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCatalogJobPresubmit(t *testing.T) {
+var jobConfigPath = "./../../../../prow/jobs/console/components/react-odata/react-odata.yaml"
+var runIfChangedConfig = "^components/react-odata/"
+var buildScriptPath = "/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build.sh"
+var ODataComponentPath = "/home/prow/go/src/github.com/kyma-project/console/components/react-odata"
+
+func TestReactODataJobPresubmit(t *testing.T) {
 	// WHEN
-	jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/console/catalog/catalog.yaml")
+	jobConfig, err := tester.ReadJobConfig(jobConfigPath)
 	// THEN
 	require.NoError(t, err)
 
-	expName := "pre-master-console-catalog"
+	expName := "pre-master-console-react-odata"
 	actualPresubmit := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-project/console"], expName, "master")
 	require.NotNil(t, actualPresubmit)
 	assert.Equal(t, expName, actualPresubmit.Name)
@@ -25,21 +30,21 @@ func TestCatalogJobPresubmit(t *testing.T) {
 	assert.False(t, actualPresubmit.Optional)
 	assert.Equal(t, "github.com/kyma-project/console", actualPresubmit.PathAlias)
 	tester.AssertThatHasExtraRefTestInfra(t, actualPresubmit.JobBase.UtilityConfig, "master")
-	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepo, tester.PresetGcrPush, tester.PresetBuildPr)
-	assert.Equal(t, "^catalog/", actualPresubmit.RunIfChanged)
-	tester.AssertThatJobRunIfChanged(t, *actualPresubmit, "catalog/some_random_file.js")
+	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, tester.PresetBuildPr)
+	assert.Equal(t, runIfChangedConfig, actualPresubmit.RunIfChanged)
+	tester.AssertThatJobRunIfChanged(t, *actualPresubmit, "components/react-odata/some_random_file.js")
 	assert.Equal(t, tester.ImageNodeBuildpackLatest, actualPresubmit.Spec.Containers[0].Image)
-	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build.sh"}, actualPresubmit.Spec.Containers[0].Command)
-	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/console/catalog"}, actualPresubmit.Spec.Containers[0].Args)
+	assert.Equal(t, []string{buildScriptPath}, actualPresubmit.Spec.Containers[0].Command)
+	assert.Equal(t, []string{ODataComponentPath}, actualPresubmit.Spec.Containers[0].Args)
 }
 
-func TestCatalogJobPostsubmit(t *testing.T) {
+func TestReactODataJobPostsubmit(t *testing.T) {
 	// WHEN
-	jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/console/catalog/catalog.yaml")
+	jobConfig, err := tester.ReadJobConfig(jobConfigPath)
 	// THEN
 	require.NoError(t, err)
 
-	expName := "post-master-console-catalog"
+	expName := "post-master-console-react-odata"
 	actualPost := tester.FindPostsubmitJobByName(jobConfig.Postsubmits["kyma-project/console"], expName, "master")
 	require.NotNil(t, actualPost)
 	assert.Equal(t, expName, actualPost.Name)
@@ -49,9 +54,9 @@ func TestCatalogJobPostsubmit(t *testing.T) {
 	assert.True(t, actualPost.Decorate)
 	assert.Equal(t, "github.com/kyma-project/console", actualPost.PathAlias)
 	tester.AssertThatHasExtraRefTestInfra(t, actualPost.JobBase.UtilityConfig, "master")
-	tester.AssertThatHasPresets(t, actualPost.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepo, tester.PresetGcrPush, tester.PresetBuildConsoleMaster)
-	assert.Equal(t, "^catalog/", actualPost.RunIfChanged)
+	tester.AssertThatHasPresets(t, actualPost.JobBase, tester.PresetBuildConsoleMaster)
+	assert.Equal(t, runIfChangedConfig, actualPost.RunIfChanged)
 	assert.Equal(t, tester.ImageNodeBuildpackLatest, actualPost.Spec.Containers[0].Image)
-	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build.sh"}, actualPost.Spec.Containers[0].Command)
-	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/console/catalog"}, actualPost.Spec.Containers[0].Args)
+	assert.Equal(t, []string{buildScriptPath}, actualPost.Spec.Containers[0].Command)
+	assert.Equal(t, []string{ODataComponentPath}, actualPost.Spec.Containers[0].Args)
 }
