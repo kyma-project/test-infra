03052ab425fb9652e36387020a645d4782498695
santiago ventura <santiago.ventura@sap.com>
2019-03-05 02:59:43 -0800

    Fix deletetion of docker image in gke cluster script

--- prow/scripts/cluster-integration/kyma-gke-end-to-end-test.sh
+++ prow/scripts/cluster-integration/kyma-gke-end-to-end-test.sh
@@ -48,7 +48,7 @@ removeCluster() {
     EXIT_STATUS=$?
 
     shout "Fetching OLD_TIMESTAMP from cluster to be deleted"
-	readonly OLD_TIMESTAMP=$(gcloud container clusters describe "${CLUSTER_NAME}" --zone="${GCLOUD_COMPUTE_ZONE}" --project="${GCLOUD_PROJECT_NAME}" --format=json | jq --raw-output '.resourceLabels."created-at"')
+	readonly OLD_TIMESTAMP=$(gcloud container clusters describe "${CLUSTER_NAME}" --zone="${GCLOUD_COMPUTE_ZONE}" --project="${GCLOUD_PROJECT_NAME}"  --format=json | jq --raw-output '.createTime' | cut -f1 -d"T" | sed "s/-//g")
 
     shout "Deprovision cluster: \"${CLUSTER_NAME}\""
     date
