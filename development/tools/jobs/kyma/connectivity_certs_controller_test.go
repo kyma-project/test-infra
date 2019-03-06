951381dfcfe62ff2dc9953ab70b9a9e9f4cace68
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2019-03-06 14:18:02 +0100

    add test functions for new release jobs

--- development/tools/jobs/kyma/connectivity_certs_controller_test.go
+++ development/tools/jobs/kyma/connectivity_certs_controller_test.go
@@ -8,6 +8,28 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestConnectivityCertsControllerReleases(t *testing.T) {
+	// WHEN
+	unsupportedReleases := []string{"release-0.6", "release-0.7"}
+
+	for _, currentRelease := range getSupportedReleases(unsupportedReleases) {
+		t.Run(currentRelease, func(t *testing.T) {
+            jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/kyma/components/connectivity-certs-controller/connectivity-certs-controller.yaml")
+			// THEN
+			require.NoError(t, err)
+			actualPresubmit := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-project/kyma"], tester.GetReleaseJobName("kyma-components-connectivity-certs-controller", currentRelease), currentRelease)
+			require.NotNil(t, actualPresubmit)
+			assert.False(t, actualPresubmit.SkipReport)
+			assert.True(t, actualPresubmit.Decorate)
+			assert.Equal(t, "github.com/kyma-project/kyma", actualPresubmit.PathAlias)
+			tester.AssertThatHasExtraRefTestInfra(t, actualPresubmit.JobBase.UtilityConfig, currentRelease)
+			tester.AssertThatHasPresets(t, actualPresubmit.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepo, tester.PresetGcrPush, tester.PresetBuildRelease)
+			assert.True(t, actualPresubmit.AlwaysRun)
+			tester.AssertThatExecGolangBuildpack(t, actualPresubmit.JobBase, tester.ImageGolangBuildpackLatest, "/home/prow/go/src/github.com/kyma-project/kyma/components/connectivity-certs-controller")
+		})
+	}
+}
+
 func TestConnectivityCertsControllerJobsPresubmit(t *testing.T) {
 	// WHEN
 	jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/kyma/components/connectivity-certs-controller/connectivity-certs-controller.yaml")
