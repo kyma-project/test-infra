2fcf242ab15bdb92f1f3ac5fc6bc237a04e6e0ef
tuxgoose <1phillip.guzman@gmail.com>
2019-03-08 12:21:45 -0600

    updating the kyma-gke-nightly every 10 minutes no docker run

--- prow/scripts/cluster-integration/kyma-gke-long-lasting.sh
+++ prow/scripts/cluster-integration/kyma-gke-long-lasting.sh
@@ -178,19 +178,21 @@ function generateAndExportLetsEncryptCert() {
 
     mkdir letsencrypt
     cp /etc/credentials/sa-gke-kyma-integration/service-account.json letsencrypt
-    docker run  --name certbot \
-        --rm  \
-        -v "$(pwd)/letsencrypt:/etc/letsencrypt"    \
-        certbot/dns-google \
-        certonly \
-        -m "kyma.bot@sap.com" \
-        --agree-tos \
-        --no-eff-email \
-        --dns-google \
-        --dns-google-credentials /etc/letsencrypt/service-account.json \
-        --server https://acme-v02.api.letsencrypt.org/directory \
-        --dns-google-propagation-seconds=600 \
-        -d "*.${DOMAIN}"
+	gsutil cp gs://kyma-prow-secrets/nightly-aks-github-integration-app-client-id.encrypted .
+	ls night* | wc -l
+    # docker run  --name certbot \
+    #     --rm  \
+    #     -v "$(pwd)/letsencrypt:/etc/letsencrypt"    \
+    #     certbot/dns-google \
+    #     certonly \
+    #     -m "kyma.bot@sap.com" \
+    #     --agree-tos \
+    #     --no-eff-email \
+    #     --dns-google \
+    #     --dns-google-credentials /etc/letsencrypt/service-account.json \
+    #     --server https://acme-v02.api.letsencrypt.org/directory \
+    #     --dns-google-propagation-seconds=600 \
+    #     -d "*.${DOMAIN}"
 
     TLS_CERT=$(base64 -i ./letsencrypt/live/"${DOMAIN}"/fullchain.pem | tr -d '\n')
     export TLS_CERT
