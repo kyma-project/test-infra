2337aaab8b5de97d7cd752a84e8143b1c2d6f5c3
Krystian Cieślik <3641496+crabtree@users.noreply.github.com>
2019-03-08 07:56:00 +0100

    Create network before cluster provisioning (#708)
    * Create network before cluster provisioning

---
+++ prow/scripts/cluster-integration/helpers/delete-network.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Description: Deletes the GCP network for cluster
+
+# Expected vars:
+# - GCLOUD_NETWORK_NAME - name for the new GCP network
+# - GCLOUD_PROJECT_NAME - name of GCP project
+
+set -o errexit
+
+discoverUnsetVar=false
+
+for var in GCLOUD_NETWORK_NAME GCLOUD_PROJECT_NAME; do
+    if [ -z "${!var}" ] ; then
+        echo "ERROR: $var is not set"
+        discoverUnsetVar=true
+    fi
+done
+if [ "${discoverUnsetVar}" = true ] ; then
+    exit 1
+fi
+
+gcloud compute networks delete "${GCLOUD_NETWORK_NAME}" \
+ --project="${GCLOUD_PROJECT_NAME}"
