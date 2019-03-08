8fd9bee1dea6b9fae02e07d5a14fad4cafb73af5
Krystian Cieslik <crabtree@users.noreply.github.com>
2019-03-08 13:56:22 +0100

    Add region variable

--- prow/scripts/cluster-integration/helpers/create-network-with-subnet.sh
+++ prow/scripts/cluster-integration/helpers/create-network-with-subnet.sh
@@ -6,12 +6,13 @@
 # - GCLOUD_NETWORK_NAME - name for the new GCP network
 # - GCLOUD_SUBNET_NAME - name for the subnet of the network
 # - GCLOUD_PROJECT_NAME - name of GCP project
+# - CLOUDSDK_COMPUTE_REGION - name of the GCP region for the network resource
 
 set -o errexit
 
 discoverUnsetVar=false
 
-for var in GCLOUD_NETWORK_NAME GCLOUD_SUBNET_NAME GCLOUD_PROJECT_NAME; do
+for var in GCLOUD_NETWORK_NAME GCLOUD_SUBNET_NAME GCLOUD_PROJECT_NAME CLOUDSDK_COMPUTE_REGION; do
     if [ -z "${!var}" ] ; then
         echo "ERROR: $var is not set"
         discoverUnsetVar=true
@@ -23,6 +24,7 @@ fi
 
 gcloud compute networks create "${GCLOUD_NETWORK_NAME}" \
  --project="${GCLOUD_PROJECT_NAME}" \
+ --region="${CLOUDSDK_COMPUTE_REGION}" \
  --subnet-mode=custom
 
 gcloud compute networks subnets create "${GCLOUD_SUBNET_NAME}" \
