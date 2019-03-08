b059ff99d1dc4edaea1e591b6fd1e469602ce167
Jasiak <piotr.jasiak@sap.com>
2019-03-08 15:12:26 +0100

    change slack func

--- development/tools/pkg/synchronizer/tools/slack.go
+++ development/tools/pkg/synchronizer/tools/slack.go
@@ -1,7 +1,6 @@
 package tools
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/nlopes/slack"
@@ -25,12 +24,8 @@ var (
 // SendMessage sent message to specifuc channel based on slack token
 func SendMessage(token, channelName string, msg []Message) error {
 	api := slack.New(token)
-	channelID, err := findChannelID(api, channelName)
-	if err != nil {
-		return errors.Wrap(err, "during find channel ID")
-	}
 
-	resp, timestamp, err := api.PostMessage(channelID,
+	resp, timestamp, err := api.PostMessage(channelName,
 		slack.MsgOptionUsername(username),
 		slack.MsgOptionText(messageHeader, false),
 		slack.MsgOptionPostMessageParameters(slack.PostMessageParameters{IconEmoji: icon, Markdown: true}),
@@ -42,21 +37,6 @@ func SendMessage(token, channelName string, msg []Message) error {
 
 	log.Printf("Slack sent message to channel %q, slack response: %q, slack timestamp: %s", channelName, resp, timestamp)
 	return nil
-}
-
-func findChannelID(api *slack.Client, name string) (string, error) {
-	ch, err := api.GetChannels(false)
-	if err != nil {
-		return "", errors.Wrap(err, "during fetch channels list")
-	}
-
-	for _, slackChannel := range ch {
-		if slackChannel.Name == name {
-			return slackChannel.ID, nil
-		}
-	}
-
-	return "", fmt.Errorf("Cannot find channel with name %q on slack", name)
 }
 
 func generateMessage(content []Message) slack.Attachment {
