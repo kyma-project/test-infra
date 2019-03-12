ab749a1942434099eeb80feb024721c56709c40f
Michał Hudy <michal@hudy.pl>
2019-03-12 18:42:34 +0100

    Fix docsbuilder and failery jobs

--- development/tools/jobs/kyma/failery_test.go
+++ development/tools/jobs/kyma/failery_test.go
@@ -61,6 +61,7 @@ func TestFaileryJobPostsubmit(t *testing.T) {
 	tester.AssertThatHasExtraRefTestInfra(t, actualPost.JobBase.UtilityConfig, "master")
 	tester.AssertThatHasPresets(t, actualPost.JobBase, tester.PresetBuildMaster)
 	assert.Equal(t, tester.ImageGolangBuildpackLatest, actualPost.Spec.Containers[0].Image)
+	assert.Equal(t, "^tools/failery/", actualPost.RunIfChanged)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build.sh"}, actualPost.Spec.Containers[0].Command)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/kyma/tools/failery"}, actualPost.Spec.Containers[0].Args)
 }
