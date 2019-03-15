1b6ef4a578c6ef0d0505f94cda2b22f2eaae61c5
Daniel Roth <daniel.roth02@sap.com>
2019-03-15 15:03:17 -0700

    enable paging for github api

--- development/tools/pkg/firewallcleaner/github.go
+++ development/tools/pkg/firewallcleaner/github.go
@@ -41,21 +41,29 @@ func NewGithubAPI(ctx context.Context, githubAccessToken, repoOwner, repoName st
 //ClosedPullRequests returns a list of pull requests, that are closed for a long enough time (currently 3 hour window).
 func (g *githubAPIWrapper) ClosedPullRequests(ctx context.Context) []*github.PullRequest {
 	opts := &github.PullRequestListOptions{State: "closed"}
-	pulls, resp, err := g.githubClient.PullRequests.List(ctx, g.repoOwner, g.repoName, opts)
 	var prList []*github.PullRequest
-	if err != nil {
-		common.Shout("error reading PRs from '%s'", g.repoName)
-	}
-	if resp.StatusCode != 200 {
-		common.Shout("something does not add up to 200: %s", resp.Status)
-	}
-	for _, p := range pulls {
-		common.Shout("\"%s (PR #%d)\" is %s", p.GetTitle(), p.GetNumber(), p.GetState())
-		now := time.Now()
-		cutoffTime := now.Add(time.Hour * 3 * -1).Add(time.Minute * 15 * -1) // 3 hours 15 minutes to be definitely after the 3 hour lifetime window
-		if "closed" == p.GetState() && p.GetClosedAt().Before(cutoffTime) {
-			prList = append(prList, p)
+	for {
+		pulls, resp, err := g.githubClient.PullRequests.List(ctx, g.repoOwner, g.repoName, opts)
+
+		if err != nil {
+			common.Shout("error reading PRs from '%s'", g.repoName)
 		}
+		if resp.StatusCode != 200 {
+			common.Shout("something does not add up to 200: %s", resp.Status)
+		}
+		for _, p := range pulls {
+			common.Shout("\"%s (PR #%d)\" is %s", p.GetTitle(), p.GetNumber(), p.GetState())
+			now := time.Now()
+			cutoffTime := now.Add(time.Hour * 3 * -1).Add(time.Minute * 15 * -1) // 3 hours 15 minutes to be definitely after the 3 hour lifetime window
+			if "closed" == p.GetState() && p.GetClosedAt().Before(cutoffTime) {
+				prList = append(prList, p)
+			}
+		}
+
+		if resp.NextPage == 0 {
+			break
+		}
+		opts.Page = resp.NextPage
 	}
 	return prList
 }
