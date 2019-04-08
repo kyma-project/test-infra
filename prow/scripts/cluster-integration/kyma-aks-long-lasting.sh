630701997e2f853a87de1dc471bcab76f7c65a83
Szecowka, Adam <adam.szecowka@gmail.com>
2019-04-08 17:20:11 +0200

    stability checker uses Octopus

--- prow/scripts/cluster-integration/kyma-aks-long-lasting.sh
+++ prow/scripts/cluster-integration/kyma-aks-long-lasting.sh
@@ -318,46 +318,6 @@ function installKyma() {
     fi
 }
 
-function installStabilityChecker() {
-    shout "Install stability-checker"
-    date
-
-	STATS_FAILING_TEST_REGEXP=${STATS_FAILING_TEST_REGEXP:-"'([0-9A-Za-z_-]+)' (?:has Failed status?|failed due to too long Running status?|failed due to too long Pending status?|failed with Unknown status?)"}
-	STATS_SUCCESSFUL_TEST_REGEXP=${STATS_SUCCESSFUL_TEST_REGEXP:-"Test of '([0-9A-Za-z_-]+)' was successful"}
-	STATS_ENABLED="true"
-
-	SC_DIR=${TEST_INFRA_SOURCES_DIR}/stability-checker
-
-	kubectl create -f "${SC_DIR}/local/provisioning.yaml"
-	bash "${SC_DIR}/local/helpers/isready.sh" kyma-system app  stability-test-provisioner
-	kubectl exec stability-test-provisioner -n kyma-system --  mkdir -p /home/input
-	kubectl cp "${KYMA_SCRIPTS_DIR}/testing.sh" stability-test-provisioner:/home/input/ -n kyma-system
-	kubectl cp "${KYMA_SCRIPTS_DIR}/utils.sh" stability-test-provisioner:/home/input/ -n kyma-system
-	kubectl cp "${KYMA_SCRIPTS_DIR}/testing-common.sh" stability-test-provisioner:/home/input/ -n kyma-system
-    kubectl cp "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/get-helm-certs.sh" stability-test-provisioner:/home/input/pre-start-scripts.sh -n kyma-system
-	kubectl delete pod -n kyma-system stability-test-provisioner
-
-    # create a secret with service account used for storing logs
-    kubectl create secret generic sa-stability-fluentd-storage-writer --from-file=service-account.json=/etc/credentials/sa-stability-fluentd-storage-writer/service-account.json -n kyma-system
-
-	helm install --set clusterName="${CLUSTER_NAME}" \
-	        --set logsPersistence.enabled=true \
-	        --set slackClientWebhookUrl="${SLACK_CLIENT_WEBHOOK_URL}" \
-	        --set slackClientChannelId="${STABILITY_SLACK_CLIENT_CHANNEL_ID}" \
-	        --set slackClientToken="${SLACK_CLIENT_TOKEN}" \
-	        --set stats.enabled="${STATS_ENABLED}" \
-	        --set stats.failingTestRegexp="${STATS_FAILING_TEST_REGEXP}" \
-	        --set stats.successfulTestRegexp="${STATS_SUCCESSFUL_TEST_REGEXP}" \
-	        --set testResultWindowTime="${TEST_RESULT_WINDOW_TIME}" \
-	        "${SC_DIR}/deploy/chart/stability-checker" \
-	        --namespace=kyma-system \
-	        --name=stability-checker \
-	        --wait \
-	        --timeout=600 \
-	        --tls
-}
-
-
 init
 azureAuthenticating
 
@@ -378,4 +338,6 @@ installTiller
 installKyma
 "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/get-helm-certs.sh"
 
-installStabilityChecker
+shout "Install stability-checker"
+date
+"${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/install-stability-checker.sh"
