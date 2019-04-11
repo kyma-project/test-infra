f7223b4f3d2892430acd5e716d71c335cdedad7e
tuxgoose <1phillip.guzman@gmail.com>
2019-04-11 14:43:57 -0500

    fixing linting issues

--- prow/scripts/cluster-integration/kyma-aks-long-lasting.sh
+++ prow/scripts/cluster-integration/kyma-aks-long-lasting.sh
@@ -258,9 +258,9 @@ function generateAndExportLetsEncryptCert() {
     encryptCerts
 
 	#copy the cert to cloud
-	gsutil cp ./letsencrypt/live/${DOMAIN}/nightly-gke-tls-integration-app-client-cert.encrypted gs://kyma-prow-secrets/
+	gsutil cp "./letsencrypt/live/${DOMAIN}/nightly-gke-tls-integration-app-client-cert.encrypted gs://kyma-prow-secrets/"
     #copy the private key to cloud
-    gsutil cp ./letsencrypt/live/${DOMAIN}/nightly-gke-tls-integration-app-client-key.encrypted gs://kyma-prow-secrets/
+    gsutil cp "./letsencrypt/live/${DOMAIN}/nightly-gke-tls-integration-app-client-key.encrypted gs://kyma-prow-secrets/"
 
     gsutil setmeta  -h "Cache-Control:public, max-age=60" gs://kyma-prow-secrets/nightly-gke-tls-integration-app-client-key.encrypted
     gsutil setmeta  -h "Cache-Control:public, max-age=60" gs://kyma-prow-secrets/nightly-gke-tls-integration-app-client-client.encrypted
