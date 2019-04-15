9026321eef8cf099b4199c5d0359a48603551a33
Katarzyna Kepka <kasia.kepka23@gmail.com>
2019-04-15 14:59:33 +0200

    test for XIP job2

--- development/tools/jobs/kyma/kyma_integration_test.go
+++ development/tools/jobs/kyma/kyma_integration_test.go
@@ -134,7 +134,7 @@ func TestKymaGKEXipJobPresubmit(t *testing.T) {
 	assert.Equal(t, "github.com/kyma-project/kyma", actualJob.PathAlias)
 	assert.True(t, actualJob.Decorate)
 	assert.False(t, actualJob.SkipReport)
-	assert.False(t, actualPresubmit.AlwaysRun)
+	assert.False(t, actualJob.AlwaysRun)
 	tester.AssertThatHasExtraRefTestInfra(t, actualJob.JobBase.UtilityConfig, "master")
 	tester.AssertThatSpecifiesResourceRequests(t, actualJob.JobBase)
 	assert.Equal(t, tester.ImageBootstrapHelm20181121, actualJob.Spec.Containers[0].Image)
