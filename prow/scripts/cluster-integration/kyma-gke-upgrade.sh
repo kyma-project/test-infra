8c607dfbbd35a1209701405468acc09b775504bc
Jakub Kabza <jakub.kabza@sap.com>
2019-04-15 09:15:06 +0200

    fix kyma-gke-upgrade.sh

--- prow/scripts/cluster-integration/kyma-gke-upgrade.sh
+++ prow/scripts/cluster-integration/kyma-gke-upgrade.sh
@@ -387,8 +387,10 @@ createTestResources() {
     prepareTestResult=$?
     set -o errexit
 
+    local testContainerName="runner"
+
     echo "Logs for prepare data operation to test e2e upgrade: "
-    kubectl logs -n "${UPGRADE_TEST_NAMESPACE}" -l "${UPGRADE_TEST_RESOURCE_LABEL}=${UPGRADE_TEST_LABEL_VALUE_PREPARE}"
+    kubectl logs -n "${UPGRADE_TEST_NAMESPACE}" -l "${UPGRADE_TEST_RESOURCE_LABEL}=${UPGRADE_TEST_LABEL_VALUE_PREPARE}" -c "${testContainerName}"
     if [ "${prepareTestResult}" != 0 ]; then
         echo "Exit status for prepare upgrade e2e tests: ${prepareTestResult}"
         exit "${prepareTestResult}"
