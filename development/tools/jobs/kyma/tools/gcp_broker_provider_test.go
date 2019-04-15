d95761303af408e0cb9b798a0ad82bef0467a15f
Szymik, Lukasz <lukasz.szymik@sap.com>
2019-04-15 13:56:32 +0200

    fix

--- development/tools/jobs/kyma/tools/gcp_broker_provider_test.go
+++ development/tools/jobs/kyma/tools/gcp_broker_provider_test.go
@@ -37,7 +37,7 @@ func TestGCPBrokerProviderJobsPresubmit(t *testing.T) {
 	assert.Len(t, jobConfig.Presubmits, 1)
 	kymaPresubmits, ex := jobConfig.Presubmits["kyma-project/kyma"]
 	assert.True(t, ex)
-	assert.Len(t, kymaPresubmits, 4)
+	assert.Len(t, kymaPresubmits, 5)
 
 	actualPresubmit := kymaPresubmits[0]
 	expName := "pre-master-kyma-tools-gcp-broker-provider"
