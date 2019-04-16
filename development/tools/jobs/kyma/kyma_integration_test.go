cd5c644f104b25b97390f09934de024c9a6f371a
Katarzyna Kepka <kasia.kepka23@gmail.com>
2019-04-16 12:01:44 +0200

    update test 1

--- development/tools/jobs/kyma/kyma_integration_test.go
+++ development/tools/jobs/kyma/kyma_integration_test.go
@@ -138,6 +138,7 @@ func TestKymaGKEXipJobPresubmit(t *testing.T) {
 	tester.AssertThatHasExtraRefTestInfra(t, actualJob.JobBase.UtilityConfig, "master")
 	tester.AssertThatSpecifiesResourceRequests(t, actualJob.JobBase)
 	assert.Equal(t, tester.ImageBootstrapHelm20181121, actualJob.Spec.Containers[0].Image)
+	assert.Equal(t, []string{"-c", "${KYMA_PROJECT_DIR}/test-infra/prow/scripts/cluster-integration/kyma-gke-integration-xip.sh"}, actualJob.Spec.Containers[0].Args)
 	tester.AssertThatHasPresets(t, actualJob.JobBase, tester.PresetGCProjectEnv, tester.PresetBuildPr,
 		tester.PresetDindEnabled, "preset-build-pr",
 		"preset-sa-gke-kyma-integration", "preset-gc-compute-envs",
