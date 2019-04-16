d2d69a0ce4ee926c7a7dee8948018b66bc6d13f4
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-04-16 16:05:33 +0200

    Wait for pods instead of ProwJobs

--- prow/scripts/job-waiter.sh
+++ prow/scripts/job-waiter.sh
@@ -5,7 +5,7 @@ requiredJobLabelKey=${REQUIRED_JOB_LABEL_KEY:-kyma-job-type}
 requiredJobLabelValue=${REQUIRED_JOB_LABEL_VALUE:-component}
 maxRetries=${MAX_RETRIES:-100}
 
-eventGUID=$(kubectl get prowjob "${prowJobId}" -ojson | jq -r '.metadata."labels"."event-GUID"')
+eventGUID=$(kubectl get pod "${prowJobId}" -ojson | jq -r '.metadata."labels"."event-GUID"')
 
 echo "Prow Job ID: ${prowJobId}"
 echo "GUID: ${eventGUID}"
@@ -15,7 +15,7 @@ echo "===="
 
 i=0
 until [[ ${i} -eq ${maxRetries} ]]; do
-    unsuccessfulJobs=$(kubectl get prowjob -l event-GUID="${eventGUID}","${requiredJobLabelKey}"="${requiredJobLabelValue}" --no-headers -o=custom-columns='NAME:metadata.name,STATE:status.state' | grep -v 'success')
+    unsuccessfulJobs=$(kubectl get pod -l event-GUID="${eventGUID}","${requiredJobLabelKey}"="${requiredJobLabelValue}" --no-headers -o=custom-columns='NAME:metadata.name,STATE:status.phase' | grep -v 'Succeeded')
 
     if [[ -z "$unsuccessfulJobs" ]]
     then
@@ -24,11 +24,11 @@ until [[ ${i} -eq ${maxRetries} ]]; do
     fi
 
     # Check if there are any failed jobs. If so, quit early
-    failedJobs=$(echo "${unsuccessfulJobs}" | grep 'error\|failure')
+    failedJobs=$(echo "${unsuccessfulJobs}" | grep 'CrashLoopBackOff\|Failed')
 
     if [[ -n "$failedJobs" ]];
     then
-            echo "Jobs with 'failed' state detected. Exiting with code 1..."
+            echo "Jobs with 'Failed' state detected. Exiting with code 1..."
             exit 1
     fi
 
