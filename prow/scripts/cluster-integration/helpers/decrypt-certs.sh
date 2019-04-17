2b615bbb30450199dc654203b926d6991fd10223
tuxgoose <1phillip.guzman@gmail.com>
2019-04-17 15:29:33 -0500

    edited decrypt and aks long lasting

--- prow/scripts/cluster-integration/helpers/decrypt-certs.sh
+++ prow/scripts/cluster-integration/helpers/decrypt-certs.sh
@@ -5,3 +5,10 @@ printf "decrypting nightly-gke-tls-integration-app-client-key.encrypted"
 	--key "${KYMA_ENCRYPTION_KEY}" \
 	--ciphertext-file letsencrypt/live/"${DOMAIN}"/nightly-gke-tls-integration-app-client-key.encrypted \
   --plaintext-file letsencrypt/live/"${DOMAIN}"/privkey.pem 
+
+	printf "decrypting letsencrypt/live/"${DOMAIN}"/nightly-gke-tls-integration-app-client-cert.encrypted"
+   gcloud kms decrypt --location global \
+	--keyring "${KYMA_KEYRING}" \
+	--key "${KYMA_ENCRYPTION_KEY}" \
+	--ciphertext-file letsencrypt/live/"${DOMAIN}"/nightly-gke-tls-integration-app-client-cert.encrypted \
+  --plaintext-file "./letsencrypt/live/${DOMAIN}/fullchain.pem"
