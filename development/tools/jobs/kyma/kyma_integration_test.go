1448525af5b14bb2b1c67c3ee113d0bc5ab4924f
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2019-04-23 10:31:42 +0200

    Fix tests

--- development/tools/jobs/kyma/kyma_integration_test.go
+++ development/tools/jobs/kyma/kyma_integration_test.go
@@ -328,7 +328,7 @@ func TestKymaIntegrationJobPeriodics(t *testing.T) {
 	require.NoError(t, err)
 
 	periodics := jobConfig.Periodics
-	assert.Len(t, periodics, 13)
+	assert.Len(t, periodics, 14)
 
 	expName := "orphaned-disks-cleaner"
 	disksCleanerPeriodic := tester.FindPeriodicJobByName(periodics, expName)
