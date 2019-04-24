015a64e77fc4ff514d6106c75f819c1c484c1ecb
Szecowka, Adam <adam.szecowka@gmail.com>
2019-04-24 13:20:45 +0200

    incease initial waiting period to 1m

--- development/tools/cmd/jobguard/run.sh
+++ development/tools/cmd/jobguard/run.sh
@@ -6,4 +6,4 @@ ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo "PULL NUMBER: ${PULL_NUMBER}"
 echo "PULL SHA: ${PULL_PULL_SHA}"
 
-env GITHUB_TOKEN=${GITHUB_TOKEN} INITIAL_SLEEP_TIME=5s PULL_NUMBER=${PULL_NUMBER} PULL_SHA=${PULL_PULL_SHA} JOB_NAME_PATTERN="(pre-master-kyma-components-.*)|(pre-master-kyma-tests-.*)" ${ROOT_PATH}/job-guard
+env GITHUB_TOKEN=${GITHUB_TOKEN} INITIAL_SLEEP_TIME=1m PULL_NUMBER=${PULL_NUMBER} PULL_SHA=${PULL_PULL_SHA} JOB_NAME_PATTERN="(pre-master-kyma-components-.*)|(pre-master-kyma-tests-.*)" ${ROOT_PATH}/job-guard
