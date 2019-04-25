a0a8e48621579a289079b74c87fd766fd50dce28
Szecowka, Adam <adam.szecowka@gmail.com>
2019-04-25 11:01:17 +0200

    fix

--- development/tools/cmd/jobguard/run.sh
+++ development/tools/cmd/jobguard/run.sh
@@ -4,16 +4,14 @@ ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 KYMA_PROJECT_DIR=${KYMA_PROJECT_DIR:-"/home/prow/go/src/github.com/kyma-project"}
 echo "KYMA PROJECT DIR ${KYMA_PROJECT_DIR}"
-echo "PULL NUMBER: ${PULL_NUMBER}"
-echo "PULL SHA: ${PULL_PULL_SHA}"
+echo "COMMIT SHA: ${PULL_PULL_SHA}"
 export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
 
 cd ${ROOT_PATH}
 
 env GITHUB_TOKEN=${GITHUB_TOKEN} \
     INITIAL_SLEEP_TIME=1m \
-    PULL_NUMBER=${PULL_NUMBER} \
-    PULL_SHA=${PULL_PULL_SHA} \
+    COMMIT_SHA=${PULL_PULL_SHA} \
     JOB_NAME_PATTERN="(pre-master-kyma-components-.*)|(pre-master-kyma-tests-.*)" \
     PROW_CONFIG_FILE="${TEST_INFRA_SOURCES_DIR}/prow/config.yaml" \
     PROW_JOBS_DIRECTORY="${TEST_INFRA_SOURCES_DIR}/prow/jobs" \
