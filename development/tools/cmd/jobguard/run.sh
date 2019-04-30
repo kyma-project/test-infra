e1236c75204eee8fb057525fb7d77653b650d110
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-04-30 14:28:24 +0200

    Add Job Guard utility (#904)

---
+++ development/tools/cmd/jobguard/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+KYMA_PROJECT_DIR=${KYMA_PROJECT_DIR:-"/home/prow/go/src/github.com/kyma-project"}
+echo "KYMA PROJECT DIR ${KYMA_PROJECT_DIR}"
+echo "COMMIT SHA: ${PULL_PULL_SHA}"
+export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
+
+cd "${ROOT_PATH}" || exit 1
+
+dep ensure -v -vendor-only
+
+env GITHUB_TOKEN="${GITHUB_TOKEN}" \
+    INITIAL_SLEEP_TIME=2m \
+    COMMIT_SHA="${PULL_PULL_SHA}" \
+    JOB_NAME_PATTERN="(pre-master-kyma-components-.*)|(pre-master-kyma-tests-.*)" \
+    PROW_CONFIG_FILE="${TEST_INFRA_SOURCES_DIR}/prow/config.yaml" \
+    PROW_JOBS_DIRECTORY="${TEST_INFRA_SOURCES_DIR}/prow/jobs" \
+    go run main.go
