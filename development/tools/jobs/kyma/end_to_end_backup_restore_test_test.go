cd77c637f07d0fc24c87006c7f903775bb88ab36
Szymon Gibala <szymon.gibala@sap.com>
2019-05-08 08:43:40 +0200

    Adjust unit tests

--- development/tools/jobs/kyma/end_to_end_backup_restore_test_test.go
+++ development/tools/jobs/kyma/end_to_end_backup_restore_test_test.go
@@ -10,9 +10,8 @@ import (
 
 func TestEndToEndBackupRstoreTestReleases(t *testing.T) {
 	// WHEN
-	unsupportedReleases := []tester.SupportedRelease{tester.Release07}
 
-	for _, currentRelease := range tester.GetKymaReleaseBranchesBesides(unsupportedReleases) {
+	for _, currentRelease := range tester.GetAllKymaReleaseBranches() {
 		t.Run(currentRelease, func(t *testing.T) {
 			jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/kyma/tests/end-to-end/backup-restore-test/backup-restore-test.yaml")
 			// THEN
