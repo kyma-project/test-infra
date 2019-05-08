10700f1ab625a4e43619724abf329456ff8e1a06
Szymon Gibala <szymon.gibala@sap.com>
2019-05-08 07:06:36 +0200

    Modify components jobs

--- development/tools/jobs/tester/tester.go
+++ development/tools/jobs/tester/tester.go
@@ -97,10 +97,9 @@ type SupportedRelease = string
 // When we removing support for given version, there remove
 // its entry also here.
 const (
-	Release07 SupportedRelease = "release-0.7"
-	Release08 SupportedRelease = "release-0.8"
 	Release09 SupportedRelease = "release-0.9"
 	Release10 SupportedRelease = "release-1.0"
+	Release11 SupportedRelease = "release-1.1"
 )
 
 // Release allows you to execute checks on given release
@@ -121,7 +120,7 @@ type jobRunner interface {
 
 // GetAllKymaReleaseBranches returns all supported kyma release branches
 func GetAllKymaReleaseBranches() []SupportedRelease {
-	return []SupportedRelease{Release09, Release10}
+	return []SupportedRelease{Release09, Release10, Release11}
 }
 
 // GetKymaReleaseBranchesBesides filters all available releases by given unsupported ones
