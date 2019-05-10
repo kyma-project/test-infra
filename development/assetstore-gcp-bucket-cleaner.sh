0027a7ee19b3e29bbecb5e70764a75453ff539a0
Marcin Witalis <marcin.witalis@sap.com>
2019-05-10 16:36:09 +0200

    fix missing dependencies in assetstore-gcp-bucket-cleaner script

--- development/assetstore-gcp-bucket-cleaner.sh
+++ development/assetstore-gcp-bucket-cleaner.sh
@@ -6,6 +6,13 @@ set -o pipefail
 EXCLUDED_BUCKETS='kyma-prow-secrets,kyma-prow-logs,kyma-prow-artifacts,kyma-development-artifacts,kyma-backup-restore,eu.artifacts.sap-kyma-prow.appspot.com'
 BUCKET_REGEXP_NAME="^.+-([a-z0-9]+$)"
 
+if [ ! -d "${DEVELOPMENT_DIR}/tools/vendor" ]; then
+    echo "Vendoring 'tools'"
+    pushd "${DEVELOPMENT_DIR}/tools"
+    dep ensure -v -vendor-only
+    popd
+fi
+
 go run "${DEVELOPMENT_DIR}"/tools/cmd/gcscleaner/main.go \
       -bucketNameRegexp  "${BUCKET_REGEXP_NAME}"\
       -excludedBuckets "${EXCLUDED_BUCKETS}"\
