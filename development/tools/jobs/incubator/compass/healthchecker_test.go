4b4450087ba56242ab6d2f061406561b29b58bc3
Tomasz Gorgol <t.gorgol@sap.com>
2019-05-16 12:21:06 +0200

    Rename healthcheck component to healthchecker

--- development/tools/jobs/incubator/compass/healthchecker_test.go
+++ development/tools/jobs/incubator/compass/healthchecker_test.go
@@ -8,19 +8,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const healthcheckJobPath = "./../../../../../prow/jobs/incubator/compass/components/healthcheck/healthcheck.yaml"
+const healthcheckerJobPath = "./../../../../../prow/jobs/incubator/compass/components/healthchecker/healthchecker.yaml"
 
-func TestHealthcheckJobReleases(t *testing.T) {
+func TestHealthcheckerJobReleases(t *testing.T) {
 	// WHEN
 	unsupportedReleases := []tester.SupportedRelease{tester.Release09, tester.Release10, tester.Release11}
 
 	for _, currentRelease := range tester.GetKymaReleaseBranchesBesides(unsupportedReleases) {
 		t.Run(currentRelease, func(t *testing.T) {
-			jobConfig, err := tester.ReadJobConfig(healthcheckJobPath)
+			jobConfig, err := tester.ReadJobConfig(healthcheckerJobPath)
 			// THEN
 			require.NoError(t, err)
 
-			actualPre := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-incubator/compass"], tester.GetReleaseJobName("compass-components-healthcheck", currentRelease), currentRelease)
+			actualPre := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-incubator/compass"], tester.GetReleaseJobName("compass-components-healthchecker", currentRelease), currentRelease)
 			require.NotNil(t, actualPre)
 
 			assert.False(t, actualPre.SkipReport)
@@ -29,18 +29,18 @@ func TestHealthcheckJobReleases(t *testing.T) {
 			tester.AssertThatHasExtraRefTestInfra(t, actualPre.JobBase.UtilityConfig, currentRelease)
 			tester.AssertThatHasPresets(t, actualPre.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepoIncubator, tester.PresetGcrPush, tester.PresetBuildRelease)
 			assert.True(t, actualPre.AlwaysRun)
-			tester.AssertThatExecGolangBuildpack(t, actualPre.JobBase, tester.ImageGolangBuildpack1_11, "/home/prow/go/src/github.com/kyma-incubator/compass/components/healthcheck")
+			tester.AssertThatExecGolangBuildpack(t, actualPre.JobBase, tester.ImageGolangBuildpack1_11, "/home/prow/go/src/github.com/kyma-incubator/compass/components/healthchecker")
 		})
 	}
 }
 
-func TestHealthcheckJobPresubmit(t *testing.T) {
+func TestHealthcheckerJobPresubmit(t *testing.T) {
 	// WHEN
-	jobConfig, err := tester.ReadJobConfig(healthcheckJobPath)
+	jobConfig, err := tester.ReadJobConfig(healthcheckerJobPath)
 	// THEN
 	require.NoError(t, err)
 
-	actualPre := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-incubator/compass"], "pre-master-compass-components-healthcheck", "master")
+	actualPre := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-incubator/compass"], "pre-master-compass-components-healthchecker", "master")
 	require.NotNil(t, actualPre)
 
 	assert.Equal(t, 10, actualPre.MaxConcurrency)
@@ -50,18 +50,18 @@ func TestHealthcheckJobPresubmit(t *testing.T) {
 	assert.Equal(t, "github.com/kyma-incubator/compass", actualPre.PathAlias)
 	tester.AssertThatHasExtraRefTestInfra(t, actualPre.JobBase.UtilityConfig, "master")
 	tester.AssertThatHasPresets(t, actualPre.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepoIncubator, tester.PresetGcrPush, tester.PresetBuildPr)
-	assert.Equal(t, "^components/healthcheck/", actualPre.RunIfChanged)
-	tester.AssertThatJobRunIfChanged(t, *actualPre, "components/healthcheck/some_random_file.go")
-	tester.AssertThatExecGolangBuildpack(t, actualPre.JobBase, tester.ImageGolangBuildpack1_11, "/home/prow/go/src/github.com/kyma-incubator/compass/components/healthcheck")
+	assert.Equal(t, "^components/healthchecker/", actualPre.RunIfChanged)
+	tester.AssertThatJobRunIfChanged(t, *actualPre, "components/healthchecker/some_random_file.go")
+	tester.AssertThatExecGolangBuildpack(t, actualPre.JobBase, tester.ImageGolangBuildpack1_11, "/home/prow/go/src/github.com/kyma-incubator/compass/components/healthchecker")
 }
 
-func TestHealthcheckJobPostsubmit(t *testing.T) {
+func TestHealthcheckerJobPostsubmit(t *testing.T) {
 	// WHEN
-	jobConfig, err := tester.ReadJobConfig(healthcheckJobPath)
+	jobConfig, err := tester.ReadJobConfig(healthcheckerJobPath)
 	// THEN
 	require.NoError(t, err)
 
-	actualPost := tester.FindPostsubmitJobByName(jobConfig.Postsubmits["kyma-incubator/compass"], "post-master-compass-components-healthcheck", "master")
+	actualPost := tester.FindPostsubmitJobByName(jobConfig.Postsubmits["kyma-incubator/compass"], "post-master-compass-components-healthchecker", "master")
 	require.NotNil(t, actualPost)
 
 	assert.Equal(t, 10, actualPost.MaxConcurrency)
@@ -69,7 +69,7 @@ func TestHealthcheckJobPostsubmit(t *testing.T) {
 	assert.Equal(t, "github.com/kyma-incubator/compass", actualPost.PathAlias)
 	tester.AssertThatHasExtraRefTestInfra(t, actualPost.JobBase.UtilityConfig, "master")
 	tester.AssertThatHasPresets(t, actualPost.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepoIncubator, tester.PresetGcrPush, tester.PresetBuildMaster)
-	assert.Equal(t, "^components/healthcheck/", actualPost.RunIfChanged)
-	tester.AssertThatJobRunIfChanged(t, *actualPost, "components/healthcheck/some_random_file.go")
-	tester.AssertThatExecGolangBuildpack(t, actualPost.JobBase, tester.ImageGolangBuildpack1_11, "/home/prow/go/src/github.com/kyma-incubator/compass/components/healthcheck")
+	assert.Equal(t, "^components/healthchecker/", actualPost.RunIfChanged)
+	tester.AssertThatJobRunIfChanged(t, *actualPost, "components/healthchecker/some_random_file.go")
+	tester.AssertThatExecGolangBuildpack(t, actualPost.JobBase, tester.ImageGolangBuildpack1_11, "/home/prow/go/src/github.com/kyma-incubator/compass/components/healthchecker")
 }
