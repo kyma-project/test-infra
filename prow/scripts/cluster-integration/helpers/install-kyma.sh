925d6239d643b414fb64c35b7900bcc142301fa6
santiago ventura <santiago.ventura@sap.com>
2019-05-15 23:40:54 -0700

    Fix error code SC2153 and SC1090 in script install-kyma.sh

--- prow/scripts/cluster-integration/helpers/install-kyma.sh
+++ prow/scripts/cluster-integration/helpers/install-kyma.sh
@@ -19,51 +19,51 @@
 source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/library.sh"
 function installKyma() {
 
-	kymaUnsetVar=false
+    kymaUnsetVar=false
 
-	for var in DOCKER_PUSH_REPOSITORY KYMA_SOURCES_DIR DOCKER_PUSH_DIRECTORY GOOGLE_APPLICATION_CREDENTIALS STANDARIZED_NAME REPO_OWNER REPO_NAME CURRENT_TIMESTAMP; do
-    	if [ -z "${!var}" ] ; then
-        	echo "ERROR: $var is not set"
-        	kymaUnsetVar=true
-    	fi
-	done
+    for var in DOCKER_PUSH_REPOSITORY KYMA_SOURCES_DIR DOCKER_PUSH_DIRECTORY GOOGLE_APPLICATION_CREDENTIALS STANDARIZED_NAME REPO_OWNER REPO_NAME CURRENT_TIMESTAMP; do
+        if [ -z "${!var}" ] ; then
+            echo "ERROR: $var is not set"
+            kymaUnsetVar=true
+        fi
+    done
 
-		if [[ "${PERFORMACE_CLUSTER_SETUP}" == "" ]]; then
-		for var in REMOTEENVS_IP_ADDRESS GATEWAY_IP_ADDRESS DOMAIN; do
-			if [ -z "${!var}" ] ; then
-				echo "ERROR: $var is not set"
-				kymaUnsetVar=true
-			fi
-		done
-	fi
+    if [[ "${PERFORMACE_CLUSTER_SETUP}" == "" ]]; then
+        for var in REMOTEENVS_IP_ADDRESS GATEWAY_IP_ADDRESS DOMAIN; do
+            if [ -z "${!var}" ] ; then
+                echo "ERROR: $var is not set"
+                kymaUnsetVar=true
+            fi
+        done
+    fi
 
-	if [ "${kymaUnsetVar}" = true ] ; then
-    	exit 1
-	fi
+    if [ "${kymaUnsetVar}" = true ] ; then
+        exit 1
+    fi
 
-	# shellcheck disable=SC2153
-	KYMA_RESOURCES_DIR="${KYMA_SOURCES_DIR}/installation/resources"
-	INSTALLER_YAML="${KYMA_RESOURCES_DIR}/installer.yaml"
-	INSTALLER_CONFIG="${KYMA_RESOURCES_DIR}/installer-config-cluster.yaml.tpl"
-	INSTALLER_CR="${KYMA_RESOURCES_DIR}/installer-cr-cluster.yaml.tpl"
+    # shellcheck disable=SC2153
+    KYMA_RESOURCES_DIR="${KYMA_SOURCES_DIR}/installation/resources"
+    INSTALLER_YAML="${KYMA_RESOURCES_DIR}/installer.yaml"
+    INSTALLER_CONFIG="${KYMA_RESOURCES_DIR}/installer-config-cluster.yaml.tpl"
+    INSTALLER_CR="${KYMA_RESOURCES_DIR}/installer-cr-cluster.yaml.tpl"
 
-	shout "Build Kyma-Installer Docker image"
-	date
+    shout "Build Kyma-Installer Docker image"
+    date
 
     if [[ "${PERFORMACE_CLUSTER_SETUP}" == "" ]]; then
 
-		export KYMA_INSTALLER_IMAGE="${DOCKER_PUSH_REPOSITORY}${DOCKER_PUSH_DIRECTORY}/${STANDARIZED_NAME}/${REPO_OWNER}/${REPO_NAME}:${CURRENT_TIMESTAMP}"
+        export KYMA_INSTALLER_IMAGE="${DOCKER_PUSH_REPOSITORY}${DOCKER_PUSH_DIRECTORY}/${STANDARIZED_NAME}/${REPO_OWNER}/${REPO_NAME}:${CURRENT_TIMESTAMP}"
 
-		createImage
+        createImage
 
-		# shellcheck disable=SC2153
-		PROMTAIL_CONFIG_NAME=promtail-k8s-1-14.yaml
-		# shellcheck disable=SC1090
-		source "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/generate-and-export-letsencrypt-TLS-cert.sh
+        # shellcheck disable=SC2153
+        PROMTAIL_CONFIG_NAME=promtail-k8s-1-14.yaml
+        # shellcheck disable=SC1090
+        source "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/generate-and-export-letsencrypt-TLS-cert.sh
 
-		shout "Apply Kyma config"
-		date
-		"${KYMA_SCRIPTS_DIR}"/concat-yamls.sh "${INSTALLER_YAML}" "${INSTALLER_CONFIG}" \
+        shout "Apply Kyma config"
+        date
+        "${KYMA_SCRIPTS_DIR}"/concat-yamls.sh "${INSTALLER_YAML}" "${INSTALLER_CONFIG}" \
 			| sed -e 's;image: eu.gcr.io/kyma-project/.*/installer:.*$;'"image: ${KYMA_INSTALLER_IMAGE};" \
 			| sed -e "s/__DOMAIN__/${DOMAIN}/g" \
 			| sed -e "s/__REMOTE_ENV_IP__/${REMOTEENVS_IP_ADDRESS}/g" \
@@ -79,16 +79,16 @@ function installKyma() {
 
     else
 
-		export KYMA_INSTALLER_IMAGE="${DOCKER_PUSH_REPOSITORY}${DOCKER_PUSH_DIRECTORY}:${CURRENT_TIMESTAMP}"
+        export KYMA_INSTALLER_IMAGE="${DOCKER_PUSH_REPOSITORY}${DOCKER_PUSH_DIRECTORY}:${CURRENT_TIMESTAMP}"
 
-	    createImage
+        createImage
 
-		INSTALLER_CR="${KYMA_RESOURCES_DIR}/installer-cr-cluster.yaml.tpl"
-		PROMTAIL_CONFIG_NAME=promtail-k8s-1-14.yaml
+        INSTALLER_CR="${KYMA_RESOURCES_DIR}/installer-cr-cluster.yaml.tpl"
+        PROMTAIL_CONFIG_NAME=promtail-k8s-1-14.yaml
 
-		shout "Apply Kyma config"
-		date
-		"${KYMA_SCRIPTS_DIR}"/concat-yamls.sh "${INSTALLER_YAML}" "${INSTALLER_CONFIG}" \
+        shout "Apply Kyma config"
+        date
+        "${KYMA_SCRIPTS_DIR}"/concat-yamls.sh "${INSTALLER_YAML}" "${INSTALLER_CONFIG}" \
 			| sed -e 's;image: eu.gcr.io/kyma-project/.*/installer:.*$;'"image: ${KYMA_INSTALLER_IMAGE};" \
 			| sed -e "s/__REMOTE_ENV_IP__/${REMOTEENVS_IP_ADDRESS}/g" \
 			| sed -e "s/__SKIP_SSL_VERIFY__/true/g" \
@@ -101,30 +101,30 @@ function installKyma() {
 
     fi
 
-	waitUntilInstallerApiAvailable
+    waitUntilInstallerApiAvailable
 
-	if [[ "${PERFORMACE_CLUSTER_SETUP}" != "" ]]; then
-		kubectl config set-context "gke_${CLOUDSDK_CORE_PROJECT}_${CLOUDSDK_COMPUTE_ZONE}_${INPUT_CLUSTER_NAME}" --namespace=default
-	fi
+    if [[ "${PERFORMACE_CLUSTER_SETUP}" != "" ]]; then
+        kubectl config set-context "gke_${CLOUDSDK_CORE_PROJECT}_${CLOUDSDK_COMPUTE_ZONE}_${INPUT_CLUSTER_NAME}" --namespace=default
+    fi
 
-	shout "Trigger installation"
-	date
+    shout "Trigger installation"
+    date
 
     sed -e "s/__VERSION__/0.0.1/g" "${INSTALLER_CR}"  | sed -e "s/__.*__//g" | kubectl apply -f-
-	kubectl label installation/kyma-installation action=install --overwrite
-	"${KYMA_SCRIPTS_DIR}"/is-installed.sh --timeout 30m
+    kubectl label installation/kyma-installation action=install --overwrite
+    "${KYMA_SCRIPTS_DIR}"/is-installed.sh --timeout 30m
 }
 
 function createImage() {
-	shout "Kyma Installer Image: ${KYMA_INSTALLER_IMAGE}"
-	# shellcheck disable=SC1090
-	source "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-image.sh"
+    shout "Kyma Installer Image: ${KYMA_INSTALLER_IMAGE}"
+    # shellcheck disable=SC1090
+    source "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-image.sh"
 }
 
 function waitUntilInstallerApiAvailable() {
     shout "Waiting for Installer API"
 
-	attempts=5
+    attempts=5
     for ((i=1; i<=attempts; i++)); do
         numberOfLines=$(kubectl api-versions | grep -c "installer.kyma-project.io")
         if [[ "$numberOfLines" == "1" ]]; then
