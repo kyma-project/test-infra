2093bb55ca993ff008ca006fb5946fc3e3e9b6ce
Karol Furgol <furgolkarol@gmail.com>
2019-05-30 14:51:31 +0200

    rename fixes

--- development/tools/jobs/incubator/compass/compass_end_to_end_test.go
+++ development/tools/jobs/incubator/compass/compass_end_to_end_test.go
@@ -8,19 +8,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const compassIntegrationTestJobPath = "./../../../../../prow/jobs/incubator/compass/tests/compass-integration/compass-integration.yaml"
+const compassEndToEndTestJobPath = "./../../../../../prow/jobs/incubator/compass/tests/end-to-end/end-to-end.yaml"
 
-func TestCompassIntegrationJobReleases(t *testing.T) {
+func TestCompassEndToEndJobReleases(t *testing.T) {
 	// WHEN
 	unsupportedReleases := []tester.SupportedRelease{tester.Release09, tester.Release10, tester.Release11}
 
 	for _, currentRelease := range tester.GetKymaReleaseBranchesBesides(unsupportedReleases) {
 		t.Run(currentRelease, func(t *testing.T) {
-			jobConfig, err := tester.ReadJobConfig(compassIntegrationTestJobPath)
+			jobConfig, err := tester.ReadJobConfig(compassEndToEndTestJobPath)
 			// THEN
 			require.NoError(t, err)
 
-			actualPre := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-incubator/compass"], tester.GetReleaseJobName("compass-tests-compass-integration", currentRelease), currentRelease)
+			actualPre := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-incubator/compass"], tester.GetReleaseJobName("compass-tests-end-to-end", currentRelease), currentRelease)
 			require.NotNil(t, actualPre)
 
 			assert.False(t, actualPre.SkipReport)
@@ -29,18 +29,18 @@ func TestCompassIntegrationJobReleases(t *testing.T) {
 			tester.AssertThatHasExtraRefTestInfra(t, actualPre.JobBase.UtilityConfig, currentRelease)
 			tester.AssertThatHasPresets(t, actualPre.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepoIncubator, tester.PresetGcrPush, tester.PresetBuildRelease)
 			assert.True(t, actualPre.AlwaysRun)
-			tester.AssertThatExecGolangBuildpack(t, actualPre.JobBase, tester.ImageGolangBuildpack1_11, "/home/prow/go/src/github.com/kyma-incubator/compass/tests/compass-integration")
+			tester.AssertThatExecGolangBuildpack(t, actualPre.JobBase, tester.ImageGolangBuildpack1_11, "/home/prow/go/src/github.com/kyma-incubator/compass/tests/end-to-end")
 		})
 	}
 }
 
-func TestCompassIntegrationJobPresubmit(t *testing.T) {
+func TestCompassEndToEndJobPresubmit(t *testing.T) {
 	// WHEN
-	jobConfig, err := tester.ReadJobConfig(compassIntegrationTestJobPath)
+	jobConfig, err := tester.ReadJobConfig(compassEndToEndTestJobPath)
 	// THEN
 	require.NoError(t, err)
 
-	actualPre := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-incubator/compass"], "pre-master-compass-tests-compass-integration", "master")
+	actualPre := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-incubator/compass"], "pre-master-compass-tests-end-to-end", "master")
 	require.NotNil(t, actualPre)
 
 	assert.Equal(t, 10, actualPre.MaxConcurrency)
@@ -50,18 +50,18 @@ func TestCompassIntegrationJobPresubmit(t *testing.T) {
 	assert.Equal(t, "github.com/kyma-incubator/compass", actualPre.PathAlias)
 	tester.AssertThatHasExtraRefTestInfra(t, actualPre.JobBase.UtilityConfig, "master")
 	tester.AssertThatHasPresets(t, actualPre.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepoIncubator, tester.PresetGcrPush, tester.PresetBuildPr)
-	assert.Equal(t, "^tests/compass-integration/", actualPre.RunIfChanged)
-	tester.AssertThatJobRunIfChanged(t, *actualPre, "tests/compass-integration/some_random_file.go")
-	tester.AssertThatExecGolangBuildpack(t, actualPre.JobBase, tester.ImageGolangBuildpack1_11, "/home/prow/go/src/github.com/kyma-incubator/compass/tests/compass-integration")
+	assert.Equal(t, "^tests/end-to-end/", actualPre.RunIfChanged)
+	tester.AssertThatJobRunIfChanged(t, *actualPre, "tests/end-to-end/some_random_file.go")
+	tester.AssertThatExecGolangBuildpack(t, actualPre.JobBase, tester.ImageGolangBuildpack1_11, "/home/prow/go/src/github.com/kyma-incubator/compass/tests/end-to-end")
 }
 
-func TestCompassIntegrationJobPostsubmit(t *testing.T) {
+func TestCompassEndToEndJobPostsubmit(t *testing.T) {
 	// WHEN
-	jobConfig, err := tester.ReadJobConfig(compassIntegrationTestJobPath)
+	jobConfig, err := tester.ReadJobConfig(compassEndToEndTestJobPath)
 	// THEN
 	require.NoError(t, err)
 
-	actualPost := tester.FindPostsubmitJobByName(jobConfig.Postsubmits["kyma-incubator/compass"], "post-master-compass-tests-compass-integration", "master")
+	actualPost := tester.FindPostsubmitJobByName(jobConfig.Postsubmits["kyma-incubator/compass"], "post-master-compass-tests-end-to-end", "master")
 	require.NotNil(t, actualPost)
 
 	assert.Equal(t, 10, actualPost.MaxConcurrency)
@@ -69,7 +69,7 @@ func TestCompassIntegrationJobPostsubmit(t *testing.T) {
 	assert.Equal(t, "github.com/kyma-incubator/compass", actualPost.PathAlias)
 	tester.AssertThatHasExtraRefTestInfra(t, actualPost.JobBase.UtilityConfig, "master")
 	tester.AssertThatHasPresets(t, actualPost.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepoIncubator, tester.PresetGcrPush, tester.PresetBuildMaster)
-	assert.Equal(t, "^tests/compass-integration/", actualPost.RunIfChanged)
-	tester.AssertThatJobRunIfChanged(t, *actualPost, "tests/compass-integration/some_random_file.go")
-	tester.AssertThatExecGolangBuildpack(t, actualPost.JobBase, tester.ImageGolangBuildpack1_11, "/home/prow/go/src/github.com/kyma-incubator/compass/tests/compass-integration")
+	assert.Equal(t, "^tests/end-to-end/", actualPost.RunIfChanged)
+	tester.AssertThatJobRunIfChanged(t, *actualPost, "tests/end-to-end/some_random_file.go")
+	tester.AssertThatExecGolangBuildpack(t, actualPost.JobBase, tester.ImageGolangBuildpack1_11, "/home/prow/go/src/github.com/kyma-incubator/compass/tests/end-to-end")
 }
