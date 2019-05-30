78b3738db48d57b69f11e350d993630f17512bfa
Mateusz Szostok <szostok.mateusz@gmail.com>
2019-05-30 13:48:55 +0200

    Extract the JOB_NAME_PATTERN param to enviroment variable

--- development/tools/cmd/jobguard/run.sh
+++ development/tools/cmd/jobguard/run.sh
@@ -3,6 +3,7 @@
 ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 KYMA_PROJECT_DIR=${KYMA_PROJECT_DIR:-"/home/prow/go/src/github.com/kyma-project"}
+JOB_NAME_PATTERN=${JOB_NAME_PATTERN:-"(pre-master-kyma-components-.*)|(pre-master-kyma-tests-.*)"}
 echo "KYMA PROJECT DIR ${KYMA_PROJECT_DIR}"
 echo "COMMIT SHA: ${PULL_PULL_SHA}"
 export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
@@ -14,7 +15,7 @@ dep ensure -v -vendor-only
 env GITHUB_TOKEN="${GITHUB_TOKEN}" \
     INITIAL_SLEEP_TIME=2m \
     COMMIT_SHA="${PULL_PULL_SHA}" \
-    JOB_NAME_PATTERN="(pre-master-kyma-components-.*)|(pre-master-kyma-tests-.*)" \
+    JOB_NAME_PATTERN="${JOB_NAME_PATTERN}" \
     PROW_CONFIG_FILE="${TEST_INFRA_SOURCES_DIR}/prow/config.yaml" \
     PROW_JOBS_DIRECTORY="${TEST_INFRA_SOURCES_DIR}/prow/jobs" \
     go run main.go
