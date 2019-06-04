2bf67b4099a9a35fca75d010916b67cb7f45486a
Jakub Kabza <jakub.kabza@sap.com>
2019-06-04 18:22:52 +0200

    test

--- development/tools/jobs/kyma/kyma_github-release_test.go
+++ development/tools/jobs/kyma/kyma_github-release_test.go
@@ -9,6 +9,7 @@ import (
 )
 
 func TestKymaGithubReleaseJobPostsubmit(t *testing.T) {
+	// WHEN
 	for _, currentRelease := range tester.GetAllKymaReleaseBranches() {
 		t.Run(currentRelease, func(t *testing.T) {
 			jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/kyma/kyma-github-release.yaml")
@@ -16,10 +17,13 @@ func TestKymaGithubReleaseJobPostsubmit(t *testing.T) {
 			require.NoError(t, err)
 			actualPostsubmit := tester.FindPostsubmitJobByName(jobConfig.Postsubmits["kyma-project/kyma"], tester.GetReleasePostSubmitJobName("kyma-github-release", currentRelease), currentRelease)
 			require.NotNil(t, actualPostsubmit)
+
+			//then
 			assert.True(t, actualPostsubmit.Decorate)
 			tester.AssertThatHasExtraRefTestInfra(t, actualPostsubmit.JobBase.UtilityConfig, currentRelease)
 			tester.AssertThatHasPresets(t, actualPostsubmit.JobBase, "preset-sa-kyma-artifacts", "preset-bot-github-token")
 			assert.Equal(t, tester.ImageGolangBuildpackLatest, actualPostsubmit.Spec.Containers[0].Image)
+			assert.Equal(t, []string{"-c", "/home/prow/go/src/github.com/kyma-project/test-infra/development/github-release.sh -targetCommit=${RELEASE_TARGET_COMMIT} -githubRepoOwner=${REPO_OWNER} -githubRepoName=${REPO_NAME} -githubAccessToken=${BOT_GITHUB_TOKEN} -releaseVersionFilePath=/home/prow/go/src/github.com/kyma-project/test-infra/prow/RELEASE_VERSION"}, actualPostsubmit.Spec.Containers[0].Args)
 		})
 	}
 }
