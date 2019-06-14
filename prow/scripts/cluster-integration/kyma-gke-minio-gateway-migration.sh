01d5708a85aeaaaf6ab1173f85a8ef9d081a9445
marcin witalis <45931826+m00g3n@users.noreply.github.com>
2019-06-14 20:10:11 +0200

    Fix migration test job (#1146)

--- prow/scripts/cluster-integration/kyma-gke-minio-gateway-migration.sh
+++ prow/scripts/cluster-integration/kyma-gke-minio-gateway-migration.sh
@@ -320,14 +320,18 @@ function upload_sample_file_to_minio {
         -H "Content-Type: ${CONTENT_TYPE}" \
         -H "Authorization: AWS ${ACCESS_KEY}:${CHECKSUM}" \
 	--insecure \
+	--silent \
         "${MINIO_HOST}"/"${RESOURCE}"
     set +u
 }
 
+set -e
 # upload samples to minIO
 PUBLIC_BUCKET=$(kubectl -n kyma-system get configmap asset-upload-service -o jsonpath="{.data.public}" | xargs -n1 echo)
 PRIVATE_BUCKET=$(kubectl -n kyma-system get configmap asset-upload-service -o jsonpath="{.data.private}" | xargs -n1 echo)
 
+shout "Upload files to minIO"
+date
 upload_sample_file_to_minio "${PUBLIC_BUCKET}" sample
 upload_sample_file_to_minio "${PUBLIC_BUCKET}" sampledir/sample
 
@@ -345,8 +349,8 @@ kubectl label -n kyma-installer secret "${ASSET_STORE_RESOURCE_NAME}" "installer
     --data "minio.gcsgateway.enabled=true" \
     --data "minio.gcsgateway.projectId=${CLOUDSDK_CORE_PROJECT}" \
     --data "minio.DeploymentUpdate.type=RollingUpdate" \
-    --data "minio.DeploymentUpdate.maxSurge=\"0\"" \
-    --data "minio.DeploymentUpdate.maxUnavailable=\"50%\"" \
+    --data "minio.DeploymentUpdate.maxSurge=0" \
+    --data "minio.DeploymentUpdate.maxUnavailable=50%" \
     --label "component=assetstore"
 
 # trigger installation
@@ -370,19 +374,26 @@ function download_sample_file_from_minio {
     curl -H "Date: ${DATE}" \
          -H "Content-Type: ${CONTENT_TYPE}" \
          -H "Authorization: AWS ${ACCESS_KEY}:${CHECKSUM}" \
+	 --silent \
 	 --insecure \
-         "${MINIO_HOST}"/"${RESOURCE}"
+         "${MINIO_HOST}"/"${RESOURCE}" > /dev/null
     set +u
 }
 
 ACCESS_KEY=$(kubectl get secret assetstore-minio -n kyma-system -o jsonpath="{.data.accesskey}" | base64 -d | xargs -n1 echo)
 SECRET_KEY=$(kubectl get secret assetstore-minio -n kyma-system -o jsonpath="{.data.secretkey}" | base64 -d | xargs -n1 echo)
 
+shout "Verify minIO bucket migration"
+date
 download_sample_file_from_minio "${PUBLIC_BUCKET}" sample
 download_sample_file_from_minio "${PUBLIC_BUCKET}" sampledir/sample
 
 download_sample_file_from_minio "${PRIVATE_BUCKET}" sample
 download_sample_file_from_minio "${PRIVATE_BUCKET}" sampledir/sample
+
+shout "Test Kyma"
+date
+"${KYMA_SCRIPTS_DIR}"/testing.sh
 
 shout "Success"
 
