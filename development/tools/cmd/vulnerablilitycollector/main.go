3518995fc8cb2c249b80b1754a572255b1cedef9
tuxgoose <1phillip.guzman@gmail.com>
2019-06-25 10:30:55 -0500

    added test and slack function

--- development/tools/cmd/vulnerablilitycollector/main.go
+++ development/tools/cmd/vulnerablilitycollector/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
@@ -12,34 +13,43 @@ import (
 var (
 	project = flag.String("project", "", "Project ID [Required]")
 	url     = flag.String("url", "", "Resource Url [Required]")
+	collect = flag.Bool("collect", true, "Collecting resources")
+	dryrun  = flag.Bool("dryrun", false, "Dryrun is enabled")
 )
 
 func main() {
 	flag.Parse()
 
-	vs, error := vulnerabilitycollector.FindVulnerabilityOccurrencesForImage(*url, *project)
-	if error != nil {
-		log.Fatalf("Could not get authenticated client: %v", error)
-	}
-	var count int
-	var warn int
-	var packages map[string]string
-	for _, element := range vs {
+	if !*dryrun {
+		vs, error := vulnerabilitycollector.FindVulnerabilityOccurrencesForImage(*url, *project)
 
-		req := element.GetVulnerability()
-		packages = make(map[string]string)
-		if strings.Contains("HIGH", req.Severity.String()) {
-
-			log.Warn("Severity ", req.Severity, " ", req.PackageIssue[0].AffectedLocation.Package, " ", req.PackageIssue[0].AffectedLocation.Version.Name)
-			packages[req.PackageIssue[0].AffectedLocation.Package] = req.PackageIssue[0].AffectedLocation.Version.Name
-			warn++
+		if error != nil {
+			log.Fatalf("Could not get authenticated client: %v", error)
 		}
-		count++
-	}
-	if warn > 0 {
-		log.Warn("Number of High issues ", warn)
-	}
+		var count int
+		var warn int
+		var packages map[string]string
+		packages = make(map[string]string)
+		for _, element := range vs {
 
-	log.Infof("Number of issues %d", count)
+			req := element.GetVulnerability()
+			if strings.Contains("HIGH", req.Severity.String()) {
+
+				log.Warn("Severity ", req.Severity, " ", req.PackageIssue[0].AffectedLocation.Package, " ", req.PackageIssue[0].AffectedLocation.Version.Name)
+				packages[req.PackageIssue[0].AffectedLocation.Package] = req.PackageIssue[0].AffectedLocation.Version.Name
+				warn++
+
+			}
+			count++
+		}
+		if warn > 0 {
+			log.Warn("Number of High issues ", warn)
+		}
+
+		log.Infof("Number of issues %d", count)
+		fmt.Println(packages)
+		vulnerabilitycollector.WriteToSlack(*url, warn, packages, count)
+
+	}
 
 }
