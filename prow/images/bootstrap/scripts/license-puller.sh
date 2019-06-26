e450f072257a07db5fc9cb42ade01bda09c9bd45
Michał Hudy <michal@hudy.pl>
2019-06-26 19:33:37 +0200

    Introduce License Puller

---
+++ prow/images/bootstrap/scripts/license-puller.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+#Description: This script is responsible for downloading license files for dependencies
+#
+#Expected vars:
+# - BUILDPACK_NAME: name of the buildpack
+
+set -e
+
+readonly TMP_DIR="./.license-puller"
+readonly LICENSES_DIR="./licenses"
+
+LANGUAGE=
+function init() {
+    if [[ -z ${BUILDPACK_NAME} ]]; then
+        echo "BUILDPACK_NAME variable is not set"
+        exit 1
+    fi
+
+    if [[ ${BUILDPACK_NAME} =~ golang ]]; then
+        LANGUAGE=golang
+    fi
+
+    if [[ ${BUILDPACK_NAME} =~ node ]]; then
+        LANGUAGE=node
+    fi
+    readonly LANGUAGE
+
+    if [[ -z ${LANGUAGE} ]]; then
+        echo "Unsupported buildpack '${BUILDPACK_NAME}'"
+        exit 1
+    fi
+
+    echo "Will work in '${LANGUAGE}' mode"
+}
+
+# TODO: This is temporary solution for Golang
+function pullGoLicenses() {
+    echo "Pulling licenses for Golang dependencies"
+
+    go list -json ./... > "${TMP_DIR}/golang.json"
+
+    jq -sr '[{ data: map(.) } | .data[] | select(has("ImportMap")) | .ImportMap | keys[]] | unique | values[]' "${TMP_DIR}/golang.json" \
+        | sed -e 's/sigs\.k8s\.io/github\.com\/kubernetes-sigs/g' \
+        | sed -e 's/k8s\.io/github\.com\/kubernetes/g' \
+        | grep -oE "^[^\/]+\/[^\/]+\/[^\/]+" \
+        | sort -u \
+        | while IFS=$'\t' read -r repository; do
+            local outputDir="${LICENSES_DIR}/${repository}"
+            mkdir -p "${outputDir}"
+
+            downloadLicense "${outputDir}" "${repository}"
+        done
+}
+
+# TODO: This is temporary solution for Golang
+function downloadLicense() {
+    local output=${1}
+    local repository=${2}
+    local url="https://${repository/github.com/raw.githubusercontent.com}/master"
+
+    echo "Downloading license from '${repository}' to '${output}''"
+    for file in "LICENSE" "LICENSE.md" "LICENSE.txt" "UNLICENSE"; do
+        if [[ "$(ls -A "${output}")" ]]; then
+            break
+        fi
+
+        echo "  Trying with '${file}' file..."
+        set +e
+        curl "${url}/${file}" -sL --fail --output "${output}/${file}"
+        set -e
+    done
+
+    if [[ -z "$(ls -A "${output}")" ]]; then
+        echo "  Cannot find license file for '${repository}'"
+        rm -rf "${output}"
+    else
+        echo "  Downloaded"
+    fi
+}
+
+function pullNodeLicenses() {
+    echo "Pulling licenses for Node dependencies"
+
+    echo "Installing license-checker"
+    npm i -g license-checker
+
+    echo "Gathering dependencies"
+    npx license-checker --production --json --direct --out "${TMP_DIR}/node.json"
+
+    echo "Copying license files to '${LICENSES_DIR}'"
+    jq -r '. | keys[] as $key | [$key, (.[$key] | .licenseFile)] | @tsv' "${TMP_DIR}/node.json" |
+        while IFS=$'\t' read -r key licenseFile; do
+            if [[ -z "${licenseFile}" ]]; then
+                continue
+            fi
+
+            local outputDir="${LICENSES_DIR}/${key}"
+            echo "Copying '${licenseFile}' to '${outputDir}'"
+            mkdir -p "${outputDir}"
+            cp "${licenseFile}" "${outputDir}/"
+        done
+}
+
+function main() {
+    init
+    mkdir -p "${TMP_DIR}"
+    mkdir -p "${LICENSES_DIR}"
+
+    if [[ ${LANGUAGE} == golang ]]; then
+        pullGoLicenses
+    else
+        pullNodeLicenses
+    fi
+
+    rm -rf "${TMP_DIR}"
+}
+
+main
