3a124ce21fa6fc197531a8082f6b125ca41e9688
Tomasz Gorgol <t.gorgol@sap.com>
2019-07-15 16:39:32 +0200

    Add compass schema-migrator component build job

--- development/tools/jobs/tester/tester.go
+++ development/tools/jobs/tester/tester.go
@@ -77,6 +77,8 @@ const (
 	ImageNodeChromiumBuildpackLatest = "eu.gcr.io/kyma-project/prow/test-infra/buildpack-node-chromium:v20181207-d46c013"
 	// ImageBootstrapLatest means Bootstrap image
 	ImageBootstrapLatest = "eu.gcr.io/kyma-project/prow/test-infra/bootstrap:v20181121-f3ea5ce"
+	// ImageBootstrap20190604 represents boostrap image published on 2019.06.04
+	ImageBootstrap20190604 = "eu.gcr.io/kyma-project/test-infra/bootstrap:v20190604-d08e7fe"
 	// ImageBootstrap20181204 represents boostrap image published on 2018.12.04
 	ImageBootstrap20181204 = "eu.gcr.io/kyma-project/prow/test-infra/bootstrap:v20181204-a6e79be"
 	// ImageBootstrap001 represents version 0.0.1 of bootstrap image
