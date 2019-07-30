20c6ca4886808a8dcfc68a78300ea07fbc4ba5d0
Michał Hudy <michal@hudy.pl>
2019-07-30 13:46:49 +0200

    Optional

--- development/tools/jobs/kyma/cms_services_test.go
+++ development/tools/jobs/kyma/cms_services_test.go
@@ -47,7 +47,7 @@ func TestCmsServicesJobPresubmit(t *testing.T) {
 	assert.Equal(t, 10, actualPresubmit.MaxConcurrency)
 	assert.False(t, actualPresubmit.SkipReport)
 	assert.True(t, actualPresubmit.Decorate)
-	assert.False(t, actualPresubmit.Optional)
+	assert.True(t, actualPresubmit.Optional)
 	assert.Equal(t, "github.com/kyma-project/kyma", actualPresubmit.PathAlias)
 
 	tester.AssertThatHasExtraRefTestInfra(t, actualPresubmit.JobBase.UtilityConfig, "master")
