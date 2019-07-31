4ebcabe042b5a0599663c56508bbe8900f5721ee
kasiakepka <kasia.kepka23@gmail.com>
2019-07-31 14:59:56 +0200

    adjust VM settings (#1311)

--- prow/scripts/provision-vm-compass.sh
+++ prow/scripts/provision-vm-compass.sh
@@ -89,7 +89,8 @@ for ZONE in ${EU_ZONES}; do
     gcloud compute instances create "compass-integration-test-${RANDOM_ID}" \
         --metadata enable-oslogin=TRUE \
         --image "${IMAGE}" \
-        --machine-type n1-standard-4 \
+        --custom-cpu 2
+        --custom-memory 12
         --zone "${ZONE}" \
         --boot-disk-size 30 "${LABELS[@]}" &&\
     shout "Created compass-integration-test-${RANDOM_ID} in zone ${ZONE}" && break
