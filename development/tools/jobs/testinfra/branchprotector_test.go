66e33a8f941335309cf27b8f060478cb65965ccc
Piotr Jasiak <piotr.jasiak@sap.com>
2019-08-07 14:30:07 +0200

    Add helm-broker to prow pipelines (#1345)
    * Add helm-broker to prow pipelines
    
    * Remove kyma component: helm-broker job

--- development/tools/jobs/testinfra/branchprotector_test.go
+++ development/tools/jobs/testinfra/branchprotector_test.go
@@ -32,6 +32,7 @@ func TestBranchProtection(t *testing.T) {
 		{"kyma-project", "examples", "master", []string{"license/cla"}, 1},
 		{"kyma-project", "addons", "master", []string{"license/cla"}, 1},
 		{"kyma-project", "cli", "master", []string{"license/cla"}, 1},
+		{"kyma-project", "helm-broker", "master", []string{"license/cla"}, 1},
 		{"kyma-incubator", "varkes", "master", []string{"license/cla"}, 1},
 		{"kyma-incubator", "vstudio-extension", "master", []string{"license/cla"}, 1},
 		{"kyma-incubator", "service-catalog-tester", "master", []string{"license/cla"}, 1},
