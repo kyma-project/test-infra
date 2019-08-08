20876e1d014e859c24c4e82b0cd19949032b6417
Krzysztof <6783567+kwiatekus@users.noreply.github.com>
2019-08-08 14:36:03 +0200

    Merge catalog-ui instances-ui brokers-ui build pipelines (#1358)
    Merge catalog-ui, instances-ui, brokers-ui build pipelines

--- development/tools/jobs/console/service-catalog-ui_test.go
+++ development/tools/jobs/console/service-catalog-ui_test.go
@@ -8,13 +8,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestInstancesJobPresubmit(t *testing.T) {
+func TestServiceCatalogUIJobPresubmit(t *testing.T) {
 	// WHEN
-	jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/console/instances/instances.yaml")
+	jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/console/service-catalog-ui/service-catalog-ui.yaml")
 	// THEN
 	require.NoError(t, err)
 
-	expName := "pre-master-console-instances"
+	expName := "pre-master-console-service-catalog-ui"
 	actualPresubmit := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-project/console"], expName, "master")
 	require.NotNil(t, actualPresubmit)
 	assert.Equal(t, expName, actualPresubmit.Name)
@@ -26,20 +26,20 @@ func TestInstancesJobPresubmit(t *testing.T) {
 	assert.Equal(t, "github.com/kyma-project/console", actualPresubmit.PathAlias)
 	tester.AssertThatHasExtraRefTestInfra(t, actualPresubmit.JobBase.UtilityConfig, "master")
 	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepo, tester.PresetGcrPush, tester.PresetBuildPr)
-	assert.Equal(t, "(^instances/)|(^components/react/)", actualPresubmit.RunIfChanged)
-	tester.AssertThatJobRunIfChanged(t, *actualPresubmit, "instances/some_random_file.js")
+	assert.Equal(t, "(^service-catalog-ui/)|(^components/react/)", actualPresubmit.RunIfChanged)
+	tester.AssertThatJobRunIfChanged(t, *actualPresubmit, "service-catalog-ui/some_random_file.js")
 	assert.Equal(t, tester.ImageNodeBuildpackLatest, actualPresubmit.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build.sh"}, actualPresubmit.Spec.Containers[0].Command)
-	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/console/instances"}, actualPresubmit.Spec.Containers[0].Args)
+	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/console/service-catalog-ui"}, actualPresubmit.Spec.Containers[0].Args)
 }
 
-func TestInstancesJobPostsubmit(t *testing.T) {
+func TestServiceCatalogUIJobPostsubmit(t *testing.T) {
 	// WHEN
-	jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/console/instances/instances.yaml")
+	jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/console/service-catalog-ui/service-catalog-ui.yaml")
 	// THEN
 	require.NoError(t, err)
 
-	expName := "post-master-console-instances"
+	expName := "post-master-console-service-catalog-ui"
 	actualPost := tester.FindPostsubmitJobByName(jobConfig.Postsubmits["kyma-project/console"], expName, "master")
 	require.NotNil(t, actualPost)
 	assert.Equal(t, expName, actualPost.Name)
@@ -50,8 +50,8 @@ func TestInstancesJobPostsubmit(t *testing.T) {
 	assert.Equal(t, "github.com/kyma-project/console", actualPost.PathAlias)
 	tester.AssertThatHasExtraRefTestInfra(t, actualPost.JobBase.UtilityConfig, "master")
 	tester.AssertThatHasPresets(t, actualPost.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepo, tester.PresetGcrPush, tester.PresetBuildConsoleMaster)
-	assert.Equal(t, "(^instances/)|(^components/react/)", actualPost.RunIfChanged)
+	assert.Equal(t, "(^service-catalog-ui/)|(^components/react/)", actualPost.RunIfChanged)
 	assert.Equal(t, tester.ImageNodeBuildpackLatest, actualPost.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build.sh"}, actualPost.Spec.Containers[0].Command)
-	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/console/instances"}, actualPost.Spec.Containers[0].Args)
+	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/console/service-catalog-ui"}, actualPost.Spec.Containers[0].Args)
 }
