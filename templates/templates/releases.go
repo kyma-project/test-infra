e621f2285a0c974a58515ede3226c004b63580c3
Szymon Janota <szymon.janota@sap.com>
2019-08-13 08:36:55 +0200

    Generate releases for testing

--- templates/templates/releases.go
+++ templates/templates/releases.go
@@ -5,17 +5,17 @@ package tester
 // When we removing support for given version, there remove
 // its entry also here.
 var (
-	Release14 = mustParse("1.4")
-	Release13 = mustParse("1.3")
-	Release12 = mustParse("1.2")
+{{- range .Global.releases }}
+	Release{{ . | replace "." "" }} = mustParse("{{ . }}")
+{{- end }}
 )
 
 // GetAllKymaReleaseBranches returns all supported kyma release branches
 func GetAllKymaReleaseBranches() []*SupportedRelease {
 	return []*SupportedRelease{
-		Release14,
-		Release13,
-		Release12,
+{{- range .Global.releases }}
+		Release{{ . | replace "." "" }},
+{{- end }}
 	}
 }
 
