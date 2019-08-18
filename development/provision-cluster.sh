c668cb2c70ccbc0716835066acdf6bb238b5c6e8
dekiel <przemyslaw.pokrywka@sap.com>
2019-08-18 09:42:22 +0200

    Alligned installation scripts to recent GCP changes. Corrected errors in installation scripts. Added missing steps in installation scripts. Automated manual steps by improving installation scripts. Implemented prow slack notification channel in alertmanager. Implemented prow slack channel notification template. Implemented prow alerting rules in prometheus.

--- development/provision-cluster.sh
+++ development/provision-cluster.sh
@@ -15,9 +15,17 @@ if [ -z "$ZONE" ]; then
       exit 1
 fi
 
-NUM_NODES=${NUM_NODES:-2} # default 2 nodes
+#NUM_NODES=${NUM_NODES:-2} # default 2 nodes
+NUM_NODES=4
 
 echo "Provisioning cluster '${CLUSTER_NAME}' (${NUM_NODES} nodes) in project '${PROJECT}' and zone '${ZONE}'"
 
 gcloud container --project "${PROJECT}" clusters create "${CLUSTER_NAME}" \
-  --zone "${ZONE}" --machine-type n1-standard-1 --num-nodes "${NUM_NODES}"
+  --zone "${ZONE}" --issue-client-certificate --enable-basic-auth --machine-type n1-standard-1 --num-nodes "${NUM_NODES}"
+
+if [ -z "$WORKLOAD_CLUSTER_NAMER" ]; then
+    echo "Provisioning workload cluster '${WORKLOAD_CLUSTER_NAME}' (${NUM_NODES} nodes) in project '${PROJECT}' and zone '${ZONE}'"
+    gcloud container --project "${PROJECT}" clusters create "${WORKLOAD_CLUSTER_NAME}" \
+        --zone "${ZONE}" --issue-client-certificate --enable-basic-auth --machine-type n1-standard-1 --num-nodes "${NUM_NODES}"
+fi
+
