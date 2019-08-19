4365715a2074089292e74dff9a4855a1a05f56cd
Daniel Roth <daniel.roth02@sap.com>
2019-08-19 07:35:21 -0700

    DNS & IP cleaner fix (move from bash to golang cleaners) (#884)
    * add ip & dns cleaner in golang

---
+++ development/tools/cmd/dnscleaner/README.md
@@ -0,0 +1,54 @@
+# DNS Cleaner
+
+## Overview
+
+This command finds and removes DNS entries created by the `kyma-gke-long-lasting` job in a Google Cloud Platform (GCP) project.
+
+The `kyma-gke-long-lasting` job creates a GKE cluster to install and test Kyma.
+
+## Usage
+
+For safety reasons, the dry-run mode is the default one.
+To run it, use:
+```bash
+env GOOGLE_APPLICATION_CREDENTIALS={path to service account file} go run main.go \
+    --project={gcloud project name} \
+    --zone={gcloud zone} \
+    --name={resource name} \
+    --address={address of the resource}
+```
+
+To turn the dry-run mode off, use:
+```bash
+env GOOGLE_APPLICATION_CREDENTIALS={path to service account file} go run main.go \
+    --project={gcloud project name} \
+    --zone={gcloud zone} \
+    --name={resource name} \
+    --address={address of the resource} \
+    --dryRun=false
+```
+
+### Flags
+
+See the list of available flags:
+
+| Name                      | Required | Description                                                                                          |
+| :------------------------ | :------: | :--------------------------------------------------------------------------------------------------- |
+| **--project**             |   YES    | GCP project name.
+| **--zone**                |   YES    | GCP zone name.
+| **--name**                |   YES    | GCP DNS resource name.
+| **--address**             |   YES    | GCP resource's attached IP.
+| **--rtype**               |    NO    | DNS Record Type to search for. The default value is `A`.
+| **--ttl**                 |    NO    | TTL of the resource. The default value is `300`.
+| **--maxAttempts**         |    NO    | Maximum number of retries in the backoff. The default value is `3`.
+| **--backoff**             |    NO    | Initial backoff in seconds for the first retry. The backoff will increase after this time. The default value is `5`.
+| **--dryRun**              |    NO    | The boolean value that controls the dry-run mode. The default value is `true`.
+
+### Environment variables
+
+See the list of available environment variables:
+
+| Name                                  | Required | Description                                                                                          |
+| :------------------------------------ | :------: | :--------------------------------------------------------------------------------------------------- |
+| **GOOGLE_APPLICATION_CREDENTIALS**    |    YES   | The path to the service account file. The service account requires at least `container.clusters.list` and `container.clusters.delete` Google IAM permissions. |
+
---
+++ development/tools/pkg/dnscleaner/remover.go
@@ -0,0 +1,95 @@
+package dnscleaner
+
+import (
+	"context"
+	"time"
+
+	"github.com/kyma-project/test-infra/development/tools/pkg/common"
+	log "github.com/sirupsen/logrus"
+	dns "google.golang.org/api/dns/v1"
+)
+
+//go:generate mockery -name=DNSAPI -output=automock -outpkg=automock -case=underscore
+
+// DNSAPI abstracts access to DNS API in GCP
+type DNSAPI interface {
+	RemoveDNSEntry(ctx context.Context, project, zone string, record *dns.ResourceRecordSet) error
+	LookupDNSEntry(ctx context.Context, project, zone, name, address, recordType string, recordTTL int64) (*dns.ResourceRecordSet, error)
+}
+
+// DNSEntryRemover deletes IPs provisioned by gke-long-lasting prow jobs.
+type DNSEntryRemover struct {
+	dnsAPI      DNSAPI
+	maxAttempts uint
+	backoff     uint
+	makeChanges bool
+}
+
+// New returns a new instance of dnsEntryRemover
+func New(dnsAPI DNSAPI, maxAttempts, backoff uint, makeChanges bool) *DNSEntryRemover {
+	if maxAttempts < 1 {
+		maxAttempts = 1
+	}
+	return &DNSEntryRemover{dnsAPI, maxAttempts, backoff, makeChanges}
+}
+
+// Run executes dns removal process for specified dns record-set
+func (der *DNSEntryRemover) Run(project, zone, dnsName, dnsAddress, recordType string, recordTTL int64) error {
+	common.Shout("Trying to retrieve DNS entry with name \"%s\" in project \"%s\", available in zone \"%s\" with Address: \"%s\"", dnsName, project, zone, dnsAddress)
+
+	ctx := context.Background()
+
+	backoff := der.backoff
+	var getErr error
+	var recordSet *dns.ResourceRecordSet
+	for attempt := uint(0); attempt < der.maxAttempts; attempt++ {
+		entry, lookupErr := der.dnsAPI.LookupDNSEntry(ctx, project, zone, dnsName, dnsAddress, recordType, recordTTL)
+		if entry != nil {
+			recordSet = entry
+			break
+		}
+		if attempt < der.maxAttempts {
+			time.Sleep(time.Duration(backoff) * time.Second)
+			backoff = backoff * 2
+			getErr = lookupErr
+		}
+	}
+	if recordSet == nil {
+		return getErr
+	}
+
+	common.Shout("Trying to delete DNS retrieved record set with name \"%s\" in project \"%s\", available in zone \"%s\" with Address: \"%s\"", dnsName, project, zone, dnsAddress)
+
+	var msgPrefix string
+	if !der.makeChanges {
+		msgPrefix = "[DRY RUN] "
+	}
+
+	var delErr error
+	backoff = der.backoff
+	if der.makeChanges {
+		for attempt := uint(0); attempt < der.maxAttempts; attempt++ {
+			removalErr := der.dnsAPI.RemoveDNSEntry(ctx, project, zone, recordSet)
+			if removalErr == nil {
+				delErr = nil
+				break
+			}
+			if removalErr.Error() == dnsDeletionFailed {
+				delErr = removalErr
+				break
+			}
+			if attempt < der.maxAttempts {
+				time.Sleep(time.Duration(backoff) * time.Second)
+				backoff = backoff * 2
+				delErr = removalErr
+			}
+		}
+	}
+	if delErr != nil {
+		log.Errorf("Could not delete DNS entry with name \"%s\" in zone \"%s\" of project \"%s\", got error: %s", dnsName, zone, project, delErr.Error())
+	} else {
+		log.Infof("%sRequested deletion of DNS entry with name \"%s\" in zone \"%s\" of project \"%s\"", msgPrefix, dnsName, zone, project)
+	}
+
+	return delErr
+}
