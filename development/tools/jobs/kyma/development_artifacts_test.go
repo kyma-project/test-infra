bed2030489754001619ba9bfd8df68eabf68c12f
kubadz <jakub.dziechciewicz@sap.com>
2019-08-27 11:34:45 +0200

    use newer buildpack-golang image in development-artifacts job

--- development/tools/jobs/kyma/development_artifacts_test.go
+++ development/tools/jobs/kyma/development_artifacts_test.go
@@ -28,7 +28,7 @@ func TestPresubmitDevelopmentArtifacts(t *testing.T) {
 	tester.AssertThatHasPresets(t, job.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepo, "preset-kyma-development-artifacts-bucket", tester.PresetGcrPush, tester.PresetBuildPr)
 	require.Len(t, job.Spec.Containers, 1)
 	cont := job.Spec.Containers[0]
-	assert.Equal(t, "eu.gcr.io/kyma-project/prow/test-infra/buildpack-golang:v20181204-a6e79be", cont.Image)
+	assert.Equal(t, tester.ImageGolangBuildpack1_12, cont.Image)
 	require.Len(t, cont.Command, 1)
 	assert.Equal(t, "/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build-kyma-development-artifacts.sh", cont.Command[0])
 	require.Len(t, job.Spec.Volumes, 1)
@@ -53,7 +53,7 @@ func TestPostsubmitDevelopmentArtifcts(t *testing.T) {
 	tester.AssertThatHasPresets(t, job.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepo, "preset-kyma-development-artifacts-bucket", tester.PresetGcrPush, tester.PresetBuildMaster)
 	require.Len(t, job.Spec.Containers, 1)
 	cont := job.Spec.Containers[0]
-	assert.Equal(t, "eu.gcr.io/kyma-project/prow/test-infra/buildpack-golang:v20181204-a6e79be", cont.Image)
+	assert.Equal(t, tester.ImageGolangBuildpack1_12, cont.Image)
 	require.Len(t, cont.Command, 1)
 	assert.Equal(t, "/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build-kyma-development-artifacts.sh", cont.Command[0])
 	require.Len(t, job.Spec.Volumes, 1)
