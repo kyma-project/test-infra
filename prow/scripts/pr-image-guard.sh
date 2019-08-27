fcc6e377a73019c3e7bc227a704c5c90913fee42
Krystian Cieslik <crabtree@users.noreply.github.com>
2019-08-27 14:02:11 +0200

    Add pr images guard job

---
+++ prow/scripts/pr-image-guard.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+readonly RESOURCES_PATH="/home/prow/go/src/github.com/kyma-project/kyma/resources"
+
+RES=$(grep -e 'version:\s*[Pp][Rr]-.*' -e 'image:.*:[Pp][Rr]-.*' -r "${RESOURCES_PATH}" | wc -l | xargs)
+
+exit $RES
