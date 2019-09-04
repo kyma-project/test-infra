999abf4017ffd715f7108dab532fbe9855cc2b54
Mariusz Szymański <mariusz.szymanski@sap.com>
2019-09-04 09:35:27 +0200

    Provisioner pipeline (#1431)
    * Provisioner added
    
    * Test name fix
    
    * Provisioner tests added

---
+++ development/tools/jobs/incubator/compass/provisioner_tests_test.go
@@ -0,0 +1,51 @@
+package compass_test
+
+import (
+	"testing"
+
+	"github.com/kyma-project/test-infra/development/tools/jobs/tester"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const provisionerTestsJobPath = "./../../../../../prow/jobs/incubator/compass/tests/provisioner/provisioner-tests.yaml"
+
+func TestProvisionerTestsJobPresubmit(t *testing.T) {
+	// WHEN
+	jobConfig, err := tester.ReadJobConfig(provisionerTestsJobPath)
+	// THEN
+	require.NoError(t, err)
+
+	actualPre := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-incubator/compass"], "pre-master-compass-tests-provisioner-tests", "master")
+	require.NotNil(t, actualPre)
+
+	assert.Equal(t, 10, actualPre.MaxConcurrency)
+	assert.False(t, actualPre.SkipReport)
+	assert.True(t, actualPre.Decorate)
+	assert.False(t, actualPre.Optional)
+	assert.Equal(t, "github.com/kyma-incubator/compass", actualPre.PathAlias)
+	tester.AssertThatHasExtraRefTestInfra(t, actualPre.JobBase.UtilityConfig, "master")
+	tester.AssertThatHasPresets(t, actualPre.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepoIncubator, tester.PresetGcrPush, tester.PresetBuildPr)
+	assert.Equal(t, "^tests/provisioner-tests/", actualPre.RunIfChanged)
+	tester.AssertThatJobRunIfChanged(t, *actualPre, "tests/provisioner-tests/some_random_file.go")
+	tester.AssertThatExecGolangBuildpack(t, actualPre.JobBase, tester.ImageGolangBuildpack1_11, "/home/prow/go/src/github.com/kyma-incubator/compass/tests/provisioner-tests")
+}
+
+func TestProvisionerTestsJobPostsubmit(t *testing.T) {
+	// WHEN
+	jobConfig, err := tester.ReadJobConfig(provisionerTestsJobPath)
+	// THEN
+	require.NoError(t, err)
+
+	actualPost := tester.FindPostsubmitJobByName(jobConfig.Postsubmits["kyma-incubator/compass"], "post-master-compass-tests-provisioner-tests", "master")
+	require.NotNil(t, actualPost)
+
+	assert.Equal(t, 10, actualPost.MaxConcurrency)
+	assert.True(t, actualPost.Decorate)
+	assert.Equal(t, "github.com/kyma-incubator/compass", actualPost.PathAlias)
+	tester.AssertThatHasExtraRefTestInfra(t, actualPost.JobBase.UtilityConfig, "master")
+	tester.AssertThatHasPresets(t, actualPost.JobBase, tester.PresetDindEnabled, tester.PresetDockerPushRepoIncubator, tester.PresetGcrPush, tester.PresetBuildMaster)
+	assert.Equal(t, "^tests/provisioner-tests/", actualPost.RunIfChanged)
+	tester.AssertThatJobRunIfChanged(t, *actualPost, "tests/provisioner-tests/some_random_file.go")
+	tester.AssertThatExecGolangBuildpack(t, actualPost.JobBase, tester.ImageGolangBuildpack1_11, "/home/prow/go/src/github.com/kyma-incubator/compass/tests/provisioner-tests")
+}
