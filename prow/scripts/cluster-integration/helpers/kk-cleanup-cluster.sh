e8fac47aad68c0ad027114634f0c0ceaaf09d1bf
Katarzyna Kepka <kasia.kepka23@gmail.com>
2019-09-06 08:39:42 +0200

    new versions of scripts

---
+++ prow/scripts/cluster-integration/helpers/kk-cleanup-cluster.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+
+#Description: Deletes a GKE cluster if exists along with DNS_RECORDS and STATIC IPs etc.
+#
+#Expected vars:
+# - CLUSTER_NAME: name of the GKE cluster
+# - TEST_INFRA_SOURCES_DIR: absolute path for test-infra/ directory
+# - TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS: absolute path for test-infra/prow/scripts/cluster-integration/helpers directory
+# - CLOUDSDK_COMPUTE_REGION: region where the GKE cluster is e.g. europe-west1-b
+# - CLOUDSDK_DNS_ZONE_NAME: dns zone
+#
+#Permissions: In order to run this script you need to use a service account with "Compute Admin,DNS Administrator, Kubernetes Engine Admin, Kubernetes Engine Cluster Admin, Service Account User, Storage Admin" role
+
+# shellcheck disable=SC1090
+source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/library.sh"
+
+function cleanup() {
+	
+	shout "Running cleanup-cluster process"
+	discoverUnsetVar=false
+
+	for var in CLUSTER_NAME TEST_INFRA_SOURCES_DIR TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS CLOUDSDK_COMPUTE_REGION CLOUDSDK_DNS_ZONE_NAME; do
+		if [ -z "${!var}" ] ; then
+			echo "ERROR: $var is not set"
+			discoverUnsetVar=true
+		fi
+	done
+
+	if [[ "${PERFORMACE_CLUSTER_SETUP}" == "" ]]; then
+		for var in GCLOUD_NETWORK_NAME GCLOUD_SUBNET_NAME; do
+			if [ -z "${!var}" ] ; then
+				echo "ERROR: $var is not set"
+				discoverUnsetVar=true
+			fi
+		done
+	fi
+
+	if [ "${discoverUnsetVar}" = true ] ; then
+		exit 1
+	fi
+
+    CLUSTER_EXISTS=$(gcloud container clusters list --filter="name~^${CLUSTER_NAME}" --format json | jq '.[].name' | tr -d '"' | wc -l)
+    if [[ "$CLUSTER_EXISTS" -gt 0 ]]; then
+		echo "Cleaning up: $CLUSTER_NAME"
+		removeCluster
+		echo "Cluster: $CLUSTER_NAME cleanup completed, moving to NET and DNS resources cleanup"
+		removeResources
+	else
+		echo "Cluster: $CLUSTER_NAME not found, cleaning up NET and DNS resources"
+		removeResources
+    fi
+
+}
+
+function removeCluster() {
+	#Turn off exit-on-error so that next step is executed even if previous one fails.
+	set +e
+
+    shout "Fetching OLD_TIMESTAMP from cluster to be deleted"
+	readonly OLD_TIMESTAMP=$(gcloud container clusters describe "${CLUSTER_NAME}" --zone="${GCLOUD_COMPUTE_ZONE}" --project="${GCLOUD_PROJECT_NAME}" --format=json | jq --raw-output '.resourceLabels."created-at-readable"')
+
+	shout "Delete cluster $CLUSTER_NAME"
+	"${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/deprovision-gke-cluster.sh
+	TMP_STATUS=$?
+	if [[ ${TMP_STATUS} -ne 0 ]]; then EXIT_STATUS=${TMP_STATUS}; fi
+
+	MSG=""
+	if [[ ${EXIT_STATUS} -ne 0 ]]; then MSG="(exit status: ${EXIT_STATUS})"; fi
+	shout "Cluster removal is finished: ${MSG}"
+	date
+
+	shout "Delete temporary Kyma-Installer Docker image with timestamp: ${OLD_TIMESTAMP}"
+	date
+	echo "KYMA_INSTALLER_IMAGE=${DOCKER_PUSH_REPOSITORY}${DOCKER_PUSH_DIRECTORY}/${CLUSTER_NAME}/${REPO_OWNER}/${REPO_NAME}:${OLD_TIMESTAMP}"
+	KYMA_INSTALLER_IMAGE="${DOCKER_PUSH_REPOSITORY}${DOCKER_PUSH_DIRECTORY}/${CLUSTER_NAME}/${REPO_OWNER}/${REPO_NAME}:${OLD_TIMESTAMP}" "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/delete-image.sh
+	TMP_STATUS=$?
+	if [[ ${TMP_STATUS} -ne 0 ]]; then EXIT_STATUS=${TMP_STATUS}; fi
+
+	set -e
+}
+
+function removeResources() {
+    if [[ "${PERFORMACE_CLUSTER_SETUP}" == "" ]]; then
+		set +e
+
+		shout "Delete Gateway DNS Record"
+		date
+		GATEWAY_IP_ADDRESS=$(gcloud compute addresses describe "${CLUSTER_NAME}" --format json --region "${CLOUDSDK_COMPUTE_REGION}" | jq '.address' | tr -d '"')
+		GATEWAY_DNS_FULL_NAME="*.${DOMAIN}"
+
+		shout "running /delete-dns-record.sh --project=${GCLOUD_PROJECT_NAME} --zone=${CLOUDSDK_DNS_ZONE_NAME} --name=${GATEWAY_DNS_FULL_NAME} --address=${GATEWAY_IP_ADDRESS} --dryRun=false"
+
+		"${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/delete-dns-record.sh --project="${GCLOUD_PROJECT_NAME}" --zone="${CLOUDSDK_DNS_ZONE_NAME}" --name="${GATEWAY_DNS_FULL_NAME}" --address="${GATEWAY_IP_ADDRESS}" --dryRun=false
+		TMP_STATUS=$?
+		if [[ ${TMP_STATUS} -ne 0 ]]; then EXIT_STATUS=${TMP_STATUS}; fi
+
+		shout "Release Gateway IP Address"
+		date
+		GATEWAY_IP_ADDRESS_NAME=${CLUSTER_NAME}
+		"${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/release-ip-address.sh --project="${GCLOUD_PROJECT_NAME}" --ipname="${GATEWAY_IP_ADDRESS_NAME}" --region="${CLOUDSDK_COMPUTE_REGION}" --dryRun=false
+		TMP_STATUS=$?
+		if [[ ${TMP_STATUS} -ne 0 ]]; then EXIT_STATUS=${TMP_STATUS}; fi
+
+    fi
+
+	MSG=""
+	if [[ ${EXIT_STATUS} -ne 0 ]]; then MSG="(exit status: ${EXIT_STATUS})"; fi
+	shout "DNS, Gateway IP and Kyma installer image cleanup is finished: ${MSG}"
+	date
+
+	set -e
+}
+
+cleanup
---
+++ prow/scripts/cluster-integration/kk-gke-long-lasting.sh
@@ -0,0 +1,241 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail  # Fail a pipe if any sub-command fails.
+
+discoverUnsetVar=false
+
+for var in INPUT_CLUSTER_NAME DOCKER_PUSH_REPOSITORY DOCKER_PUSH_DIRECTORY KYMA_PROJECT_DIR CLOUDSDK_CORE_PROJECT CLOUDSDK_COMPUTE_REGION CLOUDSDK_COMPUTE_ZONE CLOUDSDK_DNS_ZONE_NAME GOOGLE_APPLICATION_CREDENTIALS SLACK_CLIENT_TOKEN SLACK_CLIENT_WEBHOOK_URL STABILITY_SLACK_CLIENT_CHANNEL_ID; do
+    if [ -z "${!var}" ] ; then
+        echo "ERROR: $var is not set"
+        discoverUnsetVar=true
+    fi
+done
+if [ "${discoverUnsetVar}" = true ] ; then
+    exit 1
+fi
+
+export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
+export TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS="${TEST_INFRA_SOURCES_DIR}/prow/scripts/cluster-integration/helpers"
+export KYMA_SOURCES_DIR="${KYMA_PROJECT_DIR}/kyma"
+export KYMA_SCRIPTS_DIR="${KYMA_SOURCES_DIR}/installation/scripts"
+
+export GCLOUD_PROJECT_NAME="${CLOUDSDK_CORE_PROJECT}"
+export GCLOUD_COMPUTE_ZONE="${CLOUDSDK_COMPUTE_ZONE}"
+export GCLOUD_SERVICE_KEY_PATH="${GOOGLE_APPLICATION_CREDENTIALS}"
+
+export REPO_OWNER="kyma-project"
+export REPO_NAME="kyma"
+readonly CURRENT_TIMESTAMP=$(date +%Y%m%d)
+
+CLUSTER_NAME=$(echo "${INPUT_CLUSTER_NAME}" | tr "[:upper:]" "[:lower:]")
+export CLUSTER_NAME
+
+export GCLOUD_NETWORK_NAME="kk-gke-long-lasting-net"
+export GCLOUD_SUBNET_NAME="kk-gke-long-lasting-subnet"
+
+if [ -z "${SERVICE_CATALOG_CRD}" ]; then
+	export SERVICE_CATALOG_CRD="false"
+fi
+
+TEST_RESULT_WINDOW_TIME=${TEST_RESULT_WINDOW_TIME:-3h}
+# shellcheck disable=SC1090
+source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/library.sh"
+
+function createCluster() {
+	shout "Reserve IP Address for Ingressgateway"
+	date
+	GATEWAY_IP_ADDRESS_NAME="${CLUSTER_NAME}"
+	GATEWAY_IP_ADDRESS=$(IP_ADDRESS_NAME=${GATEWAY_IP_ADDRESS_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/reserve-ip-address.sh)
+	echo "Created IP Address for Ingressgateway: ${GATEWAY_IP_ADDRESS}"
+
+	shout "Create DNS Record for Ingressgateway IP"
+	date
+	GATEWAY_DNS_FULL_NAME="*.${CLUSTER_NAME}.${DNS_DOMAIN}"
+	IP_ADDRESS=${GATEWAY_IP_ADDRESS} DNS_FULL_NAME=${GATEWAY_DNS_FULL_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/create-dns-record.sh
+
+	NETWORK_EXISTS=$("${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/network-exists.sh")
+	if [ "$NETWORK_EXISTS" -gt 0 ]; then
+		shout "Create ${GCLOUD_NETWORK_NAME} network with ${GCLOUD_SUBNET_NAME} subnet"
+		date
+		"${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-network-with-subnet.sh"
+	else
+		shout "Network ${GCLOUD_NETWORK_NAME} exists"
+	fi
+
+	shout "Provision cluster: \"${CLUSTER_NAME}\""
+	date
+	
+	if [ -z "$MACHINE_TYPE" ]; then
+		export MACHINE_TYPE="${DEFAULT_MACHINE_TYPE}"
+	fi
+	if [ -z "${CLUSTER_VERSION}" ]; then
+		export CLUSTER_VERSION="${DEFAULT_CLUSTER_VERSION}"
+	fi
+	env ADDITIONAL_LABELS="created-at=${CURRENT_TIMESTAMP}" "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/provision-gke-cluster.sh
+}
+
+function waitUntilInstallerApiAvailable() {
+    shout "Waiting for Installer API"
+
+	attempts=5
+    for ((i=1; i<=attempts; i++)); do
+        numberOfLines=$(kubectl api-versions | grep -c "installer.kyma-project.io")
+        if [[ "$numberOfLines" == "1" ]]; then
+            echo "API found"
+            break
+        elif [[ "${i}" == "${attempts}" ]]; then
+            echo "ERROR: API not found, exit"
+            exit 1
+        fi
+
+        echo "Sleep for 3 seconds"
+        sleep 3
+    done
+}
+
+function installKyma() {
+
+	kymaUnsetVar=false
+
+  # shellcheck disable=SC2043
+	for var in GATEWAY_IP_ADDRESS ; do
+    	if [ -z "${!var}" ] ; then
+        	echo "ERROR: $var is not set"
+        	kymaUnsetVar=true
+    	fi
+	done
+	if [ "${kymaUnsetVar}" = true ] ; then
+    	exit 1
+	fi
+
+	KYMA_INSTALLER_IMAGE="${DOCKER_PUSH_REPOSITORY}${DOCKER_PUSH_DIRECTORY}/${CLUSTER_NAME}/${REPO_OWNER}/${REPO_NAME}:${CURRENT_TIMESTAMP}"
+
+	shout "Build Kyma-Installer Docker image"
+	date
+	KYMA_INSTALLER_IMAGE="${KYMA_INSTALLER_IMAGE}" "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/create-image.sh
+
+	KYMA_RESOURCES_DIR="${KYMA_SOURCES_DIR}/installation/resources"
+	INSTALLER_YAML="${KYMA_RESOURCES_DIR}/installer.yaml"
+	INSTALLER_CR="${KYMA_RESOURCES_DIR}/installer-cr-cluster.yaml.tpl"
+
+	"${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/get-letsencrypt-cert.sh"
+	TLS_CERT=$(base64 -i ./letsencrypt/live/"${DOMAIN}"/fullchain.pem | tr -d '\n')
+	export TLS_CERT
+	TLS_KEY=$(base64 -i ./letsencrypt/live/"${DOMAIN}"/privkey.pem   | tr -d '\n')
+	export TLS_KEY
+
+	shout "Apply Kyma config"
+	date
+
+    sed -e 's;image: eu.gcr.io/kyma-project/.*/installer:.*$;'"image: ${KYMA_INSTALLER_IMAGE};" "${INSTALLER_YAML}" \
+        | kubectl apply -f-
+
+    "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-config-map.sh" --name "installation-config-overrides" \
+        --data "global.domainName=${DOMAIN}" \
+        --data "global.loadBalancerIP=${GATEWAY_IP_ADDRESS}"
+
+    "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-config-map.sh" --name "core-test-ui-acceptance-overrides" \
+        --data "test.acceptance.ui.logging.enabled=true" \
+        --label "component=core"
+
+    "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-config-map.sh" --name "cluster-certificate-overrides" \
+        --data "global.tlsCrt=${TLS_CERT}" \
+        --data "global.tlsKey=${TLS_KEY}"
+
+    "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-config-map.sh" --name "monitoring-config-overrides" \
+        --data "global.alertTools.credentials.slack.channel=${KYMA_ALERTS_CHANNEL}" \
+        --data "global.alertTools.credentials.slack.apiurl=${KYMA_ALERTS_SLACK_API_URL}" \
+        --label "component=monitoring"
+
+    "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-config-map.sh" --name "istio-overrides" \
+        --data "gateways.istio-ingressgateway.loadBalancerIP=${GATEWAY_IP_ADDRESS}" \
+        --label "component=istio"
+
+	if [ "${SERVICE_CATALOG_CRD}" = "true" ]; then
+         applyServiceCatalogCRDOverride
+    fi
+
+	waitUntilInstallerApiAvailable
+
+	shout "Trigger installation"
+	date
+
+    sed -e "s/__VERSION__/0.0.1/g" "${INSTALLER_CR}"  | sed -e "s/__.*__//g" | kubectl apply -f-
+	"${KYMA_SCRIPTS_DIR}"/is-installed.sh --timeout 30m
+
+	if [ -n "$(kubectl get service -n kyma-system apiserver-proxy-ssl --ignore-not-found)" ]; then
+		shout "Create DNS Record for Apiserver proxy IP"
+		date
+		APISERVER_IP_ADDRESS=$(kubectl get service -n kyma-system apiserver-proxy-ssl -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+		APISERVER_DNS_FULL_NAME="apiserver.${CLUSTER_NAME}.${DNS_DOMAIN}"
+		IP_ADDRESS=${APISERVER_IP_ADDRESS} DNS_FULL_NAME=${APISERVER_DNS_FULL_NAME} "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-dns-record.sh"
+	fi
+}
+
+function addGithubDexConnector() {
+    pushd "${KYMA_PROJECT_DIR}/test-infra/development/tools"
+    dep ensure -v -vendor-only
+    popd
+    export DEX_CALLBACK_URL="https://dex.${DOMAIN}/callback"
+    go run "${KYMA_PROJECT_DIR}/test-infra/development/tools/cmd/enablegithubauth/main.go"
+}
+
+function applyServiceCatalogCRDOverride(){
+    shout "Apply override for ServiceCatalog to enable CRD implementation"
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: service-catalog-overrides
+  namespace: kyma-installer
+  labels:
+    installer: overrides
+    component: service-catalog
+    kyma-project.io/installation: ""
+data:
+  service-catalog-apiserver.enabled: "false"
+  service-catalog-crds.enabled: "true"
+EOF
+}
+
+shout "Authenticate"
+date
+init
+
+DNS_DOMAIN="$(gcloud dns managed-zones describe "${CLOUDSDK_DNS_ZONE_NAME}" --format="value(dnsName)")"
+export DNS_DOMAIN
+DOMAIN="${CLUSTER_NAME}.${DNS_DOMAIN%?}"
+export DOMAIN
+
+shout "Add Github Dex Connector"
+date
+addGithubDexConnector
+
+shout "Cleanup"
+date
+"${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/kk-cleanup-cluster.sh"
+
+shout "Create new cluster"
+date
+createCluster
+
+shout "Install tiller"
+date
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user="$(gcloud config get-value account)"
+"${KYMA_SCRIPTS_DIR}"/install-tiller.sh
+
+shout "Install kyma"
+date
+installKyma
+"${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/get-helm-certs.sh"
+
+shout "Install stability-checker"
+date
+(
+export TEST_INFRA_SOURCES_DIR KYMA_SCRIPTS_DIR TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS \
+        CLUSTER_NAME SLACK_CLIENT_WEBHOOK_URL STABILITY_SLACK_CLIENT_CHANNEL_ID SLACK_CLIENT_TOKEN TEST_RESULT_WINDOW_TIME
+"${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/install-stability-checker.sh"
+)
+
