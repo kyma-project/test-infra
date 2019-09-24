d1526ef45fad438c7e2afabbccf5a1487fd67c57
Szymon Janota <szymon.janota@sap.com>
2019-09-24 09:02:02 +0200

    Fix tests

--- development/tools/jobs/kyma/tests_test.go
+++ development/tools/jobs/kyma/tests_test.go
@@ -1,10 +1,11 @@
 package kyma
 
 import (
+	"testing"
+
 	"github.com/kyma-project/test-infra/development/tools/jobs/releases"
 	"github.com/kyma-project/test-infra/development/tools/jobs/tester"
 	"github.com/kyma-project/test-infra/development/tools/jobs/tester/jobsuite"
-	"testing"
 )
 
 var tests = []struct {
@@ -20,23 +21,22 @@ var tests = []struct {
 	{path: "application-registry-tests", image: tester.ImageGolangBuildpackLatest},
 	{path: "asset-store", image: tester.ImageGolangBuildpack1_11},
 	{path: "compass-runtime-agent", image: tester.ImageGolangBuildpack1_11,
-		additionalOptions:[]jobsuite.Option{
+		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("tests"),
 		},
 	},
 	{path: "connection-token-handler-tests", image: tester.ImageGolangBuildpackLatest},
 	{path: "connector-service-tests", image: tester.ImageGolangBuildpackLatest},
 	{path: "console-backend-service", image: tester.ImageGolangBuildpack1_11,
-		additionalOptions:[]jobsuite.Option{
+		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("tests"),
+			jobsuite.Until(releases.Release15),
 		},
 	},
 	{path: "console-backend-service", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("tests-generic"),
 			jobsuite.Since(releases.Release16),
-			jobsuite.DockerRepositoryPresetSuffix("test-infra"),
-			jobsuite.Optional(),
 		},
 	},
 	{path: "end-to-end/backup-restore-test", image: tester.ImageGolangBuildpack1_11},
@@ -46,24 +46,24 @@ var tests = []struct {
 		jobsuite.RunIfChanged("^tests/end-to-end/upgrade/[^chart]", "tests/end-to-end/upgrade/fix"),
 	}},
 	{path: "event-bus", image: tester.ImageGolangBuildpack1_11,
-		additionalOptions:[]jobsuite.Option{
+		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("tests"),
 		},
 	},
 	{path: "integration/api-controller", image: tester.ImageGolangBuildpack1_12,
-		additionalOptions:[]jobsuite.Option{
+		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("tests"),
 		},
 	},
 	{path: "integration/apiserver-proxy", image: tester.ImageGolangBuildpack1_12,
-		additionalOptions:[]jobsuite.Option{
+		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("tests"),
 		},
 	},
 	{path: "integration/cluster-users", image: tester.ImageBootstrapLatest},
 	{path: "integration/dex", image: tester.ImageGolangBuildpack1_12},
 	{path: "integration/event-service", image: tester.ImageGolangBuildpack1_11,
-		additionalOptions:[]jobsuite.Option{
+		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("tests"),
 		},
 	},
