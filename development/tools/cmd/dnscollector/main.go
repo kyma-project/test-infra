877b78d78e15f6cd89b072bcb47b0585c49d5a85
Piotr Jasiak <piotr.jasiak@sap.com>
2019-09-27 12:34:27 +0200

    remove kyma-gke-service-catalog-crd-periodic job (#1520)

--- development/tools/cmd/dnscollector/main.go
+++ development/tools/cmd/dnscollector/main.go
@@ -16,7 +16,7 @@ import (
 	dns "google.golang.org/api/dns/v1"
 )
 
-const defaultAddressRegexpList = "(remoteenvs-)?gkeint-(pr|commit)-.*,(remoteenvs-)?gke-upgrade-(pr|commit)-.*,(remoteenvs-)?(load-test|e2etest),gke-central-(pr|commit)-.*,gke-backup-(pr|commit)-.*,service-catalog-crd-periodic,gkecompint-(pr|commit)-.*,gke-minio-(pr|commit)-.*,gke-minio-min-(pr|commit).*"
+const defaultAddressRegexpList = "(remoteenvs-)?gkeint-(pr|commit)-.*,(remoteenvs-)?gke-upgrade-(pr|commit)-.*,(remoteenvs-)?(load-test|e2etest),gke-central-(pr|commit)-.*,gke-backup-(pr|commit)-.*,gkecompint-(pr|commit)-.*,gke-minio-(pr|commit)-.*,gke-minio-min-(pr|commit).*"
 const minAgeInHours = 1
 const minPatternLength = 5
 
