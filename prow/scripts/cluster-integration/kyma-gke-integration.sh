ce8671f23af82743b75fe0f681bf503df36440a2
Michał Hudy <michal@hudy.pl>
2019-09-30 11:05:47 +0200

    asdfas

--- prow/scripts/cluster-integration/kyma-gke-integration.sh
+++ prow/scripts/cluster-integration/kyma-gke-integration.sh
@@ -43,6 +43,8 @@ if [ "${discoverUnsetVar}" = true ] ; then
 fi
 
 kubectl version --client
+./usr/local/bin/kubectl version --client
+
 #Exported variables
 export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
 export KYMA_SOURCES_DIR="${KYMA_PROJECT_DIR}/kyma"
