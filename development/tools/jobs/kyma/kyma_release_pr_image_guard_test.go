db8dfc4baec2dc66bd0579952b5875e6d59b9067
Michał Hudy <michal@hudy.pl>
2019-09-30 09:50:23 +0200

    Revert "Bump integration jobs to Kubernetes 1.14 (#1490)"
    This reverts commit f2837f733d44d607eda663bc02c9f4fcb606c577.

--- development/tools/jobs/kyma/kyma_release_pr_image_guard_test.go
+++ development/tools/jobs/kyma/kyma_release_pr_image_guard_test.go
@@ -1,7 +1,6 @@
 package kyma
 
 import (
-	"github.com/kyma-project/test-infra/development/tools/jobs/tester/preset"
 	"testing"
 
 	"github.com/kyma-project/test-infra/development/tools/jobs/tester"
@@ -14,14 +13,14 @@ func TestKymaReleasePrImageGuard(t *testing.T) {
 	jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/kyma/kyma-release-pr-image-guard.yaml")
 	// THEN
 	require.NoError(t, err)
-	actualPresubmit := tester.FindPresubmitJobByNameAndBranch(jobConfig.Presubmits["kyma-project/kyma"], "pre-release-pr-image-guard", "release-*")
+	actualPresubmit := tester.FindPresubmitJobByName(jobConfig.Presubmits["kyma-project/kyma"], "pre-release-pr-image-guard", "release-*")
 	require.NotNil(t, actualPresubmit)
 	assert.Equal(t, 10, actualPresubmit.MaxConcurrency)
 	assert.False(t, actualPresubmit.SkipReport)
 	assert.True(t, actualPresubmit.Decorate)
 	assert.Equal(t, "github.com/kyma-project/kyma", actualPresubmit.PathAlias)
 	tester.AssertThatHasExtraRefTestInfra(t, actualPresubmit.JobBase.UtilityConfig, "master")
-	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, preset.DindEnabled)
+	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, tester.PresetDindEnabled)
 	assert.Equal(t, tester.ImageBootstrap20181204, actualPresubmit.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/pr-image-guard.sh"}, actualPresubmit.Spec.Containers[0].Command)
 }
