afa00895b7374b00803e20f718500e490099b5d4
Michał Hudy <michal@hudy.pl>
2019-10-02 18:56:16 +0200

    Add kind to toolbox image and high load node pool to workload cluster (#1534)

--- development/provision-cluster.sh
+++ development/provision-cluster.sh
@@ -27,5 +27,18 @@ if [ -z "$WORKLOAD_CLUSTER_NAMER" ]; then
     echo "Provisioning workload cluster '${WORKLOAD_CLUSTER_NAME}' (${NUM_NODES} nodes) in project '${PROJECT}' and zone '${ZONE}'"
     gcloud container --project "${PROJECT}" clusters create "${WORKLOAD_CLUSTER_NAME}" \
         --zone "${ZONE}" --issue-client-certificate --enable-basic-auth --machine-type n1-standard-1 --num-nodes "${NUM_NODES}"
+
+    echo "Adding high load node pool to cluster ${WORKLOAD_CLUSTER_NAME}' in project '${PROJECT}' and zone '${ZONE}'"
+    gcloud container node-pools create high-load \
+        --project "${PROJECT}" \
+        --cluster "${WORKLOAD_CLUSTER_NAME}" \
+        --zone "${ZONE}" \
+        --machine-type n1-standard-16 \
+        --node-taints resources-usage=high:NoSchedule \
+        --num-nodes 0 \
+        --enable-autoscaling \
+        --max-nodes 5 \
+        --min-nodes 0 \
+        --disk-type pd-ssd
 fi
 
