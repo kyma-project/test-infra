ab20ff617019824c76962b6fd5a14896ad0c747b
Magda Strek <magdalena.strek@sap.com>
2019-10-03 11:12:21 +0200

    kyma-operator generic job

--- development/tools/jobs/kyma/components_test.go
+++ development/tools/jobs/kyma/components_test.go
@@ -89,6 +89,13 @@ var components = []struct {
 	{path: "function-controller", image: tester.ImageGolangKubebuilderBuildpackLatest},
 	{path: "kubeless-images/nodejs", image: tester.ImageGolangBuildpack1_11},
 	{path: "kyma-operator", image: tester.ImageGolangBuildpack1_12},
+	{path: "kyma-operator", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.JobFileSuffix("generic"),
+			jobsuite.Since(releases.Release17),
+			jobsuite.Optional(),
+		},
+	},
 	{path: "namespace-controller", image: tester.ImageGolangBuildpackLatest},
 	{path: "service-binding-usage-controller", image: tester.ImageGolangBuildpack1_11},
 	{path: "xip-patch", image: tester.ImageBootstrapLatest},
