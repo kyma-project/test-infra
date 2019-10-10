761ffd4b1ceb67c204f092e86baf7a68e653176c
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2019-10-10 11:11:49 +0200

    Dex static user configurer generic job switch (#1590)
    * Switch to generic job

--- development/tools/jobs/kyma/components_test.go
+++ development/tools/jobs/kyma/components_test.go
@@ -93,12 +93,15 @@ var components = []struct {
 			jobsuite.RunIfChanged("components/console-backend-service/main.go", "scripts/go-dep.mk"),
 		},
 	},
-	{path: "dex-static-user-configurer", image: tester.ImageBootstrapLatest},
+	{path: "dex-static-user-configurer", image: tester.ImageBootstrapLatest,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.Until(releases.Release15),
+		},
+	},
 	{path: "dex-static-user-configurer", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("generic"),
 			jobsuite.Since(releases.Release17),
-			jobsuite.Optional(),
 		},
 	},
 	{path: "etcd-tls-setup-job", image: tester.ImageGolangBuildpack1_11},
