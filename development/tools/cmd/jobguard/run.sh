51cae757944725bc17229a4428b3a61c3012e814
Miskiewicz, Piotr <piotr.miskiewicz@sap.com>
2019-10-10 15:44:52 +0200

    Job guard waits for generic prow jobs

--- development/tools/cmd/jobguard/run.sh
+++ development/tools/cmd/jobguard/run.sh
@@ -3,7 +3,7 @@
 ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 KYMA_PROJECT_DIR=${KYMA_PROJECT_DIR:-"/home/prow/go/src/github.com/kyma-project"}
-JOB_NAME_PATTERN=${JOB_NAME_PATTERN:-"(pre-master-kyma-components-.*)|(pre-master-kyma-tests-.*)"}
+JOB_NAME_PATTERN=${JOB_NAME_PATTERN:-"(pre-master-kyma-components-.*)|(pre-master-kyma-tests-.*)|(pre-kyma-components-.*)|(pre-kyma-tests-.*)"}
 echo "KYMA PROJECT DIR ${KYMA_PROJECT_DIR}"
 echo "COMMIT SHA: ${PULL_PULL_SHA}"
 export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
