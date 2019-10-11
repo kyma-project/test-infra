011786db0a6831ef360348d5d429d62b23baa88b
Jakub Kabza <jakub.kabza@sap.com>
2019-10-11 09:36:20 +0200

    Apiserver proxy tests generic job (#1601)

--- development/tools/jobs/kyma/tests_test.go
+++ development/tools/jobs/kyma/tests_test.go
@@ -68,6 +68,13 @@ var tests = []struct {
 			jobsuite.JobFileSuffix("tests"),
 		},
 	},
+	{path: "integration/apiserver-proxy", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.JobFileSuffix("tests-generic"),
+			jobsuite.Since(releases.Release17),
+			jobsuite.Optional(),
+		},
+	},
 	{path: "integration/cluster-users", image: tester.ImageBootstrapLatest},
 	{path: "integration/dex", image: tester.ImageGolangBuildpack1_12},
 	{path: "integration/dex", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
