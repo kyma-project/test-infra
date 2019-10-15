f598a6396101e7986b99482a4eba79dddbd75a2a
Jakub Kabza <jakub.kabza@sap.com>
2019-10-15 13:44:58 +0200

    apiserver-proxy integration tests: switch to generic (#1616)

--- development/tools/jobs/kyma/tests_test.go
+++ development/tools/jobs/kyma/tests_test.go
@@ -73,13 +73,13 @@ var tests = []struct {
 	{path: "integration/apiserver-proxy", image: tester.ImageGolangBuildpack1_12,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("tests"),
+			jobsuite.Until(releases.Release15),
 		},
 	},
 	{path: "integration/apiserver-proxy", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("tests-generic"),
 			jobsuite.Since(releases.Release17),
-			jobsuite.Optional(),
 		},
 	},
 	{path: "integration/cluster-users", image: tester.ImageBootstrapLatest},
