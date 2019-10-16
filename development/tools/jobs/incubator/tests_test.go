d86c91883d2920357d1fcdfe232193d138dfbc6b
Damian Badura <damian.badura@sap.com>
2019-10-16 16:42:41 +0200

    Add tests for components tests job

---
+++ development/tools/jobs/incubator/tests_test.go
@@ -0,0 +1,60 @@
+package incubator
+
+import (
+	"testing"
+
+	"github.com/kyma-project/test-infra/development/tools/jobs/releases"
+	"github.com/kyma-project/test-infra/development/tools/jobs/tester"
+	"github.com/kyma-project/test-infra/development/tools/jobs/tester/jobsuite"
+)
+
+var tests = []struct {
+	name              string
+	image             string
+	suite             func(config *jobsuite.Config) jobsuite.Suite
+	additionalOptions []jobsuite.Option
+}{
+	{name: "connector-tests",
+		image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.JobFileSuffix("tests-generic"),
+			jobsuite.CompassRepo(),
+			jobsuite.Since(releases.Release17),
+		},
+	},
+	{name: "end-to-end",
+		image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.JobFileSuffix("tests-generic"),
+			jobsuite.CompassRepo(),
+			jobsuite.Since(releases.Release17),
+		},
+	},
+	{name: "provisioner-tests",
+		image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.JobFileSuffix("tests-generic"),
+			jobsuite.CompassRepo(),
+			jobsuite.Since(releases.Release17),
+		},
+	},
+}
+
+func TestTestJobs(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			opts := []jobsuite.Option{
+				jobsuite.CompassTest(test.name, test.image),
+				jobsuite.KymaRepo(),
+				jobsuite.AllReleases(),
+			}
+			opts = append(opts, test.additionalOptions...)
+			cfg := jobsuite.NewConfig(opts...)
+			suite := test.suite
+			if suite == nil {
+				suite = tester.NewComponentSuite
+			}
+			suite(cfg).Run(t)
+		})
+	}
+}
