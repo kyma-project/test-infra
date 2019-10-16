713b79825ccaf896b3726210f93b11fc66c40095
Jakub Kabza <jakub.kabza@sap.com>
2019-10-16 09:09:08 +0200

    cluster-users test: add generic job

--- development/tools/jobs/kyma/tests_test.go
+++ development/tools/jobs/kyma/tests_test.go
@@ -83,6 +83,13 @@ var tests = []struct {
 		},
 	},
 	{path: "integration/cluster-users", image: tester.ImageBootstrapLatest},
+	{path: "integration/cluster-users", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.JobFileSuffix("tests-generic"),
+			jobsuite.Since(releases.Release17),
+			jobsuite.Optional(),
+		},
+	},
 	{path: "integration/dex", image: tester.ImageGolangBuildpack1_12},
 	{path: "integration/dex", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
