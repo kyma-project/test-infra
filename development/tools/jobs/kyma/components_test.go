d952ffa7dae582f787c81a9900f7cc848e6979d6
Rafal Potempa <rafal.potem@gmail.com>
2019-10-27 13:46:22 +0100

    Migrate application-registry

--- development/tools/jobs/kyma/components_test.go
+++ development/tools/jobs/kyma/components_test.go
@@ -66,12 +66,10 @@ var components = []struct {
 			jobsuite.JobFileSuffix("generic"),
 		},
 	},
-	{path: "application-registry", image: tester.ImageGolangBuildpackLatest},
 	{path: "application-registry", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.Since(releases.Release17),
 			jobsuite.JobFileSuffix("generic"),
-			jobsuite.Optional(),
 		},
 	},
 	{path: "asset-metadata-service", image: tester.ImageGolangBuildpack1_11},
