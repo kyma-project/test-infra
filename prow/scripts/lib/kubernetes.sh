b928f25d55c8758a7865cc5af1a32f471a77b65a
Michał Hudy <michal@hudy.pl>
2019-10-28 13:22:34 +0100

    Kubectl version

--- prow/scripts/lib/kubernetes.sh
+++ prow/scripts/lib/kubernetes.sh
@@ -2,7 +2,15 @@
 
 function kubernetes::ensure_kubectl {
     # TODO: (@michal-hudy) Install proper kubectl version for current system
+    if command -v kubectl >/dev/null 2>&1; then
+        log "Removing built-in kubectl version"
+        rm -f "$(command -v kubectl)"
+    fi
+
     echo "Install kubectl in version ${1}"
     curl -LO "https://storage.googleapis.com/kubernetes-release/release/${1}/bin/linux/amd64/kubectl" --fail \
-        && chmod +x kubectl
+        && chmod +x kubectl \
+        && mv kubectl /usr/local/bin/kubectl
+
+    kubectl version --client
 }
