3a2110042b7a7b32212837aeba25d8f992d9bf5c
michal.kempski@sap.com <michal.kempski@sap.com>
2019-12-02 10:18:33 +0100

    Create job for health-check-proxy tool

--- development/tools/jobs/kyma/tools_test.go
+++ development/tools/jobs/kyma/tools_test.go
@@ -20,6 +20,12 @@ var tools = []struct {
 			jobsuite.Since(releases.Release17),
 		},
 	},
+	{path: "health-check-proxy", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.Since(releases.Release19),
+			jobsuite.Optional(),
+		},
+	},
 }
 
 func TestToolsJobs(t *testing.T) {
