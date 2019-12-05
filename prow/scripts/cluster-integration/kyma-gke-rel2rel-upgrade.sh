feb15cd11f995f194ed09e482f31d3f1dd27b1dd
Mateusz Szostok <szostok.mateusz@gmail.com>
2019-12-05 13:01:59 +0100

    Add opt to inject addons only if necessary for upgrade plans (#1809)

--- prow/scripts/cluster-integration/kyma-gke-rel2rel-upgrade.sh
+++ prow/scripts/cluster-integration/kyma-gke-rel2rel-upgrade.sh
@@ -344,8 +344,6 @@ createTestResources() {
     shout "Create e2e upgrade test resources"
     date
 
-    injectTestingAddons
-
     if [  -f "$(helm home)/ca.pem" ]; then
         local HELM_ARGS="--tls"
     fi
@@ -405,9 +403,29 @@ upgradeKyma() {
     fi
 }
 
+inject_addons_if_necessary() {
+  tdWithAddon=$(kubectl get td --all-namespaces -l testing.kyma-project.io/require-testing-addon=true -o custom-columns=NAME:.metadata.name --no-headers=true)
+
+  if [ -z "$tdWithAddon" ]
+  then
+      log::info "- Skipping injecting ClusterAddonsConfiguration"
+  else
+      log::info "- Creating ClusterAddonsConfiguration which provides the testing addons"
+      injectTestingAddons
+      if [[ $? -eq 1 ]]; then
+        exit 1
+      fi
+
+      trap removeTestingAddons EXIT
+  fi
+}
+
+
 testKyma() {
     shout "Test Kyma end-to-end upgrade scenarios"
     date
+
+    inject_addons_if_necessary
 
     if [  -f "$(helm home)/ca.pem" ]; then
         local HELM_ARGS="--tls"
