8d22e27b2e15c5a0b8a07e04e9b7acf107f1f08e
Borja Clemente <borja.clemente.castanera@sap.com>
2019-12-09 16:20:16 +0100

    Reenable install and test and remove debug prints

--- prow/scripts/cluster-integration/helpers/deprovision-gardener-cluster.sh
+++ prow/scripts/cluster-integration/helpers/deprovision-gardener-cluster.sh
@@ -14,7 +14,5 @@
 
 readonly NAMESPACE="garden-${GARDENER_PROJECT_NAME}"
 
-RES=$(kubectl --kubeconfig ${GARDENER_CREDENTIALS} -n ${NAMESPACE} annotate shoot "${GARDENER_CLUSTER_NAME}" confirmation.garden.sapcloud.io/deletion=true --overwrite)
-echo "Annotate shoot for deletion: ${RES}"
-RES=$(kubectl --kubeconfig ${GARDENER_CREDENTIALS} -n ${NAMESPACE} delete   shoot "${GARDENER_CLUSTER_NAME}")
-echo "Delete shoot CRD: ${RES}"
+kubectl --kubeconfig ${GARDENER_CREDENTIALS} -n ${NAMESPACE} annotate shoot "${GARDENER_CLUSTER_NAME}" confirmation.garden.sapcloud.io/deletion=true --overwrite
+kubectl --kubeconfig ${GARDENER_CREDENTIALS} -n ${NAMESPACE} delete   shoot "${GARDENER_CLUSTER_NAME}"
