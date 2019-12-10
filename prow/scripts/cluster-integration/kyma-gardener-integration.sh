aa9169125407b22d435a0d163f3ac34b59bb27e3
Borja Clemente <borja.clemente.castanera@sap.com>
2019-12-10 09:42:07 +0100

    Remove install and test phases to test cleanup

--- prow/scripts/cluster-integration/kyma-gardener-integration.sh
+++ prow/scripts/cluster-integration/kyma-gardener-integration.sh
@@ -129,14 +129,14 @@ kyma provision gardener \
 
 
 
-shout "Installing Kyma"
-date
-yes | kyma install --non-interactive --source latest --timeout=2h #--domain "${DOMAIN}" --tlsCert "${TLS_CERT}" --tlsKey "${TLS_KEY}"
+# shout "Installing Kyma"
+# date
+# yes | kyma install --non-interactive --source latest --timeout=2h #--domain "${DOMAIN}" --tlsCert "${TLS_CERT}" --tlsKey "${TLS_KEY}"
 
 
-shout "Checking the versions"
-date
-kyma version
+# shout "Checking the versions"
+# date
+# kyma version
 
 
 # if [ -n "$(kubectl get service -n istio-system istio-ingressgateway --ignore-not-found)" ]; then
@@ -158,48 +158,48 @@ kyma version
 # fi
 
 
-shout "Running Kyma tests"
-date
-echo "- Creating ClusterAddonsConfiguration which provides the testing addons"
-injectTestingAddons
-if [[ $? -eq 1 ]]; then
-    exit 1
-fi
+# shout "Running Kyma tests"
+# date
+# echo "- Creating ClusterAddonsConfiguration which provides the testing addons"
+# injectTestingAddons
+# if [[ $? -eq 1 ]]; then
+#     exit 1
+# fi
 
-readonly CONCURRENCY=5
-kyma test run \
-                --name "${SUITE_NAME}" \
-                --concurrency "${CONCURRENCY}" \
-                --max-retries 1 \
-                --timeout "1h" \
-                --watch \
-                --non-interactive
+# readonly CONCURRENCY=5
+# kyma test run \
+#                 --name "${SUITE_NAME}" \
+#                 --concurrency "${CONCURRENCY}" \
+#                 --max-retries 1 \
+#                 --timeout "1h" \
+#                 --watch \
+#                 --non-interactive
 
 
-echo "Test Summary"
-kyma test status "${SUITE_NAME}" -owide
+# echo "Test Summary"
+# kyma test status "${SUITE_NAME}" -owide
 
-statusSucceeded=$(kubectl get cts "${SUITE_NAME}"  -ojsonpath="{.status.conditions[?(@.type=='Succeeded')]}")
-if [[ "${statusSucceeded}" != *"True"* ]]; then
-    echo "- Fetching logs due to test suite failure"
+# statusSucceeded=$(kubectl get cts "${SUITE_NAME}"  -ojsonpath="{.status.conditions[?(@.type=='Succeeded')]}")
+# if [[ "${statusSucceeded}" != *"True"* ]]; then
+#     echo "- Fetching logs due to test suite failure"
 
-    echo "- Fetching logs from testing pods in Failed status..."
-    kyma test logs "${SUITE_NAME}" --test-status Failed
+#     echo "- Fetching logs from testing pods in Failed status..."
+#     kyma test logs "${SUITE_NAME}" --test-status Failed
 
-    echo "- Fetching logs from testing pods in Unknown status..."
-    kyma test logs "${SUITE_NAME}" --test-status Unknown
+#     echo "- Fetching logs from testing pods in Unknown status..."
+#     kyma test logs "${SUITE_NAME}" --test-status Unknown
 
-    echo "- Fetching logs from testing pods in Running status due to running afer test suite timeout..."
-    kyma test logs "${SUITE_NAME}" --test-status Running
+#     echo "- Fetching logs from testing pods in Running status due to running afer test suite timeout..."
+#     kyma test logs "${SUITE_NAME}" --test-status Running
 
-    echo "ClusterTestSuite details"
-    kubectl get cts "${SUITE_NAME}" -oyaml
+#     echo "ClusterTestSuite details"
+#     kubectl get cts "${SUITE_NAME}" -oyaml
 
-    exit 1
-fi
+#     exit 1
+# fi
 
-echo "ClusterTestSuite details"
-kubectl get cts "${SUITE_NAME}" -oyaml
+# echo "ClusterTestSuite details"
+# kubectl get cts "${SUITE_NAME}" -oyaml
 
 shout "Success"
 
