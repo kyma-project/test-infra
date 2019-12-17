3e00d4d7f287d5cf4a9a6ac771729fe8f0b3d58d
Michał Hudy <michal@hudy.pl>
2019-12-17 08:03:50 +0100

    Linter

--- prow/scripts/lib/helm.sh
+++ prow/scripts/lib/helm.sh
@@ -8,7 +8,7 @@
 #   $3 - Destination directory
 function helm::ensure_client {
     echo "Install Helm client in version ${1}"
-    wget -q https://storage.googleapis.com/kubernetes-helm/helm-${1}-${2}-amd64.tar.gz -O - \
+    wget -q "https://storage.googleapis.com/kubernetes-helm/helm-${1}-${2}-amd64.tar.gz" -O - \
         | tar -xzO "${2}-amd64/helm" > "${3}/helm" \
         && chmod +x "${3}/helm"
 
