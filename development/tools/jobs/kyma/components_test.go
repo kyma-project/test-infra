b62a0c9c1a4b7387a143ce6e24484b154353754b
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2020-01-14 12:55:33 +0100

    Fix relase version

--- development/tools/jobs/kyma/components_test.go
+++ development/tools/jobs/kyma/components_test.go
@@ -129,7 +129,7 @@ var components = []struct {
 	{path: "istio-installer", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("generic"),
-			jobsuite.Since(releases.Release19),
+			jobsuite.Since(releases.Release110),
 		},
 	},
 	{path: "k8s-dashboard-proxy", image: tester.ImageGolangBuildpack1_11},
