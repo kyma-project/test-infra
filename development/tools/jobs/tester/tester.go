3dc469ab507174e4cdf3dc04b970772025ca8ea9
kasiakepka <kasia.kepka23@gmail.com>
2020-01-27 10:20:37 +0100

    Preset for k8s 1.14 (#1956)
    * K8s 1.14 template

--- development/tools/jobs/tester/tester.go
+++ development/tools/jobs/tester/tester.go
@@ -48,7 +48,7 @@ const (
 	// ImageBootstrap001 represents version 0.0.1 of bootstrap image
 	ImageBootstrap001 = "eu.gcr.io/kyma-project/prow/bootstrap:0.0.1"
 	// ImageKymaClusterInfraLatest represents boostrap image published on 20.11.2019
-	ImageKymaClusterInfraLatest = "eu.gcr.io/kyma-project/test-infra/kyma-cluster-infra:v20191120-66b27aac"
+	ImageKymaClusterInfraLatest = "eu.gcr.io/kyma-project/test-infra/kyma-cluster-infra:v20200124-8f253e51"
 	// ImageKymaClusterInfra20190528 represents boostrap image published on 28.05.2019
 	ImageKymaClusterInfra20190528 = "eu.gcr.io/kyma-project/test-infra/kyma-cluster-infra:v20190528-8897828"
 	// ImageBootstrapHelm20181121 represents verion of bootstrap-helm image
