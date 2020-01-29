5c70a1f58e73fa5b52968b0f9a1e7899ba6e61f3
Marco Bebway <marco.bebway@sap.com>
2020-01-29 10:40:49 +0100

    Add prow job for the Knative Kafka channel contract tests (#1962)

--- development/tools/jobs/kyma/tests_test.go
+++ development/tools/jobs/kyma/tests_test.go
@@ -176,6 +176,13 @@ var tests = []struct {
 			jobsuite.Since(releases.Release19),
 		},
 	},
+	{path: "contract/knative-channel-kafka", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.JobFileSuffix("tests-generic"),
+			jobsuite.Since(releases.Release110),
+			jobsuite.Optional(),
+		},
+	},
 	{path: "end-to-end/backup", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("generic"),
