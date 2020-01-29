8754e101c4c237e5776f5f306f23132c1216cf9e
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2020-01-29 08:46:59 +0100

    add generic job definition for core external solution test

--- development/tools/jobs/kyma/tests_test.go
+++ development/tools/jobs/kyma/tests_test.go
@@ -97,6 +97,13 @@ var tests = []struct {
 			jobsuite.Until(releases.Release18),
 		},
 	},
+	{path: "end-to-end/external-solution-integration", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.JobFileSuffix("tests-generic"),
+			jobsuite.Since(releases.Release110),
+			jobsuite.Optional(),
+		},
+	},
 	{path: "end-to-end/kubeless-integration", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("tests-generic"),
