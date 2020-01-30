166a299eb1c87e0b24da493d8a3d55ed3fb1efc4
kasiakepka <kasia.kepka23@gmail.com>
2020-01-30 10:13:42 +0100

    regex change

--- development/tools/cmd/dnscollector/main.go
+++ development/tools/cmd/dnscollector/main.go
@@ -16,7 +16,7 @@ import (
 	dns "google.golang.org/api/dns/v1"
 )
 
-const defaultAddressRegexpList = "(remoteenvs-)?gkeint-(pr|commit|rel)-.*,(remoteenvs-)?gke-upgrade-(pr|commit|rel)-.*,(remoteenvs-)?(e2etest),gke-central-(pr|commit|rel)-.*,gke-backup-(pr|commit|rel)-.*,gkecompint-(pr|commit)-.*,gke-minio-(pr|commit)-.*,gke-minio-min-(pr|commit).*,service-catalog.*,apiserver.gke.*"
+const defaultAddressRegexpList = "(remoteenvs-)?gkeint-(pr|commit|rel)-.*,(remoteenvs-)?gke-upgrade-(pr|commit|rel)-.*,(remoteenvs-)?(e2etest),gke-central-(pr|commit|rel)-.*,gke-backup-(pr|commit|rel)-.*,gkecompint-(pr|commit)-.*,gke-minio-(pr|commit)-.*,gke-minio-min-(pr|commit)-.*,service-catalog.*,apiserver.gke.*"
 const minAgeInHours = 1
 const minPatternLength = 5
 
