a028655030997e4713d07a216b210c19d22907b5
Rafał Potempa <33723064+rafalpotempa@users.noreply.github.com>
2020-02-05 13:02:35 +0100

    Add optional Compass integration job for development of tests for Azure on Gardener (#1986)
    * Add optional prow job for testing Provisioner acceptance tests

--- development/tools/jobs/kyma/kyma_compass_integration_test.go
+++ development/tools/jobs/kyma/kyma_compass_integration_test.go
@@ -46,6 +46,40 @@ func TestKymaGKECompassIntegrationPresubmit(t *testing.T) {
 	assert.Equal(t, "${KYMA_PROJECT_DIR}/test-infra/prow/scripts/cluster-integration/kyma-gke-compass-integration.sh", compassCont.Args[1])
 	tester.AssertThatContainerHasEnv(t, compassCont, "CLOUDSDK_COMPUTE_ZONE", "europe-west4-b")
 	tester.AssertThatSpecifiesResourceRequests(t, actualJob.JobBase)
+
+	// when
+	actualJob = tester.FindPresubmitJobByNameAndBranch(jobConfig.Presubmits["kyma-project/kyma"], "pre-master-kyma-gke-compass-integration-provisioner-test", "master")
+	require.NotNil(t, actualJob)
+
+	// then
+	assert.True(t, actualJob.Optional)
+	assert.True(t, actualJob.Decorate)
+	assert.Equal(t, "resources/compass", actualJob.RunIfChanged)
+	assert.Equal(t, "github.com/kyma-project/kyma", actualJob.PathAlias)
+	assert.Equal(t, 10, actualJob.MaxConcurrency)
+	assert.False(t, actualJob.SkipReport)
+	tester.AssertThatHasPresets(t, actualJob.JobBase,
+		"preset-kyma-guard-bot-github-token",
+		"preset-kyma-keyring",
+		"preset-kyma-encryption-key",
+		"preset-kms-gc-project-env",
+		"preset-sa-gke-kyma-integration",
+		"preset-gc-compute-envs",
+		"preset-gc-project-env",
+		"preset-docker-push-repository-gke-integration",
+		"preset-dind-enabled",
+		"preset-kyma-artifacts-bucket",
+		"preset-build-pr",
+	)
+	require.Len(t, actualJob.Spec.Containers, 1)
+	compassCont = actualJob.Spec.Containers[0]
+	assert.Equal(t, tester.ImageKymaClusterInfraLatest, compassCont.Image)
+	assert.Equal(t, []string{"bash"}, compassCont.Command)
+	require.Len(t, compassCont.Args, 2)
+	assert.Equal(t, "-c", compassCont.Args[0])
+	assert.Equal(t, "${KYMA_PROJECT_DIR}/test-infra/prow/scripts/cluster-integration/kyma-gke-compass-integration.sh", compassCont.Args[1])
+	tester.AssertThatContainerHasEnv(t, compassCont, "CLOUDSDK_COMPUTE_ZONE", "europe-west4-b")
+	tester.AssertThatSpecifiesResourceRequests(t, actualJob.JobBase)
 }
 
 func TestKymaGKECompassIntegrationPostsubmit(t *testing.T) {
