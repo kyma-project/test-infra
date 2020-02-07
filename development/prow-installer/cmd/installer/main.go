4d8df03ff7edd433636ce7e865adbe1641ea24da
Kamil Kasperski <kamil.kasperski@sap.com>
2020-02-07 10:29:04 +0100

    installer create SAccounts impl based on upstream

--- development/prow-installer/cmd/installer/main.go
+++ development/prow-installer/cmd/installer/main.go
@@ -4,68 +4,83 @@ import (
 	"context"
 	"flag"
 	"github.com/kyma-project/test-infra/development/prow-installer/pkg/cluster"
-	"github.com/kyma-project/test-infra/development/prow-installer/pkg/installer"
+	"github.com/kyma-project/test-infra/development/prow-installer/pkg/config"
+	"github.com/kyma-project/test-infra/development/prow-installer/pkg/serviceaccount"
 	"github.com/kyma-project/test-infra/development/prow-installer/pkg/storage"
 	log "github.com/sirupsen/logrus"
 )
 
 var (
-	config          = flag.String("config", "", "Config file path [Required]")
-	credentialsfile = flag.String("credentials-file", "", "Google Application Credentials file path [Required]")
+	configPath      = flag.String("configPath", "", "Config file path [Required]")
+	credentialsFile = flag.String("credentials-file", "", "Google Application Credentials file path [Required]")
 )
 
 func main() {
 	flag.Parse()
 
-	if *config == "" {
-		log.Fatalf("Missing required argument : -config")
+	if *configPath == "" {
+		log.Fatalf("Missing required argument : -configPath")
 	}
 
-	if *credentialsfile == "" {
+	if *credentialsFile == "" {
 		log.Fatalf("Missing required argument : -credentials-file")
 	}
 
-	var Config installer.Config
-	if err := Config.ReadConfig(*config); err != nil {
-		log.Fatalf("Error reading config file %v", err)
+	readConfig, err := config.ReadConfig(*configPath)
+	if err != nil {
+		log.Fatalf("Error reading configPath file %v", err)
 	}
-	//Config.Labels["created-on"] = time.Now()
+	//configPath.Labels["created-on"] = time.Now()
 	ctx := context.Background()
 
-	//TODO: add installer actions
-
 	storageConfig := &storage.Option{
-		ProjectID:      Config.Project,
-		LocationID:     Config.Location,
-		Prefix:         Config.Prefix,
-		ServiceAccount: *credentialsfile,
+		ProjectID:      readConfig.Project,
+		LocationID:     readConfig.Location,
+		Prefix:         readConfig.Prefix,
+		ServiceAccount: *credentialsFile,
 	}
 	clusterConfig := &cluster.Option{
-		ProjectID:      Config.Project,
-		ZoneID:         Config.Zone,
-		ServiceAccount: *credentialsfile,
+		ProjectID:      readConfig.Project,
+		ZoneID:         readConfig.Zone,
+		ServiceAccount: *credentialsFile,
 	}
-	//secretsConfig := &secrets.Option{
-	//	ProjectID:      Config.Project,
-	//	LocationID:     Config.Location,
-	//	KmsRing:        Config.KeyringName,
-	//	KmsKey:         Config.EncryptionKeyName,
-	//	ServiceAccount: *credentialsfile,
-	//}
 
-	clusterClient, err := cluster.NewClient(ctx, *clusterConfig, *credentialsfile)
+	clusterClient, err := cluster.NewClient(ctx, *clusterConfig, *credentialsFile)
 	if err != nil {
 		log.Fatalf("An error occurred during cluster client configuration: %v", err)
 	}
-	if err := clusterClient.Create(ctx, Config.ClusterName, Config.Labels, 3, false); err != nil {
+	if err := clusterClient.Create(ctx, readConfig.ClusterName, readConfig.Labels, 1, false); err != nil {
 		log.Fatalf("Failed to create cluster: %v", err)
 	}
 
-	storageClient, err := storage.NewClient(ctx, *storageConfig, *credentialsfile)
+	storageClient, err := storage.NewClient(ctx, *storageConfig, *credentialsFile)
 	if err != nil {
 		log.Fatalf("An error occurred during storage client configuration: %v", err)
 	}
-	if err := storageClient.CreateBucket(ctx, Config.BucketName); err != nil {
-		log.Fatalf("Failed to create bucket: %s, %s", Config.BucketName, err)
+	if err := storageClient.CreateBucket(ctx, readConfig.BucketName); err != nil {
+		log.Fatalf("Failed to create bucket: %s, %s", readConfig.BucketName, err)
+	}
+
+	iamService, err := serviceaccount.NewService(*credentialsFile)
+	if err != nil {
+		log.Fatalf("Failed to create IAM service %v", err)
+	}
+	//crmService, err := roles.NewService(*credentialsFile)
+	//if err != nil {
+	//	log.Fatalf("Failed to create CRM service %v", err)
+	//}
+
+	iamClient := serviceaccount.NewClient(readConfig.Prefix, &iamService)
+	//crmClient, err := roles.New(crmService)
+
+	for _, serviceAccount := range readConfig.ServiceAccounts {
+		opts := serviceaccount.SAOptions{
+			Name:    serviceAccount.Name,
+			Roles:   serviceAccount.Roles,
+			Project: readConfig.Project,
+		}
+		if _, err := iamClient.CreateSA(opts); err != nil {
+			log.Errorf("Error creating Service Account %v", err)
+		}
 	}
 }
