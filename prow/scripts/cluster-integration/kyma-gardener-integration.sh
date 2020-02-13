9a44606170c28566ecd1d82ad4126761bfd315b5
Rakesh Garimella <Rakesh.garimella@sap.com>
2020-02-13 16:11:36 +0100

    Change vnet cidr to gardener default (#2030)

--- prow/scripts/cluster-integration/kyma-gardener-integration.sh
+++ prow/scripts/cluster-integration/kyma-gardener-integration.sh
@@ -103,7 +103,7 @@ set -x
 kyma provision gardener \
         --target-provider azure --secret "${GARDENER_KYMA_PROW_PROVIDER_SECRET_NAME}" \
         --name "${CLUSTER_NAME}" --project "${GARDENER_KYMA_PROW_PROJECT_NAME}" --credentials "${GARDENER_KYMA_PROW_KUBECONFIG}" \
-        --region "${GARDENER_REGION}" -t "${MACHINE_TYPE}" --disk-size 35 --disk-type=Standard_LRS --extra vnetcidr="10.250.0.0/19" \
+        --region "${GARDENER_REGION}" -t "${MACHINE_TYPE}" --disk-size 35 --disk-type=Standard_LRS --extra vnetcidr="10.250.0.0/16" \
         --nodes 4
 )
 
