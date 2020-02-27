2661afe9e4187ac1a6b64c86783867764e7e4281
Kamil Kasperski <kamil.kasperski@sap.com>
2020-02-27 13:00:07 +0100

    Added Rooted current directory, added check if path does not exist in Do() function

--- development/prow-installer/pkg/kubectl/wrapper.go
+++ development/prow-installer/pkg/kubectl/wrapper.go
@@ -44,6 +44,9 @@ func (kac *KubeApplyCommand) Do() error {
 	if kac.Path == "" {
 		return fmt.Errorf("path cannot be empty")
 	}
+	if _, err := os.Stat(kac.Path); os.IsNotExist(err) {
+		return fmt.Errorf("path does not exist")
+	}
 
 	cmd.Args = append(cmd.Args, kac.Type, kac.Path)
 
@@ -68,17 +71,24 @@ func GenerateKubeconfig(endpoint, cadata, name string) (string, error) {
 		return "", fmt.Errorf("name cannot be empty")
 	}
 
-	path := filepath.FromSlash(".kube/" + name + "_config")
+	cwd, _ := os.Getwd() // it might be TempDir() as well
+	path := filepath.FromSlash(cwd + "/.kube/")
+	file := name + "_config"
+
 	if _, err := os.Stat(path); os.IsNotExist(err) {
-		if err := os.MkdirAll(".kube", 0700); err != nil {
+		log.Debugf("%s does not exist. Creating directory...", path)
+		if err := os.MkdirAll(path, 0700); err != nil {
 			return "", fmt.Errorf("unexpected error during folder creation %w", err)
 		}
 	}
-	f, err := os.Create(path)
+	log.Debugf("Creating path %s", path+file)
+	f, err := os.Create(path + file)
 	if err != nil {
 		return "", fmt.Errorf("error creating kubeconfig file %w", err)
 	}
 	defer f.Close()
+
+	log.Debugf("Generating GCP Kubeconfig file with credentials for host: %s, name: %s", endpoint, name)
 	kubeconfigTemplate := fmt.Sprintf(`apiVersion: v1
 kind: Config
 clusters:
