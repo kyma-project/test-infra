e2adff95959819255f239d7d3a6a8f7dfc3c9015
Suleyman Akbas <suleyman.akbas@sap.com>
2020-03-09 11:32:17 +0100

    Add jobs for Kiali and Node Exporter images (#2121)

--- development/tools/jobs/incubator/third-party-images/fluent-bit/fluent_bit_test.go
+++ development/tools/jobs/incubator/third-party-images/fluent-bit/fluent_bit_test.go
@@ -1,4 +1,4 @@
-package fluentbit_test
+package third_party_images_test
 
 import (
 	"testing"
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const fluentbitJobPath = "./../../../../../prow/jobs/incubator/fluentbit/fluentbit.yaml"
+const fluentbitJobPath = "./../../../../../../prow/jobs/incubator/third-party-images/fluent-bit/fluent-bit.yaml"
 
 func TestFluentbitJobsPresubmit(t *testing.T) {
 	// when
@@ -18,18 +18,18 @@ func TestFluentbitJobsPresubmit(t *testing.T) {
 
 	// then
 	require.NoError(t, err)
-	actualPresubmit := tester.FindPresubmitJobByNameAndBranch(jobConfig.Presubmits["kyma-incubator/fluentbit"], "pre-master-fluentbit", "master")
+	actualPresubmit := tester.FindPresubmitJobByNameAndBranch(jobConfig.Presubmits["kyma-incubator/third-party-images"], "pre-master-tpi-fluent-bit", "master")
 	require.NotNil(t, actualPresubmit)
 
 	assert.Equal(t, 10, actualPresubmit.MaxConcurrency)
 	assert.False(t, actualPresubmit.SkipReport)
 	assert.True(t, actualPresubmit.Decorate)
-	assert.True(t, actualPresubmit.AlwaysRun)
-	assert.Equal(t, "github.com/kyma-incubator/fluentbit", actualPresubmit.PathAlias)
+	assert.Equal(t, "^fluent-bit/", actualPresubmit.RunIfChanged)
+	assert.Equal(t, "github.com/kyma-incubator/third-party-images", actualPresubmit.PathAlias)
 	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, preset.DindEnabled, preset.DockerPushRepoIncubator, preset.GcrPush, preset.BuildPr)
 	assert.Equal(t, tester.ImageBootstrap20181204, actualPresubmit.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build.sh"}, actualPresubmit.Spec.Containers[0].Command)
-	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-incubator/fluentbit"}, actualPresubmit.Spec.Containers[0].Args)
+	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-incubator/third-party-images/fluent-bit"}, actualPresubmit.Spec.Containers[0].Args)
 }
 
 func TestFluentbitJobPostsubmit(t *testing.T) {
@@ -39,14 +39,14 @@ func TestFluentbitJobPostsubmit(t *testing.T) {
 	// then
 	require.NoError(t, err)
 
-	actualPostsubmit := tester.FindPostsubmitJobByNameAndBranch(jobConfig.Postsubmits["kyma-incubator/fluentbit"], "post-master-fluentbit", "master")
+	actualPostsubmit := tester.FindPostsubmitJobByNameAndBranch(jobConfig.Postsubmits["kyma-incubator/third-party-images"], "post-master-tpi-fluent-bit", "master")
 	require.NotNil(t, actualPostsubmit)
 
 	assert.Equal(t, 10, actualPostsubmit.MaxConcurrency)
 	assert.True(t, actualPostsubmit.Decorate)
-	assert.Equal(t, "github.com/kyma-incubator/fluentbit", actualPostsubmit.PathAlias)
+	assert.Equal(t, "github.com/kyma-incubator/third-party-images", actualPostsubmit.PathAlias)
 	tester.AssertThatHasPresets(t, actualPostsubmit.JobBase, preset.DindEnabled, preset.DockerPushRepoIncubator, preset.GcrPush, preset.BuildMaster)
 	assert.Equal(t, tester.ImageBootstrap20181204, actualPostsubmit.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build.sh"}, actualPostsubmit.Spec.Containers[0].Command)
-	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-incubator/fluentbit"}, actualPostsubmit.Spec.Containers[0].Args)
+	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-incubator/third-party-images/fluent-bit"}, actualPostsubmit.Spec.Containers[0].Args)
 }
