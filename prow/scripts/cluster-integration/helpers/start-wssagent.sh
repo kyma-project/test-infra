f1d8fc48c20a9094e1b3957a7d3126aa86256a1a
Katarzyna Kepka <kasia.kepka23@gmail.com>
2020-03-23 15:58:56 +0100

    whitesource improvements

--- prow/scripts/cluster-integration/helpers/start-wssagent.sh
+++ prow/scripts/cluster-integration/helpers/start-wssagent.sh
@@ -14,7 +14,6 @@
 
 set -o errexit
 
-
 if [ -f "../../prow/scripts/library.sh" ]; then
     export TEST_INFRA_SOURCES_DIR="../.."
 
@@ -28,6 +27,10 @@ fi
 
 # shellcheck disable=SC1090
 source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/library.sh"
+
+# whitesource config
+GO_CONFIG_PATH="${TEST_INFRA_SOURCES_DIR}/prow/images/whitesource-scanner/go-wss-unified-agent.config"
+JAVASCRIPT_CONFIG_PATH="${TEST_INFRA_SOURCES_DIR}/prow/images/whitesource-scanner/javascript-wss-unified-agent.config"
 
 # authenticate gcloud client
 init
@@ -43,22 +46,24 @@ USERKEY=$(cat "whitesource-userkey")
 "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/decrypt.sh" "whitesource-apikey" "whitesource-apikey.encrypted"
 APIKEY=$(cat "whitesource-apikey")
 
+
 case "${SCAN_LANGUAGE}" in
     golang)
         echo "SCAN: golang (dep)"
-        sed -i.bak "s|go.dependencyManager=godep|go.dependencyManager=dep|g" /wss/wss-unified-agent.config
+        sed -i.bak "s|go.dependencyManager=|go.dependencyManager=dep|g" $CONFIG_PATH
+        CONFIG_PATH=$GO_CONFIG_PATH
         ;;
 
     golang-mod)
         echo "SCAN: golang-mod"
-        sed -i.bak "s|go.dependencyManager=godep|go.dependencyManager=modules|g" /wss/wss-unified-agent.config
+        sed -i.bak "s|go.dependencyManager=|go.dependencyManager=modules|g" $CONFIG_PATH
+        CONFIG_PATH=$GO_CONFIG_PATH
         ;;
         
     javascript)
-        echo "SCAN: JAVASTRIPT, disable scanning for go dependencies"
-        sed -i.bak "s|go.resolveDependencies=true|# go.resolveDependencies=true|g" /wss/wss-unified-agent.config
-        sed -i.bak "s|go.collectDependenciesAtRuntime=false|# go.collectDependenciesAtRuntime=false|g" /wss/wss-unified-agent.config
-        sed -i.bak "s|go.dependencyManager=godep|# go.dependencyManager=godep|g" /wss/wss-unified-agent.config
+        echo "SCAN: javascript"
+        CONFIG_PATH=$JAVASCRIPT_CONFIG_PATH
+
         ;;
         
     *)
@@ -73,10 +78,10 @@ echo "***********************************"
 KYMA_SRC="${GITHUB_ORG_DIR}/${PROJECTNAME}"
 
 # resolve deps for console repository
-if [ "${PROJECTNAME}" == "console" ]; then
-    cd "$KYMA_SRC"
-    make resolve
-fi    
+#if [ "${PROJECTNAME}" == "console" ]; then
+#    cd "$KYMA_SRC"
+#    make resolve
+#fi    
 
 function scanFolder() { # expects to get the fqdn of folder passed to scan
     if [[ $1 == "" ]]; then
@@ -91,15 +96,11 @@ function scanFolder() { # expects to get the fqdn of folder passed to scan
     cd "${FOLDER}" # change to passed parameter
     PROJNAME=$2
 
-    # adjust global setting ignoreSourceFiles=false
-    sed -i.bak "s|#ignoreSourceFiles=true|ignoreSourceFiles=true|g;" /wss/wss-unified-agent.config
-    sed -i.bak "s|#resolveAllDependencies=false|resolveAllDependencies=false|g;" /wss/wss-unified-agent.config
-
     if [[ $CUSTOM_PROJECTNAME == "" ]]; then 
     # use custom projectname for kyma-mod scans in order not to override kyma (dep) scan results
-        sed -i.bak "s|apiKey=|apiKey=${APIKEY}|g; s|productName=|productName=${PRODUCTNAME}|g; s|userKey=|userKey=${USERKEY}|g; s|projectName=|projectName=${PROJNAME}|g" /wss/wss-unified-agent.config
+        sed -i.bak "s|apiKey=|apiKey=${APIKEY}|g; s|productName=|productName=${PRODUCTNAME}|g; s|userKey=|userKey=${USERKEY}|g; s|projectName=|projectName=${PROJNAME}|g" $CONFIG_PATH
     else
-        sed -i.bak "s|apiKey=|apiKey=${APIKEY}|g; s|productName=|productName=${PRODUCTNAME}|g; s|userKey=|userKey=${USERKEY}|g; s|projectName=|projectName=${CUSTOM_PROJECTNAME}|g" /wss/wss-unified-agent.config
+        sed -i.bak "s|apiKey=|apiKey=${APIKEY}|g; s|productName=|productName=${PRODUCTNAME}|g; s|userKey=|userKey=${USERKEY}|g; s|projectName=|projectName=${CUSTOM_PROJECTNAME}|g" $CONFIG_PATH
     fi
 
     echo "Product name - $PRODUCTNAME"
@@ -113,9 +114,9 @@ function scanFolder() { # expects to get the fqdn of folder passed to scan
         echo "***********************************"
         if [ -z "$JAVA_OPTS" ]; then
             echo "no additional java_opts set"
-            java -jar /wss/wss-unified-agent.jar -c /wss/wss-unified-agent.config
+            java -jar /wss/wss-unified-agent.jar -c $CONFIG_PATH
         else
-            java "${JAVA_OPTS}" -jar /wss/wss-unified-agent.jar -c /wss/wss-unified-agent.config
+            java "${JAVA_OPTS}" -jar /wss/wss-unified-agent.jar -c $CONFIG_PATH
         fi
     else 
         echo "***********************************"
