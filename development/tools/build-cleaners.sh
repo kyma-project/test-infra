b65730f0ba1825ff76f878bc6d80fae238a0f520
Kamil Kasperski <kamil.kasperski@sap.com>
2020-03-31 15:38:49 +0200

    added build for image with tools

---
+++ development/tools/build-cleaners.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eu
+
+# shellcheck disable=SC2046
+readonly TOOLS_DIR="${PWD}/development/tools"
+cd "$TOOLS_DIR"
+
+mkdir -p ./bin
+dep ensure -v -vendor-only
+
+for D in "$TOOLS_DIR"/cmd/*;
+do
+  if [ -d "$D" ];
+  then
+    name=$(basename "${D}")
+    echo "building ${name}..."
+    cd "$TOOLS_DIR"/cmd/"$name"
+    go build -o "$TOOLS_DIR"/bin/"${name}" -ldflags="-s -w" main.go
+    # shellcheck disable=SC2086
+    chmod a+x "$TOOLS_DIR"/bin/"${name}"
+  fi
+done
+upx -q "$TOOLS_DIR"/bin/*
