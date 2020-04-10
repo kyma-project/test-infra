9a7327223247dcfe2adb1dbb4af7b1f1192136af
Kamil Kasperski <kamil.kasperski@sap.com>
2020-04-10 14:24:43 +0200

    fixed typo

--- development/github-stats.sh
+++ development/github-stats.sh
@@ -10,9 +10,9 @@ echo "--------------------------------------------------------------------------
 
 export APP_GITHUB_ACCESS_TOKEN=${BOT_GITHUB_TOKEN}
 
-/prow-tools/cmd/githubstats -o kyma-project -r kyma
-/prow-tools/cmd/githubstats -o kyma-project -r helm-broker
-/prow-tools/cmd/githubstats -o kyma-project -r rafter
-/prow-tools/cmd/githubstats -o kyma-project -r test-infra
+/prow-tools/githubstats -o kyma-project -r kyma
+/prow-tools/githubstats -o kyma-project -r helm-broker
+/prow-tools/githubstats -o kyma-project -r rafter
+/prow-tools/githubstats -o kyma-project -r test-infra
 
-/prow-tools/cmd/githubstats -o kyma-incubator -r compass
+/prow-tools/githubstats -o kyma-incubator -r compass
