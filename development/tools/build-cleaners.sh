03c03a321138d235fe08b786fb48586fcf40fa8c
Kamil Kasperski <kamil.kasperski@sap.com>
2020-04-17 09:48:21 +0200

    quick fixup of build-cleaners.sh (#2250)

--- development/tools/build-cleaners.sh
+++ development/tools/build-cleaners.sh
@@ -15,6 +15,10 @@ do
     name=$(basename "${D}")
     echo "building ${name}..."
     cd "${TOOLS_DIR}/cmd/${name}"
+    if [ -a "nobuild.lock" ]; then
+      echo "No go files. Skipped..."
+      continue
+    fi
     CGO_ENABLED=0 go build -o "${TOOLS_DIR}/bin/${name}" -ldflags="-s -w" main.go
     chmod a+x "${TOOLS_DIR}/bin/${name}"
   fi
