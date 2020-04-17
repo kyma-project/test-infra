68dd6e6b82d3534f87a46c59eaf67bee3b784ae7
Daniel Roth <daniel.roth02@sap.com>
2020-04-17 12:38:47 -0700

    remove -u from linter

--- stability-checker/before-commit.sh
+++ stability-checker/before-commit.sh
@@ -97,7 +97,7 @@ fi
 #  GO LINT
 ##
 echo "? golint"
-go get -u golang.org/x/lint/golint
+go get golang.org/x/lint/golint
 golintResult=$(echo "${goFilesToCheck}" | xargs -L1 golint)
 if [ "${#golintResult}" != 0 ]; then
     echo -e "${RED}✗ golint${NC}\\n${golintResult}"
@@ -108,7 +108,7 @@ fi
 ##
 # GO VET
 ##
-packagesToVet=("./cmd/..." "./internal/..." "./pkg/...")
+packagesToVet=("./cmd/..." "./internal/...")
 
 for vPackage in "${packagesToVet[@]}"; do
 	vetResult=$(go vet "${vPackage}")
