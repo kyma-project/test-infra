f79f3e7e13608d04054c6b6b5e76e7f58b6e7bc5
Kamil Kasperski <kamil.kasperski@sap.com>
2020-04-27 12:17:55 +0200

    updated before-commit.sh for mods

--- stability-checker/before-commit.sh
+++ stability-checker/before-commit.sh
@@ -8,21 +8,21 @@ INVERTED='\033[7m'
 NC='\033[0m' # No Color
 
 echo -e "${INVERTED}"
-echo "USER: " + $USER
-echo "PATH: " + $PATH
-echo "GOPATH:" + $GOPATH
+echo "USER: " + "$USER"
+echo "PATH: " + "$PATH"
+echo "GOPATH:" + "$GOPATH"
 echo -e "${NC}"
 
 
 ##
-# DEP ENSURE
+# GO MOD VERIFY
 ##
-dep ensure -v --vendor-only
+go mod verify
 ensureResult=$?
 if [ ${ensureResult} != 0 ]; then
-	echo -e "${RED}✗ dep ensure -v --vendor-only${NC}\n$ensureResult${NC}"
+	echo -e "${RED}✗ go mod verify${NC}\n$ensureResult${NC}"
 	exit 1
-else echo -e "${GREEN}√ dep ensure -v --vendor-only${NC}"
+else echo -e "${GREEN}√ go mod verify${NC}"
 fi
 
 ##
@@ -36,7 +36,7 @@ if [ "$1" == "$CI_FLAG" ]; then
 fi
 
 for binary in "${binaries[@]}"; do
-	${buildEnv} go build -o ${binary} ./cmd/${binary}
+	${buildEnv} go build -o "${binary}" ./cmd/"${binary}"
 	goBuildResult=$?
 	if [ ${goBuildResult} != 0 ]; then
 		echo -e "${RED}✗ go build ${binary} ${NC}\n $goBuildResult${NC}"
@@ -46,30 +46,18 @@ for binary in "${binaries[@]}"; do
 done
 
 ##
-# DEP STATUS
-##
-echo "? dep status"
-depResult=$(dep status -v)
-if [ $? != 0 ]; then
-	echo -e "${RED}✗ dep status\n$depResult${NC}"
-	exit 1
-else echo -e "${GREEN}√ dep status${NC}"
-fi
-
-##
 #  GO LINT
 ##
-go build -o golint-vendored ./vendor/golang.org/x/lint/golint
+go get golang.org/x/lint/golint
 buildLintResult=$?
 if [ ${buildLintResult} != 0 ]; then
-	echo -e "${RED}✗ go build lint${NC}\n$buildLintResult${NC}"
+	echo -e "${RED}✗ go get golint${NC}\n$buildLintResult${NC}"
 	exit 1
 fi
 
-golintResult=$(echo "${goFilesToCheck}" | xargs -L1 ./golint-vendored)
-rm golint-vendored
+golintResult=$(echo "${goFilesToCheck}" | xargs -L1 golint)
 
-if [ $(echo ${#golintResult}) != 0 ]; then
+if [ "${#golintResult}" != 0 ]; then
 	echo -e "${RED}✗ golint\n$golintResult${NC}"
 	exit 1
 else echo -e "${GREEN}√ golint${NC}"
@@ -78,17 +66,16 @@ fi
 ##
 # GO IMPORTS & FMT
 ##
-go build -o goimports-vendored ./vendor/golang.org/x/tools/cmd/goimports
+go get golang.org/x/tools/cmd/goimports
 buildGoImportResult=$?
 if [ ${buildGoImportResult} != 0 ]; then
 	echo -e "${RED}✗ go build goimports${NC}\n$buildGoImportResult${NC}"
 	exit 1
 fi
 
-goImportsResult=$(echo "${goFilesToCheck}" | xargs -L1 ./goimports-vendored -w -l)
-rm goimports-vendored
+goImportsResult=$(echo "${goFilesToCheck}" | xargs -L1 goimports -w -l)
 
-if [ $(echo ${#goImportsResult}) != 0 ]; then
+if [ "${#goImportsResult}" != 0 ]; then
 	echo -e "${RED}✗ goimports and fmt ${NC}\n$goImportsResult${NC}"
 	exit 1
 else echo -e "${GREEN}√ goimports and fmt ${NC}"
@@ -100,8 +87,8 @@ fi
 packagesToVet=("./cmd/..." "./internal/...")
 
 for vPackage in "${packagesToVet[@]}"; do
-	vetResult=$(go vet ${vPackage})
-	if [ $(echo ${#vetResult}) != 0 ]; then
+	vetResult=$(go vet "${vPackage}")
+	if [ "${#vetResult}" != 0 ]; then
 		echo -e "${RED}✗ go vet ${vPackage} ${NC}\n$vetResult${NC}"
 		exit 1
 	else echo -e "${GREEN}√ go vet ${vPackage} ${NC}"
