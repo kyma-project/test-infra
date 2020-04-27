306b0b01c27c36bb493aadc28a9f5c4b8354cb36
Kwiatosz, Krzysztof <kwiatekus@users.noreply.github.com>
2020-04-27 16:45:27 +0200

    core-ui-test

---
+++ development/tools/jobs/console/console_test.go
@@ -0,0 +1,57 @@
+package console
+
+import (
+	"path"
+	"testing"
+
+	"github.com/kyma-project/test-infra/development/tools/jobs/releases"
+	"github.com/kyma-project/test-infra/development/tools/jobs/tester"
+	"github.com/kyma-project/test-infra/development/tools/jobs/tester/jobsuite"
+)
+
+const (
+	jobBasePath = "./../../../../prow/jobs/"
+)
+
+var components = []struct {
+	path              string
+	image             string
+	suite             func(config *jobsuite.Config) jobsuite.Suite
+	additionalOptions []jobsuite.Option
+}{
+	{path: "core-ui", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.Since(releases.Release111),
+		},
+	},
+}
+
+func TestConsoleJobs(t *testing.T) {
+	testedConfigurations := make(map[string]struct{})
+	repos := map[string]struct{}{}
+	for _, component := range components {
+		t.Run(component.path, func(t *testing.T) {
+			opts := []jobsuite.Option{
+				jobsuite.Project(component.path, component.image),
+				jobsuite.ConsoleRepo(),
+				jobsuite.AllReleases(),
+			}
+
+			opts = append(opts, component.additionalOptions...)
+
+			cfg := jobsuite.NewConfig(opts...)
+			suite := component.suite
+			if suite == nil {
+				suite = tester.NewComponentSuite
+			}
+			ts := suite(cfg)
+
+			if pathProvider, ok := ts.(jobsuite.JobConfigPathProvider); ok {
+				testedConfigurations[path.Clean(pathProvider.JobConfigPath())] = struct{}{}
+			}
+			repos[cfg.Repository] = struct{}{}
+			ts.Run(t)
+		})
+	}
+	// t.Run("All Files covered by test", jobsuite.CheckFilesAreTested(repos, testedConfigurations, jobBasePath, "components"))
+}
