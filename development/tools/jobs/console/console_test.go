a19eed658c3a31fb60c53e922e40be8d1b04736f
Kucharska <agata.kucharska@sap.com>
2020-05-12 11:16:03 +0200

    Create optional job for content

--- development/tools/jobs/console/console_test.go
+++ development/tools/jobs/console/console_test.go
@@ -54,6 +54,13 @@ var components = []struct {
 			jobsuite.Since(releases.Release111),
 		},
 	},
+	{path: "content", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.Since(releases.Release112),
+			jobsuite.JobFileSuffix("tmp"),
+			jobsuite.Optional(),
+		},
+	},
 }
 
 func TestConsoleJobs(t *testing.T) {
