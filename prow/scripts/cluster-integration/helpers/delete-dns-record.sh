25f0a022c282ad837cee06f3c41654750e8968b3
Kamil Kasperski <kamil.kasperski@sap.com>
2020-05-25 12:22:32 +0200

    removed resources-cleanup.sh

--- prow/scripts/cluster-integration/helpers/delete-dns-record.sh
+++ prow/scripts/cluster-integration/helpers/delete-dns-record.sh
@@ -3,10 +3,11 @@
 set -e
 set -o pipefail
 
-readonly DEVELOPMENT_DIR="$( cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../../development" && pwd )"
-
-readonly TOOL_DIR=dnscleaner
-readonly OBJECT_NAME="Long lasting cluster DNS cleaner"
-
-"${DEVELOPMENT_DIR}"/resources-cleanup.sh "${TOOL_DIR}" "${OBJECT_NAME}" "$@"
+if [ -x /prow-tools/dnscleaner ];
+then
+  /prow-tools/dnscleaner "$@"
+else
+  cd "development/tools"
+  go run "cmd/dnscleaner" "$@"
+fi
 echo "DNS Record deleted, but it can be visible for some time due to DNS caches"
