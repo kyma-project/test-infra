fe67c72133a3174ec37e5186bbc9d2fb036cd285
Kamil Kasperski <kamil.kasperski@sap.com>
2020-06-01 10:38:05 +0200

    Increased number of attempts for DNS resolving from 3 to 10

--- prow/scripts/cluster-integration/helpers/delete-dns-record.sh
+++ prow/scripts/cluster-integration/helpers/delete-dns-record.sh
@@ -8,5 +8,5 @@ readonly DEVELOPMENT_DIR="$( cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.
 readonly TOOL_DIR=dnscleaner
 readonly OBJECT_NAME="Long lasting cluster DNS cleaner"
 
-"${DEVELOPMENT_DIR}"/resources-cleanup.sh "${TOOL_DIR}" "${OBJECT_NAME}" "$@"
+"${DEVELOPMENT_DIR}"/resources-cleanup.sh "${TOOL_DIR}" "${OBJECT_NAME}" --attempts 10 "$@"
 echo "DNS Record deleted, but it can be visible for some time due to DNS caches"
