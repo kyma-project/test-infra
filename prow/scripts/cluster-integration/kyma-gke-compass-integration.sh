03022c721058b8498079fd7c295f0a2bf003bed3
Rafal Potempa <rafal.potem@gmail.com>
2020-06-02 10:23:42 +0200

    Export KYMA_SOURCES_DIR

--- prow/scripts/cluster-integration/kyma-gke-compass-integration.sh
+++ prow/scripts/cluster-integration/kyma-gke-compass-integration.sh
@@ -15,6 +15,7 @@ if [ "${discoverUnsetVar}" = true ] ; then
 fi
 
 export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
+export KYMA_SOURCES_DIR="${KYMA_PROJECT_DIR}/kyma"
 export TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS="${TEST_INFRA_SOURCES_DIR}/prow/scripts/cluster-integration/helpers"
 
 # shellcheck disable=SC1090
