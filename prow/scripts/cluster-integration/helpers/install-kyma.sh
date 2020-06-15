f414c4307548ce9399752245d437010b9c25162b
Adam Wałach <adam.walach@sap.com>
2020-06-15 12:05:12 +0200

    Revert new gcr credentials
    Revert 3149f38e5772c5f360f8cccdc70fc410f5319e2a 5de4baa09593994c17a685e1181bffbb04af061b 322af9fdf0ac0f3d678f06a93d5fd17f1543f0cc (#2461)

--- prow/scripts/cluster-integration/helpers/install-kyma.sh
+++ prow/scripts/cluster-integration/helpers/install-kyma.sh
@@ -16,12 +16,11 @@
 
 # shellcheck disable=SC1090
 source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/library.sh"
-export TEST_INFRA_SOURCES_DIR
 function installKyma() {
 
     kymaUnsetVar=false
 
-    for var in DOCKER_PUSH_REPOSITORY KYMA_SOURCES_DIR DOCKER_PUSH_DIRECTORY GOOGLE_APPLICATION_CREDENTIALS STANDARIZED_NAME REPO_OWNER REPO_NAME CURRENT_TIMESTAMP SA_TEST_GCR_PUSH_GOOGLE_APPLICATION_CREDENTIALS; do
+    for var in DOCKER_PUSH_REPOSITORY KYMA_SOURCES_DIR DOCKER_PUSH_DIRECTORY GOOGLE_APPLICATION_CREDENTIALS STANDARIZED_NAME REPO_OWNER REPO_NAME CURRENT_TIMESTAMP; do
         if [ -z "${!var}" ] ; then
             echo "ERROR: $var is not set"
             kymaUnsetVar=true
@@ -104,7 +103,7 @@ function installKyma() {
 function createImage() {
     shout "Kyma Installer Image: ${KYMA_INSTALLER_IMAGE}"
     # shellcheck disable=SC1090
-    source "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-image.sh" "${SA_TEST_GCR_PUSH_GOOGLE_APPLICATION_CREDENTIALS}"
+    source "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-image.sh"
 }
 
 function waitUntilInstallerApiAvailable() {
