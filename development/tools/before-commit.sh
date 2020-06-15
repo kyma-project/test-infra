1d51aaa9b1b9b4a1baf3950b5237e5c822762168
Kamil Kasperski <kamil.kasperski@sap.com>
2020-06-15 15:38:56 +0200

    before-commit.sh rewrite to parse directories as arguments

--- development/tools/before-commit.sh
+++ development/tools/before-commit.sh
@@ -6,6 +6,12 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 INVERTED='\033[7m'
 NC='\033[0m' # No Color
+CI_ENABLED=0
+if [ "$1" == "$CI_FLAG" ]; then
+  CI_ENABLED=1
+  shift
+fi
+DIRS_TO_CHECK=("$@")
 
 echo -e "${INVERTED}"
 echo "USER: ${USER}"
@@ -14,19 +20,17 @@ echo "GOPATH: ${GOPATH}"
 echo -e "${NC}"
 
 function check_result() {
-    local step=$1
-    local result=$2
-    local output=$3
+  local step=$1
+  local result=$2
+  local output=$3
 
-    if [[ ${result} != 0 ]]; then
-        echo -e "${RED}✗ ${step}${NC}\\n${output}"
-        exit 1
-    else
-        echo -e "${GREEN}√ ${step}${NC}"
-    fi
+  if [[ ${result} != 0 ]]; then
+    echo -e "${RED}✗ ${step}${NC}\\n${output}"
+    exit 1
+  else
+    echo -e "${GREEN}√ ${step}${NC}"
+  fi
 }
-
-goFilesToCheck=$(find . -type f -name "*.go" | grep -E -v "/vendor/|/automock/|/testdata/")
 
 ##
 # GO MOD VERIFY
@@ -48,31 +52,27 @@ fi
 # GO TEST
 ##
 echo "? go test"
-if [ "$1" == "$CI_FLAG" ]; then
-  go test -count=1 ./pkg/...
-else
-  go test -count=1 ./...
-fi
+go test -count=1 "${DIRS_TO_CHECK[@]}"
+
 check_result "go test" $?
 
 echo "? go build"
 buildEnv=""
-if [ "$1" == "$CI_FLAG" ]; then
-	# build binary statically
-	buildEnv="env CGO_ENABLED=0"
+if [ $CI_ENABLED -eq 1 ]; then
+  # build binary statically
+  buildEnv="env CGO_ENABLED=0"
 fi
 
-while IFS= read -r -d '' directory
-do
-    cmdName=$(basename "${directory}")
-    if [ -a "${directory}/nobuild.lock" ]; then
-      continue
-    fi
-    ${buildEnv} go build -o "${cmdName}" "${directory}"
-    buildResult=$?
-    rm "${cmdName}"
-    check_result "go build ${directory}" "${buildResult}"
-done <   <(find "./cmd" -mindepth 1 -type d -print0)
+while IFS= read -r -d '' directory; do
+  cmdName=$(basename "${directory}")
+  if [ -a "${directory}/nobuild.lock" ]; then
+    continue
+  fi
+  ${buildEnv} go build -o "${cmdName}" "${directory}"
+  buildResult=$?
+  rm "${cmdName}"
+  check_result "go build ${directory}" "${buildResult}"
+done < <(find "./cmd" -mindepth 1 -type d -print0)
 
 ##
 #  GO LINT
@@ -84,7 +84,7 @@ if [ ${buildLintResult} != 0 ]; then
   exit 1
 fi
 
-golintResult=$(echo "${goFilesToCheck}" | xargs -L1 "${GOPATH}"/bin/golint)
+golintResult=$("${GOPATH}"/bin/golint "${DIRS_TO_CHECK[@]}")
 
 if [ "${#golintResult}" != 0 ]; then
   echo -e "${RED}✗ golint\n$golintResult${NC}"
@@ -102,22 +102,22 @@ if [ ${buildGoImportResult} != 0 ]; then
   exit 1
 fi
 
-goImportsResult=$(echo "${goFilesToCheck}" | xargs -L1 "${GOPATH}"/bin/goimports -w -l)
+dirs=$(go list -f '{{ .Dir }}' "${DIRS_TO_CHECK[@]}" | grep -E -v "/vendor|/automock|/testdata")
+test -z "$(for d in $dirs; do goimports -w -l "$d"/*.go; done)"
+goImportsResult=$?
 
-if [ "${#goImportsResult}" != 0 ]; then
+if [ $goImportsResult != 0 ]; then
   echo -e "${RED}✗ goimports and fmt ${NC}\n$goImportsResult${NC}"
   exit 1
 else
   echo -e "${GREEN}√ goimports and fmt ${NC}"
 fi
+
 ##
 # GO VET
 ##
 echo "? go vet"
-packagesToVet=("./cmd/..." "./jobs/..." "./pkg/...")
-
-for vPackage in "${packagesToVet[@]}"; do
-	vetResult=$(go vet "${vPackage}")
-    check_result "go vet ${vPackage}" "${#vetResult}" "${vetResult}"
+for vPackage in "${DIRS_TO_CHECK[@]}"; do
+  vetResult=$(go vet "${vPackage}")
+  check_result "go vet ${vPackage}" "${#vetResult}" "${vetResult}"
 done
-
