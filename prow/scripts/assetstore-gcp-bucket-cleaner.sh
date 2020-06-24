6e5e5627b862ad057c44b850777952f004d2df94
Kamil Kasperski <kamil.kasperski@sap.com>
2020-06-24 16:11:41 +0200

    Add kyma-prow-workloads-access-storage-logs to the excluded bucket list

--- prow/scripts/assetstore-gcp-bucket-cleaner.sh
+++ prow/scripts/assetstore-gcp-bucket-cleaner.sh
@@ -3,7 +3,7 @@
 set -e
 set -o pipefail
 
-EXCLUDED_BUCKETS='kyma-prowjobs-secrets,kyma-prow-secrets,kyma-prow-logs,kyma-prow-artifacts,kyma-development-artifacts,kyma-backup-restore,eu.artifacts.sap-kyma-prow.appspot.com,kyma-prow-access-storage-logs'
+EXCLUDED_BUCKETS='kyma-prowjobs-secrets,kyma-prow-secrets,kyma-prow-logs,kyma-prow-artifacts,kyma-development-artifacts,kyma-backup-restore,eu.artifacts.sap-kyma-prow.appspot.com,kyma-prow-access-storage-logs,kyma-prow-workloads-access-storage-logs'
 BUCKET_REGEXP_NAME="^.+-([a-z0-9]+$)"
 BUCKET_OBJECT_WORKERS_NUMBER=10
 
