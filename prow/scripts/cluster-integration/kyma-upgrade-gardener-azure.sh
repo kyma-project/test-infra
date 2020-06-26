ed0f025aff38d6cbdd551612bfb8952bbf39d42e
Hisar Balik <hisar.balik@sap.com>
2020-06-26 11:40:00 +0200

    updated

--- prow/scripts/cluster-integration/kyma-upgrade-gardener-azure.sh
+++ prow/scripts/cluster-integration/kyma-upgrade-gardener-azure.sh
@@ -166,8 +166,8 @@ function upgradeKyma() {
     shout "Deleting old tiller installation"
     kubectl delete -f "${KYMA_SOURCES_DIR}/installation/resources/tiller.yaml"
     kubectl delete namespace kyma-installer
-    kubectl delete installations.installer.kyma-project.io kyma-installation
-    kubectl delete crd installations.installer.kyma-project.io
+    kubectl delete installations.installer.kyma-project.io kyma-installation --ignore-not-found
+    kubectl delete crd installations.installer.kyma-project.io --ignore-not-found
     kyma install \
         --ci \
         --source latest-published \
