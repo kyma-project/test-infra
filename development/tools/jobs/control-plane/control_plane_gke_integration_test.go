b4ffaa3dd7cce1d0eb6ed118450b0140e3f228ac
Paweł Kosiec <pawel.kosiec@gmail.com>
2020-07-02 18:24:11 +0200

    Update Provisioner integration tests job trigger (#2553)

--- development/tools/jobs/control-plane/control_plane_gke_integration_test.go
+++ development/tools/jobs/control-plane/control_plane_gke_integration_test.go
@@ -18,7 +18,7 @@ func TestKCPGKEIntegrationPresubmit(t *testing.T) {
 	require.NotNil(t, actualJob)
 
 	// then
-	assert.True(t, actualJob.Optional)
+	assert.False(t, actualJob.Optional)
 	assert.True(t, actualJob.Decorate)
 	assert.Equal(t, "^((resources\\S+|installation\\S+)(\\.[^.][^.][^.]+$|\\.[^.][^dD]$|\\.[^mM][^.]$|\\.[^.]$|/[^.]+$))", actualJob.RunIfChanged)
 	assert.Equal(t, "github.com/kyma-project/control-plane", actualJob.PathAlias)
