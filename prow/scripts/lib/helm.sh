0430ff184046c20fbe9d1e67510d0edaa859aed5
Katarzyna Kepka <kasia.kepka23@gmail.com>
2020-07-03 11:27:48 +0200

    formatting

--- prow/scripts/lib/helm.sh
+++ prow/scripts/lib/helm.sh
@@ -9,8 +9,8 @@
 function helm::ensure_client {
     echo "Install Helm client in version ${1}"
 
-    wget https://get.helm.sh/helm-${1}-${2}-amd64.tar.gz -O - | tar -xzO "${2}-amd64/helm" > "${3}/helm" \
+    wget "https://get.helm.sh/helm-${1}-${2}-amd64.tar.gz" -O - | tar -xzO "${2}-amd64/helm" > "${3}/helm" \
         && chmod +x "${3}/helm"
-        rm -rf helm-${1}-${2}-amd64.tar.gz linux-amd64
+        rm -rf "helm-${1}-${2}-amd64.tar.gz" linux-amd64
 
 }
