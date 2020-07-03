2467a668f4b65d26374e993e05b7dc4d8089f6fb
Katarzyna Kepka <kasia.kepka23@gmail.com>
2020-07-03 11:16:22 +0200

    ms key

--- prow/scripts/kind-upgrade-kyma.sh
+++ prow/scripts/kind-upgrade-kyma.sh
@@ -81,7 +81,7 @@ TUNE_INOTIFY="false"
 START_DOCKER="false"
 
 ENSURE_HELM="false"
-readonly HELM_VERSION="dev-v3"
+readonly HELM_VERSION="v3.2.1"
 
 CLUSTER_NAME="kyma"
 KUBERNETES_VERSION="v1.16.3"
--- prow/scripts/lib/helm.sh
+++ prow/scripts/lib/helm.sh
@@ -8,8 +8,9 @@
 #   $3 - Destination directory
 function helm::ensure_client {
     echo "Install Helm client in version ${1}"
-    wget -q "https://storage.googleapis.com/kubernetes-helm/helm-${1}-${2}-amd64.tar.gz" -O - \
-        | tar -xzO "${2}-amd64/helm" > "${3}/helm" \
+
+    wget https://get.helm.sh/helm-${1}-${2}-amd64.tar.gz -O - | tar -xzO "${2}-amd64/helm" > "${3}/helm" \
         && chmod +x "${3}/helm"
+        rm -rf helm-${1}-${2}-amd64.tar.gz linux-amd64
 
 }
