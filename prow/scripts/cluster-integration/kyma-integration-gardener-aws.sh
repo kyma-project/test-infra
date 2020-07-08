2c5ea72ead727ee66c3d3f5deb7e4988dfb15a38
I332735 <suleyman.akbas@sap.com>
2020-07-08 11:05:02 +0200

    Decrease minimum node count on Gardener jobs

--- prow/scripts/cluster-integration/kyma-integration-gardener-aws.sh
+++ prow/scripts/cluster-integration/kyma-integration-gardener-aws.sh
@@ -149,7 +149,7 @@ kyma provision gardener aws \
         --secret "${GARDENER_KYMA_PROW_PROVIDER_SECRET_NAME}" --name "${CLUSTER_NAME}" \
         --project "${GARDENER_KYMA_PROW_PROJECT_NAME}" --credentials "${GARDENER_KYMA_PROW_KUBECONFIG}" \
         --region "${GARDENER_REGION}" -z "${GARDENER_ZONES}" -t "${MACHINE_TYPE}" \
-        --scaler-max 4 --scaler-min 3 \
+        --scaler-max 4 --scaler-min 2 \
         --kube-version=${GARDENER_CLUSTER_VERSION}
 )
 
