90e3551b931b95d7ebe92ec0cf17e46f0ce4b22d
Katarzyna Kepka <kasia.kepka23@gmail.com>
2020-07-09 15:48:50 +0200

    Revert "Decrease minimum node count on Gardener jobs (#2561)"
    This reverts commit 1b52ac6157eb4e89fa22a47c7c64a2ef847f4612.

--- prow/scripts/cluster-integration/kyma-integration-gardener-azure.sh
+++ prow/scripts/cluster-integration/kyma-integration-gardener-azure.sh
@@ -212,7 +212,7 @@ provision_cluster() {
         --secret "${GARDENER_KYMA_PROW_PROVIDER_SECRET_NAME}" --name "${CLUSTER_NAME}" \
         --project "${GARDENER_KYMA_PROW_PROJECT_NAME}" --credentials "${GARDENER_KYMA_PROW_KUBECONFIG}" \
         --region "${GARDENER_REGION}" -z "${GARDENER_ZONES}" -t "${MACHINE_TYPE}" \
-        --scaler-max 4 --scaler-min 2 \
+        --scaler-max 4 --scaler-min 3 \
         --kube-version=${GARDENER_CLUSTER_VERSION} \
         --verbose
     set +x
