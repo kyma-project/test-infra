41757fdfbfce576bd4453dadd39965256afd795d
Przemek Pokrywka <dekiel@users.noreply.github.com>
2020-07-13 22:44:31 +0200

    First version of prowjob tester. (#2576)
    * First version of prowjob tester.
    
    * Added prowjob for pjtester.
    Added preset.
    Updated makefiles.
    
    * Added commit SHA as image env.
    
    * Corrected typo
    
    * executed goimports
    
    * Corrected prow-tools_image def name.

---
+++ development/tools/cmd/pjtester/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kyma-project/test-infra/development/tools/pkg/pjtester"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var (
+	log     = logrus.New()
+	rootCmd = &cobra.Command{
+		Use:   "pjtester",
+		Short: "pjtester generate new prowjob spec and schedule it",
+		Long:  "pjtester  generate new prowjob spec from provided path. It reuse PR refs.",
+		Run: func(cmd *cobra.Command, args []string) {
+			pjtester.SchedulePJ()
+		},
+	}
+)
+
+func main() {
+	log.SetOutput(os.Stdout)
+	log.SetLevel(logrus.InfoLevel)
+	if _, present := os.LookupEnv("IMAGE_COMMIT"); present {
+		fmt.Printf("IMAGE_COMMIT: %s\n", os.Getenv("IMAGE_COMMIT"))
+	}
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
