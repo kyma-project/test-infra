9f3a54f2132eef6f8f25e4e00df8327c58f5d5c4
dekiel <przemyslaw.pokrywka@sap.com>
2020-07-27 14:19:42 +0200

    Corrected failing tests.

--- prow/scripts/vpathguard.sh
+++ prow/scripts/vpathguard.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=SC1091
 source /home/prow/go/src/github.com/kyma-project/master_test-infra/prow/scripts/log.sh
+
+# Print test-infra commit on which image was built.
+if [ -n "${IMAGE_COMMIT:+set}" ]; then
+  echo "$IMAGE_COMMIT"
+fi
 
 if [ -d /home/prow/go/src/github.com/kyma-project/test-infra/vpath ]; then
   log::error "Directory vpath is present. Remove it to merge PR."
