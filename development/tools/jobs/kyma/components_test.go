b2d93e7142f1616e9468cb89afdd117f5f4a299f
Mateusz Puczyński <mateusz.puczynski@sap.com>
2020-07-27 16:41:45 +0200

    Add seperate directory for function-runtimes (#2636)

--- development/tools/jobs/kyma/components_test.go
+++ development/tools/jobs/kyma/components_test.go
@@ -137,6 +137,13 @@ var components = []struct {
 			jobsuite.AllReleases(),
 		},
 	},
+	{path: "function-runtimes", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.JobFileSuffix("generic"),
+			jobsuite.Since(releases.Release113),
+			jobsuite.Optional(),
+		},
+	},
 	{path: "event-service", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("generic"),
