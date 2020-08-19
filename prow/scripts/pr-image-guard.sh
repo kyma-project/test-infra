79b8b91a08a58c6e483834fd5a6e810a28031477
dariadomagala <daria.domagala@sap.com>
2020-08-19 14:33:12 +0200

    Fix pr-image-guard script

--- prow/scripts/pr-image-guard.sh
+++ prow/scripts/pr-image-guard.sh
@@ -3,8 +3,9 @@
 set -e
 
 readonly RESOURCES_PATH="/home/prow/go/src/github.com/kyma-project/kyma/resources"
+readonly TESTS_PATH="/home/prow/go/src/github.com/kyma-project/kyma/tests"
 
 # shellcheck disable=SC2126
-RES=$(grep -e 'version:\s*[Pp][Rr]-.*' -e 'image:.*:[Pp][Rr]-.*' -r "${RESOURCES_PATH}" -B 2 || true)
+RES=$(grep -e 'version:\s.*[Pp][Rr]-.*' -e 'image:.*:[Pp][Rr]-.*' -e 'tag:\s.*[Pp][Rr]-.*' -r "${RESOURCES_PATH}" -r "${TESTS_PATH}" -B 2 || true)
 echo -n "$RES"
 exit "$(echo -n "$RES" | wc -l | xargs)"
