14341e98284ab7b42bfa46e0b69be11453619c56
Mateusz Puczynski <mateusz.puczynski@sap.com>
2020-08-20 13:32:21 +0200

    next round of pjtester

--- prow/scripts/cluster-integration/kyma-gke-upgrade.sh
+++ prow/scripts/cluster-integration/kyma-gke-upgrade.sh
@@ -32,6 +32,7 @@
 set -o errexit
 
 discoverUnsetVar=false
+enableTestLogCollector=false
 
 for var in REPO_OWNER REPO_NAME DOCKER_PUSH_REPOSITORY KYMA_PROJECT_DIR CLOUDSDK_CORE_PROJECT CLOUDSDK_COMPUTE_REGION CLOUDSDK_DNS_ZONE_NAME GOOGLE_APPLICATION_CREDENTIALS KYMA_ARTIFACTS_BUCKET BOT_GITHUB_TOKEN GCR_PUSH_GOOGLE_APPLICATION_CREDENTIALS; do
     if [[ -z "${!var}" ]] ; then
@@ -138,14 +139,15 @@ cleanup() {
 }
 
 runTestLogCollector(){
-    if [[ "$BUILD_TYPE" == "master" ]]; then
-        shout "Install test-log-collector"
-        date
-
-        ( 
-            export TEST_INFRA_SOURCES_DIR LOG_COLLECTOR_SLACK_TOKEN # LOG_COLLECTOR_SLACK_TOKEN needs to be added here
-            "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/install-test-log-collector.sh" || true # we want it to work on "best effort" basis, which does not interfere with cluster 
-        )    
+    if [ "${enableTestLogCollector}" = true ] ; then
+        if [[ "$BUILD_TYPE" == "master" ]]; then
+            shout "Install test-log-collector"
+            date
+            export PROW_JOB_NAME="post-master-kyma-gke-integration"
+            ( 
+                "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/install-test-log-collector.sh" || true # we want it to work on "best effort" basis, which does not interfere with cluster 
+            )    
+        fi    
     fi
 }
 
@@ -506,6 +508,8 @@ createTestResources
 upgradeKyma
 
 remove_addons_if_necessary
+
+enableTestLogCollector=true # enable test-log-collector before tests; if prowjob fails before test phase we do not have any reason to enable it earlier
 
 testKyma
 
