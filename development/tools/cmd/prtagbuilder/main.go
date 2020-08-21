0468247056bccdf2568f3128fc71faa3734750d3
dekiel <przemyslaw.pokrywka@sap.com>
2020-08-21 14:34:42 +0200

    Changed github client to google/go-github/github

--- development/tools/cmd/prtagbuilder/main.go
+++ development/tools/cmd/prtagbuilder/main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
 	"github.com/kyma-project/test-infra/development/tools/pkg/prtagbuilder"
 	"github.com/sirupsen/logrus"
-	prowflagutil "k8s.io/test-infra/prow/flagutil"
 )
 
 var (
-	log       = logrus.New()
-	ghOptions prowflagutil.GitHubOptions
+	log = logrus.New()
 )
 
 func main() {
@@ -21,11 +18,5 @@ func main() {
 	if _, present := os.LookupEnv("IMAGE_COMMIT"); present {
 		fmt.Printf("IMAGE_COMMIT: %s\n", os.Getenv("IMAGE_COMMIT"))
 	}
-	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	ghOptions.AddFlagsWithoutDefaultGitHubTokenPath(fs)
-	_ = fs.Parse(os.Args[1:])
-	if err := ghOptions.Validate(false); err != nil {
-		logrus.WithError(err).Fatalf("github options validation failed")
-	}
-	prtagbuilder.BuildPrTag(ghOptions)
+	prtagbuilder.BuildPrTag()
 }
--- development/tools/pkg/prtagbuilder/prtagbuilder.go
+++ development/tools/pkg/prtagbuilder/prtagbuilder.go
@@ -1,67 +1,69 @@
 package prtagbuilder
 
 import (
+	"context"
 	"fmt"
-	"net/http"
+	"regexp"
+	"strconv"
 
-	"k8s.io/test-infra/prow/config/secret"
-
+	"github.com/google/go-github/v31/github"
 	"github.com/sirupsen/logrus"
-	prowflagutil "k8s.io/test-infra/prow/flagutil"
-	"k8s.io/test-infra/prow/github"
 	"k8s.io/test-infra/prow/pod-utils/downwardapi"
 )
 
-type githubClient interface {
-	GetPullRequest(org, repo string, number int) (*github.PullRequest, error)
-	GetSingleCommit(org, repo, SHA string) (github.SingleCommit, error)
+const (
+	ghProxyURL = "http://ghproxy/"
+)
+
+var (
+	client *github.Client
+	ctx    = context.Background()
+)
+
+func findPRNumber(commit *github.RepositoryCommit) string {
+	re := regexp.MustCompile(`^.*\(#(?P<prNumber>\d*)\)\s*$`)
+	matches := re.FindStringSubmatch(*commit.Commit.Message)
+	if len(matches) != 2 {
+		logrus.Fatalf("failed find PR number in commit message, found %s matched strings", len(matches))
+	}
+	return matches[1]
 }
 
-type SingleCommit struct {
-	Commit struct {
-		Tree struct {
-			SHA string `json:"sha"`
-		} `json:"tree"`
-	} `json:"commit"`
-}
-
-// GetSingleCommit returns a single commit.
-//
-// See https://developer.github.com/v3/repos/#get
-func GetSingleCommit(c github.Client, org, repo, SHA string) (SingleCommit, error) {
-	durationLogger := c.log("GetSingleCommit", org, repo, SHA)
-	defer durationLogger()
-
-	var commit SingleCommit
-	_, err := c.request(&request{
-		method:    http.MethodGet,
-		path:      fmt.Sprintf("/repos/%s/%s/commits/%s", org, repo, SHA),
-		exitCodes: []int{200},
-	}, &commit)
-	return commit, err
-}
-
-func BuildPrTag(ghOptions prowflagutil.GitHubOptions) {
-	var secretAgent *secret.Agent
-	if ghOptions.TokenPath != "" {
-		secretAgent = &secret.Agent{}
-		if err := secretAgent.Start([]string{ghOptions.TokenPath}); err != nil {
-			logrus.WithError(err).Fatal("Failed to start secret agent")
+func verifyPR(pr *github.PullRequest, commitSHA string) bool {
+	if *pr.Merged {
+		if *pr.MergeCommitSHA != commitSHA {
+			logrus.Fatalf("commit SHA and matched PR merge commit SHA doesn't match")
 		}
 	}
+	return true
+}
+func BuildPrTag() {
 	jobSpec, err := downwardapi.ResolveSpecFromEnv()
 	if err != nil {
 		logrus.WithError(err).Fatalf("failed to read JOB_SPEC prowjob env")
 	}
-	var ghClient githubClient
-	ghClient, err = ghOptions.GitHubClient(secretAgent, false)
+	client, err = github.NewEnterpriseClient(ghProxyURL, ghProxyURL, nil)
 	if err != nil {
-		logrus.WithError(err).Fatalf("failed create github client")
+		logrus.WithError(err).Fatalf("failed get new github client")
 	}
-	var commit github.SingleCommit
-	commit, err = ghClient.GetSingleCommit(jobSpec.Refs.Org, jobSpec.Refs.Repo, jobSpec.Refs.BaseSHA)
+	_, _, err = client.APIMeta(ctx)
 	if err != nil {
-		logrus.WithError(err).Fatal("failed get commit")
+		logrus.WithError(err).Warnf("failed connecting to ghproxy")
+		client = github.NewClient(nil)
 	}
-	fmt.Printf("%v", commit)
+	commit, _, err := client.Repositories.GetCommit(ctx, jobSpec.Refs.Org, jobSpec.Refs.Repo, jobSpec.Refs.BaseSHA)
+	if err != nil {
+		logrus.WithError(err).Fatalf("failed get commit %s", jobSpec.Refs.BaseSHA)
+	}
+	prNumber, err := strconv.Atoi(findPRNumber(commit))
+	if err != nil {
+		logrus.WithError(err).Fatalf("failed convert PR number to intiger")
+	}
+	pr, _, err := client.PullRequests.Get(ctx, jobSpec.Refs.Org, jobSpec.Refs.Repo, prNumber)
+	if err != nil {
+		logrus.WithError(err).Fatalf("failed get Pull Request number %s", prNumber)
+	}
+	if verifyPR(pr, jobSpec.Refs.BaseSHA) {
+		fmt.Printf("PR%d", prNumber)
+	}
 }
