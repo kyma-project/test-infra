270ca3119920afe7a0aee4bb8522ad16e9ad4ad8
Rakesh Garimella <rakesh.garimella@sap.com>
2020-08-24 09:31:31 +0200

    Fix if statements

--- prow/scripts/cluster-integration/kyma-gke-upgrade-xip.sh
+++ prow/scripts/cluster-integration/kyma-gke-upgrade-xip.sh
@@ -212,7 +212,7 @@ function installKyma() {
     shout "Use released artifacts from version ${LAST_RELEASE_VERSION}"
     date
 
-    if [[ "$LAST_RELEASE_VERSION" eq "1.14.0" ]]; then
+    if [ "$LAST_RELEASE_VERSION" eq "1.14.0" ]; then
         curl -L --silent --fail --show-error "https://github.com/kyma-project/kyma/releases/download/${LAST_RELEASE_VERSION}/kyma-installer-cluster.yaml" --output /tmp/kyma-gke-upgradeability/last-release-installer.yaml
         kubectl apply -f /tmp/kyma-gke-upgradeability/last-release-installer.yaml
     else
