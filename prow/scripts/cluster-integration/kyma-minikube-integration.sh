06fdb7f58cadea112db384704b8330f655d0ce12
Kamil Kasperski <kamil.kasperski@sap.com>
2020-09-02 14:19:52 +0200

    use kyma cli in kyma-integration jobs

---
+++ prow/scripts/cluster-integration/kyma-minikube-integration.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+set -e
+
+driver="none"
+
+installKymaCLI() {
+    local settings
+    local kyma_version
+    mkdir -p "$HOME/bin"
+    export PATH="$HOME/bin:${PATH}"
+    os=$(host::os)
+
+    pushd "$HOME/bin" || exit
+
+    echo "Install kyma CLI ${os} locally to $HOME/bin..."
+
+    curl -sSLo kyma "https://storage.googleapis.com/kyma-cli-stable/kyma-${os}?alt=media"
+    chmod +x kyma
+    kyma_version=$(kyma version --client)
+    echo "Kyma CLI version: ${kyma_version}"
+
+    echo "OK"
+
+    popd || exit
+
+    eval "${settings}"
+}
+
+host::os() {
+  local host_os
+  case "$(uname -s)" in
+    Darwin)
+      host_os=darwin
+      ;;
+    Linux)
+      host_os=linux
+      ;;
+    *)
+      >&2 echo -e "Unsupported host OS. Must be Linux or Mac OS X."
+      exit 1
+      ;;
+  esac
+  echo "${host_os}"
+}
+
+echo "--> Installing Kyma CLI"
+if ! [[ -x "$(command -v kyma)" ]]; then
+  echo "Kyma CLI not found"
+  installKymaCLI
+else
+  echo "Kyma CLI is already installed"
+  kyma_version=$(kyma version --client)
+  echo "Kyma CLI version: ${kyma_version}"
+fi
+echo "--> Done"
+
+echo "--> Provision Kyma cluster on minikube using VM driver ${driver}"
+kyma provision minikube --ci --vm-driver="${driver}" --non-interactive
+echo "--> Done"
+
+echo "--> Installing Kyma on minikube cluster"
+kyma install --ci --source="local" --src-path=./kyma --non-interactive
+echo "--> Done"
+
+echo "--> Run kyma tests"
+echo "  List test definitions"
+kyma test definitions --ci
+echo "  Run tests"
+kyma test run --ci --watch --max-retries=3 --non-interactive
