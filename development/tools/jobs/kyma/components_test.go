b0484797814eba791a880e123333cfdb3aeb538a
Sayan Hazra <sayan.hazra@sap.com>
2020-09-18 04:36:20 +0200

    Fixed job for event-publisher-proxy

--- development/tools/jobs/kyma/components_test.go
+++ development/tools/jobs/kyma/components_test.go
@@ -4,6 +4,7 @@ import (
 	"path"
 	"testing"
 
+	"github.com/kyma-project/test-infra/development/tools/jobs/releases"
 	"github.com/kyma-project/test-infra/development/tools/jobs/tester"
 	"github.com/kyma-project/test-infra/development/tools/jobs/tester/jobsuite"
 )
@@ -153,6 +154,13 @@ var components = []struct {
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("generic"),
 			jobsuite.AllReleases(),
+		},
+	},
+	{path: "event-publisher-proxy", image: tester.ImageBootstrap20181204, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.JobFileSuffix("generic"),
+			jobsuite.Since(releases.Release117),
+			jobsuite.Optional(),
 		},
 	},
 }
