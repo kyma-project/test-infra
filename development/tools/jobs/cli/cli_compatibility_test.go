826de1b3f93a2e741deaac2c99d0276d94a0681b
Borja Clemente <borja.clemente.castanera@sap.com>
2020-10-02 16:35:13 +0200

    First draft of the CLI compatibility periodic jobs
    Signed-off-by: Borja Clemente <borja.clemente.castanera@sap.com>

---
+++ development/tools/jobs/cli/cli_compatibility_test.go
@@ -0,0 +1,52 @@
+package kymacli_test
+
+import (
+	"testing"
+
+	"github.com/kyma-project/test-infra/development/tools/jobs/tester"
+	"github.com/kyma-project/test-infra/development/tools/jobs/tester/preset"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const cliCompatibilityJobPath = "./../../../../prow/jobs/cli/cli-compatibility.yaml"
+
+func TestKymaCliCompatibilityPeriodics(t *testing.T) {
+	// WHEN
+	jobConfig, err := tester.ReadJobConfig(cliCompatibilityJobPath)
+	// THEN
+	require.NoError(t, err)
+
+	periodics := jobConfig.AllPeriodics()
+	assert.Len(t, periodics, 2)
+
+	// Compatibility with previous release
+	expName := "kyma-cli-compatibility-1"
+	actualPeriodic := tester.FindPeriodicJobByName(periodics, expName)
+	require.NotNil(t, actualPeriodic)
+	assert.Equal(t, expName, actualPeriodic.Name)
+	assert.True(t, actualPeriodic.Decorate)
+	assert.Equal(t, "00 00 * * 0", actualPeriodic.Cron)
+	tester.AssertThatHasExtraRepoRef(t, actualPeriodic.JobBase.UtilityConfig, []string{"test-infra"})
+	tester.AssertThatHasPresets(t, actualPeriodic.JobBase, preset.GCProjectEnv, "preset-sa-vm-kyma-integration")
+	assert.Equal(t, tester.ImageGolangKubebuilder2BuildpackLatest, actualPeriodic.Spec.Containers[0].Image)
+	tester.AssertThatSpecifiesResourceRequests(t, actualPeriodic.JobBase)
+	tester.AssertThatContainerHasEnv(t, actualPeriodic.Spec.Containers[0], "COMPAT_BACKTRACK", "1")
+	tester.AssertThatContainerHasEnv(t, actualPeriodic.Spec.Containers[0], "GO111MODULE", "on")
+	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/compatibility-cli.sh"}, actualPeriodic.Spec.Containers[0].Command)
+
+	// Compatibility 2 releases back
+	expName = "kyma-cli-compatibility-2"
+	actualPeriodic = tester.FindPeriodicJobByName(periodics, expName)
+	require.NotNil(t, actualPeriodic)
+	assert.Equal(t, expName, actualPeriodic.Name)
+	assert.True(t, actualPeriodic.Decorate)
+	assert.Equal(t, "00 03 * * 0", actualPeriodic.Cron)
+	tester.AssertThatHasExtraRepoRef(t, actualPeriodic.JobBase.UtilityConfig, []string{"test-infra"})
+	tester.AssertThatHasPresets(t, actualPeriodic.JobBase, preset.GCProjectEnv, "preset-sa-vm-kyma-integration")
+	assert.Equal(t, tester.ImageGolangKubebuilder2BuildpackLatest, actualPeriodic.Spec.Containers[0].Image)
+	tester.AssertThatSpecifiesResourceRequests(t, actualPeriodic.JobBase)
+	tester.AssertThatContainerHasEnv(t, actualPeriodic.Spec.Containers[0], "COMPAT_BACKTRACK", "2")
+	tester.AssertThatContainerHasEnv(t, actualPeriodic.Spec.Containers[0], "GO111MODULE", "on")
+	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/compatibility-cli.sh"}, actualPeriodic.Spec.Containers[0].Command)
+}
---
+++ prow/scripts/compatibility-cli.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+###
+# This script is designed to check the compatibility of the Kyma CLI with previous Kyma versions.
+# It will calculate which Kyma version should be tested and will pass it on to the 'provision-vm-cli.sh' script
+#
+# INPUT:
+# - COMPAT_BACKTRACK: number of versions to go back in Kyma
+#
+# REQUIREMENTS:
+# - git
+###
+
+set -o errexit
+
+VARIABLES=(
+    COMPAT_BACKTRACK
+)
+
+for var in "${VARIABLES[@]}"; do
+    if [ -z "${!var}" ] ; then
+        echo "ERROR: $var is not set"
+        discoverUnsetVar=true
+    fi
+done
+if [ "${discoverUnsetVar}" = true ] ; then
+    exit 1
+fi
+
+#Exported variables
+export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
+export TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS="${TEST_INFRA_SOURCES_DIR}/prow/scripts"
+
+# shellcheck disable=SC1090
+source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/library.sh"
+
+echo "--------------------------------------------------------------------------------"
+echo "Kyma CLI compatibility checker"
+echo "--------------------------------------------------------------------------------"
+
+# Get all non RC release tags
+RELEASES=$(curl -s https://api.github.com/repos/kyma-project/kyma/releases | grep -E '(tag_name": ")(\d+\.\d+\.\d+\",?$)' | awk '{print $2}')
+
+# Clean up spaces and quotes
+RELEASES=${RELEASES//[,\"]/ }
+
+# Split into array
+RELEASES=(${RELEASES})
+
+# Go through releases ignoring patch versions in descending order until we skip the desired number of minor releases
+
+# remove patch
+CURRENT=$(echo ${RELEASES[0]} | awk -F'.' '{print $1"."$2}')
+for r in ${RELEASES[@]}; do
+    # remove patch from candidate
+    WANT=$(echo ${r} | awk -F'.' '{print $1"."$2}')
+
+    if [[ $WANT != $CURRENT ]]; then
+        # check if we need to backtrack more
+        if [[ $COMPAT_BACKTRACK == 1 ]]; then
+            # Found the target release
+            TARGET=$r
+            break
+        else
+            # Still need to backtrack further
+            COMPAT_BACKTRACK=$(($COMPAT_BACKTRACK - 1))
+            CURRENT=$(echo ${r} | awk -F'.' '{print $1"."$2}')
+        fi
+    fi
+done
+
+shout "Checking Kyma CLI compatibility with Kyma ${TARGET}"
+date
+
+# Call CLI integration script with the target release
+"${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}"/provision-vm-cli.sh --kyma-version "${TARGET}"
