54e38162b0528af239217e8496992d7584718225
Nils Seip <github@nilstschmidt.de>
2020-10-05 15:59:41 +0200

    Set domain in helm install command for `kyma-upgrade-gardener-azure` test (#2818)

--- prow/scripts/cluster-integration/kyma-upgrade-gardener-azure.sh
+++ prow/scripts/cluster-integration/kyma-upgrade-gardener-azure.sh
@@ -257,6 +257,10 @@ function installTestChartOrFail() {
   local name=$2
   local namespace=$3
 
+  # get server IP for HTTPS protocol and split away wildcard ".*" from it using sed
+  local domain
+  domain=$(kubectl get gateways.networking.istio.io --namespace kyma-system kyma-gateway -o jsonpath='{.spec.servers[?(@.port.protocol=="HTTPS")].hosts[0]}' | sed s/\*\.//g)
+
   shout "Create ${name} resources"
   date
 
@@ -265,7 +269,7 @@ function installTestChartOrFail() {
       --create-namespace \
       "${path}" \
       --timeout "${HELM_TIMEOUT_SEC}" \
-      --set domain="${DOMAIN}" \
+      --set domain="${domain}" \
       --wait
 
   prepareResult=$?
