c388437a74e4689ba4d6414cf32ae67fbbbde89b
Benjamin Somhegyi <benjamin.somhegyi@sap.com>
2020-10-12 12:08:49 +0200

    Enable using 1.16.0 as Kyma version in control-plane

--- prow/scripts/cluster-integration/control-plane-gke-integration.sh
+++ prow/scripts/cluster-integration/control-plane-gke-integration.sh
@@ -213,9 +213,24 @@ function applyKymaOverrides() {
     --data "pushgateway.enabled=true" \
     --label "component=monitoring"
 
-  "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-config-map.sh" --name "istio-overrides" \
-    --data "gateways.istio-ingressgateway.loadBalancerIP=${GATEWAY_IP_ADDRESS}" \
-    --label "component=istio"
+  cat << EOF > "$PWD/kyma_istio_operator"
+apiVersion: install.istio.io/v1alpha1
+kind: IstioOperator
+metadata:
+  namespace: istio-system
+spec:
+  components:
+    ingressGateways:
+      - name: istio-ingressgateway
+        k8s:
+          service:
+            loadBalancerIP: ${GATEWAY_IP_ADDRESS}
+            type: LoadBalancer
+EOF
+
+  "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-config-map-file.sh" --name "istio-overrides" \
+    --label "component=istio" \
+    --file "$PWD/kyma_istio_operator"
 
   "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/create-config-map.sh" --name "api-gateway-overrides" \
     --data "tests.env.gatewayName=compass-istio-gateway" \
@@ -410,7 +425,7 @@ function installControlPlane() {
   | sed -e "s/__VERSION__/0.0.1/g" \
   | sed -e "s/__.*__//g" \
   | kubectl apply -f-
-  
+
   shout "Installation triggered"
   date
   "${KCP_SCRIPTS_DIR}"/is-installed.sh --timeout 30m
