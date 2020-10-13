94cba09d811ffbd78f86c53be3dee3fd66273076
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2020-10-13 18:36:11 +0200

    remove color codes

--- prow/scripts/lib/log.sh
+++ prow/scripts/lib/log.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-INVERTED='\033[7m'
-YELLOW='\e[33m'
-NC='\033[0m' # No Color
-
 # log::date retruns current date in format expected by logs
 function log::date {
     date +"%Y/%m/%d %T %Z"
@@ -18,9 +12,9 @@ function log::date {
 function log::banner {
   local logdate
   logdate=$(log::date)
-  echo -e "${INVERTED}${logdate} [INFO] *************************************************************************************${NC}"
-  echo -e "${INVERTED}${logdate} [INFO] * $* ${NC}"
-  echo -e "${INVERTED}${logdate} [INFO] *************************************************************************************${NC}"
+  echo -e "${logdate} [INFO] *************************************************************************************"
+  echo -e "${logdate} [INFO] * $*"
+  echo -e "${logdate} [INFO] *************************************************************************************"
 }
 
 # log::info prints message with info level
@@ -28,29 +22,29 @@ function log::banner {
 # Arguments:
 #   $* - Message
 function log::info {
-    echo -e "${INVERTED}$(log::date) [INFO] $* ${NC}"
+    echo -e "$(log::date) [INFO] $*"
 }
 
-# log::info prints a message with info level in green
+# log::info prints a message with info level
 #
 # Arguments:
 #   $* - Message
 function log::success {
-    echo -e "${GREEN}$(log::date) [INFO] $* ${NC}"
+    echo -e "$(log::date) [INFO] SUCCESS: $*"
 }
 
-# log::info prints a message with warning level in yellow
+# log::info prints a message with warning level
 #
 # Arguments:
 #   $* - Message
 function log::warn {
-    echo -e "${YELLOW}$(log::date) [WARN] $* ${NC}"
+    echo -e "$(log::date) [WARN] $*"
 }
 
-# log::info prints a message with error level in red
+# log::info prints a message with error level
 #
 # Arguments:
 #   $* - Message
 function log::error {
-    >&2 echo -e "${RED}$(log::date) [ERRO] $* ${NC}"
+    >&2 echo -e "$(log::date) [ERROR] $*"
 }
