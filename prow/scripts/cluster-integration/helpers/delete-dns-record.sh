224cbc101b93472535400c53b0e156f0c389cfcc
Adam Walach <adam.walach@sap.com>
2020-11-02 09:22:54 +0100

    Add ttl param to cleaner

--- prow/scripts/cluster-integration/helpers/delete-dns-record.sh
+++ prow/scripts/cluster-integration/helpers/delete-dns-record.sh
@@ -5,10 +5,10 @@ set -o pipefail
 
 if [ -x /prow-tools/dnscleaner ];
 then
-  /prow-tools/dnscleaner --attempts=10 "$@"
+  /prow-tools/dnscleaner --attempts=10 --ttl 60 "$@"
 else
   cd "development/tools"
-  go run "cmd/dnscleaner" --attempts=10 "$@"
+  go run "cmd/dnscleaner" --attempts=10 --ttl 60 "$@"
 fi
 
 echo "DNS Record deleted, but it can be visible for some time due to DNS caches"
