cf7828f182f1a4ad3d50df8be90a3382f03a65bd
Rakesh Garimella <rakesh.garimella@sap.com>
2020-11-10 15:50:08 +0100

    Fixes

--- prow/scripts/cluster-integration/kyma-gke-rel2rel-upgrade.sh
+++ prow/scripts/cluster-integration/kyma-gke-rel2rel-upgrade.sh
@@ -59,6 +59,8 @@ export UPGRADE_TEST_LABEL_VALUE_EXECUTE="executeTests"
 export TEST_CONTAINER_NAME="tests"
 export HELM_TIMEOUT_SEC=10000s # timeout in sec for helm install/test operation
 export TEST_TIMEOUT_SEC=600    # timeout in sec for test pods until they reach the terminating state
+export EXTERNAL_SOLUTION_TEST_NAMESPACE="integration-test"
+export EXTERNAL_SOLUTION_TEST_RELEASE_NAME="${EXTERNAL_SOLUTION_TEST_NAMESPACE}"
 
 
 KYMA_LABEL_PREFIX="kyma-project.io"
