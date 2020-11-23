4a58b4725957ab38879de199edbde1820cc54e74
Kamil Kasperski <kamil.kasperski@sap.com>
2020-11-23 11:31:29 +0100

    Edit create-azure-event-hubs-secret.sh to use azure.sh lib (#2968)
    * Edit create-azure-event-hubs-secret.sh to use azure.sh lib
    
    * Edit create-azure-event-hubs-secret.sh to use azure.sh lib
    
    * update prowjobs
    
    * add titles to version

--- prow/scripts/lib/azure.sh
+++ prow/scripts/lib/azure.sh
@@ -5,19 +5,27 @@ LIBDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd)"
 # shellcheck source=prow/scripts/lib/log.sh
 source "${LIBDIR}"/log.sh
 
-function az::verify_binary {
+function az::verify_deps {
   if ! [[ -x $(command -v az) ]]; then
     log::error "'az' command not found in \$PATH. Exiting..."
     exit 1
   else
+    echo "Azure CLI Version:"
     az version
+  fi
+  if ! [[ -x $(command -v jq) ]]; then
+    log::error "'jq' command not found in \$PATH. Exiting..."
+    exit 1
+  else
+    echo "jq version:"
+    jq --version
   fi
 }
 
 # az::login logs in to the azure service using provided credentials file in the function argument.
 # Function accepts JSON file formatted below:
 # {
-#   "tenant": "tenant_id",
+#   "tenant_id": "tenant_id",
 #   "app_id": "subscription_app_id",
 #   "secret": "subscription_secret"
 # }
@@ -31,18 +39,32 @@ function az::login {
   if [[ -z "$1" ]]; then
     log::error "Azure credentials file not provided. please provide azure credentials filepath in the argument. Exiting..."
     exit 1
-  elif ! [[ -f "$1" ]]; then
-    log:error "Azure credentials file not found. Make sure it is present under the provided filepath. Exiting..."
+  elif [[ ! -f "$1" ]]; then
+    log::error "Azure credentials file not found. Make sure it is present under the provided filepath. Exiting..."
     exit 1
   fi
   AZURE_CREDENTIALS_FILE="$1"
-  read -r AZURE_SUBSCRIPTION_TENANT AZURE_SUBSCRIPTION_APP_ID AZURE_SUBSCRIPTION_SECRET < <(jq -r '.tenant, .app_id, .secret' "$AZURE_CREDENTIALS_FILE")
+  AZURE_SUBSCRIPTION_TENANT=$(jq -r '.tenant_id' "$AZURE_CREDENTIALS_FILE")
+  AZURE_SUBSCRIPTION_APP_ID=$(jq -r '.app_id' "$AZURE_CREDENTIALS_FILE")
+  AZURE_SUBSCRIPTION_SECRET=$(jq -r '.secret' "$AZURE_CREDENTIALS_FILE")
 
   # login
+  log::info "Logging in to Azure"
   az login --service-principal -u "${AZURE_SUBSCRIPTION_APP_ID}" -p "${AZURE_SUBSCRIPTION_SECRET}" --tenant "${AZURE_SUBSCRIPTION_TENANT}"
+  log::info "Successfully logged-in!"
+}
+
+# az::set_subscription sets the subscription using provided subscription ID in the argument.
+function az::set_subscription {
+  if [[ -z "$1" ]]; then
+    log::error "Azure Subscription ID is not found. Please provide azure subscription ID in the argument. Exiting..."
+    exit 1
+  fi
+  az account set \
+    --subscription "$1"
 }
 
 # This check will trigger everytime the file is sourced.
 # This should allow easy checking for the related requirements without copying loads of code.
 # If more checks will be needed we should add another function there.
-az::verify_binary
+az::verify_deps
