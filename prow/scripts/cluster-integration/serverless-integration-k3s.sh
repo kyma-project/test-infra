c76642c50caacdd2c38caceba5a35d63e716dc99
Mateusz Puczynski <mateusz.puczynski@sap.com>
2020-12-01 10:18:21 +0100

    get rid of yq

--- prow/scripts/cluster-integration/serverless-integration-k3s.sh
+++ prow/scripts/cluster-integration/serverless-integration-k3s.sh
@@ -88,8 +88,6 @@ docker run -d \
 install::prereq(){
     curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
     apt-get -y install jq
-    wget -q https://github.com/mikefarah/yq/releases/download/3.4.1/yq_linux_amd64 -O /usr/bin/yq &&\
-    chmod +x /usr/bin/yq
 }
 
 install::k3s() {
