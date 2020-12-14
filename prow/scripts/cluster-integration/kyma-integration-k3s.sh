ea035513557601cf1a430266cf233d53fca5cdf8
Krystian Cieslik <crabtree@users.noreply.github.com>
2020-12-14 23:12:35 +0100

    Rename provisioning script for integration tests on minikube

---
+++ prow/scripts/cluster-integration/kyma-integration-k3s.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+export KYMA_SOURCES_DIR="./kyma"
+export LOCAL_KYMA_DIR="./local-kyma"
+
+install::prereq() {
+    curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+    curl -sL https://deb.nodesource.com/setup_14.x | bash -
+    apt-get -y install jq nodejs
+}
+
+install::kyma() {
+    mv "${KYMA_SOURCES_DIR}/resources" "${LOCAL_KYMA_DIR}/"
+
+    pushd ${LOCAL_KYMA_DIR}
+    ./create-cluster-k3s.sh
+    ./install-istio.sh -f config-istio.yaml
+    ./install-kyma.sh
+    popd
+}
+
+run::tests() {
+    pushd "${KYMA_SOURCES_DIR}/tests/fast-integration"
+    npm install
+    npm test
+    popd
+}
+
+install::prereq
+install::kyma
+run::tests
