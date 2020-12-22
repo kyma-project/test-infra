7486d35e2416d923a02edb2b6ac371fdc463a494
Kamil Kasperski <kamil.kasperski@sap.com>
2020-12-22 12:56:24 +0100

    Add several new functions to azure.sh, remove aks-library.sh (#3053)
    * Add several new functions to azure.sh, remove aks-library.sh
    
    * rename local variable

--- prow/scripts/lib/azure.sh
+++ prow/scripts/lib/azure.sh
@@ -3,7 +3,7 @@
 LIBDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd)"
 
 # shellcheck source=prow/scripts/lib/log.sh
-source "${LIBDIR}"/log.sh
+source "${LIBDIR}/log.sh"
 
 function az::verify_deps {
   if ! [[ -x $(command -v az) ]]; then
@@ -60,8 +60,101 @@ function az::set_subscription {
     log::error "Azure Subscription ID is not found. Please provide azure subscription ID in the argument. Exiting..."
     exit 1
   fi
+  log::info "Setting Azure subscription..."
   az account set \
     --subscription "$1"
+}
+
+# az::create_resource_group creates resource group in a given region
+#
+# Arguments
+# $1 - resource group name to be created
+# $2 - region in which group should be created
+function az::create_resource_group {
+  if [[ -z "$1" ]]; then
+    log::error "Resource group name not present. Provide resource group name as 1st argument. Exiting..."
+    exit 1
+  fi
+  if [[ -z "$2" ]]; then
+    log::error "Region not present. Provide region as 2nd argument. Exiting..."
+    exit 1
+  fi
+  local rsGroup
+  local azRegion
+  rsGroup=$1
+  azRegion=$2
+
+  log::info "Creating resouce group \"$rsGroup\" in a region \"$azRegion\""
+  az group create --name "${rsGroup}" --location "${azRegion}"
+  until [[ $(az group exists --name "${rsGroup}" -o json) == true ]]; do
+		sleep 15
+		counter=$(( counter + 1 ))
+		if (( counter == 5 )); then
+			log::error "\n---\nAzure resource group ${rsGroup} still not present after one minute wait.\n---"
+			exit 1
+		fi
+	done
+}
+
+# az::provision_aks_cluster creates an AKS cluster
+#
+# Required exported variables
+# RS_GROUP - azure resource group
+# REGION - azure region
+# CLUSTER_SIZE - azure cluster size
+# CLUSTER_VERSION - desired k8s cluster version
+# CLUSTER_ADDONS - addidional AKS addons
+# AZURE_CREDENTIALS_FILE - credentials file, refer to az::login
+#
+# Arguments
+# $1 - cluster name
+function az::provision_aks_cluster {
+  if [[ -z "$1" ]]; then
+    log::error "Cluster name not present. Provide cluster name as an argument. Exiting..."
+    exit 1
+  fi
+  local CLUSTER_NAME
+  CLUSTER_NAME=$1
+
+  log::info "Provisioning AKS cluster"
+  AKS_CLUSTER_VERSION=$(az aks get-versions -l "${REGION}" | jq '.orchestrators|.[]|select(.orchestratorVersion | contains("'"${CLUSTER_VERSION}"'"))' | jq -s '.' | jq -r 'sort_by(.orchestratorVersion | split(".") | map(tonumber)) | .[-1].orchestratorVersion')
+	log::info "Latest available version is: ${AKS_CLUSTER_VERSION}"
+
+  az aks create \
+      --resource-group "${RS_GROUP}" \
+      --name "${CLUSTER_NAME}" \
+      --node-count 3 \
+      --node-vm-size "${CLUSTER_SIZE}" \
+      --kubernetes-version "${AKS_CLUSTER_VERSION}" \
+      --enable-addons "${CLUSTER_ADDONS}" \
+      --service-principal "$(jq -r '.app_id' "$AZURE_CREDENTIALS_FILE")" \
+      --client-secret "$(jq -r '.secret' "$AZURE_CREDENTIALS_FILE")" \
+      --generate-ssh-keys \
+      --zones 1 2 3
+}
+
+# az ::reserve_ip_address reserves IP address and returns it to STDOUT
+#
+# Required exported variables
+# RS_GROUP - resource group (must match the cluster RS group)
+# REGION - Azure region in which IP is reserved
+#
+# Arguments
+# $1 - IP address name used for identification
+function az::reserve_ip_address {
+  if [[ -z "$1" ]]; then
+    log::error "IP address name not present. Provide IP address name as an argument. Exiting..."
+    exit 1
+  fi
+  local ipName
+  ipName=$1
+
+  if az network public-ip create -g "${RS_GROUP}" -n "${ipName}" -l "${REGION}" --allocation-method static; then
+    az network public-ip show -g "${RS_GROUP}" -n "${ipName}" --query ipAddress -o tsv
+  else
+    log::error "Could not create IP address. Exiting..."
+    exit 1
+  fi
 }
 
 # This check will trigger everytime the file is sourced.
