b772f39ed3ba030adae0fe43819d6d2a77316a81
Kamil Kasperski <kamil.kasperski@sap.com>
2020-12-22 14:32:25 +0100

    use gcloud.sh in the kyma-gke-integration.sh

--- prow/scripts/lib/utils.sh
+++ prow/scripts/lib/utils.sh
@@ -12,3 +12,40 @@ utils::check_required_vars() {
       exit 1
     fi
 }
+# utils::generate_self_signed_cert generates self-signed certificate for the given domain
+#
+# Optional exported variables
+# CERT_VALID_DAYS - days when the certificate is valid
+# Arguments
+# $1 - domain name
+function utils::generate_self_signed_cert() {
+  if [ -z "$1" ]; then
+    echo "Domain name is empty. Exiting..."
+    exit 1
+  fi
+  local DOMAIN
+  DOMAIN=$1
+  local CERT_PATH
+  local KEY_PATH
+  tmpDir=$(mktemp -d)
+  CERT_PATH="${tmpDir}/cert.pem"
+  KEY_PATH="${tmpDir}/key.pem"
+  CERT_VALID_DAYS=${CERT_VALID_DAYS:-5}
+
+  openssl req -x509 -nodes -days "${CERT_VALID_DAYS}" -newkey rsa:4069 \
+                   -subj "/CN=${DOMAIN}" \
+                   -reqexts SAN -extensions SAN \
+                   -config <(cat /etc/ssl/openssl.cnf \
+          <(printf "\\n[SAN]\\nsubjectAltName=DNS:*.%s" "${DOMAIN}")) \
+                   -keyout "${KEY_PATH}" \
+                   -out "${CERT_PATH}"
+
+  TLS_CERT=$(base64 "${CERT_PATH}" | tr -d '\n')
+  TLS_KEY=$(base64 "${KEY_PATH}" | tr -d '\n')
+
+  echo "${TLS_CERT}"
+  echo "${TLS_KEY}"
+
+  rm "${KEY_PATH}"
+  rm "${CERT_PATH}"
+}
