c423793123f0fdbdac1626857aa4464b299af1e6
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2020-12-22 16:12:31 +0100

    Testing with minikube

--- prow/scripts/provision-vm-cli.sh
+++ prow/scripts/provision-vm-cli.sh
@@ -40,8 +40,8 @@ else
 fi
 
 # Support configuration via ENV vars (can be be overwritten by CLI args)
-KUBERNETES_RUNTIME="${KUBERNETES_RUNTIME:=k3s}"
-TEST_SUITE="${TEST_SUITE:=alpha}"
+KUBERNETES_RUNTIME="${KUBERNETES_RUNTIME:=minikube}"
+TEST_SUITE="${TEST_SUITE:=default}"
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
