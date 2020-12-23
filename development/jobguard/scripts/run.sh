8ae96207e5f2f5f3cae265ca03e29e799404c266
Adam Wałach <adam.walach@sap.com>
2020-12-23 14:57:44 +0100

    Fix protecode-guard (#3062)
    * Fix protecode-guard
    * Improve jobguard

--- development/jobguard/scripts/run.sh
+++ development/jobguard/scripts/run.sh
@@ -4,13 +4,17 @@ ROOT_PATH=$( dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" )
 
 KYMA_PROJECT_DIR=${KYMA_PROJECT_DIR:-"/home/prow/go/src/github.com/kyma-project"}
 JOB_NAME_PATTERN=${JOB_NAME_PATTERN:-"(pre-master-kyma-components-.*)|(pre-master-kyma-tests-.*)|(pre-kyma-components-.*)|(pre-kyma-tests-.*)"}
+TIMEOUT=${JOBGUARD_TIMEOUT:-"15m"}
 echo "KYMA PROJECT DIR ${KYMA_PROJECT_DIR}"
 echo "COMMIT SHA: ${PULL_PULL_SHA}"
+echo "JOBGUARD_TIMEOUT: ${TIMEOUT}"
+
 export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
 
 if [ -x "/prow-tools/jobguard" ]; then
   env GITHUB_TOKEN="${BOT_GITHUB_TOKEN}" \
       INITIAL_SLEEP_TIME=1m \
+      TIMEOUT="${TIMEOUT}" \
       COMMIT_SHA="${PULL_PULL_SHA}" \
       JOB_NAME_PATTERN="${JOB_NAME_PATTERN}" \
       PROW_CONFIG_FILE="${TEST_INFRA_SOURCES_DIR}/prow/config.yaml" \
@@ -21,6 +25,7 @@ else
   cd "${ROOT_PATH}/cmd/jobguard" || exit 1
   env GITHUB_TOKEN="${BOT_GITHUB_TOKEN}" \
       INITIAL_SLEEP_TIME=1m \
+      TIMEOUT="${TIMEOUT}" \
       COMMIT_SHA="${PULL_PULL_SHA}" \
       JOB_NAME_PATTERN="${JOB_NAME_PATTERN}" \
       PROW_CONFIG_FILE="${TEST_INFRA_SOURCES_DIR}/prow/config.yaml" \
