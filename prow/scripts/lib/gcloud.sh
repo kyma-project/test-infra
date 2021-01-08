1de160895f93f5d77ba1f4c3a298d5d940450892
Piotr Halama <piotr.halama@sap.com>
2021-01-08 12:14:04 +0100

    scripts: fix gcloud::cleanup

--- prow/scripts/lib/gcloud.sh
+++ prow/scripts/lib/gcloud.sh
@@ -432,7 +432,6 @@ function gcloud::delete_docker_image() {
 # APISERVER_DNS_FULL_NAME
 # APISERVER_IP_ADDRESS
 function gcloud::cleanup {
-  # TODO (@Ressetkk): Not yet finished.
   if [ -n "$CLEANUP_CLUSTER" ]; then
     log::info "Removing cluster $CLUSTER_NAME"
     gcloud::deprovision_gke_cluster "$CLUSTER_NAME"
