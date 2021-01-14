bd024304c4e2040a0d3ae1502573f0dab8a4a67c
Piotr Halama <piotr.halama@sap.com>
2021-01-14 15:40:47 +0100

    revert changes

--- prow/scripts/lib/gardener/aws.sh
+++ prow/scripts/lib/gardener/aws.sh
@@ -19,10 +19,6 @@ gardener::cleanup() {
     #!!! Must be at the beginning of this function !!!
     EXIT_STATUS=$?
 
-    if [[ -n "${SUITE_NAME}" ]]; then
-        kyma::test_summary
-    fi
-
     if [ "${ERROR_LOGGING_GUARD}" = "true" ]; then
         log::error "AN ERROR OCCURED! Take a look at preceding log entries."
     fi
