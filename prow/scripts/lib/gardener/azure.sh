4b12084308fedc03a760d7531593812ec719f672
Krystian Cieślik <3641496+crabtree@users.noreply.github.com>
2021-01-19 09:13:25 +0100

    Remove production overrides to cli install call (#3139)

--- prow/scripts/lib/gardener/azure.sh
+++ prow/scripts/lib/gardener/azure.sh
@@ -165,7 +165,6 @@ gardener::install_kyma() {
             --ci \
             --source "${KYMA_SOURCE}" \
             -c "${INSTALLATION_RESOURCES_DIR}"/installer-cr-azure-eventhubs.yaml.tpl \
-            -o "${INSTALLATION_RESOURCES_DIR}"/installer-config-production.yaml.tpl \
             -o "${INSTALLATION_RESOURCES_DIR}"/installer-config-azure-eventhubs.yaml.tpl \
             -o "${EVENTHUB_SECRET_OVERRIDE_FILE}" \
             --timeout 60m \
