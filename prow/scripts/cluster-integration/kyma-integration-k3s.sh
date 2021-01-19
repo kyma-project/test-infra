c46e52de9ff52abfacbb70f54043d957c9c79a3e
Mateusz Puczynski <mateusz.puczynski@sap.com>
2021-01-19 16:52:21 +0100

    [WIP] test fast-integration with nodejs installer

--- prow/scripts/cluster-integration/kyma-integration-k3s.sh
+++ prow/scripts/cluster-integration/kyma-integration-k3s.sh
@@ -11,6 +11,7 @@ prereq_test() {
     command -v npm >/dev/null 2>&1 || { echo >&2 "npm not found"; exit 1; }
     command -v jq >/dev/null 2>&1 || { echo >&2 "jq not found"; exit 1; }
     command -v helm >/dev/null 2>&1 || { echo >&2 "helm not found"; exit 1; }
+    command -v kubectl >/dev/null 2>&1 || { echo >&2 "kubectl not found"; exit 1; }
 }
 
 install_kyma() {
@@ -22,10 +23,10 @@ install_kyma() {
     REGISTRY_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' /registry.localhost)
     echo "${REGISTRY_IP} registry.localhost" >> /etc/hosts
     
-    ./install-istio.sh -f config-istio.yaml
+    # ./install-istio.sh -f config-istio.yaml
     
-    REGISTRY_VALUES="dockerRegistry.enableInternal=false,dockerRegistry.serverAddress=registry.localhost:5000,dockerRegistry.registryAddress=registry.localhost:5000,containers.manager.envs.functionBuildExecutorImage.value=eu.gcr.io/kyma-project/external/aerfio/kaniko-executor:v1.3.0" \
-      ./install-kyma.sh
+    # REGISTRY_VALUES="dockerRegistry.enableInternal=false,dockerRegistry.serverAddress=registry.localhost:5000,dockerRegistry.registryAddress=registry.localhost:5000,containers.manager.envs.functionBuildExecutorImage.value=eu.gcr.io/kyma-project/external/aerfio/kaniko-executor:v1.3.0" \
+    #   ./install-kyma.sh
     
     popd
 }
@@ -33,8 +34,7 @@ install_kyma() {
 run_tests() {
     pushd "${KYMA_SOURCES_DIR}/tests/fast-integration"
 
-    npm install
-    npm test
+    make test
     
     popd
 }
