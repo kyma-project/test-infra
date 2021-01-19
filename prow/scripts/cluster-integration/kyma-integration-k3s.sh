3a411acad51b0bb910160e05e711685d842d1ede
Mateusz Puczynski <mateusz.puczynski@sap.com>
2021-01-19 17:40:39 +0100

    remove comments

--- prow/scripts/cluster-integration/kyma-integration-k3s.sh
+++ prow/scripts/cluster-integration/kyma-integration-k3s.sh
@@ -15,18 +15,11 @@ prereq_test() {
 }
 
 prepare_k3s() {
-    # mv "${KYMA_SOURCES_DIR}/resources" "${LOCAL_KYMA_DIR}/"
-
     pushd ${LOCAL_KYMA_DIR}
     ./create-cluster-k3s.sh
 
     REGISTRY_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' /registry.localhost)
     echo "${REGISTRY_IP} registry.localhost" >> /etc/hosts
-    
-    # ./install-istio.sh -f config-istio.yaml
-    
-    # REGISTRY_VALUES="dockerRegistry.enableInternal=false,dockerRegistry.serverAddress=registry.localhost:5000,dockerRegistry.registryAddress=registry.localhost:5000,containers.manager.envs.functionBuildExecutorImage.value=eu.gcr.io/kyma-project/external/aerfio/kaniko-executor:v1.3.0" \
-    #   ./install-kyma.sh
     
     popd
 }
