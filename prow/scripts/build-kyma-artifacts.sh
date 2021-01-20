582600966471ee3ed83771755dbd1c27bb4825d1
Adam Wałach <adam.walach@sap.com>
2021-01-20 10:00:20 +0100

    Fix build sh (#3154)

--- prow/scripts/build-kyma-artifacts.sh
+++ prow/scripts/build-kyma-artifacts.sh
@@ -9,32 +9,76 @@
 set -e
 
 readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# shellcheck disable=SC1090
-source "${SCRIPT_DIR}/library.sh"
+# shellcheck source=prow/scripts/lib/gcloud.sh
+source "${SCRIPT_DIR}/lib/gcloud.sh"
+# shellcheck source=prow/scripts/lib/docker.sh
+source "${SCRIPT_DIR}/lib/docker.sh"
 
-function export_variables() {
-   DOCKER_TAG=$(cat "${SCRIPT_DIR}/../RELEASE_VERSION")
-   echo "Reading docker tag from RELEASE_VERSION file, got: ${DOCKER_TAG}"
+# copy_artifacts copies artifacts to the destined bucket path.
+# it accepts one argument BUCKET_PATH which should be formatted as:
+# gs://bucket-name/bucket-folder
+function copy_artifacts {
+  BUCKET_PATH=$1
+  log::info "Copying artifacts to $BUCKET_PATH..."
 
-   readonly DOCKER_TAG
-   export DOCKER_TAG
+  gsutil cp  "installation/scripts/is-installed.sh" "$BUCKET_PATH/is-installed.sh"
+  gsutil cp "${ARTIFACTS}/kyma-installer-cluster.yaml" "$BUCKET_PATH/kyma-installer-cluster.yaml"
+  gsutil cp "${ARTIFACTS}/kyma-installer-cluster-runtime.yaml" "$BUCKET_PATH/kyma-installer-cluster-runtime.yaml"
+
+  gsutil cp "${ARTIFACTS}/kyma-config-local.yaml" "$BUCKET_PATH/kyma-config-local.yaml"
+  gsutil cp "${ARTIFACTS}/kyma-installer-local.yaml" "$BUCKET_PATH/kyma-installer-local.yaml"
+
+  gsutil cp "${ARTIFACTS}/kyma-installer.yaml" "$BUCKET_PATH/kyma-installer.yaml"
+  gsutil cp "${ARTIFACTS}/kyma-installer-cr-cluster.yaml" "$BUCKET_PATH/kyma-installer-cr-cluster.yaml"
+  gsutil cp "${ARTIFACTS}/kyma-installer-cr-local.yaml" "$BUCKET_PATH/kyma-installer-cr-local.yaml"
+  gsutil cp "${ARTIFACTS}/kyma-installer-cr-cluster-runtime.yaml" "$BUCKET_PATH/kyma-installer-cr-cluster-runtime.yaml"
 }
 
-init
-export_variables
+gcloud::authenticate "${GOOGLE_APPLICATION_CREDENTIALS}"
+docker::start
 
-make -C /home/prow/go/src/github.com/kyma-project/kyma/tools/kyma-installer ci-create-release-artifacts
+if [ -n "${PULL_NUMBER}" ]; then
+  DOCKER_TAG="PR-${PULL_NUMBER}"
+elif [[ "${PULL_BASE_REF}" =~ ^release-.* ]]; then
+  DOCKER_TAG=$(cat "${SCRIPT_DIR}/../RELEASE_VERSION")
+  if [[ "${REPO_OWNER}" == "kyma-project" && "${REPO_NAME}" == "kyma" ]]; then
+    NEXT_RELEASE="$DOCKER_TAG"
+    echo "Checking if ${NEXT_RELEASE} was already published on github..."
+    RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" https://api.github.com/repos/kyma-project/kyma/releases/tags/"${NEXT_RELEASE}")
+    if [[ $RESPONSE != 404* ]]; then
+        echo "The ${NEXT_RELEASE} is already published on github. Stopping."
+        exit 1
+    fi
+  fi
+else
+  DOCKER_TAG="${PULL_BASE_SHA::8}"
+fi
+export DOCKER_TAG
+echo "DOCKER_TAG: ${DOCKER_TAG}"
 
-gsutil cp "${ARTIFACTS}/kyma-installer-cluster.yaml" "${KYMA_ARTIFACTS_BUCKET}/${DOCKER_TAG}/kyma-installer-cluster.yaml"
-gsutil cp "${ARTIFACTS}/kyma-installer-cluster-runtime.yaml" "${KYMA_ARTIFACTS_BUCKET}/${DOCKER_TAG}/kyma-installer-cluster-runtime.yaml"
+log::info "Building kyma-installer"
+make -C "tools/kyma-installer" release
 
-gsutil cp "${ARTIFACTS}/kyma-config-local.yaml" "${KYMA_ARTIFACTS_BUCKET}/${DOCKER_TAG}/kyma-config-local.yaml"
-gsutil cp "${ARTIFACTS}/kyma-installer-local.yaml" "${KYMA_ARTIFACTS_BUCKET}/${DOCKER_TAG}/kyma-installer-local.yaml"
+log::info "Create Kyma artifacts"
+if [[ -n "${PULL_NUMBER}" ]] && [[ "${PULL_BASE_REF}" =~ ^release-.* ]]; then
+  # work only on presubmit release branch.
+  log::info "workaround for release presubmits - rollback release kyma-installer to develop for the PRs"
+  cp "installation/resources/installer.yaml" "/tmp/installer.tpl.yaml"
+  sed -E ";s;image: eu.gcr.io\/kyma-project\/kyma-installer:.+;image: eu.gcr.io\/kyma-project\/develop\/installer:latest;" < "/tmp/installer.tpl.yaml" > "installation/resources/installer.yaml"
+fi
+env KYMA_INSTALLER_VERSION="${DOCKER_TAG}" ARTIFACTS_DIR="${ARTIFACTS}" "installation/scripts/release-generate-kyma-installer-artifacts.sh"
 
-gsutil cp "${ARTIFACTS}/kyma-installer.yaml" "${KYMA_ARTIFACTS_BUCKET}/${DOCKER_TAG}/kyma-installer.yaml"
-gsutil cp "${ARTIFACTS}/kyma-installer-cr-cluster.yaml" "${KYMA_ARTIFACTS_BUCKET}/${DOCKER_TAG}/kyma-installer-cr-cluster.yaml"
-gsutil cp "${ARTIFACTS}/kyma-installer-cr-local.yaml" "${KYMA_ARTIFACTS_BUCKET}/${DOCKER_TAG}/kyma-installer-cr-local.yaml"
-gsutil cp "${ARTIFACTS}/kyma-installer-cr-cluster-runtime.yaml" "${KYMA_ARTIFACTS_BUCKET}/${DOCKER_TAG}/kyma-installer-cr-cluster-runtime.yaml"
+log::info "Content of the local artifacts directory"
+ls -la "${ARTIFACTS}"
+gcloud::authenticate "$SA_KYMA_ARTIFACTS_GOOGLE_APPLICATION_CREDENTIALS"
 
-
-"${SCRIPT_DIR}"/changelog-generator.sh
+if [ -n "$PULL_NUMBER" ]; then
+  copy_artifacts "${KYMA_DEVELOPMENT_ARTIFACTS_BUCKET}/${DOCKER_TAG}"
+elif [[ "$PULL_BASE_REF" =~ ^release-.* ]]; then
+  copy_artifacts "${KYMA_ARTIFACTS_BUCKET}/${DOCKER_TAG}"
+  # TODO this script needs to be revisited for future improvements...
+  "${SCRIPT_DIR}"/changelog-generator.sh
+else
+  copy_artifacts "${KYMA_DEVELOPMENT_ARTIFACTS_BUCKET}/master-${DOCKER_TAG}"
+  copy_artifacts "${KYMA_DEVELOPMENT_ARTIFACTS_BUCKET}/master"
+fi
