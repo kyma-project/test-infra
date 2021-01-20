095bda332dbec9f97b8cb1623cae1383e3c0b703
Tobias Schuhmacher <73824711+tobiscr@users.noreply.github.com>
2021-01-20 17:13:23 +0100

    Remove unncessary overrides

--- prow/scripts/lib/cli-alpha.sh
+++ prow/scripts/lib/cli-alpha.sh
@@ -2,14 +2,6 @@
 
 # cli-alpha::deploy starts Kyma installation using the alpha deploy command
 #
-# Arguments:
-#	$1 - OPtional path to an overrides file
 function cli-alpha::deploy {
-	# shellcheck disable=SC2119
-	local overrides=$1
-
-	kyma alpha deploy \
-		-v \
-		"$( if [ -n "$overrides" ]; then echo "-f $overrides"; fi )"
-
+	kyma alpha deploy -v
 }
