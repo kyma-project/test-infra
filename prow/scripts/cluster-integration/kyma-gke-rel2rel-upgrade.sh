1bbf577a97ffccf6fe7004baeb4b1f93f4f50eed
Kamil Kasperski <kamil.kasperski@sap.com>
2021-01-27 11:59:17 +0100

    Bump kyma previous version to 1.17.1 in kyma-gke-rel2rel-upgrade.sh

--- prow/scripts/cluster-integration/kyma-gke-rel2rel-upgrade.sh
+++ prow/scripts/cluster-integration/kyma-gke-rel2rel-upgrade.sh
@@ -117,7 +117,7 @@ downloadAssets() {
 
     #SOURCE_VERSION=$(getSourceVersion)
     #SOURCE_VERSION=$(getLastRCVersion)
-    SOURCE_VERSION="1.16.0-rc3"
+    SOURCE_VERSION="1.17.1"
     TARGET_VERSION="${PULL_BASE_REF}"
 
     shout "Upgrade from ${SOURCE_VERSION} to ${TARGET_VERSION}"
