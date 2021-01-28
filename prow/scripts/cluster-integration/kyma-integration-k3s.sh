774682b12875c27803d93445b38b458dd9c72253
Mateusz Puczynski <mateusz.puczynski@sap.com>
2021-01-28 12:10:36 +0100

    Migrate to use Makefile in fast-integration-tests

--- prow/scripts/cluster-integration/kyma-integration-k3s.sh
+++ prow/scripts/cluster-integration/kyma-integration-k3s.sh
@@ -11,25 +11,33 @@ prereq_test() {
     command -v npm >/dev/null 2>&1 || { echo >&2 "npm not found"; exit 1; }
     command -v jq >/dev/null 2>&1 || { echo >&2 "jq not found"; exit 1; }
     command -v helm >/dev/null 2>&1 || { echo >&2 "helm not found"; exit 1; }
-    command -v kubectl >/dev/null 2>&1 || { echo >&2 "kubectl not found"; exit 1; }
 }
 
-prepare_k3s() {
+install_kyma() {
+    mv "${KYMA_SOURCES_DIR}/resources" "${LOCAL_KYMA_DIR}/"
+
     pushd ${LOCAL_KYMA_DIR}
     ./create-cluster-k3s.sh
 
     REGISTRY_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' /registry.localhost)
     echo "${REGISTRY_IP} registry.localhost" >> /etc/hosts
     
+    ./install-istio.sh -f config-istio.yaml
+    
+    REGISTRY_VALUES="dockerRegistry.enableInternal=false,dockerRegistry.serverAddress=registry.localhost:5000,dockerRegistry.registryAddress=registry.localhost:5000,containers.manager.envs.functionBuildExecutorImage.value=eu.gcr.io/kyma-project/external/aerfio/kaniko-executor:v1.3.0" \
+      ./install-kyma.sh
+    
     popd
 }
 
 run_tests() {
     pushd "${KYMA_SOURCES_DIR}/tests/fast-integration"
+
     make ci
+    
     popd
 }
 
 prereq_test
-prepare_k3s
+install_kyma
 run_tests
