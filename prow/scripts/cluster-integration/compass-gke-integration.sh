de748c46ff3c8a2a4aef5b56099338ac686fbf92
i356426 <dimitar.petrov@sap.com>
2021-01-29 15:27:21 +0200

    Fix compass-gke-integration

--- prow/scripts/cluster-integration/compass-gke-integration.sh
+++ prow/scripts/cluster-integration/compass-gke-integration.sh
@@ -347,7 +347,7 @@ log::info "Install Compass"
 installCompass
 
 log::info "Test Kyma with Compass"
-"${TEST_INFRA_SOURCES_DIR}"/prow/scripts/kyma-testing.sh
+CONCURRENCY=1 "${TEST_INFRA_SOURCES_DIR}"/prow/scripts/kyma-testing.sh
 
 log::success "Success"
 
