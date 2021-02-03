a1da8eb1ccc18c4ebbab89d4e636abdecbe9e550
Kamil Kasperski <kamil.kasperski@sap.com>
2021-02-03 14:40:25 +0100

    Rework build-generic.sh: add ability to define build targets

--- prow/scripts/build-generic.sh
+++ prow/scripts/build-generic.sh
@@ -9,14 +9,15 @@ source "${SCRIPT_DIR}/lib/gcloud.sh"
 source "${SCRIPT_DIR}/lib/docker.sh"
 
 usage () {
-    echo "Usage: \$ ${BASH_SOURCE[1]} /path/to/component"
+    echo "Usage: \$ ${BASH_SOURCE[1]} /path/to/component [Makefile targets]"
     exit 1
 }
 
-readonly SOURCES_DIR=$1
-
-if [[ -z "${SOURCES_DIR}" ]]; then
-    usage
+# get first argument and assume it's a path to the sources.
+readonly SOURCES_DIR=$1; shift
+if [[ ! -d "${SOURCES_DIR}" ]]; then
+  echo -e "Error: Directory \"$SOURCES_DIR\" does not exist."
+  usage
 fi
 
 gcloud::authenticate "${GOOGLE_APPLICATION_CREDENTIALS}"
@@ -34,4 +35,9 @@ fi
 export DOCKER_TAG
 echo DOCKER_TAG "${DOCKER_TAG}"
 
-make -C "${SOURCES_DIR}" release
+# Adding script argument checking allows to define custom build targets because `ci-release` is not in several Makefiles.
+if [ -n "$1" ]; then
+  make -C "${SOURCES_DIR}" "$@"
+else
+  make -C "${SOURCES_DIR}" release
+fi
