20e545a1cae483b89a4d67169af6dd9c6a8e908d
Piotr Halama <piotr.halama@sap.com>
2021-02-16 14:33:35 +0100

    build-generic: add time to output

--- prow/scripts/build-generic.sh
+++ prow/scripts/build-generic.sh
@@ -37,7 +37,7 @@ echo DOCKER_TAG "${DOCKER_TAG}"
 
 # Adding script argument checking allows to define custom build targets because `ci-release` is not in several Makefiles.
 if [ -n "$1" ]; then
-  make -C "${SOURCES_DIR}" "$@"
+  make -C "${SOURCES_DIR}" "$@" | while read line ; do echo "$(date +"%Y/%m/%d %T %Z")| $line"; done;
 else
-  make -C "${SOURCES_DIR}" release
+  make -C "${SOURCES_DIR}" release | while read line ; do echo "$(date +"%Y/%m/%d %T %Z")| $line"; done;
 fi
