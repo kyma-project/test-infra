e69dd86392ae49d9ab123928f6f304b2a2808225
Borja Clemente <clebs@users.noreply.github.com>
2021-02-18 10:55:50 +0100

    Sort releases in compatibility job (#3301)
    It is possible to have a patch release after a higher major or minor release.
    Sorting the releases fixes this issue when backtracking for compatibility.
    
    Signed-off-by: Borja Clemente <borja.clemente.castanera@sap.com>

--- prow/scripts/compatibility-cli.sh
+++ prow/scripts/compatibility-cli.sh
@@ -43,6 +43,9 @@ RELEASES=${RELEASES//\-rc[0-9]}
 # Split into array
 RELEASES=(${RELEASES})
 
+# sort the releases in case there was a patch release after another higher minor release (e.g. chronologically: 1.0.1, 1.1.0, 1.0.0)
+RELEASES=($(printf "%s\n" "${RELEASES[@]}" | sort -r))
+
 # Remove duplicates
 RELEASES=($(printf "%s\n" "${RELEASES[@]}" | uniq))
 
