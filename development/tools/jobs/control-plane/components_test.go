152c4d0f8e64378d9e5ce3b30bfa4e1272ca9b0b
Piotr Halama <piotr.halama@sap.com>
2021-02-23 14:22:02 +0100

    tests

--- development/tools/jobs/control-plane/components_test.go
+++ development/tools/jobs/control-plane/components_test.go
@@ -20,7 +20,7 @@ var components = []struct {
 }{
 	{
 		name:  "provisioner",
-		image: tester.ImageBootstrapTestInfraLatest,
+		image: tester.ImageGolangToolboxLatest,
 		suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("generic"),
@@ -30,7 +30,7 @@ var components = []struct {
 	},
 	{
 		name:  "kyma-environment-broker",
-		image: tester.ImageBootstrapTestInfraLatest,
+		image: tester.ImageGolangToolboxLatest,
 		suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("generic"),
@@ -40,7 +40,7 @@ var components = []struct {
 	},
 	{
 		name:  "schema-migrator",
-		image: tester.ImageBootstrapTestInfraLatest,
+		image: tester.ImageGolangToolboxLatest,
 		suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("kcp-generic"),
@@ -50,7 +50,7 @@ var components = []struct {
 	},
 	{
 		name:  "metris",
-		image: tester.ImageBootstrapTestInfraLatest,
+		image: tester.ImageGolangToolboxLatest,
 		suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("generic"),
@@ -60,7 +60,7 @@ var components = []struct {
 	},
 	{
 		name:  "kubeconfig-service",
-		image: tester.ImageBootstrapTestInfraLatest,
+		image: tester.ImageGolangToolboxLatest,
 		suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("generic"),
@@ -70,7 +70,7 @@ var components = []struct {
 	},
 	{
 		name:  "subscription-cleanup-job",
-		image: tester.ImageBootstrapTestInfraLatest,
+		image: tester.ImageGolangToolboxLatest,
 		suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("generic"),
