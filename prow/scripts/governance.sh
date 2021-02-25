0b6f5b9130a1d55ce8db16be1670e0c2a7dfdc65
Tomasz Gorgol <t.gorgol@sap.com>
2021-02-25 13:00:01 +0100

    Switch used MILV image to in-house version

--- prow/scripts/governance.sh
+++ prow/scripts/governance.sh
@@ -9,7 +9,7 @@ source "${SCRIPT_DIR}/lib/docker.sh"
 source "${SCRIPT_DIR}/lib/log.sh"
 
 readonly ARGS=("$@")
-readonly MILV_IMAGE="eu.gcr.io/kyma-project/external/magicmatatjahu/milv:0.0.7-beta"
+readonly MILV_IMAGE="eu.gcr.io/kyma-project/incubator/milv:056a4dae"
 VOLUME_DIR=""
 OUTPUT=0
 
