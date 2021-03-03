e6e385d1ebe2b56eca92f3296a272eb70a27d5ec
Piotr Halama <piotr.halama@sap.com>
2021-03-03 11:52:43 +0100

    set

--- prow/scripts/cluster-integration/kyma-integration-gardener.sh
+++ prow/scripts/cluster-integration/kyma-integration-gardener.sh
@@ -129,10 +129,8 @@ utils::save_psp_list "${ARTIFACTS}/kyma-psp.json"
 # TODO make this less ugly
 if [[ -n "$CLOUDSDK_CORE_PROJECT" ]]; then
     log::info saving logs to "gardener-${COMMON_NAME}"
-    set -x
     pid_logs=$(kubectl logs view-pid)
     gcloud logging write "gardener-${COMMON_NAME}" "$pid_logs"
-    set +x
 else
     log::warn "CLOUDSDK_CORE_PROJECT not set, not sending logs"
 fi
