3084ec0f3193d2f52dc5aad673eacab6bff557a0
Kwiatosz, Krzysztof <kwiatekus@users.noreply.github.com>
2021-03-10 17:21:28 +0100

    init pj test

--- prow/scripts/cluster-integration/kyma2-integration-k3s.sh
+++ prow/scripts/cluster-integration/kyma2-integration-k3s.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+export KYMA_SOURCES_DIR=~/go/src/github.com/kyma-project/kyma
+export DEBUG=1
+# export OIDC_ISSUER=$1
+# export OIDC_CLIENT_ID=$2
 
+# ./create-cluster-k3d-oidc.sh https://apskyxzcl.accounts400.ondemand.com 92dc3801-576b-465b-82ad-f69ed244d1e7
 set -o errexit
 set -o pipefail
 
-export KYMA_SOURCES_DIR="./kyma"
+# export KYMA_SOURCES_DIR="./kyma"
 
 prereq_test() {
     command -v node >/dev/null 2>&1 || { echo >&2 "node not found"; exit 1; }
@@ -20,22 +25,42 @@ load_env() {
     fi
 }
 
+prepare_cli(){
+    log::info "Building Kyma CLI"
+    cd "${KYMA_PROJECT_DIR}/cli"
+    make build-linux
+    mv "${KYMA_PROJECT_DIR}/cli/bin/kyma-linux" "${KYMA_PROJECT_DIR}/cli/bin/kyma"
+    export PATH="${KYMA_PROJECT_DIR}/cli/bin:${PATH}"
+}
+
 prepare_k3s() {
-    echo "starting cluster"
-    # TODO pass OIDC setup 
     pushd "${KYMA_SOURCES_DIR}/tests/fast-integration/hack"
-    ./create-cluster-k3d.sh
+    ./create-cluster-k3d-oidc.sh https://apskyxzcl.accounts400.ondemand.com 92dc3801-576b-465b-82ad-f69ed244d1e7
     popd
+    # kyma alpha provision k3s --name "kyma" --server-args "--kube-apiserver-arg=oidc-issuer-url=${OIDC_ISSUER}" --server-args "--kube-apiserver-arg=oidc-client-id=${OIDC_CLIENT_ID}" --server-args "--kube-apiserver-arg=oidc-username-claim=sub" --server-args "--kube-apiserver-arg=oidc-username-prefix=-" --server-args "--kube-apiserver-arg=oidc-groups-claim=groups"
 }
 
 install_kyma() {
+    pushd "${PWD}"
+    kyma alpha deploy -w ${KYMA_SOURCES_DIR} -s local -p evaluation -f ./values.yaml --non-interactive
+    popd
+}
+
+patch_coredns(){
+    export REGISTRY_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' /registry.localhost) 
+    kubectl -n kube-system patch cm coredns --patch "$(cat k3d-coredns-patch.yaml | envsubst )"
+}
+
+test_kyma() {
     pushd "${KYMA_SOURCES_DIR}/tests/fast-integration"
-    npm install
-    ./kyma.js install 
+    npm run test-no-install
     popd
 }
 
 prereq_test
 load_env
+prepare_cli
 prepare_k3s
 install_kyma
+patch_coredns
+# test_kyma
