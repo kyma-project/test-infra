a3955e909517bf707ea1ff50806ab761fec4a135
Piotr Halama <piotr.halama@sap.com>
2021-03-29 14:15:30 +0200

    Main branch: rename for website, community and examples repos

--- development/tools/jobs/governance/website_governance_test.go
+++ development/tools/jobs/governance/website_governance_test.go
@@ -53,7 +53,7 @@ func TestWebsiteGovernanceJobPeriodic(t *testing.T) {
 	assert.True(t, actualPeriodic.Decorate)
 	assert.Equal(t, "0 6 * * 1-5", actualPeriodic.Cron)
 	tester.AssertThatHasPresets(t, actualPeriodic.JobBase, preset.DindEnabled)
-	tester.AssertThatHasExtraRepoRefCustom(t, actualPeriodic.JobBase.UtilityConfig, []string{"test-infra", "website"}, []string{"main", "master"})
+	tester.AssertThatHasExtraRepoRefCustom(t, actualPeriodic.JobBase.UtilityConfig, []string{"test-infra", "website"}, []string{"main", "main"})
 	assert.Equal(t, tester.ImageBootstrapTestInfraLatest, actualPeriodic.Spec.Containers[0].Image)
 	assert.Equal(t, []string{tester.GovernanceScriptDir}, actualPeriodic.Spec.Containers[0].Command)
 	repositoryDirArg := fmt.Sprintf("%s/website", tester.KymaProjectDir)
