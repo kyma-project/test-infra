c02f3235f2b76218f48977c9db6c320cd4f3a104
Piotr Halama <piotr.halama@sap.com>
2021-04-01 11:26:43 +0200

    warn

--- prow/scripts/cluster-integration/helpers/cleanup-gardener.sh
+++ prow/scripts/cluster-integration/helpers/cleanup-gardener.sh
@@ -79,6 +79,6 @@ do
             utils::deprovision_gardener_cluster "${GARDENER_KYMA_PROW_PROJECT_NAME}" "${CLUSTER}" "${GARDENER_KYMA_PROW_KUBECONFIG}"
         fi
     else
-        echo " level=warning msg=\“Cluster is excluded, deletion will be skipped. Name: \"${CLUSTER}\""
+        echo "level=warning msg=\“Cluster is excluded, deletion will be skipped. Name: \"${CLUSTER}\""
     fi
 done
