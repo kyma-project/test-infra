24a0096ef25911c2908298fc11041a25e8e7bc24
Piotr Halama <piotr.halama@sap.com>
2021-04-01 10:50:04 +0200

    Gardener cleanup: add option to exclude clusters

--- prow/scripts/cluster-integration/helpers/cleanup-gardener.sh
+++ prow/scripts/cluster-integration/helpers/cleanup-gardener.sh
@@ -9,7 +9,8 @@
 # - KYMA_PROJECT_DIR - directory path with Kyma sources to use for importing prow scripts
 # - GARDENER_KYMA_PROW_KUBECONFIG - Kubeconfig of the Gardener service account of the project
 # - GARDENER_KYMA_PROW_PROJECT_NAME Name of the gardener project where the clusters will be cleaned up.
-
+# Arguments:
+# --excluded-clusters -  list of comma separated clusters that won't get removed
 readonly SECONDS_PER_HOUR=3600
 
 set -e
@@ -31,6 +32,26 @@ requiredVars=(
 
 utils::check_required_vars "${requiredVars[@]}"
 
+EXCLUDED_CLUSTERS=""
+while [[ $# -gt 0 ]]
+do
+
+    key="$1"
+
+    case ${key} in
+        --excluded-clusters)
+            EXCLUDED_CLUSTERS="$2"
+            shift
+            shift
+            ;;
+        *)
+            echo "Unknown flag ${1}"
+            exit 1
+            ;;
+    esac
+done
+EXCLUDED_CLUSTERS=(${EXCLUDED_CLUSTERS//,/ }) # convert comma separated clusters string to array
+
 echo "--------------------------------------------------------------------------------"
 echo "Removing Gardener clusters allocated by failed/terminated integration jobs...  "
 echo "--------------------------------------------------------------------------------"
@@ -43,18 +64,21 @@ CLUSTERS=(${CLUSTERS//,/ }) # convert comma separated clusters string to array
 # cleanup all clusters
 for CLUSTER in "${CLUSTERS[@]}"
 do
-    # check cluster age
-    # shellcheck disable=SC2016
-    CREATION_TIME="$(kubectl --kubeconfig "${GARDENER_KYMA_PROW_KUBECONFIG}" -n garden-"${GARDENER_KYMA_PROW_PROJECT_NAME}" get shoots "$CLUSTER" -o go-template='{{.metadata.creationTimestamp}}')"
+    omit_cluster=$(utils::check_if_in_list "$CLUSTER" "${EXCLUDED_CLUSTERS[@]}")
+    if [ "$omit_cluster" = "false" ]; then
+        # check cluster age
+        # shellcheck disable=SC2016
+        CREATION_TIME="$(kubectl --kubeconfig "${GARDENER_KYMA_PROW_KUBECONFIG}" -n garden-"${GARDENER_KYMA_PROW_PROJECT_NAME}" get shoots "$CLUSTER" -o go-template='{{.metadata.creationTimestamp}}')"
 
-    # convert to timestamp for age calculation
-    CREATION_TS="$(date -d "${CREATION_TIME}" +%s)" # On macOS use: CREATION_TS=$(date -jf "%Y-%m-%dT%H:%M:%SZ" ${CREATION_TIME} +%s)
-    NOW_TS="$(date +%s)"
-    HOURS_OLD=$(( (NOW_TS - CREATION_TS) / SECONDS_PER_HOUR ))
-    
-    # clusters older than 4h get deleted
-    if [[ ${HOURS_OLD} -ge 4 ]]; then
-        log::info "Deprovision cluster: \"${CLUSTER}\" (${HOURS_OLD}h old)"
-        utils::deprovision_gardener_cluster "${GARDENER_KYMA_PROW_PROJECT_NAME}" "${CLUSTER}" "${GARDENER_KYMA_PROW_KUBECONFIG}"
+        # convert to timestamp for age calculation
+        CREATION_TS="$(date -d "${CREATION_TIME}" +%s)" # On macOS use: CREATION_TS=$(date -jf "%Y-%m-%dT%H:%M:%SZ" ${CREATION_TIME} +%s)
+        NOW_TS="$(date +%s)"
+        HOURS_OLD=$(( (NOW_TS - CREATION_TS) / SECONDS_PER_HOUR ))
+        
+        # clusters older than 4h get deleted
+        if [[ ${HOURS_OLD} -ge 4 ]]; then
+            log::info "Deprovision cluster: \"${CLUSTER}\" (${HOURS_OLD}h old)"
+            utils::deprovision_gardener_cluster "${GARDENER_KYMA_PROW_PROJECT_NAME}" "${CLUSTER}" "${GARDENER_KYMA_PROW_KUBECONFIG}"
+        fi
     fi
 done
