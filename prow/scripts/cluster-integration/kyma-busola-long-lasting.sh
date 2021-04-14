54bfef12a69917b7baf15f9ba613ccea0e398760
Piotr Halama <piotr.halama@sap.com>
2021-04-14 11:44:37 +0200

    Busola: fix long lasting (#3543)
    * Busola: fix long lasting
    
    * rename flag

--- prow/scripts/cluster-integration/kyma-busola-long-lasting.sh
+++ prow/scripts/cluster-integration/kyma-busola-long-lasting.sh
@@ -99,7 +99,7 @@ function provisionKyma2(){
     --profile=evaluation \
     --source="${KYMA_VERSION}" \
     --value global.environment.gardener=true \
-    --workers-count=4
+    --concurrency=4
 
 }
 
@@ -155,7 +155,7 @@ export KUBECONFIG="${GARDENER_KYMA_PROW_KUBECONFIG}"
 if [[ $BUSOLA_PROVISION_TYPE == "KYMA" ]]; then
     log::info "Kyma cluster name: ${KYMA_COMMON_NAME}"
     delete_cluster "${KYMA_COMMON_NAME}"
-    provisionKyma2 "master" "${KYMA_COMMON_NAME}"
+    provisionKyma2 "main" "${KYMA_COMMON_NAME}"
 elif [[ $BUSOLA_PROVISION_TYPE == "BUSOLA" ]]; then
     log::info "Busola cluster name: ${BUSOLA_COMMON_NAME}"
     delete_cluster "${BUSOLA_COMMON_NAME}"
