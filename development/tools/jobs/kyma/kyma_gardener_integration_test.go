1d93f3c4c6bf0728fed311d171adbaace5ee8402
Julia Iskierka <jul.iskierka@gmail.com>
2021-04-22 13:00:02 +0200

    Fixes

--- development/tools/jobs/kyma/kyma_gardener_integration_test.go
+++ development/tools/jobs/kyma/kyma_gardener_integration_test.go
@@ -25,7 +25,7 @@ func TestKymaGardenerGCPKymaToKyma2JobPeriodics(t *testing.T) {
 	assert.Equal(t, "0 0 7-17 ? * 1-5", job.Cron)
 	assert.Equal(t, job.DecorationConfig.Timeout.Get(), 4*time.Hour)
 	assert.Equal(t, job.DecorationConfig.GracePeriod.Get(), 10*time.Minute)
-	tester.AssertThatHasPresets(t, job.JobBase, preset.GardenerGCPIntegration, preset.KymaCLIStable, preset.ClusterVersion)
+	tester.AssertThatHasPresets(t, job.JobBase, preset.GardenerAzureIntegration, preset.KymaCLIStable, preset.ClusterVersion)
 	tester.AssertThatHasExtraRepoRefCustom(t, job.JobBase.UtilityConfig, []string{"test-infra", "kyma"}, []string{"main", "main"})
 	assert.Equal(t, tester.ImageKymaIntegrationLatest, job.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"-c", "${KYMA_PROJECT_DIR}/test-infra/prow/scripts/cluster-integration/kyma-upgrade-gardener.sh"}, job.Spec.Containers[0].Args)
@@ -56,6 +56,8 @@ func TestKymaGardenerAzureIntegrationJobPeriodics(t *testing.T) {
 	tester.AssertThatContainerHasEnv(t, job.Spec.Containers[0], "GARDENER_REGION", "northeurope")
 	tester.AssertThatContainerHasEnv(t, job.Spec.Containers[0], "GARDENER_ZONES", "1")
 	tester.AssertThatContainerHasEnv(t, job.Spec.Containers[0], "KYMA_PROJECT_DIR", "/home/prow/go/src/github.com/kyma-project")
+	tester.AssertThatContainerHasEnv(t, job.Spec.Containers[0], "REGION", "northeurope")
+	tester.AssertThatContainerHasEnv(t, job.Spec.Containers[0], "RS_GROUP", "kyma-gardener-azure")
 	tester.AssertThatSpecifiesResourceRequests(t, job.JobBase)
 
 	jobName = "kyma-alpha-integration-evaluation-gardener-azure"
