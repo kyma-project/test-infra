d1df126b8b3c60d84b3889078898af32a961bdea
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2021-05-14 16:08:39 +0200

    debug kyma 2.0 upgrade

--- prow/scripts/cluster-integration/kyma-upgrade-gardener.sh
+++ prow/scripts/cluster-integration/kyma-upgrade-gardener.sh
@@ -144,7 +144,7 @@ gardener::post_upgrade_test_fast_integration_kyma
 set -e
 
 log::info "Sleeping for 5 minutes..."
-sleep 303
+sleep 304
 log::info "Done sleeping!"
 
 #!!! Must be at the end of the script !!!
