58ab013a709e8f1ec1c68d2c0272d46ba04589cc
Piotr Halama <piotr.halama@sap.com>
2021-05-19 15:12:21 +0200

    welp

--- prow/scripts/cluster-integration/kyma-busola-long-lasting.sh
+++ prow/scripts/cluster-integration/kyma-busola-long-lasting.sh
@@ -150,8 +150,8 @@ function deleteKyma(){
 
     # This can be deleted when it's implemented by installer
     # remove CRDs
-    log::info "removing CRDs"
-    kubectl api-resources --verbs=list --namespaced -o name | grep kyma-project.io | sed -e 's/.*/kubectl delete crd & --grace-period 0 --force=true --wait=false/ ' | sh
+    # log::info "removing CRDs"
+    # kubectl api-resources --verbs=list --namespaced -o name | grep kyma-project.io | sed -e 's/.*/kubectl delete crd & --force=true --wait=false/ ' | sh
     
     log::info "Cluster deleted"
 }
