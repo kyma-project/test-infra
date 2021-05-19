5df5aec3a17240af94a39d72ce97aa3752550add
Piotr Halama <piotr.halama@sap.com>
2021-05-19 14:55:11 +0200

    Comment out CRDs

--- prow/scripts/cluster-integration/kyma-busola-long-lasting.sh
+++ prow/scripts/cluster-integration/kyma-busola-long-lasting.sh
@@ -152,12 +152,16 @@ function deleteKyma(){
     # This can be deleted when it's implemented by installer
     # remove CRDs
 <<<<<<< HEAD
+<<<<<<< HEAD
     # kubectl api-resources --verbs=list --namespaced -o name | grep kyma-project.io | sed -e 's/.*/kubectl delete crd & --force=true --wait=false/ ' | sh
     # kubectl delete namespace kyma-system --wait=true
     # log::info "Cluster deleted"
 
 =======
     kubectl api-resources --verbs=list --namespaced -o name | grep kyma-project.io | sed -e 's/.*/kubectl delete crd & --force=true --wait=false/ ' | sh
+=======
+    # kubectl api-resources --verbs=list --namespaced -o name | grep kyma-project.io | sed -e 's/.*/kubectl delete crd & --force=true --wait=false/ ' | sh
+>>>>>>> 68d26a37 (Comment out CRDs)
     
     log::info "Cluster deleted"
 >>>>>>> 8f5373a2 (that shoudl be doen by cli)
