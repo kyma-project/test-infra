8bd2e845e5cbfbf827fcaa29b0a30e2113d0c03e
Piotr Halama <piotr.halama@sap.com>
2021-05-21 10:54:46 +0200

    split

--- prow/scripts/github-issues.sh
+++ prow/scripts/github-issues.sh
@@ -8,7 +8,7 @@ set -e
 "--bqDataset" "github_issues" \
 "--bqTable" "github_com_kyma_project" \
 "--bqCredentials" "${GOOGLE_APPLICATION_CREDENTIALS}" \
-"--githubToken" "${BOT_GITHUB_TOKEN}" \
+"--githubToken" "${BOT_GITHUB_TOKEN}"
 
 
 /prow-tools/githubissues \
