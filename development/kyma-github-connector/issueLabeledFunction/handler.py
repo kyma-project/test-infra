c13a79b925f9e144f4b9fa5baeaf311e802ec766
dekiel <przemyslaw.pokrywka@sap.com>
2021-05-27 18:11:27 +0200

    gh-connector prod values

--- development/kyma-github-connector/issueLabeledFunction/handler.py
+++ development/kyma-github-connector/issueLabeledFunction/handler.py
@@ -14,8 +14,6 @@ def main(event, context):
 	)
 	print("Using Slack api base URL: {}".format(app.client.base_url))
 	print("Sending notifications to channel: {}".format(os.environ['NOTIFICATION_SLACK_CHANNEL']))
-	# Get cloud events data.
-	#msg = json.loads(base64.b64decode(event["data"]))
 	msg = event["data"]
 	label = msg["label"]["name"]
 	title = msg["issue"]["title"]
@@ -30,7 +28,7 @@ def main(event, context):
 	issue_url = msg["issue"]["html_url"]
 	# Run only for internal-incident and customer-incident labels
 	if (label == "internal-incident") or (label == "customer-incident"):
-		print("sending notification to channel: {}".format(os.environ['NOTIFICATION_SLACK_CHANNEL']))
+		print("label matched, sending notification")
 		try:
 			# Deliver message to the channel.
 			result = app.client.chat_postMessage(channel=os.environ['NOTIFICATION_SLACK_CHANNEL'],
@@ -71,6 +69,5 @@ def main(event, context):
 			assert result["ok"]
 			print("sent notification for issue #{}".format(number))
 		except SlackApiError as e:
-			assert result["ok"] is False
 			print(f"Got an error: {e.response['error']}")
 			print("failed sent notification for issue #{}".format(number))
