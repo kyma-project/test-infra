2f28d180245b1fe7575f9908b65e555fa7bb8e57
dekiel <przemyslaw.pokrywka@sap.com>
2021-06-01 16:06:28 +0200

    force kyma provision to fail

--- prow/scripts/lib/gardener/gcp.sh
+++ prow/scripts/lib/gardener/gcp.sh
@@ -88,7 +88,8 @@ gardener::provision_cluster() {
             --project "${GARDENER_KYMA_PROW_PROJECT_NAME}" --credentials "${GARDENER_KYMA_PROW_KUBECONFIG}" \
             --region "${GARDENER_REGION}" -z "${GARDENER_ZONES}" -t "${MACHINE_TYPE}" \
             --scaler-max 4 --scaler-min 2 \
-            --kube-version="${GARDENER_CLUSTER_VERSION}" || false
+            --kube-version="${GARDENER_CLUSTER_VERSION}"
+    false
     trap - ERR
     )
 
