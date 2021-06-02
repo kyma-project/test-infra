1612745ddff15a371af9d26d6eaff263dd90fc04
Valentin Vieriu <valentin.vieriu@gmail.com>
2021-06-02 06:43:31 +0200

    testing nt8

--- prow/scripts/cluster-integration/kyma-busola-long-lasting.sh
+++ prow/scripts/cluster-integration/kyma-busola-long-lasting.sh
@@ -196,7 +196,7 @@ else
 fi
 
 if [ -z "$COMMON_NAME_PREFIX" ] ; then
-    COMMON_NAME_PREFIX="nt7"
+    COMMON_NAME_PREFIX="nt8"
 fi
 readonly KYMA_NAME_SUFFIX="kyma"
 readonly BUSOLA_NAME_SUFFIX="busola"
