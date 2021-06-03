709fcdcb245d009c5bad85967455ddbdcbdd5d63
Valentin Vieriu <valentin.vieriu@gmail.com>
2021-06-03 08:35:15 +0200

    nt13

--- prow/scripts/cluster-integration/kyma-busola-long-lasting.sh
+++ prow/scripts/cluster-integration/kyma-busola-long-lasting.sh
@@ -196,7 +196,7 @@ else
 fi
 
 if [ -z "$COMMON_NAME_PREFIX" ] ; then
-    COMMON_NAME_PREFIX="nt12"
+    COMMON_NAME_PREFIX="nt13"
 fi
 readonly KYMA_NAME_SUFFIX="kyma"
 readonly BUSOLA_NAME_SUFFIX="busola"
