e1084c9e6ffb4f0096a57fcde70355f18b792d24
Karol Szwaj <karol.szwaj@gmail.com>
2021-06-11 14:58:06 +0200

    API Gateway integration tests refactoring (#3713)
    * Add prow job for running API-Gateway integration tests on Gardener cluster
    
     * Review changes
    
     * Render templates
    
     * Add post submit integration job
    
     * Disable testgrid
    
     * Make this job mandatory
    
    * Rework tests to use existing integration-gardener script
    
    * Remove vpath

---
+++ prow/scripts/cluster-integration/helpers/integration-tests.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+function integration_tests::install_kyma() {
+  log::info "Installing Kyma from local source using components file"
+  kyma alpha deploy --ci --components-file "$PWD/components.yaml" --value global.isBEBEnabled=true --source=local --workspace "${KYMA_SOURCES_DIR}" --verbose
+}
+
+function api-gateway::prepare_components_file() {
+  log::info "Preparing Kyma installation with Dex, Ory and API-Gateway"
+
+cat << EOF > "$PWD/components.yaml"
+defaultNamespace: kyma-system
+prerequisites:
+  - name: "cluster-essentials"
+  - name: "istio"
+    namespace: "istio-system"
+  - name: "certificates"
+    namespace: "istio-system"
+components:
+  - name: "dex"
+  - name: "ory"
+  - name: "api-gateway"
+EOF
+}
+
+function api-gateway::prepare_test_environments() {
+  log::info "Prepare test environment variables"
+
+  # Preparing needed environment variables for API Gateway tests, these can be moved later on.
+  export TEST_HYDRA_ADDRESS="https://oauth2.${CLUSTER_NAME}.kyma-prow.shoot.canary.k8s-hana.ondemand.com"
+  TEST_USER_EMAIL="$(kubectl get secret -n kyma-system admin-user --template="{{.data.email}}" | base64 --decode)"
+  export TEST_USER_EMAIL
+  TEST_USER_PASSWORD="$(kubectl get secret -n kyma-system admin-user --template="{{.data.password}}" | base64 --decode)"
+  export TEST_USER_PASSWORD
+  export TEST_REQUEST_TIMEOUT="120"
+  export TEST_REQUEST_DELAY="10"
+  export INGRESSGATEWAY_ADDRESS="istio-ingressgateway.istio-system.svc.cluster.local"
+  export TEST_DOMAIN="${CLUSTER_NAME}.kyma-prow.shoot.canary.k8s-hana.ondemand.com"
+  export TEST_CLIENT_TIMEOUT=30s
+  export TEST_RETRY_MAX_ATTEMPTS="5"
+  export TEST_RETRY_DELAY="5"
+  export TEST_GATEWAY_NAME="kyma-gateway"
+  export TEST_GATEWAY_NAMESPACE="kyma-system"
+}
+
+function api-gateway::launch_tests() {
+  log::info "Running Kyma API-Gateway tests"
+
+  pushd "${KYMA_SOURCES_DIR}/tests/integration/api-gateway/gateway-tests"
+  go test -v ./main_test.go
+  popd
+
+  log::success "Tests completed"
+}
