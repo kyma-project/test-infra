27ecd6d06a9cfe9c86a52e8964d4d5efacfcebd8
Damian Badura <damian.badura@sap.com>
2021-06-16 13:42:01 +0200

    add new job

--- development/tools/jobs/governance/kyma_governance_test.go
+++ development/tools/jobs/governance/kyma_governance_test.go
@@ -39,6 +39,36 @@ func TesKymaGovernanceJobPresubmit(t *testing.T) {
 	assert.Equal(t, []string{"--repository", "kyma"}, actualPresubmit.Spec.Containers[0].Args)
 }
 
+func TesKymaGovernanceKyma20JobPresubmit(t *testing.T) {
+	// WHEN
+	jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/governance.yaml")
+	// THEN
+	require.NoError(t, err)
+
+	assert.Len(t, jobConfig.PresubmitsStatic, 1)
+	presubmits := jobConfig.AllStaticPresubmits([]string{"kyma-project/kyma"})
+	assert.Len(t, presubmits, 1)
+
+	expName := "pre-kyma-docu-2.0-governance"
+	actualPresubmit := tester.FindPresubmitJobByNameAndBranch(presubmits, expName, "master")
+	require.NotNil(t, actualPresubmit)
+	assert.Equal(t, expName, actualPresubmit.Name)
+	assert.Equal(t, []string{"^master$", "^main$"}, actualPresubmit.Branches)
+	assert.Equal(t, 10, actualPresubmit.MaxConcurrency)
+	assert.False(t, actualPresubmit.SkipReport)
+
+	assert.Equal(t, "github.com/kyma-project/kyma", actualPresubmit.PathAlias)
+	tester.AssertThatHasExtraRefTestInfra(t, actualPresubmit.JobBase.UtilityConfig, "main")
+	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, preset.BuildPr, preset.DindEnabled)
+	assert.Equal(t, "milv.config.yaml|.md$", actualPresubmit.RunIfChanged)
+	assert.True(t, tester.IfPresubmitShouldRunAgainstChanges(*actualPresubmit, true, "milv.config.yaml"))
+	assert.True(t, tester.IfPresubmitShouldRunAgainstChanges(*actualPresubmit, true, "some_markdown.md"))
+	assert.Equal(t, tester.ImageBootstrapTestInfraLatest, actualPresubmit.Spec.Containers[0].Image)
+	assert.Equal(t, []string{tester.GovernanceScriptDir}, actualPresubmit.Spec.Containers[0].Command)
+	assert.Equal(t, []string{"--repository", "kyma"}, actualPresubmit.Spec.Containers[0].Args)
+	assert.True(t, actualPresubmit.Optional)
+}
+
 func TestKymaGovernanceJobPeriodic(t *testing.T) {
 	// WHEN
 	jobConfig, err := tester.ReadJobConfig("./../../../../prow/jobs/governance.yaml")
