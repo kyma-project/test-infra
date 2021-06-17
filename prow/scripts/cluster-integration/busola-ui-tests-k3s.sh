822d08a6f2ed660cb667c061a790fd946e0a7400
Wojciech Sołtys <wojciech.soltys@sap.com>
2021-06-17 13:37:26 +0200

    Remove memory and cpu debug messages

--- prow/scripts/cluster-integration/busola-ui-tests-k3s.sh
+++ prow/scripts/cluster-integration/busola-ui-tests-k3s.sh
@@ -124,5 +124,5 @@ cp "$PWD/kubeconfig-kyma.yaml" "$PWD/busola-tests/fixtures/kubeconfig.yaml"
 mkdir -p "$PWD/busola-tests/cypress/screenshots"
 
 echo "STEP: Running Cypress tests inside Docker"
-docker run --entrypoint /bin/bash --network=host -v "$PWD/busola-tests:/tests" -w /tests $CYPRESS_IMAGE -c "npm ci --no-optional; NO_COLOR=1 DEBUG=cypress:server:util:process_profiler cypress run --browser chrome --headless"
+docker run --entrypoint /bin/bash --network=host -v "$PWD/busola-tests:/tests" -w /tests $CYPRESS_IMAGE -c "npm ci --no-optional; NO_COLOR=1 cypress run --browser chrome --headless"
 
