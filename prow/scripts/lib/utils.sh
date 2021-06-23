38a3000ddc954db2768aafab4c6c3e45374641bb
dekiel <przemyslaw.pokrywka@sap.com>
2021-06-23 15:43:18 +0200

    yet another common name fix

--- prow/scripts/lib/utils.sh
+++ prow/scripts/lib/utils.sh
@@ -627,11 +627,14 @@ utils::generate_commonName() {
         namePrefix="$namePrefix-"
     fi
 
+    namePrefix=$(echo "$namePrefix" | tr '_' '-')
+    namePrefix=${namePrefix#-}
+
     local randomNameSuffix
     randomNameSuffix=$(LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c6)
     # return value
     # shellcheck disable=SC2034
-    utils_generate_commonName_return_commonName=$(echo "$namePrefix$id$randomNameSuffix" | tr "[:upper:]" "[:lower:]" | tr '_' '-')
+    utils_generate_commonName_return_commonName=$(echo "$namePrefix$id$randomNameSuffix" | tr "[:upper:]" "[:lower:]" )
 }
 
 # check_empty_arg will check if first argument is empty.
@@ -723,8 +726,7 @@ function utils::generate_vars_for_build {
         # shellcheck disable=SC2034
         utils_generate_vars_for_build_return_kymaSource="$commitID"
     elif [ -n "$prowjobName" ]; then
-        prowjobName=${prowjobName: -25:25}
-        prowjobName=${prowjobName#-}
+        prowjobName=${prowjobName: -20:20}
         utils::generate_commonName \
             -n "$prowjobName"
         # shellcheck disable=SC2034
