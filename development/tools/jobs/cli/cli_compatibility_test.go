ee89201b86c1da0de0a9a237c6986cd89f275986
Adrian Stobbe <adrian.stobbe@sap.com>
2021-06-23 12:25:34 +0200

    test

--- development/tools/jobs/cli/cli_compatibility_test.go
+++ development/tools/jobs/cli/cli_compatibility_test.go
@@ -31,22 +31,22 @@ func TestKymaCliCompatibilityPeriodics(t *testing.T) {
 	tester.AssertThatHasPresets(t, actualPeriodic.JobBase, preset.GCProjectEnv, "preset-sa-vm-kyma-integration")
 	assert.Equal(t, tester.ImageGolangKubebuilder2BuildpackLatest, actualPeriodic.Spec.Containers[0].Image)
 	tester.AssertThatSpecifiesResourceRequests(t, actualPeriodic.JobBase)
-	tester.AssertThatContainerHasEnv(t, actualPeriodic.Spec.Containers[0], "COMPAT_BACKTRACK", "1")
+	tester.AssertThatContainerHasEnv(t, actualPeriodic.Spec.Containers[0], "COMPAT_BACKTRACK", "0")
 	tester.AssertThatContainerHasEnv(t, actualPeriodic.Spec.Containers[0], "GO111MODULE", "on")
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/compatibility-cli.sh"}, actualPeriodic.Spec.Containers[0].Command)
 
 	// Compatibility 2 releases back
-	expName = "kyma-cli-compatibility-2"
-	actualPeriodic = tester.FindPeriodicJobByName(periodics, expName)
-	require.NotNil(t, actualPeriodic)
-	assert.Equal(t, expName, actualPeriodic.Name)
+	// expName = "kyma-cli-compatibility-2"
+	// actualPeriodic = tester.FindPeriodicJobByName(periodics, expName)
+	// require.NotNil(t, actualPeriodic)
+	// assert.Equal(t, expName, actualPeriodic.Name)
 
-	assert.Equal(t, "00 03 * * *", actualPeriodic.Cron)
-	tester.AssertThatHasExtraRepoRefCustom(t, actualPeriodic.JobBase.UtilityConfig, []string{"test-infra", "cli"}, []string{"main", "main"})
-	tester.AssertThatHasPresets(t, actualPeriodic.JobBase, preset.GCProjectEnv, "preset-sa-vm-kyma-integration")
-	assert.Equal(t, tester.ImageGolangKubebuilder2BuildpackLatest, actualPeriodic.Spec.Containers[0].Image)
-	tester.AssertThatSpecifiesResourceRequests(t, actualPeriodic.JobBase)
-	tester.AssertThatContainerHasEnv(t, actualPeriodic.Spec.Containers[0], "COMPAT_BACKTRACK", "2")
-	tester.AssertThatContainerHasEnv(t, actualPeriodic.Spec.Containers[0], "GO111MODULE", "on")
-	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/compatibility-cli.sh"}, actualPeriodic.Spec.Containers[0].Command)
+	// assert.Equal(t, "00 03 * * *", actualPeriodic.Cron)
+	// tester.AssertThatHasExtraRepoRefCustom(t, actualPeriodic.JobBase.UtilityConfig, []string{"test-infra", "cli"}, []string{"main", "main"})
+	// tester.AssertThatHasPresets(t, actualPeriodic.JobBase, preset.GCProjectEnv, "preset-sa-vm-kyma-integration")
+	// assert.Equal(t, tester.ImageGolangKubebuilder2BuildpackLatest, actualPeriodic.Spec.Containers[0].Image)
+	// tester.AssertThatSpecifiesResourceRequests(t, actualPeriodic.JobBase)
+	// tester.AssertThatContainerHasEnv(t, actualPeriodic.Spec.Containers[0], "COMPAT_BACKTRACK", "1")
+	// tester.AssertThatContainerHasEnv(t, actualPeriodic.Spec.Containers[0], "GO111MODULE", "on")
+	// assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/compatibility-cli.sh"}, actualPeriodic.Spec.Containers[0].Command)
 }
