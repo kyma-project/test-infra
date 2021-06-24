4ad68c8f090fa2142365ae8e5e51a655edc32ffb
Kamil Kasperski <kamil.kasperski@sap.com>
2021-06-24 12:23:20 +0200

    Initial git hooks

---
+++ development/hooks/pre-commit.d/01-rendertemplates.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! [ `command -v go`]; then
+  echo -e "ERROR: golang is not available. Please install the latest version of Go from https://golang.org/dl/!"
+  exit 1
+fi
+
+
+pushd "$(git rev-parse --show-toplevel)"
+go run development/tools/cmd/rendertemplates -config templates/config.yaml
+popd
+
+git add prow/jobs
