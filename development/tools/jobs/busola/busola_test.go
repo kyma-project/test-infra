0196cc4cf43fe8c6f7ed6eee8b978a9c3c316088
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2021-06-25 15:40:44 +0200

    Remove old busola jobs (#3810)
    * Remove old busola jobs
    
    * Templates
    
    * Oh, and make busola-web required
    
    * Do not skip report

--- development/tools/jobs/busola/busola_test.go
+++ development/tools/jobs/busola/busola_test.go
@@ -21,21 +21,6 @@ var components = []struct {
 	suite             func(config *jobsuite.Config) jobsuite.Suite
 	additionalOptions []jobsuite.Option
 }{
-	{path: "busola-core-ui", image: tester.ImageGolangBuildpack1_16, suite: tester.NewGenericComponentSuite,
-		additionalOptions: []jobsuite.Option{
-			jobsuite.AllReleases(),
-		},
-	},
-	{path: "busola-core", image: tester.ImageGolangBuildpack1_16, suite: tester.NewGenericComponentSuite,
-		additionalOptions: []jobsuite.Option{
-			jobsuite.AllReleases(),
-		},
-	},
-	{path: "busola-service-catalog-ui", image: tester.ImageGolangBuildpack1_16, suite: tester.NewGenericComponentSuite,
-		additionalOptions: []jobsuite.Option{
-			jobsuite.AllReleases(),
-		},
-	},
 	{path: "busola-web", image: tester.ImageGolangBuildpack1_16, suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.AllReleases(),
