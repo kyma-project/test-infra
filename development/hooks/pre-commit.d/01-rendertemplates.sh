ef3ee7aa7d2eac0cc8073d2101d486f1e7cb490b
Kamil Kasperski <kamil.kasperski@sap.com>
2021-06-25 17:50:53 +0200

    remove useless command check

--- development/hooks/pre-commit.d/01-rendertemplates.sh
+++ development/hooks/pre-commit.d/01-rendertemplates.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v "go")" ]; then
-  echo -e "WARN: golang is not available. Skipping hook...
-Please install the latest version of Go from https://golang.org/dl/!"
-  exit 0
-fi
-
 echo "Running rendertemplates pre-commit hook..."
 (
   cd "$(git rev-parse --show-toplevel)" || exit 1
     if files=$(go run development/tools/cmd/rendertemplates/main.go -config templates/config.yaml -show-output-dir=true); then
-      modified=$(git diff --exit-code --name-only "$files") && echo "No changes made. Continuing..." || echo "Templates have been regenerated and automatically added to your commit.
+      # shellcheck disable=SC2086
+      modified=$(git diff --exit-code --name-only $files) && echo "No changes made. Continuing..." || echo "Templates have been regenerated and automatically added to your commit.
 
   Modified files:
   $modified"
@@ -18,5 +13,6 @@ echo "Running rendertemplates pre-commit hook..."
       echo "Failed to render templates. Commit aborted."
       exit 1
     fi
-    git add -u "$files"
+    # shellcheck disable=SC2086
+    git add -u $files
 )
