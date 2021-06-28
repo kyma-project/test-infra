a103ad403fa8fbc6b36aa4186ac7675faa0138a8
Adam Wałach <adam.walach@sap.com>
2021-06-28 09:44:45 +0200

    Update job definitions for release of Kyma 1.24 (#3796)
    * Update job definitions
    
    * Clean up

--- development/tools/jobs/kyma/components_test.go
+++ development/tools/jobs/kyma/components_test.go
@@ -143,12 +143,6 @@ var components = []struct {
 			jobsuite.AllReleases(),
 		},
 	},
-	{path: "event-sources", image: tester.ImageGolangBuildpack1_16, suite: tester.NewGenericComponentSuite,
-		additionalOptions: []jobsuite.Option{
-			jobsuite.JobFileSuffix("generic"),
-			jobsuite.AllReleases(),
-		},
-	},
 	{path: "event-publisher-proxy", image: tester.ImageGolangBuildpack1_16, suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("generic"),
