37760c9f71ce16710b389b9d52bedff47716d9c9
Piotr Halama <piotr.halama@sap.com>
2021-06-30 11:13:05 +0200

    cleanup

--- prow/scripts/lib/docker.sh
+++ prow/scripts/lib/docker.sh
@@ -34,9 +34,9 @@ function docker::start {
     printf '=%.0s' {1..80}; echo
 
     if [[ -n "${GCR_PUSH_GOOGLE_APPLICATION_CREDENTIALS}" ]]; then
-      docker::authenticate -a "${GCR_PUSH_GOOGLE_APPLICATION_CREDENTIALS}"
+      docker::authenticate "${GCR_PUSH_GOOGLE_APPLICATION_CREDENTIALS}"
     elif [[ -n "${GOOGLE_APPLICATION_CREDENTIALS}" ]]; then
-      docker::authenticate -a "${GOOGLE_APPLICATION_CREDENTIALS}"
+      docker::authenticate "${GOOGLE_APPLICATION_CREDENTIALS}"
     else
       echo "Skipping docker authentication in GCR. No credentials provided."
     fi
@@ -50,27 +50,9 @@ function docker::start {
 }
 
 # docker::authenticate sets the docker user based on the provided credentials
-# Arguments:
-# required:
-# a - auth key
+# the script accepts one argument which should be proper auth key
 function docker::authenticate() {
-
-    local OPTIND
-    local authKey
-
-    while getopts ":c:a:j:z:h:" opt; do
-        case $opt in
-            a)
-                authKey="$OPTARG" ;;
-            \?)
-                echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
-            :)
-                echo "Option -$OPTARG argument not provided" >&2 ;;
-        esac
-    done
-
-    utils::check_empty_arg "$authKey" "Auth key was not provided. Exiting..."
-
+  authKey=$1
     if [[ -n "${authKey}" ]]; then
       client_email=$(jq -r '.client_email' < "${authKey}")
       echo "Authenticating in registry ${DOCKER_PUSH_REPOSITORY%%/*} as $client_email"
