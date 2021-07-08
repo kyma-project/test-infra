e08d2afa9eeb6c70dbbffd9bae35d8cb7c79b03d
Piotr Kopec <piotr@kubermatic.com>
2021-07-08 22:46:41 +0200

    add prow job to run `cluster-users` tests
    It's part of migrating `cluster-users` tests
    from Octopus test to component tests.
    
    Related kyma-project/kyma/issues/11324

--- prow/scripts/cluster-integration/kyma-integration-minikube.sh
+++ prow/scripts/cluster-integration/kyma-integration-minikube.sh
@@ -3,46 +3,161 @@
 set -e
 
 readonly driver="none"
-readonly testsuiteName="testsuite-all"
+readonly testSuiteName=${TEST_SUITE:-"testsuite-all"}
 KYMA_TEST_TIMEOUT=${KYMA_TEST_TIMEOUT:=1h}
 
+export KYMA_SOURCES_DIR="./kyma"
+
 install::kyma_cli() {
-    local settings
-    local kyma_version
-    mkdir -p "/usr/local/bin"
-    os=$(host::os)
+  local settings
+  local kyma_version
+  mkdir -p "/usr/local/bin"
+  os=$(host::os)
 
-    pushd "/usr/local/bin" || exit
+  pushd "/usr/local/bin" || exit
 
-    echo "Install kyma CLI ${os} locally to /usr/local/bin..."
+  echo "Install kyma CLI ${os} locally to /usr/local/bin..."
 
-    curl -sSLo kyma "https://storage.googleapis.com/kyma-cli-stable/kyma-${os}?alt=media"
-    chmod +x kyma
-    kyma_version=$(kyma version --client)
-    echo "Kyma CLI version: ${kyma_version}"
+  curl -sSLo kyma "https://storage.googleapis.com/kyma-cli-stable/kyma-${os}?alt=media"
+  chmod +x kyma
+  kyma_version=$(kyma version --client)
+  echo "Kyma CLI version: ${kyma_version}"
 
-    echo "OK"
+  echo "OK"
 
-    popd || exit
+  popd || exit
 
-    eval "${settings}"
+  eval "${settings}"
 }
 
 host::os() {
   local host_os
   case "$(uname -s)" in
-    Darwin)
-      host_os=darwin
-      ;;
-    Linux)
-      host_os=linux
-      ;;
-    *)
-      >&2 echo -e "Unsupported host OS. Must be Linux or Mac OS X."
-      exit 1
-      ;;
+  Darwin)
+    host_os=darwin
+    ;;
+  Linux)
+    host_os=linux
+    ;;
+  *)
+    echo >&2 -e "Unsupported host OS. Must be Linux or Mac OS X."
+    exit 1
+    ;;
   esac
   echo "${host_os}"
+}
+
+function testsuite::testsuite-all() {
+  echo "  List test definitions"
+  kyma test definitions --ci
+  echo "  Run tests"
+  kyma test run \
+    --ci \
+    --watch \
+    --max-retries=1 \
+    --name="${testSuiteName}" \
+    --timeout="${KYMA_TEST_TIMEOUT}" \
+    --selector kyma-project.io/test.integration=true
+
+  ENDTIME=$(date +%s)
+  echo "  Test time: $((ENDTIME - STARTTIME)) seconds."
+
+  echo "  Test summary"
+  kyma test status "${testSuiteName}" -owide
+  statusSucceeded=$(kubectl get cts "${testSuiteName}" -ojsonpath="{.status.conditions[?(@.type=='Succeeded')]}")
+
+  # disable exit on error to get as many logs as possible
+  set +e
+
+  echo "  Generate junit results"
+  kyma test status "${testSuiteName}" -ojunit | sed 's/ (executions: [0-9]*)"/"/g' >junit_kyma_octopus-test-suite.xml
+  if [[ "${statusSucceeded}" != *"True"* ]]; then
+    echo "- Fetching logs from testing pods in Failed status..."
+    kyma test logs "${testSuiteName}" --test-status Failed
+
+    echo "- Fetching logs from testing pods in Unknown status..."
+    kyma test logs "${testSuiteName}" --test-status Unknown
+
+    echo "- Fetching logs from testing pods in Running status due to running afer test suite timeout..."
+    kyma test logs "${testSuiteName}" --test-status Running
+    exit 1
+  fi
+}
+
+function cluster-users::setup_env_vars() {
+  echo "--> Setting up variables"
+  ADMIN_EMAIL=$(kubectl -n kyma-system get secrets admin-user -o=jsonpath="{.data.email}" | base64 -d)
+  echo "---> ADMIN_EMAIL: $ADMIN_EMAIL"
+  export ADMIN_EMAIL
+  ADMIN_PASSWORD=$(kubectl -n kyma-system get secrets admin-user -o=jsonpath="{.data.password}" | base64 -d)
+  echo "---> ADMIN_PASSWORD: $ADMIN_PASSWORD"
+  export ADMIN_PASSWORD
+  DEVELOPER_EMAIL="$(kubectl -n kyma-system get secrets test-developer-user -o=jsonpath="{.data.email}" | base64 -d)"
+  echo "---> DEVELOPER_EMAIL: $DEVELOPER_EMAIL"
+  export DEVELOPER_EMAIL
+  DEVELOPER_PASSWORD="$(kubectl -n kyma-system get secrets test-developer-user -o=jsonpath="{.data.password}" | base64 -d)"
+  echo "---> DEVELOPER_PASSWORD: $DEVELOPER_PASSWORD"
+  export DEVELOPER_PASSWORD
+  VIEW_EMAIL="$(kubectl -n kyma-system get secrets test-read-only-user -o=jsonpath="{.data.email}" | base64 -d)"
+  echo "---> VIEW_EMAIL: $VIEW_EMAIL"
+  export VIEW_EMAIL
+  VIEW_PASSWORD="$(kubectl -n kyma-system get secrets test-read-only-user -o=jsonpath="{.data.password}" | base64 -d)"
+  echo "---> VIEW_PASSWORD: $VIEW_PASSWORD"
+  export VIEW_PASSWORD
+  NAMESPACE_ADMIN_EMAIL="$(kubectl -n kyma-system get secrets test-namespace-admin-user -o=jsonpath="{.data.email}" | base64 -d)"
+  echo "---> NAMESPACE_ADMIN_EMAIL: $NAMESPACE_ADMIN_EMAIL"
+  export NAMESPACE_ADMIN_EMAIL
+  NAMESPACE_ADMIN_PASSWORD="$(kubectl -n kyma-system get secrets test-namespace-admin-user -o=jsonpath="{.data.password}" | base64 -d)"
+  echo "---> NAMESPACE_ADMIN_PASSWORD: $NAMESPACE_ADMIN_PASSWORD"
+  export NAMESPACE_ADMIN_PASSWORD
+  export KYMA_SYSTEM="kyma-system"
+  export SYSTEM_NAMESPACE="kyma-system"
+  # shellcheck disable=SC2155
+  local namespace_id=$(tr </dev/urandom -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1 | tr '[:upper:]' '[:lower:]')
+  CUSTOM_NAMESPACE="test-namespace-$namespace_id"
+  echo "---> CUSTOM_NAMESPACE: $CUSTOM_NAMESPACE"
+  export CUSTOM_NAMESPACE
+  export NAMESPACE="default"
+  export IAM_KUBECONFIG_SVC_FQDN="http://localhost:8123"
+  export DEX_SERVICE_SERVICE_HOST="localhost"
+  export DEX_SERVICE_SERVICE_PORT_HTTP=5556
+}
+
+function cluster-users::port-forward() {
+  echo "--> Port forwarding"
+  # cluster-user tests need to access dex. Previously tests were run from within cluster.
+  kubectl -n kyma-system port-forward deployment/dex 5556:5556 &
+  kubectl -n kyma-system port-forward deployment/iam-kubeconfig-service 8123:8000 &
+  sleep 3s # waiting for port-forward to actually forward traffic.
+}
+
+function cluster-users::port-forward-cleanup() {
+  echo "--> Closing port forwarding"
+  pkill kubectl
+}
+
+function cluster-users::launch_tests() {
+  echo "Running Kyma cluster-user tests"
+
+  echo "--> Starting SAR testing"
+  pushd "${KYMA_SOURCES_DIR}/resources/cluster-users/files"
+  if ! bash sar-test.sh; then
+    echo "Tests failed"
+    exit 1
+  fi
+  popd
+
+  echo "Tests completed"
+}
+
+function testsuite::cluster-users() {
+
+  echo "Starting cluster-users tests"
+  cluster-users::port-forward
+  cluster-users::setup_env_vars
+  cluster-users::launch_tests
+  cluster-users::port-forward-cleanup
+
 }
 
 echo "--> Installing Kyma CLI"
@@ -60,8 +175,8 @@ echo "--> Done"
 echo "--> Provision Kyma cluster on minikube using VM driver ${driver}"
 STARTTIME=$(date +%s)
 yes | kyma provision minikube \
-               --ci \
-               --vm-driver="${driver}"
+  --ci \
+  --vm-driver="${driver}"
 ENDTIME=$(date +%s)
 echo "  Execution time: $((ENDTIME - STARTTIME)) seconds."
 echo "--> Done"
@@ -69,48 +184,27 @@ echo "--> Done"
 echo "--> Installing Kyma on minikube cluster"
 STARTTIME=$(date +%s)
 yes | kyma install \
-     --ci \
-     --source="local" \
-     --src-path=./kyma
+  --ci \
+  --source="local" \
+  --src-path=./kyma
 ENDTIME=$(date +%s)
 echo "  Install time: $((ENDTIME - STARTTIME)) seconds."
 echo "--> Done"
 
 echo "--> Run kyma tests"
 STARTTIME=$(date +%s)
-echo "  List test definitions"
-kyma test definitions --ci
-echo "  Run tests"
-kyma test run \
-          --ci \
-          --watch \
-          --max-retries=1 \
-          --name="${testsuiteName}" \
-          --timeout="${KYMA_TEST_TIMEOUT}" \
-          --selector kyma-project.io/test.integration=true
 
-ENDTIME=$(date +%s)
-echo "  Test time: $((ENDTIME - STARTTIME)) seconds."
-
-echo "  Test summary"
-kyma test status "${testsuiteName}" -owide
-statusSucceeded=$(kubectl get cts "${testsuiteName}" -ojsonpath="{.status.conditions[?(@.type=='Succeeded')]}")
-
-# disable exit on error to get as many logs as possible
-set +e
-
-echo "  Generate junit results"
-kyma test status "${testsuiteName}" -ojunit | sed 's/ (executions: [0-9]*)"/"/g' > junit_kyma_octopus-test-suite.xml
-if [[ "${statusSucceeded}" != *"True"* ]]; then
-  echo "- Fetching logs from testing pods in Failed status..."
-  kyma test logs "${testsuiteName}" --test-status Failed
-
-  echo "- Fetching logs from testing pods in Unknown status..."
-  kyma test logs "${testsuiteName}" --test-status Unknown
-
-  echo "- Fetching logs from testing pods in Running status due to running afer test suite timeout..."
-  kyma test logs "${testsuiteName}" --test-status Running
+case ${testSuiteName} in
+"testsuite-all")
+  testsuite::testsuite-all
+  ;;
+"cluster-users")
+  testsuite::cluster-users
+  ;;
+*)
+  echo "testsuite not selected"
   exit 1
-fi
+  ;;
+esac
 
 echo "--> Success"
--- prow/scripts/provision-vm-and-start-kyma-minikube.sh
+++ prow/scripts/provision-vm-and-start-kyma-minikube.sh
@@ -5,10 +5,17 @@
 
 set -o errexit
 
-readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly TEST_INFRA_SOURCES_DIR="$(cd "${SCRIPT_DIR}/../../" && pwd)"
 readonly TMP_DIR=$(mktemp -d)
 readonly JUNIT_REPORT_PATH="${ARTIFACTS:-${TMP_DIR}}/junit_kyma_octopus-test-suite.xml"
+readonly testSuiteScript=${TEST_SUITE_SCRIPT:-"cluster-integration/kyma-integration-minikube.sh"}
+readonly TEST_SUITE=${TEST_SUITE:-"testsuite-all"}
+
+if [[ ! -f ${SCRIPT_DIR}/${testSuiteScript} ]]; then
+  echo "Fatal: selected script ${SCRIPT_DIR}/${testSuiteScript} does not exist"
+  exit 1
+fi
 
 # shellcheck source=prow/scripts/lib/log.sh
 source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/lib/log.sh"
@@ -52,40 +59,38 @@ else
 fi
 
 POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
+while [[ $# -gt 0 ]]; do
   key="$1"
 
   case ${key} in
-    --image)
-      IMAGE="$2"
-      testCustomImage "${IMAGE}"
-      shift
-      shift
-      ;;
-    --*)
-      echo "Unknown flag ${1}"
-      exit 1
-      ;;
-    *)    # unknown option
-      POSITIONAL+=("$1") # save it in an array for later
-      shift # past argument
-      ;;
+  --image)
+    IMAGE="$2"
+    testCustomImage "${IMAGE}"
+    shift
+    shift
+    ;;
+  --*)
+    echo "Unknown flag ${1}"
+    exit 1
+    ;;
+  *) # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift              # past argument
+    ;;
   esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
-
 
 if [[ -z "$IMAGE" ]]; then
   log::info "Provisioning vm using the latest default custom image ..."
 
   IMAGE=$(gcloud compute images list --sort-by "~creationTimestamp" \
-       --filter "family:custom images AND labels.default:yes" --limit=1 | tail -n +2 | awk '{print $1}')
+    --filter "family:custom images AND labels.default:yes" --limit=1 | tail -n +2 | awk '{print $1}')
 
   if [[ -z "$IMAGE" ]]; then
-   log::error "There are no default custom images, the script will exit ..." && exit 1
+    log::error "There are no default custom images, the script will exit ..." && exit 1
   fi
- fi
+fi
 
 ZONE_LIMIT=${ZONE_LIMIT:-5}
 EU_ZONES=$(gcloud compute zones list --filter="name~europe" --limit="${ZONE_LIMIT}" | tail -n +2 | awk '{print $1}')
@@ -93,24 +98,29 @@ STARTTIME=$(date +%s)
 for ZONE in ${EU_ZONES}; do
   log::info "Attempting to create a new instance named kyma-integration-test-${RANDOM_ID} in zone ${ZONE} using image ${IMAGE}"
   gcloud compute instances create "kyma-integration-test-${RANDOM_ID}" \
-      --metadata enable-oslogin=TRUE \
-      --image "${IMAGE}" \
-      --machine-type n1-standard-4 \
-      --zone "${ZONE}" \
-      --boot-disk-size 200 "${LABELS[@]}" &&\
-  log::info "Created kyma-integration-test-${RANDOM_ID} in zone ${ZONE}" && break
+    --metadata enable-oslogin=TRUE \
+    --image "${IMAGE}" \
+    --machine-type n1-standard-4 \
+    --zone "${ZONE}" \
+    --boot-disk-size 200 "${LABELS[@]}" &&
+    log::info "Created kyma-integration-test-${RANDOM_ID} in zone ${ZONE}" && break
   log::error "Could not create machine in zone ${ZONE}"
 done || exit 1
 ENDTIME=$(date +%s)
 echo "VM creation time: $((ENDTIME - STARTTIME)) seconds."
 
-trap cleanup exit INT
+if [[ ${TEST_SUITE} == "testsuite-all" ]]; then
+  trap cleanup exit INT
+fi
 
 log::info "Copying Kyma to the instance"
 #shellcheck disable=SC2088
 utils::compress_send_to_vm "${ZONE}" "kyma-integration-test-${RANDOM_ID}" "/home/prow/go/src/github.com/kyma-project/kyma" "~/kyma"
 
 log::info "Triggering the installation"
-gcloud compute ssh --strict-host-key-checking=no --quiet --zone="${ZONE}" --command="sudo bash" --ssh-flag="-o ServerAliveInterval=30" "kyma-integration-test-${RANDOM_ID}" < "${SCRIPT_DIR}/cluster-integration/kyma-integration-minikube.sh"
+log::info "Running testsuite ${testSuiteScript}"
+gcloud compute ssh --strict-host-key-checking=no --quiet --zone="${ZONE}" \
+  --command="sudo PULL_NUMBER=${PULL_NUMBER}  TEST_SUITE=${TEST_SUITE} bash" \
+  --ssh-flag="-o ServerAliveInterval=30" "kyma-integration-test-${RANDOM_ID}" <"${SCRIPT_DIR}/${testSuiteScript}"
 
 log::success "all done"
