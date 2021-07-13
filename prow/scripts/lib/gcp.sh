657e6244b9bf487dcb8e228228a7a90a34e92fd8
Piotr Halama <piotr.halama@sap.com>
2021-07-13 11:31:27 +0200

    Scripts: fix gcp library

--- prow/scripts/lib/gcp.sh
+++ prow/scripts/lib/gcp.sh
@@ -136,7 +136,7 @@ function gcp::provision_k8s_cluster {
     log::info "Replacing underscore with dashes in cluster name."
     clusterName=$(echo "$clusterName" | tr '_' '-')
     # Cluster name must be less than 40 characters
-    if [ "$clusterName" -ge 40 ]; then
+    if [ "${#clusterName}" -ge 40 ]; then
         log::error "Cluster name must be less than 40 characters"
         exit 1
     fi
