10574bed464ddb5828675f8c158f6de0ad67b1f4
dekiel <przemyslaw.pokrywka@sap.com>
2021-07-20 23:57:55 +0200

    added kyma repo

--- development/kyma-github-connector/githubWebhookGateway/pkg/gateway/types.go
+++ development/kyma-github-connector/githubWebhookGateway/pkg/gateway/types.go
@@ -4,6 +4,7 @@ package gateway
 type Config struct {
 	GitHubWebhookGatewayName string `envconfig:"GITHUB_WEBHOOK_GATEWAY_NAME"`
 	GitHubWebhookSecret      string `envconfig:"GITHUB_WEBHOOK_SECRET"`
+	GitHubWebhookUrlPath     string `envconfig:"GITHUB_WEBHOOK_URL_PATH"`
 	KymaEventsService        string `envconfig:"EVENTING_SERVICE"` //http://test-gh-connector-app-event-service.kyma-integration:8081/test-gh-connector-app/events
 	ListenPort               string `envconfig:"LISTEN_PORT"`
 	//EventType         string `envconfig:"EVENT_TYPE"`             // Event type published to Event Publisher Proxy.
