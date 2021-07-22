30b5ec8688d608f295ae84296bcdd9c386a7edb3
dekiel <przemyslaw.pokrywka@sap.com>
2021-07-22 10:48:41 +0200

    added firestore client

--- development/kyma-ci-force-bot/closeFailingTestInstanceService/cmd/main.go
+++ development/kyma-ci-force-bot/closeFailingTestInstanceService/cmd/main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"cloud.google.com/go/firestore"
 	"context"
+	"fmt"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/google/go-github/github"
+	"github.com/kyma-project/test-infra/development/kyma-github-connector/githubWebhookGateway/pkg/gateway"
 	log "github.com/sirupsen/logrus"
 	"github.com/vrischmann/envconfig"
+)
+
+var (
+	firestoreClient *firestore.Client
+	conf            Config
 )
 
 //Config containing all program configs
 type Config struct {
 	KymaEventsService string `envconfig:"EVENTING_SERVICE"` //http://test-gh-connector-app-event-service.kyma-integration:8081/test-gh-connector-app/events
 	ListenPort        int    `envconfig:"LISTEN_PORT"`
+	ProjectID         string `envconfig:"FIRESTORE_GCP_PROJECT_ID"`
 	//EventType         string `envconfig:"EVENT_TYPE"`             // Event type published to Event Publisher Proxy.
 	AppName string `envconfig:"APP_NAME"` // PubSub Connector application name as set in Compass.
+}
+
+func init() {
+	var err error
+	ctx := context.Background()
+	err = envconfig.Init(&conf)
+	if err != nil {
+		log.Fatal("failed create config from env variables: ", err.Error())
+	}
+	// create firestore client, it will be reused by multiple function calls
+	firestoreClient, err = firestore.NewClient(ctx, conf.ProjectID)
+	if err != nil {
+		log.Fatalf(fmt.Sprintf("Failed creating firestore client, error: %s", err.Error()))
+	}
 }
 
 func receive(event cloudevents.Event) {
--- development/kyma-ci-force-bot/closeFailingTestInstanceService/go.mod
+++ development/kyma-ci-force-bot/closeFailingTestInstanceService/go.mod
@@ -3,12 +3,13 @@ module github.com/kyma-project/test-infra/development/kyma-ci-force-bot/closeFai
 go 1.14
 
 require (
+	cloud.google.com/go/firestore v1.5.0
 	github.com/cloudevents/sdk-go/v2 v2.4.1
 	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/kyma-project/test-infra/development/kyma-github-connector/githubWebhookGateway v0.0.0-20210721090827-2262213507c1 // indirect
 	github.com/sirupsen/logrus v1.4.2
 	github.com/vrischmann/envconfig v1.2.0
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
