ab8a7a62f51c6e53e4fd6c232677ed63a5c588f2
Kamil Kasperski <kamil.kasperski@sap.com>
2021-07-29 18:22:12 +0200

    Remove docker from image-syncer

--- development/image-syncer/main.go
+++ development/image-syncer/main.go
@@ -2,21 +2,16 @@ package main
 
 import (
 	"context"
-	"encoding/base64"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/crane"
 	"io/ioutil"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
 	"github.com/jamiealquiza/envy"
-	parser "github.com/novln/docker-parser"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v2"
@@ -29,12 +24,14 @@ var (
 // SyncDef stores synchronisation definition
 type SyncDef struct {
 	TargetRepoPrefix string `yaml:"targetRepoPrefix"`
+	Sign             bool   `yaml:"sign,omitempty"`
 	Images           []Image
 }
 
 // Image stores image location
 type Image struct {
 	Source string
+	Sign   bool   `yaml:"sign,omitempty"`
 	Tag    string `yaml:"tag,omitempty"`
 }
 
@@ -43,106 +40,7 @@ type Config struct {
 	ImagesFile    string
 	TargetKeyFile string
 	DryRun        bool
-}
-
-func getAuthString(user, password string) (string, error) {
-	authConfig := types.AuthConfig{
-		Username: user,
-		Password: password,
-	}
-	encodedJSON, err := json.Marshal(authConfig)
-	if err != nil {
-		return "", err
-	}
-	return base64.URLEncoding.EncodeToString(encodedJSON), nil
-}
-
-func getImageIDAndRepoDigest(ctx context.Context, cli *client.Client, image string) (string, string, error) {
-	reader, err := cli.ImagePull(ctx, image, types.ImagePullOptions{})
-	if err != nil {
-		return "", "", fmt.Errorf("image pull failed: %w", err)
-	}
-	w := log.WriterLevel(logrus.DebugLevel)
-	defer w.Close()
-	io.Copy(w, reader)
-
-	details, _, err := cli.ImageInspectWithRaw(ctx, image)
-	if err != nil {
-		return "", "", fmt.Errorf("image inspection failed: %w", err)
-	}
-
-	ref, err := parser.Parse(image)
-	imageRepository := ref.Repository()
-
-	for _, url := range details.RepoDigests {
-		ref, err := parser.Parse(url)
-		if err != nil {
-			return "", "", fmt.Errorf("url parsing failed: %w", err)
-		}
-		if imageRepository == ref.Repository() {
-			return details.ID, ref.Tag(), nil
-		}
-	}
-
-	return "", "", fmt.Errorf("unable to find digest for '%s'", image)
-}
-
-func safeCopyImage(ctx context.Context, cli *client.Client, authString, sourceImage, targetTag, targetRepo string, dryRun bool) error {
-	if sourceImage == "" {
-		return fmt.Errorf("source image can not be empty")
-	}
-	log.Infof("Source image: %s", sourceImage)
-	sourceID, sourceDigest, err := getImageIDAndRepoDigest(ctx, cli, sourceImage)
-	if err != nil {
-		return err
-	}
-	log.Infof("Source ID: %s", sourceID)
-	log.Infof("Source repo digest: %s", sourceDigest)
-
-	target := targetRepo + sourceImage
-	if strings.Contains(sourceImage, "@sha256:") {
-		if targetTag == "" {
-			return errors.New("sha256 digest detected, but the \"tag\" was not specified")
-		}
-		imageName := strings.Split(sourceImage, "@sha256:")[0]
-		target = targetRepo + imageName + ":" + targetTag
-	}
-
-	log.Infof("Target image: %s", target)
-	targetID, targetDigest, err := getImageIDAndRepoDigest(ctx, cli, target)
-	if isImageNotFoundError(err) {
-		log.Info("Target image does not exist")
-		if dryRun {
-			log.Info("Dry-run mode - tagging and pushing skipped")
-			return nil
-		}
-		if err = cli.ImageTag(ctx, sourceImage, target); err != nil {
-			return err
-		}
-
-		log.Info("Image re-tagged")
-		log.Info("Pushing to target repo")
-		reader, err := cli.ImagePush(ctx, target, types.ImagePushOptions{RegistryAuth: authString})
-		if err != nil {
-			return err
-		}
-		w := log.WriterLevel(logrus.DebugLevel)
-		defer w.Close()
-		io.Copy(w, reader)
-
-		log.Info("Image pushed successfully")
-	} else if err != nil {
-		return err
-	} else {
-		log.Infof("Target ID: %s", targetID)
-		log.Infof("Target repo digest: %s", targetDigest)
-		if sourceID != targetID {
-			return fmt.Errorf("source and target IDs are different - probably source image has been changed")
-		}
-		log.Info("Source and target IDs are the same, nothing to do")
-	}
-
-	return nil
+	Debug         bool
 }
 
 //TODO: we should check for error type not match strings
@@ -150,8 +48,7 @@ func isImageNotFoundError(err error) bool {
 	if err == nil {
 		return false
 	}
-	if strings.Index(err.Error(), "not found: manifest unknown") != -1 ||
-		strings.HasSuffix(err.Error(), "not found") {
+	if strings.Contains(err.Error(), "MANIFEST_UNKNOWN") {
 		return true
 	}
 	return false
@@ -170,49 +67,71 @@ func cancelOnInterrupt(ctx context.Context, cancel context.CancelFunc) {
 	}()
 }
 
-func copyImages(cfg Config) error {
-
-	log.SetLevel(logrus.InfoLevel)
-
-	ctx, cancelFunc := context.WithCancel(context.Background())
-	defer cancelFunc()
-	cancelOnInterrupt(ctx, cancelFunc)
-
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+func SyncImage(ctx context.Context, src, dest string, dryRun bool, opts ...crane.Option) error {
+	log.Debug("source ", src)
+	log.Debug("destination ", dest)
+	srcImg, err := crane.Pull(src)
+	if err != nil {
+		return fmt.Errorf("source image pull error: %w", err)
+	}
+	destImg, err := crane.Pull(dest)
+	if isImageNotFoundError(err) {
+		log.Debug("Target image does not exist. Pushing image...")
+		if dryRun {
+			log.Info("Dry-Run enabled. Skipping push.")
+			return nil
+		}
+		err := crane.Push(srcImg, dest, opts...)
+		if err != nil {
+			return fmt.Errorf("push image error: %w", err)
+		}
+		return nil
+	} else if err != nil {
+		return err
+	}
+	d1, err := srcImg.Digest()
+	log.Debug("source digest ", d1)
+	if err != nil {
+		return err
+	}
+	d2, err := destImg.Digest()
+	log.Debug("target digest ", d2)
 	if err != nil {
 		return err
 	}
 
-	imagesFile, err := ioutil.ReadFile(cfg.ImagesFile)
+	if d1 != d2 {
+		return fmt.Errorf("digests are not equal: %v, %v - probably source image has been changed", d1, d2)
+	}
+	log.Debug("Digests are equal. Nothing to do.")
+	return nil
+}
+
+func GetTarget(source, targetRepo, targetTag string) (string, error) {
+	target := targetRepo + source
+	if strings.Contains(source, "@sha256:") {
+		if targetTag == "" {
+			return "", fmt.Errorf("sha256 digest detected, but the \"tag\" was not specified")
+		}
+		imageName := strings.Split(source, "@sha256:")[0]
+		target = targetRepo + imageName + ":" + targetTag
+	}
+	return target, nil
+}
+
+func ParseImagesFile(file string) (*SyncDef, error) {
+	f, err := ioutil.ReadFile(file)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	var syncDef SyncDef
-	if err := yaml.Unmarshal(imagesFile, &syncDef); err != nil {
-		return err
+	if err := yaml.Unmarshal(f, &syncDef); err != nil {
+		return nil, err
 	}
 	if syncDef.TargetRepoPrefix == "" {
-		return fmt.Errorf("TargetRepoPrefix can not be empty")
+		return nil, fmt.Errorf("targetRepoPrefix can not be empty")
 	}
-
-	jsonKey, err := ioutil.ReadFile(cfg.TargetKeyFile)
-	if err != nil {
-		return err
-	}
-
-	authString, err := getAuthString("_json_key", string(jsonKey))
-	if err != nil {
-		return err
-	}
-
-	for _, image := range syncDef.Images {
-		err = safeCopyImage(ctx, cli, authString, image.Source, image.Tag, syncDef.TargetRepoPrefix, cfg.DryRun)
-		if err != nil {
-			return err
-		}
-		log.Info("------------------------")
-	}
-	return nil
+	return &syncDef, nil
 }
 
 func main() {
@@ -224,18 +143,64 @@ func main() {
 		Short: "image-syncer copies images between docker registries",
 		Long:  `image-syncer copies docker images. It compares checksum between source and target and protects target images against overriding`,
 		Run: func(cmd *cobra.Command, args []string) {
-			if err := copyImages(cfg); err != nil {
-				log.Fatal(err)
+			logLevel := logrus.InfoLevel
+			if cfg.Debug {
+				logLevel = logrus.DebugLevel
+			}
+			log.SetLevel(logLevel)
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			cancelOnInterrupt(ctx, cancel)
+
+			imagesFile, err := ParseImagesFile(cfg.ImagesFile)
+			if err != nil {
+				log.WithError(err).Fatal("Could not parse images file")
+			}
+			authCfg, err := ioutil.ReadFile(cfg.TargetKeyFile)
+			if err != nil {
+				log.WithError(err).Fatal("Could not open target auth key JSON")
+			}
+			authOption := crane.WithAuth(&authn.Basic{Username: "_json_key", Password: string(authCfg)})
+			var failed bool
+
+		loop:
+			for _, img := range imagesFile.Images {
+				select {
+				case <-ctx.Done():
+					log.Error("Context cancelled")
+					break loop
+				default:
+				}
+				log.WithField("image", img.Source).Info("Start scan on image")
+				target, err := GetTarget(img.Source, imagesFile.TargetRepoPrefix, img.Tag)
+				if err != nil {
+					log.WithError(err).Error("Failed to get target url")
+					failed = true
+				}
+				err = SyncImage(ctx, img.Source, target, cfg.DryRun, authOption)
+				if err != nil {
+					log.WithError(err).Error("Failed to sync image")
+					failed = true
+				}
+				if err == nil {
+					log.WithField("target", target).Info("Image synced succesfully")
+				}
+			}
+			if failed {
+				log.Fatal("Some errors occurred during image sync")
+			} else {
+				log.Info("All images synced successfully")
 			}
 		},
 	}
 
 	rootCmd.PersistentFlags().StringVarP(&cfg.ImagesFile, "images-file", "i", "", "yaml file containing list of images")
-	rootCmd.PersistentFlags().StringVarP(&cfg.TargetKeyFile, "target-key-file", "t", "", "JSON key file used for authorization to target repo")
+	rootCmd.PersistentFlags().StringVarP(&cfg.TargetKeyFile, "target-repo-auth-key", "t", "", "JSON key file used for authorization to target repo")
 	rootCmd.PersistentFlags().BoolVarP(&cfg.DryRun, "dry-run", "d", true, "dry run mode")
+	rootCmd.PersistentFlags().BoolVar(&cfg.Debug, "debug", false, "enable debug mode")
 
 	rootCmd.MarkPersistentFlagRequired("images-file")
-	rootCmd.MarkPersistentFlagRequired("target-key-file")
+	rootCmd.MarkPersistentFlagRequired("target-repo-auth-key")
 	envy.ParseCobra(rootCmd, envy.CobraConfig{Prefix: "SYNCER", Persistent: true, Recursive: false})
 
 	if err := rootCmd.Execute(); err != nil {
--- go.mod
+++ go.mod
@@ -12,26 +12,22 @@ replace (
 require (
 	cloud.google.com/go v0.81.0
 	cloud.google.com/go/bigquery v1.8.0
+	cloud.google.com/go/firestore v1.1.0
 	cloud.google.com/go/pubsub v1.4.0
 	cloud.google.com/go/storage v1.12.0
 	github.com/Masterminds/semver v1.5.0
 	github.com/Masterminds/sprig v2.22.0+incompatible
-	github.com/containerd/containerd v1.5.0 // indirect
-	github.com/docker/docker v20.10.6+incompatible
 	github.com/forestgiant/sliceutil v0.0.0-20160425183142-94783f95db6c
 	github.com/ghodss/yaml v1.0.0
 	github.com/go-yaml/yaml v2.1.0+incompatible
+	github.com/google/go-containerregistry v0.5.1
 	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-github/v31 v31.0.0
 	github.com/google/go-github/v36 v36.0.0
 	github.com/google/go-querystring v1.0.0
-	github.com/google/gofuzz v1.2.1-0.20210504230335-f78f29fc09ea // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/imdario/mergo v0.3.11
 	github.com/jamiealquiza/envy v1.1.0
 	github.com/jinzhu/copier v0.3.2
-	github.com/moby/term v0.0.0-20201216013528-df9cb8a40635 // indirect
-	github.com/novln/docker-parser v1.0.0
 	github.com/onsi/gomega v1.10.3
 	github.com/pkg/errors v0.9.1
 	github.com/satori/go.uuid v1.2.0
@@ -50,5 +46,4 @@ require (
 	k8s.io/apimachinery v0.21.0
 	k8s.io/client-go v11.0.1-0.20190805182717-6502b5e7b1b5+incompatible
 	k8s.io/test-infra v0.0.0-20210407040951-51f95c2d525e
-	sigs.k8s.io/structured-merge-diff/v4 v4.1.1 // indirect
 )
