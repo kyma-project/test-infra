8ae9d44630d91105e23438a87ecc5337cd7b4d9a
dekiel <przemyslaw.pokrywka@sap.com>
2021-07-30 23:20:07 +0200

    logging to cloud logging

--- development/kyma-ci-force-bot/ciforcebot/cmd/main.go
+++ development/kyma-ci-force-bot/ciforcebot/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"time"
 
 	"cloud.google.com/go/firestore"
@@ -13,11 +14,16 @@ import (
 	"github.com/vrischmann/envconfig"
 )
 
+const (
+	errorReportingType = "type.googleapis.com/google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent"
+)
+
 var (
-	firestoreClient *firestore.Client
-	loggingClient   *logging.Client
-	logger          *logging.Logger
-	conf            Config
+	firestoreClient   *firestore.Client
+	loggingClient     *logging.Client
+	logger            *logging.Logger
+	cloudeventsClient cloudevents.Client
+	conf              Config
 )
 
 //Config containing all program configs
@@ -25,15 +31,15 @@ type Config struct {
 	KymaEventsService string `envconfig:"EVENTING_SERVICE"` //http://test-gh-connector-app-event-service.kyma-integration:8081/test-gh-connector-app/events
 	ListenPort        int    `envconfig:"LISTEN_PORT"`
 	ProjectID         string `envconfig:"FIRESTORE_GCP_PROJECT_ID"`
-	//EventType         string `envconfig:"EVENT_TYPE"`             // Event type published to Event Publisher Proxy.
-	AppName string `envconfig:"APP_NAME"` // PubSub Connector application name as set in Compass.
-	LogName string `envconfig:"LOG_NAME"` // Google cloud logging log name.
+	AppName           string `envconfig:"APP_NAME"` // PubSub Connector application name as set in Compass.
+	LogName           string `envconfig:"LOG_NAME"` // Google cloud logging log name.
+	Component         string `envconfig:"COMPONENT"`
 }
 
 type loggingPayload struct {
-	Message   string
-	Component string
-	Operation string
+	Message   string `json:"message"`
+	Operation string `json,omitempty:"operation"`
+	Type      string `json,omitempty:"@type"`
 }
 
 func init() {
@@ -41,85 +47,189 @@ func init() {
 	ctx := context.Background()
 	err = envconfig.Init(&conf)
 	if err != nil {
-		log.Fatal("failed create config from env variables: ", err.Error())
-	}
-	// create firestore client, it will be reused by multiple function calls
-	firestoreClient, err = firestore.NewClient(ctx, conf.ProjectID)
-	if err != nil {
-		log.Fatalf(fmt.Sprintf("Failed creating firestore client, error: %s", err.Error()))
+		log.Fatalf("failed create config from env variables, got error: %v", err)
 	}
 	loggingClient, err = logging.NewClient(ctx, conf.ProjectID)
 	if err != nil {
-		log.Fatalf("Failed to create goggle logging client: %v", err)
+		log.Fatalf("Failed to create goggle logging client, got error: %v", err)
 	}
-	logger = loggingClient.Logger(conf.LogName, logging.CommonLabels(map[string]string{
-		"appName":  conf.AppName,
-		"function": "closeFailingTestInstance"}))
+	randomInt := rand.Int()
+	trace := fmt.Sprintf("projects/%s/traces/%s/%d", conf.ProjectID, conf.Component, randomInt)
+	logger := loggingClient.Logger(conf.LogName, logging.CommonLabels(map[string]string{
+		"appName":   conf.AppName,
+		"component": conf.Component}))
+	// create firestore client, it will be reused by multiple function calls
+	firestoreClient, err = firestore.NewClient(ctx, conf.ProjectID)
+	if err != nil {
+		logger.Log(logging.Entry{
+			Timestamp: time.Now(),
+			Severity:  logging.Critical,
+			Trace:     trace,
+			Payload: loggingPayload{
+				Message:   fmt.Sprintf("failed create firestore client, got error: %v", err),
+				Operation: "ci-force-bot initialization on tooling kyma",
+				Type:      errorReportingType,
+			},
+		})
+	}
+	cloudeventsClient, err = cloudevents.NewClientHTTP(cloudevents.WithPort(conf.ListenPort))
+	if err != nil {
+		logger.Log(logging.Entry{
+			Timestamp: time.Now(),
+			Severity:  logging.Critical,
+			Trace:     trace,
+			Payload: loggingPayload{
+				Message:   fmt.Sprintf("failed create firestore client, got error: %v", err),
+				Operation: "ci-force-bot initialization on tooling kyma",
+				Type:      errorReportingType,
+			},
+		})
+	}
+	logger.Flush()
 }
 
 func receive(event cloudevents.Event) {
 	defer logger.Flush()
-	// do something with event.
 	ctx := context.Background()
-	log.Infof("got event of type %s", event.Type())
+
+	randomInt := rand.Int()
+	trace := fmt.Sprintf("projects/%s/traces/%s/%d", conf.ProjectID, conf.Component, randomInt)
+	logger := loggingClient.Logger(conf.LogName, logging.CommonLabels(map[string]string{
+		"appName":   conf.AppName,
+		"component": conf.Component,
+		"function":  "closeFailingTestInstance"}))
+	// do something with event.
+	logger.Log(logging.Entry{
+		Timestamp: time.Now(),
+		Severity:  logging.Info,
+		Trace:     trace,
+		Payload: loggingPayload{
+			Message:   fmt.Sprintf("got event of type %s", event.Type()),
+			Operation: "processing received cloudevents event",
+		},
+	})
+
 	issueEvent := new(github.IssuesEvent)
 	err := event.DataAs(issueEvent)
+
 	if err != nil {
-		log.Errorf("load issue event data failed")
+		logger.Log(logging.Entry{
+			Timestamp: time.Now(),
+			Severity:  logging.Critical,
+			Trace:     trace,
+			Payload: loggingPayload{
+				Message:   fmt.Sprintf("loading issue event data failed, got error: %v", err),
+				Operation: "processing received cloudevents event",
+				Type:      errorReportingType,
+			},
+		})
 	}
-	log.Infof("%d", issueEvent.Issue.GetNumber())
+	logger.Log(logging.Entry{
+		Timestamp: time.Now(),
+		Severity:  logging.Info,
+		Trace:     trace,
+		Payload: loggingPayload{
+			Message:   fmt.Sprintf("received issue closed notification for issue number: %d", issueEvent.Issue.GetNumber()),
+			Operation: "processing received cloudevents event",
+		},
+	})
+
 	iter := firestoreClient.Collection("testFailures").Where("open", "==", true).Where("githubIssueNumber", "==", issueEvent.Issue.GetNumber()).Documents(ctx)
 	failureInstances, err := iter.GetAll()
+
 	if err != nil {
-		log.Fatalf(fmt.Sprintf("failed get failure instances, error: %s", err.Error()))
+		logger.Log(logging.Entry{
+			Timestamp: time.Now(),
+			Severity:  logging.Error,
+			Trace:     trace,
+			Payload: loggingPayload{
+				Message:   fmt.Sprintf("failed get failure instances from firestore, got error: %v", err),
+				Operation: "processing received cloudevents event",
+				Type:      errorReportingType,
+			},
+		})
 	}
+
 	if len(failureInstances) == 1 {
 		failureInstance := failureInstances[0]
 		// Set failing test instance in firestore as closed.
-		_, err = failureInstance.Ref.Set(ctx, map[string]bool{"open": false}, firestore.Merge([]string{"open"}))
-		// TODO: add comment on github issue about closing test instance with respective number.
-		// TODO: add logging to stackdriver.
+		_, err := failureInstance.Ref.Set(ctx, map[string]bool{"open": false}, firestore.Merge([]string{"open"}))
+
+		if err != nil {
+			logger.Log(logging.Entry{
+				Timestamp: time.Now(),
+				Severity:  logging.Error,
+				Trace:     trace,
+				Payload: loggingPayload{
+					Message:   fmt.Sprintf("failed set failure instances as closed in firestore for github issue number %d, got error: %v", issueEvent.Issue.GetNumber(), err),
+					Operation: "processing received cloudevents event",
+					Type:      errorReportingType,
+				},
+			})
+		}
+		logger.Log(logging.Entry{
+			Timestamp: time.Now(),
+			Severity:  logging.Info,
+			Trace:     trace,
+			Payload: loggingPayload{
+				Message:   fmt.Sprintf("closed failing test instance in firestore for github issue number %d", issueEvent.Issue.GetNumber()),
+				Operation: "processing received cloudevents event",
+			},
+		})
 	} else if len(failureInstances) == 0 {
-		log.Infof("could not found open failing test instance for github issue number %d", issueEvent.Issue.GetNumber())
+		logger.Log(logging.Entry{
+			Timestamp: time.Now(),
+			Severity:  logging.Info,
+			Trace:     trace,
+			Payload: loggingPayload{
+				Message:   fmt.Sprintf("could not found open failing test instance for github issue number %d", issueEvent.Issue.GetNumber()),
+				Operation: "processing received cloudevents event",
+			},
+		})
 	} else if len(failureInstances) > 1 {
-		// TODO: Report failure to stackdriver.
-		log.Fatalf("to many open failing test instance found in firestore")
-	}
-	err = logger.Flush()
-	if err != nil {
-		log.Errorf("failed send log entry to cloud logging: got error %v", err)
+		logger.Log(logging.Entry{
+			Timestamp: time.Now(),
+			Severity:  logging.Error,
+			Trace:     trace,
+			Payload: loggingPayload{
+				Message:   fmt.Sprintf("more than one open failing test instance found in firestore for github issue number %d", issueEvent.Issue.GetNumber()),
+				Operation: "processing received cloudevents event",
+				Type:      errorReportingType,
+			},
+		})
 	}
 }
 
 func main() {
 	defer logger.Flush()
+	randomInt := rand.Int()
+	trace := fmt.Sprintf("projects/%s/traces/%s/%d", conf.ProjectID, conf.Component, randomInt)
 	logger.Log(logging.Entry{
 		Timestamp: time.Now(),
 		Severity:  logging.Info,
+		Trace:     trace,
 		Payload: loggingPayload{
-			Message:   "Test log message",
-			Component: "kyma.tooling.ci-force-bot",
-			Operation: "closeFailingTestInstance",
+			Message:   fmt.Sprintf("eventing service URL: %s", conf.KymaEventsService),
+			Operation: "starting ci-force bot on tooling kyma",
 		},
-		Trace: "testTrace",
 	})
-	var conf Config
-	err := envconfig.Init(&conf)
-	if err != nil {
-		log.Fatal("failed create config from env variables: ", err.Error())
-	}
-	log.Infof("eventing service URL: %s", conf.KymaEventsService)
-
-	client, err := cloudevents.NewClientHTTP(cloudevents.WithPort(conf.ListenPort))
-	if err != nil {
-		log.Fatalf("failed create cloudevents client: %s", err.Error())
-	}
-	err = logger.Flush()
+	err := logger.Flush()
 	if err != nil {
 		log.Errorf("failed send log entry to cloud logging: got error %v", err)
 	}
-	err = client.StartReceiver(context.Background(), receive)
+
+	err = cloudeventsClient.StartReceiver(context.Background(), receive)
+
 	if err != nil {
-		log.Errorf("failed listen for cloudevents, error: %s", err.Error())
+		logger.Log(logging.Entry{
+			Timestamp: time.Now(),
+			Severity:  logging.Critical,
+			Trace:     trace,
+			Payload: loggingPayload{
+				Message:   fmt.Sprintf("failed start listening for cloudevents, got error: %v", err),
+				Operation: "starting ci-force bot on tooling kyma",
+				Type:      errorReportingType,
+			},
+		})
 	}
 }
