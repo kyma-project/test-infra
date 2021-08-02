709b63c5fcfa8adf7dfab68526e313aa14b51da4
Benjamin Lindner <benjamin.lindner@sap.com>
2021-08-02 13:30:15 +0200

    Fix path

--- prow/scripts/cluster-integration/kyma-integration-k3d.sh
+++ prow/scripts/cluster-integration/kyma-integration-k3d.sh
@@ -51,8 +51,9 @@ function install_cli() {
 }
 
 function deploy_kyma() {
-  kyma alpha provision k3s --ci --verbose --k3d-arg "--registry-config ${KYMA_SOURCES_DIR}/tests/fast-integration/provisioner/registries.yaml"
-  
+  pwd
+  ls
+  kyma alpha provision k3s --ci --verbose --k3d-arg "--registry-config=${KYMA_SOURCES_DIR}/tests/fast-integration/provisioner/registries.yaml"
   k3d cluster list
 
   if [[ -v CENTRAL_APPLICATION_GATEWAY_ENABLED ]]; then
