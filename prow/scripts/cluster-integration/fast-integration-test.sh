f545a98ba8c9c3469ae1f077d5c0f7cb77f1d4a0
Piotr Halama <piotr.halama@sap.com>
2021-08-03 13:58:22 +0200

    refactor

--- prow/scripts/cluster-integration/fast-integration-test.sh
+++ prow/scripts/cluster-integration/fast-integration-test.sh
@@ -21,8 +21,7 @@ requiredVars=(
 
 utils::check_required_vars "${requiredVars[@]}"
 
-# get cluster kubeconfig
-if [[ $CLUSTER_PROVIDER == "azure" ]]; then
+function connect_to_azure_cluster() {
     # shellcheck source=prow/scripts/lib/azure.sh
     source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/lib/azure.sh"
 
@@ -31,15 +30,16 @@ if [[ $CLUSTER_PROVIDER == "azure" ]]; then
 
     # az::set_subscription -s "$AZURE_SUBSCRIPTION_ID"
     az aks get-credentials --resource-group "${RS_GROUP}" --name "${INPUT_CLUSTER_NAME}"
+}
 
-elif [[ $CLUSTER_PROVIDER == "gcp" ]]; then
+function connect_to_gcp_cluster() {
     # shellcheck source=prow/scripts/lib/gcp.sh
     source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/lib/gcp.sh"
 
     requiredVars=(
         CLOUDSDK_CORE_PROJECT
-	    CLOUDSDK_COMPUTE_REGION
-	    CLOUDSDK_COMPUTE_ZONE
+        CLOUDSDK_COMPUTE_REGION
+        CLOUDSDK_COMPUTE_ZONE
     )
     utils::check_required_vars "${requiredVars[@]}"
 
@@ -55,16 +55,30 @@ elif [[ $CLUSTER_PROVIDER == "gcp" ]]; then
     -z "$CLOUDSDK_COMPUTE_ZONE" \
     -R "$CLOUDSDK_COMPUTE_REGION" \
     -r "$PROVISION_REGIONAL_CLUSTER"
-else
-    ## TODO what should I put here? Is this a backend?
-    log::error "GARDENER_PROVIDER ${CLUSTER_PROVIDER} is not yet supported"
-    exit 1
-fi
+}
 
-log::info "Running Kyma Fast Integration tests"
+function connect_to_cluster() {
+    if [[ $CLUSTER_PROVIDER == "azure" ]]; then
+        connect_to_azure_cluster
+    elif [[ $CLUSTER_PROVIDER == "gcp" ]]; then
+        connect_to_gcp_cluster
+    else
+        ## TODO what should I put here? Is this a backend?
+        log::error "GARDENER_PROVIDER ${CLUSTER_PROVIDER} is not yet supported"
+        exit 1
+    fi
+}
 
-pushd /home/prow/go/src/github.com/kyma-project/kyma/tests/fast-integration
-make ci-no-install
-popd
+function test_fast_integration_kyma() {
+    log::info "Running Kyma Fast Integration tests"
+
+    pushd /home/prow/go/src/github.com/kyma-project/kyma/tests/fast-integration
+    make ci-no-install
+    popd
+}
+
+connect_to_cluster
+
+test_fast_integration_kyma
 
 log::success "Tests completed"
