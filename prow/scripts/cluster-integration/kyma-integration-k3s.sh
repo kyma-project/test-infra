985408bef8f178541b2ec4c0ba45e31207a4e4e4
Benjamin Lindner <benjamin.lindner@sap.com>
2021-08-06 11:18:48 +0200

    Flags

--- prow/scripts/cluster-integration/kyma-integration-k3s.sh
+++ prow/scripts/cluster-integration/kyma-integration-k3s.sh
@@ -66,7 +66,7 @@ function deploy_kyma() {
 
   if [[ -v CENTRAL_APPLICATION_GATEWAY_ENABLED ]]; then
     kyma_deploy_cmd+=" --value application-connector.central_application_gateway.enabled=true"
-    kyma_deploy_cmd+=" --value application-connector.central_application_connectivity_validator.enabled=true"
+    kyma_deploy_cmd+=" --value global.centralApplicationConnectivityValidatorEnabled=true"
   fi
 
   if [[ -v COMPASS_INTEGRATION_ENABLED ]]; then
