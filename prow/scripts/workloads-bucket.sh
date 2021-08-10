03a9c54814a42385edf3761a8e0e7ad6d0a54908
Piotr Halama <piotr.halama@sap.com>
2021-08-10 16:48:17 +0200

    ah, init

--- prow/scripts/workloads-bucket.sh
+++ prow/scripts/workloads-bucket.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# shellcheck source=prow/scripts/lib/docker.sh
+source "${SCRIPT_DIR}/lib/docker.sh"
+
+docker::start
+
 docker pull alpine:edge
 docker tag alpine:edge eu.gcr.io/sap-kyma-prow-workloads/alpine_edge
 docker push eu.gcr.io/sap-kyma-prow-workloads/alpine_edge
