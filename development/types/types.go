0f4f50455875bf5f6ee6ad3cd50765610a551187
Przemek Pokrywka <dekiel@users.noreply.github.com>
2021-08-13 11:51:25 +0200

    Get commiter for failure (#4026)
    * init logging package
    init github client package
    init usersmapchecker
    
    * rendertemplates
    
    * rename package to main
    
    * check users map prowjob
    pjtester
    
    * check users map prowjob
    pjtester
    
    * add kyma-bot.yaml
    
    * imports
    
    * goimports
    
    * use pr image
    
    * check users map prowjob optional
    flush gcp logs
    
    * correct stdout log messages severity
    
    * correct stdout log messages severity
    
    * set wait group count before function
    
    * explicit call wg.Done()
    
    * graceful exit with defers
    
    * logger.Flush() testing
    
    * fix interface conversion
    
    * log info when user in map
    
    * another roundd of tests
    
    * more info logging
    
    * link to users map
    
    * commented code
    added getgithubcommiter implementation
    
    * disabled pjtester
    
    * fix typo

---
+++ development/types/types.go
@@ -0,0 +1,9 @@
+package types
+
+// User holds kyma development team user details.
+// It provides mapping of various details used for integration different systems.
+type User struct {
+	ComGithubUsername          string `yaml:"com.github.username,omitempty"`
+	SapToolsGithubUsername     string `yaml:"sap.tools.github.username,omitempty"`
+	ComEnterpriseSlackUsername string `yaml:"com.enterprise.slack.username,omitempty"`
+}
