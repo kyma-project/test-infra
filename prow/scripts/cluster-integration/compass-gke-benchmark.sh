01ae9bac5be5a5804cde0706ecdd3419ee5af87a
Desislava Asenova <9128192+desislavaa@users.noreply.github.com>
2021-09-08 11:05:50 +0300

    Fix Kyma installation for Compass resources  (#4108)
    * Re-apply kyma installation
    
    * Fix benchmark job name
    
    * Remove vpath
    
    * Unify sleep time

--- prow/scripts/cluster-integration/compass-gke-benchmark.sh
+++ prow/scripts/cluster-integration/compass-gke-benchmark.sh
@@ -275,6 +275,8 @@ function installKyma() {
   gsutil cp "${COMPASS_ARTIFACTS}/kyma-installer.yaml" ${TMP_DIR}/kyma-installer.yaml
   gsutil cp "${COMPASS_ARTIFACTS}/is-kyma-installed.sh" ${TMP_DIR}/is-kyma-installed.sh
   chmod +x ${TMP_DIR}/is-kyma-installed.sh
+  kubectl apply -f ${TMP_DIR}/kyma-installer.yaml || true
+  sleep 2
   kubectl apply -f ${TMP_DIR}/kyma-installer.yaml
 
   log::info "Installation triggered. Sleeping for 10 seconds before applying check-kyma-installer..."
