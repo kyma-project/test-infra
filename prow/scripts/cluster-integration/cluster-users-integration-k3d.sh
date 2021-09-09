1f51b7bf522f44b41edf95cbc5e0afbf83f6dd29
Piotr Kopec <piotr@kubermatic.com>
2021-09-09 14:21:19 +0200

    drop tmp helper function

--- prow/scripts/cluster-integration/cluster-users-integration-k3d.sh
+++ prow/scripts/cluster-integration/cluster-users-integration-k3d.sh
@@ -9,19 +9,17 @@ export DOMAIN=${KYMA_DOMAIN:-local.kyma.dev}
 
 export KYMA_SOURCES_DIR="./kyma"
 
-function _tmp_get_kyma_sources() {
-    git clone https://github.com/piotrkpc/kyma tmp_kyma
-    pushd tmp_kyma
-    git checkout cluster-users-hydra
-    KYMA_SOURCES_DIR="./tmp_kyma"
-    popd
-}
-
 function provision_k3d_and_run_testsuite() {
+    pushd $KYMA_SOURCES_DIR
+    git status
+    git branch -v
+    git remote -v
+    git pull
+    popd
     pushd $KYMA_SOURCES_DIR/tests/integration/cluster-users
+    ls -latr .
     bash k3d-cluster-users.sh
 }
-
-_tmp_get_kyma_sources # this should be remove after https://github.com/kyma-project/kyma/pull/12052 is merged
 echo "running tests suite on k3d"
 provision_k3d_and_run_testsuite
+
