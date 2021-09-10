0b25d9a8d20227db00d17fcd7642d33f79470bc0
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2021-09-10 14:10:05 +0200

    debug

--- prow/scripts/cluster-integration/cluster-users-integration-k3d.sh
+++ prow/scripts/cluster-integration/cluster-users-integration-k3d.sh
@@ -10,29 +10,9 @@ export DOMAIN=${KYMA_DOMAIN:-local.kyma.dev}
 export KYMA_SOURCES_DIR="./kyma"
 
 function provision_k3d_and_run_testsuite() {
-    pushd $KYMA_SOURCES_DIR
-    echo 'git status:'
-    git status
-    echo 'git log -n5:'
-    git log -n5
-    echo 'git branch -v:'
-    git branch -v
-    echo 'git remote -v:'
-    git remote -v
-    #echo 'git pull:'
-    #git pull
-    echo "show filesystem:"
-    echo $PWD
-    popd
-
-    ls -al $KYMA_SOURCES_DIR
-    ls -al $KYMA_SOURCES_DIR/tests
-    ls -al $KYMA_SOURCES_DIR/integration
-    ls -al $KYMA_SOURCES_DIR/cluster-users
-
     pushd $KYMA_SOURCES_DIR/tests/integration/cluster-users
-    ls -latr .
     bash k3d-cluster-users.sh
+    popd
 }
 echo "running tests suite on k3d"
 provision_k3d_and_run_testsuite
