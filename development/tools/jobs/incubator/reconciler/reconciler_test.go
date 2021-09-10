1c1d4ba40922fc52ee526b3822eb7ef59ca9fa58
Sayan Hazra <sayan.mnit@gmail.com>
2021-09-10 11:47:51 +0200

    Reconciler test job (#4071)
    * Initial job config with tests
    
    * temp
    
    * shellcheck fixes
    
    * shellcheck fixes
    
    * shellcheck fixes
    
    * fix pjtester config
    
    * shellcheck fixes
    
    * fix pjtester config
    
    * fix pjtester config
    
    * fix gardener script
    
    * fix gardener script
    
    * changed job image to pr iamge with kubectl v1.18
    
    * fixed type error
    
    * fixed syntax errors in reconciler-e2e-gardener.sh
    
    * added log for helm version
    
    * defined /Users/faizan/kubeconfigs/kubeconfig--kymatunas--fzn-rec6.yml in script
    
    * Revert "defined /Users/faizan/kubeconfigs/kubeconfig--kymatunas--fzn-rec6.yml in script"
    
    This reverts commit ea486a977a99d99f33e7545212a20f1f7ac2cd79.
    
    * added few debug lines
    
    * defining KUBECONFIG
    
    * removed debug lines
    
    * increased timeout for test-pod creation
    
    * fixed pattern matching in case statement
    
    * added date logs
    
    * added extra_ref_kyma
    
    * shellcheck disable multiple error codes
    
    * shellcheck disable multiple error codes
    
    * fixed shellcheck errors
    
    * Fix as per PR comments
    
    * updated kyma-intergration image
    
    * added back KYMA_SOURCE
    
    * groomed script
    
    * Scope out functions to a different file
    
    * Scope out functions to a different file
    
    * Refactor
    
    * Fix stuff
    
    * Fix stuff
    
    * Fix merge issues
    
    * Fix merge issues
    
    * Trying
    
    * fixed script
    
    * Fix merge issues
    
    * Fix merge issues
    
    * Fix merge issues
    
    * undeleted reconciler tests changes
    
    * remove pj tester and revert back gomod file
    
    * enabled fast-integration tests
    
    * added pjtester
    
    * changed pjtester prNumber
    
    * Remove pjtester
    
    Co-authored-by: Muhammad Faizan <m.faizan@sap.com>

--- development/tools/jobs/incubator/reconciler/reconciler_test.go
+++ development/tools/jobs/incubator/reconciler/reconciler_test.go
@@ -17,10 +17,10 @@ func TestReconcilerJobsPresubmit(t *testing.T) {
 
 	assert.Len(t, jobConfig.PresubmitsStatic, 1)
 	kymaPresubmits := jobConfig.AllStaticPresubmits([]string{"kyma-incubator/reconciler"})
-	assert.Len(t, kymaPresubmits, 1)
+	assert.Len(t, kymaPresubmits, 2)
 
-	actualPresubmit := kymaPresubmits[0]
 	expName := "pre-main-kyma-incubator-reconciler"
+	actualPresubmit := tester.FindPresubmitJobByName(kymaPresubmits, expName)
 	assert.Equal(t, expName, actualPresubmit.Name)
 	assert.Equal(t, []string{"^main$"}, actualPresubmit.Branches)
 	assert.Equal(t, 10, actualPresubmit.MaxConcurrency)
@@ -32,6 +32,34 @@ func TestReconcilerJobsPresubmit(t *testing.T) {
 	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, preset.DindEnabled, preset.DockerPushRepoIncubator, preset.GcrPush)
 	assert.Equal(t, tester.ImageGolangBuildpack1_16, actualPresubmit.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build-generic.sh"}, actualPresubmit.Spec.Containers[0].Command)
+	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-incubator/reconciler"}, actualPresubmit.Spec.Containers[0].Args)
+}
+
+func TestReconcilerJobsPresubmitE2E(t *testing.T) {
+	// WHEN
+	jobConfig, err := tester.ReadJobConfig("./../../../../../prow/jobs/incubator/reconciler/reconciler.yaml")
+	// THEN
+	require.NoError(t, err)
+
+	assert.Len(t, jobConfig.PresubmitsStatic, 1)
+	kymaPresubmits := jobConfig.AllStaticPresubmits([]string{"kyma-incubator/reconciler"})
+	assert.Len(t, kymaPresubmits, 2)
+
+	expName := "pre-main-kyma-incubator-reconciler-e2e"
+	actualPresubmit := tester.FindPresubmitJobByName(kymaPresubmits, expName)
+	assert.Equal(t, expName, actualPresubmit.Name)
+	assert.Equal(t, []string{"^master$", "^main$"}, actualPresubmit.Branches)
+	assert.Equal(t, 10, actualPresubmit.MaxConcurrency)
+	assert.False(t, actualPresubmit.SkipReport)
+
+	assert.False(t, actualPresubmit.AlwaysRun)
+	assert.Equal(t, actualPresubmit.RunIfChanged, "^resources")
+	tester.AssertThatHasExtraRefTestInfra(t, actualPresubmit.JobBase.UtilityConfig, "main")
+
+	// @TODO: For testing using pr image name. Update to tester.xxx
+	assert.Equal(t, "eu.gcr.io/kyma-project/test-infra/kyma-integration:v20210902-035ae0cc-k8s1.18", actualPresubmit.Spec.Containers[0].Image)
+	//assert.Equal(t, tester.ImageKymaIntegrationLatest, actualPresubmit.Spec.Containers[0].Image)
+	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/cluster-integration/reconciler-e2e-gardener.sh"}, actualPresubmit.Spec.Containers[0].Command)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-incubator/reconciler"}, actualPresubmit.Spec.Containers[0].Args)
 }
 
