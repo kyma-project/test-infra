027e1e0bc9d8a92bed6cd7fac19bc048cf4c2951
Piotr Halama <piotr.halama@sap.com>
2021-09-17 12:53:04 +0200

    Fix long-lasting jobs

--- prow/scripts/cluster-integration/kyma-aks-long-lasting.sh
+++ prow/scripts/cluster-integration/kyma-aks-long-lasting.sh
@@ -215,7 +215,7 @@ function installKyma() {
 
     kyma install \
 			--ci \
-			--source "release-1.24" \
+			--source "1.24.6" \
       -o "$PWD/kyma-installer-overrides.yaml" \
 			-o "$PWD/overrides-dex-and-monitoring.yaml" \
 			-o "${TEST_INFRA_SOURCES_DIR}/prow/scripts/resources/prometheus-cluster-essentials-overrides.tpl.yaml" \
