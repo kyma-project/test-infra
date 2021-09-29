5965534b85fceadd0c16e06253a081d3726a9e14
I332735 <suleyman.akbas@sap.com>
2021-09-29 13:57:33 +0200

    Fix

--- prow/scripts/cluster-integration/reconciler-integration-k3d.sh
+++ prow/scripts/cluster-integration/reconciler-integration-k3d.sh
@@ -44,6 +44,7 @@ function run_tests() {
   echo "Install Go"
   wget -q https://golang.org/dl/go1.16.6.linux-amd64.tar.gz && sudo tar -C /usr/local -xzf go1.16.6.linux-amd64.tar.gz && export PATH=$PATH:/usr/local/go/bin && go version
 
+  export KUBECONFIG=~/.kube/config
   pushd "${RECONCILER_DIR}"
   make test-all
   popd
