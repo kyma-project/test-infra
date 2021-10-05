8b22adf3219bbed383b3341ee0cd1f1d76b99ed5
dekiel <przemyslaw.pokrywka@sap.com>
2021-10-05 17:46:58 +0200

    add github issue to the message

--- development/kyma-slack-connector/notifyCommiterFunction/handler.py
+++ development/kyma-slack-connector/notifyCommiterFunction/handler.py
@@ -8,13 +8,13 @@ from slack_sdk.errors import SlackApiError
 
 def main(event, context):
 	# Using SLACK_BOT_TOKEN environment variable
-	app = App(
-	)
+	app = App()
 	slack_api_id = os.environ['SLACK_API_ID'].replace('-', '_')
 	env_prefix = os.environ['ENV_PREFIX']
 	base_url = os.environ['{}_SLACK_CONNECTOR_{}_GATEWAY_URL'.format(env_prefix, slack_api_id)]
 	# Set Slack API base URL to the URL of slack-connector application gateway.
 	app.client.base_url = "{}/".format(base_url)
+	result = None
 	print("received message with id: {}".format(event["data"]["ID"]))
 	print("slack api base URL: {}".format(app.client.base_url))
 	print("sending notification to channel: {}".format(os.environ['NOTIFICATION_SLACK_CHANNEL']))
@@ -31,11 +31,11 @@ def main(event, context):
 				else:
 					slack_users = "<@{}>".format(commiter)
 		if slack_users != "":
-			notify_msg = "{} please check what's going on".format(slack_users)
+			notify_msg = "{} please check what's wrong".format(slack_users)
 		else:
-			notify_msg = "<!here>, couldn't find commiter slack username, please check this failure or ask commiter for it."
+			notify_msg = "<!here>, couldn't find commiter slack username, please check what's wrong or ask commiter for it."
 	else:
-		notify_msg = "<!here>, couldn't find commiter slack username, please check this failure or ask commiter for it."
+		notify_msg = "<!here>, couldn't find commiter slack username, please check what's wrong or ask commiter for it."
 	print(notify_msg)
 	channel_name = "kyma-prow-dev-null"
 	conversation_id = None
@@ -48,70 +48,106 @@ def main(event, context):
 			for channel in response["channels"]:
 				if channel["name"] == channel_name:
 					conversation_id = channel["id"]
-					#Print result
+					# Print result
 					print(f"Found conversation ID: {conversation_id}")
 					break
-# https://api.slack.com/messaging/retrieving
-# https://api.slack.com/methods/conversations.history#response
-# https://api.slack.com/methods/search.messages
-# https://slack.dev/bolt-python/tutorial/getting-started
-# https://api.slack.com/start/building/bolt-python
-# https://api.slack.com/messaging/retrieving
-# https://api.slack.com/methods/conversations.list
-# https://console.cloud.google.com/logs/query;query=error_group%2528%22CPec6unB58T4nQE%22%2529%0AlogName:%22prowjobs%22%0Aresource.type%3D%22gce_instance%22%0Aresource.labels.instance_id%3D%225616924538911123302%22;timeRange=2021-10-01T13:57:51.670Z%2F2021-10-01T14:57:51.670Z?project=sap-kyma-prow&supportedpurview=project - this should not be an error in gcp.
 	except SlackApiError as e:
 		print(f"Error: {e}")
 	conversation_history = []
 	try:
-	# Call the conversations.history method using the WebClient
-	# conversations.history returns the first 100 messages by default
-	# These results are paginated, see: https://api.slack.com/methods/conversations.history$pagination
-		result = app.client.conversations_history(channel=conversation_id, limit=10)
-		conversation_history = result["messages"]
+		# Call the conversations.history method using the WebClient
+		# conversations.history returns the first 100 messages by default
+		# These results are paginated, see: https://api.slack.com/methods/conversations.history$pagination
+		response = app.client.conversations_history(channel=conversation_id, limit=10)
+		conversation_history = response["messages"]
 	except SlackApiError as e:
 		print("Error creating conversation: {}".format(e))
 	for message in conversation_history:
-		if msg["url"] in message["text"]:
-			thread_id = message["ts"]
-			break
-	try:
-		# Deliver message to the channel.
-		# https://slack.dev/python-slack-sdk/api-docs/slack_sdk/web/slack_response.html#slack_sdk.web.slack_response.SlackResponse
-		result = app.client.chat_postMessage(channel=os.environ['NOTIFICATION_SLACK_CHANNEL'],
-											text="{} prowjob {} execution failed, view logs: {}".format(msg["job_type"], msg["job_name"], msg["url"]),
-											username="CiForceBot",
-											icon_emoji="https://www.stickpng.com/img/download/580b57fbd9996e24bc43bdfe/image",
-											link_names="true",
-											blocks=[
-												{
-													"type": "header",
-													"text": {
-														"type": "plain_text",
-														"text": "Prowjob execution failed"
-													}
-												},
-												{
-													"type": "section",
-													"text": {
-														"type": "mrkdwn",
-														"text": "*Name:* {}\n*Type:* {}\n<{}|*View logs*>".format(
-															msg["job_name"], msg["job_type"], msg["url"])
-													}
-												},
-												{
-													"type": "section",
-													"text": {
-														"type": "mrkdwn",
-														"text": "{}".format(notify_msg)
-													}
-												}
-											])
-		assert result.get("ok", False), "Assert response from slack API is OK failed. This is critical error."
-		print("sent notification for message id: {}".format(event["data"]["ID"]))
-	# https://slack.dev/python-slack-sdk/api-docs/slack_sdk/errors/index.html#slack_sdk.errors.SlackApiError
-	except SlackApiError as e:
-		# https://slack.dev/python-slack-sdk/api-docs/slack_sdk/web/slack_response.html#slack_sdk.web.slack_response.SlackResponse
-		assert e.response.get("ok", False) is False,\
-			"Assert response from slack API is not OK failed. This should not be error."
-		print(f"Got an error: {e.response['error']}")
-		print("failed sent notification for message id: {}".format(event["data"]["ID"]))
+		try:
+			if msg["url"] in message["text"]:
+				if "thread_ts" in message:
+					thread_id = message["thread_ts"]
+				else:
+					thread_id = message["ts"]
+				result = app.client.chat_postMessage(channel=os.environ['NOTIFICATION_SLACK_CHANNEL'],
+													 thread_ts=thread_id,
+													 text="Created issue #{} https://github.com/kyma-test-infra-dev/kyma/issues/{}".format(
+														 msg["githubIssueNumber"],
+														 msg["githubIssueNumber"]),
+													 username="CiForceBot",
+													 link_names=True,
+													 blocks=[
+														 {
+															 "type": "header",
+															 "text": {
+																 "type": "plain_text",
+																 "text": "Github issue created"
+															 }
+														 },
+														 {
+															 "type": "section",
+															 "text": {
+																 "type": "mrkdwn",
+																 # project and repostiory should be variabels provided by pubsub message.
+																 "text": "<https://github.com/kyma-test-infra-dev/kyma/issues/{}|*Issue #{} created.*>".format(
+																	 msg["githubIssueNumber"],
+																	 msg["githubIssueNumber"])
+															 }
+														 },
+														 {
+															 "type": "section",
+															 "text": {
+																 "type": "mrkdwn",
+																 "text": "{}".format(notify_msg)
+															 }
+														 }
+													 ])
+				break
+			if thread_id is None:
+				result = app.client.chat_postMessage(channel=os.environ['NOTIFICATION_SLACK_CHANNEL'],
+													 thread_ts=thread_id,
+													 text="{} prowjob {} execution failed, view logs: {}, issue #{}: https://github.com/kyma-test-infra-dev/kyma/issues/{}".format(
+														 msg["job_type"],
+														 msg["job_name"],
+														 msg["url"],
+														 msg["githubIssueNumber"],
+														 msg["githubIssueNumber"]),
+													 username="CiForceBot",
+													 link_names=True,
+													 blocks=[
+														 {
+															 "type": "header",
+															 "text": {
+																 "type": "plain_text",
+																 "text": "Prowjob execution failed"
+															 }
+														 },
+														 {
+															 "type": "section",
+															 "text": {
+																 "type": "mrkdwn",
+																 "text": "*Name:* {}\n*Type:* {}\n<{}|*View logs*>\n<https://github.com/kyma-test-infra-dev/kyma/issues/{}|*Issue #{}*>".format(
+																	 msg["job_name"],
+																	 msg["job_type"],
+																	 msg["url"],
+																	 msg["githubIssueNumber"],
+																	 msg["githubIssueNumber"])
+															 }
+														 },
+														 {
+															 "type": "section",
+															 "text": {
+																 "type": "mrkdwn",
+																 "text": "{}".format(notify_msg)
+															 }
+														 }
+													 ])
+			assert result.get("ok", False), "Assert response from slack API is OK failed. This is critical error."
+			print("sent notification for message id: {}".format(event["data"]["ID"]))
+		# https://slack.dev/python-slack-sdk/api-docs/slack_sdk/errors/index.html#slack_sdk.errors.SlackApiError
+		except SlackApiError as e:
+			# https://slack.dev/python-slack-sdk/api-docs/slack_sdk/web/slack_response.html#slack_sdk.web.slack_response.SlackResponse
+			assert e.response.get("ok", False) is False, \
+				"Assert response from slack API is not OK failed. This should not be error."
+			print(f"Got an error: {e.response['error']}")
+			print("failed sent notification for message id: {}".format(event["data"]["ID"]))
