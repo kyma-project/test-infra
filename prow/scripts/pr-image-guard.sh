18b622e76ca108e64f959b78de67e2d2fb09da40
Ammar Lakis <ammar.lakis@gmail.com>
2021-10-11 11:18:03 +0200

    Remove disabled SC2126
    Signed-off-by: Ammar Lakis <ammar.lakis@gmail.com>

--- prow/scripts/pr-image-guard.sh
+++ prow/scripts/pr-image-guard.sh
@@ -7,7 +7,6 @@ RESOURCES_PATH="/home/prow/go/src/github.com/kyma-project/kyma/resources"
 readonly TESTS_PATH
 TESTS_PATH="/home/prow/go/src/github.com/kyma-project/kyma/tests"
 
-# shellcheck disable=SC2126
 RES=$(grep -e 'version:\s.*[Pp][Rr]-.*' -e 'image:.*:[Pp][Rr]-.*' -e 'tag:\s.*[Pp][Rr]-.*' -r "${RESOURCES_PATH}" -r "${TESTS_PATH}" -B 2 || true)
 echo -n "$RES"
 exit "$(echo -n "$RES" | wc -l | xargs)"
