fac8d23cf9aa3096241b405fc520573192eb25d3
StanislavStefanov <39959090+StanislavStefanov@users.noreply.github.com>
2021-10-12 15:02:30 +0300

    Adjust compass authenticators config (#4292)
    * adjust authenticators configuration
    
    * add pjtester
    
    * fix path
    
    * remove vpath
    
    * remove unneeded configuration

--- prow/scripts/cluster-integration/compass-gke-benchmark.sh
+++ prow/scripts/cluster-integration/compass-gke-benchmark.sh
@@ -230,7 +230,8 @@ function applyCompassOverrides() {
     --data "global.systemFetcher.oauth.scopesClaim=scopes" \
     --data "global.systemFetcher.oauth.tenantHeaderName=x-zid" \
     --data "global.kubernetes.serviceAccountTokenJWKS=https://container.googleapis.com/v1beta1/projects/$CLOUDSDK_CORE_PROJECT/locations/$CLOUDSDK_COMPUTE_ZONE/clusters/$COMMON_NAME/jwks" \
-    --data "global.authenticators.tenant-fetcher.enabled=true" \
+    --data "global.oathkeeper.mutators.authenticationMappingServices.tenantFetcher.authenticator.enabled=true" \
+    --data "global.oathkeeper.mutators.authenticationMappingServices.subscriber.authenticator.enabled=true" \
     --data "system-broker.http.client.skipSSLValidation=true" \
     --data "operations-controller.http.client.skipSSLValidation=true" \
     --data "global.systemFetcher.http.client.skipSSLValidation=true" \
