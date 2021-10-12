35a60a20d36f6b6f537b81f5c6cfa87b45f88640
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2021-10-12 09:43:57 +0300

    adjust authenticators configuration

--- prow/scripts/cluster-integration/compass-gke-benchmark.sh
+++ prow/scripts/cluster-integration/compass-gke-benchmark.sh
@@ -230,7 +230,10 @@ function applyCompassOverrides() {
     --data "global.systemFetcher.oauth.scopesClaim=scopes" \
     --data "global.systemFetcher.oauth.tenantHeaderName=x-zid" \
     --data "global.kubernetes.serviceAccountTokenJWKS=https://container.googleapis.com/v1beta1/projects/$CLOUDSDK_CORE_PROJECT/locations/$CLOUDSDK_COMPUTE_ZONE/clusters/$COMMON_NAME/jwks" \
-    --data "global.authenticators.tenant-fetcher.enabled=true" \
+    --data "global.oathkeeper.mutators.authenticationMappingServices.tenant-fetcher.authenticator.enabled=true" \
+    --data "global.oathkeeper.mutators.authenticationMappingServices.tenant-fetcher.authenticator.createRule=true" \
+    --data "global.oathkeeper.mutators.authenticationMappingServices.subscriber.authenticator.enabled=true" \
+    --data "global.oathkeeper.mutators.authenticationMappingServices.subscriber.authenticator.createRule=false" \
     --data "system-broker.http.client.skipSSLValidation=true" \
     --data "operations-controller.http.client.skipSSLValidation=true" \
     --data "global.systemFetcher.http.client.skipSSLValidation=true" \
