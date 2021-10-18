33eff3d1f2f7d28a79d61cc3fa839c0adca1dbb7
Piotr Halama <piotr.halama@sap.com>
2021-10-18 15:39:55 +0200

    make variables local

--- development/tools/cmd/imagechecker/main.go
+++ development/tools/cmd/imagechecker/main.go
@@ -12,16 +12,15 @@ import (
 )
 
 var (
-	skipComentsInfo   = ""
-	foundIncompatible = false
-
 	kymaResourcesDirectory = flag.String("kymaDirectory", "/home/prow/go/src/github.com/kyma-project/kyma/resources/", "Path to Kyma resources")
 	skipComments           = flag.Bool("skipComments", true, "Skip commented out lines")
 )
 
 func main() {
 	flag.Parse()
+	foundIncompatible := false
 
+	skipComentsInfo := ""
 	if *skipComments {
 		skipComentsInfo = ", excluding commented out lines"
 	}
@@ -29,7 +28,7 @@ func main() {
 	// for all files in resources
 	fmt.Printf("Looking for incompatible images in \"%s\"%s:\n\n", *kymaResourcesDirectory, skipComentsInfo)
 
-	err := filepath.Walk(*kymaResourcesDirectory, walkFunction)
+	err := filepath.Walk(*kymaResourcesDirectory, getWalkFunc(&foundIncompatible))
 	if err != nil {
 		fmt.Printf("Cannot traverse directory: %s", err)
 		os.Exit(2)
@@ -43,31 +42,33 @@ func main() {
 	}
 }
 
-func walkFunction(path string, info fs.FileInfo, err error) error {
-	//pass the error further, this shouldn't ever happen
-	if err != nil {
-		return err
-	}
+func getWalkFunc(foundIncompatible *bool) filepath.WalkFunc {
+	return func(path string, info fs.FileInfo, err error) error {
+		//pass the error further, this shouldn't ever happen
+		if err != nil {
+			return err
+		}
 
-	// skip directory entries, we just want files
-	if info.IsDir() {
+		// skip directory entries, we just want files
+		if info.IsDir() {
+			return nil
+		}
+
+		// we only want to check .yaml files
+		if !strings.Contains(info.Name(), ".yaml") {
+			return nil
+		}
+
+		// check if this file contains any image: lines that aren't using new templates
+		incompatible, err := imagechecker.FileHasIncorrectImage(path, *skipComments)
+		if err != nil {
+			return nil
+		}
+
+		if incompatible {
+			*foundIncompatible = true
+		}
+
 		return nil
 	}
-
-	// we only want to check .yaml files
-	if !strings.Contains(info.Name(), ".yaml") {
-		return nil
-	}
-
-	// check if this file contains any image: lines that aren't using new templates
-	incompatible, err := imagechecker.FileHasIncorrectImage(path, *skipComments)
-	if err != nil {
-		return nil
-	}
-
-	if incompatible {
-		foundIncompatible = true
-	}
-
-	return nil
 }
