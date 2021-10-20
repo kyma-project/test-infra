593efaee2cd4a3e87a0aaae3de9ed1e8b4397fab
Wojciech Sołtys <wojciech.soltys@sap.com>
2021-10-20 14:02:30 +0200

    Improve script

--- prow/scripts/cluster-integration/busola-integration-test-docker.sh
+++ prow/scripts/cluster-integration/busola-integration-test-docker.sh
@@ -5,11 +5,12 @@ set -o pipefail
 
 CYPRESS_IMAGE="eu.gcr.io/kyma-project/external/cypress/included:8.6.0"
 
+mkdir -p "$PWD/busola-tests/cypress/screenshots"
+
 # shellcheck disable=SC2086
-docker run --rm -it --net=host --pid=host --name busola eu.gcr.io/kyma-project/busola:latest
+docker run --rm --net=host --pid=host --name busola eu.gcr.io/kyma-project/busola:latest
 
 cp "$PWD/kubeconfig-kyma.yaml" "$PWD/busola-tests/fixtures/kubeconfig.yaml"
-mkdir -p "$PWD/busola-tests/cypress/screenshots"
 
 echo "STEP: Running Cypress tests inside Docker"
 docker run --entrypoint /bin/bash --network=host -v "$PWD/busola-tests:/tests" -w /tests $CYPRESS_IMAGE -c "npm ci --no-optional; NO_COLOR=1 CYPRESS_DOMAIN=localhost:3001 cypress run --browser chrome"
