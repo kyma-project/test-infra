3757aec0700e5a92ae73e1318cb9a303a6825dba
Wojciech Sołtys <wojciech.soltys@sap.com>
2021-10-20 15:48:41 +0200

    Add generating docker tag

--- prow/scripts/cluster-integration/busola-integration-test-docker.sh
+++ prow/scripts/cluster-integration/busola-integration-test-docker.sh
@@ -7,9 +7,20 @@ CYPRESS_IMAGE="eu.gcr.io/kyma-project/external/cypress/included:8.6.0"
 
 mkdir -p "$PWD/busola-tests/cypress/screenshots"
 
+if [ -n "${PULL_NUMBER}" ]; then
+  echo "Building from PR"
+  DOCKER_TAG="PR-${PULL_NUMBER}"
+else
+  # Build artifacts using short SHA for all branches postsubmits
+  echo "Building as usual"
+  DOCKER_TAG="${PULL_BASE_SHA::8}"
+fi
+
+export DOCKER_TAG
+echo DOCKER_TAG "${DOCKER_TAG}"
+
 # shellcheck disable=SC2086
-echo $DOCKER_TAG
-docker run -d --rm --net=host --pid=host --name busola "eu.gcr.io/kyma-project/busola:$DOCKER_TAG"
+docker run -d --rm --net=host --pid=host --name busola eu.gcr.io/kyma-project/busola:$DOCKER_TAG
 
 cp "$PWD/kubeconfig-kyma.yaml" "$PWD/busola-tests/fixtures/kubeconfig.yaml"
 
