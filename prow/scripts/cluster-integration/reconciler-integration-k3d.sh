6db78090a92aae50999f0854a519a58b0191e44a
Dennis Geiselmann <44910886+dennis-ge@users.noreply.github.com>
2021-10-25 17:05:32 +0200

    use kyma alpha provision k3d command (#4322)
    * downgrade k3d to v4
    
    * add pjtester config
    
    * update pjtester config
    
    * use alpha provision k3d command
    
    * remove vpath

--- prow/scripts/cluster-integration/reconciler-integration-k3d.sh
+++ prow/scripts/cluster-integration/reconciler-integration-k3d.sh
@@ -38,7 +38,7 @@ function install_cli() {
 }
 
 function provision_k3d() {
-  kyma provision k3d -p 80:80@loadbalancer -p 443:443@loadbalancer
+  kyma alpha provision k3d -p 80:80@loadbalancer -p 443:443@loadbalancer
 }
 
 function run_tests() {
