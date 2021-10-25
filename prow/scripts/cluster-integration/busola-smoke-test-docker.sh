657d26b277106648ad9413b397a249b71eb06930
Wojciech Sołtys <74361703+Sawthis@users.noreply.github.com>
2021-10-25 09:57:32 +0200

    Add Dockerized Busola integration tests (#4328)
    * Add Dockerized Busola integration tests
    
    * Add generated files
    
    * Remove redundant file
    
    * Fix errors
    
    * Add pjtester
    
    * Improve script
    
    * Improve script
    
    * Change PR number
    
    * Improve script
    
    * Change PR number
    
    * Run Docker in detached mode
    
    * Improve script
    
    * Change PR number
    
    * Change Cypress config
    
    * Add generated file
    
    * Change latest to proper docker tag
    
    * Fix docker image path
    
    * Add echo docker tag
    
    * Add generating docker tag
    
    * Add test docker tag
    
    * Remove export
    
    * Improve scripts
    
    * Improve script
    
    * Pass DOCKER_TAG to GCP
    
    * Pass DOCKER_TAG to GCP
    
    * Fix typo
    
    * Change PR number
    
    * Rename integration to smoke first step
    
    * Change flag name
    
    * Change parameter place
    
    * Add semicolon
    
    * Change path
    
    * Change job name
    
    * Remove pjtester

---
+++ prow/scripts/cluster-integration/busola-smoke-test-docker.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+CYPRESS_IMAGE="eu.gcr.io/kyma-project/external/cypress/included:8.6.0"
+
+mkdir -p "$PWD/busola-tests/cypress/screenshots"
+
+function load_env() {
+  ENV_FILE=".env"
+  if [ -f "${ENV_FILE}" ]; then
+    # shellcheck disable=SC2046
+    export $(xargs < "${ENV_FILE}")
+  fi
+}
+
+load_env
+echo DOCKER_TAG "${DOCKER_TAG}"
+# shellcheck disable=SC2086
+docker run -d --rm --net=host --pid=host --name busola eu.gcr.io/kyma-project/busola:${DOCKER_TAG}
+
+cp "$PWD/kubeconfig-kyma.yaml" "$PWD/busola-tests/fixtures/kubeconfig.yaml"
+
+echo "STEP: Running Cypress tests inside Docker"
+docker run --entrypoint /bin/bash --network=host -v "$PWD/busola-tests:/tests" -w /tests $CYPRESS_IMAGE -c "npm ci --no-optional; NO_COLOR=1 CYPRESS_DOMAIN=http://localhost:3001 cypress run --browser chrome -C /tests/cypress-smoke.json"
