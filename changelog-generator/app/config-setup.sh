ff41321953d23dfb0053b0fa818d32ee5d1f4a7e
Ammar Lakis <ammar.lakis@gmail.com>
2021-11-09 21:34:31 +0100

    Add double quotes to vars to fix SC2086
    Signed-off-by: Ammar Lakis <ammar.lakis@gmail.com>

--- changelog-generator/app/config-setup.sh
+++ changelog-generator/app/config-setup.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
 echo "Copying configuration file..."
-cp ${CONFIG_FILE} ./package.json
+cp "${CONFIG_FILE}" ./package.json
 
 function removeLatestTag() {
     LATEST_TAG_EXISTS=false
-    if [ $(git tag -l "$LATEST_TAG") ]; then
+    if [ "$(git tag -l "$LATEST_TAG")" ]; then
         LATEST_TAG_EXISTS=true
         echo "Temporary removing 'latest' tag..."
-        LATEST_TAG_REV=$(git rev-list -n 1 ${LATEST_TAG})
-        LATEST_TAG_MESSAGE=$(git tag -l --format='%(contents)' ${LATEST_TAG})
-        git tag -d ${LATEST_TAG}
+        LATEST_TAG_REV=$(git rev-list -n 1 "${LATEST_TAG}")
+        LATEST_TAG_MESSAGE=$(git tag -l --format='%(contents)' "${LATEST_TAG}")
+        git tag -d "${LATEST_TAG}"
     fi
 }
 
