6de06300f4b83764d61c67c628d37b9cb973a0a4
Benjamin Lindner <50365642+lindnerby@users.noreply.github.com>
2021-11-12 08:28:49 +0100

    Revert "Temporary switch to alpha provision cmd to use k3d v5 (#4427)" (#4432)
    This reverts commit adce2dc9ae10ffcb0abe5191cf6790bd833fb50a.

--- development/custom-image/install-deps-debian.sh
+++ development/custom-image/install-deps-debian.sh
@@ -19,7 +19,7 @@ CRICTL_VERSION=v1.12.0
 HELM_VERSION="v3.7.1"
 DOCKER_VERSION=5:20.10.5~3-0~debian-buster
 NODEJS_VERSION="14.x"
-K3D_VERSION="5.1.0"
+K3D_VERSION="4.4.7"
 
 # install docker
 sudo apt-get update
