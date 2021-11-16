a837161d0135bc0264302bd755184b7fecdd29f7
Dennis Geiselmann <dennis.geiselmann@outlook.de>
2021-11-16 11:11:20 +0100

    remove pjtester

--- prow/scripts/compatibility-cli.sh
+++ prow/scripts/compatibility-cli.sh
@@ -17,8 +17,6 @@ VARIABLES=(
     COMPAT_BACKTRACK
 )
 
-echo "COMPAT_BACKTRACK: ${COMPAT_BACKTRACK}" # TODO remove
-
 #Exported variables
 export KYMA_PROJECT_DIR=${KYMA_PROJECT_DIR:-"/home/prow/go/src/github.com/kyma-project"}
 export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
