d11cd31a61ebb538a358d78ce27f5dddf7490e6b
Piotr Halama <piotr.halama@sap.com>
2021-11-25 12:55:35 +0100

    Image-URL-helper: add option to exclude images from being promoted

--- development/image-url-helper/cmd/promote.go
+++ development/image-url-helper/cmd/promote.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 
 	"github.com/jamiealquiza/envy"
+	"github.com/kyma-project/test-infra/development/image-url-helper/pkg/check"
 	"github.com/kyma-project/test-infra/development/image-url-helper/pkg/list"
 	"github.com/kyma-project/test-infra/development/image-url-helper/pkg/promote"
 	"github.com/spf13/cobra"
@@ -16,6 +17,7 @@ type promoteCmdOptions struct {
 	targetTag               string
 	dryRun                  bool
 	sign                    bool
+	excludesList            string
 }
 
 // PromoteCmd replaces containerRegistry and image versions with the provided ones
@@ -36,7 +38,13 @@ func PromoteCmd() *cobra.Command {
 			images := make(list.ImageMap)
 			testImages := make(list.ImageMap)
 
-			err := filepath.Walk(ResourcesDirectory, promote.GetWalkFunc(ResourcesDirectoryClean, targetContainerRegistryClean, options.targetTag, options.dryRun, images, testImages))
+			excludes, err := check.ParseExcludes(options.excludesList)
+			if err != nil {
+				fmt.Printf("Cannot parse excludes list: %s\n", err)
+				os.Exit(2)
+			}
+
+			err = filepath.Walk(ResourcesDirectory, promote.GetWalkFunc(ResourcesDirectoryClean, targetContainerRegistryClean, options.targetTag, options.dryRun, images, testImages, excludes))
 			if err != nil {
 				fmt.Printf("Cannot traverse directory: %s\n", err)
 				os.Exit(2)
@@ -64,6 +72,7 @@ func addPromoteCmdFlags(cmd *cobra.Command, options *promoteCmdOptions) {
 	cmd.Flags().StringVarP(&options.targetTag, "target-tag", "t", "", "Name of the target tag")
 	cmd.Flags().BoolVarP(&options.dryRun, "dry-run", "d", true, "Dry run enabled, nothing is changed")
 	cmd.Flags().BoolVarP(&options.sign, "sign", "s", false, "Set sign flag in outputted yaml file")
+	cmd.Flags().StringVarP(&options.excludesList, "excludes-list", "e", "", "List of excluded images")
 	cmd.MarkFlagRequired("target-container-registry")
 	envy.ParseCobra(cmd, envy.CobraConfig{Persistent: true, Prefix: "IMAGE_URL_HELPER"})
 }
