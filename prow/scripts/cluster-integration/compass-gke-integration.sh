c5f569890360f0379be41400d38f3feef3556439
Daniel Gospodinov <daniel.gospodinov@sap.com>
2021-12-06 01:24:40 +0200

    Secure node-exporter and unblock some monitors

--- prow/scripts/cluster-integration/compass-gke-integration.sh
+++ prow/scripts/cluster-integration/compass-gke-integration.sh
@@ -269,6 +269,7 @@ function applyCommonOverrides() {
 function prometheusMTLSPatch() {
   patchPrometheusForMTLS
   patchAlertManagerForMTLS
+  enableNodeExporterTLS
   patchDeploymentsToInjectSidecar
   patchKymaServiceMonitorsForMTLS
   removeKymaPeerAuthsForPrometheus
@@ -342,9 +343,10 @@ EOF
 
 function patchDeploymentsToInjectSidecar() {
   allDeploy=(
+    kiali-server
     monitoring-kube-state-metrics
     monitoring-operator
-    monitoring-prometheus-istio-server
+    api-gateway
   )
 
   resource="deployment"
@@ -367,6 +369,212 @@ function patchDeploymentsToInjectSidecar() {
   done
 }
 
+function enableNodeExporterTLS() {
+  monitor=$(cat <<"EOF"
+apiVersion: monitoring.coreos.com/v1
+kind: ServiceMonitor
+metadata:
+  annotations:
+    meta.helm.sh/release-name: monitoring
+    meta.helm.sh/release-namespace: kyma-system
+  labels:
+    app: monitoring-node-exporter
+    app.kubernetes.io/instance: monitoring
+    app.kubernetes.io/managed-by: Helm
+    app.kubernetes.io/name: monitoring
+    chart: monitoring-1.0.0
+    helm.sh/chart: monitoring-1.0.0
+    release: monitoring
+  name: monitoring-node-exporter
+  namespace: kyma-system
+spec:
+  endpoints:
+  - metricRelabelings:
+    - action: keep
+      regex: ^(go_goroutines|go_memstats_alloc_bytes|go_memstats_heap_alloc_bytes|go_memstats_heap_inuse_bytes|go_memstats_heap_sys_bytes|go_memstats_stack_inuse_bytes|node_.*|process_cpu_seconds_total|process_max_fds|process_open_fds|process_resident_memory_bytes|process_start_time_seconds|process_virtual_memory_bytes)$
+      sourceLabels:
+      - __name__
+    port: metrics
+    scheme: https
+    tlsConfig:
+      insecureSkipVerify: true
+  jobLabel: jobLabel
+  selector:
+    matchLabels:
+      app: prometheus-node-exporter
+      release: monitoring
+
+EOF
+  )
+  echo "$monitor" > monitor.yaml
+
+  daemonset=$(cat <<"EOF"
+apiVersion: apps/v1
+kind: DaemonSet
+metadata:
+  annotations:
+    deprecated.daemonset.template.generation: "1"
+    meta.helm.sh/release-name: monitoring
+    meta.helm.sh/release-namespace: kyma-system
+  labels:
+    app: prometheus-node-exporter
+    app.kubernetes.io/instance: monitoring
+    app.kubernetes.io/managed-by: Helm
+    app.kubernetes.io/name: prometheus-node-exporter
+    chart: prometheus-node-exporter-1.12.0
+    helm.sh/chart: prometheus-node-exporter-1.12.0
+    jobLabel: node-exporter
+    release: monitoring
+  name: monitoring-prometheus-node-exporter
+  namespace: kyma-system
+spec:
+  revisionHistoryLimit: 10
+  selector:
+    matchLabels:
+      app: prometheus-node-exporter
+      release: monitoring
+  template:
+    metadata:
+      labels:
+        app: prometheus-node-exporter
+        app.kubernetes.io/instance: monitoring
+        app.kubernetes.io/managed-by: Helm
+        app.kubernetes.io/name: prometheus-node-exporter
+        chart: prometheus-node-exporter-1.12.0
+        helm.sh/chart: prometheus-node-exporter-1.12.0
+        jobLabel: node-exporter
+        release: monitoring
+    spec:
+      initContainers:
+      - name: certs-init
+        image: frapsoft/openssl
+        command: ['sh', '-c', 'openssl req -newkey rsa:2048 -nodes -days 365000 -subj "/CN=$(NODE_NAME)" -keyout /etc/certs/node.key -out /etc/certs/node.csr && openssl x509 -req -days 365000 -set_serial 01 -in /etc/certs/node.csr -out /etc/certs/node.crt -CA /etc/istio/certs/ca-cert.pem -CAkey /etc/istio/certs/ca-key.pem']
+        env:
+          - name: NODE_NAME
+            valueFrom:
+              fieldRef:
+                fieldPath: spec.nodeName
+        volumeMounts:
+        - name: istio-certs
+          mountPath: /etc/istio/certs
+          readOnly: true
+        - name: node-certs
+          mountPath: /etc/certs
+      - name: web-config-init
+        image: busybox
+        command: ['sh', '-c', 'printf "tls_server_config:\n  cert_file: /etc/certs/node.crt\n  key_file: /etc/certs/node.key" > /etc/certs/web.yaml'] 
+        volumeMounts:
+        - name: node-certs
+          mountPath: /etc/certs
+      containers:
+      - args:
+        - --path.procfs=/host/proc
+        - --path.sysfs=/host/sys
+        - --path.rootfs=/host/root
+        - --web.listen-address=$(HOST_IP):9100
+        - --web.config=/etc/certs/web.yaml
+        - --collector.filesystem.ignored-mount-points=^/(dev|proc|sys|var/lib/docker/.+)($|/)
+        - --collector.filesystem.ignored-fs-types=^(autofs|binfmt_misc|cgroup|configfs|debugfs|devpts|devtmpfs|fusectl|hugetlbfs|mqueue|overlay|proc|procfs|pstore|rpc_pipefs|securityfs|sysfs|tracefs)$
+        env:
+        - name: HOST_IP
+          value: 0.0.0.0
+        image: eu.gcr.io/kyma-project/tpi/node-exporter:1.0.1-1de56388
+        imagePullPolicy: IfNotPresent
+        livenessProbe:
+          failureThreshold: 3
+          httpGet:
+            path: /
+            port: 9100
+            scheme: HTTPS
+          periodSeconds: 10
+          successThreshold: 1
+          timeoutSeconds: 1
+        name: node-exporter
+        ports:
+        - containerPort: 9100
+          hostPort: 9100
+          name: metrics
+          protocol: TCP
+        readinessProbe:
+          failureThreshold: 3
+          httpGet:
+            path: /
+            port: 9100
+            scheme: HTTPS
+          periodSeconds: 10
+          successThreshold: 1
+          timeoutSeconds: 1
+        resources: {}
+        securityContext:
+          allowPrivilegeEscalation: false
+          privileged: false
+        terminationMessagePath: /dev/termination-log
+        terminationMessagePolicy: File
+        volumeMounts:
+        - mountPath: /etc/certs
+          name: node-certs
+        - mountPath: /host/proc
+          name: proc
+          readOnly: true
+        - mountPath: /host/sys
+          name: sys
+          readOnly: true
+        - mountPath: /host/root
+          mountPropagation: HostToContainer
+          name: root
+          readOnly: true
+      dnsPolicy: ClusterFirst
+      hostNetwork: true
+      hostPID: true
+      restartPolicy: Always
+      schedulerName: default-scheduler
+      securityContext:
+        fsGroup: 65534
+        runAsGroup: 65534
+        runAsNonRoot: true
+        runAsUser: 65534
+      serviceAccount: monitoring-prometheus-node-exporter
+      serviceAccountName: monitoring-prometheus-node-exporter
+      terminationGracePeriodSeconds: 30
+      tolerations:
+      - effect: NoSchedule
+        operator: Exists
+      volumes:
+      - name: istio-certs
+        secret:
+          secretName: istio-ca-secret
+      - name: node-certs
+        emptyDir: {}
+      - hostPath:
+          path: /proc
+          type: ""
+        name: proc
+      - hostPath:
+          path: /sys
+          type: ""
+        name: sys
+      - hostPath:
+          path: /
+          type: ""
+        name: root
+  updateStrategy:
+    rollingUpdate:
+      maxUnavailable: 1
+    type: RollingUpdate
+
+EOF
+  )
+  echo "$daemonset" > daemonset.yaml
+
+  kubectl get secret istio-ca-secret --namespace=istio-system -o yaml | grep -v '^\s*namespace:\s' | kubectl replace --force --namespace=kyma-system -f -
+
+  kubectl apply --force -f monitor.yaml
+  kubectl apply --force -f daemonset.yaml
+
+  rm monitor.yaml
+  rm daemonset.yaml
+} 
+
 function patchKymaServiceMonitorsForMTLS() {
   kymaSvcMonitors=(
     kiali
@@ -378,22 +586,23 @@ function patchKymaServiceMonitorsForMTLS() {
     tracing-jaeger
     monitoring-grafana
     monitoring-alertmanager
+    monitoring-operator 
+    monitoring-kube-state-metrics 
     dex
+    api-gateway
     monitoring-prometheus-pushgateway
-    monitoring-kube-state-metrics
-    monitoring-operator
-    monitoring-prometheus-istio-server-server
   )
 
   crd="servicemonitors.monitoring.coreos.com"
   namespace="kyma-system"
   patchContent=$(cat <<"EOF"
-  - scheme: https
+    scheme: https
     tlsConfig:
       caFile: /etc/prometheus/secrets/istio.default/root-cert.pem
       certFile: /etc/prometheus/secrets/istio.default/cert-chain.pem
       keyFile: /etc/prometheus/secrets/istio.default/key.pem
       insecureSkipVerify: true
+
 EOF
   )
 
@@ -404,15 +613,11 @@ EOF
       kubectl get ${crd} -n ${namespace} "${sm}" -o yaml > "${sm}.yaml"
 
       if [[ "$OSTYPE" == "darwin"* ]]; then
-        sed -i '' -e '/ endpoints:/r tmp_patch_content.yaml' "${sm}.yaml"
-        sed -i '' -e 's/- port:/  port:/g' "${sm}.yaml"
-        sed -i '' -e 's/- metricRelabelings:/  metricRelabelings:/g' "${sm}.yaml"
-        sed -i '' -e 's/- honorLabels:/  honorLabels:/g' "${sm}.yaml"
+        sed -i '' -e '/ port:/r tmp_patch_content.yaml' "${sm}.yaml"
+        sed -i '' -e '/ path:/r tmp_patch_content.yaml' "${sm}.yaml"
       else # assume Linux otherwise
-        sed -i '/ endpoints:/r tmp_patch_content.yaml' "${sm}.yaml"
-        sed -i 's/- port:/  port:/g' "${sm}.yaml"
-        sed -i 's/- metricRelabelings:/  metricRelabelings:/g' "${sm}.yaml"
-        sed -i 's/- honorLabels:/  honorLabels:/g' "${sm}.yaml"
+        sed -i '/ port:/r tmp_patch_content.yaml' "${sm}.yaml"
+        sed -i '/ path:/r tmp_patch_content.yaml' "${sm}.yaml"
       fi
 
       kubectl apply -f "${sm}.yaml" || true
@@ -437,7 +642,6 @@ function removeKymaPeerAuthsForPrometheus() {
     ory-hydra-maester-metrics
     tracing-jaeger-operator-metrics
     tracing-jaeger-metrics
-    dex-service
     monitoring-prometheus-pushgateway
   )
 
