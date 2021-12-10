49d6df4dd909b53bc50fc83444a6a5fb3be06f17
pPrecel <filip.strozik@outlook.com>
2021-12-10 09:43:31 +0100

    draft
    add k3d templates; render templates
    
    fix path in script
    
    add pjtester
    
    create vm for k3d tests; add missing env
    
    render templates
    
    fix templates
    
    fit github token duplicate
    
    install kyma cli from the latest release
    
    fix missing env
    
    diable gardener vars validation
    
    fix missing local envs
    
    fix envs
    
    debug point
    
    export one needed env
    
    unpack given envs
    
    fix envs checking
    
    fix kyma installation on k3d
    
    send right folders to the vm
    
    fix utils.sh
    
    unfix utils; changee KYMA_PROJECT_DIR
    
    fix KYMA_VERSION env
    
    fix
    
    change kyma-project dir
    
    change kyma-project dir
    
    forward BOT_GITHUB_TOKEN
    
    change KYMA_PROJECT_DIR to .

--- prow/scripts/cluster-integration/kyma-upgrade-k3d.sh
+++ prow/scripts/cluster-integration/kyma-upgrade-k3d.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#Description: Kyma CLI Integration plan on Gardener. This scripts implements a pipeline that consists of many steps. The purpose is to install and test Kyma using the CLI on a real Gardener cluster.
+#Description: Kyma CLI Integration plan on k3s. This scripts implements a pipeline that consists of many steps. The purpose is to install and test Kyma using the CLI on a local k3d cluster.
 #There are two scenarious based on the given KYMA_MAJOR_VERSION env.
 #For the value "1": Install kyma 1.x -> upgrade to kyma 2.x
 #For the value "2": Install kyma 2.x -> upgrade to kyma from main branch
@@ -8,13 +8,7 @@
 #Expected common vars:
 # - JOB_TYPE - set up by prow (presubmit, postsubmit, periodic)
 # - KYMA_PROJECT_DIR - directory path with Kyma sources to use for installation
-# - GARDENER_REGION - Gardener compute region
-# - GARDENER_ZONES - Gardener compute zones inside the region
-# - GARDENER_CLUSTER_VERSION - Version of the Kubernetes cluster
-# - GARDENER_KYMA_PROW_KUBECONFIG - Kubeconfig of the Gardener service account
-# - GARDENER_KYMA_PROW_PROJECT_NAME - Name of the gardener project where the cluster will be integrated.
-# - GARDENER_KYMA_PROW_PROVIDER_SECRET_NAME - Name of the secret configured in the gardener project to access the cloud provider
-# - MACHINE_TYPE - (optional) machine type
+# - KYMA_MAJOR_VERSION - major version of the first installation
 #
 #Please look in each provider script for provider specific requirements
 
@@ -25,6 +19,13 @@
 set -e
 
 ENABLE_TEST_LOG_COLLECTOR=false
+
+# Unpack given envs 
+ENV_FILE=".env"
+if [ -f "${ENV_FILE}" ]; then
+# shellcheck disable=SC2046
+export $(xargs < "${ENV_FILE}")
+fi
 
 export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
 export KYMA_SOURCES_DIR="${KYMA_PROJECT_DIR}/kyma"
@@ -39,48 +40,19 @@ source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/lib/kyma.sh"
 
 # All provides require these values, each of them may check for additional variables
 requiredVars=(
-    GARDENER_PROVIDER
     KYMA_PROJECT_DIR
-    GARDENER_REGION
-    GARDENER_ZONES
-    GARDENER_CLUSTER_VERSION
-    GARDENER_KYMA_PROW_KUBECONFIG
-    GARDENER_KYMA_PROW_PROJECT_NAME
-    GARDENER_KYMA_PROW_PROVIDER_SECRET_NAME
+    KYMA_MAJOR_VERSION
 )
-
 utils::check_required_vars "${requiredVars[@]}"
 log::info "### Starting pipeline"
 
-if [[ $GARDENER_PROVIDER == "azure" ]]; then
-    # shellcheck source=prow/scripts/lib/gardener/azure.sh
-    source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/lib/gardener/azure.sh"
-elif [[ $GARDENER_PROVIDER == "aws" ]]; then
-    # shellcheck source=prow/scripts/lib/gardener/aws.sh
-    source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/lib/gardener/aws.sh"
-elif [[ $GARDENER_PROVIDER == "gcp" ]]; then
-    # shellcheck source=prow/scripts/lib/gardener/gcp.sh
-    source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/lib/gardener/gcp.sh"
-else
-    ## TODO what should I put here? Is this a backend?
-    log::error "GARDENER_PROVIDER ${GARDENER_PROVIDER} is not yet supported"
-    exit 1
-fi
-
-# nice cleanup on exit, be it succesful or on fail
-trap gardener::cleanup EXIT INT
-
-#Used to detect errors for logging purposes
 ERROR_LOGGING_GUARD="true"
 export ERROR_LOGGING_GUARD
 
-RANDOM_NAME_SUFFIX=$(LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c6)
-readonly COMMON_NAME_PREFIX="grd"
-COMMON_NAME=$(echo "${COMMON_NAME_PREFIX}${RANDOM_NAME_SUFFIX}" | tr "[:upper:]" "[:lower:]")
-export COMMON_NAME
+kyma::install_cli_last_release
 
-### Cluster name must be less than 10 characters!
-export CLUSTER_NAME="${COMMON_NAME}"
+log::info "### Provision k3s cluster"
+kyma provision k3d --ci
 
 if [[ $KYMA_MAJOR_VERSION == "1" ]]; then
     # Install Kyma form latest 1.x release
@@ -88,8 +60,16 @@ if [[ $KYMA_MAJOR_VERSION == "1" ]]; then
         -t "${BOT_GITHUB_TOKEN}" \
         -v "^1."
 
+    export INSTALLATION_OVERRIDE_STACKDRIVER="installer-config-logging-stackdiver.yaml"
     export KYMA_SOURCE="${kyma_get_last_release_version_return_version:?}"
     log::info "### Reading release version from RELEASE_VERSION file, got: ${KYMA_SOURCE}"
+
+    log::info "### Installing Kyma $KYMA_SOURCE"
+    kyma install \
+        --ci \
+        --source "${KYMA_SOURCE}" \
+        --timeout 90m \
+        --verbose
 else
     # Install kyma from latest 2.x release
     kyma::get_last_release_version \
@@ -97,57 +77,47 @@ else
 
     export KYMA_SOURCE="${kyma_get_last_release_version_return_version:?}"
     log::info "### Reading release version from RELEASE_VERSION file, got: ${KYMA_SOURCE}"
-fi
 
-# checks required vars and initializes gcloud/docker if necessary
-gardener::init
-
-# if MACHINE_TYPE is not set then use default one
-gardener::set_machine_type
-
-kyma::install_cli_last_release
-
-# currently only Azure generates overrides, but this may change in the future
-gardener::generate_overrides
-
-log::info "### Provisioning Gardener cluster"
-gardener::provision_cluster
-
-log::info "### Installing Kyma $KYMA_SOURCE"
-
-if [[ $KYMA_MAJOR_VERSION == "1" ]]; then
+    log::info "### Installing Kyma $KYMA_SOURCE"
     # uses previously set KYMA_SOURCE
-    gardener::install_kyma
-else
-    # uses previously set KYMA_SOURCE
-    kyma::deploy_kyma \
-        -s "$KYMA_SOURCES_DIR" \
-        -u "true"
+    kyma deploy \
+        --ci \
+        --source "${KYMA_SOURCE}" \
+        --timeout 90m \
+        --verbose
 fi
 
 # generate pod-security-policy list in json
 utils::save_psp_list "${ARTIFACTS}/kyma-psp.json"
 
 log::info "### Run pre-upgrade tests"
-gardener::pre_upgrade_test_fast_integration_kyma
+
+pushd "${KYMA_SOURCES_DIR}/tests/fast-integration"
+make ci-pre-upgrade
+popd
+
+log::success "Tests completed"
 
 if [[ $KYMA_MAJOR_VERSION == "1" ]]; then
     # Upgrade kyma to latest 2.x release
     export KYMA_MAJOR_VERSION="2"
-    log::info "### Installing Kyma 2.x"
 
     kyma::get_last_release_version \
         -t "${BOT_GITHUB_TOKEN}"
     export KYMA_SOURCE="${kyma_get_last_release_version_return_version:?}"
     log::info "### Reading release version from RELEASE_VERSION file, got: ${KYMA_SOURCE}"
 
-    kyma::deploy_kyma \
-        -s "$KYMA_SOURCES_DIR" \
-        -u "true"
+    log::info "### Upgrade Kyma to ${KYMA_SOURCE}"
+    kyma deploy \
+        --ci \
+        --source "${KYMA_SOURCE}" \
+        --timeout 90m \
+        --verbose
 else
     # Upgrade kyma to main branch
     export KYMA_SOURCE="main"
     
+    log::info "### Upgrade Kyma to ${KYMA_SOURCE}"
     kyma::deploy_kyma \
         -s "$KYMA_SOURCES_DIR" \
         -u "true"
@@ -155,13 +125,23 @@ fi
 
 
 log::info "### Run post-upgrade tests"
-gardener::post_upgrade_test_fast_integration_kyma
+
+pushd "${KYMA_SOURCES_DIR}/tests/fast-integration"
+make ci-post-upgrade
+popd
+
+log::success "Tests completed"
 
 log::info "### waiting some time to finish cleanups"
 sleep 60
 
 log::info "### Run pre-upgrade tests again to validate component removal"
-gardener::pre_upgrade_test_fast_integration_kyma
+
+pushd "${KYMA_SOURCES_DIR}/tests/fast-integration"
+make ci-post-upgrade
+popd
+
+log::success "Tests completed"
 
 log::info "### Remove old components"
 helm delete core -n kyma-system
@@ -176,7 +156,12 @@ helm delete permission-controller -n kyma-system
 kubectl delete ns kyma-installer --ignore-not-found=true
 
 log::info "### Run post-upgrade tests again to validate component removal"
-gardener::post_upgrade_test_fast_integration_kyma
+
+pushd "${KYMA_SOURCES_DIR}/tests/fast-integration"
+make ci-post-upgrade
+popd
+
+log::success "Tests completed"
 
 #!!! Must be at the end of the script !!!
 ERROR_LOGGING_GUARD="false"
