7478ffdc28837c4f8d88b18500e093a8ad90922d
ruanxin <xin.ruan@sap.com>
2021-12-10 14:35:26 +0100

    Fix reconciler ci job (#4553)
    * extract LAST_RELEASE_VERSION as env value, provide latest 1.x version
    
    * add pjtester
    
    * remove script to read version from GitHub
    
    * Delete pjtester.yaml
    
    * remove env value use script to get kyma version
    
    * delete pjtester.yaml
    
    * use different way to provision cluster
    
    * Delete reconciler-e2e-upgrade-gardener.sh
    
    * Update pjtester.yaml
    
    * revert deleted sh
    
    * refactor jobs
    
    * add missing DOMAIN_NAME
    
    * reuse reconciler::connect_to_shoot_cluster
    
    * remove pjtester
    
    Co-authored-by: Suleyman Akbas <suleymanakbas91@gmail.com>

--- prow/scripts/cluster-integration/reconciler-e2e-gardener.sh
+++ prow/scripts/cluster-integration/reconciler-e2e-gardener.sh
@@ -54,6 +54,21 @@ requiredVars=(
     GARDENER_KYMA_PROW_PROVIDER_SECRET_NAME
 )
 
+
+function provision_cluster() {
+    export DEFINITION_PATH="${RESOURCES_PATH}/shoot-template.yaml"
+    export DOMAIN_NAME="${INPUT_CLUSTER_NAME}"
+    log::info "Creating cluster: ${INPUT_CLUSTER_NAME}"
+    # create the cluster
+    envsubst < "${DEFINITION_PATH}" | kubectl create -f -
+
+    # wait for the cluster to be ready
+    kubectl wait --for condition="ControlPlaneHealthy" --timeout=10m shoot "${INPUT_CLUSTER_NAME}"
+    log::info "Cluster ${INPUT_CLUSTER_NAME} was created successfully"
+
+}
+
+
 utils::check_required_vars "${requiredVars[@]}"
 
 if [[ $GARDENER_PROVIDER == "azure" ]]; then
@@ -82,24 +97,21 @@ readonly COMMON_NAME_PREFIX="grd"
 utils::generate_commonName -n "${COMMON_NAME_PREFIX}"
 COMMON_NAME="${utils_generate_commonName_return_commonName:?}"
 export COMMON_NAME
-export CLUSTER_NAME="${COMMON_NAME}"
+export INPUT_CLUSTER_NAME="${COMMON_NAME}"
+export RESOURCES_PATH="${TEST_INFRA_SOURCES_DIR}/prow/scripts/resources/reconciler"
 
+export KUBECONFIG="${GARDENER_KYMA_PROW_KUBECONFIG}"
 ## ---------------------------------------------------------------------------------------
 ## Prow job execution steps
 ## ---------------------------------------------------------------------------------------
 
 log::banner "Provisioning Gardener cluster"
-# Checks required vars and initializes gcloud/docker if necessary
-gardener::init
-
-# If MACHINE_TYPE is not set then use default one
-gardener::set_machine_type
-
-# Install Kyma CLI
-kyma::install_cli
 
 # Provision garderner cluster
-gardener::provision_cluster
+provision_cluster
+
+# Connect to the newly created shoot cluster
+reconciler::connect_to_shoot_cluster
 
 # Deploy reconciler
 reconciler::deploy
