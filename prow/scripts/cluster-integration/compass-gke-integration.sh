698b21f31877dc9509fbbf261b581e37439e1bb8
Daniel Gospodinov <daniel.gospodinov@sap.com>
2021-12-21 11:44:18 +0200

    Introduce dex peerauth patch

--- prow/scripts/cluster-integration/compass-gke-integration.sh
+++ prow/scripts/cluster-integration/compass-gke-integration.sh
@@ -273,6 +273,7 @@ function prometheusMTLSPatch() {
   patchDeploymentsToInjectSidecar
   patchKymaServiceMonitorsForMTLS
   removeKymaPeerAuthsForPrometheus
+  patchDexPeerAuthForPrometheus
   patchMonitoringTests
 }
 
@@ -645,6 +646,35 @@ function removeKymaPeerAuthsForPrometheus() {
   for pa in "${allPAs[@]}"; do
     kubectl delete ${crd} -n ${namespace} "${pa}" || true
   done
+}
+
+function patchDexPeerAuthForPrometheus() {
+  crd="peerauthentication"
+  namespace="kyma-system"
+  name="dex-service"
+
+  patchDex=$(cat <<"EOF"
+  portLevelMtls:
+    5558:
+      mode: STRICT
+EOF
+  )
+
+  if kubectl get ns ${namespace} > /dev/null; then
+    echo "${patchDex}" > patch-dex.yaml
+    kubectl get ${crd} -n ${namespace} ${name} -o yaml > dex-pa.yaml
+
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      sed -i '' -e '/spec:/r patch-dex.yaml' dex-pa.yaml
+    else # assume Linux otherwise
+      sed -i '/spec:/r patch-dex.yaml' dex-pa.yaml
+    fi
+
+    kubectl apply -f dex-pa.yaml
+
+    rm dex-pa.yaml
+    rm patch-dex.yaml
+  fi
 }
 
 function patchMonitoringTests() {
