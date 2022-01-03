ce8118fe322d70571bae7f845e4f0fe3314b747b
Piotr Halama <piotr.halama@sap.com>
2022-01-03 11:19:46 +0100

    fix params

--- prow/scripts/generate-component-descriptors.sh
+++ prow/scripts/generate-component-descriptors.sh
@@ -13,21 +13,21 @@ source "$SCRIPT_DIR/lib/docker.sh"
 docker::authenticate "${GOOGLE_APPLICATION_CREDENTIALS}"
 
 params=()
-params+=("--component-version $(date +v%Y%m%d-%s)-${git_commit::8}")
-params+=("--git-commit ${git_commit}")
+
 params+=("--git-branch ${PULL_BASE_REF}")
 params+=("--output-dir ${ARTIFACTS}/cd")
-params+=("--skip-image-hashing=$skip_hashing")
 if [[ "$JOB_TYPE" == "presubmit" ]]; then
     # on presubmit use latest commit from the PR itself
     git_commit="${PULL_PULL_SHA}"
-    skip_hashing="true"
+    params+=("--skip-image-hashing=true")
 else
     # use base commit for postsubmit jobs
     git_commit="${PULL_BASE_SHA}"
-    skip_hashing="false"
     params+=("--repo-context ${DOCKER_PUSH_REPOSITORY}")
 fi
+
+params+=("--component-version $(date +v%Y%m%d-%s)-${git_commit::8}")
+params+=("--git-commit ${git_commit}")
 
 pushd "${TEST_INFRA_SOURCES_DIR}"
 echo "This tool generates component descriptor file"
