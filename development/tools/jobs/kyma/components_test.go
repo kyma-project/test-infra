b7aced66cab5d71f0cc2ff26b56366de8d850dcf
Piotr Kopec <piotr@kubermatic.com>
2022-01-04 23:15:15 +0100

    keep the pipeline for versions until 1.24

--- development/tools/jobs/kyma/components_test.go
+++ development/tools/jobs/kyma/components_test.go
@@ -95,6 +95,12 @@ var components = []struct {
 			jobsuite.JobFileSuffix("generic"),
 		},
 	},
+	{path: "permission-controller", image: tester.ImageGolangBuildpack1_16, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.JobFileSuffix("generic"),
+			jobsuite.Until(releases.Release124),
+		},
+	},
 	{path: "service-binding-usage-controller", image: tester.ImageGolangBuildpack1_16, suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("generic"),
