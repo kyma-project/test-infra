977573c87beecb60bb2409c2d3db51c5ed47fa30
VladislavPaskar <vladislav.paskar@sap.com>
2022-01-04 09:30:05 +0100

    fixes

--- prow/scripts/cluster-integration/helpers/eventing.sh
+++ prow/scripts/cluster-integration/helpers/eventing.sh
@@ -172,3 +172,32 @@ function eventing::test_fast_integration_eventing() {
 
     log::success "Eventing tests completed"
 }
+
+function eventing::pre_upgrade_test_fast_integration() {
+    log::info "Running pre upgrade Eventing E2E release tests"
+
+    pushd /home/prow/go/src/github.com/kyma-project/kyma/tests/fast-integration
+    make ci-test-eventing-pre-upgrade
+    popd
+
+    log::success "Pre upgrade Eventing tests completed"
+}
+function eventing::fast_integration_tests() {
+    log::info "Running only Eventing E2E release tests"
+
+    pushd /home/prow/go/src/github.com/kyma-project/kyma/tests/fast-integration
+    make ci-test-eventing-tests
+    popd
+
+    log::success "Eventing tests completed"
+}
+
+function eventing::post_upgrade_test_fast_integration() {
+    log::info "Running post upgrade Eventing E2E release tests and clean up the resources"
+
+    pushd /home/prow/go/src/github.com/kyma-project/kyma/tests/fast-integration
+    make ci-test-eventing-post-upgrade
+    popd
+
+    log::success "Post upgrade Eventing tests completed"
+}
--- prow/scripts/cluster-integration/skr-eventing-e2e-upgrade-test.sh
+++ prow/scripts/cluster-integration/skr-eventing-e2e-upgrade-test.sh
@@ -1,15 +1,117 @@
-# execute eventing e2e pre upgrade tests
+#!/usr/bin/env bash
+#
+# Execute eventing e2e pre upgrade tests on skr
+#
+#Description: Kyma CLI Integration plan on Gardener. This scripts implements a pipeline that consists of many steps.
+# The purpose is to install and test Kyma using the CLI on a real Gardener cluster, run the eventing fast integration tests on skr cluster, upgrade the cluster to the current PR version and the run the tests again
+#
+#Expected common vars:
+# - JOB_TYPE - set up by prow (presubmit, postsubmit, periodic)
+# - KYMA_PROJECT_DIR - directory path with Kyma sources to use for installation
+# - GARDENER_REGION - Gardener compute region
+# - GARDENER_ZONES - Gardener compute zones inside the region
+# - GARDENER_CLUSTER_VERSION - Version of the Kubernetes cluster
+# - GARDENER_KYMA_PROW_KUBECONFIG - Kubeconfig of the Gardener service account
+# - GARDENER_KYMA_PROW_PROJECT_NAME - Name of the gardener project where the cluster will be integrated
+# - GARDENER_KYMA_PROW_PROVIDER_SECRET_NAME - Name of the secret configured in the gardener project to access the cloud provider
+# - CREDENTIALS_DIR - Directory where is the EventMesh service key is mounted
+# - MACHINE_TYPE - (optional) machine type
+#
+#Please look in each provider script for provider specific requirements
 
-export SKR_SUFFIX="test"
-export SKR_INSTANCE_ID=$(uuidgen)
+## ---------------------------------------------------------------------------------------
+## Configurations and Variables
+## ---------------------------------------------------------------------------------------
 
+# exit on error, and raise error when variable is not set when used
+set -o errexit
 
-log::info "Running skr eventing e2e upgrade with testst with SKR_SUFFIX: ${SKR_SUFFIX} and SKR_INSTANCE_ID: ${SKR_INSTANCE_ID}"
+ENABLE_TEST_LOG_COLLECTOR=false
+
+export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/test-infra"
+export KYMA_SOURCES_DIR="${KYMA_PROJECT_DIR}/kyma"
+export TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS="${TEST_INFRA_SOURCES_DIR}/prow/scripts/cluster-integration/helpers"
+export EVENTMESH_SECRET_FILE="${CREDENTIALS_DIR}/serviceKey" # For eventing E2E fast-integration tests
+
+# shellcheck source=prow/scripts/lib/log.sh
+source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/lib/log.sh"
+# shellcheck source=prow/scripts/lib/utils.sh
+source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/lib/utils.sh"
+# shellcheck source=prow/scripts/lib/kyma.sh
+source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/lib/kyma.sh"
+# shellcheck source=prow/scripts/cluster-integration/helpers/eventing.sh
+source "${TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS}/eventing.sh"
+# shellcheck source=prow/scripts/lib/gardener/gardener.sh
+source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/lib/gardener/gardener.sh"
+
+# All provides require these values, each of them may check for additional variables
+requiredVars=(
+    BOT_GITHUB_TOKEN
+    GARDENER_PROVIDER
+    KYMA_PROJECT_DIR
+    GARDENER_REGION
+    GARDENER_ZONES
+    GARDENER_CLUSTER_VERSION
+    GARDENER_KYMA_PROW_KUBECONFIG
+    GARDENER_KYMA_PROW_PROJECT_NAME
+    GARDENER_KYMA_PROW_PROVIDER_SECRET_NAME
+    CREDENTIALS_DIR
+    EVENTMESH_SECRET_FILE
+)
+
+utils::check_required_vars "${requiredVars[@]}"
+log::info "### Starting pipeline"
+
+if [[ $GARDENER_PROVIDER == "azure" ]]; then
+    # shellcheck source=prow/scripts/lib/gardener/azure.sh
+    source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/lib/gardener/azure.sh"
+elif [[ $GARDENER_PROVIDER == "aws" ]]; then
+    # shellcheck source=prow/scripts/lib/gardener/aws.sh
+    source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/lib/gardener/aws.sh"
+elif [[ $GARDENER_PROVIDER == "gcp" ]]; then
+    # shellcheck source=prow/scripts/lib/gardener/gcp.sh
+    source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/lib/gardener/gcp.sh"
+else
+    log::error "GARDENER_PROVIDER ${GARDENER_PROVIDER} is not yet supported"
+    exit 1
+fi
+
+# nice cleanup on exit, be it successful or on fail
+trap gardener::cleanup EXIT INT
+
+#Used to detect errors for logging purposes
+ERROR_LOGGING_GUARD="true"
+export ERROR_LOGGING_GUARD
+
+RANDOM_NAME_SUFFIX=$(LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c6)
+readonly COMMON_NAME_PREFIX="grd"
+COMMON_NAME=$(echo "${COMMON_NAME_PREFIX}${RANDOM_NAME_SUFFIX}" | tr "[:upper:]" "[:lower:]")
+export COMMON_NAME
+
+### Cluster name must be less than 10 characters!
+export CLUSTER_NAME="${COMMON_NAME}"
+
+# set pipefail to handle right errors from tests
+set -o pipefail
+
+# Install Kyma form latest release
+kyma::get_last_release_version -t "${BOT_GITHUB_TOKEN}"
+
+export KYMA_SOURCE="${kyma_get_last_release_version_return_version:?}"
+log::info "### Reading release version from RELEASE_VERSION file, got: ${KYMA_SOURCE}"
+
+# checks required vars and initializes gcloud/docker if necessary
+gardener::init
+
+# if MACHINE_TYPE is not set then use default one
+gardener::set_machine_type
+
+log::info "Running skr eventing e2e upgrade with tests with SKR_SUFFIX: ${SKR_SUFFIX} and SKR_INSTANCE_ID: ${SKR_INSTANCE_ID}"
 
 make provision-eventing-skr
 
 # test the default Eventing backend which comes with Kyma
-eventing::pre_upgrade_test_fast_integration_eventing
+eventing::pre_upgrade_test_fast_integration
 
 # upgrade the kyma to the current PR/commit state
 KYMA_SOURCE="PR-${PULL_NUMBER}"
@@ -18,6 +120,10 @@ export KYMA_SOURCE
 make upgrade-eventing-skr
 
 # test the eventing fi tests after the upgrade
+eventing::fast_integration_tests
+
+make upgrade-eventing-skr
+
 eventing::post_upgrade_test_fast_integration_eventing
 
 #!!! Must be at the end of the script !!!
