b6dea7e74b4a4f0cc63fc4c61478fb5c1bf5fc46
Jeremy Harisch <jeremy.harisch@sap.com>
2022-01-27 17:54:16 +0100

    fix log

--- prow/scripts/cluster-integration/kyma-upgrade-skr-aws-kyma-to-kyma2.sh
+++ prow/scripts/cluster-integration/kyma-upgrade-skr-aws-kyma-to-kyma2.sh
@@ -33,7 +33,7 @@ log::info "### Starting pipeline"
 # Fetch latest Kyma2 release
 kyma::get_last_release_version -t "${BOT_GITHUB_TOKEN}"
 export KYMA_UPGRADE_VERSION="${kyma_get_last_release_version_return_version:?}"
-log::info "### Reading release version from RELEASE_VERSION file, got: ${KYMA_SOURCE}"
+log::info "### Reading release version from RELEASE_VERSION file, got: ${KYMA_UPGRADE_VERSION}"
 
 log::info "### Run make ci-skr-kyma-to-kyma2-upgrade"
 make -C /home/prow/go/src/github.com/kyma-project/kyma/tests/fast-integration ci-skr-kyma-to-kyma2-upgrade
