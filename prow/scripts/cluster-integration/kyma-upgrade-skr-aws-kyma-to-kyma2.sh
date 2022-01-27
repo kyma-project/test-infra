232dceda6b92246a72cdce6e4bca7a30d4c5ea5b
Jeremy Harisch <jeremy.harisch@sap.com>
2022-01-27 17:21:41 +0100

    might work

--- prow/scripts/cluster-integration/kyma-upgrade-skr-aws-kyma-to-kyma2.sh
+++ prow/scripts/cluster-integration/kyma-upgrade-skr-aws-kyma-to-kyma2.sh
@@ -1,4 +1,10 @@
+#!/usr/bin/env bash
 
+#Description: Kyma Integration plan on SKR AWS. This scripts implements a pipeline that consists of many steps. The purpose is to trigger the ci-skr-kyma-to-kyma2-upgrade fast-integration test in Kyma repository
+#Expected common vars:
+#
+#
+#Please look in each provider script for provider specific requirements
 
 # Fetch latest Kyma2 release
 kyma::get_last_release_version -t "${BOT_GITHUB_TOKEN}"
@@ -7,6 +13,3 @@ log::info "### Reading release version from RELEASE_VERSION file, got: ${KYMA_SO
 
 log::info "### Run make ci-skr-kyma-to-kyma2-upgrade"
 make -C /home/prow/go/src/github.com/kyma-project/kyma/tests/fast-integration ci-skr-kyma-to-kyma2-upgrade
-
-
- make -C "${KYMA_SOURCES_DIR}/tests/fast-integration" "${1}"
