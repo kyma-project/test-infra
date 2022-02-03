12583251bcdc27ec306b72a1ca0cd8253d0dda05
Piotr Halama <piotr.halama@sap.com>
2022-02-03 12:38:51 +0100

    Fix Gardener integration jobs (#4883)
    * Test integration job
    
    * Test settign source to source
    
    * fix previous test
    
    * fix
    
    * vpath

--- prow/scripts/cluster-integration/kyma-integration-gardener.sh
+++ prow/scripts/cluster-integration/kyma-integration-gardener.sh
@@ -121,7 +121,6 @@ gardener::provision_cluster
 
 if [[ "${KYMA_MAJOR_VERSION}" == "2" ]]; then
   kyma::deploy_kyma \
-    -s "$KYMA_SOURCE" \
     -p "$EXECUTION_PROFILE" \
     -d "$KYMA_SOURCES_DIR"
   if [[ "${KYMA_DELETE}" == "true" ]]; then
@@ -129,7 +128,6 @@ if [[ "${KYMA_MAJOR_VERSION}" == "2" ]]; then
     kyma::undeploy_kyma
     sleep 30
     kyma::deploy_kyma \
-       -s "$KYMA_SOURCE" \
        -p "$EXECUTION_PROFILE" \
        -d "$KYMA_SOURCES_DIR"
   fi
