0d29bd4a903b4cf658d0736d1c74e25d0f3ddf6f
marcobebway <marco.bebway@sap.com>
2022-02-07 16:41:10 +0100

    Skip fast-integration tests

--- prow/scripts/cluster-integration/kyma-integration-gardener.sh
+++ prow/scripts/cluster-integration/kyma-integration-gardener.sh
@@ -152,7 +152,8 @@ fi
 
 
 if [[ "${EXECUTION_PROFILE}" == "evaluation" ]] || [[ "${EXECUTION_PROFILE}" == "production" ]]; then
-    gardener::test_fast_integration_kyma
+    log::banner "Skip fast-integration tests"
+#    gardener::test_fast_integration_kyma
 # this will be extended with the next components
 elif [[ "${API_GATEWAY_INTEGRATION}" == "true" ]]; then
     api-gateway::configure_ory_hydra
