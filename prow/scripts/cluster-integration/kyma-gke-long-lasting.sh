b3ece98026679dbeda88da2df564187492585f05
Piotr Halama <piotr.halama@sap.com>
2022-02-16 08:52:23 +0100

    Bump Kyma to 1.24.10 on long-lasting clusters (#4958)

--- prow/scripts/cluster-integration/kyma-gke-long-lasting.sh
+++ prow/scripts/cluster-integration/kyma-gke-long-lasting.sh
@@ -136,7 +136,7 @@ function installKyma() {
 
 	kyma install \
 			--ci \
-			--source "1.24.9" \
+			--source "1.24.10" \
 			-o "$PWD/kyma-installer-overrides.yaml" \
 			-o "$PWD/overrides-dex-and-monitoring.yaml" \
 			-o "${TEST_INFRA_SOURCES_DIR}/prow/scripts/resources/prometheus-cluster-essentials-overrides.tpl.yaml" \
