26b737ee57bbf0efd1a5055da8ae4d47e1fd946f
Jeremy Harisch <48282931+jeremyharisch@users.noreply.github.com>
2022-03-01 14:54:59 +0100

    Dev branch reconciler (#5031)
    * pre-dev-reconciler-protecode-guard
    
    * Use unstable cli instead last release
    
    * USe week days for daily cluster
    
    * Suppress check
    
    * Implement dev e2e gardener pipeline
    
    * pjtester
    
    * Dummy to trigger pj tester
    
    * Dummy to trigger pj tester
    
    * Dummy to trigger pj tester
    
    * use extra ref instead clone
    
    * use cd instead pushd
    
    * go mod tidy
    
    * remove test just a tmp
    
    * Remove dev periodic
    
    * Remove unstable var
    
    * Remove pjtester
    
    * Fix test

--- prow/scripts/cluster-integration/kyma-upgrade-k3d-kyma-to-kyma2.sh
+++ prow/scripts/cluster-integration/kyma-upgrade-k3d-kyma-to-kyma2.sh
@@ -41,8 +41,8 @@ function prereq() {
     )
     utils::check_required_vars "${requiredVars[@]}"
 
-    # install kymaCLI from the last release
-    kyma::install_cli_last_release
+    log::info "### Install latest unstable Kyma CLI"
+    kyma::install_unstable_cli
 } 
 
 function provision_cluster() {
