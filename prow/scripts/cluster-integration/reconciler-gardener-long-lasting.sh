324f9f5edd94231b0528c52117e188ae3668d414
Jeremy Harisch <jeremy.harisch@sap.com>
2022-03-02 11:23:18 +0100

    Test strign truncate

--- prow/scripts/cluster-integration/reconciler-gardener-long-lasting.sh
+++ prow/scripts/cluster-integration/reconciler-gardener-long-lasting.sh
@@ -55,10 +55,9 @@ utils::check_required_vars "${requiredVars[@]}"
 reconciler::delete_cluster_if_exists
 
 # Generate new cluster name
-typeset -L2 day
 # shellcheck disable=SC2046
 # shellcheck disable=SC2005
-day=$(echo $(date +%a) | tr "[:upper:]" "[:lower:]")
+day=$(echo $(date +%a) | tr "[:upper:]" "[:lower:]" | cut -c1-2)
 export INPUT_CLUSTER_NAME="${INPUT_CLUSTER_NAME}-${day}"
 
 # Provisioning gardener long lasting cluster
