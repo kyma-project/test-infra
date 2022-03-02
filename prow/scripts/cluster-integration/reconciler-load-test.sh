7605fa8fc34a6ac21471ac44e01194bbd8e0ffc8
Xin Ruan <xin.ruan@sap.com>
2022-03-02 17:43:18 +0100

    provision another cluster

--- prow/scripts/cluster-integration/reconciler-load-test.sh
+++ prow/scripts/cluster-integration/reconciler-load-test.sh
@@ -60,11 +60,11 @@ reconciler::export_shoot_cluster_kubeconfig
 set +e
 log::banner "Deploying Monitoring for load test"
 
-#kubectl create -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/main/example/prometheus-operator-crd/monitoring.coreos.com_prometheuses.yaml
+kubectl create -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/release-0.54.1/example/prometheus-operator-crd/monitoring.coreos.com_prometheuses.yaml
 
 git clone https://github.com/prometheus-operator/kube-prometheus.git
 cd kube-prometheus
-git checkout release-0.10
+git checkout release-0.9
 kubectl apply -f manifests/setup --validate=false
 kubectl apply -f manifests/ --validate=false
 
