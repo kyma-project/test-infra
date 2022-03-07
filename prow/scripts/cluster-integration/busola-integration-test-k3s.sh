c67eaaf21ec27d0479e6d68f44ce7474ff01faa7
dariadomagala <daria.domagala@sap.com>
2022-03-07 13:27:53 +0100

    Test

--- prow/scripts/cluster-integration/busola-integration-test-k3s.sh
+++ prow/scripts/cluster-integration/busola-integration-test-k3s.sh
@@ -121,5 +121,8 @@ cp "$PWD/kubeconfig-kyma.yaml" "$PWD/busola-tests/fixtures/kubeconfig.yaml"
 mkdir -p "$PWD/busola-tests/cypress/screenshots"
 
 echo "STEP: Running Cypress tests inside Docker"
+echo "--------------------------------------------------"
+echo "This is a new prow job!"
+echo "--------------------------------------------------"
 docker run --entrypoint /bin/bash --network=host -v "$PWD/busola-tests:/tests" -w /tests $CYPRESS_IMAGE -c "npm ci --no-optional; NO_COLOR=1 npm run test"
 
