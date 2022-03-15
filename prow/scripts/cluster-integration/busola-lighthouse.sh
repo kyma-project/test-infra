3f225ac8a43653db84d8ce6d2ea51e0580b329af
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-03-15 10:16:27 +0100

    a

--- prow/scripts/cluster-integration/busola-lighthouse.sh
+++ prow/scripts/cluster-integration/busola-lighthouse.sh
@@ -118,12 +118,14 @@ kubectl wait \
 --timeout=120s
 
 echo '--------------------------------------------------------------------------------------------------------------------'
-echo $PWD
+echo "$PWD"
 echo '--------------------------------------------------------------------------------------------------------------------'
 mkdir -p "$PWD/busola-tests/fixtures"
 mkdir -p "$PWD/busola-tests/test-results"
 cp "$PWD/kubeconfig-kyma.yaml" "$PWD/busola-tests/fixtures/kubeconfig.yaml"
 
-echo "STEP: Running Lighthouse audit inside Docker"
-docker run --entrypoint /bin/bash --network=host -v "$PWD/busola-tests:/tests" -w /tests $PLAYWRIGHT_IMAGE -c "npm ci --no-optional; npx playwright install;  NO_COLOR=1 npx playwright test"
+ls "$PWD/busola-tests"
+
+echo "STEP: Running Lighthouse audit inside Docker albo i nie"
+# docker run --entrypoint /bin/bash --network=host -v "$PWD/busola-tests:/tests" -w /tests $PLAYWRIGHT_IMAGE -c "npm ci --no-optional; npx playwright install;  NO_COLOR=1 npx playwright test"
 
