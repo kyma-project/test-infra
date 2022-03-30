c8116fc1f8bbc1db32ca342cfc9e60383b58c014
Piotr Halama <piotr.halama@sap.com>
2022-03-30 09:31:11 +0200

    swap order

--- development/kyma-slack-connector/releaseClusterCreatedFunction/handler.py
+++ development/kyma-slack-connector/releaseClusterCreatedFunction/handler.py
@@ -10,41 +10,33 @@ def main(event, context):
 	app = App(
 	)
 	slack_api_id = os.environ['SLACK_API_ID'].replace('-', '_')
+	slack_channel = os.environ['NOTIFICATION_SLACK_CHANNEL']
 	env_prefix = os.environ['ENV_PREFIX']
 	base_url = os.environ['{}_SLACK_CONNECTOR_{}_GATEWAY_URL'.format(env_prefix, slack_api_id)]
 	# Set Slack API base URL to the URL of slack-connector application gateway.
 	app.client.base_url = "{}/".format(base_url)
 	print("received message with id: {}".format(event["data"]["ID"]))
 	print("Slack api base URL: {}".format(app.client.base_url))
-	print("sending notification to channel: {}".format(os.environ['NOTIFICATION_SLACK_CHANNEL']))
+	print("sending notification to channel: {}".format(slack_channel))
 	# Get cloud events data.
 	msg = json.loads(base64.b64decode(event["data"]["Data"]))
 	uploadedKubeconfig=[]
 	result=[]
-	try:
-		# push kubeconfig
-		kubeconfigFilename= "kubeconfig_"+msg["cluster_name"]+".yaml"
-		uploadedKubeconfig = app.client.files_upload(content=msg["kubeconfig"], filename=kubeconfigFilename)
-		assert uploadedKubeconfig["ok"]
-		print("uploaded kubeconfig for message id: {}".format(event["data"]["ID"]))
-	except SlackApiError as e:
-		assert uploadedKubeconfig["ok"] is False
-		print(f"Got an error: {e.response['error']}")
-		print("failed upload file for message id: {}".format(event["data"]["ID"]))
-		return
 	
 	try:
 		# Deliver message to the channel.
-		result = app.client.chat_postMessage(channel=os.environ['NOTIFICATION_SLACK_CHANNEL'],
-											text="Kyma {} was released.".format(msg["kyma_version"]),	# TODO
-											username="ProwBot",
+		result = app.client.chat_postMessage(channel=slack_channel,
+											text="Kyma {} was released.".format(msg["kyma_version"]),
+											username="ReleaseBot",
+											unfurl_links="true",
+											unfurl_media="true",
 											blocks=[
 												{
 													"type": "context",
 													"elements": [
 														{
 															"type": "mrkdwn",
-															"text": "Kyma OS was released, rejoice."
+															"text": "_Kyma OS was released_"
 														}
 													]
 												},
@@ -60,30 +52,8 @@ def main(event, context):
 													"type": "section",
 													"text": {
 														"type": "mrkdwn",
-														"text": "Kyma OS {} was released, cubeconfig for the `{}` cluster :blobwant:".format(
-																msg["kyma_version"],
+														"text": "Kubeconfig for the `{}` cluster is in the thread".format(
 																msg["cluster_name"])
-													}
-												},
-												{
-													"type": "section",
-													"text": {
-														"type": "mrkdwn",
-														"text": "<"+uploadedKubeconfig['file']['permalink']+"| >"
-													}
-												},
-												{
-													"type": "section",
-													"text": {
-														"type": "mrkdwn",
-														"text": "debug: "+uploadedKubeconfig['file']['permalink']
-													}
-												},
-												{
-													"type": "section",
-													"text": {
-														"type": "mrkdwn",
-														"text": "<"+uploadedKubeconfig['file']['permalink']+"| debug2>"
 													}
 												}
 											],
@@ -94,3 +64,22 @@ def main(event, context):
 		assert result["ok"] is False
 		print(f"Got an error: {e.response['error']}")
 		print("failed sent notification for message id: {}".format(event["data"]["ID"]))
+
+	try:
+		# push kubeconfig
+		kubeconfigFilename = "kubeconfig-"+msg["cluster_name"]+".yaml"
+		uploadedKubeconfig = app.client.files_upload(content=msg["kubeconfig"],
+														filename=kubeconfigFilename,
+														channels=slack_channel,
+														thread_ts=result["message"]["ts"],
+														initial_comment="Kubeconfig for the `{}` cluster: :blobwant:".format(
+	 																	msg["kyma_version"],
+	 																	msg["cluster_name"])
+		)
+		assert uploadedKubeconfig["ok"]
+		print("uploaded kubeconfig for message id: {}".format(event["data"]["ID"]))
+	except SlackApiError as e:
+		assert uploadedKubeconfig["ok"] is False
+		print(f"Got an error: {e.response['error']}")
+		print("failed upload file for message id: {}".format(event["data"]["ID"]))
+		return
