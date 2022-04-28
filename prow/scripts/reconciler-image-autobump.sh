6c6c72f97fc1b3fa1e536119d505c73fac7f7c3d
Xin Ruan <xin.ruan@sap.com>
2022-04-28 19:10:06 +0200

    skip replace if fields not exists

--- prow/scripts/reconciler-image-autobump.sh
+++ prow/scripts/reconciler-image-autobump.sh
@@ -83,12 +83,12 @@ function reconciler::fetch_latest_image_tag() {
 }
 
 function autobump::update_reconciler_image_tag(){
-  log::info "Update reconciler image tag in control plane"
+  log::info "Update reconciler image tag in control plane with ${RECONCILER_IMAGE_TAG}"
   cd "${CONTROL_PLANE_DIR}"
   # support old image tag update, should be removed after PR https://github.com/kyma-project/control-plane/pull/1601 merged.
   if $(yq eval '.global.images | has("mothership_reconciler")' ./resources/kcp/values.yaml); then
-    yq e -i '(.global.images.mothership_reconciler ) |= "eu.gcr.io/kyma-project/incubator/reconciler/mothership:'${RECONCILER_IMAGE_TAG}'"' ./resources/kcp/values.yaml
-    yq e -i '(.global.images.component_reconciler ) |= "eu.gcr.io/kyma-project/incubator/reconciler/component:'${RECONCILER_IMAGE_TAG}'"' ./resources/kcp/values.yaml
+    yq e -i '(.global.images.mothership_reconciler ) = "eu.gcr.io/kyma-project/incubator/reconciler/mothership:'${RECONCILER_IMAGE_TAG}'"' ./resources/kcp/values.yaml
+    yq e -i '(.global.images.component_reconciler ) = "eu.gcr.io/kyma-project/incubator/reconciler/component:'${RECONCILER_IMAGE_TAG}'"' ./resources/kcp/values.yaml
   fi
 
   if $(yq eval '.global.images | has("mothership_reconciler_version")' ./resources/kcp/values.yaml); then
