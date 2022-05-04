1a61e7c31c9e761b4525e18a54fe58660e81f064
Tom Gehrke <t.gehrke@reply.de>
2022-05-04 17:28:20 +0200

    feat: create k6 job and output logs

--- prow/scripts/performance-tests/helpers/performance-tests.sh
+++ prow/scripts/performance-tests/helpers/performance-tests.sh
@@ -2,10 +2,14 @@
 
 set -e
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 function performance_tests::run() {
   log::info "Running performance tests"
 
   kubectl create namespace perf-test
   find "$TEST_DIR/components/$TEST_COMPONENTS" -name "*.js" | xargs -I {} echo -n "--from-file={}" | xargs kubectl create configmap -n perf-test test-scripts
   kubectl get configmaps -oyaml -n perf-test test-scripts
+  kubectl create -f "$SCRIPT_DIR/job.yaml"
+  kubectl logs -n perf-test -f jobs/k6
 }
