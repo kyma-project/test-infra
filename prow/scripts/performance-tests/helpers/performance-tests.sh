60040a4889678d1e76d98777017fd0ff8814acd5
Tom Gehrke <t.gehrke@reply.de>
2022-05-04 16:20:03 +0200

    fix: create namespace and use find instead of ls for finding script files

--- prow/scripts/performance-tests/helpers/performance-tests.sh
+++ prow/scripts/performance-tests/helpers/performance-tests.sh
@@ -5,6 +5,7 @@ set -e
 function performance_tests::run() {
   log::info "Running performance tests"
 
-  ls "$TEST_DIR/components/$TEST_COMPONENTS/*.js" | xargs -I {} echo -n "--from-file={}" | xargs kubectl create configmap -n perf-test test-scripts
+  kubectl create namespace perf-test
+  find "$TEST_DIR/components/$TEST_COMPONENTS" -name "*.js" | xargs -I {} echo -n "--from-file={}" | xargs kubectl create configmap -n perf-test test-scripts
   kubectl get configmaps -oyaml -n perf-test test-scripts
 }
