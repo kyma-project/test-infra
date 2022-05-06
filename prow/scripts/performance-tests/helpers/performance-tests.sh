17d7b2e4535ee181060fc51660836e7f1ee92f36
Tom Gehrke <t.gehrke@reply.de>
2022-05-06 10:08:57 +0200

    fix: wait for pods with correct label

--- prow/scripts/performance-tests/helpers/performance-tests.sh
+++ prow/scripts/performance-tests/helpers/performance-tests.sh
@@ -11,6 +11,6 @@ function performance_tests::run() {
   find "$TEST_DIR/components/$TEST_COMPONENTS" -name "*.js" | xargs -I {} echo -n "--from-file={}" | xargs kubectl create configmap -n perf-test test-scripts
   kubectl get configmaps -oyaml -n perf-test test-scripts
   kubectl create -n perf-test -f "$SCRIPT_DIR/job.yaml"
-  kubectl wait --for=condition=ready pod -n perf-test -l app=k6
+  kubectl wait --for=condition=ready pod -n perf-test -l job-name=k6
   kubectl logs -n perf-test -f jobs/k6
 }
