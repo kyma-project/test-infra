19b2384ff9c1c43d3b8b60b7303898605b14e5d8
Piotr Halama <piotr.halama@sap.com>
2022-06-20 09:31:20 +0200

    use return instead of exit

--- prow/scripts/lib/utils.sh
+++ prow/scripts/lib/utils.sh
@@ -162,7 +162,7 @@ function utils::receive_from_vm() {
   for i in $(seq 1 5); do
     [[ ${i} -gt 1 ]] && log::info 'Retrying in 15 seconds..' && sleep 15;
     gcloud compute scp --ssh-key-file="${SSH_KEY_FILE_PATH:-/root/.ssh/user/google_compute_engine}" --verbosity="${GCLOUD_SCP_LOG_LEVEL:-error}" --strict-host-key-checking=no --quiet --recurse --zone="${ZONE}" "${REMOTE_NAME}":"${REMOTE_PATH}" "${LOCAL_PATH}" && break;
-    [[ ${i} -ge 5 ]] && log::error "Failed after $i attempts." && exit 1
+    [[ ${i} -ge 5 ]] && log::error "Failed after $i attempts." && return 1
   done;
 }
 
