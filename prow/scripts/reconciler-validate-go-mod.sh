5b089e7947bf30c5f8debb8557cacb111d28b20a
Teodor Mihaescu <teodor-adrian.mihaescu@sap.com>
2022-06-22 16:14:25 +0200

    Testing with shell script...

---
+++ prow/scripts/reconciler-validate-go-mod.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+log::banner "Validate reconciler's go.mod file"
+
+log::info "Preconfigure dependencies"
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+python2 get-pip.py
+pip install semver==2.10
+
+log::info "Execute validation script"
+# make validate-go-mod
+
+# TEST:
+pwd
+exit 1
+
+# TODO: Execute validation script directly from here
+# python2 ./scripts/validate-go-mod.py && \
+# ([ $$? -eq 0 ] && echo "Result: go.mod is VALID") \
+# || (echo "Result: go.mod is INVALID (see log above)"; exit 1)
+
+# TODO: Test script exit code
+# if [[ $? -eq 0 ]];then
+#         log::success "Tests completed"
+#     else
+#         log::error "Tests failed"
+#         exit 1
+#     fi
