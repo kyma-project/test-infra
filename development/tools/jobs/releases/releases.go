f1184fe5f7f2b795cdf7c38ac2509a1748a4c27f
Piotr Halama <piotr.halama@sap.com>
2022-06-27 13:06:35 +0200

    Update nextRelease version

--- development/tools/jobs/releases/releases.go
+++ development/tools/jobs/releases/releases.go
@@ -4,22 +4,22 @@ package releases
 
 // List of currently supported releases
 var (
+	Release25 = mustParse("2.5")
 	Release24 = mustParse("2.4")
 	Release23 = mustParse("2.3")
 	Release22 = mustParse("2.2")
-	Release21 = mustParse("2.1")
 )
 
 // GetAllKymaReleases returns all supported kyma release branches
 func GetAllKymaReleases() []*SupportedRelease {
 	return []*SupportedRelease{
+		Release24,
 		Release23,
 		Release22,
-		Release21,
 	}
 }
 
 // GetNextKymaRelease returns the version of kyma currently under development
 func GetNextKymaRelease() *SupportedRelease {
-	return Release24
+	return Release25
 }
