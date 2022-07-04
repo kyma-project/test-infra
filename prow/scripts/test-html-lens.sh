004640f1d143e2b5c810f3ef2557554722463aa9
Piotr Halama <piotr.halama@sap.com>
2022-07-04 14:28:25 +0200

    two files

--- prow/scripts/test-html-lens.sh
+++ prow/scripts/test-html-lens.sh
@@ -2,22 +2,6 @@
 
 
 
-# golangci-lint  run ./... --out-format html > "${ARTIFACTS}/report-golint.html"
+golangci-lint  run ./... --out-format html > "${ARTIFACTS}/report-golint.html"
 
-cat <<EOF > "${ARTIFACTS}/report-example.html"
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>HTML title</title>
-        <style>
-            body {background-color: white;}
-        </style>
-    </head>
-    <body>
-        <h1>Example report</h1>
-        <p>This is an example html file to show that more than one HTML file can be shown at the same time.</p>
-    </body>
-</html>
-EOF
+curl -LO "${ARTIFACTS}/report-kyma.html" "https://kyma-project.io"
