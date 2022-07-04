df6918a93609d3e227e6ead5319550bc5fbd5ea3
Piotr Halama <piotr.halama@sap.com>
2022-07-04 14:16:43 +0200

    Test HTML lenses

---
+++ prow/scripts/test-html-lens.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+
+golangci-lint  run ./... --out-format html > "${ARTIFACTS}/report-golint.html"
+
+cat <<EOF > "${ARTIFACTS}/report-example.html"
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>HTML title</title>
+    </head>
+    <body>
+        <h1>Example report</h1>
+        <p>This is an example html file to show that more than one HTML file can be shown at the same time.</p>
+    </body>
+</html>
+EOF
