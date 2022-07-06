e90ace131c3acae5b209b121581eb68d7e2be914
Bartosz Chwila <bartosz.chwila@sap.com>
2022-07-06 13:29:54 +0200

    Artifact exporting in test

--- prow/scripts/cluster-integration/helpers/integration-tests.sh
+++ prow/scripts/cluster-integration/helpers/integration-tests.sh
@@ -134,9 +134,6 @@ function api-gateway::launch_tests() {
 
   pushd "${KYMA_SOURCES_DIR}/tests/components/api-gateway"
   make test
-  echo "${ARTIFACTS}"
-  cp -v -r reports "${ARTIFACTS}"/reports
-  cp -v junit-report.xml "${ARTIFACTS}"/junit-report.xml
   popd
 
   log::success "Tests completed"
