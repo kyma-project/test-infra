9b217c72cf6b4a806abd1f009819a5eec7f95c49
Bartosz Chwila <bartosz.chwila@sap.com>
2022-07-12 16:08:53 +0200

    Fix if

--- prow/scripts/cluster-integration/kyma-integration-gardener.sh
+++ prow/scripts/cluster-integration/kyma-integration-gardener.sh
@@ -115,7 +115,6 @@ if [[ "${KYMA_MAJOR_VERSION}" == "2" ]]; then
        -p "$EXECUTION_PROFILE" \
        -d "$KYMA_SOURCES_DIR"
   fi
-fi
 # this will be extended with the next components
 elif [[ "${API_GATEWAY_INTEGRATION}" == "true" ]]; then
   api-gateway::prepare_components_file
