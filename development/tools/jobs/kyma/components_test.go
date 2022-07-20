db0f058a897581f7864fe048256a84c4abbe458f
Mohamed Elsayed <m.elsayed@gmail.com>
2022-07-20 11:12:55 +0200

    Add test jobs for functions v1alpha2 feature branch (#5757)
    * Add test jobs for functions v1alpha2 feature branch
    
    * rename jobs
    
    * pjtester
    
    * remove pjtester
    
    * fix test
    
    * fix test

--- development/tools/jobs/kyma/components_test.go
+++ development/tools/jobs/kyma/components_test.go
@@ -29,6 +29,11 @@ var components = []struct {
 			jobsuite.AllReleases(),
 		},
 	},
+	{path: "function-controller", image: tester.ImageGolangBuildpack1_16, suite: tester.NewGenericComponentSuite,
+		additionalOptions: []jobsuite.Option{
+			jobsuite.JobFileSuffix("v1alpha2-generic"),
+		},
+	},
 	{path: "function-runtimes", image: tester.ImageGolangBuildpack1_16, suite: tester.NewGenericComponentSuite,
 		additionalOptions: []jobsuite.Option{
 			jobsuite.JobFileSuffix("generic"),
