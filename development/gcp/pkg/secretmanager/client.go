82f967dc23ae4b9a6a2c5af1c39011c85421c9ed
Piotr Halama <piotr.halama@sap.com>
2022-08-04 11:24:46 +0200

    comments, linting

--- development/gcp/pkg/secretmanager/client.go
+++ development/gcp/pkg/secretmanager/client.go
@@ -9,6 +9,7 @@ import (
 	gcpsecretmanager "google.golang.org/api/secretmanager/v1"
 )
 
+// NewService creates new Service struct
 func NewService(ctx context.Context, options ...option.ClientOption) (*Service, error) {
 	secretManagerClient, err := gcpsecretmanager.NewService(ctx, options...)
 	if err != nil {
@@ -19,6 +20,8 @@ func NewService(ctx context.Context, options ...option.ClientOption) (*Service, 
 	return &Service{Service: secretManagerClient, VersionService: secretVersionsService}, nil
 }
 
+// AddSecretVersion adds a new version to a secret
+// expects secretPath in "projects/*/secrets/*" format
 func (sm *Service) AddSecretVersion(secretPath string, secretData []byte) (*gcpsecretmanager.SecretVersion, error) {
 	newVersionRequest := gcpsecretmanager.AddSecretVersionRequest{Payload: &gcpsecretmanager.SecretPayload{Data: base64.StdEncoding.EncodeToString(secretData)}}
 	newVersionCall := sm.Service.Projects.Secrets.AddVersion(secretPath, &newVersionRequest)
@@ -26,27 +29,34 @@ func (sm *Service) AddSecretVersion(secretPath string, secretData []byte) (*gcps
 	return secretVersion, err
 }
 
+// ListSecretVersions lists all versions of a secret
+// expects secretPath in "projects/*/secrets/*" format
 func (sm *Service) ListSecretVersions(secretPath string) (*gcpsecretmanager.ListSecretVersionsResponse, error) {
 	secretVersionsCall := sm.Service.Projects.Secrets.Versions.List(secretPath)
 	secretVersions, err := secretVersionsCall.Do()
 	return secretVersions, err
 }
 
+// GetSecretVersionData retrieves one version of a secret
+// expects secretPath in "projects/*/secrets/*/versions/*" format
 func (sm *Service) GetSecretVersion(secretPath string) (*gcpsecretmanager.SecretVersion, error) {
 	secretVersionsCall := sm.Service.Projects.Secrets.Versions.Get(secretPath)
 	secretVersion, err := secretVersionsCall.Do()
 	return secretVersion, err
 }
 
-func (svm *Service) DisableSecretVersion(version *gcpsecretmanager.SecretVersion) (*gcpsecretmanager.SecretVersion, error) {
+// DisableSecretVersion disables a version of a secret
+func (sm *Service) DisableSecretVersion(version *gcpsecretmanager.SecretVersion) (*gcpsecretmanager.SecretVersion, error) {
 	disableRequest := gcpsecretmanager.DisableSecretVersionRequest{}
-	disableCall := svm.VersionService.Disable(version.Name, &disableRequest)
+	disableCall := sm.VersionService.Disable(version.Name, &disableRequest)
 	returnedSecretVersion, err := disableCall.Do()
 	return returnedSecretVersion, err
 }
 
-func (svm *Service) GetSecretVersionData(secretPath string) (string, error) {
-	secretVersionCall := svm.VersionService.Access(secretPath)
+// GetSecretVersionData retrieves payload of a secret version
+// expects secretPath in "projects/*/secrets/*/versions/*" format
+func (sm *Service) GetSecretVersionData(secretPath string) (string, error) {
+	secretVersionCall := sm.VersionService.Access(secretPath)
 	secretVersion, err := secretVersionCall.Do()
 	if err != nil {
 		return "", err
--- development/gcp/pkg/secretmanager/types.go
+++ development/gcp/pkg/secretmanager/types.go
@@ -4,6 +4,7 @@ import (
 	secretmanager "google.golang.org/api/secretmanager/v1"
 )
 
+// Service contains services for secret manipulation
 type Service struct {
 	Service        *secretmanager.Service
 	VersionService *secretmanager.ProjectsSecretsVersionsService
