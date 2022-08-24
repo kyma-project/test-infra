caf082f0fce2e0bad8c9a714708e3686dd0739e0
Bartosz Chwila <bartosz.chwila@sap.com>
2022-08-24 12:33:26 +0200

    Update preset

--- development/tools/jobs/api-gateway/api_gateway_test.go
+++ development/tools/jobs/api-gateway/api_gateway_test.go
@@ -24,7 +24,7 @@ func TestApiGatewayJobsPresubmit(t *testing.T) {
 	assert.False(t, actualPresubmit.SkipReport)
 
 	assert.True(t, actualPresubmit.AlwaysRun)
-	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, preset.DindEnabled, preset.DockerPushRepoIncubator, preset.GcrPush)
+	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, preset.DindEnabled, preset.DockerPushRepoKyma, preset.GcrPush)
 	assert.Equal(t, tester.ImageGolangKubebuilder2BuildpackLatest, actualPresubmit.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build-generic.sh"}, actualPresubmit.Spec.Containers[0].Command)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/api-gateway", "ci-pr"}, actualPresubmit.Spec.Containers[0].Args)
@@ -42,7 +42,7 @@ func TestApiGatewayJobPostsubmit(t *testing.T) {
 
 	assert.Equal(t, 10, actualPostsubmit.MaxConcurrency)
 
-	tester.AssertThatHasPresets(t, actualPostsubmit.JobBase, preset.DindEnabled, preset.DockerPushRepoIncubator, preset.GcrPush)
+	tester.AssertThatHasPresets(t, actualPostsubmit.JobBase, preset.DindEnabled, preset.DockerPushRepoKyma, preset.GcrPush)
 	assert.Equal(t, tester.ImageGolangKubebuilder2BuildpackLatest, actualPostsubmit.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build-generic.sh"}, actualPostsubmit.Spec.Containers[0].Command)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/api-gateway", "ci-main"}, actualPostsubmit.Spec.Containers[0].Args)
