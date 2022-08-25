798d2e7053679f1fb996bea199cb01b679263426
Tobias Schuhmacher <t.schuhmacher@sap.com>
2022-08-25 09:56:12 +0200

    Debug reconciler status check (#5919)
    * add debug message
    
    * extend debugging
    
    * add retry

--- prow/scripts/cluster-integration/helpers/reconciler.sh
+++ prow/scripts/cluster-integration/helpers/reconciler.sh
@@ -237,6 +237,13 @@ function reconciler::wait_until_kyma_reconciled() {
   iterationsLeft=$(( RECONCILER_TIMEOUT/RECONCILER_DELAY ))
   while : ; do
     status=$(kubectl exec -n "${RECONCILER_NAMESPACE}" test-pod -c test-pod -- sh -c ". /tmp/get-reconcile-status.sh" | xargs)
+
+    if [ -z "${status}" ]; then
+      log::info "Failed to retrieve reconciliation status. Retrying previous call in debug mode"
+      kubectl exec -v=8 -n "${RECONCILER_NAMESPACE}" test-pod -c test-pod -- sh -c ". /tmp/get-reconcile-status.sh"
+      status=$(kubectl exec -n "${RECONCILER_NAMESPACE}" test-pod -c test-pod -- sh -c ". /tmp/get-reconcile-status.sh" | xargs)
+    fi
+
     if [ "${status}" = "ready" ]; then
       log::info "Kyma is reconciled"
       break
