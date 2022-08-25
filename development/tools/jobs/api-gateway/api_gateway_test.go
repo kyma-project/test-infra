22e162e06e95248f55e1312b1aece04b57bb49f1
Bartosz Chwila <103247439+barchw@users.noreply.github.com>
2022-08-25 13:49:42 +0200

    Move api-gateway repo pipeline (#5917)
    * Add new job for new api-gateway repo
    
    * Pjtester
    
    * remove old job
    
    * Move directory
    
    * update pjtester
    
    * add path
    
    * Change name
    
    * Configure all related jobs
    
    * Move test
    
    * Remove duplicate
    
    * Update path
    
    * Update preset
    
    * fix always run
    
    * Run goimports

--- development/tools/jobs/api-gateway/api_gateway_test.go
+++ development/tools/jobs/api-gateway/api_gateway_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const apiGatewayJobPath = "./../../../../../prow/jobs/incubator/api-gateway/api-gateway.yaml"
+const apiGatewayJobPath = "./../../../../prow/jobs/api-gateway/api-gateway-build.yaml"
 
 func TestApiGatewayJobsPresubmit(t *testing.T) {
 	// when
@@ -17,17 +17,17 @@ func TestApiGatewayJobsPresubmit(t *testing.T) {
 
 	// then
 	require.NoError(t, err)
-	actualPresubmit := tester.FindPresubmitJobByNameAndBranch(jobConfig.AllStaticPresubmits([]string{"kyma-incubator/api-gateway"}), "pre-main-kyma-incubator-api-gateway", "master")
+	actualPresubmit := tester.FindPresubmitJobByNameAndBranch(jobConfig.AllStaticPresubmits([]string{"kyma-project/api-gateway"}), "pre-main-kyma-project-api-gateway", "master")
 	require.NotNil(t, actualPresubmit)
 
 	assert.Equal(t, 10, actualPresubmit.MaxConcurrency)
 	assert.False(t, actualPresubmit.SkipReport)
 
 	assert.True(t, actualPresubmit.AlwaysRun)
-	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, preset.DindEnabled, preset.DockerPushRepoIncubator, preset.GcrPush)
+	tester.AssertThatHasPresets(t, actualPresubmit.JobBase, preset.DindEnabled, preset.DockerPushRepoKyma, preset.GcrPush)
 	assert.Equal(t, tester.ImageGolangKubebuilder2BuildpackLatest, actualPresubmit.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build-generic.sh"}, actualPresubmit.Spec.Containers[0].Command)
-	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-incubator/api-gateway", "ci-pr"}, actualPresubmit.Spec.Containers[0].Args)
+	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/api-gateway", "ci-pr"}, actualPresubmit.Spec.Containers[0].Args)
 }
 
 func TestApiGatewayJobPostsubmit(t *testing.T) {
@@ -37,13 +37,13 @@ func TestApiGatewayJobPostsubmit(t *testing.T) {
 	// then
 	require.NoError(t, err)
 
-	actualPostsubmit := tester.FindPostsubmitJobByNameAndBranch(jobConfig.AllStaticPostsubmits([]string{"kyma-incubator/api-gateway"}), "post-main-kyma-incubator-api-gateway", "master")
+	actualPostsubmit := tester.FindPostsubmitJobByNameAndBranch(jobConfig.AllStaticPostsubmits([]string{"kyma-project/api-gateway"}), "post-main-kyma-project-api-gateway", "master")
 	require.NotNil(t, actualPostsubmit)
 
 	assert.Equal(t, 10, actualPostsubmit.MaxConcurrency)
 
-	tester.AssertThatHasPresets(t, actualPostsubmit.JobBase, preset.DindEnabled, preset.DockerPushRepoIncubator, preset.GcrPush)
+	tester.AssertThatHasPresets(t, actualPostsubmit.JobBase, preset.DindEnabled, preset.DockerPushRepoKyma, preset.GcrPush)
 	assert.Equal(t, tester.ImageGolangKubebuilder2BuildpackLatest, actualPostsubmit.Spec.Containers[0].Image)
 	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/build-generic.sh"}, actualPostsubmit.Spec.Containers[0].Command)
-	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-incubator/api-gateway", "ci-main"}, actualPostsubmit.Spec.Containers[0].Args)
+	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/api-gateway", "ci-main"}, actualPostsubmit.Spec.Containers[0].Args)
 }
