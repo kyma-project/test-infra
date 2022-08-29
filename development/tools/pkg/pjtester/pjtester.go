dd4635487923e68c9c5d4f32f8925b39b3458dde
dekiel <przemyslaw.pokrywka@sap.com>
2022-08-29 17:54:00 +0200

    Use map[string]map[string] for better input validation.

--- development/tools/pkg/pjtester/pjtester.go
+++ development/tools/pkg/pjtester/pjtester.go
@@ -1,3 +1,11 @@
+// Package pjtester implements tool for constructing prowjob specification and running it on Prow cluster to test it.
+// Prowjobs are scheduled on Prow cluster by creating a prowjob resource on cluster directly.
+// Prowjobs are always tested on untrusted cluster.
+// Tested prowjobs are prefixed with meaningful prefix and labeled to indicate it's a pjtester scheduled prowjob test.
+// In documentation test prowjob means a prowjob which is under test.
+// Test pull request is a pr used in test prowjob execution.
+// A pjtester pull request is a pr containing pjtester.yaml file and triggering pjtester prowjob.
+// A pjtester pr and test pr can be the same pull request.
 package pjtester
 
 import (
@@ -37,7 +45,7 @@ const (
 	defaultClonePath   = "/home/prow/go/src/github.com"
 	testinfraOrg       = "kyma-project"
 	testinfraRepo      = "test-infra"
-	pjtesterConfigPath = "vpath/pjtesterv2.yaml"
+	pjtesterConfigPath = "vpath/pjtester.yaml"
 )
 
 var (
@@ -45,59 +53,54 @@ var (
 	log         = logrus.New()
 )
 
-// pjCfg holds prowjob to test name and path to it's definition.
+// pjConfig holds user provided configuration of test prowjob.
 type pjConfig struct {
-	PjName string `yaml:"pjName" validate:"required,min=1"`
-	PjPath string `yaml:"pjPath" default:"prow/jobs/"` // path relative to repository root
-	Report bool   `yaml:"report,omitempty"`
+	PjName string `yaml:"pjName" validate:"required,min=1"` // Test prowjob name.
+	PjPath string `yaml:"pjPath" default:"prow/jobs/"`      // Path to directory or file with test prowjob definition, path is relative to repository root.
+	Report bool   `yaml:"report,omitempty"`                 // Enable reporting of prowjob status, default reporting is disabled.
 }
 
-type pjOrg map[string][]pjConfig
-
-// pjCfg holds number of PR to download and fetched details.
+// prConfig holds user and github provided details about pull request.
 type prConfig struct {
-	PrNumber    int `yaml:"prNumber" validate:"required,number,min=1"`
-	pullRequest github.PullRequest
-	org         string
-	repo        string
+	PrNumber    int                `yaml:"prNumber" validate:"required,number,min=1"` // User provided pr number.
+	pullRequest github.PullRequest // Pull request details fetched from github.
+	org         string             // PR organisation name.
+	repo        string             // PR repository name.
 }
 
-// prOrg holds pr configs per repository.
-type prOrg map[string]prConfig
-
+// pjConfigs holds user provided configuration used by pjtester to find test prowjob definition.
 type pjConfigs struct {
-	PrConfig map[string]prOrg `yaml:"prConfig,omitempty"`
-	ProwJobs map[string]pjOrg `yaml:"prowJobs" validate:"required,min=1"`
+	PrConfig map[string]map[string]prConfig   `yaml:"prConfig,omitempty" validate:"max=1,dive,max=1"`           // Map key represent github organisation.
+	ProwJobs map[string]map[string][]pjConfig `yaml:"prowJobs" validate:"required,min=1,dive,min=1,dive,min=1"` // Map key represent github organisation.
 }
 
-// testCfg holds prow config to test path, prowjobs to test names and paths to it's definitions.
+// testCfg holds user provided configuration for pjtester.
 type testCfg struct {
-	PjConfigs  pjConfigs        `yaml:"pjConfigs" validate:"required,min=1"`
-	ConfigPath string           `yaml:"configPath" default:"prow/config.yaml"` // path relative to repository root
-	PrConfigs  map[string]prOrg `yaml:"prConfigs,omitempty"`
+	PjConfigs  pjConfigs                      `yaml:"pjConfigs" validate:"required"`
+	ConfigPath string                         `yaml:"configPath" default:"prow/config.yaml"` // relative path from repository root to prow configuration file
+	PrConfigs  map[string]map[string]prConfig `yaml:"prConfigs,omitempty"`                   // Holds pull request details used in test prowjobs. Map key represent github organisation name.
 }
 
 // options holds data about prowjob and pull request to test.
 type options struct {
-	// jobName       string
-	configPath    string
+	configPath    string // configPath is a location of prow config file to use to construct test prowjob.
 	jobConfigPath string
 
-	baseRef        string
-	baseSha        string
-	pullNumber     int
-	pullSha        string
-	pullAuthor     string
-	pjtesterPrOrg  string // pjtester pr org
-	pjtesterPrRepo string // pjtester pr repo
+	baseRef        string // baseRef is a base branch name of github pull request under test.
+	baseSha        string // baseSha is a git SHA of a base branch of github pull request under test
+	pullNumber     int    // pullNumber is a number of github pull request under test
+	pullSha        string // pullSha is SHA of github pull request head under test
+	pullAuthor     string // pullAuthor is an author of github pull request under test
+	pjtesterPrOrg  string // pjtesterPrOrg is a name of organisation of pull request for pjtester prowjob.
+	pjtesterPrRepo string // pjtesterPrRepo is a name of organisation of pull request for pjtester prowjob.
 
-	github              ghclient.GithubClientConfig
-	githubClient        *ghclient.GithubClient
-	gitOptions          git.ClientConfig
-	gitClient           *git.Client
-	prFinder            *prtagbuilder.GitHubClient
-	testPullRequests    map[string]prOrg
-	pjConfigPullRequest prConfig
+	github       ghclient.GithubClientConfig
+	githubClient *ghclient.GithubClient
+	gitOptions   git.ClientConfig
+	gitClient    *git.Client
+	prFinder     *prtagbuilder.GitHubClient
+	testPullRequests    map[string]map[string]prConfig // pull requests used to run test prowjobs.
+	pjConfigPullRequest prConfig                       // pull request with pjtester.yaml file and used to start pjtester.
 
 	baseSHAGetter config.RefGetter
 	headSHAGetter config.RefGetter
@@ -105,7 +108,7 @@ type options struct {
 	usePjtesterPR bool
 }
 
-// checkEnvVars validate if required env variables are set
+// checkEnvVars validate if required env variables are set.
 func checkEnvVars(varsList []string) error {
 	for _, evar := range varsList {
 		val, present := os.LookupEnv(evar)
@@ -120,7 +123,9 @@ func checkEnvVars(varsList []string) error {
 	return nil
 }
 
-// newProwK8sClientset is building Prow client for kubeconfig location provided as env variable
+// newProwK8sClientset is building Prow client for provided kubeconfig.
+// Kubeconfig location is taken from env variable.
+// TODO: Refactor function to return Clientset and error instead stop execution with error.
 func newProwK8sClientset() *prowclient.Clientset {
 	k8sConfig, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG_PATH"))
 	if err != nil {
@@ -138,14 +143,13 @@ func readTestCfg(testCfgFile string) (testCfg, error) {
 	var t testCfg
 	yamlFile, err := ioutil.ReadFile(testCfgFile)
 	if err != nil {
-		log.Fatal("Failed read test config file from virtual path test-infra/vpath/pjtester.yaml")
+		return testCfg{}, fmt.Errorf("failed read test config file from vpath/pjtester.yaml")
 	}
 	err = yaml.Unmarshal(yamlFile, &t)
 	if err != nil {
-		log.Fatal("Failed unmarshal test config yaml.")
+		return testCfg{}, fmt.Errorf("failed unmarshal test config yaml")
 	}
 
-	// TODO: provide correct and full validation tags.
 	validate := validator.New()
 	// returns nil or ValidationErrors ( []FieldError )
 	err = validate.Struct(t)
@@ -202,9 +206,9 @@ func (o *options) setProwConfigPath(testConfig testCfg) {
 
 }
 
-// newCommonOptions is building common options for all tests.
-// Options are build from PR env variables and prowjob config read from pjtester.yaml file.
-// func newCommonOptions(configPath string, ghOptions prowflagutil.GitHubOptions) options {
+// newCommonOptions is building common options and GitHub client for all tests.
+// Options are build from PR env variables.
+// TODO: Refactor function to return options and error instead stop execution with error.
 func newCommonOptions(ghOptions prowflagutil.GitHubOptions) options {
 	var o options
 	var err error
@@ -212,33 +216,27 @@ func newCommonOptions(ghOptions prowflagutil.GitHubOptions) options {
 		GitHubOptions: ghOptions,
 		DryRun:        false,
 	}
-	// baseRef is a base branch name for github pull request under test.
 	o.baseRef = os.Getenv("PULL_BASE_REF")
-	// baseSha is a git SHA of a base branch for github pull request under test
 	o.baseSha = os.Getenv("PULL_BASE_SHA")
-	// pjtesterPrOrg is a name of organisation of pull request base branch
 	o.pjtesterPrOrg = os.Getenv("REPO_OWNER")
-	// repo is a name of repository of pull request base branch
 	o.pjtesterPrRepo = os.Getenv("REPO_NAME")
-	// pullNumber is a number of github pull request under test
 	o.pullNumber, err = strconv.Atoi(os.Getenv("PULL_NUMBER"))
 	if err != nil {
 		log.WithError(err).Fatalf("could not get pull number from env var PULL_NUMBER")
 	}
-	// pullSha is a SHA of github pull request head under test
 	o.pullSha = os.Getenv("PULL_PULL_SHA")
-	// pullAuthor is an author of github pull request under test
 	o.pullAuthor = gjson.Get(os.Getenv("JOB_SPEC"), "refs.pulls.0.author").String()
 	return o
 }
 
-// getPullRequests will download details from GitHub for pull requests defined in pjtester test configuration.
-// Downloaded pull request details are added to the options.pullRequest field.
-func (o *options) getPullRequests(prconfig map[string]prOrg) (map[string]prOrg, error) {
-	prs := make(map[string]prOrg)
+// getPullRequests download pull request details from GitHub.
+// Returned map first level keys represent GitHub organisations names, second level represent repositories names.
+// Pull requests to download are provided in pjtester test configuration.
+func (o *options) getPullRequests(prconfig map[string]map[string]prConfig) (map[string]map[string]prConfig, error) {
+	pullRequests := make(map[string]map[string]prConfig)
 	for org, repos := range prconfig {
-		if _, ok := prs[org]; !ok {
-			prs[org] = prOrg{}
+		if _, ok := pullRequests[org]; !ok {
+			pullRequests[org] = make(map[string]prConfig)
 		}
 		for repo, prcfg := range repos {
 			pr, err := o.githubClient.GetPullRequest(org, repo, prcfg.PrNumber)
@@ -248,10 +246,10 @@ func (o *options) getPullRequests(prconfig map[string]prOrg) (map[string]prOrg, 
 			prcfg.pullRequest = *pr
 			prcfg.org = org
 			prcfg.repo = repo
-			prs[org][repo] = prcfg
+			pullRequests[org][repo] = prcfg
 		}
 	}
-	return prs, nil
+	return pullRequests, nil
 }
 
 // genJobSpec will generate job specifications for prowjob to test
@@ -375,7 +373,6 @@ func setRefsFromCurrentPR(pjs prowapi.ProwJobSpec, opt options) (prowapi.ProwJob
 		// set refs with details of tested PR
 		setRefs(pjs.Refs, opt.baseSha, opt.baseRef, opt.pullAuthor, opt.pullSha, opt.pullNumber)
 		pjsRefsSet = true
-		// TODO: extra setting extrarefs to separate function
 	} else {
 		for index, ref := range pjs.ExtraRefs {
 			if ref.Org == opt.pjtesterPrOrg && ref.Repo == opt.pjtesterPrRepo {
@@ -393,7 +390,6 @@ func setRefsFromConfigs(pjs prowapi.ProwJobSpec, opt options) (prowapi.ProwJobSp
 		setRefs(pjs.Refs, pr.pullRequest.Base.SHA, pr.pullRequest.Base.Ref, pr.pullRequest.AuthorAssociation, pr.pullRequest.Head.SHA, pr.pullRequest.Number)
 		pjsRefsSet = true
 	}
-	// TODO: extra setting extrarefs to separate function
 	for index, ref := range pjs.ExtraRefs {
 		// Add PR details to ExtraRefs if PR number was provided in pjtester.yaml
 		if pr, present := opt.testPullRequests[ref.Org][ref.Repo]; present {
@@ -523,14 +519,18 @@ func (o *options) checkoutTestInfra() error {
 	return nil
 }
 
+// setRefsGetters set options.baseSHAGetter and options.headSHAGetter or checkout test-infra PR with prowjob definition.
+// This is used to get correct version of prowjob definition for user provided pjtester config.
 func (o *options) setRefsGetters(currentPrOrg, currentPrRepo string) error {
-	// Test prowjob from pull request in test-infra. Checkout this PR in test-infra repo from extraRefs.
+	// Use pull request with test prowjob if it was provided in pjtester config.
 	if o.pjConfigPullRequest.pullRequest.Number != 0 {
+		// Checkout this PR in test-infra repo from extraRefs.
 		if o.pjConfigPullRequest.org == testinfraOrg && o.pjConfigPullRequest.repo == testinfraRepo {
 			err := o.checkoutTestInfra()
 			if err != nil {
 				return fmt.Errorf("")
 			}
+			//
 		} else {
 			o.baseSHAGetter = func() (string, error) {
 				return o.pjConfigPullRequest.pullRequest.Base.SHA, nil
@@ -579,10 +579,10 @@ func newTestPJ(pjCfg pjConfig, opt options, org, repo string) (prowapi.ProwJob, 
 }
 
 // SchedulePJ will generate prowjob for testing and schedule it on prow for execution.
+// TODO: This function should return error instead stopping execution.
 func SchedulePJ(ghOptions prowflagutil.GitHubOptions) {
 	logrus.SetLevel(logrus.DebugLevel)
 	logrus.SetOutput(os.Stdout)
-	// TODO: use our logging clients
 	log.SetOutput(os.Stdout)
 	log.SetLevel(logrus.DebugLevel)
 	var err error
@@ -590,26 +590,26 @@ func SchedulePJ(ghOptions prowflagutil.GitHubOptions) {
 		log.WithError(err).Fatalf("Required environment variable not set.")
 	}
 	o := newCommonOptions(ghOptions)
-	// TODO: use test pjtesterv2.yaml file, change this to use a production pjtester.yaml
+	// TODO: for test purposes using pjtesterv2.yaml file, use commented line to use a production pjtester.yaml
+	// testCfgFile := path.Join(defaultClonePath, o.pjtesterPrOrg, o.pjtesterPrRepo, pjtesterConfigPath)
 	testCfgFile := path.Join(defaultClonePath, o.pjtesterPrOrg, o.pjtesterPrRepo, "vpath/pjtesterv2.yaml")
 	// read pjtester.yaml file
 	testCfg, err := readTestCfg(testCfgFile)
 	if err != nil {
-		log.Fatal("Pjtester config validation failed.")
+		log.WithError(err).Fatal("Failed read pjtester.yaml file")
 	}
-	// configPath is a location of prow config file to test. It was read from pjtester.yaml file or set to default.
 	o.setProwConfigPath(testCfg)
 	prowClientSet := newProwK8sClientset()
 	prowClient := prowClientSet.ProwV1()
 	ghc, err := o.github.NewGithubClient()
 	if err != nil {
-		log.WithError(err).Fatal("Failed to get GitHub client")
+		log.WithError(err).Fatal("Failed create GitHub client")
 	}
 	o.githubClient = ghc
 	o.gitOptions = git.ClientConfig{}
 	o.gitClient, err = o.gitOptions.NewClient(git.WithGithubClient(o.githubClient))
 	if err != nil {
-		log.WithError(err).Fatal("Failed to get git client")
+		log.WithError(err).Fatal("Failed create git client")
 	}
 	// TODO: migrate to use test-infra/development/github/pkg/client
 	o.prFinder = prtagbuilder.NewGitHubClient(nil)
@@ -619,15 +619,16 @@ func SchedulePJ(ghOptions prowflagutil.GitHubOptions) {
 		log.Debugf("getting details of pull requests for tested prowjobs")
 		pullRequests, err := o.getPullRequests(testCfg.PrConfigs)
 		if err != nil {
-			log.WithError(err).Fatalf("Failed get pull request deatils from GitHub.")
+			log.WithError(err).Fatal("Failed get pull request deatils from GitHub.")
 		}
 		o.testPullRequests = pullRequests
 	}
 	if &testCfg.PjConfigs.PrConfig != nil {
 		pullRequests, err := o.getPullRequests(testCfg.PjConfigs.PrConfig)
 		if err != nil {
-			log.WithError(err).Fatalf("Failed get pull request deatils from GitHub.")
+			log.WithError(err).Fatal("Failed get pull request deatils from GitHub.")
 		}
+		// There is only one element in each map level. This is enforced by testCfg struct values validation.
 		for _, prorg := range pullRequests {
 			for _, prconfig := range prorg {
 				o.pjConfigPullRequest = prconfig
