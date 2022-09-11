745b990ae2432687e61307a31512737d25495200
Till Knuesting <t.knuesting@sap.com>
2022-09-12 00:49:37 +0100

    add unit test

--- development/tools/jobs/kyma/kyma_integration_test.go
+++ development/tools/jobs/kyma/kyma_integration_test.go
@@ -35,6 +35,13 @@ func TestKymaIntegrationJobsPresubmit(t *testing.T) {
 				"installation/test/test/README.MD",
 			},
 		},
+		"Should contain the kyma-integration k3d with central Application Connectivity and Compass job": {
+			givenJobName: "pre-main-kyma-integration-k3d-central-app-connectivity-compass",
+
+			expPresets: []preset.Preset{
+				preset.GCProjectEnv, preset.KymaGuardBotGithubToken, preset.BuildPr, "preset-sa-vm-kyma-integration", "preset-kyma-integration-central-app-connectivity-enabled", "preset-kyma-integration-compass-dev", "preset-kyma-integration-compass-enabled",
+			},
+		},
 		"Should contain the kyma-integration k3d with telemetry job": {
 			givenJobName: "pre-main-kyma-integration-k3d-telemetry",
 
