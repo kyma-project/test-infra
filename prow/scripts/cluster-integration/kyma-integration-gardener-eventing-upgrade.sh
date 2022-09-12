8b8fd998e892a5338d17a3d426c76b8bcf6a99c6
VladislavPaskar <vladislav.paskar@sap.com>
2022-09-12 09:38:32 +0200

    change the versions

--- prow/scripts/cluster-integration/kyma-integration-gardener-eventing-upgrade.sh
+++ prow/scripts/cluster-integration/kyma-integration-gardener-eventing-upgrade.sh
@@ -152,7 +152,7 @@ ENABLE_TEST_CLEANUP=true
 eventing::pre_upgrade_test_fast_integration
 
 # upgrade the kyma to the current PR/commit state
-KYMA_SOURCE="PR-15418"
+KYMA_SOURCE="PR-${PULL_NUMBER}"
 export KYMA_SOURCE
 
 # uses previously set KYMA_SOURCE
