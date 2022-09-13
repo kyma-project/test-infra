e95096305d3f946a4590ccd2d7201d89fe969666
Piotr Halama <piotr.halama@sap.com>
2022-09-13 14:26:49 +0200

    fix

--- development/tools/pkg/yamlmerge/merge.go
+++ development/tools/pkg/yamlmerge/merge.go
@@ -2,7 +2,6 @@ package yamlmerge
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -47,13 +46,18 @@ func MergeFiles(path, extension, target string, changeFile bool) {
 func collectFiles(path, extension string) []string {
 	var files []string
 
-	fileInfo, err := ioutil.ReadDir(path)
+	dirInfo, err := os.ReadDir(path)
 	if err != nil {
 		log.Fatalf("Couldn't read files in path: %s", err.Error())
 	}
 
-	for _, f := range fileInfo {
-		if f.Mode().IsRegular() && filepath.Ext(f.Name()) == extension {
+	for _, f := range dirInfo {
+		fileInfo, err := f.Info()
+		if err != nil {
+			log.Fatalf("Couldn't read file info: %s", err.Error())
+		}
+
+		if fileInfo.Mode().IsRegular() && filepath.Ext(fileInfo.Name()) == extension {
 			files = append(files, fmt.Sprintf("%s%s%s", path, string(os.PathSeparator), f.Name()))
 		}
 	}
