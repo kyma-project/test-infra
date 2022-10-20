cfb3af2b7d83f5e715389feb3c0615437b3e355c
dekiel <przemyslaw.pokrywka@sap.com>
2022-10-20 11:24:59 +0200

    Using cloud run instead cloud function as an execution environment. Fixed HTTP response code on errors. Fixed null pointer error when using logger inside HTTP function. Main was creating own instance instead initialize a global variable.

--- development/gcp/cloud-run/updatesecretgithubchannel/updatesecretgithubchannel.go
+++ development/gcp/cloud-run/updatesecretgithubchannel/updatesecretgithubchannel.go
@@ -1,8 +1,7 @@
-package updatesecretgithubchannel
+package main
 
 import (
 	"context"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -10,11 +9,12 @@ import (
 	"os"
 	"path"
 	"strconv"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 
 	"cloud.google.com/go/compute/metadata"
-	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	// "github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/google/go-github/v42/github"
 	"github.com/kyma-project/test-infra/development/gcp/pkg/cloudfunctions"
 	"github.com/kyma-project/test-infra/development/gcp/pkg/pubsub"
@@ -31,6 +31,7 @@ var (
 	err               error
 	githubAccessToken string
 	githubClient      *client.SapToolsClient
+	logger            *cloudfunctions.LogEntry
 )
 
 // Alias holds mapping between owners file alias and slack groups and channels names.
@@ -41,11 +42,11 @@ type SyncEvent struct {
 	SecretEndpoints []string `yaml:"secret.endpoints,omitempty"`
 }
 
+/*
 func init() {
 	// Register an HTTP function with the Functions Framework
 	functions.HTTP("UpdateSecretOverGithubChannel", myHTTPFunction)
 
-	ctx := context.Background()
 	githubAccessToken = os.Getenv("GITHUB_ACCESS_TOKEN")
 	if githubAccessToken == "" {
 		panic("environment variable GITHUB_ACCESS_TOKEN is empty")
@@ -56,23 +57,63 @@ func init() {
 		panic(fmt.Sprintf("Failed creating github client, error: %s", err.Error()))
 	}
 }
+*/
+func main() {
+	var err error
+	ctx := context.Background()
+
+	logger = cloudfunctions.NewLogger()
+	// Set component for log entries to identify all messages for this function.
+	_ = logger.WithComponent("kyma.prow.cloud-function.UpdateSecretGitHubChannel")
+
+	githubAccessToken = os.Getenv("GITHUB_ACCESS_TOKEN")
+
+	projectID, err = metadata.ProjectID()
+	if err != nil {
+		panic("failed to retrieve GCP Project ID, error: " + err.Error())
+	}
+
+	githubAccessToken = os.Getenv("GITHUB_ACCESS_TOKEN")
+	if githubAccessToken == "" {
+		panic("environment variable GITHUB_ACCESS_TOKEN is empty")
+	}
+	// create github client
+	githubClient, err = client.NewSapToolsClient(ctx, githubAccessToken)
+	if err != nil {
+		logger.LogCritical("Failed creating github client, error: %w", err)
+	}
+
+	http.HandleFunc("/", myHTTPFunction)
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		logger.LogInfo("Defaulting to port %s", port)
+	}
+	// Start HTTP server.
+	logger.LogInfo("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		logger.LogCritical(err.Error())
+	}
+}
 
 // Function myHTTPFunction is an HTTP handler
 func myHTTPFunction(w http.ResponseWriter, r *http.Request) {
 	// Your code here
 	ctx := context.Background()
-	if projectID == "" {
-		projectID, err = metadata.ProjectID()
-		if err != nil {
-			panic("failed to retrieve GCP Project ID, error: " + err.Error())
-		}
-	}
+	// if projectID == "" {
+	//	projectID, err = metadata.ProjectID()
+	//	if err != nil {
+	//		panic("failed to retrieve GCP Project ID, error: " + err.Error())
+	//	}
+	// }
 
 	// Create logger to use google cloud functions structured logging
-	logger := cloudfunctions.NewLogger()
+	//	logger := cloudfunctions.NewLogger()
 	// Set component for log entries to identify all messages for this function.
-	logger.WithComponent("kyma.prow.cloud-function.UpdateSecretGitHubChannel")
+	//	logger.WithComponent("kyma.prow.cloud-function.UpdateSecretGitHubChannel")
 	// Set trace value for log entries to identify messages from one function call.
+	logger.LogInfo(projectID)
 	logger.GenerateTraceValue(projectID, "UpdateSecretGitHubChannel")
 
 	var message pubsub.Message
@@ -93,68 +134,96 @@ func myHTTPFunction(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		if _, err := fmt.Fprint(w, "400 - message is empty!"); err != nil {
 			logger.LogError("failed send response for message id: %s", message.Message.MessageID)
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte("NOK"))
 		}
-		return
 	}
 
 	// got valid message
 	logger.LogInfo("received message with id: %s", message.Message.MessageID)
 
 	// decode base64 prow message
-	bdata := make([]byte, base64.StdEncoding.DecodedLen(len(message.Message.Data)))
-	_, err := base64.StdEncoding.Decode(bdata, message.Message.Data)
-	if err != nil {
-		logger.LogError("prow message data field base64 decoding failed")
-		return
-	}
-	logger.LogInfo(string(bdata))
+	// bdata := make([]byte, base64.StdEncoding.DecodedLen(len(message.Message.Data)))
+	// _, err := base64.StdEncoding.Decode(bdata, message.Message.Data)
+	// if err != nil {
+	//	logger.LogError("prow message data field base64 decoding failed, error: %w", err)
+	//	w.WriteHeader(http.StatusInternalServerError)
+	//	_, _ = w.Write([]byte("NOK"))
+	//	return
+	// }
+	logger.LogInfo(string(message.Message.Data))
+	logger.LogInfo("%+v", message.Message.Attributes)
 
 	if message.Message.Attributes["eventType"] == "SECRET_VERSION_ADD" {
 
 		var syncEvent SyncEvent
-		secretName := path.Base(message.Message.Attributes["secretId"])
-		syncEventFilePath := "/" + secretName + ".yaml"
+		// secretName := path.Base(message.Message.Attributes["secretId"])
+		syncEventFilePath := "/" + message.Message.Attributes["secretId"] + ".yaml"
 		// Get file from github.
+		logger.LogInfo("getting sync event file from gtihub.tools.sap%s", syncEventFilePath)
 		syncEventFile, _, resp, err := githubClient.Repositories.GetContents(ctx, "kyma", "test-infra", syncEventFilePath, &github.RepositoryContentGetOptions{Ref: "secrets-sync"})
 		if err != nil {
 			logger.LogError("got error when getting sync event file from github.tools.sap, error: %w", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte("NOK"))
+			return
 		}
 		// Check HTTP response code
 		if ok, err := githubClient.IsStatusOK(resp); !ok {
 			logger.LogError("Response status for getting file content from github is not OK, error: %w", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte("NOK"))
+			return
 		}
 		// Read file content.
 		syncEventString, err := syncEventFile.GetContent()
 		if err != nil {
 			logger.LogError("got error when getting content of sync event file, error: %w", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte("NOK"))
+			return
 		}
 		err = yaml.Unmarshal([]byte(syncEventString), &syncEvent)
 		if err != nil {
 			logger.LogError("got error when unmarshaling sync event file content, error: %w", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte("NOK"))
+			return
 		}
 		versionString := path.Base(message.Message.Attributes["versionId"])
+		logger.LogInfo("secret versionId to update: %s", versionString)
 		versionInt, err := strconv.Atoi(versionString)
 		if err != nil {
 			logger.LogError("Failed convert secret version to sync from string to integer, error: %w", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte("NOK"))
+			return
 		}
 		syncEvent.SecretVersion = versionInt
 		updatedSyncEventFile, err := yaml.Marshal(syncEvent)
 		opts := &github.RepositoryContentFileOptions{
 			Message:   github.String("New secret version added. Synchronization needed."),
 			Content:   updatedSyncEventFile,
+			SHA:       syncEventFile.SHA,
 			Branch:    github.String("secrets-sync"),
 			Committer: &github.CommitAuthor{Name: github.String("SecretSync Bot"), Email: github.String("user@example.com")},
 		}
-		reposContentResponse, response, err := githubClient.Repositories.UpdateFile(ctx, "kyma", "test-infra", syncEventFilePath, opts)
+		_, syncEventFileGitHubPath, found := strings.Cut(syncEventFilePath, "/")
+		if !found {
+			logger.LogError("failed remove leading slash from event sync file path, provided file path: %s", syncEventFilePath)
+		}
+		reposContentResponse, response, err := githubClient.Repositories.UpdateFile(ctx, "kyma", "test-infra", syncEventFileGitHubPath, opts)
 		if err != nil {
 			logger.LogError("got error when updating sync event file in github.tools.sap, error: %w", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			_, _ = w.Write([]byte("NOK"))
+			return
 		}
 		if ok, err := githubClient.IsStatusOK(response); !ok {
 			logger.LogError("Response status for updating sync event file in github is not OK, error: %w", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			_, _ = w.Write([]byte("NOK"))
+			return
 		}
 		logger.LogInfo("Updated sync event file %s in github.tools.sap with commit %s", syncEventFilePath, reposContentResponse.Commit.SHA)
 	}
--- go.mod
+++ go.mod
@@ -37,6 +37,7 @@ require (
 	github.com/google/go-containerregistry v0.7.1-0.20211203164431-c75901cce627
 	github.com/google/go-github/v40 v40.0.0
 	github.com/google/go-github/v42 v42.0.0
+	github.com/google/go-github/v48 v48.0.0
 	github.com/google/go-querystring v1.1.0
 	github.com/huandu/xstrings v1.3.2 // indirect
 	github.com/imdario/mergo v0.3.13
