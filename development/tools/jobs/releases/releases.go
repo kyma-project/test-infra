044409c114386e5ea7586212a3f9016405145a15
Piotr Halama <piotr.halama@sap.com>
2022-11-15 10:34:41 +0100

    Fix releases Go file (#6391)

--- development/tools/jobs/releases/releases.go
+++ development/tools/jobs/releases/releases.go
@@ -5,9 +5,9 @@ package releases
 // List of currently supported releases
 var (
 	Release210 = mustParse("2.10")
-	Release29 = mustParse("2.9")
-	Release28 = mustParse("2.8")
-	Release27 = mustParse("2.7")
+	Release29  = mustParse("2.9")
+	Release28  = mustParse("2.8")
+	Release27  = mustParse("2.7")
 )
 
 // GetAllKymaReleases returns all supported kyma release branches
