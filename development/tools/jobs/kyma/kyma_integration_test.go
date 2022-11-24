75c33d3f898fc66b712ba2d161b8a120d0e0deb2
Wojciech Soltys <wojciech.soltys@sap.com>
2022-11-24 14:51:08 +0100

    Remove failing presubmit jobs

--- development/tools/jobs/kyma/kyma_integration_test.go
+++ development/tools/jobs/kyma/kyma_integration_test.go
@@ -154,7 +154,7 @@ func TestKymaIntegrationJobPeriodics(t *testing.T) {
 	require.NoError(t, err)
 
 	periodics := jobConfig.AllPeriodics()
-	assert.Len(t, periodics, 17)
+	assert.Len(t, periodics, 16)
 
 	expName := "kyma-upgrade-k3d-kyma2-to-main"
 	kymaUpgradePeriodic := tester.FindPeriodicJobByName(periodics, expName)
@@ -293,15 +293,4 @@ func TestKymaIntegrationJobPeriodics(t *testing.T) {
 		functionsMetricsPeriodic.Spec.Containers[0].Command)
 	tester.AssertThatContainerHasEnv(t, functionsMetricsPeriodic.Spec.Containers[0], "INPUT_CLUSTER_NAME", "nightly")
 	tester.AssertThatContainerHasEnv(t, functionsMetricsPeriodic.Spec.Containers[0], "CLUSTER_PROVIDER", "gcp")
-
-	expName = "serverless-function-benchmark"
-	functionsBenchmarksPeriodic := tester.FindPeriodicJobByName(periodics, expName)
-	require.NotNil(t, functionsBenchmarksPeriodic)
-	assert.Equal(t, expName, functionsBenchmarksPeriodic.Name)
-
-	assert.Equal(t, "0 1,13 * * *", functionsBenchmarksPeriodic.Cron)
-	assert.Equal(t, []string{"/home/prow/go/src/github.com/kyma-project/test-infra/prow/scripts/cluster-integration/kyma-serverless-benchmarks-nightly.sh"},
-		functionsBenchmarksPeriodic.Spec.Containers[0].Command)
-	tester.AssertThatContainerHasEnv(t, functionsBenchmarksPeriodic.Spec.Containers[0], "INPUT_CLUSTER_NAME", "nightly")
-	tester.AssertThatContainerHasEnv(t, functionsBenchmarksPeriodic.Spec.Containers[0], "CLUSTER_PROVIDER", "gcp")
 }
