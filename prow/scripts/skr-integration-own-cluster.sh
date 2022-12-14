486645f3c876b289347f552ecc71e2fc4d3cfa9f
Wojciech Wrzalik <74771103+ralikio@users.noreply.github.com>
2022-12-14 13:07:22 +0100

    Update prow/scripts/skr-integration-own-cluster.sh
    Co-authored-by: Jan Wozniak <wozniak.jan@gmail.com>

--- prow/scripts/skr-integration-own-cluster.sh
+++ prow/scripts/skr-integration-own-cluster.sh
@@ -18,7 +18,7 @@ if [[ "${BUILD_TYPE}" == "pr" ]]; then
 fi
 
 export SHOOT_DEFINITION_PATH="${TEST_INFRA_SOURCES_DIR}/prow/scripts/resources/skr-test/shoot-template.yaml"
-export GARDENER_SHOOT_TEMPLATE="$(envsubst < $SHOOT_DEFINITION_PATH | base64)"
+export GARDENER_SHOOT_TEMPLATE=$(envsubst < "$SHOOT_DEFINITION_PATH" | base64)
 export GARDENER_KUBECONFIG=$GARDENER_KYMA_PROW_KUBECONFIG
 export KCP_GARDENER_NAMESPACE="garden-$GARDENER_KYMA_PROW_PROJECT_NAME"
 
