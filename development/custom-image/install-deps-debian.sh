e6cb0e01e64d13e97e972e9b9a9d81ac7d2ab403
Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
2022-12-16 07:35:03 +0100

    Switch Debian 11 back to cgroups v1 (#6597)
    * Align minikube installation steps with official docs.
    
    * Switch back to cgroups v1

--- development/custom-image/install-deps-debian.sh
+++ development/custom-image/install-deps-debian.sh
@@ -64,7 +64,7 @@ wget https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz -O - | tar -xzO
 # install minikube
 curl -Lo /tmp/minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-amd64 && \
 chmod +x /tmp/minikube && \
-sudo mv /tmp/minikube /usr/local/bin/minikube
+sudo install /tmp/minikube /usr/local/bin/minikube
 
 
 # install postgres and migrate tool
@@ -105,3 +105,7 @@ echo 'export PATH="$PATH:/usr/local/go/bin"' | sudo tee -a /etc/profile
 # pre-fetch-docker-images
 sudo docker pull eu.gcr.io/kyma-project/external/cypress/included:8.7.0
 sudo docker pull eu.gcr.io/kyma-project/test-infra/docker-registry-2:20200202
+
+sudo sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT="\)\(.*\)\("\)/\1\2 systemd.legacy_systemd_cgroup_controller=false systemd.unified_cgroup_hierarchy=false\3/' /etc/default/grub
+sudo sed -i 's/\(GRUB_CMDLINE_LINUX="\)\(.*\)\("\)/\1\2 systemd.legacy_systemd_cgroup_controller=false systemd.unified_cgroup_hierarchy=false\3/' /etc/default/grub
+sudo update-grub
