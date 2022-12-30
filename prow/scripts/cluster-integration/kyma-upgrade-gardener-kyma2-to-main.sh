1a7e3c6e4d4eeab5669c388b75be0999698b2fc8
Jeremy Harisch <jeremy.harisch@sap.com>
2022-12-30 10:38:56 +0100

    tryout #17

--- prow/scripts/cluster-integration/kyma-upgrade-gardener-kyma2-to-main.sh
+++ prow/scripts/cluster-integration/kyma-upgrade-gardener-kyma2-to-main.sh
@@ -110,10 +110,10 @@ set +x
 log::info "### Installing Kyma $KYMA_SOURCE"
 
 kyma2_install_dir="$KYMA_SOURCES_DIR/kyma2"
-# kyma::deploy_kyma -s "$KYMA_SOURCE" -d "$kyma2_install_dir" -u "true"
+kyma::deploy_kyma -s "$KYMA_SOURCE" -d "$kyma2_install_dir" -u "true"
 
 # generate pod-security-policy list in json
-# utils::save_psp_list "${ARTIFACTS}/kyma-psp.json"
+utils::save_psp_list "${ARTIFACTS}/kyma-psp.json"
 
 # Pre-Upgrade Tests
 gardener::pre_upgrade_test_fast_integration_kyma -d kyma2/"$KYMA_SOURCE"/tests/fast-integration
