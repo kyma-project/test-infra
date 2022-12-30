cb19985f0bf9759178decb52ba3a7692759be933
Jeremy Harisch <48282931+jeremyharisch@users.noreply.github.com>
2022-12-30 10:26:57 +0100

    Apply suggestions from code review
    Co-authored-by: Tobias Schuhmacher <t.schuhmacher@sap.com>

--- prow/scripts/lib/gardener/gcp.sh
+++ prow/scripts/lib/gardener/gcp.sh
@@ -137,7 +137,7 @@ gardener::test_fast_integration_kyma() {
 gardener::pre_upgrade_test_fast_integration_kyma() {
     log::info "Running pre-upgrade Kyma Fast Integration tests - GCP"
 
-    kymaDirectory="${utils::get_kyma_fast_integration_dir $@}"
+    kymaDirectory="$(utils::get_kyma_fast_integration_dir $@)"
     log::info "Switching directory to '$kymaDirectory'"
     pushd "$kymaDirectory"
     make ci-pre-upgrade
@@ -149,7 +149,7 @@ gardener::pre_upgrade_test_fast_integration_kyma() {
 gardener::post_upgrade_test_fast_integration_kyma() {
     log::info "Running pre-upgrade Kyma Fast Integration tests - GCP"
 
-    kymaDirectory="${utils::get_kyma_fast_integration_dir $@}"
+    kymaDirectory="$(utils::get_kyma_fast_integration_dir $@)"
     log::info "Switching directory to '$kymaDirectory'"
     pushd "$kymaDirectory"
     make ci-post-upgrade
