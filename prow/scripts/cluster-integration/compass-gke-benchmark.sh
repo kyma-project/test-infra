830b6e5e05b969813948f2c5827de1baac947e22
Dimitar Petrov <dimitar.petrov@sap.com>
2023-01-03 14:52:37 +0200

    Adopt K8S 1.22 in compass benchmark job

--- prow/scripts/cluster-integration/compass-gke-benchmark.sh
+++ prow/scripts/cluster-integration/compass-gke-benchmark.sh
@@ -107,7 +107,7 @@ function createCluster() {
   gcp::provision_k8s_cluster \
         -c "$COMMON_NAME" \
         -p "$CLOUDSDK_CORE_PROJECT" \
-        -v "1.21.14" \
+        -v "1.22.16" \
         -j "$JOB_NAME" \
         -J "$PROW_JOB_ID" \
         -z "$CLOUDSDK_COMPUTE_ZONE" \
