fd05236e3e88cb1b534a60514a7a23c9fe9dfdd5
dekiel <przemyslaw.pokrywka@sap.com>
2023-01-07 23:51:35 +0100

    Use terraform to describe declarative scan_logs_for_secrets application deployment

---
+++ configs/terraform/secrets-leaks-log-scanner/.terraform.lock.hcl
@@ -0,0 +1,39 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/google" {
+  version     = "4.47.0"
+  constraints = "4.47.0"
+  hashes = [
+    "h1:lLDKVdwuDFlU8AqPX7v2DCwE+EdTYa0YSBTwyu/2LB4=",
+    "zh:030359653ee8e3a7a0ec30f06f7ac0d2bf5cb8bf895d06155646114fa13766e5",
+    "zh:14b18f549466eb99cac08fd2d89c8df3591cc3688c9e5ed12fb4a957482d0e2d",
+    "zh:3a9b9d1878f4004a7fee0ab8f8dea600307ad75a872f591590f704bf504b25af",
+    "zh:412b809bc61a74857912da5c2e9deddf5edf2eb883999ffb29573146ca12097b",
+    "zh:456372baa4fbb397f2d67fa291a15d216937e24dd1c05a66d6484ca2e24829e8",
+    "zh:69dc08f0e8eb672a8be11070b4ebb319b4a1725d928498607a0534a65089f3ef",
+    "zh:bc582257b94b0d2bd225b829016963580c681b09b0545c474420a27909b53fea",
+    "zh:bf1a8bf7e54f3e709d6323bdd12b5bf5cce3b245414f8aa09cb1860aa98f407d",
+    "zh:dc322cd105f3d0d0268e75a646f45471158ac668ed665309483f19c16385783b",
+    "zh:ee3b36c484dd4aea0855300e325fe31eee067eeb759192cee3ccad6fae45f610",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:f58ac64cc632d80243c2a97a009c44e8b7672adbab11cce488a77a7efeca33f0",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/template" {
+  version = "2.2.0"
+  hashes = [
+    "h1:0wlehNaxBX7GJQnPfQwTNvvAf38Jm0Nv7ssKGMaG6Og=",
+    "zh:01702196f0a0492ec07917db7aaa595843d8f171dc195f4c988d2ffca2a06386",
+    "zh:09aae3da826ba3d7df69efeb25d146a1de0d03e951d35019a0f80e4f58c89b53",
+    "zh:09ba83c0625b6fe0a954da6fbd0c355ac0b7f07f86c91a2a97849140fea49603",
+    "zh:0e3a6c8e16f17f19010accd0844187d524580d9fdb0731f675ffcf4afba03d16",
+    "zh:45f2c594b6f2f34ea663704cc72048b212fe7d16fb4cfd959365fa997228a776",
+    "zh:77ea3e5a0446784d77114b5e851c970a3dde1e08fa6de38210b8385d7605d451",
+    "zh:8a154388f3708e3df5a69122a23bdfaf760a523788a5081976b3d5616f7d30ae",
+    "zh:992843002f2db5a11e626b3fc23dc0c87ad3729b3b3cff08e32ffb3df97edbde",
+    "zh:ad906f4cebd3ec5e43d5cd6dc8f4c5c9cc3b33d2243c89c5fc18f97f7277b51d",
+    "zh:c979425ddb256511137ecd093e23283234da0154b7fa8b21c2687182d9aea8b2",
+  ]
+}
---
+++ configs/terraform/secrets-leaks-log-scanner/main.tf
@@ -0,0 +1,325 @@
+terraform {
+  backend "gcs" {
+    bucket = "tf-state-kyma-neighbors-dev"
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.47.0"
+    }
+  }
+}
+
+variable "google_project_id" {
+  type    = string
+  default = "sap-kyma-neighbors-dev"
+}
+
+provider "google" {
+  project = var.google_project_id
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role    = "roles/run.invoker"
+    members = ["allUsers"]
+  }
+}
+
+resource "google_cloud_run_service" "scan-logs-for-secrets" {
+  name     = "scan-logs-for-secrets"
+  location = "europe-west1"
+
+  metadata {
+    annotations = {
+      "run.googleapis.com/client-name" = "terraform"
+    }
+  }
+
+  template {
+    spec {
+      containers {
+        image = "europe-west3-docker.pkg.dev/sap-kyma-neighbors-dev/kyma-neighbors-dev-test/scanlogsforsecrets:0.0.49"
+        env {
+          name  = "PROJECT_ID"
+          value = var.google_project_id
+        }
+        env {
+          name  = "COMPONENT_NAME"
+          value = "logs-scanner"
+        }
+        env {
+          name  = "APPLICATION_NAME"
+          value = "secrets-leaks-log-scanner"
+        }
+        env {
+          name  = "LISTEN_PORT"
+          value = "8080"
+        }
+        env {
+          name  = "GCS_PREFIX"
+          value = "gcsweb.build.kyma-project.io/gcs/"
+        }
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "scan-logs-for-secrets-noauth" {
+  location = google_cloud_run_service.scan-logs-for-secrets.location
+  project  = google_cloud_run_service.scan-logs-for-secrets.project
+  service  = google_cloud_run_service.scan-logs-for-secrets.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+resource "google_cloud_run_service" "move-gcs-bucket" {
+  name     = "move-gcs-bucket"
+  location = "europe-west1"
+
+  metadata {
+    annotations = {
+      "run.googleapis.com/client-name" = "terraform"
+    }
+  }
+
+  template {
+    spec {
+      containers {
+        image = "europe-west3-docker.pkg.dev/sap-kyma-neighbors-dev/kyma-neighbors-dev-test/movegcsbucket:0.0.10"
+        env {
+          name  = "PROJECT_ID"
+          value = var.google_project_id
+        }
+        env {
+          name  = "COMPONENT_NAME"
+          value = "bucket-mover"
+        }
+        env {
+          name  = "APPLICATION_NAME"
+          value = "secrets-leaks-log-scanner"
+        }
+        env {
+          name  = "LISTEN_PORT"
+          value = "8080"
+        }
+        env {
+          name  = "DST_BUCKET_NAME"
+          value = "dev-prow-logs-secured"
+        }
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "move-gcs-bucket-noauth" {
+  location = google_cloud_run_service.move-gcs-bucket.location
+  project  = google_cloud_run_service.move-gcs-bucket.project
+  service  = google_cloud_run_service.move-gcs-bucket.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+resource "google_cloud_run_service" "search-github-issue" {
+  name     = "search-github-issue"
+  location = "europe-west1"
+
+  metadata {
+    annotations = {
+      "run.googleapis.com/client-name" = "terraform"
+    }
+  }
+
+  template {
+    spec {
+      containers {
+        image = "europe-west3-docker.pkg.dev/sap-kyma-neighbors-dev/kyma-neighbors-dev-test/searchgithubissue:0.0.20"
+        env {
+          name  = "PROJECT_ID"
+          value = var.google_project_id
+        }
+        env {
+          name  = "COMPONENT_NAME"
+          value = "issue-finder"
+        }
+        env {
+          name  = "APPLICATION_NAME"
+          value = "secrets-leaks-log-scanner"
+        }
+        env {
+          name  = "LISTEN_PORT"
+          value = "8080"
+        }
+        env {
+          name  = "GITHUB_ORG"
+          value = "neighbors-team"
+        }
+        env {
+          name  = "GITHUB_REPO"
+          value = "leaks-test"
+        }
+        volume_mounts {
+          mount_path = "/etc/github-token"
+          name       = "github-token"
+        }
+      }
+      volumes {
+        name = "github-token"
+        secret {
+          secret_name = "github-token"
+        }
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "search-github-issue-noauth" {
+  location = google_cloud_run_service.search-github-issue.location
+  project  = google_cloud_run_service.search-github-issue.project
+  service  = google_cloud_run_service.search-github-issue.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+resource "google_cloud_run_service" "create-github-issue" {
+  name     = "create-github-issue"
+  location = "europe-west1"
+
+  metadata {
+    annotations = {
+      "run.googleapis.com/client-name" = "terraform"
+    }
+  }
+
+  template {
+    spec {
+      containers {
+        image = "europe-west3-docker.pkg.dev/sap-kyma-neighbors-dev/kyma-neighbors-dev-test/creategithubissue:0.0.16"
+        env {
+          name  = "PROJECT_ID"
+          value = var.google_project_id
+        }
+        env {
+          name  = "COMPONENT_NAME"
+          value = "issue-creator"
+        }
+        env {
+          name  = "APPLICATION_NAME"
+          value = "secrets-leaks-log-scanner"
+        }
+        env {
+          name  = "LISTEN_PORT"
+          value = "8080"
+        }
+        env {
+          name  = "GITHUB_ORG"
+          value = "neighbors-team"
+        }
+        env {
+          name  = "GITHUB_REPO"
+          value = "leaks-test"
+        }
+        volume_mounts {
+          mount_path = "/etc/github-token"
+          name       = "github-token"
+        }
+      }
+      volumes {
+        name = "github-token"
+        secret {
+          secret_name = "github-token"
+        }
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "create-github-issue-noauth" {
+  location = google_cloud_run_service.create-github-issue.location
+  project  = google_cloud_run_service.create-github-issue.project
+  service  = google_cloud_run_service.create-github-issue.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+resource "google_cloud_run_service" "send-slack-message" {
+  name     = "send-slack-message"
+  location = "europe-west1"
+
+  metadata {
+    annotations = {
+      "run.googleapis.com/client-name" = "terraform"
+    }
+  }
+
+  template {
+    spec {
+      containers {
+        image = "europe-west3-docker.pkg.dev/sap-kyma-neighbors-dev/kyma-neighbors-dev-test/slackmessagesender:0.0.16"
+        env {
+          name  = "PROJECT_ID"
+          value = var.google_project_id
+        }
+        env {
+          name  = "COMPONENT_NAME"
+          value = "message-sender"
+        }
+        env {
+          name  = "APPLICATION_NAME"
+          value = "kyma-slack-bot"
+        }
+        env {
+          name  = "SLACK_CHANNEL_ID"
+          value = "C01KSP10MB5"
+        }
+        env {
+          name  = "SLACK_BASE_URL"
+          value = "https://slack.com/api"
+        }
+        volume_mounts {
+          mount_path = "/etc/slack-secret"
+          name       = "slack-secret"
+        }
+      }
+      volumes {
+        name = "slack-secret"
+        secret {
+          secret_name = "common-slack-bot-token"
+        }
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "send-slack-message-noauth" {
+  location = google_cloud_run_service.send-slack-message.location
+  project  = google_cloud_run_service.send-slack-message.project
+  service  = google_cloud_run_service.send-slack-message.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+data "template_file" "scan-logs-for-secrets_yaml" {
+  template = file("${path.module}/../../../development/gcp/workflows/scan-logs-for-secrets.yaml")
+  vars = {
+    scan-logs-for-secrets-url = google_cloud_run_service.scan-logs-for-secrets.status[0].url
+    move-gcs-bucket-url       = google_cloud_run_service.move-gcs-bucket.status[0].url
+    search-github-issue-url   = google_cloud_run_service.search-github-issue.status[0].url
+    create-github-issue-url   = google_cloud_run_service.create-github-issue.status[0].url
+    send-slack-message-url    = google_cloud_run_service.send-slack-message.status[0].url
+  }
+}
+
+resource "google_workflows_workflow" "scan-logs-for-secrets" {
+  name        = "poc-scan-logs-for-secrets"
+  region      = "europe-west4"
+  description = "Workflow is triggered on pubsub ..."
+  # service_account = google_service_account.workflows_service_account.id
+  # Imported main workflow YAML file
+  #source_contents = templatefile("${path.module}/../../../development/gcp/workflows/scan-logs-for-secrets.yaml",{})
+  source_contents = data.template_file.scan-logs-for-secrets_yaml.rendered
+
+}
