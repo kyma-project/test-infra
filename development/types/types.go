6074fea98d4c00971fdfc7bb35b2ab99465e7f16
Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
2023-01-10 13:09:54 +0100

    Scan logs for secrets dependencies (#6683)
    * Dependencies for scan_logs_for_secrets services.
    
    * Added code documentation.
    Moved github and gcp types to directories holding related code.
    
    * go mod tidy
    
    * fix typos
    
    * Removed unused fields.

--- development/types/types.go
+++ development/types/types.go
@@ -1,5 +1,9 @@
 package types
 
+import (
+	"github.com/zricethezav/gitleaks/v8/report"
+)
+
 // User holds kyma development team user details.
 // It provides mapping of various details used for integration different systems.
 // It holds information about automerge notification preferences.
@@ -17,4 +21,29 @@ type Alias struct {
 	ComEnterpriseSlackGroupsnames   []string `yaml:"com.slack.enterprise.sap.groupsnames,omitempty"`
 	ComEnterpriseSlackChannelsnames []string `yaml:"com.slack.enterprise.sap.channelsnames,omitempty"`
 	AutomergeNotifications          bool     `yaml:"automerge.notification,omitempty"`
+}
+
+// TODO: Migrate all usages to development/gcp/pkg/types package.
+type GCPStorageMetadata struct {
+	BucketName *string `json:"bucketName,omitempty"`
+	Directory  *string `json:"directory,omitempty"`
+}
+
+// TODO: Migrate all usages to development/gcp/pkg/types package.
+type GCPProjectMetadata struct {
+	Project *string `json:"project,omitempty"`
+}
+
+// TODO: Migrate all usages to development/github/pkg/types package.
+type GithubIssueMetadata struct {
+	GithubOrg         *string `json:"githubOrg,omitempty"`
+	GithubRepo        *string `json:"githubRepo,omitempty"`
+	GithubIssueNumber *int    `json:"githubIssueNumber,omitempty"`
+	GithubIssueURL    *string `json:"githubIssueURL,omitempty"`
+	GithubAssignee    User    `json:"githubIssueAssignee,omitempty"`
+}
+
+type SecretsLeakScannerMessage struct {
+	LeaksFound  *bool            `json:"leaksFound"`
+	LeaksReport []report.Finding `json:"leaksReport,omitempty"`
 }
