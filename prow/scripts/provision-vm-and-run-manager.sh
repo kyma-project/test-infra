f3ee10084a8ca26504ab6620d12419db1b1749c5
Filip Strózik <filip.strozik@outlook.com>
2023-01-26 09:57:48 +0100

    Keda integration tests (#6736)
    * change serverless-manager jobs location
    
    * make scripts generic again
    
    * add pjtester
    
    * fix ENVs
    
    * fix env name
    
    * make script general again
    
    * move generated templates to right folders
    
    * make template great again
    
    * add post verify run
    
    * generate from template
    
    * rename config targets
    
    * use proper make targets
    
    * add pjtester
    
    * fix scripts quotes
    
    * rebase
    
    * fix v2
    
    * pjtester for serverless-managera
    
    * fix pjtester file
    
    * delete vpath
    
    * not priviledge jobs
    
    * refactor jobs inheritedConfigs
    
    * add vpath/pjtester.yaml config
    
    * fix pjtester
    
    * make jobs priviledged again
    
    * make job unpriviledged again
    
    * remove pjtester.yaml

--- prow/scripts/provision-vm-and-run-manager.sh
+++ prow/scripts/provision-vm-and-run-manager.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# This script is designed to provision a new vm and start serverless chart. It takes an optional positional parameter using --image flag
-# Use this flag to specify the custom image for provisining vms. If no flag is provided, the latest custom image is used.
+# This script is designed to provision a new vm and run specific target from the Makefile on the repository root path.
+#
+# INPUT:
+# - JOB_REPO_NAME: name of the repository in the kyma-project org
+# - JOB_VM_COMMAND: desired command that should be run on vm
 
 set -o errexit
 
@@ -25,8 +28,8 @@ fi
 
 cleanup() {
     ARG=$?
-    log::info "Removing instance keda-manager-test-${RANDOM_ID}"
-    gcloud compute instances delete --zone="${ZONE}" "keda-manager-test-${RANDOM_ID}" || true ### Workaround: not failing the job regardless of the vm deletion result
+    log::info "Removing instance ${JOB_REPO_NAME}-test-${RANDOM_ID}"
+    gcloud compute instances delete --zone="${ZONE}" "${JOB_REPO_NAME}-test-${RANDOM_ID}" || true ### Workaround: not failing the job regardless of the vm deletion result
     date
     exit $ARG
 }
@@ -46,9 +49,9 @@ RANDOM_ID=$(openssl rand -hex 4)
 
 LABELS=""
 if [[ -z "${PULL_NUMBER}" ]]; then
-    LABELS=(--labels "branch=$PULL_BASE_REF,job-name=keda-manager")
+    LABELS=(--labels "branch=$PULL_BASE_REF,job-name=${JOB_REPO_NAME}")
 else
-    LABELS=(--labels "pull-number=$PULL_NUMBER,job-name=keda-manager")
+    LABELS=(--labels "pull-number=$PULL_NUMBER,job-name=${JOB_REPO_NAME}")
 fi
 
 POSITIONAL=()
@@ -93,14 +96,14 @@ ZONE_LIMIT=${ZONE_LIMIT:-5}
 EU_ZONES=$(gcloud compute zones list --filter="name~europe" --limit="${ZONE_LIMIT}" | tail -n +2 | awk '{print $1}')
 STARTTIME=$(date +%s)
 for ZONE in ${EU_ZONES}; do
-    log::info "Attempting to create a new instance named keda-manager-test-${RANDOM_ID} in zone ${ZONE} using image ${IMAGE}"
-    gcloud compute instances create "keda-manager-test-${RANDOM_ID}" \
+    log::info "Attempting to create a new instance named ${JOB_REPO_NAME}-test-${RANDOM_ID} in zone ${ZONE} using image ${IMAGE}"
+    gcloud compute instances create "${JOB_REPO_NAME}-test-${RANDOM_ID}" \
         --metadata enable-oslogin=TRUE \
         --image "${IMAGE}" \
         --machine-type n1-standard-4 \
         --zone "${ZONE}" \
         --boot-disk-size 200 "${LABELS[@]}" &&\
-    log::info "Created keda-manager-test-${RANDOM_ID} in zone ${ZONE}" && break
+    log::info "Created ${JOB_REPO_NAME}-test-${RANDOM_ID} in zone ${ZONE}" && break
     log::error "Could not create machine in zone ${ZONE}"
 done || exit 1
 ENDTIME=$(date +%s)
@@ -124,10 +127,10 @@ fi
 
 log::info "Copying Reconciler to the instance"
 #shellcheck disable=SC2088
-utils::compress_send_to_vm "${ZONE}" "keda-manager-test-${RANDOM_ID}" "/home/prow/go/src/github.com/kyma-project/keda-manager" "~/keda-manager"
+utils::compress_send_to_vm "${ZONE}" "${JOB_REPO_NAME}-test-${RANDOM_ID}" "/home/prow/go/src/github.com/kyma-project/${JOB_REPO_NAME}" "~/${JOB_REPO_NAME}"
 
 log::info "Triggering the installation"
-# TODO: Below line is a workaround -> Check issue https://github.com/kyma-project/test-infra/issues/6513
-utils::ssh_to_vm_with_script -z "${ZONE}" -n "keda-manager-test-${RANDOM_ID}" -c "sudo bash -c \"export PATH=\$PATH:\$HOME/keda-manager/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/go/bin && cd \$HOME/keda-manager && make -C hack/local run\""
-#&&	PATH=$PATH:$HOME/keda-manager/bin
+
+# shellcheck disable=SC2016
+utils::ssh_to_vm_with_script -z "${ZONE}" -n "${JOB_REPO_NAME}-test-${RANDOM_ID}" -c "sudo bash -c \"cd \$HOME/${JOB_REPO_NAME} && ${JOB_VM_COMMAND}\""
 log::success "all done"
