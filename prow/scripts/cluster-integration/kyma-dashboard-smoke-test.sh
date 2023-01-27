abc31356a8d585dfd1031332691a92393cbd5c20
Mateusz Wisniewski <mwisnia97@gmail.com>
2023-01-27 15:42:33 +0100

    smoke tests for kyma-dashboard stage/prod (#6799)
    * add templates and scripts
    
    * make
    
    * pjtester
    
    * chmod
    
    * I hope, I know what I'm doing
    
    * change command
    
    * remove unnecesary scripts and provide env to template
    
    * make
    
    * change path in template
    
    * change expected-contexts-regexp
    
    * log
    
    * chnage envs in templates
    
    * adjust run if change
    
    * remove log
    
    * adjust comments and run_ifs
    
    * change busola to kyma-dashboard
    
    * remove pjtester

--- prow/scripts/cluster-integration/kyma-dashboard-smoke-test.sh
+++ prow/scripts/cluster-integration/kyma-dashboard-smoke-test.sh
@@ -17,10 +17,11 @@ function load_env() {
 
 load_env
 echo DOCKER_TAG "${DOCKER_TAG}"
+echo IMAGE_NAME "${IMAGE_NAME}"
 # shellcheck disable=SC2086
-docker run -d --rm --net=host --pid=host --name kyma-dashboard eu.gcr.io/kyma-project/kyma-dashboard-local-dev:${DOCKER_TAG}
+docker run -d --rm --net=host --pid=host --name kyma-dashboard eu.gcr.io/kyma-project/kyma-dashboard-local-${IMAGE_NAME}:${DOCKER_TAG}
 
 cp "$PWD/kubeconfig-kyma.yaml" "$PWD/kyma-dashboard-tests/fixtures/kubeconfig.yaml"
 
-echo "STEP: Running Cypress tests inside Docker"
-docker run --entrypoint /bin/bash --network=host -v "$PWD/kyma-dashboard-tests:/tests" -w /tests $CYPRESS_IMAGE -c "npm ci --no-optional; NO_COLOR=1 CYPRESS_DOMAIN=http://localhost:3001 cypress run --browser chrome -C /tests/cypress.json"
+echo "STEP: Running Cypress smoke tests inside Docker"
+docker run --entrypoint /bin/bash --network=host -v "$PWD/kyma-dashboard-tests:/tests" -w /tests $CYPRESS_IMAGE -c "npm ci --no-optional; NO_COLOR=1 CYPRESS_DOMAIN=http://localhost:3001 cypress run --browser chrome -C cypress-smoke.json"
