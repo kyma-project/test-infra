163ffafe973e717c8d0c70da3aee5ad15fbe086e
Xin Ruan <xin.ruan@sap.com>
2023-01-31 14:10:21 +0100

    fix kyma2_install_dir

--- prow/scripts/cluster-integration/kyma-upgrade-gardener-kyma2-minor-versions.sh
+++ prow/scripts/cluster-integration/kyma-upgrade-gardener-kyma2-minor-versions.sh
@@ -83,14 +83,14 @@ function upgrade() {
     utils::save_psp_list "${ARTIFACTS}/kyma-psp.json"
 
     log::info "### Run post-upgrade tests"
-    gardener::post_upgrade_test_fast_integration_kyma -d "$kyma2_install_dir/${KYMA_SOURCE}/tests/fast-integration"
+    gardener::post_upgrade_test_fast_integration_kyma -d "${kyma2_install_dir}/tests/fast-integration"
 
     log::info "### waiting some time to finish cleanups"
     sleep 60
 
     log::info "### Run pre-upgrade tests again to validate component removal"
     export KYMA_SOURCE="${minor_release_versions[$((i))]}"
-    kyma2_install_dir="$KYMA_SOURCES_DIR/kyma2.$i"
+    kyma2_install_dir="$KYMA_SOURCES_DIR/$KYMA_SOURCE"
     gardener::pre_upgrade_test_fast_integration_kyma -d "${kyma2_install_dir}/tests/fast-integration"
   done
 }
