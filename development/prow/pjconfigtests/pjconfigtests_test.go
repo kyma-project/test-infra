9dfd6dfbc7496fad3e49fa9b2da15c457d70a960
dekiel <przemyslaw.pokrywka@sap.com>
2023-02-01 18:14:22 +0100

    Testing all types of prowjobs. Ginkgo suite loads valid env vars values. Tests print prowjob name in test failed message. This makes easy to locate file to fix. Dependabot update ginkgo dockerfile up to date. Dockerfile contains required gcc. Test prowjobs run ginkgo suites recursivelly

--- development/prow/pjconfigtests/pjconfigtests_test.go
+++ development/prow/pjconfigtests/pjconfigtests_test.go
@@ -7,14 +7,40 @@ import (
 )
 
 var _ = Describe("Prowjob,", func() {
-	for _, pj := range presubmitFixtures {
-		pj := pj
-		It("has pubsub config,", func() {
-			Expect(pj.Labels).To(MatchFields(IgnoreExtras, Fields{
-				"prow.k8s.io/pubsub.project": Equal("sap-kyma-prow"),
-				"prow.k8s.io/pubsub.runID":   Not(BeZero()),
-				"prow.k8s.io/pubsub.topic":   Equal("prowjobs"),
-			}))
-		})
-	}
+	Context("of presubmit type,", func() {
+		for _, pj := range presubmitFixtures {
+			pj := pj
+			It("has pubsub config,", func() {
+				Expect(pj.Labels).To(MatchKeys(IgnoreExtras, Keys{
+					"prow.k8s.io/pubsub.project": Equal("sap-kyma-prow"),
+					"prow.k8s.io/pubsub.runID":   Not(BeZero()),
+					"prow.k8s.io/pubsub.topic":   Equal("prowjobs"),
+				}), "Presubmit %s is missing pubsub required config.", pj.Name)
+			})
+		}
+	})
+	Context("of postsubmit type,", func() {
+		for _, pj := range postsubmitFixtures {
+			pj := pj
+			It("has pubsub config,", func() {
+				Expect(pj.Labels).To(MatchKeys(IgnoreExtras, Keys{
+					"prow.k8s.io/pubsub.project": Equal("sap-kyma-prow"),
+					"prow.k8s.io/pubsub.runID":   Not(BeZero()),
+					"prow.k8s.io/pubsub.topic":   Equal("prowjobs"),
+				}), "Postsubmit %s is missing pubsub required config.", pj.Name)
+			})
+		}
+	})
+	Context("of periodic type,", func() {
+		for _, pj := range periodicFixtures {
+			pj := pj
+			It("has pubsub config,", func() {
+				Expect(pj.Labels).To(MatchKeys(IgnoreExtras, Keys{
+					"prow.k8s.io/pubsub.project": Equal("sap-kyma-prow"),
+					"prow.k8s.io/pubsub.runID":   Not(BeZero()),
+					"prow.k8s.io/pubsub.topic":   Equal("prowjobs"),
+				}), "Periodic %s is missing pubsub required config.", pj.Name)
+			})
+		}
+	})
 })
