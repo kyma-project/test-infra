f2c1f8fab8546d50bad574f3065aff83ad028cd7
Xin Ruan <xin.ruan@sap.com>
2023-02-01 15:14:11 +0100

    Fix upgrade pipeline (#6828)
    * fix kyma2_install_dir
    
    * test kyma-upgrade-gardener-kyma2-minor-versions
    
    * test kyma-upgrade-gardener-kyma2-minor-versions
    
    * test kyma-upgrade-gardener-kyma2-minor-versions
    
    * use correct directory
    
    * test kyma-upgrade-gardener-kyma2-to-main-reconciler-main
    
    * Trigger job
    
    * test kyma-upgrade-gardener-kyma2-to-main-reconciler-main
    
    * Trigger job
    
    * remove pre-upgrade tests
    
    * remove pre-upgrade tests

--- prow/scripts/cluster-integration/kyma-upgrade-gardener-kyma2-minor-versions.sh
+++ prow/scripts/cluster-integration/kyma-upgrade-gardener-kyma2-minor-versions.sh
@@ -55,10 +55,10 @@ function deploy_base() {
 
   # uses previously set KYMA_SOURCE
   kyma2_install_dir="$KYMA_SOURCES_DIR/$KYMA_SOURCE"
-  kyma::deploy_kyma -s "$KYMA_SOURCE" -d "$kyma2_install_dir" -u "true"
+  kyma::deploy_kyma -s "$KYMA_SOURCE" -d "$KYMA_SOURCES_DIR" -u "true"
 
   log::info "### test directory: '$kyma2_install_dir/tests/fast-integration'"
-  gardener::pre_upgrade_test_fast_integration_kyma -d "${kyma2_install_dir}/${KYMA_SOURCE}/tests/fast-integration"
+  gardener::pre_upgrade_test_fast_integration_kyma -d "${kyma2_install_dir}/tests/fast-integration"
 }
 
 function upgrade() {
@@ -74,7 +74,7 @@ function upgrade() {
     log::info "### Installing Kyma $KYMA_SOURCE"
 
     kyma2_install_dir="$KYMA_SOURCES_DIR/$KYMA_SOURCE"
-    kyma::deploy_kyma -s "$KYMA_SOURCE" -d "$kyma2_install_dir" -u "true"
+    kyma::deploy_kyma -s "$KYMA_SOURCE" -d "$KYMA_SOURCES_DIR" -u "true"
 
     log::info "### restart all functions in all namespaces to workaround https://github.com/kyma-project/kyma/issues/14757"
     kubectl delete pod -l=serverless.kyma-project.io/managed-by=function-controller -A
@@ -83,15 +83,10 @@ function upgrade() {
     utils::save_psp_list "${ARTIFACTS}/kyma-psp.json"
 
     log::info "### Run post-upgrade tests"
-    gardener::post_upgrade_test_fast_integration_kyma -d "$kyma2_install_dir/${KYMA_SOURCE}/tests/fast-integration"
+    gardener::post_upgrade_test_fast_integration_kyma -d "${kyma2_install_dir}/tests/fast-integration"
 
     log::info "### waiting some time to finish cleanups"
     sleep 60
-
-    log::info "### Run pre-upgrade tests again to validate component removal"
-    export KYMA_SOURCE="${minor_release_versions[$((i))]}"
-    kyma2_install_dir="$KYMA_SOURCES_DIR/kyma2.$i"
-    gardener::pre_upgrade_test_fast_integration_kyma -d "${kyma2_install_dir}/tests/fast-integration"
   done
 }
 
