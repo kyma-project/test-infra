65c06b68b412f371f145e73111540147265dd5a7
Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
2023-02-14 11:03:39 +0100

    Alert when error is logged for secrets leak detector services. (#6920)
    * Alert when error is logged for secrets leak detector services.
    Use labels on structured log entries.
    
    * Cleanup

--- configs/terraform/secrets-leaks-log-scanner/github-issue-finder.tf
+++ configs/terraform/secrets-leaks-log-scanner/github-issue-finder.tf
@@ -1,19 +1,19 @@
 resource "google_service_account" "github_issue_finder" {
-  account_id   = "github-issue-finder-cr"
+  account_id  = "github-issue-finder-cr"
   description = "Identity of cloud run instance running github issue finder service."
 }
 
 resource "google_secret_manager_secret_iam_member" "gh_issue_finder_gh_tools_kyma_bot_token_accessor" {
-  project = data.google_secret_manager_secret.gh_tools_kyma_bot_token.project
+  project   = data.google_secret_manager_secret.gh_tools_kyma_bot_token.project
   secret_id = data.google_secret_manager_secret.gh_tools_kyma_bot_token.secret_id
-  role = "roles/secretmanager.secretAccessor"
-  member = "serviceAccount:${google_service_account.github_issue_finder.email}"
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.github_issue_finder.email}"
 }
 
 resource "google_cloud_run_service" "github_issue_finder" {
   depends_on = [google_secret_manager_secret_iam_member.gh_issue_finder_gh_tools_kyma_bot_token_accessor]
-  name     = "github-issue-finder"
-  location = "europe-west3"
+  name       = "github-issue-finder"
+  location   = "europe-west3"
 
   metadata {
     annotations = {
@@ -51,7 +51,7 @@ resource "google_cloud_run_service" "github_issue_finder" {
           value = "leaks-test"
         }
         env {
-          name = "TOOLS_GITHUB_TOKEN_PATH"
+          name  = "TOOLS_GITHUB_TOKEN_PATH"
           value = "/etc/gh-token/gh-tools-kyma-bot-token"
         }
         volume_mounts {
@@ -75,4 +75,25 @@ resource "google_cloud_run_service_iam_policy" "github_issue_finder" {
   service  = google_cloud_run_service.github_issue_finder.name
 
   policy_data = data.google_iam_policy.run_invoker.policy_data
+}
+resource "google_monitoring_alert_policy" "github_issue_finder" {
+  combiner     = "OR"
+  display_name = "github-issue-finder-error-logged"
+  conditions {
+    display_name = "error-log-message"
+    condition_matched_log {
+      filter = "resource.type=cloud_run_revision AND severity>=ERROR AND jsonPayload.component=github-issue-finder AND labels.io.kyma.app=secrets-leaks-detector"
+    }
+  }
+  notification_channels = ["projects/${var.google_project_id}/notificationChannels/5909844679104799956"]
+  alert_strategy {
+    notification_rate_limit {
+      period = "6 hr"
+    }
+    auto_close = "4 days"
+  }
+  user_labels = {
+    component = "github-issue-finder"
+    app       = "secrets-leak-detector"
+  }
 }
