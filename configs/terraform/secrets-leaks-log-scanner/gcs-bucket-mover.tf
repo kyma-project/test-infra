b1fb20351d5ea2740dd41a98e9deb95b45d18fb2
Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
2023-02-17 07:44:26 +0100

    Use prod images (#6982)
    * Terraform executor prod images.
    Fixed terraform apply params for postsubmit. Pr number is not available in context of postsubmit prowjob.
    
    * Use terraform executor prod images.
    Fixed tf apply parameters.
    Fixed variable in terraform config.
    
    * Fixed prowjob service account name.
    Rendertemplate.

--- configs/terraform/secrets-leaks-log-scanner/gcs-bucket-mover.tf
+++ configs/terraform/secrets-leaks-log-scanner/gcs-bucket-mover.tf
@@ -83,7 +83,7 @@ resource "google_monitoring_alert_policy" "gcs_bucket_mover" {
       filter = "resource.type=cloud_run_revision AND severity>=ERROR AND jsonPayload.component=gcs-bucket-mover AND labels.io.kyma.app=secrets-leaks-detector"
     }
   }
-  notification_channels = ["projects/${var.google_project_id}/notificationChannels/5909844679104799956"]
+  notification_channels = ["projects/${var.gcp_project_id}/notificationChannels/5909844679104799956"]
   alert_strategy {
     notification_rate_limit {
       period = "6 hr"
