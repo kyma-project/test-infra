166cf9bb45a60f7f110b873b064baa5cc06aebc5
Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
2023-02-23 11:34:32 +0100

    Fix tf apply errors (#7013)
    * Log based alerting policy use times in seconds.
    
    * Added remote state for core module.
    Fixed granting permissions on secrets in secret-manager and mounting them in cloud runs.
    Changed resource granting project IAM roles to non-authoritative.
    Fixed syntax in workflow steps.
    
    * Named resources.
    
    * Aligned naming.

--- configs/terraform/secrets-leaks-log-scanner/secrets-leak-log-scanner.tf
+++ configs/terraform/secrets-leaks-log-scanner/secrets-leak-log-scanner.tf
@@ -1,5 +1,5 @@
 resource "google_service_account" "secrets_leak_log_scanner" {
-  account_id  = "secrets-leak-log-scanner-cr"
+  account_id  = "secrets-leak-log-scanner"
   description = "Identity of cloud run instance running log scanner service."
 }
 
@@ -69,9 +69,9 @@ resource "google_monitoring_alert_policy" "secrets_leak_log_scanner" {
   notification_channels = ["projects/${var.gcp_project_id}/notificationChannels/5909844679104799956"]
   alert_strategy {
     notification_rate_limit {
-      period = "6 hr"
+      period = "21600s"
     }
-    auto_close = "4 days"
+    auto_close = "345600s"
   }
   user_labels = {
     component = "secrets-leak-log-scanner"
