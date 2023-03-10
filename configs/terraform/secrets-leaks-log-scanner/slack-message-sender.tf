cb3632af7a1974d8bc847fcbd17a7eae28b26862
Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
2023-03-10 15:30:50 +0100

    Use latest prod images in secret leak detector. (#7194)

--- configs/terraform/secrets-leaks-log-scanner/slack-message-sender.tf
+++ configs/terraform/secrets-leaks-log-scanner/slack-message-sender.tf
@@ -24,7 +24,7 @@ resource "google_cloud_run_service" "slack_message_sender" {
     spec {
       service_account_name = google_service_account.slack_message_sender.email
       containers {
-        image = "europe-docker.pkg.dev/kyma-project/dev/test-infra/slackmessagesender@sha256:6c570aedbfa15cb60eb537f2d3d55b69a5a6a3a6242ae2eef2714aa9e841532d"
+        image = "europe-docker.pkg.dev/kyma-project/prod/test-infra/slackmessagesender:v20230309-1d421c4f"
         env {
           name  = "PROJECT_ID"
           value = var.gcp_project_id
