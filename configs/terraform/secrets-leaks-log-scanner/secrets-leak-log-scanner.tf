a350b5ffd658aaca0fc14e6b44a561f6b1a655ee
Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
2023-03-10 15:30:50 +0100

    Use latest prod images in secret leak detector. (#7194)

--- configs/terraform/secrets-leaks-log-scanner/secrets-leak-log-scanner.tf
+++ configs/terraform/secrets-leaks-log-scanner/secrets-leak-log-scanner.tf
@@ -23,7 +23,7 @@ resource "google_cloud_run_service" "secrets_leak_log_scanner" {
     spec {
       service_account_name = google_service_account.secrets_leak_log_scanner.email
       containers {
-        image = "europe-docker.pkg.dev/kyma-project/dev/test-infra/scanlogsforsecrets:PR-7082"
+        image = "europe-docker.pkg.dev/kyma-project/prod/test-infra/scanlogsforsecrets:v20230309-1d421c4f"
         env {
           name  = "PROJECT_ID"
           value = var.gcp_project_id
