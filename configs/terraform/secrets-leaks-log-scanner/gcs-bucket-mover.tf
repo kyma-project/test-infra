bc398d057aae159acf89285d52c5d07ab93477be
dekiel <przemyslaw.pokrywka@sap.com>
2023-03-10 15:25:39 +0100

    Use latest prod images in secret leak detector.

--- configs/terraform/secrets-leaks-log-scanner/gcs-bucket-mover.tf
+++ configs/terraform/secrets-leaks-log-scanner/gcs-bucket-mover.tf
@@ -47,7 +47,7 @@ resource "google_cloud_run_service" "gcs_bucket_mover" {
     spec {
       service_account_name = google_service_account.gcs_bucket_mover.email
       containers {
-        image = "europe-docker.pkg.dev/kyma-project/prod/test-infra/movegcsbucket:v20230202-40569193"
+        image = "europe-docker.pkg.dev/kyma-project/prod/test-infra/movegcsbucket:v20230309-1d421c4f"
         env {
           name  = "PROJECT_ID"
           value = var.gcp_project_id
