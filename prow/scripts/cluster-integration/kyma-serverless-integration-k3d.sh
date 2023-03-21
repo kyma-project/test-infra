0349539dac83d6f905c971f3b8b1674c69138fbd
Damian Badura <damian.badura@sap.com>
2023-03-21 09:11:41 +0100

    fix script

--- prow/scripts/cluster-integration/kyma-serverless-integration-k3d.sh
+++ prow/scripts/cluster-integration/kyma-serverless-integration-k3d.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o pipefail # Fail a pipe if any sub-command fails.
 
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  # shellcheck source=prow/scripts/cluster-integration/kyma-serverless-run-test.sh
 source "${SCRIPT_DIR}/kyma-serverless-run-test.sh"
 
 date
