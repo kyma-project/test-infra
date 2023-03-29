fcfb0491ac1ef78187ea735cd5386632c5158249
khlifi411 <46890720+khlifi411@users.noreply.github.com>
2023-03-29 15:23:28 +0100

    trim release branch name

--- prow/scripts/provision-vm-and-test-reconciler-with-cli-k3d.sh
+++ prow/scripts/provision-vm-and-test-reconciler-with-cli-k3d.sh
@@ -123,14 +123,14 @@ export KYMA_SOURCE="main"
 if [[ "${KYMA_TEST_SOURCE}" == "latest-release" ]]; then
   # Fetch latest Kyma released version
   kyma_get_last_release_version_return_version=$(curl --silent --fail --show-error -H "Authorization: token ${BOT_GITHUB_TOKEN}" "https://api.github.com/repos/kyma-project/kyma/releases" \
-      | jq -r 'del( .[] | select( (.prerelease == true) or (.draft == true) )) | sort_by(.tag_name | split(".") | map(tonumber)) | .[-1].target_commitish')
+      | jq -r 'del( .[] | select( (.prerelease == true) or (.draft == true) )) | sort_by(.tag_name | split(".") | map(tonumber)) | .[-1].target_commitish | split("/") | .[-1]')
   export KYMA_SOURCE="${kyma_get_last_release_version_return_version:?}"
 
   log::info "### Reading latest release version from RELEASE_VERSION file, got: ${KYMA_SOURCE}"
 elif [[ "${KYMA_TEST_SOURCE}" == "previous-release" ]]; then
   # Fetch previous Kyma released version
   kyma_get_previous_release_version_return_version=$(curl --silent --fail --show-error -H "Authorization: token ${BOT_GITHUB_TOKEN}" "https://api.github.com/repos/kyma-project/kyma/releases" \
-      | jq -r 'del( .[] | select( (.prerelease == true) or (.draft == true) )) | sort_by(.tag_name | split(".") | map(tonumber)) | .[-2].target_commitish')
+      | jq -r 'del( .[] | select( (.prerelease == true) or (.draft == true) )) | sort_by(.tag_name | split(".") | map(tonumber)) | .[-2].target_commitish | split("/") | .[-1]')
   export KYMA_SOURCE="${kyma_get_previous_release_version_return_version:?}"
   log::info "### Reading previous release version from RELEASE_VERSION file, got: ${KYMA_SOURCE}"
 fi
@@ -139,7 +139,7 @@ fi
 if [[ "${KYMA_UPGRADE_SOURCE}" == "latest-release" ]]; then
   # Fetch latest Kyma released version
   kyma_get_last_release_version_return_version=$(curl --silent --fail --show-error -H "Authorization: token ${BOT_GITHUB_TOKEN}" "https://api.github.com/repos/kyma-project/kyma/releases" \
-        | jq -r 'del( .[] | select( (.prerelease == true) or (.draft == true) )) | sort_by(.tag_name | split(".") | map(tonumber)) | .[-1].target_commitish')
+        | jq -r 'del( .[] | select( (.prerelease == true) or (.draft == true) )) | sort_by(.tag_name | split(".") | map(tonumber)) | .[-1].target_commitish | split("/") | .[-1]')
   export KYMA_UPGRADE_VERSION="${kyma_get_last_release_version_return_version:?}"
   log::info "### Reading latest release version from RELEASE_VERSION file, got: ${KYMA_UPGRADE_VERSION}"
 fi
