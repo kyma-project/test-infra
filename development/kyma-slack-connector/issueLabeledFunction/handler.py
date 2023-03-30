d4981876e73c48949b6e012f5870129ac112c270
dekiel <przemyslaw.pokrywka@sap.com>
2023-03-30 22:35:14 +0200

    feat(handler.py): add support for mapping Github usernames to Slack usernames fix(handler.py): fix typo in log message This commit adds support for mapping Github usernames to Slack usernames by reading a YAML file containing the mapping. The function `get_slack_username` takes a Github username and returns the corresponding Slack username from the mapping file. The function is used to find the sender and assignee Slack usernames in the `main` function. Additionally, a typo in a log message is fixed.
    test(handler_test.py): add test for issueLabeledFunction handler
    feat(function.yaml): add TOOLS_GITHUB_BOT_TOKEN environment variable
    The new file `handler_test.py` contains a test for the `issueLabeledFunction` handler. The test sets the required environment variables and runs the handler with a test event and context. The `function.yaml` file has been updated to include the `TOOLS_GITHUB_BOT_TOKEN` environment variable, which is now set using a secret key reference. This allows the function to access the `users-map.yaml` file from the `kyma/test-infra` repository.

--- development/kyma-slack-connector/issueLabeledFunction/handler.py
+++ development/kyma-slack-connector/issueLabeledFunction/handler.py
@@ -1,85 +1,106 @@
 import os
 
+import yaml
+
 from github import Github
 from slack_bolt import App
 from slack_sdk.errors import SlackApiError
 
+TOOLS_GITHUB_TEST_INFRA_REPO = "kyma/test-infra"
+TOOLS_GITHUB_HOST = "github.tools.sap"
+USERS_MAP_FILE_PATH = "users-map.yaml"
+USERS_MAP_FILE_REF = "main"
+
+
+def get_slack_username(github_username: str, users_map: list) -> str | None:
+    for item in users_map:
+        if github_username == item["sap.tools.github.username"]:
+            return item["com.slack.enterprise.sap.username"]
+    return None
+
 
 def main(event, context):
-	# Using SLACK_BOT_TOKEN environment variable
-	slack_bot_token = os.environ['SLACK_BOT_TOKEN']
-	slack_channel = os.environ['NOTIFICATION_SLACK_CHANNEL']
-	app = App(token=slack_bot_token)
-	# slack_api_id = os.environ['SLACK_API_ID'].replace('-', '_')
-	# env_prefix = os.environ['ENV_PREFIX']
-	# base_url = os.environ['{}_SLACK_CONNECTOR_{}_GATEWAY_URL'.format(env_prefix, slack_api_id)]
-	# Set Slack API base URL to the URL of slack-connector application gateway.
-	# app.client.base_url = "{}/".format(base_url)
+    # Using SLACK_BOT_TOKEN environment variable
+    slack_bot_token = os.environ['SLACK_BOT_TOKEN']
+    slack_channel = os.environ['NOTIFICATION_SLACK_CHANNEL']
+    tools_github_bot_token = os.environ['TOOLS_GITHUB_BOT_TOKEN']
+    app = App(token=slack_bot_token)
 
-	# using an access token
-	g = Github("access_token")
+    # Github Enterprise with custom hostname
+    ghclient = Github(base_url=f"https://{TOOLS_GITHUB_HOST}/api/v3", login_or_token=tools_github_bot_token)
+    repo = ghclient.get_repo(TOOLS_GITHUB_TEST_INFRA_REPO)
+    content = repo.get_contents(USERS_MAP_FILE_PATH, ref=USERS_MAP_FILE_REF)
 
-	# Github Enterprise with custom hostname
-	g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
-
-	print("Received event of type issuesevent.labeled")
-	print("Using Slack api base URL: {}".format(app.client.base_url))
-	msg = event["data"]
-	label = msg["label"]["name"]
-	title = msg["issue"]["title"]
-	number = msg["issue"]["number"]
-	repo = msg["repository"]["name"]
-	org = msg["repository"]["owner"]["login"]
-	try:
-		assignee = "Issue #{} in repository {}/{} is assigned to `{}`.".format(number, org, repo, msg["issue"]["assignee"]["login"])
-	except TypeError:
-		assignee = "Issue #{} in repository {}/{} is not assigned.".format(number, org, repo)
-	sender = msg["sender"]["login"]
-	issue_url = msg["issue"]["html_url"]
-	# Run only for internal-incident and customer-incident labels
-	if (label == "internal-incident") or (label == "customer-incident"):
-		print("Label matched, Sending notifications to channel: {}".format(os.environ['NOTIFICATION_SLACK_CHANNEL']))
-		try:
-			# Deliver message to the channel.
-			result = app.client.chat_postMessage(channel=slack_channel,
-											 text="issue {} #{} labeld as {} in {}".format(title, number, label, repo),
-											 username="GithubBot",
-											 blocks=[
-												{
-													"type": "context",
-													"elements":
-														[
-															{
-																"type": "image",
-																"image_url": "https://mpng.subpng.com/20180802/bfy/kisspng-portable-network-graphics-computer-icons-clip-art-caribbean-blue-tag-icon-free-caribbean-blue-pric-5b63afe8224040.3966331515332597521403.jpg",
-																"alt_text": "label"
-															},
-															{
-																"type": "mrkdwn",
-																"text": "SAP Github issue labeled"
-															}
-														]
-												},
-												{
-													"type": "header",
-													"text": {
-														"type": "plain_text",
-														"text": "SAP Github {}".format(label)
-														}
-												},
-												{
-													"type": "section",
-													"text":
-														{
-															"type": "mrkdwn",
-															"text": "@here {} labeled issue `{}` as `{}`.\n{} <{}|See issue here.>".format(sender, title, label, assignee, issue_url)
-														}
-												},
-												])
-			assert result["ok"]
-			print("Sent notification for issue #{}".format(number))
-		except SlackApiError as e:
-			print(f"Got an error: {e.response['error']}")
-			print("Failed sent notification for issue #{}".format(number))
-	else:
-		print(f"Label {label} is not supported, ignoring.")
+    # Read users-map.yaml file content in to a dictionary
+    users_map = yaml.load(content.decoded_content.decode(), Loader=yaml.FullLoader)
+    # Find the sender sap.tools.github.username in the users_map list and return the com.slack.enterprise.sap.username
+    sender_slack_username = get_slack_username(event["data"]["sender"]["login"], users_map)
+    # Find the assignee sap.tools.github.username in the users_map list and return the com.slack.enterprise.sap.username
+    try:
+        assignee_slack_username = get_slack_username(event["data"]["issue"]["assignee"]["login"], users_map)
+    except TypeError:
+        assignee_slack_username = None
+    print("Received event of type issuesevent.labeled")
+    print(f"Using Slack api base URL: {app.client.base_url}")
+    msg = event["data"]
+    label = msg["label"]["name"]
+    title = msg["issue"]["title"]
+    number = msg["issue"]["number"]
+    repo = msg["repository"]["name"]
+    org = msg["repository"]["owner"]["login"]
+    if assignee_slack_username:
+        assignee = f"Issue #{number} in repository {org}/{repo} is assigned to <@{assignee_slack_username}>"
+    else:
+        assignee = f"Issue #{number} in repository {org}/{repo} is not assigned."
+    if sender_slack_username:
+        sender = f"<@{sender_slack_username}>"
+    else:
+        sender = msg["sender"]["login"]
+    issue_url = msg["issue"]["html_url"]
+    # Run only for internal-incident and customer-incident labels
+    if (label == "internal-incident") or (label == "customer-incident"):
+        print(f"Label matched, Sending notifications to channel: {slack_channel}")
+        try:
+            # Deliver message to the channel.
+            result = app.client.chat_postMessage(channel=slack_channel,
+                                                 text=f"issue {title} #{number} labeld as {label} in {repo}",
+                                                 username="GithubBot",
+                                                 blocks=[
+                                                     {
+                                                         "type": "context",
+                                                         "elements":
+                                                             [
+                                                                 {
+                                                                     "type": "image",
+                                                                     "image_url": "https://mpng.subpng.com/20180802/bfy/kisspng-portable-network-graphics-computer-icons-clip-art-caribbean-blue-tag-icon-free-caribbean-blue-pric-5b63afe8224040.3966331515332597521403.jpg",
+                                                                     "alt_text": "label"
+                                                                 },
+                                                                 {
+                                                                     "type": "mrkdwn",
+                                                                     "text": "SAP Github issue labeled"
+                                                                 }
+                                                             ]
+                                                     },
+                                                     {
+                                                         "type": "header",
+                                                         "text": {
+                                                             "type": "plain_text",
+                                                             "text": f"SAP Github {label}"
+                                                         }
+                                                     },
+                                                     {
+                                                         "type": "section",
+                                                         "text":
+                                                             {
+                                                                 "type": "mrkdwn",
+                                                                 "text": f"@here {sender} labeled issue `{title}` as `{label}`.\n{assignee} <{issue_url}|See issue here.>"
+                                                             }
+                                                     },
+                                                 ])
+            assert result["ok"]
+            print(f"Sent notification for issue #{number}")
+        except SlackApiError as e:
+            print(f"Got an error: {e.response['error']}")
+            print(f"Failed sent notification for issue #{number}")
+    else:
+        print(f"Label {label} is not supported, ignoring.")
