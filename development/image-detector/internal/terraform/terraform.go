9cd0ba2148cd21f4fd2bed167112877ef7274045
Kacper Malachowski <kacper.malachowski@sap.com>
2023-04-26 09:13:19 +0200

    Extract images from terraform using regexp

---
+++ development/image-detector/internal/terraform/terraform.go
@@ -0,0 +1,33 @@
+package terraform
+
+import (
+	"io"
+	"os"
+	"regexp"
+)
+
+func Extract(path string) ([]string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return extract(f)
+}
+
+func extract(reader io.Reader) ([]string, error) {
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	re := regexp.MustCompile("image[ ]+=[ ]+\"(.*[[a-z/-]+:[a-z0-9.]+?)\"")
+	substrings := re.FindAllStringSubmatch(string(data), -1)
+
+	var images []string
+	for _, substr := range substrings {
+		images = append(images, substr[1:]...)
+	}
+
+	return images, nil
+}
