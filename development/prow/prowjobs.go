7302fa9ece47c434848ab1421b2f20a8e9224c94
Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
2023-05-02 12:59:10 +0200

    Non failing check if prowjob has owner and description annotations. (#7534)
    * Add notifications for missing annotations on prowjobs.
    
    * pjtester
    
    * fix(pjconfigtests) fix getting all static prowjobs
    
    * feat(pjconfigtests) make logs more human readable.
    
    * Fix report entry formatting.
    
    * Do not run pjconfigtests localy.
    
    * Do not load static prowjobs for changes in inrepo config.
    
    * test printing to console
    
    * Remove duplication in warning messages.
    Highlight warnings in build log.
    
    * Remove pjtester.
    Update prow config template.
    
    * Fix formatting.
    
    * Added code documentation.

--- development/prow/prowjobs.go
+++ development/prow/prowjobs.go
@@ -13,14 +13,33 @@ import (
 
 const (
 	OrgDefaultClonePath        = "/home/prow/go/src/github.com"
-	TestInfraDefaultClonePath  = "/home/prow/go/src/github.com/kyma-project/test-infra"
 	ProwConfigDefaultClonePath = "/home/prow/go/src/github.com/kyma-project/test-infra/prow/config.yaml"
 	JobConfigDefaultClonePath  = "/home/prow/go/src/github.com/kyma-project/test-infra/prow/jobs"
+	OwnerAnnotationName        = "owner"
+	DescriptionAnnotationName  = "description"
 )
 
 // NotPresubmitError provides way to inform caller that prowjob is not a presubmit type.
 // It provides brief error messages about it.
 type NotPresubmitError struct{}
+
+type MissingRequiredAnnotations struct {
+	Owner       bool
+	Description bool
+	PjName      string
+}
+
+// non-colorable String() is used by go's string formatting support but ignored by ReportEntry
+func (s MissingRequiredAnnotations) String() string {
+	if s.Owner && s.Description {
+		return fmt.Sprintf("Prowjob %s is missing required annotations: %s, %s", s.PjName, OwnerAnnotationName, DescriptionAnnotationName)
+	} else if s.Owner {
+		return fmt.Sprintf("Prowjob %s is missing required annotation: %s", s.PjName, OwnerAnnotationName)
+	} else if s.Description {
+		return fmt.Sprintf("Prowjob %s is missing reuired annotation: %s", s.PjName, DescriptionAnnotationName)
+	}
+	return ""
+}
 
 func (e *NotPresubmitError) Error() string { return "prowjob is not a presubmit type" }
 
@@ -54,29 +73,40 @@ func GetPrAuthorForPresubmit() ([]string, error) {
 	return nil, &NotPresubmitError{}
 }
 
+// GetOrgForPresubmit will provide organization name for prowjob of type presubmit.
 func GetOrgForPresubmit() (string, error) {
 	// Get data, about prowjob specification from prowjob environment variables set by prow.
 	jobSpec, err := downwardapi.ResolveSpecFromEnv()
 	if err != nil {
 		return "", fmt.Errorf("failed to read JOB_SPEC env, got error: %w", err)
 	}
-	// Get authors for presubmits only.
+	// Get org name for presubmits only.
 	if jobSpec.Type == prowapi.PresubmitJob {
 		return jobSpec.Refs.Org, nil
 	}
 	return "", &NotPresubmitError{}
 }
 
+// GetProwjobsConfigForProwjob will provide all prowjobs configuration stored in repository described by orgName and repoName.
+// If orgName and repoName are set to "kyma-project" and "test-infra" respectively, then static prowjobs configuration will be returned.
+// Otherwise, inrepo prowjobs configuration will be returned.
+// Prowjobs configuration is read from local repository clone.
 func GetProwjobsConfigForProwjob(orgName, repoName, prowConfigPath, staticJobConfigPath, inrepoConfigPath string) ([]config.Presubmit, []config.Postsubmit, []config.Periodic, error) {
+	var (
+		presubmits  []config.Presubmit
+		postsubmits []config.Postsubmit
+		periodics   []config.Periodic
+	)
 	repoIdentifier := orgName + "/" + repoName
 	conf, err := config.Load(prowConfigPath, staticJobConfigPath, nil, "")
 	if err != nil {
 		return nil, nil, nil, fmt.Errorf("error loading prow configs, got error: %w", err)
 	}
-	presubmits := conf.GetPresubmitsStatic(repoIdentifier)
-	postsubmits := conf.GetPostsubmitsStatic(repoIdentifier)
-	periodics := conf.AllPeriodics()
-	if orgName != "kyma-project" && repoName != "test-infra" {
+	if orgName == "kyma-project" && repoName == "test-infra" {
+		presubmits = conf.AllStaticPresubmits([]string{})
+		postsubmits = conf.AllStaticPostsubmits([]string{})
+		periodics = conf.AllPeriodics()
+	} else {
 		prowYAML, err := config.ReadProwYAML(logrus.WithField("repo", repoIdentifier), inrepoConfigPath, false)
 		if err != nil {
 			return nil, nil, nil, fmt.Errorf("error loading inrepo config, got error: %w", err)
@@ -85,4 +115,19 @@ func GetProwjobsConfigForProwjob(orgName, repoName, prowConfigPath, staticJobCon
 		postsubmits = append(postsubmits, prowYAML.Postsubmits...)
 	}
 	return presubmits, postsubmits, periodics, nil
+}
+
+func CheckRequiredAnnotations(name string, a map[string]string) MissingRequiredAnnotations {
+	var missingAnnotations MissingRequiredAnnotations
+	if _, ok := a[OwnerAnnotationName]; !ok {
+		missingAnnotations.Owner = true
+	}
+	if _, ok := a[DescriptionAnnotationName]; !ok {
+		missingAnnotations.Description = true
+	}
+	if missingAnnotations.Owner || missingAnnotations.Description {
+		missingAnnotations.PjName = name
+		return missingAnnotations
+	}
+	return MissingRequiredAnnotations{}
 }
