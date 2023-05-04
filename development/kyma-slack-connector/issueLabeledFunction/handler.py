b448626b9437f914b4b55c4829179e2e75eeb028
Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
2023-05-04 11:23:04 +0200

    Fix issues labeled notification (#7554)
    * Sync github tools kyma bot token.
    Use environment variables with slack bot token instead service bindings.
    
    * feat(handler.py): add support for mapping Github usernames to Slack usernames
    fix(handler.py): fix typo in log message
    This commit adds support for mapping Github usernames to Slack usernames by reading a YAML file containing the mapping. The function `get_slack_username` takes a Github username and returns the corresponding Slack username from the mapping file. The function is used to find the sender and assignee Slack usernames in the `main` function. Additionally, a typo in a log message is fixed.
    
    test(handler_test.py): add test for issueLabeledFunction handler
    feat(function.yaml): add TOOLS_GITHUB_BOT_TOKEN environment variable
    The new file `handler_test.py` contains a test for the `issueLabeledFunction` handler. The test sets the required environment variables and runs the handler with a test event and context. The `function.yaml` file has been updated to include the `TOOLS_GITHUB_BOT_TOKEN` environment variable, which is now set using a secret key reference. This allows the function to access the `users-map.yaml` file from the `kyma/test-infra` repository.
    
    * chore(slack-message-sender): add pyyaml to requirements.txt
    This commit adds pyyaml to the requirements.txt file for the slack-message-sender service in the development/gcp/cloud-run directory. This is a dependency required for the service to function properly.
    
    refactor(handler.py): remove None type from get_slack_username return type
    This commit removes the None type from the return type of the get_slack_username function, as it was causing interpreter errors. It also adds support for handling unassigned issues by returning None if the assignee is not set. The commit also includes comments to explain the changes made and the purpose of the code.
    
    refactor(handler.py): improve code readability by adding comments and whitespace
    The code has been refactored to improve readability by adding comments and whitespace. This makes it easier to understand the code and its purpose.
    
    chore(slackConnectorChart): update global-prod-values.yaml and function.yaml
    The global-prod-values.yaml file has been updated to remove the slackBotTokenSecretEnabled property, which is no longer needed. The secret is now provided by external secrets operator. The function.yaml file has been updated to use the v1alpha2 version of the serverless.kyma-project.io API, and to change the type of the function source from git to gitRepository. The source now includes the url, reference, and baseDir properties, which are used to specify the location of the function code.
    
    chore(slackConnectorChart): remove unused yaml files
    The functionServiceBindingUsage.yaml and slackBotTokenSecret.yaml files were deleted as they were no longer needed.
    
    * fix(issueLabeledFunction) added exception handling if key in item doesn't exist
    
    * pass is not needed as except is not empty.

--- development/kyma-slack-connector/issueLabeledFunction/handler.py
+++ development/kyma-slack-connector/issueLabeledFunction/handler.py
@@ -13,8 +13,11 @@ USERS_MAP_FILE_REF = "main"
 
 def get_slack_username(github_username: str, users_map: list) -> str:
     for item in users_map:
-        if github_username == item["sap.tools.github.username"]:
-            return item["com.slack.enterprise.sap.username"]
+        try:
+            if github_username == item["sap.tools.github.username"]:
+                return item["com.slack.enterprise.sap.username"]
+        except KeyError:
+            print(f"KeyError: sap.tools.github.username or com.slack.enterprise.sap.username not found in {item}")
     return None
 
 
