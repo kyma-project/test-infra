2c69957a51b355edb42b042546950c8ff3924310
Kacper Małachowski <38684517+KacperMalachowski@users.noreply.github.com>
2023-05-12 10:07:00 +0200

    Automatically update list of images in security scanners config (#7559)
    * Basic ProwJob detectorish
    
    * Add extraction from prowjobs
    
    * Return empty slice instead of nil
    
    * Return only unique images
    
    * Cleanup slightly prowjob container image detection
    
    * Detect images in k8s yaml files
    
    * Support multiple documents in file for k8s
    
    * Move prowjob detector to separate file
    
    * Adjust prowjob detector to new internal package structure
    
    * Extract images from terraform using regexp
    
    * Fix regexp for terraform files
    
    * Remove file in old structure
    
    * basic works with current detectors
    
    * Add parsing of security config
    
    * Change parsing config to only update protecode images
    
    * Add tests for main image-detector file
    
    * Move security config parser to pkg directory
    
    * Remove internal security config
    
    * Delete prowjob.go
    
    * Delete prowjob_test.go
    
    * search terraform for docker image not exact in image field
    
    * move extracting images from terraform to pkg extractimages
    
    * Move prowjob extract to pkg extractimages, fix test error message
    
    * Move k8s images extract fucntions to extractimages
    
    * Extract images from kubernetes
    
    * Use cobra flags style
    
    * Add comments to extracted package
    
    * Remove deleted kubernetes extractor
    
    * Fix path for kubernetes files
    
    * Rename extractimages to extractimageurls
    
    * Remove unecessarry functions from extractimageurls
    
    * move utils functions to extractimageurls
    
    * Remove unused loadsecurityconfig
    
    * Update image-detector/main.go
    
    * Remove cmd/image_detector.go file
    
    * Remove unecessary fucntions from extractimageurls/prowjob.go
    
    * Use k8s apps package instead of own structure
    
    * Use k8s package to define deployment struct
    
    * Add comments to functions, including not exported
    
    * Update image-detector/main.go with changes
    
    * Remove unused code from kubernetes.go
    
    * Fix kubernetes image urls extratcion

---
+++ development/image-detector/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/kyma-project/test-infra/development/pkg/extractimageurls"
+	"github.com/kyma-project/test-infra/development/pkg/securityconfig"
+	"github.com/spf13/cobra"
+	"k8s.io/test-infra/prow/config"
+)
+
+var (
+	// ProwConfig contains path to prow config file
+	ProwConfig string
+
+	// JobsConfigDir contains root path for directory containing Prow Jobs Configs
+	JobsConfigDir string
+
+	// TerraformDir contains root path to directory containing terraform files
+	TerraformDir string
+
+	// SecScannerConfig contains path to security scanners config .yaml file
+	SecScannerConfig string
+
+	// KubernetesFiles contains root path to directory containing kubernetes deployments file
+	KubernetesFiles string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "image-detector",
+	Short: "Image Detector CLI",
+	Long:  "Command-Line tool to retrieve list of images and update security-config",
+	Run: func(cmd *cobra.Command, args []string) {
+		// load images from security config
+		reader, err := os.Open(SecScannerConfig)
+		if err != nil {
+			log.Fatalf("failed to open security config file %s", err)
+		}
+		securityConfig, err := securityconfig.ParseSecurityConfig(reader)
+		if err != nil {
+			log.Fatalf("failed to parse security config file: %s", err)
+		}
+
+		images := securityConfig.Images
+
+		// get images from prow jobs
+		prowConfig, err := config.Load(ProwConfig, JobsConfigDir, nil, "")
+		if err != nil {
+			log.Fatalf("failed to load prow job config: %s", err)
+		}
+
+		images = append(images, extractimageurls.FromProwJobConfig(prowConfig.JobConfig)...)
+
+		// get images from terraform
+		files, err := extractimageurls.FindFilesInDirectory(TerraformDir, ".*.(tf|tfvars)")
+		if err != nil {
+			log.Fatalf("failed to find files in terraform directory %s: %s", TerraformDir, err)
+		}
+
+		imgs, err := extractimageurls.FromFiles(files, extractimageurls.FromTerraform)
+		if err != nil {
+			log.Fatalf("failed to extract images from terraform files: %s", err)
+		}
+
+		images = append(images, imgs...)
+
+		// get images from kubernetes
+		files, err = extractimageurls.FindFilesInDirectory(KubernetesFiles, ".*.(yaml|yml)")
+		if err != nil {
+			log.Fatalf("failed to find files in kubernetes directory %s: %s", KubernetesFiles, err)
+		}
+
+		imgs, err = extractimageurls.FromFiles(files, extractimageurls.FromKubernetesDeployments)
+		if err != nil {
+			log.Fatalf("failed to extract images from kubernetes files: %s", err)
+		}
+
+		images = append(images, imgs...)
+
+		images = extractimageurls.UniqueImages(images)
+
+		// write images to security config
+		securityConfig.Images = images
+		securityConfig.SaveToFile(SecScannerConfig)
+	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&ProwConfig, "prow-config", "", "path to the prow config file")
+	rootCmd.PersistentFlags().StringVar(&JobsConfigDir, "prow-jobs-dir", "", "path to the directory which contains prow job files")
+	rootCmd.PersistentFlags().StringVar(&TerraformDir, "terraform-dir", "", "path to the directory containing terraform files")
+	rootCmd.PersistentFlags().StringVar(&SecScannerConfig, "sec-scanner-config", "", "path to the security scanner config fiel")
+	rootCmd.PersistentFlags().StringVar(&KubernetesFiles, "kubernetes-dir", "", "path to the directory containing kubernetes deployments")
+
+	rootCmd.MarkFlagRequired("prow-config")
+	rootCmd.MarkFlagRequired("prow-jobs-dir")
+	rootCmd.MarkFlagRequired("terraform-dir")
+	rootCmd.MarkFlagRequired("sec-scanner-config")
+	rootCmd.MarkFlagRequired("kubernetes-dir")
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf("failed to run command: %s", err)
+	}
+}
