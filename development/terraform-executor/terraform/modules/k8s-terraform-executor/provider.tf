e1824de3d748e347f8ba0d3016bb5359715930e2
dekiel <przemyslaw.pokrywka@sap.com>
2023-05-18 14:32:51 +0200

    Kubernetes provider doesn't support loading config file.

--- development/terraform-executor/terraform/modules/k8s-terraform-executor/provider.tf
+++ development/terraform-executor/terraform/modules/k8s-terraform-executor/provider.tf
@@ -25,7 +25,6 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(
     data.google_container_cluster.managed_k8s_cluster.master_auth[0].cluster_ca_certificate,
   )
-  load_config_file = false
 }
 
 provider "google" {
