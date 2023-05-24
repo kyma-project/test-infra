06bb46d320f1e963f20f89248079514d473df24f
Piotr Halama <piotr.halama@sap.com>
2023-05-24 12:25:29 +0200

    descriptions

--- configs/terraform/environments/prod/variables.tf
+++ configs/terraform/environments/prod/variables.tf
@@ -107,24 +107,34 @@ variable "terraform_executor_gcp_service_account" {
 variable "constraint_templates_path" {
   type    = string
   default = "../../../../opa/gatekeeper/constraint-templates/**.yaml"
+
+  description = "Path to the constraint templates directory."
 }
 
 variable "tekton_constraints_path" {
   type    = string
   default = "../../../../tekton/deployments/gatekeeper-constraints/**.yaml"
+
+  description = "Path to the tekton cluster constraints directory."
 }
 
 variable "trusted_workloads_constraints_path" {
   type    = string
   default = "../../../../prow/cluster/resources/gatekeeper-constraints/trusted/**.yaml"
+
+  description = "Path to the trusted workloads cluster constraints directory."
 }
 
 variable "untrusted_workloads_constraints_path" {
   type    = string
   default = "../../../../prow/cluster/resources/gatekeeper-constraints/untrusted/**.yaml"
+
+  description = "Path to the untrusted workloads cluster constraints directory."
 }
 
 variable "workloads_constraints_path" {
   type    = string
   default = "../../../../prow/cluster/resources/gatekeeper-constraints/workloads/**.yaml"
+
+  description = "Path to both workload clusters constraints directory."
 }
