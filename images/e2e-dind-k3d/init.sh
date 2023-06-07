191f3c9dd6f284f37839d0781b378d9b3c24a2a4
Kamil Kasperski <kamil.kasperski@sap.com>
2023-06-07 10:37:52 +0200

    Add automatic provisioning of k3d in e2e-k3d image

--- images/e2e-dind-k3d/init.sh
+++ images/e2e-dind-k3d/init.sh
@@ -4,9 +4,21 @@ set -e
 
 LOG_DIR=${ARTIFACTS:-"/var/log"}
 
-if [[ -n "${DOCKER_IN_DOCKER_ENABLED}" ]]; then
+if [[ "${DOCKER_IN_DOCKER_ENABLED}" == "true" ]]; then
   echo "[* * *] Starting Docker in Docker"
   dockerd --data-root=/docker-graph > "${LOG_DIR}/dockerd.log" 2>&1 &
 fi
 
+if [[ "$K3D_ENABLED" == "true" ]]; then
+  ARGS=()
+  echo -n "[ * * * ] Provisioning k3d cluster"
+  if [[ "$PROVISION_REGISTRY" == "true" ]]; then
+    echo " with registry k3d-registry.localhost:5000"
+    k3d registry create registry.localhost --port 5000
+    ARGS+=( "--registry-use=k3d-registry.localhost:5000" )
+  else
+    echo
+  fi
+  k3d cluster create k3d "${ARGS[@]}"
+fi
 exec "$@"
