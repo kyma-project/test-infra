f6f03bfaf8e01b72c4127eccaf9f3cffe4859e42
Kamil Kasperski <kamil.kasperski@sap.com>
2023-06-16 10:57:41 +0200

    Add initial e2e-dind-nodejs image (#7897)
    /area ci
    /kind feature
    
    add additional debian-based image that contains nodejs16, docker, k3d and chrome browser.
    It's meant to be used with Cypress-based UI tests

---
+++ images/e2e-dind-nodejs/init.sh
@@ -0,0 +1,25 @@
+#!/usr/local/bin/dumb-init /bin/bash
+
+set -e
+
+LOG_DIR=${ARTIFACTS:-"/var/log"}
+
+if [[ "${DOCKER_IN_DOCKER_ENABLED}" == "true" ]]; then
+  echo "[ * * * ] Starting Docker in Docker"
+  dockerd --data-root=/docker-graph > "${LOG_DIR}/dockerd.log" 2>&1 &
+  sleep 5 # sleep to wait for Docker daemon - idk if we can use some kind of condition
+fi
+
+if [[ "$K3D_ENABLED" == "true" ]]; then
+  ARGS=()
+  echo -n "[ * * * ] Provisioning k3d cluster"
+  if [[ "$PROVISION_REGISTRY" == "true" ]]; then
+    echo " with registry k3d-registry.localhost:5000"
+    k3d registry create registry.localhost --port 5000
+    ARGS+=( "--registry-use=k3d-registry.localhost:5000" )
+  else
+    echo
+  fi
+  k3d cluster create k3d "${ARGS[@]}"
+fi
+exec "$@"
