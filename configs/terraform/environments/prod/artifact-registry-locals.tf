e5d75ec6062aaa204136f80748eadf01b46fc21c
Kiss, Tibor <tibor.kiss01@sap.com>
2023-06-29 12:33:08 +0200

    Get rid of locals.tf file

--- configs/terraform/environments/prod/artifact-registry-locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  artifact_registry_tags = {
-    owner  = var.artifact_registry_owner
-    module = var.artifact_registry_module
-    type   = var.artifact_registry_type
-  }
-
-  location = var.artifact_registry_multi_region == true ? var.artifact_registry_primary_area : var.gcp_region
-}
--- configs/terraform/environments/prod/artifact-registry.tf
+++ configs/terraform/environments/prod/artifact-registry.tf
@@ -2,15 +2,17 @@
 
 resource "google_artifact_registry_repository" "artifact_registry" {
   count         = var.artifact_registry_count
-  location      = local.location
+  location      = var.artifact_registry_multi_region == true ? var.artifact_registry_primary_area : var.gcp_region
   repository_id = "${lower(var.artifact_registry_prefix)}-${lower(var.artifact_registry_names[count.index])}"
   description   = "${lower(var.artifact_registry_prefix)}-${lower(var.artifact_registry_names[count.index])} repository"
   format        = "DOCKER"
 
-  labels = merge(local.artifact_registry_tags, {
-    name = "${lower(var.artifact_registry_prefix)}-${lower(var.artifact_registry_names[count.index])}"
-  })
-
+  labels = {
+    name   = "${lower(var.artifact_registry_prefix)}-${lower(var.artifact_registry_names[count.index])}"
+    owner  = var.artifact_registry_owner
+    module = var.artifact_registry_module
+    type   = var.artifact_registry_type
+  }
   docker_config {
     immutable_tags = var.immutable_artifact_registry
   }
@@ -19,7 +21,7 @@ resource "google_artifact_registry_repository" "artifact_registry" {
 resource "google_artifact_registry_repository_iam_member" "member_service_account" {
   count      = var.artifact_registry_count
   project    = var.gcp_project_id
-  location   = local.location
+  location   = var.artifact_registry_multi_region == true ? var.artifact_registry_primary_area : var.gcp_region
   repository = google_artifact_registry_repository.artifact_registry[count.index].name
   role       = "roles/artifactregistry.writer"
   member     = "serviceAccount:sa-kyma-project@sap-kyma-prow.iam.gserviceaccount.com"
