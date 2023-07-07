2a4b8bafe198fbab605c2dddba683796bb3db285
Tibor Kiss <32801432+the1bit@users.noreply.github.com>
2023-07-07 15:43:18 +0200

    Provide private image registry for cap-operator (#7977)
    * Artifact Registry (#4079)
    
    * Artifact Registry (#4079)
    
    * Apply improvements according to review comments
    
    * Variable name changes
    
    * Get rid of locals.tf file
    
    * Transform artifact-registry creation to module
    
    * Move to the modules directory
    
    * Refactor module
    
    * Update configs/terraform/environments/dev/artifact-registry/main.tf
    
    Co-authored-by: Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
    
    * Update configs/terraform/environments/dev/artifact-registry/terraform.tfvars
    
    Co-authored-by: Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
    
    * Update configs/terraform/environments/prod/artifact-registry.tf
    
    Co-authored-by: Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
    
    * Update configs/terraform/modules/artifact-registry/variables.tf
    
    Co-authored-by: Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
    
    * Update configs/terraform/modules/artifact-registry/main.tf
    
    Co-authored-by: Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
    
    * Update configs/terraform/modules/artifact-registry/main.tf
    
    Co-authored-by: Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
    
    * Remove tfstate.backup
    
    * Conditional artifact registry creation
    
    * Apply everything for dev usage
    
    * Clean output
    
    * Some default values
    
    * Update configs/terraform/modules/artifact-registry/README.md
    
    Co-authored-by: Małgorzata Świeca <malgorzata.swieca@sap.com>
    
    * Update configs/terraform/modules/artifact-registry/README.md
    
    Co-authored-by: Małgorzata Świeca <malgorzata.swieca@sap.com>
    
    * Update configs/terraform/modules/artifact-registry/README.md
    
    Co-authored-by: Małgorzata Świeca <malgorzata.swieca@sap.com>
    
    * Update configs/terraform/modules/artifact-registry/README.md
    
    Co-authored-by: Małgorzata Świeca <malgorzata.swieca@sap.com>
    
    * Update configs/terraform/modules/artifact-registry/README.md
    
    Co-authored-by: Małgorzata Świeca <malgorzata.swieca@sap.com>
    
    * Update configs/terraform/modules/artifact-registry/README.md
    
    Co-authored-by: Małgorzata Świeca <malgorzata.swieca@sap.com>
    
    * Apply changes by mmitoraj
    
    * Update document
    
    * Update configs/terraform/modules/artifact-registry/README.md
    
    Co-authored-by: Małgorzata Świeca <malgorzata.swieca@sap.com>
    
    * Update configs/terraform/modules/artifact-registry/README.md
    
    Co-authored-by: Małgorzata Świeca <malgorzata.swieca@sap.com>
    
    * Fix broken link
    
    * Doc
    
    * Link modification
    
    * Revert original
    
    ---------
    
    Co-authored-by: Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
    Co-authored-by: Małgorzata Świeca <malgorzata.swieca@sap.com>

---
+++ configs/terraform/environments/dev/artifact-registry/variables.tf
@@ -0,0 +1,56 @@
+
+###################################
+# Artifact Registry related values
+###################################
+variable "gcp_region" {
+  type        = string
+  description = "Default Google Cloud region to create resources."
+}
+
+variable "gcp_project_id" {
+  type        = string
+  description = "Google Cloud project to create resources."
+}
+
+variable "artifact_registry_names" {
+  type        = list(string)
+  description = "Artifact Registry names"
+  default     = ["ocim", "internal"]
+}
+variable "artifact_registry_owner" {
+  type        = string
+  description = "Owner inside SAP"
+}
+
+variable "artifact_registry_module" {
+  type        = string
+  description = "Module name"
+}
+
+variable "artifact_registry_type" {
+  type        = string
+  description = "Environment for the resources"
+}
+
+variable "artifact_registry_serviceaccount" {
+  type        = string
+  description = "Service Account"
+}
+
+variable "artifact_registry_multi_region" {
+  type        = bool
+  description = "Is Location type Multi-region?"
+  default     = true
+}
+
+variable "artifact_registry_primary_area" {
+  type        = string
+  description = "Location type Multi-region"
+  default     = "europe"
+}
+
+variable "immutable_artifact_registry" {
+  type        = bool
+  description = "Is Artifact registry immutable?"
+  default     = false
+}
