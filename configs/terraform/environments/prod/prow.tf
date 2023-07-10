e8f42d524f8153a9f17d515819266af9f4310ae9
Kamil Kasperski <kamil.kasperski@sap.com>
2023-07-10 15:21:05 +0200

    initial prow cluster as terraform resource

---
+++ configs/terraform/environments/prod/prow.tf
@@ -0,0 +1,38 @@
+resource "google_container_cluster" "trusted_workload" {
+  name                     = "trusted-workload-kyma-prow"
+  location                 = var.gcp_region
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  release_channel {
+    channel = "REGULAR"
+  }
+  enable_shielded_nodes = true
+  description           = "Prow control-plane cluster"
+  workload_identity_config {
+    workload_pool = "${var.gcp_project_id}.svc.id.goog"
+  }
+}
+
+resource "google_container_node_pool" "preemptible_standard_pool" {
+  name    = "standard-pool"
+  cluster = google_container_cluster.trusted_workload.id
+  autoscaling {
+    max_node_count  = 16
+    min_node_count  = 0
+    location_policy = "ANY"
+  }
+  node_config {
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
+    preemptible  = true
+    machine_type = "n2d-standard-8"
+    disk_size_gb = 200
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+    labels = {
+      workload = "prow-jobs"
+    }
+  }
+}
