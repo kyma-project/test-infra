b59ebb3a90c06edf4c17fbc68cdd27ffb9050aa4
Kamil Kasperski <kamil.kasperski@sap.com>
2023-07-12 18:00:07 +0200

    Move old Prow deployments to new kustomization-based approach
    /kind deprecation
    /area ci

--- prow/cluster/deploy.sh
+++ prow/cluster/deploy.sh
@@ -10,41 +10,7 @@ set -o pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 prow_components=(
-"test-pods_namespace.yaml"
-"crier_rbac.yaml"
-"crier_deployment.yaml"
-"crier_service.yaml"
-"deck_rbac.yaml"
-"deck_deployment.yaml"
-"deck_service.yaml"
-"gce-ssd-retain_storageclass.yaml"
-"ghproxy.yaml"
-"gcsweb-kyma-prow_managedcertificate.yaml"
-"gcsweb.yaml"
-"gcsweb_tls_ingress.yaml"
-"hook_rbac.yaml"
-"hook_deployment.yaml"
-"hook_service.yaml"
-"horologium_rbac.yaml"
-"horologium_deployment.yaml"
-"horologium_service.yaml"
-"halogen.yaml"
 "pjtester_prowjob-scheduler_rbac.yaml"
-"prow_controller_manager_rbac.yaml"
-"prow_controller_manager_deployment.yaml"
-"prow_controller_manager_service.yaml"
-"prowjob_customresourcedefinition.yaml"
-"pushgateway_deployment.yaml"
-"sinker_rbac.yaml"
-"sinker_deployment.yaml"
-"sinker_service.yaml"
-"statusreconciler_rbac.yaml"
-"statusreconciler_deployment.yaml"
-"status-kyma-prow_managedcertificate.yaml"
-"tide_rbac.yaml"
-"tide_deployment.yaml"
-"tide_service.yaml"
-"tls-ing_ingress.yaml"
 "web_server_deployment.yaml"
 )
 
@@ -74,6 +40,7 @@ fi
 ensure-context sap-kyma-prow europe-west3-a prow
 
 echo " Deploying Prow..."
+kubectl apply --server-side=true -k "configs/deployments/prow/overlays/dev"
 for c in "${prow_components[@]}"; do
   kubectl apply --server-side=true -f "$SCRIPT_DIR/components/$c"
 done
