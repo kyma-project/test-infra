26ed2b2c97774351aece63b5ed59b0709f8af505
marcobebway <marco.bebway@sap.com>
2023-07-14 13:02:28 +0200

    Merge scripts

--- prow/scripts/cluster-integration/kyma-integration-gardener-eventing.sh
+++ prow/scripts/cluster-integration/kyma-integration-gardener-eventing.sh
@@ -51,6 +51,10 @@ requiredVars=(
     GARDENER_KYMA_PROW_PROVIDER_SECRET_NAME
     CREDENTIALS_DIR
     EVENTMESH_SECRET_FILE
+    EVENTING_WEBHOOK_AUTH_ENABLED
+    TEST_EVENTING_AUTH_IAS_URL
+    TEST_EVENTING_AUTH_IAS_USER
+    TEST_EVENTING_AUTH_IAS_PASSWORD
 )
 
 utils::check_required_vars "${requiredVars[@]}"
@@ -72,6 +76,10 @@ else
     exit 1
 fi
 
+# needed variables to communicate with the CIS APIs
+location=""
+user=""
+
 function cleanupJobAssets() {
     # Must be at the beginning
     EXIT_STATUS=$?
@@ -79,6 +87,13 @@ function cleanupJobAssets() {
     set +e
 
     log::banner "Job Exit Status:: \"${EXIT_STATUS}\""
+
+    if [ "${EVENTING_WEBHOOK_AUTH_ENABLED}" == "true" ]; then
+        log::banner "Delete CIS application"
+
+        # delete application
+        curl -X DELETE "${TEST_EVENTING_AUTH_IAS_URL}${location}" --user "${user}"
+    fi
 
     if [[ $EXIT_STATUS != "0" ]]; then
         eventing::print_troubleshooting_logs
@@ -140,9 +155,164 @@ gardener::generate_overrides
 export CLEANUP_CLUSTER="true"
 gardener::provision_cluster
 
+## ---------------------------------------------------------------------------------------
+## Create the Eventing webhook auth secret
+## ---------------------------------------------------------------------------------------
+
+if [ "${EVENTING_WEBHOOK_AUTH_ENABLED}" == "true" ]; then
+    log::banner "Create secret kyma-system/eventing-webhook-auth"
+
+    user="${TEST_EVENTING_AUTH_IAS_USER}:${TEST_EVENTING_AUTH_IAS_PASSWORD}"
+
+    # generate uuid
+    uuid=$(cat /proc/sys/kernel/random/uuid)
+
+    # create application
+    location=$(curl "${TEST_EVENTING_AUTH_IAS_URL}/Applications/v1/" \
+    --silent \
+    --include \
+    --header 'Content-Type: application/json' \
+    --user "${user}" \
+    --data '{
+      "id": "'"${uuid}"'",
+      "name": "'"${uuid}"'",
+      "multiTenantApp": false,
+      "schemas": [
+        "urn:sap:identity:application:schemas:core:1.0",
+        "urn:sap:identity:application:schemas:extension:sci:1.0:Authentication"
+      ],
+      "branding" : {
+        "displayName": "'"${KYMA_SOURCE}"' - Kyma integration gardener Eventing"
+      },
+      "urn:sap:identity:application:schemas:extension:sci:1.0:Authentication": {
+        "ssoType": "openIdConnect",
+        "subjectNameIdentifier": "uid",
+        "rememberMeExpirationTimeInMonths": 3,
+        "passwordPolicy": "https://accounts.sap.com/policy/passwords/sap/web/1.1",
+        "userAccess": {
+          "type": "internal",
+          "userAttributesForAccess": [
+            {
+              "userAttributeName": "firstName",
+              "isRequired": false
+            },
+            {
+              "userAttributeName": "lastName",
+              "isRequired": true
+            },
+            {
+              "userAttributeName": "mail",
+              "isRequired": true
+            }
+          ]
+        },
+        "assertionAttributes": [
+          {
+            "assertionAttributeName": "first_name",
+            "userAttributeName": "firstName"
+          },
+          {
+            "assertionAttributeName": "last_name",
+            "userAttributeName": "lastName"
+          },
+          {
+            "assertionAttributeName": "mail",
+            "userAttributeName": "mail"
+          },
+          {
+            "assertionAttributeName": "user_uuid",
+            "userAttributeName": "userUuid"
+          },
+          {
+            "assertionAttributeName": "locale",
+            "userAttributeName": "language"
+          }
+        ],
+        "spnegoEnabled": false,
+        "biometricAuthenticationEnabled": false,
+        "verifyMail": true,
+        "forceAuthentication": false,
+        "trustAllCorporateIdentityProviders": false,
+        "allowIasUsers": false,
+        "riskBasedAuthentication": {
+          "defaultAction": [
+            "allow"
+          ]
+        },
+        "saml2Configuration": {
+          "defaultNameIdFormat": "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified",
+          "signSLOMessages": true,
+          "requireSignedSLOMessages": true,
+          "requireSignedAuthnRequest": false,
+          "signAssertions": true,
+          "signAuthnResponses": false,
+          "responseElementsToEncrypt": "none",
+          "digestAlgorithm": "sha256",
+          "proxyAuthnRequest": {
+            "authenticationContext": "none"
+          }
+        },
+        "openIdConnectConfiguration": {}
+      }
+    }
+    ' | grep -i ^Location: | cut -d: -f2- | sed 's/^ *\(.*\).*/\1/' | tr -d '[:space:]')
+
+    exit_status=$?
+    if [ ${exit_status} -ne 0 ]; then
+      log::error "Error occurred while creating the application"
+      exit 1
+    fi
+
+    # get client id
+    client_id=$(curl --silent "${TEST_EVENTING_AUTH_IAS_URL}${location}" \
+    --user "${user}" | jq -r '.["urn:sap:identity:application:schemas:extension:sci:1.0:Authentication"].clientId')
+
+    # generate client secret
+    client_secret=$(curl --silent "${TEST_EVENTING_AUTH_IAS_URL}${location}/apiSecrets" \
+    --header 'Content-Type: application/json' \
+    --user "${user}" \
+    --data '{
+      "authorizationScopes": [
+        "oAuth",
+        "manageApp",
+        "manageUsers"
+      ],
+      "validTo": "2030-01-01T10:00:00Z"
+    }' | jq -r '.secret')
+
+    # generate token and certs url
+    token_url="${TEST_EVENTING_AUTH_IAS_URL}/oauth2/token"
+    certs_url="${TEST_EVENTING_AUTH_IAS_URL}/oauth2/certs"
+
+    # create eventing webhook auth secret
+    kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: kyma-system
+---
+apiVersion: v1
+kind: Secret
+type: Opaque
+metadata:
+  namespace: kyma-system
+  name: eventing-webhook-auth
+stringData:
+  certs_url: "${certs_url}"
+  token_url: "${token_url}"
+  client_id: "${client_id}"
+  client_secret: "${client_secret}"
+---
+EOF
+fi
+
+## ---------------------------------------------------------------------------------------
+## Deploy & Run Kyma tests
+## ---------------------------------------------------------------------------------------
+
 # deploy Kyma
-log::info "Deploying Kyma ${KYMA_SOURCE}"
-gardener::deploy_kyma -p "$EXECUTION_PROFILE" --source "${KYMA_SOURCE}"
+log::info "Deploying Kyma ${KYMA_SOURCE} with eventingWebhookAuth.enabled=${EVENTING_WEBHOOK_AUTH_ENABLED}"
+gardener::deploy_kyma -p "$EXECUTION_PROFILE" --source "${KYMA_SOURCE}" --value eventing.controller.eventingWebhookAuth.enabled="${EVENTING_WEBHOOK_AUTH_ENABLED}"
 
 # generate pod-security-policy list in json
 utils::save_psp_list "${ARTIFACTS}/kyma-psp.json"
