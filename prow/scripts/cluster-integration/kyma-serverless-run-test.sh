6907674bb8bbbafe342a88724ef6ffa9499c9cdb
Michał Kalke <56382792+MichalKalke@users.noreply.github.com>
2023-07-19 14:10:32 +0200

    Adjust scripts to work with new tracing test (#8209)
    * adjust scritps to work with new tracing test
    
    * add pjtester
    
    * restore test-inv.md
    
    * pass args as array
    
    * rm pjtester, adjust scripts

--- prow/scripts/cluster-integration/kyma-serverless-run-test.sh
+++ prow/scripts/cluster-integration/kyma-serverless-run-test.sh
@@ -4,8 +4,7 @@ set -o pipefail # Fail a pipe if any sub-command fails.
 
 run_tests() {
   export SERVERLESS_OVERRIDES_DIR="./overrides"
-  export INTEGRATION_SUITE=${1:-serverless-integration}
-  export KYMA_SOURCES_DIR=${2:-./kyma}
+  export INTEGRATION_SUITE=("$@")
 
   date
 
@@ -19,6 +18,6 @@ run_tests() {
   export PATH=${PATH}:/usr/local/go/bin
 
   export APP_TEST_CLEANUP="onSuccessOnly"
-  (cd "${KYMA_SOURCES_DIR}/tests/function-controller" && make "${INTEGRATION_SUITE}")
+  (cd "${KYMA_SOURCES_DIR}/tests/function-controller" && make "${INTEGRATION_SUITE[@]}")
   return $?
 }
