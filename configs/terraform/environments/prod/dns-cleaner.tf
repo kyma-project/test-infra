649f04b0f4adfbd4c6a14e5a67a32b45b4152115
Wojciech Soltys <wojciech.soltys@sap.com>
2023-07-20 10:04:43 +0200

    Change resources names

--- configs/terraform/environments/prod/dns-cleaner.tf
+++ configs/terraform/environments/prod/dns-cleaner.tf
@@ -1,22 +1,22 @@
-resource "google_service_account" "sa-gke-kyma-integration" {
-  account_id   = "sa-gke-kyma-integration@sap-kyma-prow.iam.gserviceaccount.com"
-  display_name = "sa-gke-kyma-integration"
+resource "google_service_account" "sa_gke_kyma_integration" {
+  account_id   = "sa_gke_kyma_integration@sap-kyma-prow.iam.gserviceaccount.com"
+  display_name = "sa_gke_kyma_integration"
 }
 
-resource "google_project_iam_binding" "view-container-analysis-ccurrences" {
+resource "google_project_iam_binding" "view_container_analysis_ccurrences" {
   project = var.gcp_project_id
   role    = "roles/containeranalysis.occurrences.viewer"
-  members = ["serviceAccount:${google_service_account.sa-gke-kyma-integration.email}"]
+  members = ["serviceAccount:${google_service_account.sa_gke_kyma_integration.email}"]
 }
 
-resource "google_project_iam_binding" "dns-reader" {
+resource "google_project_iam_binding" "dns_reader" {
   project = var.gcp_project_id
   role    = "roles/dns.reader"
-  members = ["serviceAccount:${google_service_account.sa-gke-kyma-integration.email}"]
+  members = ["serviceAccount:${google_service_account.sa_gke_kyma_integration.email}"]
 }
 
-resource "google_project_iam_binding" "bucket-get" {
-  project = "sap-kyma-prow"
+resource "google_project_iam_binding" "bucket_get" {
+  project = var.gcp_project_id
   role    = "projects/sap-kyma-prow/roles/BucketGet"
-  members = ["serviceAccount:${google_service_account.sa-gke-kyma-integration.email}"]
+  members = ["serviceAccount:${google_service_account.sa_gke_kyma_integration.email}"]
 }
