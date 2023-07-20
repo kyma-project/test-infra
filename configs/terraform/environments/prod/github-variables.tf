9b153930df639faa83ec0b8708f53beff1c70b48
dekiel <przemyslaw.pokrywka@sap.com>
2023-07-20 23:05:15 +0200

    Install gcp workload identity federation setup for github workload. Setup github action variables for gcp auth github action.

---
+++ configs/terraform/environments/prod/github-variables.tf
@@ -0,0 +1,10 @@
+variable "kyma-project-github-org" {
+  type        = string
+  default     = "kyma-project"
+  description = "The GitHub organization where the Kyma project is hosted"
+}
+variable "kyma-incubator-github-org" {
+  type        = string
+  default     = "kyma-incubator"
+  description = "The GitHub organization where the Kyma Incubator is hosted"
+}
