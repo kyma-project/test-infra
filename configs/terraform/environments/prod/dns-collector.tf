75e006cfa6f088f0864e6c09f6047f86651202c2
Wojciech Soltys <wojciech.soltys@sap.com>
2023-07-21 10:11:09 +0200

    Improve IAM bindings names

--- configs/terraform/environments/prod/dns-collector.tf
+++ configs/terraform/environments/prod/dns-collector.tf
@@ -6,19 +6,19 @@ resource "google_service_account" "sa_gke_kyma_integration" {
   description  = "Service account is used by Prow to integrate with GKE."
 }
 
-resource "google_project_iam_binding" "dns_cleaner_view_container_analysis_occurrences" {
+resource "google_project_iam_binding" "dns_collector_container_analysis_occurrences_viewer" {
   project = var.gcp_project_id
   role    = "roles/containeranalysis.occurrences.viewer"
   members = ["serviceAccount:${google_service_account.sa_gke_kyma_integration.email}"]
 }
 
-resource "google_project_iam_binding" "dns_reader" {
+resource "google_project_iam_binding" "dns_collector_dns_reader" {
   project = var.gcp_project_id
   role    = "roles/dns.reader"
   members = ["serviceAccount:${google_service_account.sa_gke_kyma_integration.email}"]
 }
 
-resource "google_project_iam_binding" "bucket_get" {
+resource "google_project_iam_binding" "dns_collector_bucket_get" {
   project = var.gcp_project_id
   role    = "projects/sap-kyma-prow/roles/BucketGet"
   members = ["serviceAccount:${google_service_account.sa_gke_kyma_integration.email}"]
