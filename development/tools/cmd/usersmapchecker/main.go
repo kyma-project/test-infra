ef0455b9fae47094c322de7da62f0e1a28bf1178
Patryk Dobrowolski <akiioto@gmail.com>
2023-07-28 13:50:48 +0200

    goimports

--- development/tools/cmd/usersmapchecker/main.go
+++ development/tools/cmd/usersmapchecker/main.go
@@ -6,9 +6,9 @@ import (
 	"os"
 	"sync"
 	"sync/atomic"
-	
+
 	log "github.com/sirupsen/logrus"
-	
+
 	gcplogging "github.com/kyma-project/test-infra/development/gcp/pkg/logging"
 	"github.com/kyma-project/test-infra/development/github/pkg/client"
 	"github.com/kyma-project/test-infra/development/prow"
@@ -17,48 +17,48 @@ import (
 // Example fields in gcp logging.
 // logName: "projects/sap-kyma-prow/logs/stdout"
 //
-//      resource: {
-//        labels: {
-//          cluster_name: "trusted-workload-kyma-prow"
-//          container_name: "test"
-//          location: "europe-west3"
-//          namespace_name: "default"
-//          pod_name: "cbb59657-fa91-11eb-baea-4e9acc7ce5e6"
-//          project_id: "sap-kyma-prow"
-//        }
-//        type: "k8s_container"
+//	resource: {
+//	  labels: {
+//	    cluster_name: "trusted-workload-kyma-prow"
+//	    container_name: "test"
+//	    location: "europe-west3"
+//	    namespace_name: "default"
+//	    pod_name: "cbb59657-fa91-11eb-baea-4e9acc7ce5e6"
+//	    project_id: "sap-kyma-prow"
+//	  }
+//	  type: "k8s_container"
 //
-//      labels: {
-//        compute.googleapis.com/resource_name: "gke-trusted-workload-k-high-cpu-16-32-c8294afe-skrq"
-//        k8s-pod/created-by-prow: "true"
-//        k8s-pod/event-GUID: "cb549a8a-fa91-11eb-80a9-35f1ac609512"
-//        k8s-pod/preset-build-main: "true"
-//        k8s-pod/preset-cluster-use-ssd: "true"
-//        k8s-pod/preset-cluster-version: "true"
-//        k8s-pod/preset-debug-commando-oom: "true"
-//        k8s-pod/preset-dind-enabled: "true"
-//        k8s-pod/preset-docker-push-repository-gke-integration: "true"
-//        k8s-pod/preset-gc-compute-envs: "true"
-//        k8s-pod/preset-gc-project-env: "true"
-//        k8s-pod/preset-gke-upgrade-post-job: "true"
-//        k8s-pod/preset-kyma-artifacts-bucket: "true"
-//        k8s-pod/preset-kyma-guard-bot-github-token: "true"
-//        k8s-pod/preset-log-collector-slack-token: "true"
-//        k8s-pod/preset-sa-gke-kyma-integration: "true"
-//        k8s-pod/preset-sa-test-gcr-push: "true"
-//        k8s-pod/prow_k8s_io/build-id: "1425409012446269440"
-//        k8s-pod/prow_k8s_io/context: "post-main-kyma-gke-upgrade"
-//        k8s-pod/prow_k8s_io/id: "cbb59657-fa91-11eb-baea-4e9acc7ce5e6"
-//        k8s-pod/prow_k8s_io/job: "post-main-kyma-gke-upgrade"
-//        k8s-pod/prow_k8s_io/plank-version: "v20210714-62f15287bd"
-//        k8s-pod/prow_k8s_io/pubsub_project: "sap-kyma-prow"
-//        k8s-pod/prow_k8s_io/pubsub_runID: "post-main-kyma-gke-upgrade"
-//        k8s-pod/prow_k8s_io/pubsub_topic: "prowjobs"
-//        k8s-pod/prow_k8s_io/refs_base_ref: "main"
-//        k8s-pod/prow_k8s_io/refs_org: "kyma-project"
-//        k8s-pod/prow_k8s_io/refs_repo: "kyma"
-//        k8s-pod/prow_k8s_io/type: "postsubmit"
-//      }
+//	labels: {
+//	  compute.googleapis.com/resource_name: "gke-trusted-workload-k-high-cpu-16-32-c8294afe-skrq"
+//	  k8s-pod/created-by-prow: "true"
+//	  k8s-pod/event-GUID: "cb549a8a-fa91-11eb-80a9-35f1ac609512"
+//	  k8s-pod/preset-build-main: "true"
+//	  k8s-pod/preset-cluster-use-ssd: "true"
+//	  k8s-pod/preset-cluster-version: "true"
+//	  k8s-pod/preset-debug-commando-oom: "true"
+//	  k8s-pod/preset-dind-enabled: "true"
+//	  k8s-pod/preset-docker-push-repository-gke-integration: "true"
+//	  k8s-pod/preset-gc-compute-envs: "true"
+//	  k8s-pod/preset-gc-project-env: "true"
+//	  k8s-pod/preset-gke-upgrade-post-job: "true"
+//	  k8s-pod/preset-kyma-artifacts-bucket: "true"
+//	  k8s-pod/preset-kyma-guard-bot-github-token: "true"
+//	  k8s-pod/preset-log-collector-slack-token: "true"
+//	  k8s-pod/preset-sa-gke-kyma-integration: "true"
+//	  k8s-pod/preset-sa-test-gcr-push: "true"
+//	  k8s-pod/prow_k8s_io/build-id: "1425409012446269440"
+//	  k8s-pod/prow_k8s_io/context: "post-main-kyma-gke-upgrade"
+//	  k8s-pod/prow_k8s_io/id: "cbb59657-fa91-11eb-baea-4e9acc7ce5e6"
+//	  k8s-pod/prow_k8s_io/job: "post-main-kyma-gke-upgrade"
+//	  k8s-pod/prow_k8s_io/plank-version: "v20210714-62f15287bd"
+//	  k8s-pod/prow_k8s_io/pubsub_project: "sap-kyma-prow"
+//	  k8s-pod/prow_k8s_io/pubsub_runID: "post-main-kyma-gke-upgrade"
+//	  k8s-pod/prow_k8s_io/pubsub_topic: "prowjobs"
+//	  k8s-pod/prow_k8s_io/refs_base_ref: "main"
+//	  k8s-pod/prow_k8s_io/refs_org: "kyma-project"
+//	  k8s-pod/prow_k8s_io/refs_repo: "kyma"
+//	  k8s-pod/prow_k8s_io/type: "postsubmit"
+//	}
 func main() {
 	// exitCode holds exit code to report at the end of main execution, it's safe to set it from multiple goroutines.
 	var exitCode atomic.Value
@@ -78,12 +78,12 @@ func main() {
 	logger := logClient.NewProwjobLogger().WithGeneratedTrace()
 	// Flush all buffered messages when exiting from main function.
 	defer logger.Flush()
-	
+
 	// Create a stdout logger
 	stdoutLogger := log.WithField("context", "checking if user exists in users map")
-	
+
 	contextLogger := logger.WithContext("checking if user exists in users map")
-	
+
 	defer contextLogger.Flush()
 	// Github access token, provided by preset-bot-github-sap-token
 	accessToken := os.Getenv("BOT_GITHUB_SAP_TOKEN")
@@ -94,7 +94,7 @@ func main() {
 		contextLogger.LogError(fmt.Sprintf("failed creating sap tools github client, got error: %v", err))
 		stdoutLogger.Error(fmt.Sprintf("failed creating sap tools github client, got error: %v", err))
 	}
-	
+
 	githubComClient, err := client.NewClient(ctx, githubComAccessToken)
 	if err != nil {
 		contextLogger.LogError(fmt.Sprintf("failed creating github.com client, got error: %v", err))
@@ -117,7 +117,7 @@ func main() {
 			stdoutLogger.Error(fmt.Sprintf("error when getting pr author for presubmit: got error %v", err))
 		}
 	}
-	
+
 	org, err := prow.GetOrgForPresubmit()
 	if err != nil {
 		if notPresubmit := prow.IsNotPresubmitError(err); *notPresubmit {
