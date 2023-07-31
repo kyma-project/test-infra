6ac113550cbe03ff75f858d80d51673aefdbfa0b
Kacper Małachowski <38684517+KacperMalachowski@users.noreply.github.com>
2023-07-31 11:01:46 +0200

    Update images used by terraform (#8453)

--- configs/terraform/secrets-leaks-log-scanner/slack-message-sender.tf
+++ configs/terraform/secrets-leaks-log-scanner/slack-message-sender.tf
@@ -24,7 +24,7 @@ resource "google_cloud_run_service" "slack_message_sender" {
     spec {
       service_account_name = google_service_account.slack_message_sender.email
       containers {
-        image = "europe-docker.pkg.dev/kyma-project/prod/test-infra/slackmessagesender:v20230309-1d421c4f"
+        image = "europe-docker.pkg.dev/kyma-project/prod/test-infra/slackmessagesender:v20230725-f46dea4c"
         env {
           name  = "PROJECT_ID"
           value = var.gcp_project_id
