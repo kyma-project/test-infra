32c474b5cc84946f63312fb304e69d2decc50c4f
dekiel <przemyslaw.pokrywka@sap.com>
2023-08-02 11:56:31 +0200

    4

--- configs/terraform/environments/prod/dns.tf
+++ configs/terraform/environments/prod/dns.tf
@@ -1,6 +1,7 @@
 # first comment
 #second comment
 # third comment
+#fourth comment
 resource "google_dns_managed_zone" "build_kyma" {
   dns_name = "build.kyma-project.io."
   name     = "build-kyma"
