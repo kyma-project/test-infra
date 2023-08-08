e861ab38e7c24e16ed65b650dac7c2197f85f022
Piotr Halama <piotr.halama@sap.com>
2023-08-08 15:33:02 +0200

    Bump slack-message-sender image

--- configs/terraform/modules/github-webhook-gateway/github-webhook-gateway.tf
+++ configs/terraform/modules/github-webhook-gateway/github-webhook-gateway.tf
@@ -33,7 +33,7 @@ resource "google_cloud_run_service" "github_webhook_gateway" {
     spec {
       service_account_name = google_service_account.github_webhook_gateway.email
       containers {
-        image = "europe-docker.pkg.dev/kyma-project/prod/test-infra/ko/github-webhook-gateway:v20230808-b281686e"
+        image = "europe-docker.pkg.dev/kyma-project/prod/test-infra/ko/github-webhook-gateway:v20230808-b797f009"
         env {
           name  = "PROJECT_ID"
           value = var.gcp_project_id
