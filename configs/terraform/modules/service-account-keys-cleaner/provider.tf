5000702156760e1ae383894942e19cc29f93181c
Kacper Małachowski <38684517+KacperMalachowski@users.noreply.github.com>
2023-08-21 07:24:49 +0200

    Merge terraform configs (#8706)
    * Move rotate-service-account and service-account-keys-cleaner
    
    * Add secrets rotator to prod root module
    
    * Move dev env to root module

--- configs/terraform/modules/service-account-keys-cleaner/provider.tf
+++ configs/terraform/modules/service-account-keys-cleaner/provider.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.55.0"
-    }
-
-    google-beta = {
-      source = "hashicorp/google-beta"
+      version = ">=4.55.0"
     }
   }
 }
