128bc62fd6ccb2fa14cb31b104b4c2812abb06b6
Piotr Halama <piotr.halama@sap.com>
2023-09-05 12:33:09 +0200

    Bump CORS-proxy image

--- configs/terraform/modules/cors-proxy/cors-proxy.tf
+++ configs/terraform/modules/cors-proxy/cors-proxy.tf
@@ -28,7 +28,7 @@ resource "google_cloud_run_service" "cors_proxy" {
   template {
     spec {
       containers {
-        image = "europe-docker.pkg.dev/kyma-project/prod/test-infra/ko/cors-proxy:v20230818-828f7c97"
+        image = "europe-docker.pkg.dev/kyma-project/prod/test-infra/ko/cors-proxy:v20230905-210cd7c5"
         env {
           name  = "COMPONENT_NAME"
           value = "cors-proxy"
