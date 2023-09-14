8e3dbf17817c92dffaada9c162f87ed362c95f13
Piotr Halama <piotr.halama@sap.com>
2023-09-14 10:39:08 +0200

    Bump cloud run images (#8918)

--- configs/terraform/modules/secrets-leaks-log-scanner/secrets-leak-log-scanner.tf
+++ configs/terraform/modules/secrets-leaks-log-scanner/secrets-leak-log-scanner.tf
@@ -23,7 +23,7 @@ resource "google_cloud_run_service" "secrets_leak_log_scanner" {
     spec {
       service_account_name = google_service_account.secrets_leak_log_scanner.email
       containers {
-        image = "europe-docker.pkg.dev/kyma-project/prod/test-infra/ko/scan-logs-for-secrets:v20230802-a960e47d"
+        image = "europe-docker.pkg.dev/kyma-project/prod/test-infra/ko/scan-logs-for-secrets:v20230914-c7902318"
         env {
           name  = "PROJECT_ID"
           value = var.gcp_project_id
