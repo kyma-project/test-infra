a90e219cb864c3b2d2cf4aa84eb42b9bee56f38c
Michał Kalke <56382792+MichalKalke@users.noreply.github.com>
2023-10-03 15:49:05 +0200

    Move serverless jobs to serverless manager file (#8961)
    * Move serverless post/pull to serverless manager file
    
    * Generate jobs templates
    
    * Change path to dockerfile for python and gitserver
    
    * Bump pjtester and adjust gitserver dockerfile path
    
    * Adjust jobConfig for post python39
    
    * Undo typo
    
    * Fix python39 dockerfile path
    
    * Add remainig tests for lint, unit tests, integration k3s
    
    * Adjust pjtester to newly added tests
    
    * Change path to serverless-manager
    
    * Change path to serverless-manager/config/serverless
    
    * Adjust script to work with new path to serverless
    
    * Add new script handling new make targets in serverless-manager
    
    * Restore kyma-serverless-integration-k3d.sh
    
    * Change auth-integration-k3d for serverless-manager to run serverless-integration-k3d-operator
    
    * Add double quotes
    
    * Delete pjtester
    
    * remove unneded code
    
    * test pjtester
    
    * remove pjtester
    
    * Make jobs
    
    ---------
    
    Co-authored-by: Damian Badura <damian.badura@sap.com>

---
+++ prow/scripts/cluster-integration/kyma-serverless-integration-k3d-operator.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail # Fail a pipe if any sub-command fails.
+
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# shellcheck source=prow/scripts/cluster-integration/kyma-serverless-run-test.sh
+source "${SCRIPT_DIR}/kyma-serverless-run-test.sh"
+# shellcheck source=prow/scripts/lib/serverless-shared-k3s.sh
+source "${SCRIPT_DIR}/../lib/serverless-shared-k3s.sh"
+
+date
+
+make -C "$SERVERLESS_SOURCES"/hack/ci run-without-lifecycle-manager-operator
+
+export INTEGRATION_SUITE=("$@")
+KYMA_SOURCES_DIR="${SERVERLESS_SOURCES}" run_tests "${INTEGRATION_SUITE[@]}"
+TEST_STATUS=$?
+set -o errexit
+
+collect_results
+echo "Exit code ${TEST_STATUS}"
+
+exit ${TEST_STATUS}
