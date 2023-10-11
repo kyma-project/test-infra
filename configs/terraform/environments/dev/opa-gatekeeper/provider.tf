c7e96dd0c751ffc547fedd5598680ffdbc5decd6
Piotr Halama <piotr.halama@sap.com>
2023-10-11 10:31:35 +0200

    Use supported version of tf kubectl plugin

--- configs/terraform/environments/dev/opa-gatekeeper/provider.tf
+++ configs/terraform/environments/dev/opa-gatekeeper/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
+      source  = "alekc/kubectl"
+      version = ">= 2.0.0"
     }
     google = {
       source  = "hashicorp/google"
