a52d5232dcbf81e9279a5fbd5f8c29ba268bdea9
Benjamin Lindner <50365642+lindnerby@users.noreply.github.com>
2023-10-12 11:40:57 +0200

    Fix reconciler e2e pipelines after serverless removal (#9086)
    * add pjtester
    
    * Add debug echo
    
    * Add jobs
    
    * temp add serverless operator
    
    * ok..
    
    * add namespace
    
    * temp apply serverless
    
    * test lastest release pipeline as well
    
    * delete unused component templates
    
    * Remove from pjtester
    
    * reference the new template for 2.19
    
    * add pjtester
    
    * remove pjtester
    
    ---------
    
    Co-authored-by: Badr, Nesma <nesma.badr@sap.com>

--- prow/scripts/reconciler/reconciler-upgrade-kyma2-latest-to-main-gardener.sh
+++ prow/scripts/reconciler/reconciler-upgrade-kyma2-latest-to-main-gardener.sh
@@ -122,6 +122,10 @@ reconciler::trigger_kyma_reconcile
 # Wait until reconciliation is complete
 reconciler::wait_until_kyma_reconciled
 
+if [[ $KYMA_UPGRADE_SOURCE != "main" ]]; then
+  kubectl apply -f https://github.com/kyma-project/serverless-manager/releases/latest/download/serverless-operator.yaml
+  kubectl apply -f https://github.com/kyma-project/serverless-manager/releases/latest/download/default_serverless_cr.yaml -n kyma-system
+fi
 make -C "../../kyma-project/kyma/tests/fast-integration" ci-pre-upgrade
 
 ## ---------------------------------------------------------------------------------------
