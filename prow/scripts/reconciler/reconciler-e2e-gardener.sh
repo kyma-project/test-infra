45b9fa3e27131497c4f21643a6722b3012836ab1
Benjamin Lindner <50365642+lindnerby@users.noreply.github.com>
2023-10-13 13:09:06 +0200

    Remove fast-integration from reconciler e2e pipelines (#9105)
    * Remove fast-integration ci execution from reconciler e2e pipelines
    
    * Remove fast-integration ci execution from reconciler nightly e2e

--- prow/scripts/reconciler/reconciler-e2e-gardener.sh
+++ prow/scripts/reconciler/reconciler-e2e-gardener.sh
@@ -23,8 +23,6 @@
 set -e
 pwd
 
-ENABLE_TEST_LOG_COLLECTOR=false
-
 # Exported variables
 # KYMA_SOURCE set to dummy value, required by gardener/gcp.sh
 export KYMA_SOURCE="main"
@@ -104,14 +102,6 @@ reconciler::trigger_kyma_reconcile
 
 # Wait until reconciliation is complete
 reconciler::wait_until_kyma_reconciled
-
-### Once Kyma is installed run the fast integration test
-echo "Executing test"
-if [[ $KYMA_TEST_SOURCE == "latest-release" ]]; then
-  kubectl apply -f https://github.com/kyma-project/serverless-manager/releases/latest/download/serverless-operator.yaml
-  kubectl apply -f https://github.com/kyma-project/serverless-manager/releases/latest/download/default_serverless_cr.yaml -n kyma-system
-fi
-make -C ../../kyma-project/kyma/tests/fast-integration ci
 
 #!!! Must be at the end of the script !!!
 ERROR_LOGGING_GUARD="false"
