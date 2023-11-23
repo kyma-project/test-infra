77d242a0a126e920885e70cd18947e04e6e31b4d
dekiel <przemyslaw.pokrywka@sap.com>
2023-11-23 17:49:34 +0100

    Move build in ado code to separate package. This can be tested independent and reused in other tools.

--- pkg/azuredevops/pipelines/templateParams.go
+++ pkg/azuredevops/pipelines/templateParams.go
@@ -6,39 +6,36 @@ import (
 
 type OCIImageBuilderTemplateParams map[string]string
 
-func (p OCIImageBuilderTemplateParams) SetRepoName(repoName string) {
-	p["RepoName"] = repoName
+func (p OCIImageBuilderTemplateParams) SetRepoName(repo string) {
+	p["RepoName"] = repo
 }
-func (p OCIImageBuilderTemplateParams) SetRepoOwner(repoOwner string) {
-	p["RepoOwner"] = repoOwner
+func (p OCIImageBuilderTemplateParams) SetRepoOwner(owner string) {
+	p["RepoOwner"] = owner
 }
 func (p OCIImageBuilderTemplateParams) SetJobType(jobType string) {
 	p["JobType"] = jobType
 }
-func (p OCIImageBuilderTemplateParams) SetPullNumber(pullNumber string) {
-	p["PullNumber"] = pullNumber
+func (p OCIImageBuilderTemplateParams) SetPullNumber(number string) {
+	p["PullNumber"] = number
 }
-func (p OCIImageBuilderTemplateParams) SetPullBaseSHA(pullBaseSHA string) {
-	p["PullBaseSHA"] = pullBaseSHA
+func (p OCIImageBuilderTemplateParams) SetPullBaseSHA(sha string) {
+	p["PullBaseSHA"] = sha
 }
 func (p OCIImageBuilderTemplateParams) SetImageName(name string) {
 	p["ImageName"] = name
 }
-func (p OCIImageBuilderTemplateParams) SetDockerfilePath(dockerfile string) {
-	p["DockerfilePath"] = dockerfile
+func (p OCIImageBuilderTemplateParams) SetDockerfilePath(path string) {
+	p["DockerfilePath"] = path
 }
 func (p OCIImageBuilderTemplateParams) SetBuildContext(context string) {
 	p["BuildContext"] = context
 }
-func (p OCIImageBuilderTemplateParams) SetExportTags(exportTags bool) {
-	p["ExportTags"] = strconv.FormatBool(exportTags)
+func (p OCIImageBuilderTemplateParams) SetExportTags(export bool) {
+	p["ExportTags"] = strconv.FormatBool(export)
 }
-func (p OCIImageBuilderTemplateParams) SetPlatforms(platforms string) {
-	p["Platforms"] = platforms
+func (p OCIImageBuilderTemplateParams) SetBuildArgs(args string) {
+	p["BuildArgs"] = args
 }
-func (p OCIImageBuilderTemplateParams) SetBuildArgs(buildArgs string) {
-	p["BuildArgs"] = buildArgs
-}
-func (p OCIImageBuilderTemplateParams) SetImageTags(imageTags string) {
-	p["ImageTags"] = imageTags
+func (p OCIImageBuilderTemplateParams) SetImageTags(tags string) {
+	p["ImageTags"] = tags
 }
