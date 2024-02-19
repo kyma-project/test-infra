25cc0df72111588e76cff564c96ccaebabf4d2ce
Kacper Małachowski <38684517+KacperMalachowski@users.noreply.github.com>
2024-02-19 17:58:35 +0100

    Workload Identity Fedration Tofu config fix (#9841)
    * Add custom module for worload identity federation
    
    * Add output with provider name to wif module
    
    * Add configuration for workload identity federation
    
    * Link opentofu registry issue
    
    * Use google workload identiyy federation module instead of custom one
    
    * Remove gcp-workload-identity-federation module.

--- configs/terraform/environments/prod/provider.tf
+++ configs/terraform/environments/prod/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.76.0"
+      version = ">= 5.6.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
