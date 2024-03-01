944e04bc59cbb005459240679f5cab72f5078bb8
Kacper Małachowski <38684517+KacperMalachowski@users.noreply.github.com>
2024-03-01 12:00:05 +0100

    Update configs/terraform/environments/prod/github-com.tf
    Co-authored-by: Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>

--- configs/terraform/environments/prod/github-com.tf
+++ configs/terraform/environments/prod/github-com.tf
@@ -1,3 +1,8 @@
+# TODO:
+# These actions variables must exist in github in order to let terraform executor run.
+# They are required for getting terraform executor github personal access token from gcp secret manager.
+# We must develop a solution for creating a minimal initial setup to let terraform executor apply our whole config.
+
 # GCP project id that contains secret manager with secrets
 resource "github_actions_variable" "gcp_kyma_project_project_id" {
   provider      = github.kyma_project
