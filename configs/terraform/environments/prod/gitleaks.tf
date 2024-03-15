97054e0979c69652b33ddeece66d2b22f78db2b2
Kacper Małachowski <38684517+KacperMalachowski@users.noreply.github.com>
2024-03-15 08:13:48 +0100

    Use proper principal binding for gitleaks (#10131)

--- configs/terraform/environments/prod/gitleaks.tf
+++ configs/terraform/environments/prod/gitleaks.tf
@@ -24,7 +24,7 @@ data "github_repository" "gitleaks_repository" {
 resource "google_service_account_iam_binding" "gitleaks_workload_identity_federation_binding" {
   for_each = data.github_repository.gitleaks_repository
   members = [
-    "principal://iam.googleapis.com/projects/${data.google_client_config.gcp.project}/locations/global/workloadIdentityPools/${module.gh_com_kyma_project_workload_identity_federation.pool_name}/subject/repository_id:${each.value.repo_id}:repository_owner_id:${data.github_organization.kyma-project.id}:workflow:${var.gitleaks_workflow_name}"
+    "principal://iam.googleapis.com/${module.gh_com_kyma_project_workload_identity_federation.pool_name}/subject/repository_id:${each.value.repo_id}:repository_owner_id:${data.github_organization.kyma-project.id}:workflow:${var.gitleaks_workflow_name}"
   ]
   role               = "roles/iam.workloadIdentityUser"
   service_account_id = google_service_account.gitleaks_secret_accesor.name
