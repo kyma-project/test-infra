5fc66cebc8bae539e7f3b705a5634093319b198f
Wojciech Sołtys <74361703+Sawthis@users.noreply.github.com>
2024-04-05 08:33:46 +0200

    Refactor of get OIDC token action (#10313)

--- .github/actions/expose-jwt-action/index.js
+++ .github/actions/expose-jwt-action/index.js
@@ -1,19 +1,15 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 async function run() {
-try {
-  // Get aud and request token
-  const audience = core.getInput('audience');
-  const jwt = await core.getIDToken(audience);
-  core.setOutput("jwt", jwt);
-  core.exportVariable('JWT', jwt);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+  try {
+    // Get aud and request token
+    const audience = core.getInput('audience');
+    console.log(`audience in the Javascript action: ${audience}`);
+    const jwt = await core.getIDToken(audience);
+    core.setOutput("jwt", jwt);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run()
