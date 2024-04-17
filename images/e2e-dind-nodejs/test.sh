90546f9ddffd984a4de42f931cfcd14b79a83216
Kamil Kasperski <kamil.kasperski@sap.com>
2024-04-17 12:35:44 +0200

    Revert "bug: remove running dockerd in dind-enabled image test (#10418)"
    This reverts commit 2f74c2cdabe7745358959cee11e6540504fe3770.

--- images/e2e-dind-nodejs/test.sh
+++ images/e2e-dind-nodejs/test.sh
@@ -3,10 +3,14 @@
 set -e
 
 echo "Binary existence checks"
-docker run --rm \
+docker run --rm --privileged \
+  -v /sys/fs/cgroup:/sys/fs/cgroup \
+  -v /lib/modules:/lib/modules:ro \
+  -e DOCKER_IN_DOCKER_ENABLED=true \
   "$IMG" bash -c '
   set -e
-  docker --version
+  cat $ARTIFACTS/docker-info.log
+  docker ps -a
   helm version
   kubectl version --client
   k3d version
