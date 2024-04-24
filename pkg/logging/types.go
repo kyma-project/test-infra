ba8cae9d177909efcbedba7485c3a19aac64d3b5
Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
2024-04-24 09:49:17 +0200

    Added narrow and smaller interfaces. (#10485)

--- pkg/logging/types.go
+++ pkg/logging/types.go
@@ -1,15 +1,44 @@
 package logging
 
-// LoggerInterface is a Logger interface for all implementations in kyma-project/test-infra
+import (
+	"go.uber.org/zap"
+)
+
+// LoggerInterface is a Logger interface collecting other main Logger interfaces.
+// It is used for compatibility with existing implementations in kyma-project/test-infra
 type LoggerInterface interface {
+	SimpleLoggerInterface
+	FormatedLoggerInterface
+	StructuredLoggerInterface
+}
+
+// SimpleLoggerInterface is a Logger interface with simple logging methods.
+type SimpleLoggerInterface interface {
 	Info(args ...interface{})
 	Warn(args ...interface{})
 	Error(args ...interface{})
 	Debug(args ...interface{})
-	Debugf(template string, args ...interface{})
+}
+
+// FormatedLoggerInterface is a Logger interface with logging methods supporting formated logging messages.
+type FormatedLoggerInterface interface {
 	Infof(template string, args ...interface{})
+	Warnf(template string, args ...interface{})
 	Errorf(template string, args ...interface{})
+	Debugf(template string, args ...interface{})
+}
+
+// StructuredLoggerInterface is a Logger interface with logging methods supporting structured logging.
+type StructuredLoggerInterface interface {
+	Infow(message string, keysAndValues ...interface{})
+	Warnw(message string, keysAndValues ...interface{})
+	Errorw(message string, keysAndValues ...interface{})
 	Debugw(message string, keysAndValues ...interface{})
+}
+
+// WithLoggerInterface is a Logger interface with support for adding context fields to the logger.
+type WithLoggerInterface interface {
+	With(args ...interface{}) *zap.SugaredLogger
 }
 
 // Logger is an interface to interact with Google logging.
