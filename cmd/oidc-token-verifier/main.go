b046a41c4a6e4a5d6561cd859bdf639e3efee62e
dekiel <przemyslaw.pokrywka@sap.com>
2024-05-04 21:43:59 +0200

    Parsing flags in commands which defines it. Logging

--- cmd/oidc-token-verifier/main.go
+++ cmd/oidc-token-verifier/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 
 	tioidc "github.com/kyma-project/test-infra/pkg/oidc"
@@ -25,17 +26,42 @@ type options struct {
 
 var (
 	// TrustedOIDCIssuers = map[string]string{"https://token.actions.githubusercontent.com": "https://token.actions.githubusercontent.com/.well-known/jwks"}
-	rootCmd = &cobra.Command{
-		Use:   "oidc",
+	rootCmd    *cobra.Command
+	claimsCmd  *cobra.Command
+	extractCmd *cobra.Command
+	opts       = options{}
+)
+
+func NewRootCmd() *cobra.Command {
+	rootCmd := &cobra.Command{
+		Use:   "token",
 		Short: "OIDC token verifier and claim extractor",
 		Long: `oidc is a CLI tool to verify OIDC tokens and extract claims from them. It can use cached public keys to verify tokens.
 	It use OIDC discovery to get the public keys and verify the token whenever the public keys are not cached or expired.`,
 	}
-	claimsCmd = &cobra.Command{
+	rootCmd.PersistentFlags().StringVarP(&opts.token, "token", "t", "", "OIDC token")
+	rootCmd.PersistentFlags().StringVarP(&opts.newPublicKeysVarName, "new-keys-var", "n", "OIDC_NEW_PUBLIC_KEYS", "Name of the environment variable to set when new public keys are fetched")
+	rootCmd.PersistentFlags().StringSliceVarP(&opts.trustedWorkflows, "trusted-workflows", "w", []string{}, "List of trusted workflows")
+	err := rootCmd.MarkPersistentFlagRequired("trusted-workflows")
+	if err != nil {
+		panic(err)
+	}
+	rootCmd.PersistentFlags().StringVarP(&opts.clientID, "client-id", "c", "image-builder", "OIDC token client ID")
+	rootCmd.PersistentFlags().StringVarP(&opts.publicKeyPath, "public-key-path", "p", "", "Path to the cached public keys directory")
+	rootCmd.PersistentFlags().BoolVarP(&opts.debug, "debug", "d", false, "Enable debug mode")
+	return rootCmd
+}
+
+func NewClaimsCmd() *cobra.Command {
+	claimsCmd := &cobra.Command{
 		Use:   "claims",
 		Short: "Work with OIDC claims",
 	}
-	extractCmd = &cobra.Command{
+	return claimsCmd
+}
+
+func NewExtractCmd() *cobra.Command {
+	extractCmd := &cobra.Command{
 		Use:   "extract",
 		Short: "Verify token and extract claims from an OIDC token",
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -45,39 +71,48 @@ var (
 			return nil
 		},
 	}
-	opts = options{}
-)
+	extractCmd.PersistentFlags().StringVarP(&opts.outputPath, "claims-output-path", "o", "", "Path to write the extracted claims too")
+	err := extractCmd.MarkPersistentFlagRequired("claims-output-path")
+	if err != nil {
+		panic(err)
+	}
+	return extractCmd
+
+}
 
 func init() {
-	rootCmd.PersistentFlags().StringVarP(&opts.token, "token", "t", "", "OIDC token")
-	err := rootCmd.MarkPersistentFlagRequired("token")
-	if err != nil {
-		panic(err)
-	}
-	rootCmd.PersistentFlags().StringVarP(&opts.newPublicKeysVarName, "new-public-keys-var", "n", "OIDC_NEW_PUBLIC_KEYS", "Name of the environment variable to set when new public keys are fetched")
-	err = rootCmd.MarkPersistentFlagRequired("new-public-keys-var-name")
-	if err != nil {
-		panic(err)
-	}
-	rootCmd.PersistentFlags().StringSliceVarP(&opts.trustedWorkflows, "trusted-workflows", "w", []string{}, "List of trusted workflows")
-	err = rootCmd.MarkPersistentFlagRequired("trusted-workflows")
-	if err != nil {
-		panic(err)
-	}
-	rootCmd.PersistentFlags().StringVarP(&opts.clientID, "client-id", "c", "", "OIDC token client ID")
-	err = rootCmd.MarkPersistentFlagRequired("client-id")
-	if err != nil {
-		panic(err)
-	}
-	rootCmd.PersistentFlags().StringVarP(&opts.publicKeyPath, "public-key-path", "p", "", "Path to the public keys directory")
-	rootCmd.PersistentFlags().BoolVarP(&opts.debug, "debug", "d", false, "Enable debug mode")
-	extractCmd.PersistentFlags().StringVarP(&opts.outputPath, "claims-output-path", "o", "", "Path to write the extracted claims")
-	err = extractCmd.MarkPersistentFlagRequired("claims-output-path")
-	if err != nil {
-		panic(err)
-	}
-	rootCmd.AddCommand(claimsCmd)
+	// rootCmd.PersistentFlags().StringVarP(&opts.token, "token", "t", "", "OIDC token")
+	// err := rootCmd.MarkPersistentFlagRequired("token")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// rootCmd.PersistentFlags().StringVarP(&opts.newPublicKeysVarName, "new-keys-var", "n", "OIDC_NEW_PUBLIC_KEYS", "Name of the environment variable to set when new public keys are fetched")
+	// err := rootCmd.MarkPersistentFlagRequired("new-keys-var")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// rootCmd.PersistentFlags().StringSliceVarP(&opts.trustedWorkflows, "trusted-workflows", "w", []string{}, "List of trusted workflows")
+	// err := rootCmd.MarkPersistentFlagRequired("trusted-workflows")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// rootCmd.PersistentFlags().StringVarP(&opts.clientID, "client-id", "c", "image-builder", "OIDC token client ID")
+	// err = rootCmd.MarkPersistentFlagRequired("client-id")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// rootCmd.PersistentFlags().StringVarP(&opts.publicKeyPath, "public-key-path", "p", "", "Path to the cached public keys directory")
+	// rootCmd.PersistentFlags().BoolVarP(&opts.debug, "debug", "d", false, "Enable debug mode")
+	// extractCmd.PersistentFlags().StringVarP(&opts.outputPath, "claims-output-path", "o", "", "Path to write the extracted claims too")
+	// err = extractCmd.MarkPersistentFlagRequired("claims-output-path")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	rootCmd = NewRootCmd()
+	claimsCmd = NewClaimsCmd()
+	extractCmd = NewExtractCmd()
 	claimsCmd.AddCommand(extractCmd)
+	rootCmd.AddCommand(claimsCmd)
 }
 
 // extractClaims verifies the OIDC token and extracts the claims from it.
@@ -91,42 +126,67 @@ func init() {
 func (opts *options) extractClaims() error {
 	// https://stackoverflow.com/questions/25609734/testing-stdout-with-go-and-ginkgo
 	var (
-		logger *zap.Logger
-		err    error
+		zapLogger *zap.Logger
+		err       error
 	)
 	if opts.debug {
-		logger, err = zap.NewDevelopment()
+		zapLogger, err = zap.NewDevelopment()
 	} else {
-		logger, err = zap.NewProduction()
+		zapLogger, err = zap.NewProduction()
 	}
 	if err != nil {
 		return err
 	}
-	sugaredLogger := logger.Sugar()
+	logger := zapLogger.Sugar()
 
-	verifyConfig, err := tioidc.NewVerifierConfig(sugaredLogger, opts.clientID)
-	if err != nil {
-		return err
+	// Check if token flag is set.
+	// If not check if AUTHORIZATION environment variable is set.
+	// If neither is set, return an error.
+	if opts.token == "" {
+		logger.Infow("Token flag not provided, checking for AUTHORIZATION environment variable")
+		opts.token = os.Getenv("AUTHORIZATION")
+		if opts.token == "" {
+			return fmt.Errorf("token not provided, set the --token flag or the AUTHORIZATION environment variable with the OIDC token")
+		}
+		logger.Infow("Token found in AUTHORIZATION environment variable, using the token")
 	}
+	logger.Infow("Token flag provided, using the token from the flag")
+	logger.Debugw("Token value", "token", opts.token)
 
-	tokenProcessor, err := tioidc.NewTokenProcessor(sugaredLogger, tioidc.TrustedOIDCIssuers, opts.token, verifyConfig)
+	logger.Infow("Using the following trusted workflows", "trusted-workflows", opts.trustedWorkflows)
+	logger.Infow("Using the following client ID", "client-id", opts.clientID)
+	logger.Infow("Using the following public key path", "public-key-path", opts.publicKeyPath)
+	logger.Infow("Using the following new public keys environment variable", "new-keys-var", opts.newPublicKeysVarName)
+	logger.Infow("Using the following claims output path", "claims-output-path", opts.outputPath)
+
+	verifyConfig, err := tioidc.NewVerifierConfig(logger, opts.clientID)
 	if err != nil {
 		return err
 	}
+	logger.Infow("Verifier config created", "config", verifyConfig)
+
+	tokenProcessor, err := tioidc.NewTokenProcessor(logger, tioidc.TrustedOIDCIssuers, opts.token, verifyConfig)
+	if err != nil {
+		return err
+	}
+	logger.Infow("Token processor created for trusted issuer", "issuer", tokenProcessor.Issuer())
 
 	ctx := context.Background()
-	provider, err := tioidc.NewProviderFromDiscovery(ctx, sugaredLogger, tokenProcessor.Issuer())
+	provider, err := tioidc.NewProviderFromDiscovery(ctx, logger, tokenProcessor.Issuer())
 	if err != nil {
 		return err
 	}
+	logger.Infow("Provider created using OIDC discovery", "issuer", tokenProcessor.Issuer())
 
 	verifier := provider.NewVerifier(verifyConfig)
+	logger.Infow("New verifier created")
 
 	claims := tioidc.Claims{}
 	err = tokenProcessor.Claims(ctx, &verifier, claims)
 	if err != nil {
 		return err
 	}
+	logger.Infow("Token verified, extracted claims", "claims", claims)
 	// TODO: verify claims values against expected values
 
 	// write claims to file
@@ -177,8 +237,7 @@ func (opts *options) extractClaims() error {
 // }
 
 func main() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		panic(err)
 	}
 }
