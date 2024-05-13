166c4060409324e9648b13b03075022b62316647
Wojciech Soltys <wojciech.soltys@sap.com>
2024-05-13 12:08:27 +0200

    Fix appendToTags function for sets.Tags

--- cmd/image-builder/main.go
+++ cmd/image-builder/main.go
@@ -237,6 +237,21 @@ func prepareADOTemplateParameters(options options) (adopipelines.OCIImageBuilder
 
 	templateParameters.SetUseKanikoConfigFromPR(options.testKanikoBuildConfig)
 
+	dockerfilePath, err := getDockerfileDirPath(options)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// Load environment variables from the envFile.
+	if len(options.envFile) > 0 {
+		envs, err := loadEnv(os.DirFS(dockerfilePath), options.envFile)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		appendToSetsOfTags(&options.buildArgs, envs)
+		fmt.Printf("Resulting buildArgs %v\n", options.buildArgs)
+	}
 	if len(options.buildArgs) > 0 {
 		templateParameters.SetBuildArgs(options.buildArgs.String())
 	}
@@ -245,7 +260,7 @@ func prepareADOTemplateParameters(options options) (adopipelines.OCIImageBuilder
 		templateParameters.SetImageTags(options.tags.String())
 	}
 
-	err := templateParameters.Validate()
+	err = templateParameters.Validate()
 	if err != nil {
 		return nil, fmt.Errorf("failed validating ADO template parameters, err: %w", err)
 	}
@@ -471,6 +486,17 @@ func appendMissing(target *map[string]string, source []tags.Tag) {
 
 // appendMissing appends key, values pairs from source array to target map
 func appendToTags(target *[]tags.Tag, source map[string]string) {
+	fmt.Printf("Appending envs %v to tags %v\n", source, target)
+	if len(source) > 0 {
+		for key, value := range source {
+			*target = append(*target, tags.Tag{Name: key, Value: value})
+		}
+	}
+	fmt.Printf("Resulting tags %v\n", target)
+}
+
+// appendMissing appends key, values pairs from source array to target map
+func appendToSetsOfTags(target *sets.Tags, source map[string]string) {
 	fmt.Printf("Appending envs %v to tags %v\n", source, target)
 	if len(source) > 0 {
 		for key, value := range source {
