df83afeadee01c326c19ac9f01da2785f4edebd7
Wojciech Soltys <wojciech.soltys@sap.com>
2024-09-03 13:41:38 +0200

    print ADO githubURL variable

--- cmd/oidc-token-verifier/main.go
+++ cmd/oidc-token-verifier/main.go
@@ -145,7 +145,7 @@ func (opts *options) extractClaims() error {
 		return err
 	}
 	logger.Infow("Token processor created for trusted issuer", "issuer", tokenProcessor.Issuer())
-	os.Setenv("GITHUB_URL", tokenProcessor.GetIssuer().GetGithubURL())
+	fmt.Printf("##vso[task.setvariable variable=githubURL] %s", tokenProcessor.GetIssuer().GetGithubURL())
 
 	ctx := context.Background()
 	// Create a new provider using OIDC discovery to get the public keys.
