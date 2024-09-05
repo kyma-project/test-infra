c1f50f360420fe410a3abcf828c0df18df9656e7
dekiel <przemyslaw.pokrywka@sap.com>
2024-09-05 13:17:12 +0200

    Add custom attribute mapping to identify if reusable workflow is running for pull request or push event.

--- configs/terraform/environments/prod/image-syncer.tf
+++ configs/terraform/environments/prod/image-syncer.tf
@@ -1,6 +1,6 @@
-resource "google_artifact_registry_repository_iam_member" "image_syncer_writer" {
+resource "google_artifact_registry_repository_iam_member" "image_syncer_prod_repo_writer" {
   location   = google_artifact_registry_repository.prod_docker_repository.location
   repository = google_artifact_registry_repository.prod_docker_repository.name
   role       = "roles/artifactregistry.createOnPushWriter"
-  member     = "principalSet://iam.googleapis.com/${module.gh_com_kyma_project_workload_identity_federation.pool_name}/attribute.reusable_workflow_ref/${var.image_builder_reusable_workflow_ref}"
+  member = "principalSet://iam.googleapis.com/${module.gh_com_kyma_project_workload_identity_federation.pool_name}/attribute.reusable_workflow_run/event_name:push:repository_owner_id:${data.github_organization.kyma-project.id}:reusable_workflow_ref:${var.image_builder_reusable_workflow_ref}"
 }
