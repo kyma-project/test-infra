c255f84e4026d5cb2b5e4d4a547c608e10ef013a
Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
2024-09-06 13:55:22 +0200

    Update configs/terraform/environments/prod/image-syncer.tf
    Co-authored-by: Wojciech Sołtys <74361703+Sawthis@users.noreply.github.com>

--- configs/terraform/environments/prod/image-syncer.tf
+++ configs/terraform/environments/prod/image-syncer.tf
@@ -2,5 +2,5 @@ resource "google_artifact_registry_repository_iam_member" "image_syncer_prod_rep
   location   = google_artifact_registry_repository.prod_docker_repository.location
   repository = google_artifact_registry_repository.prod_docker_repository.name
   role       = "roles/artifactregistry.createOnPushWriter"
-  member = "principalSet://iam.googleapis.com/${module.gh_com_kyma_project_workload_identity_federation.pool_name}/attribute.reusable_workflow_run/event_name:push:repository_owner_id:${data.github_organization.kyma-project.id}:reusable_workflow_ref:${var.image_builder_reusable_workflow_ref}"
+  member = "principalSet://iam.googleapis.com/${module.gh_com_kyma_project_workload_identity_federation.pool_name}/attribute.reusable_workflow_run/event_name:push:repository_owner_id:${data.github_organization.kyma-project.id}:reusable_workflow_ref:${var.image_syncer_reusable_workflow_ref}"
 }
