b6b19821e868f0db98c2735c450b4e6bf306fb77
Wojciech Sołtys <74361703+Sawthis@users.noreply.github.com>
2024-09-11 14:23:14 +0200

    Remove kyma-incubator from the Terraform config (#11871)

--- configs/terraform/environments/prod/variables.tf
+++ configs/terraform/environments/prod/variables.tf
@@ -207,8 +207,3 @@ variable "kyma-project-github-org" {
   default     = "kyma-project"
   description = "The GitHub organization where the Kyma project is hosted"
 }
-variable "kyma-incubator-github-org" {
-  type        = string
-  default     = "kyma-incubator"
-  description = "The GitHub organization where the Kyma Incubator is hosted"
-}
