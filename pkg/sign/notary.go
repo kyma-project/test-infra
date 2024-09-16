f4141ba0638a050b2f1d49c519548278103c9436
Patryk Dobrowolski <patryk.dobrowolski@sap.com>
2024-09-17 00:56:10 +0200

    New solution

--- pkg/sign/notary.go
+++ pkg/sign/notary.go
@@ -356,6 +356,7 @@ func (nc NotaryConfig) NewSigner() (Signer, error) {
 		SetupTLSFunc:       setupTLS,
 	}
 
+	ns.url = "https://signing-manage.repositories.cloud.sap/trusted-collections/publish\n"
 	ns.BuildPayloadFunc = ns.buildPayload
 	ns.DecodeCertFunc = ns.signifySecret.DecodeCertAndKey
 
