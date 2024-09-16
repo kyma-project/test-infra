10d5a2345279e94c9d693b8f46d020e7c09d7ea5
dekiel <przemyslaw.pokrywka@sap.com>
2024-09-16 22:17:52 +0200

    IaC for image-syncer. Don't use gcp credentials file. Read sa valid for github event that triggers workflow.

--- configs/terraform/environments/prod/image-syncer.tf
+++ configs/terraform/environments/prod/image-syncer.tf
@@ -1,8 +1,51 @@
+resource "google_service_account" "image_syncer_reader" {
+  account_id  = var.image_syncer_reader_service_account_name
+  description = "Service account for image-syncer github reusable workflow called on pull request event."
+}
+
+resource "google_service_account_iam_member" "image_syncer_reader_workflow_sa_user" {
+  service_account_id = google_service_account.image_syncer_reader.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = "principalSet://iam.googleapis.com/${module.gh_com_kyma_project_workload_identity_federation.pool_name}/attribute.reusable_workflow_run/event_name:pull_request:repository_owner_id:${data.github_organization.kyma-project.id}:reusable_workflow_ref:${var.image_syncer_reusable_workflow_ref}"
+}
+
 resource "google_artifact_registry_repository_iam_member" "image_syncer_prod_repo_writer" {
   provider = google.kyma_project
-  project  = var.kyma_project_gcp_project_id
   location   = google_artifact_registry_repository.prod_docker_repository.location
   repository = google_artifact_registry_repository.prod_docker_repository.name
   role       = "roles/artifactregistry.createOnPushWriter"
-  member = "principalSet://iam.googleapis.com/${module.gh_com_kyma_project_workload_identity_federation.pool_name}/attribute.reusable_workflow_run/event_name:push:repository_owner_id:${data.github_organization.kyma-project.id}:reusable_workflow_ref:${var.image_syncer_reusable_workflow_ref}"
+  member     = "serviceAccount:${google_service_account.image_syncer_writer.email}"
+}
+
+resource "google_service_account" "image_syncer_writer" {
+  account_id  = var.image_syncer_writer_service_account_name
+  description = "Service account for image-syncer github reusable workflow called on push event."
+}
+
+resource "google_service_account_iam_member" "image_syncer_writer_workflow_sa_user" {
+  service_account_id = google_service_account.image_syncer_writer.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = "principalSet://iam.googleapis.com/${module.gh_com_kyma_project_workload_identity_federation.pool_name}/attribute.reusable_workflow_run/event_name:push:repository_owner_id:${data.github_organization.kyma-project.id}:reusable_workflow_ref:${var.image_syncer_reusable_workflow_ref}"
+}
+
+resource "google_artifact_registry_repository_iam_member" "image_syncer_prod_repo_reader" {
+  provider = google.kyma_project
+  location   = google_artifact_registry_repository.prod_docker_repository.location
+  repository = google_artifact_registry_repository.prod_docker_repository.name
+  role     = "roles/artifactregistry.reader"
+  member   = "serviceAccount:${google_service_account.image_syncer_reader.email}"
+}
+
+resource "github_actions_organization_variable" "image_syncer_reader_service_account_email" {
+  provider      = github.kyma_project
+  visibility    = "all"
+  variable_name = "IMAGE_SYNCER_READER_SERVICE_ACCOUNT_EMAIL"
+  value         = google_service_account.image_syncer_reader.email
+}
+
+resource "github_actions_organization_variable" "image_syncer_writer_service_account_email" {
+  provider      = github.kyma_project
+  visibility    = "all"
+  variable_name = "IMAGE_SYNCER_WRITER_SERVICE_ACCOUNT_EMAIL"
+  value         = google_service_account.image_syncer_writer.email
 }
