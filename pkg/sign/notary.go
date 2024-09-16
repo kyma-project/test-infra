ce89c6f766edc74b1c0c4fc93834a5819d62cd91
Patryk Dobrowolski <patryk.dobrowolski@sap.com>
2024-09-17 01:44:14 +0200

    Ready to review, no hardcodes. 85% test coverage

--- pkg/sign/notary.go
+++ pkg/sign/notary.go
@@ -359,7 +359,6 @@ func (nc NotaryConfig) NewSigner() (Signer, error) {
 		SetupTLSFunc:       setupTLS,
 	}
 
-	ns.url = "https://signing-manage.repositories.cloud.sap/trusted-collections/publish"
 	ns.BuildPayloadFunc = ns.buildPayload
 	ns.DecodeCertFunc = ns.signifySecret.DecodeCertAndKey
 
