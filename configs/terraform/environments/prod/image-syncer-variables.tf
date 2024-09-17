37de9188a2fa3c8c70ecef6d0ec42b7f935ba0fd
Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>
2024-09-17 14:43:17 +0200

    Iac for image-syncer (#11921)
    * IaC for image-syncer.
    Don't use gcp credentials file.
    Read sa valid for github event that triggers workflow.
    
    * Code comments.
    Better names.
    
    * Update .github/workflows/image-syncer.yml
    
    Co-authored-by: Patryk Dobrowolski <patryk.dobrowolski@sap.com>
    
    ---------
    
    Co-authored-by: Patryk Dobrowolski <patryk.dobrowolski@sap.com>

--- configs/terraform/environments/prod/image-syncer-variables.tf
+++ configs/terraform/environments/prod/image-syncer-variables.tf
@@ -3,3 +3,15 @@ variable "image_syncer_reusable_workflow_ref" {
   description = "The value of GitHub OIDC token job_workflow_ref claim of the image-syncer reusable workflow in the test-infra repository. This is used to identify token exchange requests for image-syncer reusable workflow."
   default     = "kyma-project/test-infra/.github/workflows/image-syncer.yml@refs/heads/main"
 }
+
+variable "image_syncer_reader_service_account_name" {
+  type        = string
+  description = "The service account name of the image-syncer service account."
+  default     = "image-syncer-reader"
+}
+
+variable "image_syncer_writer_service_account_name" {
+  type        = string
+  description = "The service account name of the image-syncer service account."
+  default     = "image-syncer-writer"
+}
