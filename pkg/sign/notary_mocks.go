df642522d3597ad187bd84706e8ce299b43934f4
Patryk Dobrowolski <patryk.dobrowolski@sap.com>
2024-09-17 11:11:48 +0200

    remove unused mock

--- pkg/sign/notary_mocks.go
+++ pkg/sign/notary_mocks.go
@@ -1,38 +1,5 @@
 package sign
 
-import (
-	"net/http"
-)
-
-// MockRoundTripper allows us to mock HTTP client behavior for tests.
-type MockRoundTripper struct {
-	RoundTripFunc func(req *http.Request) (*http.Response, error)
-}
-
-func (m *MockRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
-	return m.RoundTripFunc(req)
-}
-
-type MockImage struct{}
-
-func (m *MockImage) Manifest() (*Manifest, error) {
-	return &Manifest{
-		Config: struct {
-			Digest struct {
-				Hex string
-			}
-			Size int64
-		}{
-			Digest: struct {
-				Hex string
-			}{
-				Hex: "abc123def456",
-			},
-			Size: 12345678,
-		},
-	}, nil
-}
-
 // MockParseReference is a mock function for ParseReferenceFunc
 func MockParseReference(image string) (Reference, error) {
 	return image, nil // In a simple case, we return the string itself as Reference
