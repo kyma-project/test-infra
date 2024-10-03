1b281d79ec6c9b7f1ef5754ec4eae569709f3efe
Kacper Małachowski <kacper.malachowski@sap.com>
2024-10-03 16:49:43 +0200

    Add cleanup policies for our registries

--- configs/terraform/environments/prod/artifact-registry-variables.tf
+++ configs/terraform/environments/prod/artifact-registry-variables.tf
@@ -25,6 +25,14 @@ variable "prod_docker_repository" {
     immutable_tags         = bool
     mode                   = string
     cleanup_policy_dry_run = bool
+    cleanup_policies       = list(object({
+      id      = string
+      action  = string
+      condition = object({
+        tag_state = string
+        older_than = string
+      })
+    }))
     labels = map(string)
   })
   default = {
@@ -35,8 +43,119 @@ variable "prod_docker_repository" {
     immutable_tags         = false
     mode                   = "STANDARD_REPOSITORY"
     cleanup_policy_dry_run = true
+    cleanup_policies = [ {
+      id = "delete_untagged"
+      action = "DELETE"
+      condition = {
+        tag_state = "UNTAGGED"
+        older_than = null
+      }
+    } ]
     labels = {
       "type" = "production"
     }
+  }
+}
+
+variable "docker_cache_repository" {
+  type = object({
+    name                   = string
+    description            = string
+    location               = string
+    format                 = string
+    immutable_tags         = bool
+    mode                   = string
+    cleanup_policy_dry_run = bool
+    cleanup_policies       = list(object({
+      id      = string
+      action  = string
+      condition = object({
+        tag_state = string
+        older_than = string
+      })
+    }))
+  })
+  default = {
+    name = "cache"
+    description = "Cache repo for kyma-project"
+    location = "europe"
+    format = "DOCKER"
+    immutable_tags = false
+    mode = "STANDARD_REPOSITORY"
+    cleanup_policy_dry_run = true
+    cleanup_policies = [ 
+      {
+        id = "delete_untagged"
+        action = "DELETE"
+        condition = {
+          tag_state = "UNTAGGED"
+          older_than = null
+        }
+      },
+      {
+        id = "delete_older_than_week"
+        action = "DELETE"
+        condition = {
+          tag_state = "ANY"
+          # Google provider does not support the time units, so we need to provide the time in seconds
+          older_than = "604800s" # 604800s = 7 days
+        }
+      }
+    ]
+  }
+}
+
+variable "docker_dev_repository" {
+  type = object({
+    name                   = string
+    description            = string
+    location               = string
+    format                 = string
+    immutable_tags         = bool
+    mode                   = string
+    cleanup_policy_dry_run = bool
+    cleanup_policies       = list(object({
+      id      = string
+      action  = optional(string)
+      condition = optional(object({
+        tag_state = optional(string)
+        tag_prefixes = optional(list(string), [])
+        older_than = optional(string)
+      }))
+    }))
+    labels = map(string)
+  })
+  default = {
+    name = "dev"
+    description = "Development images for kyma-projec"
+    location = "europe"
+    format = "DOCKER"
+    immutable_tags = false
+    mode = "STANDARD_REPOSITORY"
+    cleanup_policy_dry_run = true
+    labels = {
+      "type" = "development"
+    }
+    cleanup_policies = [ 
+      {
+        id = "delete_untagged"
+        action = "DELETE"
+        condition = {
+          tag_state = "UNTAGGED"
+          older_than = null
+        }
+      },
+      {
+        id = "delete_old_pr"
+        action = "DELETE"
+        condition = {
+          tag_state = "TAGGED"
+          
+          # Google provider does not support the time units, 
+          # so we need to provide the time in seconds.
+          older_than = "2592000s" # 2592000s = 720h = 30 days
+        }
+      }
+    ]
   }
 }
--- configs/terraform/environments/prod/artifact-registry.tf
+++ configs/terraform/environments/prod/artifact-registry.tf
@@ -19,7 +19,7 @@ module "artifact_registry" {
 
 import {
   to = google_artifact_registry_repository.prod_docker_repository
-  id = "projects/${var.kyma_project_gcp_project_id}/locations/${var.prod_docker_repository.location}/repositories/${var.prod_docker_repository.name}"
+  id = "projects/kyma-project/locations/europe/repositories/prod"
 }
 
 resource "google_artifact_registry_repository" "prod_docker_repository" {
@@ -33,4 +33,78 @@ resource "google_artifact_registry_repository" "prod_docker_repository" {
   docker_config {
     immutable_tags = var.prod_docker_repository.immutable_tags
   }
+
+  dynamic "cleanup_policies" {
+    for_each = var.prod_docker_repository.cleanup_policies
+    content {
+      id = cleanup_policies.value.id
+      action = cleanup_policies.value.action
+      condition {
+        tag_state = cleanup_policies.value.condition.tag_state
+        older_than = cleanup_policies.value.condition.older_than
+      }
+    }
+  }
+}
+
+import {
+  to = google_artifact_registry_repository.docker_cache
+  id = "projects/kyma-project/locations/europe/repositories/cache"
+}
+
+resource "google_artifact_registry_repository" "docker_cache" {
+  provider = google.kyma_project
+  location = var.docker_cache_repository.location
+  repository_id = var.docker_cache_repository.name
+  description = var.docker_cache_repository.description
+  format = var.docker_cache_repository.format
+  cleanup_policy_dry_run = var.docker_cache_repository.cleanup_policy_dry_run
+
+  docker_config {
+    immutable_tags = var.docker_cache_repository.immutable_tags
+  }
+
+  dynamic "cleanup_policies" {
+    for_each = var.docker_cache_repository.cleanup_policies
+    content {
+      id = cleanup_policies.value.id
+      action = cleanup_policies.value.action
+      condition {
+        tag_state = cleanup_policies.value.condition.tag_state
+        older_than = cleanup_policies.value.condition.older_than
+      }
+    }
+  }
+}
+
+import {
+  to = google_artifact_registry_repository.docker_dev
+  id = "projects/kyma-project/locations/europe/repositories/dev"
+}
+
+resource "google_artifact_registry_repository" "docker_dev" {
+  provider = google.kyma_project
+  location = var.docker_dev_repository.location
+  repository_id = var.docker_dev_repository.name
+  description = var.docker_dev_repository.description
+  format = var.docker_dev_repository.format
+  cleanup_policy_dry_run = var.docker_dev_repository.cleanup_policy_dry_run
+
+  docker_config {
+    immutable_tags = var.docker_dev_repository.immutable_tags
+  }
+
+  dynamic "cleanup_policies" {
+    for_each = var.docker_dev_repository.cleanup_policies
+    content {
+      id = cleanup_policies.value.id
+      action = cleanup_policies.value.action
+      condition {
+        tag_state = cleanup_policies.value.condition.tag_state
+        older_than = cleanup_policies.value.condition.older_than
+      }
+    }
+  }
+
+  labels = var.docker_dev_repository.labels
 }
