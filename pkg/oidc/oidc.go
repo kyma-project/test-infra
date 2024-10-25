ab50b0f6715c7fee12094a3792cd83e064ded431
Patryk Dobrowolski <patryk.dobrowolski@sap.com>
2024-10-25 15:23:25 +0200

    test

--- pkg/oidc/oidc.go
+++ pkg/oidc/oidc.go
@@ -328,8 +328,7 @@ func NewTokenProcessor(
 // tokenIssuer gets the issuer from the token.
 // It doesn't verify the token, just parses its claims.
 // It's used to create a new TokenProcessor.
-// TODO: signAlgorithm should be a TokenProcessor field.
-// TODO: should we abstract usage of jwt and go-jose libraries?
+// TODO: signAlgorithm shoul abstract usage of jwt and go-jose libraries?
 func (tokenProcessor *TokenProcessor) tokenIssuer(signAlgorithm []string) (string, error) {
 	logger := tokenProcessor.logger
 	logger.Debugw("Getting issuer from token")
