61d2b784040b92a4a38b98c288c042248ad476c3
Patryk Dobrowolski <patryk.dobrowolski@sap.com>
2024-10-28 15:15:29 +0100

    test

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -15,7 +15,7 @@ type User struct {
 }
 
 // Alias holds mapping between owners file alias and slack groups and channels names.
-// It holds information if automerge notification is enabled.
+// It holds information if automerge notification isenabled.
 type Alias struct {
 	ComGithubAliasname              string   `yaml:"com.github.aliasname,omitempty"`
 	ComEnterpriseSlackGroupsnames   []string `yaml:"com.slack.enterprise.sap.groupsnames,omitempty"`
