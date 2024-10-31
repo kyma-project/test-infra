9d68b8b6f28e8c0215d9d9023c5093de88676e7c
Kacper Małachowski <38684517+KacperMalachowski@users.noreply.github.com>
2024-10-31 09:07:04 +0100

    Unify naming

--- configs/terraform/environments/prod/iam.tf
+++ configs/terraform/environments/prod/iam.tf
@@ -1,10 +1,10 @@
 # Setups IAM for the project administrators in the kyma-project GCP project.
 import {
   id = "kyma-project roles/editor group:kyma_developer_admin@sap.com"
-  to = google_project_iam_member.kyma_developer_admins_editor
+  to = google_project_iam_member.kyma_developer_admin_editor
 }
 
-resource "google_project_iam_member" "kyma_developer_admins_editor" {
+resource "google_project_iam_member" "kyma_developer_admin_editor" {
   provider = google.kyma_project
   project = var.kyma_project_gcp_project_id
   role = "roles/editor"
@@ -12,7 +12,7 @@ resource "google_project_iam_member" "kyma_developer_admins_editor" {
 }
 
 # Add roles required to see audit logs in kyma-project GCP project.
-resource "google_project_iam_member" "kyma_developer_admins_logging_viewer" {
+resource "google_project_iam_member" "kyma_developer_admin_logging_viewer" {
   provider = google.kyma_project
   project = var.kyma_project_gcp_project_id
   role = "roles/logging.viewer"
