6c2e50b7007287d9aa7874c9f5e95afc6312d857
Kacper Małachowski <38684517+KacperMalachowski@users.noreply.github.com>
2025-01-31 12:36:33 +0100

    Revert "Bumping test-infra and testimages (#12620)" (#12622)
    This reverts commit 0732d42ed6a103e7d36fa3db704f7c153f2ac3b3.

--- configs/terraform/modules/security-dashboard-token/security-dashboard-token.tf
+++ configs/terraform/modules/security-dashboard-token/security-dashboard-token.tf
@@ -31,7 +31,7 @@ resource "google_cloud_run_service" "security_dashboard_token" {
   template {
     spec {
       containers {
-        image = "europe-docker.pkg.dev/kyma-project/prod/dashboard-token-proxy:v20250131-885542fa" #gitleaks:allow ignore gitleaks detection
+        image = "europe-docker.pkg.dev/kyma-project/prod/dashboard-token-proxy:v20250131-871459a2" #gitleaks:allow ignore gitleaks detection
         env {
           name = "CLIENT_SECRET"
           value_from {
