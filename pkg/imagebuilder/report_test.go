cc524d3a63d98a9c3a8150b5b961d6b352a9de4c
Kacper Małachowski <kacper.malachowski@sap.com>
2025-02-26 15:18:30 +0100

    Update the format to agreed with SRE

--- pkg/imagebuilder/report_test.go
+++ pkg/imagebuilder/report_test.go
@@ -21,6 +21,11 @@ var _ = Describe("Report", func() {
 2025-01-31T08:32:23.7346062Z     "status": "Succeeded",
 2025-01-31T08:32:23.7357582Z     "pushed": true,
 2025-01-31T08:32:23.7358184Z     "signed": false,
+2025-01-31T08:32:23.7358759Z     "tags": [
+2025-01-31T08:32:23.7359525Z         "PR-477"
+2025-01-31T08:32:23.7360295Z     ],
+2025-01-31T08:32:23.7360618Z     "repository_path": "europe-docker.pkg.dev/kyma-project/dev/",
+2025-01-31T08:32:23.7361207Z     "image_name": "github-tools-sap/conduit-cli",
 2025-01-31T08:32:23.7363009Z     "images_list": [
 2025-01-31T08:32:23.7363276Z         "europe-docker.pkg.dev/kyma-project/dev/github-tools-sap/conduit-cli:PR-477"
 2025-01-31T08:32:23.7363276Z     ],
@@ -30,12 +35,14 @@ var _ = Describe("Report", func() {
 2025-01-31T08:32:23.7416532Z 
 2025-01-31T08:32:23.7530550Z ##[section]Finishing: prepare_image_build_report`
 		expectedReport := &BuildReport{
-			Status:       "Succeeded",
-			IsPushed:     true,
-			IsSigned:     false,
-			IsProduction: false,
-			Images:       []string{"europe-docker.pkg.dev/kyma-project/dev/github-tools-sap/conduit-cli:PR-477"},
-			Digest:       "sha256:215151561",
+			Status:      "Succeeded",
+			IsPushed:    true,
+			IsSigned:    false,
+			Images:      []string{"europe-docker.pkg.dev/kyma-project/dev/github-tools-sap/conduit-cli:PR-477"},
+			Digest:      "sha256:215151561",
+			Name:        "github-tools-sap/conduit-cli",
+			Tags:        []string{"PR-477"},
+			RegistryURL: "europe-docker.pkg.dev/kyma-project/dev/",
 		}
 
 		It("parses the image build report", func() {
@@ -82,11 +89,13 @@ var _ = Describe("Report", func() {
 2025-01-31T08:32:23.7416532Z 
 2025-01-31T08:32:23.7530550Z ##[section]Finishing: prepare_image_build_report`
 		expectedReport := &BuildReport{
-			Status:       "Succeeded",
-			IsPushed:     true,
-			IsSigned:     false,
-			IsProduction: false,
-			Images:       []string{"europe-docker.pkg.dev/kyma-project/dev/github-tools-sap/conduit-cli:PR-477"},
+			Status:      "Succeeded",
+			IsPushed:    true,
+			IsSigned:    false,
+			Images:      []string{"europe-docker.pkg.dev/kyma-project/dev/github-tools-sap/conduit-cli:PR-477"},
+			Name:        "github-tools-sap/conduit-cli",
+			Tags:        []string{"PR-477"},
+			RegistryURL: "europe-docker.pkg.dev/kyma-project/dev/",
 		}
 
 		It("parses the image build report", func() {
@@ -99,12 +108,14 @@ var _ = Describe("Report", func() {
 
 	Describe("WriteReportToFile", func() {
 		report := &BuildReport{
-			Status:       "Succeeded",
-			IsPushed:     true,
-			IsSigned:     false,
-			IsProduction: false,
-			Images:       []string{"europe-docker.pkg.dev/kyma-project/dev/github-tools-sap/conduit-cli:PR-477"},
-			Digest:       "sha256:215151561",
+			Status:      "Succeeded",
+			IsPushed:    true,
+			IsSigned:    false,
+			Images:      []string{"europe-docker.pkg.dev/kyma-project/dev/github-tools-sap/conduit-cli:PR-477"},
+			Digest:      "sha256:215151561",
+			Name:        "github-tools-sap/conduit-cli",
+			Tags:        []string{"PR-477"},
+			RegistryURL: "europe-docker.pkg.dev/kyma-project/dev/",
 		}
 
 		It("writes the report to a file", func() {
