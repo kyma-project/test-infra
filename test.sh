71c9ecc66ec2b6965f352e5be018a8adb528c86d
Kacper Małachowski <kacper.malachowski@sap.com>
2025-03-05 08:53:48 +0100

    Remove test script, update action

--- test.sh
+++
@@ -1,8 +0,0 @@
-
-WORKFLOW_REF_REGEX=^kyma-project/test-infra/.github/workflows/image-builder-test.yml@refs/pull/[0-9]+$/m
-
-# Allow only the test workflow to use the image-builder image from the input
-if [[ ! "kyma-project/test-infra/.github/workflows/image-builder-test.yml@refs/pull/142" =~ $WORKFLOW_REF_REGEX ]] && [[ -n "" ]]; then
-    echo "Only main branch is allowed to trigger this workflow."
-    exit 1
-fi
