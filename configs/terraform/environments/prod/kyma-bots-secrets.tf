88626eedb2b9e7695aa1c64630e66bf1b66338ba
Kacper Małachowski <kacper.malachowski@sap.com>
2025-03-27 10:53:15 +0100

    Format terraform files

--- configs/terraform/environments/prod/kyma-bots-secrets.tf
+++ configs/terraform/environments/prod/kyma-bots-secrets.tf
@@ -69,8 +69,8 @@ resource "kubernetes_cluster_role_binding" "access_kyma_bot_github_tokens_untrus
 
 # 
 resource "github_actions_variable" "kyma_autobump_bot_github_token_secret_name" {
-  provider = github.kyma_project
-  repository = data.github_repository.test_infra.name
+  provider      = github.kyma_project
+  repository    = data.github_repository.test_infra.name
   variable_name = "KYMA_AUTOBUMP_BOT_GITHUB_SECRET_NAME"
-  value = var.kyma_bot_github_sap_token_secret_name
+  value         = var.kyma_bot_github_sap_token_secret_name
 }
