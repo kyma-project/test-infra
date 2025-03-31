c543c85426610e4823eb10595ec30a300e49d587
Kacper Małachowski <kacper.malachowski@sap.com>
2025-03-31 10:16:18 +0200

    Prepare CLA Assistant

--- cmd/cloud-run/claassistant/claassitant.go
+++ cmd/cloud-run/claassistant/claassitant.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/kyma-project/test-infra/pkg/gcp/cloudfunctions"
+)
+
+func main() {}
+
+func CLAAssistant(logger *cloudfunctions.LogEntry) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+	}
+}
