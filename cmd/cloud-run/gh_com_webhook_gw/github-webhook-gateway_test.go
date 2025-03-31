ca8b53d662a5d94c46a07e4a365df71c31c30a94
Kacper Małachowski <kacper.malachowski@sap.com>
2025-03-31 08:57:53 +0200

    Add comment about tests

--- cmd/cloud-run/gh_com_webhook_gw/github-webhook-gateway_test.go
+++ cmd/cloud-run/gh_com_webhook_gw/github-webhook-gateway_test.go
@@ -1,1 +1,7 @@
 package main
+
+// TODO(kacpermalachowski): Add unit tests
+// We need a way to mock the pubsub client
+// Is there a way to mock the pubsub client?
+// Google suggest `pstest` package, but it's considered "expermental" and may change without notice.
+// See: https://pkg.go.dev/cloud.google.com/go/pubsub/pstest
