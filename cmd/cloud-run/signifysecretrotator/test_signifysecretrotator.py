f3f9df478d6f3e94c58b19947218b228d75b74cb
Kacper Małachowski <kacper.malachowski@sap.com>
2025-04-08 08:22:05 +0200

    Remove assert logs

--- cmd/cloud-run/signifysecretrotator/test_signifysecretrotator.py
+++ cmd/cloud-run/signifysecretrotator/test_signifysecretrotator.py
@@ -26,33 +26,18 @@ class TestValidateMessage(unittest.TestCase):
         message = {"labels": {"type": "incorrect_type"}}
         result = validate_message(self.logger, message)
         self.assertFalse(result)
-        self.logger.info.assert_called_once_with(
-            "Incorrect message type received. Expected %s, got %s. Ignoring message.",
-            "incorrect_type",
-            self.secret_rotate_message_type,
-        )
 
     def test_validate_message_missing_labels(self):
         """Test validate_message with missing labels"""
         message = {}
         result = validate_message(self.logger, message)
         self.assertFalse(result)
-        self.logger.info.assert_called_once_with(
-            "Incorrect message type received. Expected %s, got %s. Ignoring message.",
-            None,
-            self.secret_rotate_message_type,
-        )
 
     def test_validate_message_missing_type(self):
         """Test validate_message with missing type in labels"""
         message = {"labels": {}}
         result = validate_message(self.logger, message)
         self.assertFalse(result)
-        self.logger.info.assert_called_once_with(
-            "Incorrect message type received. Expected %s, got %s. Ignoring message.",
-            None,
-            self.secret_rotate_message_type,
-        )
 
 
 if __name__ == "__main__":
