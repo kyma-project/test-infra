287cd168351ac58767f77928dfe261e77f0bc275
dekiel <przemyslaw.pokrywka@sap.com>
2025-04-30 19:29:56 +0200

    Add dev-modules-internal repository.

--- configs/terraform/environments/prod/restricted-markets-delivery.tf
+++ configs/terraform/environments/prod/restricted-markets-delivery.tf
@@ -7,7 +7,7 @@ resource "google_service_account" "restricted-markets-artifactregistry-reader" {
 resource "google_service_account_iam_member" "restricted_markets_artifactregistry_reader_impersonation" {
   service_account_id = google_service_account.restricted-markets-artifactregistry-reader.name
   role               = "roles/iam.serviceAccountTokenCreator"
-  member             = "serviceAccount:${var.sre-restricted-markets-artifactregistry-reader.sre-registry-reader-sa}"
+  member             = "serviceAccount:${var.sre-restricted-markets-artifactregistry-reader.sre-registry-reader-sa-fqdn}"
 }
 
 resource "google_artifact_registry_repository_iam_member" "kyma_modules_reader" {
@@ -17,15 +17,22 @@ resource "google_artifact_registry_repository_iam_member" "kyma_modules_reader" 
   member     = "serviceAccount:${google_service_account.restricted-markets-artifactregistry-reader.email}"
 }
 
+resource "google_artifact_registry_repository_iam_member" "dev_modules_internal_reader" {
+  repository = google_artifact_registry_repository.dev_modules_internal.name
+  location   = google_artifact_registry_repository.dev_modules_internal.location
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:${google_service_account.restricted-markets-artifactregistry-reader.email}"
+}
+
 variable "sre-restricted-markets-artifactregistry-reader" {
   type = object({
-    registry-reader-sa     = string
-    sa-description         = string
-    sre-registry-reader-sa = string
+    registry-reader-sa          = string
+    sa-description              = string
+    sre-registry-reader-sa-fqdn = string
   })
   default = {
-    registry-reader-sa     = "restricted-markets-reg-reader"
-    sa-description         = "Service account for restricted markets delivery with artifact registry reader access"
-    sre-registry-reader-sa = "gcr-writer@sap-ti-dx-kyma-mps-dev.iam.gserviceaccount.com"
+    registry-reader-sa          = "restricted-markets-reg-reader"
+    sa-description              = "Service account for restricted markets delivery artifact registry reader access"
+    sre-registry-reader-sa-fqdn = "gcr-writer@sap-ti-dx-kyma-mps-dev.iam.gserviceaccount.com"
   }
 }
