b22860ef00ece86155c34d04e2e94c2a2a80e093
Kacper Małachowski <38684517+KacperMalachowski@users.noreply.github.com>
2025-05-08 09:04:36 +0200

    Move registries to module definition (#13023)
    * Move registries to module definition
    
    * Use new definition of artifact registry
    
    * Fix definition for image-syncer
    
    * Move dev registry into module
    
    * Rename modules to better describe it funcitons
    
    * Fix moved block
    
    * Restore mode to use it later
    
    * Do not use mode yet
    
    * Align names
    
    * Fix reference
    
    * Fix moved block
    
    * Fix
    
    * Align naming, align with google lcoud naming
    
    ---------
    
    Co-authored-by: Przemek Pokrywka <12400578+dekiel@users.noreply.github.com>

--- configs/terraform/environments/prod/artifact-registry.tf
+++ configs/terraform/environments/prod/artifact-registry.tf
@@ -7,73 +7,60 @@ module "artifact_registry" {
   }
 
 
-  for_each               = var.kyma_project_artifact_registry_collection
-  repository_name        = each.value.name
-  description            = each.value.description
-  type                   = each.value.type
-  immutable_tags         = each.value.immutable
-  multi_region           = each.value.multi_region
-  owner                  = each.value.owner
+  for_each                  = var.kyma_project_artifact_registry_collection
+  repository_name           = each.value.name
+  description               = each.value.description
+  type                      = each.value.type
+  immutable_tags            = each.value.immutable
+  multi_region              = each.value.multi_region
+  owner                     = each.value.owner
   repoAdmin_serviceaccounts = each.value.repoAdmin_serviceaccounts
-  reader_serviceaccounts = each.value.reader_serviceaccounts
-  public                 = each.value.public
-  cleanup_policy_dry_run = each.value.cleanup_policy_dry_run
-  cleanup_policies       = each.value.cleanup_policies
+  reader_serviceaccounts    = each.value.reader_serviceaccounts
+  public                    = each.value.public
+  cleanup_policy_dry_run    = each.value.cleanup_policy_dry_run
+  cleanup_policies          = each.value.cleanup_policies
 }
 
-# TODO (dekiel): move to the module modules/artifact-registry
-resource "google_artifact_registry_repository" "prod_docker_repository" {
-  provider               = google.kyma_project
-  labels                 = var.prod_docker_repository.labels
-  location               = var.prod_docker_repository.location
-  repository_id          = var.prod_docker_repository.name
+moved {
+  from = google_artifact_registry_repository.prod_docker_repository
+  to   = module.prod_docker_repository.google_artifact_registry_repository.artifact_registry
+}
+
+module "prod_docker_repository" {
+  source = "../../modules/artifact-registry"
+
+  providers = {
+    google = google.kyma_project
+  }
+
+  repository_name        = var.prod_docker_repository.name
   description            = var.prod_docker_repository.description
+  location               = var.prod_docker_repository.location
+  immutable_tags         = var.prod_docker_repository.immutable_tags
   format                 = var.prod_docker_repository.format
+  cleanup_policies       = var.prod_docker_repository.cleanup_policies
   cleanup_policy_dry_run = var.prod_docker_repository.cleanup_policy_dry_run
-  docker_config {
-    immutable_tags = var.prod_docker_repository.immutable_tags
-  }
+  type                   = var.prod_docker_repository.type
 
-  cleanup_policies {
-    id     = "delete-untagged"
-    action = "DELETE"
-    condition {
-      tag_state = "UNTAGGED"
-    }
-  }
 }
 
-# TODO (dekiel): move to the module modules/artifact-registry
-resource "google_artifact_registry_repository" "docker_dev" {
-  provider               = google.kyma_project
-  location               = var.docker_dev_repository.location
-  repository_id          = var.docker_dev_repository.name
-  description            = var.docker_dev_repository.description
-  format                 = var.docker_dev_repository.format
-  cleanup_policy_dry_run = var.docker_dev_repository.cleanup_policy_dry_run
+moved {
+  from = google_artifact_registry_repository.docker_dev
+  to   = module.dev_docker_repository.google_artifact_registry_repository.artifact_registry
+}
 
-  docker_config {
-    immutable_tags = var.docker_dev_repository.immutable_tags
+module "dev_docker_repository" {
+  source = "../../modules/artifact-registry"
+
+  providers = {
+    google = google.kyma_project
   }
 
-  cleanup_policies {
-    id     = "delete-untagged"
-    action = "DELETE"
-    condition {
-      tag_state = "UNTAGGED"
-    }
-  }
-
-  cleanup_policies {
-    id     = "delete-old-pr-images"
-    action = "DELETE"
-    condition {
-      tag_state = "TAGGED"
-      # Equivalent to PR-*
-      tag_prefixes = ["PR-"]
-      older_than   = var.docker_dev_repository.pr_images_max_age
-    }
-  }
-
-  labels = var.docker_dev_repository.labels
+  repository_name        = var.dev_docker_repository.name
+  description            = var.dev_docker_repository.description
+  location               = var.dev_docker_repository.location
+  immutable_tags         = var.dev_docker_repository.immutable_tags
+  format                 = var.dev_docker_repository.format
+  cleanup_policies       = var.dev_docker_repository.cleanup_policies
+  cleanup_policy_dry_run = var.dev_docker_repository.cleanup_policy_dry_run
 }
