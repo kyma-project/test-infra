9c25b5bc310e893b25b5498239c8117a4cce09b7
Patryk Dobrowolski <akiioto@gmail.com>
2025-05-14 12:17:14 +0200

    Add Authentication configuration for dockerhub in dockerhub-mirror repo (#12929)
    * Add Authentication configuration for dockerhub in dockerhub-mirror repository
    
    * test module on dockerhub_mirror and cache
    
    * test module on dockerhub_mirror and cache
    
    * test module on dockerhub_mirror and cache
    
    * test module on dockerhub_mirror and cache
    
    * test module on dockerhub_mirror and cache
    
    * test module on dockerhub_mirror and cache
    
    * test module on dockerhub_mirror and cache
    
    * test module on dockerhub_mirror and cache
    
    * test module on dockerhub_mirror and cache
    
    * dockerhub credentials variable (combined secret name and user name)
    
    * remove latest? dockerhub credential is not detected?
    
    * tried to give secretaccessor for terraform planner
    
    * revert latest version of secret
    
    * Prevent destroy prod registry
    
    * remove default values from remote_repository_config variable
    
    * Fixing notation; Rework to use variables instead of hardcoded strings
    
    * refine root module reference to module
    
    * adjustment
    
    * adjustment
    
    * adjustment
    
    * adjustment
    
    * adjustment
    
    * prevent destroy
    
    * prevent destroy as dynamic
    
    * test local
    
    * test local
    
    * test local
    
    * test local
    
    * repository_prevent_destroy as variable
    
    * Formatting on every file
    
    * correct image-builder-variables
    
    * add open tofu issue, as explanation of workaround
    
    * adjust variables after merge
    
    * remove old resource registry
    
    * Move variable from main to variables.tf; Add one() function to check collection have 0 or null values; flatten docker_repository variable
    
    * Fix one() variable passing as a list
    
    * Fix field name in remote_repository_config usage in module
    
    * Fix usage of local remote repository config
    
    * Replace for_each with local variable instead of direct
    
    * pass value to dynamic resource from for_each iterator in a resource
    
    * add iterator for for_each
    
    * fix iterator for for_each
    
    * fix iterator for for_each
    
    * Update artifact-registry.tf
    
    * move artifact_registry as protected_repository test
    
    * Move registry to not destroy
    
    * protect artifact registry to not be deleted
    
    * protect artifact registry to not be deleted
    
    * switch default prevent destroy to false
    
    * set explicit prevent destroy for modules-submission repositories
    
    * revert default value of prevent destroy
    
    * change repository_prevent_destroy from string to bool

--- configs/terraform/environments/prod/artifact-registry.tf
+++ configs/terraform/environments/prod/artifact-registry.tf
@@ -1,3 +1,8 @@
+moved {
+  from = module.artifact_registry["modules-internal"].google_artifact_registry_repository.artifact_registry
+  to   = module.artifact_registry["modules-internal"].google_artifact_registry_repository.protected_repository[0]
+}
+
 # TODO (dekiel): remove after migration to modulectl is done
 module "artifact_registry" {
   source = "../../modules/artifact-registry"
@@ -22,8 +27,8 @@ module "artifact_registry" {
 }
 
 moved {
-  from = google_artifact_registry_repository.prod_docker_repository
-  to   = module.prod_docker_repository.google_artifact_registry_repository.artifact_registry
+  from = module.prod_docker_repository.google_artifact_registry_repository.artifact_registry
+  to   = module.prod_docker_repository.google_artifact_registry_repository.protected_repository[0]
 }
 
 module "prod_docker_repository" {
@@ -33,20 +38,19 @@ module "prod_docker_repository" {
     google = google.kyma_project
   }
 
-  repository_name        = var.prod_docker_repository.name
-  description            = var.prod_docker_repository.description
-  location               = var.prod_docker_repository.location
-  immutable_tags         = var.prod_docker_repository.immutable_tags
-  format                 = var.prod_docker_repository.format
-  cleanup_policies       = var.prod_docker_repository.cleanup_policies
-  cleanup_policy_dry_run = var.prod_docker_repository.cleanup_policy_dry_run
-  type                   = var.prod_docker_repository.type
-
+  repository_name            = var.prod_docker_repository.name
+  description                = var.prod_docker_repository.description
+  location                   = var.prod_docker_repository.location
+  immutable_tags             = var.prod_docker_repository.immutable_tags
+  format                     = var.prod_docker_repository.format
+  cleanup_policies           = var.prod_docker_repository.cleanup_policies
+  cleanup_policy_dry_run     = var.prod_docker_repository.cleanup_policy_dry_run
+  repository_prevent_destroy = var.prod_docker_repository.repository_prevent_destroy
 }
 
 moved {
-  from = google_artifact_registry_repository.docker_dev
-  to   = module.dev_docker_repository.google_artifact_registry_repository.artifact_registry
+  from = module.dev_docker_repository.google_artifact_registry_repository.artifact_registry
+  to   = module.dev_docker_repository.google_artifact_registry_repository.protected_repository[0]
 }
 
 module "dev_docker_repository" {
@@ -56,11 +60,13 @@ module "dev_docker_repository" {
     google = google.kyma_project
   }
 
-  repository_name        = var.dev_docker_repository.name
-  description            = var.dev_docker_repository.description
-  location               = var.dev_docker_repository.location
-  immutable_tags         = var.dev_docker_repository.immutable_tags
-  format                 = var.dev_docker_repository.format
-  cleanup_policies       = var.dev_docker_repository.cleanup_policies
-  cleanup_policy_dry_run = var.dev_docker_repository.cleanup_policy_dry_run
+  repository_name            = var.dev_docker_repository.name
+  description                = var.dev_docker_repository.description
+  location                   = var.dev_docker_repository.location
+  immutable_tags             = var.dev_docker_repository.immutable_tags
+  format                     = var.dev_docker_repository.format
+  cleanup_policies           = var.dev_docker_repository.cleanup_policies
+  cleanup_policy_dry_run     = var.dev_docker_repository.cleanup_policy_dry_run
+  type                       = var.dev_docker_repository.type
+  repository_prevent_destroy = var.dev_docker_repository.repository_prevent_destroy
 }
