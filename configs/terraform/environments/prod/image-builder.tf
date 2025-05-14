5cb1f996421cdbec70c1c0250aa8d0d2c4442544
Patryk Dobrowolski <akiioto@gmail.com>
2025-05-14 12:51:31 +0200

    Use correct attribute reference to get secret path (#13050)

--- configs/terraform/environments/prod/image-builder.tf
+++ configs/terraform/environments/prod/image-builder.tf
@@ -110,7 +110,7 @@ module "dockerhub_mirror" {
     description = var.dockerhub_mirror.remote_repository_config.description
     docker_public_repository = var.dockerhub_mirror.remote_repository_config.docker_public_repository
     upstream_username = var.dockerhub_credentials != null ? var.dockerhub_credentials.username : null
-    upstream_password_secret = data.google_secret_manager_secret_version.dockerhub_oat_secret[0].secret
+    upstream_password_secret = data.google_secret_manager_secret_version.dockerhub_oat_secret[0].name
   }
 }
 
