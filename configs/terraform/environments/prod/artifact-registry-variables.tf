900a6994174c11eddb1d10b9b319412e9b6b9749
Patryk Dobrowolski <patryk.dobrowolski@sap.com>
2025-09-02 13:59:15 +0200

    Remove legacy configurations of resources after migration to modulectl

--- configs/terraform/environments/prod/artifact-registry-variables.tf
+++ configs/terraform/environments/prod/artifact-registry-variables.tf
@@ -1,32 +1,3 @@
-# TODO (dekiel): remove after migration to modulectl is done
-variable "kyma_project_artifact_registry_collection" {
-  type = map(object({
-    name                      = string
-    owner                     = string
-    type                      = string
-    description               = string
-    repoAdmin_serviceaccounts = optional(list(string), [])
-    writer_serviceaccounts    = optional(list(string), [])
-    reader_serviceaccounts    = optional(list(string), [])
-    primary_area              = optional(string, "europe")
-    multi_region              = optional(bool, true)
-    public                    = optional(bool, false)
-    immutable                 = optional(bool, false)
-    cleanup_policy_dry_run    = optional(bool, false)
-    cleanup_policies = optional(list(object({
-      id     = string
-      action = string
-      condition = optional(object({
-        tag_state             = string
-        tag_prefixes          = optional(list(string), [])
-        package_name_prefixes = optional(list(string), [])
-        older_than            = optional(string, "")
-      }))
-    })))
-  }))
-}
-
-
 variable "prod_docker_repository" {
   type = object({
     name                   = string
--- configs/terraform/environments/prod/artifact-registry.tf
+++ configs/terraform/environments/prod/artifact-registry.tf
@@ -1,26 +1,3 @@
-# TODO (dekiel): remove after migration to modulectl is done
-module "artifact_registry" {
-  source = "../../modules/artifact-registry"
-
-  providers = {
-    google = google.kyma_project
-  }
-
-
-  for_each                  = var.kyma_project_artifact_registry_collection
-  repository_name           = each.value.name
-  description               = each.value.description
-  type                      = each.value.type
-  immutable_tags            = each.value.immutable
-  multi_region              = each.value.multi_region
-  owner                     = each.value.owner
-  repoAdmin_serviceaccounts = each.value.repoAdmin_serviceaccounts
-  reader_serviceaccounts    = each.value.reader_serviceaccounts
-  public                    = each.value.public
-  cleanup_policy_dry_run    = each.value.cleanup_policy_dry_run
-  cleanup_policies          = each.value.cleanup_policies
-}
-
 module "prod_docker_repository" {
   source = "../../modules/artifact-registry"
 
