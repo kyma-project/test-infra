6fabedf09d7cb9681c60147c1f303e424531f4af
dekiel <przemyslaw.pokrywka@sap.com>
2026-01-16 14:42:25 +0100

    Referencce correct secret

--- configs/terraform/environments/prod/autobump-security-config.tf
+++ configs/terraform/environments/prod/autobump-security-config.tf
@@ -17,5 +17,5 @@ resource "github_actions_variable" "kyma_autobump_bot_github_token_secret_name" 
   provider      = github.kyma_project
   repository    = data.github_repository.test_infra.name
   variable_name = "KYMA_AUTOBUMP_BOT_GITHUB_SECRET_NAME"
-  value         = var.kyma_bot_github_sap_token_secret_name
+  value         = google_secret_manager_secret.kyma_bot_public_github_token.secret_id
 }
