766c96d1be6c3b75de9dcca202d895db2a639fbf
Patryk Dobrowolski <patryk.dobrowolski@sap.com>
2026-01-20 11:27:14 +0100

    Add azure-pipeline-image-builder service account to restricted registry access
    - Create new hierarchical group 'kyma-restricted-registry-image-signer@sap.com'
    - Add azure-pipeline-image-builder@kyma-project.iam.gserviceaccount.com to image-signer group
    - Grant image-signer group read access to kyma-restricted-images-prod registry
    - Update architecture diagram to reflect new group structure

--- configs/terraform/environments/prod/restricted-registry-hierarchical-groups.tf
+++ configs/terraform/environments/prod/restricted-registry-hierarchical-groups.tf
@@ -36,6 +36,19 @@ resource "google_cloud_identity_group_membership" "image_builder_restricted_mark
   }
 }
 
+resource "google_cloud_identity_group_membership" "azure_pipeline_image_builder_sa_to_hierarchical_group" {
+  provider = google.kyma_project
+  group    = var.restricted_registry_hierarchical_groups.image_signer_group_name
+
+  preferred_member_key {
+    id = google_service_account.kyma_project_image_builder.email
+  }
+
+  roles {
+    name = "MEMBER"
+  }
+}
+
 # Layer 2 → Layer 3: Hierarchical Groups join Registry Access Groups
 
 resource "google_cloud_identity_group_membership" "security_scanners_group_to_prod_read" {
@@ -97,6 +110,19 @@ resource "google_cloud_identity_group_membership" "markets_delivery_group_to_pro
     name = "MEMBER"
   }
 }
+
+resource "google_cloud_identity_group_membership" "image_signer_group_to_prod_read" {
+  group = var.restricted_registry_iam_groups.prod_read_group_name
+
+  preferred_member_key {
+    id = var.restricted_registry_hierarchical_groups.image_signer
+  }
+
+  roles {
+    name = "MEMBER"
+  }
+}
+
 # Image builder with accesses to restricted registries
 resource "google_cloud_identity_group_membership" "image_builder_group_to_dev_read" {
   group = var.restricted_registry_iam_groups.dev_read_group_name
