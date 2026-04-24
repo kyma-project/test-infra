31158b2b97c0ab62e7f84ef760d12da28077fb56
dekiel <przemyslaw.pokrywka@sap.com>
2026-04-24 13:31:38 +0200

    refactor: group hosted runner variables into single object variable

--- configs/terraform/environments/prod/github-actions-hosted-runners-variables.tf
+++ configs/terraform/environments/prod/github-actions-hosted-runners-variables.tf
@@ -4,32 +4,22 @@ variable "telemetry_manager_runner_group_name" {
   description = "Name of the GitHub Actions runner group for telemetry-manager"
 }
 
-variable "telemetry_manager_hosted_runner_name" {
-  type        = string
-  default     = "telemetry-4core"
-  description = "Name and label of the GitHub-hosted larger runner for telemetry-manager"
-}
+variable "telemetry_manager_hosted_runner" {
+  type = object({
+    name         = string
+    size         = string
+    max_runners  = number
+    image_id     = string
+    image_source = string
+  })
 
-variable "telemetry_manager_hosted_runner_size" {
-  type        = string
-  default     = "4-core"
-  description = "Machine size for the hosted runner (e.g., 4-core, 8-core, 16-core)"
-}
+  default = {
+    name         = "telemetry-4core"
+    size         = "4-core"
+    max_runners  = 30
+    image_id     = "2306"
+    image_source = "github"
+  }
 
-variable "telemetry_manager_hosted_runner_max_runners" {
-  type        = number
-  default     = 30
-  description = "Maximum number of runners that can be scaled up simultaneously"
-}
-
-variable "telemetry_manager_hosted_runner_image_id" {
-  type        = string
-  default     = "2306"
-  description = "Image ID for the hosted runner (2306 = Ubuntu Latest 24.04)"
-}
-
-variable "telemetry_manager_hosted_runner_image_source" {
-  type        = string
-  default     = "github"
-  description = "Image source for the hosted runner (github, partner, or custom)"
+  description = "Configuration for the telemetry-manager GitHub-hosted larger runner"
 }
