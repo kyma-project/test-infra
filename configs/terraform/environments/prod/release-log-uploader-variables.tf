79a5129a34bb224e8a3b7d1afd94516da151c915
Patryk Dobrowolski <patryk.dobrowolski@sap.com>
2026-05-14 10:49:09 +0200

    feat(terraform): add WIF bindings for release-log-uploader SA
    Grant release-log-uploader@sap-kyma-prow.iam.gserviceaccount.com
    workloadIdentityUser role for workflows in kyma-project/compliancy
    (github.com) and kyma/compliancy (github.tools.sap).

---
+++ configs/terraform/environments/prod/release-log-uploader-variables.tf
@@ -0,0 +1,17 @@
+variable "release_log_uploader_service_account_name" {
+  type        = string
+  description = "The service account name of the release-log-uploader service account."
+  default     = "release-log-uploader"
+}
+
+variable "release_log_uploader_compliancy_workflow_ref_public_github" {
+  type        = string
+  description = "The value of GitHub OIDC token job_workflow_ref claim of the release log upload workflow in kyma-project/compliancy repository on github.com."
+  default     = "compliancy/.github/workflows/release-log-upload.yaml@refs/heads/main"
+}
+
+variable "release_log_uploader_compliancy_workflow_ref_internal_github" {
+  type        = string
+  description = "The value of GitHub OIDC token job_workflow_ref claim of the release log upload workflow in kyma/compliancy repository on github.tools.sap."
+  default     = "compliancy/.github/workflows/release-log-upload.yaml@refs/heads/main"
+}
---
+++ configs/terraform/environments/prod/release-log-uploader.tf
@@ -0,0 +1,16 @@
+resource "google_service_account" "release_log_uploader" {
+  account_id  = var.release_log_uploader_service_account_name
+  description = "Service account for release log upload workflow in kyma-project/compliancy and kyma/compliancy repositories."
+}
+
+resource "google_service_account_iam_member" "release_log_uploader_wif_public_github" {
+  service_account_id = google_service_account.release_log_uploader.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${module.gh_com_kyma_project_workload_identity_federation.pool_name}/attribute.reusable_workflow_ref/${data.github_organization.kyma_project.login}/${var.release_log_uploader_compliancy_workflow_ref_public_github}"
+}
+
+resource "google_service_account_iam_member" "release_log_uploader_wif_internal_github" {
+  service_account_id = google_service_account.release_log_uploader.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${local.internal_github_wif_pool_name}/attribute.reusable_workflow_ref/${data.github_organization.kyma_internal.login}/${var.release_log_uploader_compliancy_workflow_ref_internal_github}"
+}
