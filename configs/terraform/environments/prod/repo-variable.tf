7a5c5075ffe79d5e36dfca684d4c8ecf853ded86
Dawo9889 <dawid.gala@protonmail.com>
2026-08-13 13:00:48 +0200

    fix(iac): correct variable name format in repo-variable.tf

--- configs/terraform/environments/prod/repo-variable.tf
+++ configs/terraform/environments/prod/repo-variable.tf
@@ -1,6 +1,6 @@
 resource "github_actions_variable" "terraform_runner_github_app_secret_name" {
   provider      = github
   repository    = "test-infra"
-  variable_name = "testing-variable"
+  variable_name = "testing_variable"
   value         = "test-value"
 }
