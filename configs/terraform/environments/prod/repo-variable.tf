24e3b2078f02f864ae98c01ac9beb474b0337641
Dawo9889 <dawid.gala@protonmail.com>
2026-08-13 13:04:09 +0200

    fix(iac): use github.kyma_project provider alias in repo-variable.tf

--- configs/terraform/environments/prod/repo-variable.tf
+++ configs/terraform/environments/prod/repo-variable.tf
@@ -1,5 +1,5 @@
 resource "github_actions_variable" "terraform_runner_github_app_secret_name" {
-  provider      = github
+  provider      = github.kyma_project
   repository    = "test-infra"
   variable_name = "testing_variable"
   value         = "test-value"
